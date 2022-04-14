5c5831b0690628f076216b3135506fb341758f93
Caen De Silva <caen@desilva.se>
2022-04-14 23:09:10 +0200

    Rework server to proxy media files directly instead of copying

--- server.php
+++ server.php
@@ -1,10 +1,19 @@
 <?php
 
 /**
- * Laravel - A PHP Framework For Web Artisans
+ * Hyde Realtime Server
+ * 
+ * Important! This feature is experimental at best and is in no way intended to be used in production.
+ * Though, why you would want a dynamic web server to serve static HTML I don't know anyway.
+ * But again, this is just a local testing tool.
+ * 
+ * Please report any bugs -- and there will be bugs -- on GitHub!
+ * 
+ * @author Caen De Silva <caen@desilva.se>
+ * @package Hyde/RealtimeCompiler
+ * @license MIT
  *
- * @package  Laravel
- * @author   Taylor Otwell <taylorotwell@gmail.com>
+ * This file serves as an initial router and is based on the one from Laravel (license MIT) 
  */
 
 // @todo Handle routes ending in .html
@@ -12,22 +21,28 @@
 // @todo Cache compiled files
 
 define('PROXY_START', microtime(true));
-define('HYDE_PATH', realpath('../../../'));
+define('HYDE_PATH', realpath('../../'));
 
 $uri = urldecode(
   parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH)
 );
 
+// If uri ends in .html, strip it
+if (str_ends_with($uri, '.html')) {
+  $uri = substr($uri, 0, -5);
+}
+
 // This file allows us to emulate Apache's "mod_rewrite" functionality from the
 // built-in PHP web server. This provides a convenient way to test a Laravel
 // application without having installed a "real" web server software here.
-if ($uri !== '/' && file_exists(getcwd().'/public'.$uri)) {
-  return false;
-}
 
 // Bootstrap any application services.
 require 'vendor/autoload.php';
 
-// Serve the application.
+// If it is a media asset, proxy it directly without booting the entire RC
+if (str_starts_with($uri, '/media')) {
+    \Hyde\RealtimeCompiler\HydeRC::serveMedia(basename($uri));
+}
 
+// Serve the application.
 \Hyde\RealtimeCompiler\HydeRC::boot($uri);
--- src/HydeRC.php
+++ src/HydeRC.php
@@ -21,4 +21,44 @@ class HydeRC
     {
         return round((microtime(true) - PROXY_START) * 1000, 2);
     }
+
+    public static function serveMedia(string $basename)
+    {
+        // First check if file exists in the _site/media directory
+        $media_path = HYDE_PATH . '/_site/media/' . $basename;
+        if (file_exists($media_path)) {
+            return static::serveStatic($media_path);
+        }
+        // If not, check if file exists in the _media directory
+        $media_path = HYDE_PATH . '/_media/' . $basename;
+        if (file_exists($media_path)) {
+            return static::serveStatic($media_path);
+        }
+        // Send 404 header
+        header('HTTP/1.0 404 Not Found');
+        exit();
+    }
+
+    /** @internal */
+    private static function serveStatic(string $path)
+    {
+        header('Content-Type: ' . static::getStaticContentType($path));
+        header('Content-Length: ' . filesize($path));
+        readfile($path);
+        exit;
+    }
+
+    /** @internal */
+    private static function getStaticContentType(string $path)
+    {
+        if (str_ends_with($path, '.css')) {
+            return 'text/css';
+        }
+
+        if (str_ends_with($path, '.js')) {
+            return 'text/javascript';
+        }
+
+        return mime_content_type($path);
+    }
 }
