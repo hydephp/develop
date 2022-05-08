26dc47c20853d2cfc4641067e53415a0234e2804
Caen De Silva <caen@desilva.se>
2022-05-08 17:05:21 +0200

    Adds basic syntax highlighting to error output

--- src/Actions/CompilesSourceFile.php
+++ src/Actions/CompilesSourceFile.php
@@ -31,10 +31,10 @@ class CompilesSourceFile
     public function execute(): bool|string
     {
         Server::log('Compiler: Building page...');
-        $output =  shell_exec('php '.HYDE_PATH.'/hyde rebuild '.$this->path);
+        $output =  shell_exec('php '.HYDE_PATH.'/hyde rebuild '.$this->path.' --ansi');
         Server::log('Compiler: ' . $output);
 
-        return $this->catchExceptions($output) ?:
+        return $this->catchExceptions($output ?? 'No output received') ?:
             file_get_contents(HYDE_PATH . '/_site/'. $this->formatPathname());
     }
 
@@ -62,9 +62,44 @@ class CompilesSourceFile
         // Might be too general, can always add an array of FQSN exceptions
         if (strpos($output, 'Exception') !== false) {
             Server::log("Error: \033[0;31mException detected in output.\033[0m\n");
-            return "<h1>Error: Exception  detected in output.</h1>\n<pre>".e($output).'</pre>';
+            return $this->formatErrorHTML($output);
         }
 
         return false;
+    }
+
+    private function formatErrorHTML(string $output): string
+    {
+        $dictionary = array(
+            // Replace ANSI colors with HTML
+            "\033[33m" => '<span style="color:gold">',
+            "\033[32m" => '<span style="color:green">',
+            "\033[37m" => '<span style="color:lightgray">',
+            "\033[39;1m" => '<span style="color:white">',
+            "\033[41;1m" => '<span style="color:white;background-color:red;font-size:120%;">',
+            "\033[39m"   => '</span>' ,
+            "\033[39;22m"   => '</span>' ,
+            "\033[49;22m"   => '</span>' ,
+
+            // Basic syntax highlighting
+            '&lt;?php' => '<span style="color:lightblue">&lt;?php</span>',
+            '?&gt;' => '<span style="color:lightblue">?&gt;</span>',
+            'echo' => '<span style="color:lightpink">echo</span>',
+            '/&gt;' => '<span style="color:lightpink">/&gt;</span>',
+            '➜' => '<span style="color:orange">➜</span>',
+            '▕' => '<span style="color:lightgray">▕</span>',
+            '/**' => '<span style="color:gray">/**</span><span style="color:lightgray">',
+            '**/' => '</span><span style="color:gray">**/</span>',
+            '//' => '<span style="color:gray">//</span>',
+            '\\' => '<span style="opacity:0.75">\</span>',
+            '$' => '<span style="color:lightgreen">$</span>',
+            '(' => '<span style="color:gray">(</span>',
+            ')' => '<span style="color:gray">)</span>',
+                
+
+        );
+        $output = str_replace(array_keys($dictionary), $dictionary, e($output));
+
+        return '<h1>Error: Exception detected in output.</h1>'."\n\n".'<pre style="background:black;color:white;font-size:14px;font-family:monospace;padding:16px;width:fit-content;">'.($output).'</pre>';
     }
 }
