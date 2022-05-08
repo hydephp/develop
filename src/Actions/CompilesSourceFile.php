c50063bc8be1d7a9b1b537096dcd88c3a370871b
Caen De Silva <caen@desilva.se>
2022-05-08 15:13:42 +0200

    Add basic error handling, fix #3

--- src/Actions/CompilesSourceFile.php
+++ src/Actions/CompilesSourceFile.php
@@ -34,7 +34,8 @@ class CompilesSourceFile
         $output =  shell_exec('php '.HYDE_PATH.'/hyde rebuild '.$this->path);
         Server::log('Compiler: ' . $output);
 
-        return file_get_contents(HYDE_PATH . '/_site/'. $this->formatPathname());
+        return $this->catchExceptions($output) ?:
+            file_get_contents(HYDE_PATH . '/_site/'. $this->formatPathname());
     }
 
     /**
@@ -51,5 +52,19 @@ class CompilesSourceFile
         $filename = str_replace('.md', '.html', $filename);
 
         return $filename;
+    }
+
+    /**
+     * (Try to) Catch any exceptions, otherwise return false if it's safe to proceed.
+     */
+    private function catchExceptions(string $output): string|false
+    {
+        // Might be too general, can always add an array of FQSN exceptions
+        if (strpos($output, 'Exception') !== false) {
+            Server::log("Error: \033[0;31mException detected in output.\033[0m\n");
+            return "<h1>Error: Exception  detected in output.</h1>\n<pre>".e($output).'</pre>';
+        }
+
+        return false;
     }
 }
