52fb9ec4da7b378503c46be4b59dd7d2dce20abd
Caen De Silva <caen@desilva.se>
2022-06-04 11:25:52 +0200

    Register and boot the HttpKernel

--- bin/server.php
+++ bin/server.php
@@ -4,3 +4,6 @@ define('BASE_PATH', realpath(getcwd()));
 define('HYDE_START', microtime(true));
 
 require_once sprintf("%s/vendor/autoload.php", BASE_PATH);
+
+$app = \Desilva\Microserve\Microserve::boot(Hyde\RealtimeCompiler\Http\HttpKernel::class);
+$app->handle();
