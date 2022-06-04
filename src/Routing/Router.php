d91beb7e79285ec8c17f0f24e4a86002d14d1591
Caen De Silva <caen@desilva.se>
2022-06-04 11:30:18 +0200

    Implement core router scaffolding

--- src/Routing/Router.php
+++ src/Routing/Router.php
@@ -2,7 +2,20 @@
 
 namespace Hyde\RealtimeCompiler\Routing;
 
+use Desilva\Microserve\Request;
+use Desilva\Microserve\Response;
+
 class Router
 {
-    
+    protected Request $request;
+
+    public function __construct(Request $request)
+    {
+        $this->request = $request;
+    }
+
+    public function handle(): Response
+    {
+        return Response::make(501, 'Not Implemented');
+    }
 }
