e52affb8c4baa93e547ed360a7fbe36a7159c65f
Caen De Silva <caen@desilva.se>
2022-06-04 12:33:44 +0200

    Add Whoops exception handler

--- bin/server.php
+++ bin/server.php
@@ -5,6 +5,11 @@ define('HYDE_START', microtime(true));
 
 require_once sprintf("%s/vendor/autoload.php", BASE_PATH);
 
-$app = \Desilva\Microserve\Microserve::boot(Hyde\RealtimeCompiler\Http\HttpKernel::class);
-$app->handle() // Process the request and create the response
-    ->send(); // Send the response to the client
+try {
+    $app = \Desilva\Microserve\Microserve::boot(\Hyde\RealtimeCompiler\Http\HttpKernel::class);
+    $app->handle() // Process the request and create the response
+        ->send(); // Send the response to the client
+} catch (Throwable $exception) {
+    \Hyde\RealtimeCompiler\Http\ExceptionHandler::handle($exception);
+    exit($exception->getCode());
+}
