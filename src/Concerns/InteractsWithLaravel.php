0cd12101df0dee3968aade2f49dbd38e4e4c811a
Caen De Silva <caen@desilva.se>
2022-06-04 12:54:46 +0200

    Create and boot the Laravel application instance

--- src/Concerns/InteractsWithLaravel.php
+++ src/Concerns/InteractsWithLaravel.php
@@ -2,7 +2,24 @@
 
 namespace Hyde\RealtimeCompiler\Concerns;
 
+use Illuminate\Contracts\Console\Kernel;
+use Illuminate\Foundation\Application;
+
 trait InteractsWithLaravel
 {
+    protected Application $laravel;
 
+    protected function createApplication(): void
+    {
+        $this->laravel = require_once BASE_PATH . '/bootstrap/app.php';
+    }
+
+    protected function bootApplication(): void
+    {
+        if (! isset($this->laravel))  {
+            $this->createApplication();
+        }
+
+        $this->laravel->make(Kernel::class)->bootstrap();
+    }
 }
