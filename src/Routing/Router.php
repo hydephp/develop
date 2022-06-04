4d39701e4612157598d2d56586a2483936dc3e18
Caen De Silva <caen@desilva.se>
2022-06-04 11:32:18 +0200

    Create shouldProxy() helper
    If the request is not for a web page, we assume it's a static asset, which we instead want to proxy.

--- src/Routing/Router.php
+++ src/Routing/Router.php
@@ -18,4 +18,23 @@ class Router
     {
         return Response::make(501, 'Not Implemented');
     }
+
+    /**
+     * If the request is not for a web page, we assume it's
+     * a static asset, which we instead want to proxy.
+     */
+    protected function shouldProxy(Request $request): bool
+    {
+        if (str_starts_with($request->path, '/media/')) {
+            return true;
+        }
+
+        $extension = pathinfo($request->path)['extension'] ?? null;
+
+        if ($extension === null || $extension === 'html') {
+            return false;
+        }
+
+        return true;
+    }
 }
