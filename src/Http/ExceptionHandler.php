4fa99335190e3adbd5fda6f06aa9e3530b45fd6a
Caen De Silva <caen@desilva.se>
2022-06-04 12:32:39 +0200

    Create ExceptionHandler.php

---
+++ src/Http/ExceptionHandler.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Hyde\RealtimeCompiler\Http;
+
+use Desilva\Microserve\Response;
+
+class ExceptionHandler
+{
+    public static function handle(\Throwable $exception): Response
+    {
+        $whoops = new \Whoops\Run;
+        $whoops->allowQuit(false);
+        $whoops->writeToOutput(false);
+        $whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
+        $html = $whoops->handleException($exception);
+
+        return Response::make(500, 'Internal Server Error', [
+            'Content-Type' => 'text/html',
+            'Content-Length' => strlen($html),
+            'body' => $html,
+        ]);
+    }
+}
