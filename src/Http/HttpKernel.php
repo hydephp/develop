658acb5255da609e11f23e5fed851fac3a55deb7
Caen De Silva <caen@desilva.se>
2022-06-04 11:30:25 +0200

    Register the router

--- src/Http/HttpKernel.php
+++ src/Http/HttpKernel.php
@@ -3,8 +3,14 @@
 namespace Hyde\RealtimeCompiler\Http;
 
 use Desilva\Microserve\HttpKernel as BaseHttpKernel;
+use Desilva\Microserve\Request;
+use Desilva\Microserve\Response;
+use Hyde\RealtimeCompiler\Routing\Router;
 
 class HttpKernel extends BaseHttpKernel
 {
-
+    public function handle(Request $request): Response
+    {
+        return (new Router($request))->handle();
+    }
 }
