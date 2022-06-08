e68a1fa5c98003a60a8148bee98e76ecb678eff6
Caen De Silva <caen@desilva.se>
2022-06-08 22:01:33 +0200

    Add deprecation notice

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,10 @@
 <?php
 
+/**
+ * @deprecated v0.35.x-dev - This file has moved to app/bootstrap.php,
+ *                           and will be removed in a future release.
+ */
+
 /*
 |--------------------------------------------------------------------------
 | Create The Application
