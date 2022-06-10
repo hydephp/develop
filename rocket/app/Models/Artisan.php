e44a6bf69394035464fa59a12f1a72dfdafd4201
Caen De Silva <caen@desilva.se>
2022-06-10 17:37:06 +0200

    Rocket: Create the Artisan proxy

--- rocket/app/Models/Artisan.php
+++ rocket/app/Models/Artisan.php
@@ -7,5 +7,26 @@ namespace App\Models;
  */
 class Artisan
 {
+    protected string $path;
 
+    public function __construct(string $absoluteProjectPath)
+    {
+        $this->path = $absoluteProjectPath;
+    }
+
+    /**
+     * Run a command in the project's HydeCLI and return the output.
+     */
+    public function run(string $command): string
+    {
+        return shell_exec('cd '.$this->path.' && php hyde ' . $command);
+    }
+
+    /**
+     * Run a command in the project's HydeCLI and stream the output.
+     */
+    public function passthru(string $command): void
+    {
+        passthru('cd '.$this->path.' && php hyde ' . $command);
+    }
 }
