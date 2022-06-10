f89556b907a82fbee8b8ee96ef5b77df50f6e95a
Caen De Silva <caen@desilva.se>
2022-06-10 13:54:47 +0200

    Rocket: Fix wrong class metadata location

--- rocket/resources/views/partials/layout.blade.php
+++ rocket/resources/views/partials/layout.blade.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
-<html lang="en">
-<head @class(['windows' => (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN')])>
+<html lang="en" @class(['windows' => (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN')])>
+<head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
