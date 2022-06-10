ed5521d7ad131533858377b18abb1632b92b9088
Caen De Silva <caen@desilva.se>
2022-06-10 13:50:57 +0200

    Rocket: Format code

--- rocket/resources/views/dashboard.blade.php
+++ rocket/resources/views/dashboard.blade.php
@@ -15,26 +15,27 @@
                 Project Information
             </caption>
             <thead>
-            <tr>
-                <th>Project Name</th>
-                <th>Project Path</th>
-                <th>Hyde Version</th>
-                <th colspan="1" class="windows">Open project directory in</th>
-            </tr>
+                <tr>
+                    <th>Project Name</th>
+                    <th>Project Path</th>
+                    <th>Hyde Version</th>
+                    <th colspan="1" class="windows">Open project directory in</th>
+                </tr>
             </thead>
             <tbody>
-            <tr>
-                <td>{{$project->name}}</td>
-                <td>{{$project->path}}</td>
-                <td>{{$project->hyde()->version()}}</td>
-                <td class="windows">
-                    <form action="/fileapi/open" method="POST">
-                        <input type="hidden" name="path" value="">
-                        <input type="hidden" name="back" value="{{request()->path()}}">
-                        <button type="submit">Windows Explorer</button>
-                    </form>
-                </td>
-            </tr>
+                <tr>
+                    <td>{{$project->name}}</td>
+                    <td>{{$project->path}}</td>
+                    <td>{{$project->hyde()->version()}}</td>
+                    <td class="windows">
+                        <form action="/fileapi/open" method="POST">
+                            <input type="hidden" name="path" value="">
+                            <input type="hidden" name="back" value="{{request()->path()}}">
+                            <button type="submit">Windows Explorer</button>
+                        </form>
+                    </td>
+                </tr>
             </tbody>
         </table>
+    </section>
 @endsection
