ca2a5210b38b8ed492563a6eae19aeeb0fee0fa7
Caen De Silva <caen@desilva.se>
2022-06-10 11:56:13 +0200

    Rocket: Show the application dashboard

--- rocket/app/Http/Controllers/DashboardController.php
+++ rocket/app/Http/Controllers/DashboardController.php
@@ -14,5 +14,13 @@ class DashboardController extends Controller
         //
     }
 
-    //
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('dashboard');
+    }
 }
