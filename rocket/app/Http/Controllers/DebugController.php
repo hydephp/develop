4aca3fec491743bc66ec64fd5fe6ec07e8adb831
Caen De Silva <caen@desilva.se>
2022-06-10 17:37:33 +0200

    Rocket: Output HydeCLI debug screen

--- rocket/app/Http/Controllers/DebugController.php
+++ rocket/app/Http/Controllers/DebugController.php
@@ -2,12 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Project;
 use Hyde\Framework\Hyde;
 
 class DebugController extends Controller
 {
     public function __invoke()
     {
+        ob_get_clean();
+
         echo '<pre>';
 
         echo '<h1>Hyde Rocket Debug Screen</h1>';
@@ -19,7 +22,9 @@ class DebugController extends Controller
             'PHP version' => phpversion() . ' (' . PHP_SAPI . ')',
             'Lumen version' => app()->version(),
         ];
-
         dump($information);
+
+        echo '<h2>HydePHP Debug Information</h2>';
+        Project::get('artisan')->passthru('debug');
     }
 }
