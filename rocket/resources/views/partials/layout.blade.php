7cdda61c3631daf421433ca83fb581d654c7f049
Caen De Silva <caen@desilva.se>
2022-06-10 13:25:23 +0200

    Rocket: Detect server operating system

--- rocket/resources/views/partials/layout.blade.php
+++ rocket/resources/views/partials/layout.blade.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
+<head @class(['windows' => (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN')])>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
