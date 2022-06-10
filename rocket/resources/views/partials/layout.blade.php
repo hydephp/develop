34c9ad87d96a2e42927956785a3968ccdc1e11d8
Caen De Silva <caen@desilva.se>
2022-06-10 13:29:12 +0200

    Rocket: Create app.scss
    Create app.css
    
    Create app.css.map
    
    Update layout.blade.php

--- rocket/resources/views/partials/layout.blade.php
+++ rocket/resources/views/partials/layout.blade.php
@@ -5,6 +5,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
 	<title>Hyde Rocket - @yield('title', 'Dashboard')</title>
+    <link rel="stylesheet" href="/app.css">
 </head>
 <body>
 	<main>
