8e55c86dc4389fdda804d86633ed061a5b823bcf
Caen De Silva <caen@desilva.se>
2022-06-11 08:16:07 +0200

    Rocket: Create ApiController.php

---
+++ rocket/app/Http/Controllers/ApiController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Hyde\Rocket\Http\Controllers;
+
+/**
+ * Controller for general API requests that don't currently require their own controller.
+ */
+class ApiController extends Controller
+{
+    public function index()
+    {
+        //
+    }
+}
