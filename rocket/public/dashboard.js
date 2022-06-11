2a7a88910596beff2e325bcee5bf63bd935a3579
Caen De Silva <caen@desilva.se>
2022-06-11 09:00:05 +0200

    Rocket: Better handle the HydeRC state

--- rocket/public/dashboard.js
+++ rocket/public/dashboard.js
@@ -1,3 +1,5 @@
+let HydeRCState = 1; // 0 = offline, 1 = pinging/unknown, 2 = online
+
 document.addEventListener('DOMContentLoaded', function () {
 	pingRC();
 	setInterval(pingRC, 30000);
@@ -8,15 +10,30 @@ function pingRC() {
 	.then(response => response.json())
 	.then(data => {
 		if (data.success === true) {
-			document.querySelectorAll('.needs-realtime-compiler').forEach(function (button) {
-				button.disabled = false;
-				button.setAttribute('title', 'View with Realtime Compiler');
-			});
+			HydeRCState = 2;
+			handleRCStateChange();
 		} else {
-			document.querySelectorAll('.needs-realtime-compiler').forEach(function (button) {
-				button.disabled = true;
-				button.setAttribute('title', 'Realtime Compiler is not running');
-			});
+			HydeRCState = 0;
+			handleRCStateChange();
 		}
 	});
+}
+
+function handleRCStateChange() {
+	if (HydeRCState === 0) {
+		document.querySelectorAll('.needs-realtime-compiler').forEach(function (button) {
+			button.disabled = true;
+			button.setAttribute('title', 'Realtime Compiler is not running');
+		});
+	} else if (HydeRCState === 2) {
+		document.querySelectorAll('.needs-realtime-compiler').forEach(function (button) {
+			button.disabled = false;
+			button.setAttribute('title', 'View with Realtime Compiler');
+		});
+	} else {
+		document.querySelectorAll('.needs-realtime-compiler').forEach(function (button) {
+			button.disabled = true;
+			button.setAttribute('title', 'Pinging Realtime Compiler...');
+		});
+	}	
 }
