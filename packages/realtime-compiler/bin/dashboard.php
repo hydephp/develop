6ea801aa59b7b08ffa651e6e558bd7589fa606f4
Caen De Silva <caen@desilva.se>
2022-06-12 14:39:56 +0200

    Set the frontend app name

--- packages/realtime-compiler/bin/dashboard.php
+++ packages/realtime-compiler/bin/dashboard.php
@@ -22,9 +22,6 @@ $kernel = $app->make(Illuminate\Contracts\Console\Kernel::class);
 $kernel->bootstrap();
 \Hyde\Framework\Hyde::setBasePath(BASE_PATH);
 
-// Get the request page
-$page = isset($_GET['page']) ? $_GET['page'] : 'index';
-
 
 // Create the Hyde interface
 $hyde = new class() extends Hyde\Framework\Hyde {};
@@ -42,6 +39,11 @@ $project = new class
 	}
 };
 
+// Get the request page
+$page = isset($_GET['page']) ? $_GET['page'] : 'index';
+
+// Set the app name
+$appname = e($project->name) . ' CMS';
 ?>
 
 <!DOCTYPE html>
@@ -52,7 +54,7 @@ $project = new class
 <meta name="description" content="" />
 <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors" />
 <!-- Created with the Bootstrap Dashboard Template https://getbootstrap.com/docs/5.1/examples/dashboard/ -->
-<title>HydeCMS - <?= e($project->name) ?></title>
+<title><?= $appname ?></title>
 <!-- Bootstrap core CSS -->
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" />
 <!-- Dashboard CSS -->
@@ -169,7 +171,7 @@ body {
 <body>
    <header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
       <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="#">
-      <?=e($project->name) ?>
+         <?= $appname ?>
       </a>
       <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
