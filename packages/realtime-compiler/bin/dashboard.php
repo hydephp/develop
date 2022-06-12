6e6126b3d32bf34508bfa47875740b01cc811043
Caen De Silva <caen@desilva.se>
2022-06-12 13:22:01 +0200

    Add the project information table

--- packages/realtime-compiler/bin/dashboard.php
+++ packages/realtime-compiler/bin/dashboard.php
@@ -35,12 +35,19 @@ $project = new class
 {
     public string $path;
     public string $name;
+	public string $version;
 
     public function __construct()
     {
         $this->path = BASE_PATH;
         $this->name = ucwords(str_replace('-', ' ', basename(BASE_PATH)));
+		$this->version = $this->getVersion();
     }
+
+	protected function getVersion(): string
+	{
+		return json_decode(file_get_contents($this->path . '/composer.json'))->require->{'hyde/framework'};
+	}
 };
 ?>
 
@@ -263,131 +270,23 @@ body {
                </div>
             </div>
             <canvas class="my-4 w-100" id="myChart" width="900" height="380"></canvas>
-            <h2>Section title</h2>
+			
+            <h2>
+				Project Information
+			</h2>
             <div class="table-responsive">
                <table class="table table-striped table-sm">
                   <thead>
                      <tr>
-                        <th scope="col">#</th>
-                        <th scope="col">Header</th>
-                        <th scope="col">Header</th>
-                        <th scope="col">Header</th>
-                        <th scope="col">Header</th>
+						<th scope="col">Project Name</th>
+						<th scope="col">Project Path</th>
+						<th scope="col">Hyde Version</th>
                      </tr>
                   </thead>
                   <tbody>
-                     <tr>
-                        <td>1,001</td>
-                        <td>random</td>
-                        <td>data</td>
-                        <td>placeholder</td>
-                        <td>text</td>
-                     </tr>
-                     <tr>
-                        <td>1,002</td>
-                        <td>placeholder</td>
-                        <td>irrelevant</td>
-                        <td>visual</td>
-                        <td>layout</td>
-                     </tr>
-                     <tr>
-                        <td>1,003</td>
-                        <td>data</td>
-                        <td>rich</td>
-                        <td>dashboard</td>
-                        <td>tabular</td>
-                     </tr>
-                     <tr>
-                        <td>1,003</td>
-                        <td>information</td>
-                        <td>placeholder</td>
-                        <td>illustrative</td>
-                        <td>data</td>
-                     </tr>
-                     <tr>
-                        <td>1,004</td>
-                        <td>text</td>
-                        <td>random</td>
-                        <td>layout</td>
-                        <td>dashboard</td>
-                     </tr>
-                     <tr>
-                        <td>1,005</td>
-                        <td>dashboard</td>
-                        <td>irrelevant</td>
-                        <td>text</td>
-                        <td>placeholder</td>
-                     </tr>
-                     <tr>
-                        <td>1,006</td>
-                        <td>dashboard</td>
-                        <td>illustrative</td>
-                        <td>rich</td>
-                        <td>data</td>
-                     </tr>
-                     <tr>
-                        <td>1,007</td>
-                        <td>placeholder</td>
-                        <td>tabular</td>
-                        <td>information</td>
-                        <td>irrelevant</td>
-                     </tr>
-                     <tr>
-                        <td>1,008</td>
-                        <td>random</td>
-                        <td>data</td>
-                        <td>placeholder</td>
-                        <td>text</td>
-                     </tr>
-                     <tr>
-                        <td>1,009</td>
-                        <td>placeholder</td>
-                        <td>irrelevant</td>
-                        <td>visual</td>
-                        <td>layout</td>
-                     </tr>
-                     <tr>
-                        <td>1,010</td>
-                        <td>data</td>
-                        <td>rich</td>
-                        <td>dashboard</td>
-                        <td>tabular</td>
-                     </tr>
-                     <tr>
-                        <td>1,011</td>
-                        <td>information</td>
-                        <td>placeholder</td>
-                        <td>illustrative</td>
-                        <td>data</td>
-                     </tr>
-                     <tr>
-                        <td>1,012</td>
-                        <td>text</td>
-                        <td>placeholder</td>
-                        <td>layout</td>
-                        <td>dashboard</td>
-                     </tr>
-                     <tr>
-                        <td>1,013</td>
-                        <td>dashboard</td>
-                        <td>irrelevant</td>
-                        <td>text</td>
-                        <td>visual</td>
-                     </tr>
-                     <tr>
-                        <td>1,014</td>
-                        <td>dashboard</td>
-                        <td>illustrative</td>
-                        <td>rich</td>
-                        <td>data</td>
-                     </tr>
-                     <tr>
-                        <td>1,015</td>
-                        <td>random</td>
-                        <td>tabular</td>
-                        <td>information</td>
-                        <td>text</td>
-                     </tr>
+					<td><?= $project->name ?></td>
+					<td><?= $project->path ?></td>
+					<td><?= $project->version ?></td>
                   </tbody>
                </table>
             </div>
