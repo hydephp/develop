f488e87eb695419491759512c59cdd0ebb268c11
Caen De Silva <caen@desilva.se>
2022-06-12 14:09:17 +0200

    Change logout link to lead back to site

--- packages/realtime-compiler/bin/dashboard.php
+++ packages/realtime-compiler/bin/dashboard.php
@@ -173,7 +173,7 @@ body {
       <input class="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search" />
       <div class="navbar-nav">
          <div class="nav-item text-nowrap">
-            <a class="nav-link px-3" href="#">Sign out</a>
+            <a class="nav-link px-3" href="../">Back to site</a>
          </div>
       </div>
    </header>
