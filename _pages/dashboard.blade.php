b4c6f949ba9831a15a833217c40d6277ff2897e6
Caen De Silva <caen@desilva.se>
2022-06-12 22:47:58 +0200

    Update header

--- _pages/dashboard.blade.php
+++ _pages/dashboard.blade.php
@@ -3,7 +3,19 @@
 @php($title = "Dashboard")
 
 <main class="mx-auto max-w-7xl py-16 px-8">
-	<h1 class="text-center text-3xl font-bold">Dashboard</h1>
+	<header class="text-center prose dark:prose-invert mx-auto">
+		<h1 class="text-3xl font-bold">Project Dashboard</h1>
+		<p>
+			<strong>
+				Here you can get a quick overview of your project.
+			</strong>
+		</p>
+		<p>
+			While this is useful when developing locally,
+			you may not want to use it when compiling
+			for production.
+		</p>
+	</header>
 </main>
 
 @endsection
