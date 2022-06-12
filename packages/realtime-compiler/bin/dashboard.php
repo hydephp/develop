44e0fe4e1635a3e338e8880ebd8d67c37d15a29f
Caen De Silva <caen@desilva.se>
2022-06-12 12:37:20 +0200

    Abort if user is not localhost

--- packages/realtime-compiler/bin/dashboard.php
+++ packages/realtime-compiler/bin/dashboard.php
@@ -1,3 +1,12 @@
+<?php
+	if ($_SERVER['REMOTE_ADDR'] !== '::1') {
+		header('HTTP/1.1 403 Forbidden');
+		echo '<h1>HTTP/1.1 403 - Access Denied</h1>';
+		echo '<p>You must be on localhost to access this page. Refusing to serve request.</p>';
+		exit;
+	}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
