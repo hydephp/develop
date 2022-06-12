2ce5047c15d2b522dd12e75c80fc5fbd82b7790a
Caen De Silva <caen@desilva.se>
2022-06-12 15:16:23 +0200

    Use the page name for the title

--- packages/realtime-compiler/bin/dashboard.php
+++ packages/realtime-compiler/bin/dashboard.php
@@ -75,7 +75,7 @@ $pagename = $appname .' - '. $routes[$page];
 <meta name="description" content="" />
 <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors" />
 <!-- Created with the Bootstrap Dashboard Template https://getbootstrap.com/docs/5.1/examples/dashboard/ -->
-<title><?= $appname ?></title>
+<title><?= $pagename ?></title>
 <!-- Bootstrap core CSS -->
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" />
 <!-- Dashboard CSS -->
