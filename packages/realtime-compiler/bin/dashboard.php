d4857c0bb36dc3c389712099369ce0914d0ab4c2
Caen De Silva <caen@desilva.se>
2022-06-12 16:26:51 +0200

    Add extra padding to main element on large screens

--- packages/realtime-compiler/bin/dashboard.php
+++ packages/realtime-compiler/bin/dashboard.php
@@ -187,7 +187,7 @@ $pagename = $appname .' - '. $routes[$page];
                </ul>
             </div>
          </nav>
-         <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+         <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4 px-xl-5 pt-xl-3">
             <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
                <h1 class="h2"><?= $pagename ?></h1>
             </div>
