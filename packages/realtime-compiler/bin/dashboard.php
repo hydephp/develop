43bc9e98040391036149181dfe2e3dafa69d9fd7
Caen De Silva <caen@desilva.se>
2022-06-12 16:25:01 +0200

    Create functional sidebar link to dashboard

--- packages/realtime-compiler/bin/dashboard.php
+++ packages/realtime-compiler/bin/dashboard.php
@@ -117,9 +117,9 @@ $pagename = $appname .' - '. $routes[$page];
             <div class="position-sticky pt-3">
                <ul class="nav flex-column">
                   <li class="nav-item">
-                     <a class="nav-link active" aria-current="page" href="#">
-                     <span data-feather="home"></span>
-                     Dashboard
+                     <a <?= $page === 'index' ? 'class="nav-link active" aria-current="page"' : 'class="nav-link"' ?>" href="dashboard.php">
+                        <span data-feather="home"></span>
+                        Dashboard
                      </a>
                   </li>
                   <li class="nav-item">
