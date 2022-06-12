90658c6c6eb67b7d0be2b93f8b2b277f00070377
Caen De Silva <caen@desilva.se>
2022-06-12 16:25:32 +0200

    Link navbar brand to dashboard

--- packages/realtime-compiler/bin/dashboard.php
+++ packages/realtime-compiler/bin/dashboard.php
@@ -99,7 +99,7 @@ $pagename = $appname .' - '. $routes[$page];
 </head>
 <body>
    <header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
-      <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="#">
+      <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="dashboard.php">
          <?= $appname ?>
       </a>
       <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
