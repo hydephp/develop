27a7c525218d062e894c23a94eaa0d3e50bcb312
Caen De Silva <caen@desilva.se>
2022-06-12 13:12:40 +0200

    Remove favicons

--- packages/realtime-compiler/bin/dashboard.php
+++ packages/realtime-compiler/bin/dashboard.php
@@ -138,13 +138,6 @@ body {
 	box-shadow: 0 0 0 3px rgba(255, 255, 255, .25);
 }
 </style>
-<!-- Favicons -->
-<link rel="apple-touch-icon" href="/docs/5.1/assets/img/favicons/apple-touch-icon.png" sizes="180x180" />
-<link rel="icon" href="/docs/5.1/assets/img/favicons/favicon-32x32.png" sizes="32x32" type="image/png" />
-<link rel="icon" href="/docs/5.1/assets/img/favicons/favicon-16x16.png" sizes="16x16" type="image/png" />
-<link rel="manifest" href="/docs/5.1/assets/img/favicons/manifest.json" />
-<link rel="mask-icon" href="/docs/5.1/assets/img/favicons/safari-pinned-tab.svg" color="#7952b3" />
-<link rel="icon" href="/docs/5.1/assets/img/favicons/favicon.ico" />
 <meta name="theme-color" content="#7952b3" />
 <style>
 .bd-placeholder-img {
