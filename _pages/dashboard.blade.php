75aec77dc2aad404db8ed8f916f8f3301617fca7
Caen De Silva <caen@desilva.se>
2022-06-12 22:39:13 +0200

    Create dashboard.blade.php

---
+++ _pages/dashboard.blade.php
@@ -0,0 +1,9 @@
+@extends('hyde::layouts.app')
+@section('content')
+@php($title = "Dashboard")
+
+<main class="mx-auto max-w-7xl py-16 px-8">
+	<h1 class="text-center text-3xl font-bold">Dashboard</h1>
+</main>
+
+@endsection
