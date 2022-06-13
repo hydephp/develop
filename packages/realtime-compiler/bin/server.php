41cf010446ea4a1402e77b8ebd5b183e094abef1
Caen De Silva <caen@desilva.se>
2022-06-13 15:52:51 +0200

    Remove dashboard.php re-router

--- packages/realtime-compiler/bin/server.php
+++ packages/realtime-compiler/bin/server.php
@@ -3,12 +3,6 @@
 define('BASE_PATH', realpath(getcwd()));
 define('HYDE_START', microtime(true));
 
-// If request path is dashboard.php then load that file instead
-if (isset($_SERVER['REQUEST_URI']) && str_starts_with($_SERVER['REQUEST_URI'], '/dashboard.php')) {
-	require_once __DIR__ . '/dashboard.php';
-	exit;
-}
-
 require_once sprintf('%s/vendor/autoload.php', BASE_PATH);
 
 try {
