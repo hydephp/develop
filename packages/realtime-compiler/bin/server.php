7718fcdab48de8b39ef836e9619227b560346901
Caen De Silva <caen@desilva.se>
2022-06-13 16:08:53 +0200

    Add auxiliary exception handler

--- packages/realtime-compiler/bin/server.php
+++ packages/realtime-compiler/bin/server.php
@@ -1,15 +1,23 @@
 <?php
 
-define('BASE_PATH', realpath(getcwd()));
-define('HYDE_START', microtime(true));
-
-require_once sprintf('%s/vendor/autoload.php', BASE_PATH);
-
 try {
-    $app = \Desilva\Microserve\Microserve::boot(\Hyde\RealtimeCompiler\Http\HttpKernel::class);
-    $app->handle() // Process the request and create the response
-        ->send(); // Send the response to the client
-} catch (Throwable $exception) {
-    \Hyde\RealtimeCompiler\Http\ExceptionHandler::handle($exception);
-    exit($exception->getCode());
+    define('BASE_PATH', realpath(getcwd()));
+    define('HYDE_START', microtime(true));
+
+    require_once sprintf('%s/vendor/autoload.php', BASE_PATH);
+
+    try {
+        $app = \Desilva\Microserve\Microserve::boot(\Hyde\RealtimeCompiler\Http\HttpKernel::class);
+        $app->handle() // Process the request and create the response
+            ->send(); // Send the response to the client
+    } catch (Throwable $exception) {
+        \Hyde\RealtimeCompiler\Http\ExceptionHandler::handle($exception);
+        exit($exception->getCode());
+    }
+} catch (\Throwable $th) {
+    // Auxiliary exception handler
+    echo '<h1>Something went really wrong!</h1>';
+    echo '<p>An error occurred that the core exception handler failed to process. Here\'s all we know:</p>';
+    echo '<h2>Initial exception:</h2><pre>' . print_r($exception, true).'</pre>';
+    echo '<h2>Auxiliary exception:</h2><pre>' . print_r($th, true).'</pre>';
 }
