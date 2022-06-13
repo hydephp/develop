1a724f6d76f89f9d0fd126e60ec79c8ef9358094
Caen De Silva <caen@desilva.se>
2022-06-13 12:02:00 +0200

    Place content overview tables in section

--- _pages/dashboard.blade.php
+++ _pages/dashboard.blade.php
@@ -17,108 +17,114 @@
 		</p>
 	</header>
 
-	<section class="prose dark:prose-invert mx-auto mt-8">
-		<h2>Blade Pages</h2>
-		<table>
-			<thead>
-				<tr>
-					<th>Title</th>
-					<th>Source File</th>
-				</tr>
-			</thead>
-			<tbody>
-				@foreach (\Hyde\Framework\Models\BladePage::all() as $page)
-				<tr>
-                    <td>
-						<a href="{{ Hyde::pageLink($page->slug . '.html') }}">
-                            {{ Hyde::titleFromSlug($page->view) }}
-						</a>
-					</td>
-                    <td>
-                        {{ \Hyde\Framework\Models\BladePage::$sourceDirectory .'/'. $page->slug . \Hyde\Framework\Models\BladePage::$fileExtension }}
-                    </td>
-				</tr>
-				@endforeach
-			</tbody>
-		</table>
-	</section>
+    <section class="prose dark:prose-invert mx-auto mt-8">
+        <header>
+            <h2>Content Overview</h2>
+        </header>
 
-	<section class="prose dark:prose-invert mx-auto mt-8">
-		<h2>Markdown Pages</h2>
-		<table>
-			<thead>
-				<tr>
-					<th>Title</th>
-					<th>Source File</th>
-				</tr>
-			</thead>
-			<tbody>
-				@foreach (\Hyde\Framework\Models\MarkdownPage::all() as $page)
-				<tr>
-                    <td>
-						<a href="{{ Hyde::pageLink($page->slug . '.html') }}">
-							{{ $page->title }}
-						</a>
-					</td>
-                    <td>
-                        {{ \Hyde\Framework\Models\MarkdownPage::$sourceDirectory .'/'. $page->slug . \Hyde\Framework\Models\MarkdownPage::$fileExtension }}
-                    </td>
-				</tr>
-				@endforeach
-			</tbody>
-		</table>
-	</section>
-
-	<section class="prose dark:prose-invert mx-auto mt-8">
-		<h2>Documentation Pages</h2>
-		<table>
-			<thead>
-				<tr>
-					<th>Title</th>
-					<th>Source File</th>
-				</tr>
-			</thead>
-			<tbody>
-				@foreach (\Hyde\Framework\Models\DocumentationPage::all() as $page)
-				<tr>
-					<td>
-						<a href="{{ Hyde::docsDirectory() .'/'. Hyde::pageLink($page->slug . '.html') }}">
-							{{ $page->title }}
-						</a>
-					</td>
-                    <td>
-                        {{ \Hyde\Framework\Models\DocumentationPage::$sourceDirectory .'/'. $page->slug . \Hyde\Framework\Models\DocumentationPage::$fileExtension }}
-                    </td>
-				</tr>
-				@endforeach
-			</tbody>
-		</table>
-	</section>
-
-	<section class="prose dark:prose-invert mx-auto mt-8">
-		<h2>Blog Posts</h2>
-		<table>
-			<thead>
-				<tr>
-					<th>Title</th>
-					<th>Source File</th>
-				</tr>
-			</thead>
-			<tbody>
-				@foreach (\Hyde\Framework\Models\MarkdownPost::all() as $post)
-				<tr>
-					<td>
-						<a href="posts/{{ Hyde::pageLink($post->slug . '.html') }}">
-							{{ $post->title }}
-						</a>
-					</td>
-					<td>
-                        {{ \Hyde\Framework\Models\MarkdownPost::$sourceDirectory .'/'. $post->slug . \Hyde\Framework\Models\MarkdownPost::$fileExtension }}
-                    </td>
-				</tr>
-				@endforeach
-			</tbody>
-		</table>
-	</section>
+        <section class="prose dark:prose-invert mx-auto mt-8">
+            <h3>Blade Pages</h3>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Title</th>
+                        <th>Source File</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach (\Hyde\Framework\Models\BladePage::all() as $page)
+                    <tr>
+                        <td>
+                            <a href="{{ Hyde::pageLink($page->slug . '.html') }}">
+                                {{ Hyde::titleFromSlug($page->view) }}
+                            </a>
+                        </td>
+                        <td>
+                            {{ \Hyde\Framework\Models\BladePage::$sourceDirectory .'/'. $page->slug . \Hyde\Framework\Models\BladePage::$fileExtension }}
+                        </td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </section>
+    
+        <section class="prose dark:prose-invert mx-auto mt-8">
+            <h3>Markdown Pages</h3>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Title</th>
+                        <th>Source File</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach (\Hyde\Framework\Models\MarkdownPage::all() as $page)
+                    <tr>
+                        <td>
+                            <a href="{{ Hyde::pageLink($page->slug . '.html') }}">
+                                {{ $page->title }}
+                            </a>
+                        </td>
+                        <td>
+                            {{ \Hyde\Framework\Models\MarkdownPage::$sourceDirectory .'/'. $page->slug . \Hyde\Framework\Models\MarkdownPage::$fileExtension }}
+                        </td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </section>
+    
+        <section class="prose dark:prose-invert mx-auto mt-8">
+            <h3>Documentation Pages</h3>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Title</th>
+                        <th>Source File</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach (\Hyde\Framework\Models\DocumentationPage::all() as $page)
+                    <tr>
+                        <td>
+                            <a href="{{ Hyde::docsDirectory() .'/'. Hyde::pageLink($page->slug . '.html') }}">
+                                {{ $page->title }}
+                            </a>
+                        </td>
+                        <td>
+                            {{ \Hyde\Framework\Models\DocumentationPage::$sourceDirectory .'/'. $page->slug . \Hyde\Framework\Models\DocumentationPage::$fileExtension }}
+                        </td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </section>
+    
+        <section class="prose dark:prose-invert mx-auto mt-8">
+            <h3>Blog Posts</h3>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Title</th>
+                        <th>Source File</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach (\Hyde\Framework\Models\MarkdownPost::all() as $post)
+                    <tr>
+                        <td>
+                            <a href="posts/{{ Hyde::pageLink($post->slug . '.html') }}">
+                                {{ $post->title }}
+                            </a>
+                        </td>
+                        <td>
+                            {{ \Hyde\Framework\Models\MarkdownPost::$sourceDirectory .'/'. $post->slug . \Hyde\Framework\Models\MarkdownPost::$fileExtension }}
+                        </td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </section>
+    </section>
 </main>
 @endsection
