7340e98a8f0bd2f39c1806eb72bea2f3d81c7a5b
Caen De Silva <caen@desilva.se>
2022-06-15 21:26:16 +0200

    Update HydeValidateCommand.php

--- packages/framework/src/Commands/HydeValidateCommand.php
+++ packages/framework/src/Commands/HydeValidateCommand.php
@@ -2,41 +2,36 @@
 
 namespace Hyde\Framework\Commands;
 
-use Hyde\Framework\Hyde;
+use Hyde\Framework\Actions\ValidationCheck;
+use Hyde\Framework\Services\ValidationService;
 use LaravelZero\Framework\Commands\Command;
 
-/**
- * Runs the validation tests.
- */
 class HydeValidateCommand extends Command
 {
-    /**
-     * The signature of the command.
-     *
-     * @var string
-     */
     protected $signature = 'validate';
-
-    /**
-     * The description of the command.
-     *
-     * @var string
-     */
     protected $description = 'Run a series of tests to validate your setup and help you optimize your site.';
 
-    /**
-     * Execute the console command.
-     *
-     * @return int
-     */
     public function handle(): int
     {
         $this->info('Running validation tests!');
 
-        $this->line(shell_exec(Hyde::path('vendor/bin/pest').' --group=validators'));
+        foreach (ValidationService::checks() as $check) {
+            $this->check($check);
+        }
 
         $this->info('All done!');
 
         return 0;
+    }
+
+    protected function check(ValidationCheck $check): void
+    {
+        $check->run();
+
+        if ($check->passed()) {
+            $this->info($check->message());
+        } else {
+            $this->error($check->message());
+        }
     }
 }
