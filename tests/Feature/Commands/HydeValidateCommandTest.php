246e0190c1d81ae0d494c1f704256da07eac06d8
Caen De Silva <caen@desilva.se>
2022-06-15 22:02:21 +0200

    Rename file 'ValidateCommandTest' to 'HydeValidateCommandTest.php'

--- tests/Feature/Commands/HydeValidateCommandTest.php
+++ tests/Feature/Commands/HydeValidateCommandTest.php
@@ -4,7 +4,7 @@ namespace Hyde\Testing\Feature\Commands;
 
 use Hyde\Testing\TestCase;
 
-class ValidateCommandTest extends TestCase
+class HydeValidateCommandTest extends TestCase
 {
     public function test_validate_command_can_run()
     {
