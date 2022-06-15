36f624e61c5123ca759a7fd42bb908d6e531a9c4
Caen De Silva <caen@desilva.se>
2022-06-15 22:10:55 +0200

    Test ValidationService::checks() returns an array of ValidationCheck objects

--- tests/Feature/Services/ValidationServiceTest.php
+++ tests/Feature/Services/ValidationServiceTest.php
@@ -2,6 +2,8 @@
 
 namespace Hyde\Testing\Feature\Services;
 
+use Hyde\Framework\Actions\ValidationCheck;
+use Hyde\Framework\Services\ValidationService;
 use Hyde\Testing\TestCase;
 
 /**
@@ -11,5 +13,11 @@ use Hyde\Testing\TestCase;
  */
 class ValidationServiceTest extends TestCase
 {
-    //
+    public function test_checks_returns_an_array_of_validation_check_objects()
+    {
+        $checks = ValidationService::checks();
+
+        $this->assertIsArray($checks);
+        $this->assertContainsOnlyInstancesOf(ValidationCheck::class, $checks);
+    }
 }
