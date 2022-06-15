55a4299267745cbe471a587c841471b5493a1a05
Caen De Silva <caen@desilva.se>
2022-06-15 22:37:11 +0200

    Port CheckIfA404PageExistsTest.php

--- packages/framework/src/Services/ValidationService.php
+++ packages/framework/src/Services/ValidationService.php
@@ -2,6 +2,7 @@
 
 namespace Hyde\Framework\Services;
 use Hyde\Framework\Actions\ValidationCheck;
+use Hyde\Framework\Hyde;
 
 /**
  * @see \Hyde\Testing\Feature\Services\ValidationServiceTest
@@ -11,9 +12,13 @@ class ValidationService
     public static function checks(): array
     {
         return [
-            new ValidationCheck('True is true', function () {
-                return true === true;
-            }, 'How did you get here?'),
+            new ValidationCheck('Your site has a 404 page', function () {
+                return file_exists(Hyde::path('_pages/404.md'))
+                    || file_exists(Hyde::path('_pages/404.blade.php'));
+                },
+                'Could not find an 404.md or 404.blade.php file!',
+                'You can publish the default one using `php hyde publish:views`'
+            ),
 
             new ValidationCheck('True is false', function () {
                 return true === false;
