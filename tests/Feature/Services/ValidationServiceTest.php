2e3b3be2c96b840ad917f310871d663a99e23285
Caen De Silva <caen@desilva.se>
2022-06-16 14:29:45 +0200

    Update base test with new assertions for rewrite

--- tests/Feature/Services/ValidationServiceTest.php
+++ tests/Feature/Services/ValidationServiceTest.php
@@ -2,7 +2,6 @@
 
 namespace Hyde\Testing\Feature\Services;
 
-use Hyde\Framework\Actions\ValidationCheck;
 use Hyde\Framework\Services\ValidationService;
 use Hyde\Testing\TestCase;
 
@@ -10,15 +9,19 @@ use Hyde\Testing\TestCase;
  * Class ValidationServiceTest.
  *
  * @covers \Hyde\Framework\Services\ValidationService
- * @covers \Hyde\Framework\Actions\ValidationCheck
+ * @covers \Hyde\Framework\Models\ValidationResult
  */
 class ValidationServiceTest extends TestCase
 {
-    public function test_checks_returns_an_array_of_validation_check_objects()
+    public function test_checks_returns_an_array_of_validation_check_methods()
     {
         $checks = ValidationService::checks();
-
         $this->assertIsArray($checks);
-        $this->assertContainsOnlyInstancesOf(ValidationCheck::class, $checks);
+
+        // Assert each key starts with 'check_' and is a valid class method name
+        foreach ($checks as $check) {
+            $this->assertStringStartsWith('check_', $check);
+            $this->assertTrue(method_exists(ValidationService::class, $check));
+        }
     }
 }
