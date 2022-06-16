0c7890d2a04189d7ab547ead96447f9171d651ce
Caen De Silva <caen@desilva.se>
2022-06-16 12:22:18 +0200

    Format code

--- packages/framework/src/Services/ValidationService.php
+++ packages/framework/src/Services/ValidationService.php
@@ -16,23 +16,20 @@ class ValidationService
             new ValidationCheck('Your site has a 404 page', function () {
                 return file_exists(Hyde::path('_pages/404.md'))
                     || file_exists(Hyde::path('_pages/404.blade.php'));
-                },
-                'Could not find an 404.md or 404.blade.php file!',
+                }, 'Could not find an 404.md or 404.blade.php file!',
                 'You can publish the default one using `php hyde publish:views`'
             ),
 
             new ValidationCheck('Your site has an index page', function () {
                 return file_exists(Hyde::path('_pages/index.md'))
                     || file_exists(Hyde::path('_pages/index.blade.php'));
-            },
-                'Could not find an index.md or index.blade.php file in the _pages directory!',
+            }, 'Could not find an index.md or index.blade.php file in the _pages directory!',
                 'You can publish the one of the built in templates using `php hyde publish:homepage`'
             ),
 
             new ValidationCheck('A site URL is set', function () {
                 return (bool) Hyde::uriPath();
-            },
-                'Could not find a site URL in the config or .env file!',
+            }, 'Could not find a site URL in the config or .env file!',
             'Adding it may improve SEO as it allows for generating canonical URL, sitemaps, and RSS feeds.'),
         ];
     }
