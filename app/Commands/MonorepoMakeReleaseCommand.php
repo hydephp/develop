2e294e8aefee99bb129b7f0dcd9eede553944158
Caen De Silva <caen@desilva.se>
2022-06-17 13:31:03 +0200

    Sketch out the desired behaviour

--- app/Commands/MonorepoMakeReleaseCommand.php
+++ app/Commands/MonorepoMakeReleaseCommand.php
@@ -17,6 +17,20 @@ class MonorepoMakeReleaseCommand extends Command
     {
         $this->title('Creating a new release!');
 
+        // Fetch remote and abort if we are not synced with the upstream repository.
+        // Also abort if we are not on the master branch.
+
+        // First, get the current tag and increment it depending on the desired semver tag.
+
+        // Then, move the Unreleased section in the changelog to the desired release,
+        // remove any empty sections?
+
+        // Then create a new Unreleased template
+
+        // Next, create syndicated release drafts
+        // (they are drafts as some GitHub actions may need to run before the release is ready
+        // plus, it's best if a human actually reviews everything first )
+
         return 0;
     }
 }
