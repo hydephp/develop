da8638819b082576a8a82d9f10c06926214defb5
Caen De Silva <caen@desilva.se>
2022-06-17 15:03:59 +0200

    Get the tag

--- app/Commands/MonorepoMakeReleaseCommand.php
+++ app/Commands/MonorepoMakeReleaseCommand.php
@@ -32,11 +32,35 @@ class MonorepoMakeReleaseCommand extends Command
             $this->info('This is a dry run. No changes will be pushed to GitHub.');
         }
 
+        $this->task('Fetching origin remote', function() {
+            if (! $this->dryRun) {
+                $this->git('fetch origin');
+            }
+        });
+
         // Fetch remote and abort if we are not synced with the upstream repository.
-        // Also abort if we are not on the master branch.
+        // Also abort if we are not on the master branch. (Not doing this now as I am not on the master branch.)
 
         // First, get the current tag and increment it depending on the desired semver tag.
+        $this->task('Getting current tag', function() {
+            $this->currentTag = trim(shell_exec('git describe --tags --abbrev=0'));
+        });
+        $this->line('Current tag: <info>' . $this->currentTag . '</info>');
 
+        // Prompt for what type of release we are creating.
+        $this->task('Getting release type', function() {
+            $this->releaseType = $this->choice('What type of release are you creating?', [
+                'patch', 'minor', 'major'
+            ], 'patch');
+        });
+        $this->line('Release type: <info>' . $this->releaseType . '</info>');
+
+        // Increment the current tag.
+        $this->task('Incrementing current tag', function() {
+            $this->tag = $this->incrementTag($this->currentTag, $this->releaseType);
+        });
+        $this->line('New tag: <info>' . $this->tag . '</info>');
+        
         // Then, move the Unreleased section in the changelog to the desired release,
         // remove any empty sections?
 
@@ -47,5 +71,35 @@ class MonorepoMakeReleaseCommand extends Command
         // plus, it's best if a human actually reviews everything first )
 
         return 0;
+    }
+
+    protected function incrementTag(string $tag, string $releaseType): string
+    {
+        $prefix = substr($tag, 0, 1);
+        $suffix = substr($tag, strpos($tag, '-')); 
+        $tag = substr($tag, 1, strpos($tag, '-') - 1);
+        $tag = explode('.', $tag);
+
+        foreach ($tag as $key => $value) {
+            $tag[$key] = (int) $value;
+        }
+
+        switch ($releaseType) {
+            case 'patch':
+                $tag[2]++;
+                break;
+            case 'minor':
+                $tag[1]++;
+                $tag[2] = 0;
+                break;
+            case 'major':
+                $tag[0]++;
+                $tag[1] = 0;
+                $tag[2] = 0;
+                break;
+        }
+
+        $tag = implode('.', $tag);
+        return $prefix . $tag . $suffix;
     }
 }
