fa2e1f8d01475d95859bcd1af07e8c563abf37f4
Caen De Silva <caen@desilva.se>
2022-06-18 22:42:45 +0200

    Create HydeSmartDocsTest.php
    Update HydeSmartDocs.php
    Update HydeSmartDocsFacade.php

---
+++ packages/framework/tests/Feature/Services/HydeSmartDocsTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Hyde\Testing\Framework\Feature\Services;
+
+use Hyde\Testing\TestCase;
+
+/**
+ * @covers \Hyde\Framework\Services\HydeSmartDocsTest
+ * @covers \Hyde\Framework\Concerns\FacadeHelpers\HydeSmartDocsFacade
+ */
+class HydeSmartDocsTest extends TestCase
+{
+    //
+}
