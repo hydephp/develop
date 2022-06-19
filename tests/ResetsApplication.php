6aec197da0c5b456ce22af3b2cca94d05933aac8
Caen De Silva <caen@desilva.se>
2022-06-19 22:37:56 +0200

    Add more granular reset methods

--- tests/ResetsApplication.php
+++ tests/ResetsApplication.php
@@ -13,14 +13,37 @@ trait ResetsApplication
 {
     public function resetApplication()
     {
-        File::deleteDirectory(Hyde::path('_pages'));
-        File::deleteDirectory(Hyde::path('_posts'));
-        // File::deleteDirectory(Hyde::path('_media'));
-        File::deleteDirectory(Hyde::path('_docs'));
-        File::deleteDirectory(Hyde::path('_site'));
+        $this->resetMedia();
+        $this->resetPages();
+        $this->resetPosts();
+        $this->resetDocs();
+        $this->resetSite();
+    }
 
-        (new CreatesDefaultDirectories)->__invoke();
+    public function resetMedia()
+    {
+        File::cleanDirectory(Hyde::path('_media'));
+    }
+
+    public function resetPages()
+    {
+        File::cleanDirectory(Hyde::path('_pages'));
         Hyde::copy(Hyde::vendorPath('resources/views/homepages/welcome.blade.php'), Hyde::path('_pages/index.blade.php'));
         Hyde::copy(Hyde::vendorPath('resources/views/pages/404.blade.php'), Hyde::path('_pages/404.blade.php'));
+    }
+
+    public function resetPosts()
+    {
+        File::cleanDirectory(Hyde::path('_posts'));
+    }
+
+    public function resetDocs()
+    {
+        File::cleanDirectory(Hyde::path('_docs'));
+    }
+
+    public function resetSite()
+    {
+        File::cleanDirectory(Hyde::path('_site'));
     }
 }
