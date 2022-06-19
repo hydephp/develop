008b5e5091abe1fd5a7bd557da154c8dfdcfd95a
Caen De Silva <caen@desilva.se>
2022-06-19 13:05:52 +0200

    Create DocumentationPageTest.php

---
+++ packages/framework/tests/Unit/DocumentationPageTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Hyde\Testing\Framework\Unit;
+
+use Hyde\Testing\TestCase;
+
+/**
+ * @covers \Hyde\Framework\Models\DocumentationPage
+ */
+class DocumentationPageTest extends TestCase
+{
+
+}
