e9f27592b8ac67b248e4076a4906ed99d15baf92
Caen De Silva <caen@desilva.se>
2022-06-21 16:51:28 +0200

    Create BuildTaskContract.php

---
+++ packages/framework/src/Contracts/BuildTaskContract.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Hyde\Framework\Contracts;
+
+use Illuminate\Console\OutputStyle;
+
+interface BuildTaskContract
+{
+    public function __construct(?OutputStyle $output = null);
+
+    public function run(): void;
+    public function then(): void;
+
+    public function handle(): void;
+    public function getDescription(): string;
+    public function getExecutionTime(): string;
+}
