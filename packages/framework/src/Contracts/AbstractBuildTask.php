f9d3ae3b72be8f7c261970bc7e04fdcf11dae0fa
Caen De Silva <caen@desilva.se>
2022-06-21 16:51:44 +0200

    Create AbstractBuildTask.php

---
+++ packages/framework/src/Contracts/AbstractBuildTask.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Hyde\Framework\Contracts;
+
+use Illuminate\Console\Concerns\InteractsWithIO;
+use Illuminate\Console\OutputStyle;
+
+abstract class AbstractBuildTask implements BuildTaskContract
+{
+    use InteractsWithIO;
+
+    protected static string $description = 'Generic build task';
+
+    protected float $timeStart;
+
+    public function __construct(?OutputStyle $output = null)
+    {
+        $this->output = $output;
+        $this->timeStart = microtime(true);
+    }
+
+    public function handle(): void
+    {
+        $this->write('<comment>'.$this->getDescription().'...</comment> ');
+
+        $this->run();
+        $this->then();
+
+        $this->newLine();
+    }
+
+    abstract public function run(): void;
+
+    public function then(): void
+    {
+        $this->write('<info>Done.</info> '.$this->getExecutionTime());
+    }
+
+    public function getDescription(): string
+    {
+        return static::$description;
+    }
+
+    public function getExecutionTime(): string
+    {
+        return number_format((microtime(true) - $this->timeStart) * 1000, 2) . 'ms';
+    }
+
+    public function write(string $message): void
+    {
+        $this->output->write($message);
+    }
+
+    public function writeln(string $message): void
+    {
+        $this->output->writeln($message);
+    }
+}
