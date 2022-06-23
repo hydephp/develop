cd66592923e526da540c46313dfca2af895095b2
Caen De Silva <caen@desilva.se>
2022-06-23 22:12:54 +0200

    Use the Asset facade

--- packages/framework/resources/views/layouts/styles.blade.php
+++ packages/framework/resources/views/layouts/styles.blade.php
@@ -1,12 +1,12 @@
 {{-- The core HydeFront stylesheet --}}
-@unless(Hyde::assetManager()->hasMediaFile('hyde.css'))
-<link rel="stylesheet" href="{{ Hyde::assetManager()->cdnLink('hyde.css') }}">
+@unless(Asset::hasMediaFile('hyde.css'))
+<link rel="stylesheet" href="{{ Asset::cdnLink('hyde.css') }}">
 @else
 <link rel="stylesheet" href="{{ Hyde::relativeLink('media/hyde.css', $currentPage) }}">
 @endunless
 
 {{-- The compiled Tailwind/App styles --}}
-@if(Hyde::assetManager()->hasMediaFile('app.css'))
+@if(Asset::hasMediaFile('app.css'))
 <link rel="stylesheet" href="{{ Hyde::relativeLink('media/app.css', $currentPage) }}">
 @endif
 
