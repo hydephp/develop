335dcf5a7087fd3d02f937d91ce7a448f48cb7e1
Caen De Silva <caen@desilva.se>
2022-06-23 21:52:49 +0200

    Add tests for automatic CDN and local Hyde.css support

--- packages/framework/tests/Unit/Views/StylesComponentViewTest.php
+++ packages/framework/tests/Unit/Views/StylesComponentViewTest.php
@@ -3,6 +3,7 @@
 namespace Hyde\Framework\Testing\Unit\Views;
 
 use Hyde\Framework\Hyde;
+use Hyde\Framework\Services\AssetService;
 use Hyde\Testing\TestCase;
 use Illuminate\Support\Facades\Blade;
 
@@ -67,6 +68,44 @@ class StylesComponentViewTest extends TestCase
                 
                 @include("hyde::layouts.styles")'
              )
+        );
+    }
+
+    // Test component renders link to hyde.css when it exists
+    public function test_component_renders_link_to_hyde_css_when_it_exists()
+    {
+        touch(Hyde::path('_media/hyde.css'));
+        $this->assertStringContainsString('<link rel="stylesheet" href="media/hyde.css"', $this->renderTestView());
+        unlink(Hyde::path('_media/hyde.css'));
+    }
+
+    // Test component does not render link to hyde.css when it does not exist
+    public function test_component_does_not_render_link_to_hyde_css_when_it_does_not_exist()
+    {
+        $this->assertStringNotContainsString('<link rel="stylesheet" href="media/hyde.css"', $this->renderTestView());
+    }
+
+    // Test component renders CDN link when no local file exists
+    public function test_component_renders_cdn_link_when_no_local_file_exists()
+    {
+        $this->assertStringContainsString('https://cdn.jsdelivr.net/npm/hydefront', $this->renderTestView());
+    }
+
+    // test component does not render CDN link when a local file exists
+    public function test_component_does_not_render_cdn_link_when_a_local_file_exists()
+    {
+        touch(Hyde::path('_media/hyde.css'));
+        $this->assertStringNotContainsString('https://cdn.jsdelivr.net/npm/hydefront', $this->renderTestView());
+        unlink(Hyde::path('_media/hyde.css'));
+    }
+
+    // test CDN link uses the correct version defined in the asset manager
+    public function test_cdn_link_uses_the_correct_version_defined_in_the_asset_manager()
+    {
+        $expectedVersion = (new AssetService)->version();
+        $this->assertStringContainsString(
+            'https://cdn.jsdelivr.net/npm/hydefront@' . $expectedVersion . '/dist/hyde.css',
+            $this->renderTestView()
         );
     }
 }
