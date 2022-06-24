af3edb76b0d4305da6255e1dd5b357d770617dde
Caen De Silva <caen@desilva.se>
2022-06-24 22:19:01 +0200

    Create DataCollection.php

---
+++ packages/framework/src/Modules/DataCollections/DataCollection.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Hyde\Framework\Modules\DataCollections;
+
+/**
+ * Provides a facade for generating on-the-fly Laravel Collections
+ * from static data files such as Markdown components and YAML files.
+ */
+class DataCollection
+{
+    //
+}
