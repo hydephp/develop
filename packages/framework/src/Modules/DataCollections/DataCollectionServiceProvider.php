3ac510f6ee21b093c1cc87a5c0fa219401173ab0
Caen De Silva <caen@desilva.se>
2022-06-24 22:23:54 +0200

    Register the class alias

--- packages/framework/src/Modules/DataCollections/DataCollectionServiceProvider.php
+++ packages/framework/src/Modules/DataCollections/DataCollectionServiceProvider.php
@@ -2,13 +2,17 @@
 
 namespace Hyde\Framework\Modules\DataCollections;
 
+use Illuminate\Foundation\AliasLoader;
 use Illuminate\Support\ServiceProvider;
 
 class DataCollectionServiceProvider extends ServiceProvider
 {
     public function register()
     {
-        //
+        // Register the class alias
+        AliasLoader::getInstance()->alias(
+            'Collection', DataCollection::class
+        );
     }
 
     public function boot()
