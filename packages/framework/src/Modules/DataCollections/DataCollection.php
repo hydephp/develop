f56bb0a9097b14662c77abd141638c7042192506
Caen De Silva <caen@desilva.se>
2022-06-24 22:33:32 +0200

    Add base Markdown collection facade

--- packages/framework/src/Modules/DataCollections/DataCollection.php
+++ packages/framework/src/Modules/DataCollections/DataCollection.php
@@ -2,11 +2,29 @@
 
 namespace Hyde\Framework\Modules\DataCollections;
 
+use Hyde\Framework\Hyde;
+use Hyde\Framework\Services\MarkdownFileService;
+use Illuminate\Support\Collection as BaseCollection;
+
 /**
  * Provides a facade for generating on-the-fly Laravel Collections
  * from static data files such as Markdown components and YAML files.
  */
 class DataCollection
 {
-    //
+    /**
+     * Get a collection of Markdown documents in the _data/<$key> directory.
+     * Each Markdown file will be parsed into a MarkdownDocument with front matter.
+     */
+    public static function markdown(string $key): BaseCollection
+    {
+        $files = glob(Hyde::path('_data/' . $key . '/*.md'));
+        $collection = new BaseCollection();
+        foreach ($files as $file) {
+            $collection->push(
+                (new MarkdownFileService($file))->get()
+            );
+        }
+        return $collection;
+    }
 }
