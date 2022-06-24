339bd62621ac7660a094e464f207073aa1d0e8aa
Caen De Silva <caen@desilva.se>
2022-06-24 23:12:23 +0200

    Add type annotations

--- packages/framework/src/Modules/DataCollections/Facades/Collection.php
+++ packages/framework/src/Modules/DataCollections/Facades/Collection.php
@@ -14,6 +14,9 @@ class Collection
     /**
      * Get a collection of Markdown documents in the _data/<$key> directory.
      * Each Markdown file will be parsed into a MarkdownDocument with front matter.
+     *
+     * @param string $key for a subdirectory of the _data directory
+     * @return DataCollection<\Hyde\Framework\Models\MarkdownDocument>
      */
     public static function markdown(string $key): DataCollection
     {
