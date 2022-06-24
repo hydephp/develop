c644fd7fcd3bd7a21451fb6556f9c118cbce5201
Caen De Silva <caen@desilva.se>
2022-06-24 22:50:49 +0200

    Create base collection class to hold data

--- packages/framework/src/Modules/DataCollections/DataCollection.php
+++ packages/framework/src/Modules/DataCollections/DataCollection.php
@@ -2,7 +2,32 @@
 
 namespace Hyde\Framework\Modules\DataCollections;
 
-class DataCollection
+use Hyde\Framework\Hyde;
+use Illuminate\Support\Collection;
+
+class DataCollection extends Collection
 {
-    //
+    public string $key;
+    public string $name;
+
+    protected float $timeStart;
+    public float $parseTimeInMs;
+
+    public static string $dataPath = '_data';
+
+    public function __construct(string $key, ?string $name = null)
+    {
+        $this->timeStart = microtime(true);
+        $this->key = $key;
+        $this->name = $name ?? Hyde::titleFromSlug($key);
+
+        parent::__construct();
+    }
+
+    public function push(...$values)
+    {
+        $this->parseTimeInMs = round((microtime(true) - $this->timeStart) * 1000, 2);
+
+        return parent::push($values);
+    }
 }
--- packages/framework/src/Modules/DataCollections/Facades/Collection.php
+++ packages/framework/src/Modules/DataCollections/Facades/Collection.php
@@ -4,8 +4,8 @@ namespace Hyde\Framework\Modules\DataCollections\Facades;
 
 use Hyde\Framework\Hyde;
 use Hyde\Framework\Models\MarkdownDocument;
+use Hyde\Framework\Modules\DataCollections\DataCollection;
 use Hyde\Framework\Services\MarkdownFileService;
-use Illuminate\Support\Collection as BaseCollection;
 
 /**
  * Provides a facade for generating on-the-fly Laravel Collections
@@ -13,29 +13,18 @@ use Illuminate\Support\Collection as BaseCollection;
  */
 class Collection
 {
-    public static string $dataPath = '_data';
-
     /**
      * Get a collection of Markdown documents in the _data/<$key> directory.
      * Each Markdown file will be parsed into a MarkdownDocument with front matter.
      */
-    public static function markdown(string $key): BaseCollection
+    public static function markdown(string $key): DataCollection
     {
-        $time_start = microtime(true);
-        $collection = new BaseCollection();
-        $collection->key = $key;
-        $collection->name = Hyde::titleFromSlug($key);
-        foreach (glob(Hyde::path(static::$dataPath . '/' . $key . '/*' . MarkdownDocument::$fileExtension)) as $file) {
+        $collection = new DataCollection($key);
+        foreach (glob(Hyde::path(DataCollection::$dataPath . '/' . $key . '/*' . MarkdownDocument::$fileExtension)) as $file) {
             $collection->push(
                 (new MarkdownFileService($file))->get()
             );
         }
-        $collection->parseTimeInMs = static::getExecutionTime($time_start);
         return $collection;
-    }
-
-    protected static function getExecutionTime(float $time_start): float
-    {
-        return round((microtime(true) - $time_start) * 1000, 2);
     }
 }
