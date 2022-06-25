bfc9421b3a4fa24ee0a27f5e87d6613777a8d3d9
Caen De Silva <caen@desilva.se>
2022-06-25 22:17:23 +0200

    Add general note for all Route related Contracts

--- packages/framework/src/Modules/Router/Concerns/RouterContract.php
+++ packages/framework/src/Modules/Router/Concerns/RouterContract.php
@@ -4,6 +4,15 @@ namespace Hyde\Framework\Modules\Router\Concerns;
 
 use Illuminate\Support\Collection;
 
+/**
+ * General note for all Route related Contracts while in development:
+ *
+ * My intention is for these contracts to allow Hyde to be easily extended.
+ * Just know, that the contracts may at this point not cover everything they should.
+ * As a package developer, you should look at how Hyde defines models to understand
+ * what your extension needs. And please, submit a PR to update the interfaces with
+ * anything missing. Thank you. Oh, and remember that in 0.x anything may change.
+ */
 interface RouterContract
 {
     public static function getInstance(): RouterContract;
