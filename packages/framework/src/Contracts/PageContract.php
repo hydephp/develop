2da0852c55913d129db9e9f9978e59494e761f56
Caen De Silva <caen@desilva.se>
2022-06-25 20:55:13 +0200

    Update internal documentation

--- packages/framework/src/Contracts/PageContract.php
+++ packages/framework/src/Contracts/PageContract.php
@@ -4,6 +4,17 @@ namespace Hyde\Framework\Contracts;
 
 use Illuminate\Support\Collection;
 
+/**
+ * Defines the requirements for a Page Model to be used and usable by the Hyde Framework.
+ *
+ * A Page Model is a class that contains the data for a single page,
+ * as well as class-wide information for Hyde about how to find,
+ * parse, process, render, and compile, and store the page.
+ *
+ * Note that in addition, certain static class properties are required.
+ * These are defined in the AbstractPage class, Therefore, all Page Models
+ * must extend that class, which in turn implements this interface.
+ */
 interface PageContract
 {
     /**
@@ -28,7 +39,7 @@ interface PageContract
     /**
      * Parse a source file slug into a page model.
      *
-     * @param  string  $slug
+     * @param  string  $slug Base name of the source file, without the file extension or the source directory.
      * @return \Hyde\Framework\Contracts\AbstractPage
      *
      * @see \Hyde\Framework\Testing\Unit\PageModelParseHelperTest
