a055322a3c3d653d11c4cf5d3e1830f0d3d153a5
Caen De Silva <caen@desilva.se>
2022-06-25 22:12:48 +0200

    Add PHPDocs

--- packages/framework/src/Modules/Router/Concerns/RoutableContract.php
+++ packages/framework/src/Modules/Router/Concerns/RoutableContract.php
@@ -4,11 +4,16 @@ namespace Hyde\Framework\Modules\Router\Concerns;
 
 interface RoutableContract
 {
+    /** Get the directory name of where the model source files are found, relative to Hyde root */
     public static function getRouteSourcePath(): string;
 
+    /** Get the directory name of where compiled files should be placed, relative to _site */
     public static function getRouteOutputPath(): string;
 
-    /** @see \Hyde\Framework\Contracts\PageContract::files() */
+    /**
+     * Get the array list of source files for this model.
+     * @see \Hyde\Framework\Contracts\PageContract::files()
+     */
     public static function files(): array;
 
     /** Resolve a slug into a Hyde relative source file path with directory and extension */
