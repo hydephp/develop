69ac66120097b8fc3ff601a30ff0ef88af0bf9bb
Caen De Silva <caen@desilva.se>
2022-06-25 21:05:30 +0200

    Test the class singleton constructor

--- packages/framework/src/Testing/Modules/Router/RouterTest.php
+++ packages/framework/src/Testing/Modules/Router/RouterTest.php
@@ -2,6 +2,7 @@
 
 namespace Hyde\Framework\Testing\Modules\Router;
 
+use Hyde\Framework\Modules\Router\Router;
 use Hyde\Testing\TestCase;
 
 /**
@@ -9,5 +10,27 @@ use Hyde\Testing\TestCase;
  */
 class RouterTest extends TestCase
 {
-    //
+    public function testRouterConstructorIsProtected()
+    {
+        $this->assertTrue(
+            (new \ReflectionClass(Router::class))->getConstructor()->isProtected(),
+            'Failed asserting that constructor has protected visibility.'
+        );
+    }
+
+    public function test_get_instance_returns_route_instance()
+    {
+        $this->assertInstanceOf(
+            Router::class,
+            Router::getInstance()
+        );
+    }
+
+    public function test_get_instance_returns_same_instance()
+    {
+        $this->assertSame(
+            Router::getInstance(),
+            Router::getInstance()
+        );
+    }
 }
