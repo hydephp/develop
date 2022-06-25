c67c0f67597aaae44f6bf55522681942347b255d
Caen De Silva <caen@desilva.se>
2022-06-25 14:47:36 +0200

    Add and document base properties

--- packages/framework/src/Modules/Router/Concerns/BaseRoute.php
+++ packages/framework/src/Modules/Router/Concerns/BaseRoute.php
@@ -4,9 +4,25 @@ namespace Hyde\Framework\Modules\Router\Concerns;
 
 abstract class BaseRoute implements RouteContract
 {
+    /**
+     * @var string<\Hyde\Framework\Contracts\AbstractPage> $sourceModel
+     */
     protected string $sourceModel;
+
+    /**
+     * @var string relative path to the source file
+     */
     protected string $sourceFile;
+
+    /**
+     * @var string the calculated route key/name
+     */
     protected string $name;
+
+    /**
+     * @var string the calculated HTML file path
+     */
+    protected string $path;
 
     public function __construct(string $sourceModel, string $sourceFile)
     {
