f86603f069226930f7481539afac7784cc66697d
Caen De Silva <caen@desilva.se>
2022-06-25 15:13:31 +0200

    Remove sketch annotations

--- packages/framework/src/Modules/Router/Concerns/RouteContract.php
+++ packages/framework/src/Modules/Router/Concerns/RouteContract.php
@@ -2,15 +2,6 @@
 
 namespace Hyde\Framework\Modules\Router\Concerns;
 
-/**
- * protected @var string  $sourceModel (abstract page model)
- * protected @var string  $sourceFile (relative Hyde path)
- * protected @var string  $name (generated route key)
- * protected @var string  $path (generated HTML path)
- *
- * protected @method  generateRouteName(): void;
- * protected @method  generateOutputPath(): void;
- */
 interface RouteContract
 {
     public function __construct(string $sourceModel, string $sourceFile);
