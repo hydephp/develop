4e260853d047908535d088716376a4eb93cd1259
Caen De Silva <caen@desilva.se>
2022-06-25 13:07:43 +0200

    Remove Copilot comments and clean up test names

--- tests/Hyde/Feature/Hyde/Framework/Testing/Modules/DataCollections/DataCollectionTest/DataCollectionTest.php
+++ tests/Hyde/Feature/Hyde/Framework/Testing/Modules/DataCollections/DataCollectionTest/DataCollectionTest.php
@@ -27,28 +27,24 @@ class DataCollectionTest extends TestCase
         $this->assertInstanceOf(Collection::class, $class);
     }
 
-    // test constructor sets key
     public function test_constructor_sets_key()
     {
         $class = new DataCollection('foo');
         $this->assertEquals('foo', $class->key);
     }
 
-    // test key is required
     public function test_key_is_required()
     {
         $this->expectException(\ArgumentCountError::class);
         new DataCollection();
     }
 
-    // test Get Collection method returns the collection instance
     public function test_get_collection_method_returns_the_collection_instance()
     {
         $class = new DataCollection('foo');
         $this->assertSame($class, $class->getCollection());
     }
 
-    // test get collection method sets parse time in ms
     public function test_get_collection_method_sets_parse_time_in_ms()
     {
         $class = new DataCollection('foo');
@@ -56,7 +52,6 @@ class DataCollectionTest extends TestCase
         $this->assertIsFloat($class->parseTimeInMs);
     }
 
-    // test get markdown files method returns empty array if the specified directory does not exist
     public function test_get_markdown_files_method_returns_empty_array_if_the_specified_directory_does_not_exist()
     {
         $class = new DataCollection('foo');
@@ -64,7 +59,6 @@ class DataCollectionTest extends TestCase
         $this->assertEmpty($class->getMarkdownFiles());
     }
 
-    // test get markdown files method returns empty array if no files are found in specified directory
     public function test_get_markdown_files_method_returns_empty_array_if_no_files_are_found_in_specified_directory()
     {
         mkdir(Hyde::path('_data/foo'));
@@ -74,7 +68,6 @@ class DataCollectionTest extends TestCase
         rmdir(Hyde::path('_data/foo'));
     }
 
-    // test get markdown files method returns an array of markdown files in the specified directory
     public function test_get_markdown_files_method_returns_an_array_of_markdown_files_in_the_specified_directory()
     {
         mkdir(Hyde::path('_data/foo'));
@@ -89,7 +82,6 @@ class DataCollectionTest extends TestCase
         File::deleteDirectory(Hyde::path('_data/foo'));
     }
 
-    // test get markdown files method does not include files in subdirectories
     public function test_get_markdown_files_method_does_not_include_files_in_subdirectories()
     {
         mkdir(Hyde::path('_data/foo'));
@@ -102,7 +94,6 @@ class DataCollectionTest extends TestCase
         File::deleteDirectory(Hyde::path('_data/foo'));
     }
 
-    // test get markdown files method does not include files with extensions other than .md
     public function test_get_markdown_files_method_does_not_include_files_with_extensions_other_than_md()
     {
         mkdir(Hyde::path('_data/foo'));
@@ -114,13 +105,11 @@ class DataCollectionTest extends TestCase
         File::deleteDirectory(Hyde::path('_data/foo'));
     }
 
-    // test static markdown helper returns new data collection instance
     public function test_static_markdown_helper_returns_new_data_collection_instance()
     {
         $this->assertInstanceOf(DataCollection::class, DataCollection::markdown('foo'));
     }
 
-    // test static markdown helper discovers and parses markdown files in the specified directory
     public function test_static_markdown_helper_discovers_and_parses_markdown_files_in_the_specified_directory()
     {
         mkdir(Hyde::path('_data/foo'));
@@ -134,7 +123,6 @@ class DataCollectionTest extends TestCase
         File::deleteDirectory(Hyde::path('_data/foo'));
     }
 
-    // test static markdown helper ignores files starting with an underscore
     public function test_static_markdown_helper_ignores_files_starting_with_an_underscore()
     {
         mkdir(Hyde::path('_data/foo'));
@@ -144,7 +132,6 @@ class DataCollectionTest extends TestCase
         File::deleteDirectory(Hyde::path('_data/foo'));
     }
     
-    // test markdown facade returns same result as static markdown helper
     public function test_markdown_facade_returns_same_result_as_static_markdown_helper()
     {
         $expected = DataCollection::markdown('foo');
@@ -155,14 +142,13 @@ class DataCollectionTest extends TestCase
     }
 
     // Test DataCollectionServiceProvider registers the facade as an alias
-    public function test_DataCollectionServiceProvider_registers_the_facade_as_an_alias()
+    public function test_data_collection_service_provider_registers_the_facade_as_an_alias()
     {
         $this->assertArrayHasKey('MarkdownCollection', AliasLoader::getInstance()->getAliases());
         $this->assertContains(MarkdownCollection::class, AliasLoader::getInstance()->getAliases());
     }
 
-    // test DataCollectionServiceProvider creates the _data directory if it does not exist
-    public function test_DataCollectionServiceProvider_creates_the__data_directory_if_it_does_not_exist()
+    public function test_data_collection_service_provider_creates_the__data_directory_if_it_does_not_exist()
     {
         File::deleteDirectory(Hyde::path('_data'));
         $this->assertFileDoesNotExist(Hyde::path('_data'));
@@ -172,14 +158,12 @@ class DataCollectionTest extends TestCase
         $this->assertFileExists(Hyde::path('_data'));
     }
 
-    // test class has static source directory property
-    public function testClassHasStaticSourceDirectoryProperty()
+    public function test_class_has_static_source_directory_property()
     {
         $this->assertEquals('_data', DataCollection::$sourceDirectory);
     }
 
-    // test source directory can be changed
-    public function testSourceDirectoryCanBeChanged()
+    public function test_source_directory_can_be_changed()
     {
         DataCollection::$sourceDirectory = 'foo';
         mkdir(Hyde::path('foo/bar'), recursive: true);
