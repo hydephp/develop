2cbec2c8284ea258324cd822caa378a99efb6c38
Caen De Silva <caen@desilva.se>
2022-06-25 14:32:08 +0200

    Create RouteContract.php

---
+++ packages/framework/src/Modules/Router/Concerns/RouteContract.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Hyde\Framework\Modules\Router\Concerns;
+
+/**
+ * protected @var string  $sourceModel (abstract page model)
+ * protected @var string  $sourceFile (relative Hyde path)
+ * protected @var string  $name (basename/slug)
+ */
+interface RouteContract
+{
+    public function __construct(string $sourceModel, string $sourceFile);
+
+    public function getSourceFile(): string;
+    public function getSourceModel(): string;
+    public function getPath(): string;
+    public function getName(): string;
+
+    public function __toString(): string;
+}
