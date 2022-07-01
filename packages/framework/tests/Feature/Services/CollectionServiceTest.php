f94ed285ac6ad28108f78641119ea9e1e8c927dd
Caen De Silva <caen@desilva.se>
2022-07-01 22:14:30 +0200

    Matrix test multiple configuration options

--- packages/framework/tests/Feature/Services/CollectionServiceTest.php
+++ packages/framework/tests/Feature/Services/CollectionServiceTest.php
@@ -9,6 +9,7 @@ use Hyde\Framework\Models\MarkdownPage;
 use Hyde\Framework\Models\MarkdownPost;
 use Hyde\Framework\Services\CollectionService;
 use Hyde\Testing\TestCase;
+use Illuminate\Support\Facades\File;
 
 /**
  * @covers \Hyde\Framework\Services\CollectionService
@@ -47,6 +48,42 @@ class CollectionServiceTest extends TestCase
         $this->unitTestMarkdownBasedPageList(MarkdownPage::class, '_pages/foo.md');
         $this->unitTestMarkdownBasedPageList(MarkdownPost::class, '_posts/foo.md');
         $this->unitTestMarkdownBasedPageList(DocumentationPage::class, '_docs/foo.md');
+    }
+
+    public function test_get_source_file_list_for_model_method_finds_customized_model_properties()
+    {
+        $matrix = [
+            MarkdownPage::class,
+            MarkdownPost::class,
+            DocumentationPage::class,
+        ];
+
+        foreach ($matrix as $model) {
+            // Setup
+            @mkdir(Hyde::path('foo'));
+            $sourceDirectoryBackup = $model::$sourceDirectory;
+            $fileExtensionBackup = $model::$fileExtension;
+
+            // Test baseline
+            $this->unitTestMarkdownBasedPageList($model, $model::$sourceDirectory.'/foo.md');
+
+            // Set the source directory to a custom value
+            $model::$sourceDirectory = 'foo';
+
+            // Test customized
+            $this->unitTestMarkdownBasedPageList($model, 'foo/foo.md');
+
+            // Set file extension to a custom value
+            $model::$fileExtension = 'foo';
+
+            // Test customized
+            $this->unitTestMarkdownBasedPageList($model, 'foo/foo.foo', 'foo');
+
+            // Cleanup
+            File::deleteDirectory(Hyde::path('foo'));
+            $model::$sourceDirectory = $sourceDirectoryBackup;
+            $model::$fileExtension = $fileExtensionBackup;
+        }
     }
 
     public function test_get_source_file_list_returns_false_for_invalid_method()
@@ -97,11 +134,11 @@ class CollectionServiceTest extends TestCase
         unlink(Hyde::path('_posts/_foo.md'));
     }
 
-    protected function unitTestMarkdownBasedPageList(string $model, string $path)
+    protected function unitTestMarkdownBasedPageList(string $model, string $path, ?string $expected = null)
     {
         touch(Hyde::path($path));
 
-        $expected = basename($path,'.md');
+        $expected = $expected ?? basename($path,'.md');
 
         $this->assertEquals([$expected], CollectionService::getSourceFileListForModel($model));
 
