92f685263419c721546f9faf81ecad6cb6592219
Caen De Silva <caen@desilva.se>
2022-07-02 11:31:04 +0200

    Revert "Update PHPDoc information"
    This reverts commit 87d7e2a94000840fe849d4a58efc56423925b9db.

--- packages/framework/src/Models/MarkdownPage.php
+++ packages/framework/src/Models/MarkdownPage.php
@@ -5,12 +5,6 @@ namespace Hyde\Framework\Models;
 use Hyde\Framework\Contracts\AbstractPage;
 use Hyde\Framework\Models\Parsers\MarkdownPageParser;
 
-/**
- * The base class for all Markdown-based Page Models.
- *
- * The other Markdown based pages extend this class,
- * to take advantage of Markdown specific helpers.
- */
 class MarkdownPage extends AbstractPage
 {
     public static string $sourceDirectory = '_pages';
