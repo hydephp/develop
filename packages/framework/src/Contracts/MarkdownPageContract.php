b06ca7560d10424231bace37cd389af459e752cf
Caen De Silva <caen@desilva.se>
2022-07-02 14:17:29 +0200

    Define the markdown helper method

--- packages/framework/src/Contracts/MarkdownPageContract.php
+++ packages/framework/src/Contracts/MarkdownPageContract.php
@@ -2,7 +2,14 @@
 
 namespace Hyde\Framework\Contracts;
 
+use Hyde\Framework\Models\MarkdownDocument;
+
 interface MarkdownPageContract
 {
-	//
+	/**
+	 * Get the page's Markdown Document object.
+	 *
+	 * @return \Hyde\Framework\Models\MarkdownDocument
+	 */
+	public function markdown(): MarkdownDocument;
 }
