hydephp/develop

commit d120027d82300aa1bd9c7e4ac0eddd0b90671dfb

Remove service provider support for removed config option

packages/framework/src/HydeServiceProvider.php
@@ -64,19 +64,10 @@ class HydeServiceProvider extends ServiceProvider
 
         $this->discoverBladeViewsIn('_pages');
 
-        /** @deprecated v0.43.0-beta and is used here as a fallback for compatibility */
-        if (config('hyde.output_directory') === null) {
-            $this->storeCompiledSiteIn(config(
-                'hyde.site_output_path',
-                Hyde::path('_site')
-            ));
-        } else {
-            // Newer version which is safer.
-            $this->storeCompiledSiteIn(Hyde::path(config(
-                trim('hyde.output_directory', '_site'),
-                '/\\'
-            )));
-        }
+        $this->storeCompiledSiteIn(Hyde::path(config(
+            trim('hyde.output_directory', '_site'),
+            '/\\'
+        )));
 
         $this->commands([
             Commands\HydePublishHomepageCommand::class,

commit 785131d41f998932b0a367550d96c3b18e5847e5

Fix bug caused by function parameter mismatch
Fix bug in d120027d8 caused by improper function ordering leading to there being no fallback if the output directory config is not set.

packages/framework/src/HydeServiceProvider.php
@@ -43,10 +43,9 @@ class HydeServiceProvider extends ServiceProvider
 
         $this->discoverBladeViewsIn('_pages');
 
-        $this->storeCompiledSiteIn(Hyde::path(config(
-            trim('hyde.output_directory', '_site'),
-            '/\\'
-        )));
+        $this->storeCompiledSiteIn(Hyde::path(
+            trim(config('hyde.output_directory', '_site'), '/\\')
+        ));
 
         $this->commands([
             Commands\HydePublishHomepageCommand::class,