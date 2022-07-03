06f0bef1d5f250dfeae19eebd5453f52562f6e74
Caen De Silva <caen@desilva.se>
2022-07-03 22:42:47 +0200

    Create Route.php

---
+++ packages/framework/src/Models/Routing/Route.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Hyde\Framework\Models\Routing;
+
+use Hyde\Framework\Contracts\PageContract;
+
+class Route implements RouteContract
+{
+    protected PageContract $sourceModel;
+
+    public function __construct(PageContract $sourceModel)
+    {
+        $this->sourceModel = $sourceModel;
+    }
+
+    public function getSourceModel(): PageContract
+    {
+        // TODO: Implement getSourceModel() method.
+    }
+
+    public function getSourceFilePath(): string
+    {
+        // TODO: Implement getSourceFilePath() method.
+    }
+
+    public function getOutputFilePath(): string
+    {
+        // TODO: Implement getOutputFilePath() method.
+    }
+}
