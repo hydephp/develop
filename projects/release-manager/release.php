54f5ec2cd8c9185b387618e41ff6fb1686c1d7e6
Caen De Silva <caen@desilva.se>
2022-07-03 20:36:05 +0200

    Create release.php

---
+++ projects/release-manager/release.php
@@ -0,0 +1,54 @@
+<?php
+
+// Internal script I bodged together, don't use in production, verify before committing, etc.
+
+// Run with `php .\projects\release-manager\release.php`
+
+echo "Preparing a new syndicated HydePHP release!\n";
+
+echo "Using NPM for versioning...\n";
+
+$version = trim(shell_exec('npm version minor --no-git-tag-version')) . '-beta';
+
+echo "Version: $version\n";
+
+echo "Updating Hyde composer.json...\n";
+
+// get just the minor number
+$shortVersion = substr($version, 3);
+// trim everything after the first dot
+$shortVersion = substr($shortVersion, 0, strpos($shortVersion, '.'));
+
+echo "Short version: $shortVersion\n";
+$composerJson = json_decode(file_get_contents(__DIR__.'./../../packages/hyde/composer.json'), true);
+$composerJson['require']['hyde/framework'] = "^0.$shortVersion";
+file_put_contents(__DIR__.'./../../packages/hyde/composer.json', json_encode($composerJson, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES));
+
+echo "Transforming upcoming release notes... \n";
+
+$notes = file_get_contents(__DIR__ . '/../../RELEASE_NOTES.md');
+$notes = str_replace('## [Unreleased]', '## '.$version, $notes);
+$notes = str_replace('YYYY-MM-DD', date('Y-m-d'), $notes);
+echo "Done. \n";
+
+echo "Resetting upcoming release notes stub";
+copy(__DIR__ .'/release-notes.stub', __DIR__ . '/../../RELEASE_NOTES.md');
+
+echo "Updating changelog with the upcoming release notes... ";
+
+$changelog = file_get_contents(__DIR__ . '/../../CHANGELOG.md');
+
+$needle = '<!-- CHANGELOG_START -->';
+
+$changelog = substr_replace($changelog, $needle ."\n\n". $notes, strpos($changelog, $needle), strlen($needle));
+file_put_contents(__DIR__ . '/../../CHANGELOG.md', $changelog);
+
+echo "Done. \n";
+
+$title = "$version - " . date('Y-m-d');
+$body = ltrim(substr($notes, strpos($notes, "\n") + 2));
+
+echo "\nAll done!\nNext, verify the changes, then you can commit the release with the following message: \n";
+echo "$title\n";
+echo "And here is a link to publish the release: \n";
+echo "https://github.com/hydephp/develop/releases/new?tag=$version&title=".urlencode($title)."&body=".urlencode($body)."\n";
