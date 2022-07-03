55927c43236eca0f80c6b14e66eef2b46b8346e1
Caen De Silva <caen@desilva.se>
2022-07-03 22:45:31 +0200

    Add PHPDocs

--- packages/framework/src/Models/Routing/Route.php
+++ packages/framework/src/Models/Routing/Route.php
@@ -6,23 +6,32 @@ use Hyde\Framework\Contracts\PageContract;
 
 abstract class Route implements RouteContract
 {
+    /**
+     * The source model for the route.
+     *
+     * @var \Hyde\Framework\Contracts\PageContract $sourceModel
+     */
     protected PageContract $sourceModel;
 
+    /** @inheritDoc */
     public function __construct(PageContract $sourceModel)
     {
         $this->sourceModel = $sourceModel;
     }
 
+    /** @inheritDoc */
     public function getSourceModel(): PageContract
     {
         // TODO: Implement getSourceModel() method.
     }
 
+    /** @inheritDoc */
     public function getSourceFilePath(): string
     {
         // TODO: Implement getSourceFilePath() method.
     }
 
+    /** @inheritDoc */
     public function getOutputFilePath(): string
     {
         // TODO: Implement getOutputFilePath() method.
--- packages/framework/src/Models/Routing/RouteContract.php
+++ packages/framework/src/Models/Routing/RouteContract.php
@@ -6,11 +6,31 @@ use Hyde\Framework\Contracts\PageContract;
 
 interface RouteContract
 {
+    /**
+     * Construct a new Route instance for the given page model.
+     *
+     * @param \Hyde\Framework\Contracts\PageContract $sourceModel
+     */
     public function __construct(PageContract $sourceModel);
 
+    /**
+     * Get the source model for the route.
+     *
+     * @return \Hyde\Framework\Contracts\PageContract
+     */
     public function getSourceModel(): PageContract;
 
+    /**
+     * Get the path to the source file.
+     *
+     * @return string Path relative to the root of the project.
+     */
     public function getSourceFilePath(): string;
 
+    /**
+     * Get the path to the output file.
+     *
+     * @return string Path relative to the site output directory.
+     */
     public function getOutputFilePath(): string;
 }
