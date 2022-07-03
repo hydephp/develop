a263d93057e0ebfb06daef12e28a41296a062f59
Caen De Silva <caen@desilva.se>
2022-07-03 21:14:50 +0200

    Remove the about section as well

--- projects/release-manager/release.php
+++ projects/release-manager/release.php
@@ -44,6 +44,13 @@ foreach ($defaults as $default) {
     $notes = str_replace($default, '', $notes);
 }
 
+$notes = str_replace('Keep an Unreleased section at the top to track upcoming changes.
+
+This serves two purposes:
+
+1. People can see what changes they might expect in upcoming releases
+2. At release time, you can move the Unreleased section changes into a new release version section.', '', $notes);
+
 $notes = trim($notes);
 
 $notes = str_replace('## [Unreleased]', '## '.$version, $notes);
