0ec2c3c05626b17a8b5f346c16f469cc67ea87a9
Caen De Silva <caen@desilva.se>
2022-07-04 12:31:04 +0200

    Create BuildService.php

---
+++ packages/framework/src/Services/BuildService.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Hyde\Framework\Services;
+
+/**
+ * Moves logic from the build command to a service.
+ *
+ * Handles the build loop which generates the static site.
+ */
+class BuildService
+{
+    //
+}
