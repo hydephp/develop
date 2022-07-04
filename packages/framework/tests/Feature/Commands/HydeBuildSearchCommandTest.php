70dec797026a79ff6f2986f9814fbd7295190471
Caen De Silva <caen@desilva.se>
2022-07-04 20:29:23 +0200

    Move shared test code to setup/teardown methods

--- packages/framework/tests/Feature/Commands/HydeBuildSearchCommandTest.php
+++ packages/framework/tests/Feature/Commands/HydeBuildSearchCommandTest.php
@@ -10,38 +10,46 @@ use Hyde\Testing\TestCase;
  */
 class HydeBuildSearchCommandTest extends TestCase
 {
+    protected function setUp(): void
+    {
+        parent::setUp();
+        unlinkIfExists(Hyde::path('_site/docs/search.json'));
+        unlinkIfExists(Hyde::path('_site/docs/search.html'));
+    }
+
+    protected function tearDown(): void
+    {
+        unlinkIfExists(Hyde::path('_site/docs/search.html'));
+        unlinkIfExists(Hyde::path('_site/docs/search.json'));
+        parent::tearDown();
+    }
+
+
     public function test_it_creates_the_search_json_file()
     {
-        unlinkIfExists(Hyde::path('_site/docs/search.json'));
         $this->artisan('build:search')
             ->expectsOutput('Generating documentation site search index...')
             ->assertExitCode(0);
 
         $this->assertFileExists(Hyde::path('_site/docs/search.json'));
-        unlink(Hyde::path('_site/docs/search.json'));
     }
 
     public function test_it_creates_the_search_page()
     {
-        unlinkIfExists(Hyde::path('_site/docs/search.html'));
         $this->artisan('build:search')
             ->expectsOutput('Generating documentation site search index...')
             ->assertExitCode(0);
 
         $this->assertFileExists(Hyde::path('_site/docs/search.html'));
-        unlink(Hyde::path('_site/docs/search.html'));
-        unlink(Hyde::path('_site/docs/search.json'));
     }
 
     public function test_it_does_not_create_the_search_page_if_disabled()
     {
         config(['docs.create_search_page' => false]);
-        unlinkIfExists(Hyde::path('_site/docs/search.html'));
         $this->artisan('build:search')
             ->expectsOutput('Generating documentation site search index...')
             ->assertExitCode(0);
 
         $this->assertFileDoesNotExist(Hyde::path('_site/docs/search.html'));
-        unlink(Hyde::path('_site/docs/search.json'));
     }
 }
