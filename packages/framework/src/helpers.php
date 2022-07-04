3494ed8c69c2531fec6707d1306ada4cd711bf66
Caen De Silva <caen@desilva.se>
2022-07-04 18:59:35 +0200

    Add `array_map_unique()` helper

--- packages/framework/src/helpers.php
+++ packages/framework/src/helpers.php
@@ -26,3 +26,24 @@ if (! function_exists('unslash')) {
         return trim($string, '/\\');
     }
 }
+
+if (! function_exists('array_map_unique')) {
+    /**
+     * Map a callback over an array and remove duplicates.
+     *
+     * Important! The callback and the array parameter positions
+     * are reversed compared to the PHP function.
+     *
+     * @param  array|\Illuminate\Support\Collection  $array  $array
+     * @param  callable  $callback
+     * @return array
+     */
+    function array_map_unique(array|\Illuminate\Support\Collection $array, callable $callback): array
+    {
+        if ($array instanceof \Illuminate\Support\Collection) {
+            $array = $array->toArray();
+        }
+
+        return array_unique(array_map($callback, $array));
+    }
+}
