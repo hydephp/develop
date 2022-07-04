8816da52c6d44f1440c36de58b811699fbef8598
Caen De Silva <caen@desilva.se>
2022-07-04 13:17:41 +0200

    Port methods from BuildActionRunner

--- packages/framework/src/Services/BuildService.php
+++ packages/framework/src/Services/BuildService.php
@@ -23,4 +23,40 @@ class BuildService
 
         $this->router = Router::getInstance();
     }
+
+    protected function canRunBuildAction(array $collection, string $name, ?string $verb = null): bool
+    {
+        if (sizeof($collection) < 1) {
+            $this->line('No '.$name.' found. Skipping...');
+            $this->newLine();
+
+            return false;
+        }
+
+        $this->comment(($verb ?? 'Creating')." $name...");
+
+        return true;
+    }
+
+    /** @internal */
+    protected function runBuildAction(string $model): void
+    {
+        $collection = CollectionService::getSourceFileListForModel($model);
+        $modelName = $this->getModelPluralName($model);
+        if ($this->canRunBuildAction($collection, $modelName)) {
+            $this->withProgressBar(
+                $collection,
+                function ($basename) use ($model) {
+                    new StaticPageBuilder(
+                        DiscoveryService::getParserInstanceForModel(
+                            $model,
+                            $basename
+                        )->get(),
+                        true
+                    );
+                }
+            );
+            $this->newLine(2);
+        }
+    }
 }
