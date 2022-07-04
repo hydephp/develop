c8eee6c39eed4a086dd963194fb9a8d3f8aa04dd
Caen De Silva <caen@desilva.se>
2022-07-04 17:00:25 +0200

    Setup helpers to mock output

--- packages/framework/tests/Feature/BuildServiceTest.php
+++ packages/framework/tests/Feature/BuildServiceTest.php
@@ -2,8 +2,13 @@
 
 namespace Hyde\Framework\Testing\Feature;
 
+use Hyde\Framework\Services\BuildService;
 use Hyde\Testing\ResetsApplication;
 use Hyde\Testing\TestCase;
+use Illuminate\Console\OutputStyle;
+use Mockery;
+use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Output\BufferedOutput;
 
 /**
  * @covers \Hyde\Framework\Services\BuildService
@@ -22,5 +27,30 @@ class BuildServiceTest extends TestCase
     {
         $this->resetSite();
         parent::tearDown();
+    }
+
+    public function test_build_service()
+    {
+        $service = $this->makeService();
+
+        $this->assertInstanceOf(BuildService::class, $service);
+    }
+
+    protected function makeService(): BuildService
+    {
+        return new BuildService($this->mockOutputInterface());
+    }
+
+    protected function mockOutputInterface()
+    {
+        $this->app->bind(OutputStyle::class, function () {
+            return Mockery::mock(OutputStyle::class.'[askQuestion]', [
+                (new ArrayInput([])), Mockery::mock(BufferedOutput::class.'[doWrite]')
+                    ->shouldAllowMockingProtectedMethods()
+                    ->shouldIgnoreMissing(),
+            ]);
+        });
+
+       return $this->app->make(OutputStyle::class);
     }
 }
