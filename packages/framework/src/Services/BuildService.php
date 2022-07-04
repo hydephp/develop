f1083760cea977a215fdd2915bad1a74972b4ff0
Caen De Silva <caen@desilva.se>
2022-07-04 13:34:26 +0200

    Simplify the build action runner code

--- packages/framework/src/Services/BuildService.php
+++ packages/framework/src/Services/BuildService.php
@@ -37,16 +37,17 @@ class BuildService
         $this->runBuildAction(DocumentationPage::class);
     }
 
-    protected function canRunBuildAction(array $collection, string $name, ?string $verb = null): bool
+    protected function canRunBuildAction(array $collection, string $model): bool
     {
+        $name = $this->getModelPluralName($model);
+
         if (sizeof($collection) < 1) {
-            $this->line('No '.$name.' found. Skipping...');
-            $this->newLine();
+            $this->line("No $name found. Skipping...\n");
 
             return false;
         }
 
-        $this->comment(($verb ?? 'Creating')." $name...");
+        $this->comment("Creating $name...");
 
         return true;
     }
@@ -55,8 +56,7 @@ class BuildService
     protected function runBuildAction(string $model): void
     {
         $collection = CollectionService::getSourceFileListForModel($model);
-        $modelName = $this->getModelPluralName($model);
-        if ($this->canRunBuildAction($collection, $modelName)) {
+        if ($this->canRunBuildAction($collection, $model)) {
             $this->withProgressBar(
                 $collection,
                 function ($basename) use ($model) {
