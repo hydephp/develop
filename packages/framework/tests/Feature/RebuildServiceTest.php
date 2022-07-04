480e7248a677f91bfe3a26e8f738bf6c888ff0c2
Caen De Silva <caen@desilva.se>
2022-07-04 21:11:22 +0200

    Replace unnecessary file generations with touching foo files

--- packages/framework/tests/Feature/RebuildServiceTest.php
+++ packages/framework/tests/Feature/RebuildServiceTest.php
@@ -17,12 +17,12 @@ class RebuildServiceTest extends TestCase
 {
     public function test_service_method()
     {
-        createTestPost();
-        $service = new RebuildService('_posts/test-post.md');
+        touch(Hyde::path('_posts/foo.md'));
+        $service = new RebuildService('_posts/foo.md');
         $service->execute();
         $this->assertNotNull($service->model);
-        unlink(Hyde::path('_posts/test-post.md'));
-        unlink(Hyde::path('_site/posts/test-post.html'));
+        unlink(Hyde::path('_posts/foo.md'));
+        unlink(Hyde::path('_site/posts/foo.html'));
     }
 
     public function test_execute_methods()
@@ -32,15 +32,15 @@ class RebuildServiceTest extends TestCase
         $this->runExecuteTest('_docs');
         $this->runExecuteTest('_pages', '.blade.php');
 
-        unlink(Hyde::path('_site/test-file.html'));
-        unlink(Hyde::path('_site/docs/test-file.html'));
-        unlink(Hyde::path('_site/posts/test-file.html'));
+        unlink(Hyde::path('_site/foo.html'));
+        unlink(Hyde::path('_site/docs/foo.html'));
+        unlink(Hyde::path('_site/posts/foo.html'));
     }
 
     protected function runExecuteTest(string $prefix, string $suffix = '.md')
     {
-        $path = $prefix.'/test-file'.$suffix;
-        createTestPost($path);
+        $path = $prefix.'/foo'.$suffix;
+        touch(Hyde::path($path));
         $service = new RebuildService($path);
         $result = $service->execute();
         $this->assertInstanceOf(StaticPageBuilder::class, $result);
