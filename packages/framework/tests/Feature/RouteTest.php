abb6ee86d461fe9513d74f8c118f031b4d11cbf9
Caen De Silva <caen@desilva.se>
2022-07-05 08:25:28 +0200

    Add tests for the Route facade

--- packages/framework/tests/Feature/RouteTest.php
+++ packages/framework/tests/Feature/RouteTest.php
@@ -10,10 +10,12 @@ use Hyde\Framework\Models\Pages\MarkdownPost;
 use Hyde\Framework\Modules\Routing\Route;
 use Hyde\Framework\Modules\Routing\RouteContract;
 use Hyde\Framework\Modules\Routing\RouteNotFoundException;
+use Hyde\Framework\Facades\Route as RouteFacade;
 use Hyde\Testing\TestCase;
 
 /**
  * @covers \Hyde\Framework\Modules\Routing\Route
+ * @covers \Hyde\Framework\Facades\Route
  */
 class RouteTest extends TestCase
 {
@@ -158,5 +160,31 @@ class RouteTest extends TestCase
         touch(Hyde::path('_docs/foo.md'));
         $this->assertEquals(new Route(DocumentationPage::parse('foo')), Route::getFromSource('_docs/foo.md'));
         unlink(Hyde::path('_docs/foo.md'));
+    }
+    
+    /** @covers RouteFacade::get */
+    public function test_route_facade_get_method_calls_get_method()
+    {
+        $this->assertEquals(Route::get('index'), RouteFacade::get('index'));
+    }
+
+    /** @covers RouteFacade::getOrFail */
+    public function test_route_facade_getOrFail_method_calls_getOrFail_method()
+    {
+        $this->assertEquals(Route::getOrFail('index'), RouteFacade::getOrFail('index'));
+    }
+
+    /** @covers RouteFacade::getFromSource */
+    public function test_route_facade_getFromSource_method_calls_getFromSource_method()
+    {
+        $this->assertEquals(Route::getFromSource('_pages/index.blade.php'),
+            RouteFacade::getFromSource('_pages/index.blade.php'));
+    }
+
+    /** @covers RouteFacade::getFromSourceOrFail */
+    public function test_route_facade_getFromSourceOrFail_method_calls_getFromSourceOrFail_method()
+    {
+        $this->assertEquals(Route::getFromSourceOrFail('_pages/index.blade.php'),
+            RouteFacade::getFromSourceOrFail('_pages/index.blade.php'));
     }
 }
