256ff0d8bee35a1a5fa055577ff8b69fe7a7d7dc
Caen De Silva <caen@desilva.se>
2022-07-05 13:53:16 +0200

    Get the home route

--- packages/framework/src/Modules/Navigation/NavigationMenu.php
+++ packages/framework/src/Modules/Navigation/NavigationMenu.php
@@ -7,16 +7,19 @@ use Hyde\Framework\Contracts\PageContract;
 use Hyde\Framework\Models\Pages\DocumentationPage;
 use Hyde\Framework\Models\Pages\MarkdownPost;
 use Hyde\Framework\Modules\Routing\Route;
+use Hyde\Framework\Modules\Routing\RouteNotFoundException;
 use Hyde\Framework\Modules\Routing\Router;
 use Illuminate\Support\Collection;
 
 class NavigationMenu extends Collection
 {
+    protected Route $homeRoute;
     protected Route $currentRoute;
 
     public function __construct(Route $currentRoute)
     {
         $this->currentRoute = $currentRoute;
+        $this->homeRoute = $this->getHomeRoute();
 
         parent::__construct();
     }
@@ -31,6 +34,7 @@ class NavigationMenu extends Collection
         Router::getInstance()->getRoutes()->each(function (Route $route) {
             $this->addLink($route);
         });
+
 
         return $this;
     }
@@ -54,5 +58,10 @@ class NavigationMenu extends Collection
     protected function hasHiddenProperty(PageContract $page): bool
     {
         return ($page instanceof AbstractMarkdownPage) && $page->markdown()->matter('hidden');
+    }
+
+    protected function getHomeRoute(): Route
+    {
+        return Route::get('index') ?? throw new RouteNotFoundException('index');
     }
 }
