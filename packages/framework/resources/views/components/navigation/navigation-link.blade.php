0310328dee45a3e5fb736142a5d84b15e0b0fa83
Caen De Silva <caen@desilva.se>
2022-07-05 19:25:40 +0200

    Update Blade views to use the new navigation service

--- packages/framework/resources/views/components/navigation/navigation-link.blade.php
+++ packages/framework/resources/views/components/navigation/navigation-link.blade.php
@@ -1,6 +1,12 @@
-<a href="{{ $item['route'] }}" {{ $item['current'] ? 'aria-current="page"' : '' }}
+@php
+/** @var \Hyde\Framework\Modules\Navigation\NavigationMenuItemContract $item */
+/** @var \Hyde\Framework\Contracts\PageContract $page */
+$current = $page->getRoute() === $item->getRoute();
+@endphp
+
+<a href="{{ $item->getRoute()->getOutputFilePath() }}" {{ $current ? 'aria-current="page"' : '' }}
 	@class(['block my-2 md:my-0 md:inline-block py-1 text-gray-700 hover:text-gray-900 dark:text-gray-100'
-	, 'border-l-4 border-indigo-500 md:border-none font-medium -ml-6 pl-5 md:ml-0 md:pl-0 bg-gray-100 dark:bg-gray-800 md:bg-transparent dark:md:bg-transparent'=> $item['current']
+	, 'border-l-4 border-indigo-500 md:border-none font-medium -ml-6 pl-5 md:ml-0 md:pl-0 bg-gray-100 dark:bg-gray-800 md:bg-transparent dark:md:bg-transparent'=> $current
 	])>
-	{{ $item['title'] }}
+    {{ $item->navigationMenuTitle() }}
 </a>
