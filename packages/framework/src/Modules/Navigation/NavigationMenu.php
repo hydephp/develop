86c31ec772a1a117b2016b001a2d6d4164320aff
Caen De Silva <caen@desilva.se>
2022-07-05 09:57:33 +0200

    Extend the Collection class

--- packages/framework/src/Modules/Navigation/NavigationMenu.php
+++ packages/framework/src/Modules/Navigation/NavigationMenu.php
@@ -3,14 +3,17 @@
 namespace Hyde\Framework\Modules\Navigation;
 
 use Hyde\Framework\Modules\Routing\Route;
+use Illuminate\Support\Collection;
 
-class NavigationMenu
+class NavigationMenu extends Collection
 {
     protected Route $route;
 
     public function __construct(Route $route)
     {
         $this->route = $route;
+
+        parent::__construct();
     }
 
     public static function create(Route $currentRoute): static
