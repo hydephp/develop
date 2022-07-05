90e905e700179aef69c19ed6eb4b5bb6e3ae12c1
Caen De Silva <caen@desilva.se>
2022-07-05 13:35:18 +0200

    Remove granular method coverage annotations

--- packages/framework/tests/Feature/RouteFacadeTest.php
+++ packages/framework/tests/Feature/RouteFacadeTest.php
@@ -12,26 +12,22 @@ use Hyde\Testing\TestCase;
  */
 class RouteFacadeTest extends TestCase
 {
-    /** @covers Route::get */
     public function test_route_facade_get_method_calls_get_method()
     {
         $this->assertEquals(BaseRoute::get('index'), Route::get('index'));
     }
 
-    /** @covers Route::getFromKey */
     public function test_route_facade_get_from_key_method_calls_get_from_key_method()
     {
         $this->assertEquals(BaseRoute::getFromKey('index'), Route::getFromKey('index'));
     }
 
-    /** @covers Route::getFromSource */
     public function test_route_facade_get_from_source_method_calls_get_from_source_method()
     {
         $this->assertEquals(BaseRoute::getFromSource('_pages/index.blade.php'),
                Route::getFromSource('_pages/index.blade.php'));
     }
 
-    /** @covers Route::getFromModel */
     public function test_route_facade_get_from_model_method_calls_get_from_model_method()
     {
         $page = new BladePage('index');
