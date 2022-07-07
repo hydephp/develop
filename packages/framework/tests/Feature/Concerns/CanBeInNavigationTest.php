6eafafe0aa08e0f1a4d2501212ba7988227f29d9
Caen De Silva <caen@desilva.se>
2022-07-07 19:24:17 +0200

    Remove Copilot comments

--- packages/framework/tests/Feature/Concerns/CanBeInNavigationTest.php
+++ packages/framework/tests/Feature/Concerns/CanBeInNavigationTest.php
@@ -2,7 +2,6 @@
 
 namespace Hyde\Framework\Testing\Feature\Concerns;
 
-use Hyde\Framework\Concerns\CanBeInNavigation;
 use Hyde\Framework\Contracts\AbstractMarkdownPage;
 use Hyde\Framework\Models\MarkdownDocument;
 use Hyde\Framework\Models\Pages\MarkdownPage;
@@ -15,7 +14,6 @@ use Hyde\Framework\Models\Pages\DocumentationPage;
  */
 class CanBeInNavigationTest extends TestCase
 {
-    // test showInNavigation returns false for MarkdownPost
     public function test_show_in_navigation_returns_false_for_markdown_post()
     {
         $page = $this->mock(MarkdownPost::class)->makePartial();
@@ -23,7 +21,6 @@ class CanBeInNavigationTest extends TestCase
         $this->assertFalse($page->showInNavigation());
     }
 
-    // test showInNavigation returns true for DocumentationPage if slug is index
     public function test_show_in_navigation_returns_true_for_documentation_page_if_slug_is_index()
     {
         $page = $this->mock(DocumentationPage::class)->makePartial();
@@ -32,7 +29,6 @@ class CanBeInNavigationTest extends TestCase
         $this->assertTrue($page->showInNavigation());
     }
 
-    // test showInNavigation returns false for DocumentationPage if slug is not index
     public function test_show_in_navigation_returns_false_for_documentation_page_if_slug_is_not_index()
     {
         $page = $this->mock(DocumentationPage::class)->makePartial();
@@ -41,7 +37,6 @@ class CanBeInNavigationTest extends TestCase
         $this->assertFalse($page->showInNavigation());
     }
 
-    // test showInNavigation returns false for AbstractMarkdownPage if matter('navigation.hidden', false) is true
     public function test_show_in_navigation_returns_false_for_abstract_markdown_page_if_matter_navigation_hidden_is_true()
     {
         $page = $this->mock(AbstractMarkdownPage::class)->makePartial();
@@ -51,7 +46,6 @@ class CanBeInNavigationTest extends TestCase
         $this->assertFalse($page->showInNavigation());
     }
 
-    // test showInNavigation returns true for AbstractMarkdownPage if matter('navigation.hidden', false) is false
     public function test_show_in_navigation_returns_true_for_abstract_markdown_page_if_matter_navigation_hidden_is_false()
     {
         $page = $this->mock(AbstractMarkdownPage::class)->makePartial();
@@ -62,7 +56,6 @@ class CanBeInNavigationTest extends TestCase
         $this->assertTrue($page->showInNavigation());
     }
 
-    // test showInNavigation returns true for AbstractMarkdownPage if matter('navigation.hidden', false) is not set
     public function test_show_in_navigation_returns_true_for_abstract_markdown_page_if_matter_navigation_hidden_is_not_set()
     {
         $page = $this->mock(AbstractMarkdownPage::class)->makePartial();
@@ -73,7 +66,6 @@ class CanBeInNavigationTest extends TestCase
         $this->assertTrue($page->showInNavigation());
     }
 
-    // test showInNavigation returns false if slug is present in config('hyde.navigation.exclude', ['404'])
     public function test_show_in_navigation_returns_false_if_slug_is_present_in_config_hyde_navigation_exclude()
     {
         $page = $this->mock(MarkdownPage::class)->makePartial();
@@ -86,7 +78,6 @@ class CanBeInNavigationTest extends TestCase
         $this->assertFalse($page->showInNavigation());
     }
 
-    // test showInNavigation returns false if slug is 404
     public function test_show_in_navigation_returns_true_if_slug_is_404()
     {
         $page = $this->mock(MarkdownPage::class)->makePartial();
@@ -96,7 +87,6 @@ class CanBeInNavigationTest extends TestCase
         $this->assertFalse($page->showInNavigation());
     }
 
-    // test showInNavigation defaults to true if all checks pass
     public function test_show_in_navigation_defaults_to_true_if_all_checks_pass()
     {
         $page = $this->mock(MarkdownPage::class)->makePartial();
@@ -106,7 +96,6 @@ class CanBeInNavigationTest extends TestCase
         $this->assertTrue($page->showInNavigation());
     }
 
-    // test navigationMenuPriority returns front matter value of navigation.priority if AbstractMarkdownPage and not null
     public function test_navigation_menu_priority_returns_front_matter_value_of_navigation_priority_if_abstract_markdown_page_and_not_null()
     {
         $page = $this->mock(AbstractMarkdownPage::class)->makePartial();
@@ -115,7 +104,6 @@ class CanBeInNavigationTest extends TestCase
         $this->assertEquals(1, $page->navigationMenuPriority());
     }
 
-    // test navigationMenuPriority returns specified config value if slug exists in config('hyde.navigation.order', [])
     public function test_navigation_menu_priority_returns_specified_config_value_if_slug_exists_in_config_hyde_navigation_order()
     {
         $page = $this->mock(MarkdownPage::class)->makePartial();
@@ -128,7 +116,6 @@ class CanBeInNavigationTest extends TestCase
         $this->assertEquals(1, $page->navigationMenuPriority());
     }
 
-    // test navigationMenuPriority gives precedence to front matter over config('hyde.navigation.order', [])
     public function test_navigation_menu_priority_gives_precedence_to_front_matter_over_config_hyde_navigation_order()
     {
         $page = $this->mock(AbstractMarkdownPage::class)->makePartial();
@@ -142,7 +129,6 @@ class CanBeInNavigationTest extends TestCase
         $this->assertEquals(1, $page->navigationMenuPriority());
     }
 
-    // test navigationMenuPriority returns 100 for DocumentationPage
     public function test_navigation_menu_priority_returns_100_for_documentation_page()
     {
         $page = $this->mock(DocumentationPage::class)->makePartial();
@@ -152,7 +138,6 @@ class CanBeInNavigationTest extends TestCase
         $this->assertEquals(100, $page->navigationMenuPriority());
     }
 
-    // test navigationMenuPriority returns 0 if slug is index
     public function test_navigation_menu_priority_returns_0_if_slug_is_index()
     {
         $page = $this->mock(MarkdownPage::class)->makePartial();
@@ -162,7 +147,6 @@ class CanBeInNavigationTest extends TestCase
         $this->assertEquals(0, $page->navigationMenuPriority());
     }
 
-    // test navigationMenuPriority does not return 0 if slug is index but model is documentation page
     public function test_navigation_menu_priority_does_not_return_0_if_slug_is_index_but_model_is_documentation_page()
     {
         $page = $this->mock(DocumentationPage::class)->makePartial();
@@ -172,7 +156,6 @@ class CanBeInNavigationTest extends TestCase
         $this->assertEquals(100, $page->navigationMenuPriority());
     }
 
-    // test navigationMenuPriority returns 10 if slug is posts
     public function test_navigation_menu_priority_returns_10_if_slug_is_posts()
     {
         $page = $this->mock(MarkdownPage::class)->makePartial();
@@ -182,7 +165,6 @@ class CanBeInNavigationTest extends TestCase
         $this->assertEquals(10, $page->navigationMenuPriority());
     }
 
-    // test navigationMenuPriority defaults to 1000 if no other conditions are met
     public function test_navigation_menu_priority_defaults_to_1000_if_no_other_conditions_are_met()
     {
         $page = $this->mock(MarkdownPage::class)->makePartial();
@@ -192,7 +174,6 @@ class CanBeInNavigationTest extends TestCase
         $this->assertEquals(1000, $page->navigationMenuPriority());
     }
 
-    // test navigationMenuTitle if AbstractMarkdownPage and matter('navigation.title') !== null) should return navigation.title matter
     public function test_navigation_menu_title_returns_navigation_title_matter_if_set()
     {
         $page = $this->mock(AbstractMarkdownPage::class)->makePartial();
@@ -201,7 +182,6 @@ class CanBeInNavigationTest extends TestCase
         $this->assertEquals('foo', $page->navigationMenuTitle());
     }
 
-    // test navigationMenuTitle if AbstractMarkdownPage and matter('title') !== null) should return title matter
     public function test_navigation_menu_title_returns_title_matter_if_set()
     {
         $page = $this->mock(AbstractMarkdownPage::class)->makePartial();
@@ -210,7 +190,6 @@ class CanBeInNavigationTest extends TestCase
         $this->assertEquals('foo', $page->navigationMenuTitle());
     }
 
-    // test navigationMenuTitle navigation.title has precedence over title
     public function test_navigation_menu_title_navigation_title_has_precedence_over_title()
     {
         $page = $this->mock(AbstractMarkdownPage::class)->makePartial();
@@ -219,7 +198,6 @@ class CanBeInNavigationTest extends TestCase
         $this->assertEquals('bar', $page->navigationMenuTitle());
     }
 
-    // test navigationMenuTitle if slug === index and model instance of documentation page should return docs
     public function test_navigation_menu_title_returns_docs_if_slug_is_index_and_model_is_documentation_page()
     {
         $page = $this->mock(DocumentationPage::class)->makePartial();
@@ -229,7 +207,6 @@ class CanBeInNavigationTest extends TestCase
         $this->assertEquals('Docs', $page->navigationMenuTitle());
     }
 
-    // test navigationMenuTitle if slug === index and model not instance of documentation page should return home
     public function test_navigation_menu_title_returns_home_if_slug_is_index_and_model_is_not_documentation_page()
     {
         $page = $this->mock(MarkdownPage::class)->makePartial();
@@ -239,7 +216,6 @@ class CanBeInNavigationTest extends TestCase
         $this->assertEquals('Home', $page->navigationMenuTitle());
     }
 
-    // test navigationMenuTitle if title isset and is not empty should return title
     public function test_navigation_menu_title_returns_title_if_title_is_set_and_not_empty()
     {
         $page = $this->mock(MarkdownPage::class)->makePartial();
@@ -250,7 +226,6 @@ class CanBeInNavigationTest extends TestCase
         $this->assertEquals('foo', $page->navigationMenuTitle());
     }
 
-    // test navigationMenuTitle if no other conditions are met should fall back to Hyde::makeTitle from slug
     public function test_navigation_menu_title_falls_back_to_hyde_make_title_from_slug()
     {
         $page = $this->mock(MarkdownPage::class)->makePartial();
