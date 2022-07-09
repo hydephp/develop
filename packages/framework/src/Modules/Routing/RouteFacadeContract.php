f22e4b83b89c469f4269ec1bd59cce7c8102c1c7
Caen De Silva <caen@desilva.se>
2022-07-09 18:11:23 +0200

    The facade helpers must not return null, fix https://github.com/hydephp/develop/issues/199

--- packages/framework/src/Modules/Routing/RouteFacadeContract.php
+++ packages/framework/src/Modules/Routing/RouteFacadeContract.php
@@ -18,33 +18,33 @@ interface RouteFacadeContract
      * Alias for static::getFromKey().
      *
      * @param  string  $routeKey  Example: posts/foo.md
-     * @return \Hyde\Framework\Modules\Routing\RouteContract|null
+     * @return \Hyde\Framework\Modules\Routing\RouteContract
      */
-    public static function get(string $routeKey): ?RouteContract;
+    public static function get(string $routeKey): RouteContract;
 
     /**
      * Get a route from the Router index for the specified route key.
      *
      * @param  string  $routeKey  Example: posts/foo.md
-     * @return \Hyde\Framework\Modules\Routing\RouteContract|null
+     * @return \Hyde\Framework\Modules\Routing\RouteContract
      */
-    public static function getFromKey(string $routeKey): ?RouteContract;
+    public static function getFromKey(string $routeKey): RouteContract;
 
     /**
      * Get a route from the Router index for the specified source file path.
      *
      * @param  string  $sourceFilePath  Example: _posts/foo.md
-     * @return \Hyde\Framework\Modules\Routing\RouteContract|null
+     * @return \Hyde\Framework\Modules\Routing\RouteContract
      */
-    public static function getFromSource(string $sourceFilePath): ?RouteContract;
+    public static function getFromSource(string $sourceFilePath): RouteContract;
 
     /**
      * Get a route from the Router index for the supplied page model.
      *
      * @param  \Hyde\Framework\Contracts\PageContract  $page
-     * @return \Hyde\Framework\Modules\Routing\RouteContract|null
+     * @return \Hyde\Framework\Modules\Routing\RouteContract
      */
-    public static function getFromModel(PageContract $page): ?RouteContract;
+    public static function getFromModel(PageContract $page): RouteContract;
 
     /**
      * Get all routes from the Router index.
