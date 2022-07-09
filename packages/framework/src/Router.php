1987d430ce58c5d68f1aa2e3823cacf8f98e4dc0
Caen De Silva <caen@desilva.se>
2022-07-09 21:21:55 +0200

    Update introduction tenses

--- packages/framework/src/Router.php
+++ packages/framework/src/Router.php
@@ -19,8 +19,8 @@ use Illuminate\Support\Collection;
  * This is not a router in the traditional sense that it decides where to go.
  * Instead, it creates a pre-generated object encapsulating the Hyde autodiscovery.
  *
- * If successful, this will not only let us emulate Laravel route helpers, but also
- * serve as the canonical source of truth for the Hyde autodiscovery process.
+ * This not only let us emulate Laravel route helpers, but also serve as the
+ * canonical source of truth for the vital HydePHP autodiscovery process.
  *
  * The routes defined can then also be used to power the RealtimeCompiler without
  * having to reverse-engineer the source file mapping.
@@ -28,7 +28,7 @@ use Illuminate\Support\Collection;
  * Routes cannot be added manually, instead the route index is created using the
  * exact same rules as the current autodiscovery process and compiled file output.
  *
- * The route index shall serve as a multidimensional mapping allowing you to
+ * The route index serves as a multidimensional mapping allowing you to
  * determine where a source file will be compiled to, and where a compiled
  * file was generated from.
  *
