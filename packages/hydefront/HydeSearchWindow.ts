f28cf87e5d166f49194a7f590c900e7b5722845e
Caen De Silva <caen@desilva.se>
2022-07-14 21:39:59 +0200

    Create HydeSearchWindow.ts

---
+++ packages/hydefront/HydeSearchWindow.ts
@@ -0,0 +1,7 @@
+/**
+ * Provides a search window modal for HydePHP documentation pages.
+ *
+ * @package     HydePHP - HydeFront/HydeSearchWindow
+ * @version     v1.13.x (HydeFront)
+ * @author      Caen De Silva
+ */
