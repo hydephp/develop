6dfd4a3ea683f91cff40cde37f5a811322b7fe2b
Caen De Silva <caen@desilva.se>
2022-07-15 19:14:43 +0200

    Remove $currentPage property from route helper

--- packages/framework/src/Models/Route.php
+++ packages/framework/src/Models/Route.php
@@ -67,9 +67,9 @@ class Route implements RouteContract, RouteFacadeContract
     }
 
     /** @inheritDoc */
-    public function getLink(string $currentPage = ''): string
+    public function getLink(): string
     {
-        return Hyde::relativeLink($this->getOutputFilePath(), $currentPage);
+        return Hyde::relativeLink($this->getOutputFilePath());
     }
 
     /** @inheritDoc */
