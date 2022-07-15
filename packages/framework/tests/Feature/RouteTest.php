7b8da6b4d675ba3d61fc1cb4cfa095e3b186787a
Caen De Silva <caen@desilva.se>
2022-07-15 18:46:54 +0200

    Remove Copilot comments

--- packages/framework/tests/Feature/RouteTest.php
+++ packages/framework/tests/Feature/RouteTest.php
@@ -135,7 +135,6 @@ class RouteTest extends TestCase
         $this->assertEquals(Router::getInstance()->getRoutes(), Route::all());
     }
 
-    // test getLink uses hyde::relativeLink() to get the correct path for root pages
     public function test_get_link_returns_correct_path_for_root_pages()
     {
         $route = new Route(new MarkdownPage(slug: 'foo'));
@@ -143,7 +142,6 @@ class RouteTest extends TestCase
         $this->assertEquals('foo.html', $route->getLink());
     }
 
-    // test getLink uses hyde::relativeLink() to get the correct path for nested pages
     public function test_get_link_returns_correct_path_for_nested_pages()
     {
         $route = new Route(new MarkdownPage(slug: 'foo/bar'));
@@ -158,7 +156,6 @@ class RouteTest extends TestCase
         $this->assertEquals('../foo.html', $route->getLink('foo/bar'));
     }
 
-    // test getLink returns pretty url if enabled
     public function test_get_link_returns_pretty_url_if_enabled()
     {
         config(['hyde.pretty_urls' => true]);
@@ -167,7 +164,6 @@ class RouteTest extends TestCase
         $this->assertEquals('foo', $route->getLink());
     }
 
-    // test to string is alias for getLink
     public function test_to_string_is_alias_for_get_link()
     {
         $route = new Route(new MarkdownPage(slug: 'foo'));
