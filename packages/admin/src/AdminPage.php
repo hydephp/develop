12ada00a073254fe827bf1be117fd30325e11ebf
Caen De Silva <caen@desilva.se>
2022-07-16 20:52:18 +0200

    Add placeholder $renderable property

--- packages/admin/src/AdminPage.php
+++ packages/admin/src/AdminPage.php
@@ -7,6 +7,9 @@ use Hyde\Framework\Models\Pages\BladePage;
 
 final class AdminPage extends BladePage implements PageContract
 {
+    /** Not yet implemented, but could be used by the framework to ignore this when compiling */
+    protected static bool $renderable = false;
+
     public function getCurrentPagePath(): string
     {
         return 'admin';
