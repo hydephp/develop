a8e303547e335a6af86cd47f0b103d677be968d7
Caen De Silva <caen@desilva.se>
2022-07-16 21:25:54 +0200

    Remove boilerplate

--- packages/admin/src/AdminServiceProvider.php
+++ packages/admin/src/AdminServiceProvider.php
@@ -2,62 +2,14 @@
 
 namespace Hyde\Admin;
 
-use Hyde\Framework\Hyde;
-use Hyde\Framework\Services\RoutingService;
 use Illuminate\Support\ServiceProvider;
+use Hyde\Framework\Services\RoutingService;
 
 class AdminServiceProvider extends ServiceProvider
 {
-    /**
-     * Register the application services.
-     */
-    public function register()
-    {
-        // Automatically apply the package configuration
-        $this->mergeConfigFrom(__DIR__.'/../config/config.php', 'admin');
-
-        // Register the main class to use with the facade
-        $this->app->singleton('admin', function () {
-            return new Admin;
-        });
-    }
-
-    /**
-     * Bootstrap the application services.
-     */
     public function boot()
     {
-        /*
-         * Optional methods to load your package assets
-         */
-        // $this->loadTranslationsFrom(__DIR__.'/../resources/lang', 'admin');
-         $this->loadViewsFrom(__DIR__.'/../resources/views', 'hyde-admin');
-        // $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
-        // $this->loadRoutesFrom(__DIR__.'/routes.php');
-
-        if ($this->app->runningInConsole()) {
-            $this->publishes([
-                __DIR__.'/../config/config.php' => config_path('admin.php'),
-            ], 'config');
-
-            // Publishing the views.
-            /*$this->publishes([
-                __DIR__.'/../resources/views' => resource_path('views/vendor/admin'),
-            ], 'views');*/
-
-            // Publishing assets.
-            /*$this->publishes([
-                __DIR__.'/../resources/assets' => public_path('vendor/admin'),
-            ], 'assets');*/
-
-            // Publishing the translation files.
-            /*$this->publishes([
-                __DIR__.'/../resources/lang' => resource_path('lang/vendor/admin'),
-            ], 'lang');*/
-
-            // Registering package commands.
-            // $this->commands([]);
-        }
+        $this->loadViewsFrom(__DIR__.'/../resources/views', 'hyde-admin');
 
         RoutingService::getInstance()->addRoute(
             (new AdminPage('hyde-admin::dashboard'))->getRoute()
