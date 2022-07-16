45a795ab0a71e50e450f3dc3d299a58c3142fd50
Caen De Silva <caen@desilva.se>
2022-07-16 12:37:29 +0200

    Remove removed property

--- packages/framework/resources/views/components/navigation/navigation-brand.blade.php
+++ packages/framework/resources/views/components/navigation/navigation-brand.blade.php
@@ -1,3 +1,3 @@
-<a aria-label="Home page link" href="{{ $navigation->getHomeLink($currentPage) }}" class="font-bold px-4">
+<a aria-label="Home page link" href="{{ $navigation->getHomeLink() }}" class="font-bold px-4">
 	{{ config('hyde.name', 'HydePHP') }}
 </a>
