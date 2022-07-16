05e7e0237c50a91bfb8167f31d9762d4c23264d5
Caen De Silva <caen@desilva.se>
2022-07-16 22:47:47 +0200

    Update navigation

--- packages/admin/resources/views/dashboard.blade.php
+++ packages/admin/resources/views/dashboard.blade.php
@@ -40,7 +40,7 @@
             @include('hyde-admin::components.navigation')
 
             <!-- Header -->
-            <div class="relative bg-pink-600 md:pt-32 pb-32 pt-12">
+            <div class="relative bg-pink-600 md:pt-24 pb-32 pt-12">
                 <div class="px-4 md:px-10 mx-auto w-full">
                     <div>
                         <!-- Card stats -->
