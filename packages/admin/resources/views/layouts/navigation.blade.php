b3185de5a1ceb3b7e98ce24ae0974b0660b95d0a
Caen De Silva <caen@desilva.se>
2022-07-17 17:07:55 +0200

    Link to the dashboard

--- packages/admin/resources/views/layouts/navigation.blade.php
+++ packages/admin/resources/views/layouts/navigation.blade.php
@@ -1,7 +1,7 @@
 <nav
 	class="absolute py-8 top-0 left-0 w-full z-10 bg-transparent md:flex-row md:flex-nowrap md:justify-start flex items-center p-4">
 	<div class="w-full mx-autp items-center flex justify-between md:flex-nowrap flex-wrap md:px-10 px-4">
-		<a class="text-white text-sm uppercase hidden lg:inline-block font-semibold" href="./index.html">Dashboard</a>
+		<a class="text-white text-sm uppercase hidden lg:inline-block font-semibold" href="?route=dashboard">Dashboard</a>
 		<a class="text-white text-sm uppercase hidden lg:inline-block font-semibold" href="{{ Route::home() }}">Back to site</a>
 		{{-- <form class="md:flex hidden flex-row flex-wrap items-center lg:ml-auto mr-3">
 			<div class="relative flex w-full flex-wrap items-stretch">
