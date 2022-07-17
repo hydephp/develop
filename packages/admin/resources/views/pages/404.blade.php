e299cb9572796f2f208d0f2b1612ee79f089c6c0
Caen De Silva <caen@desilva.se>
2022-07-17 12:16:50 +0200

    Update 404.blade.php

--- packages/admin/resources/views/pages/404.blade.php
+++ packages/admin/resources/views/pages/404.blade.php
@@ -1,4 +1,11 @@
-<article class="prose">
-    <h1>404 - Not found</h1>
-    <p class="lead">Sorry, that page could not be found.</p>
-</article>
+<x-hyde-admin::header>
+    <x-hyde-admin::card>
+        <article class="prose">
+            <h2>404 - Not found</h2>
+            <p class="lead">Sorry, that page could not be found.</p>
+
+            <a href="?route=dashboard" class="mr-3">Back to dashboard</a>
+            <a href="{{ Route::home() }}">Back to site</a>
+        </article>
+    </x-hyde-admin::card>
+</x-hyde-admin::header>
