3604714c94911d130e88706ad75f58d83a237bc6
Caen De Silva <caen@desilva.se>
2022-07-17 15:39:50 +0200

    Add attributes->merge

--- packages/admin/resources/views/components/card.blade.php
+++ packages/admin/resources/views/components/card.blade.php
@@ -1,3 +1,3 @@
-<div class="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 px-4 py-3 shadow-lg rounded">
+<div {{ $attributes->merge(['class' => 'relative flex flex-col min-w-0 break-words bg-white w-full mb-6 px-6 py-4 shadow-lg rounded']) }}>
     {{ $slot }}
 </div>
