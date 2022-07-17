92e8f6d175804a4177e34b3fc034ff08675e4dc6
Caen De Silva <caen@desilva.se>
2022-07-17 17:05:29 +0200

    Add Image Gallery

--- packages/admin/resources/views/pages/media.blade.php
+++ packages/admin/resources/views/pages/media.blade.php
@@ -12,8 +12,8 @@ use Hyde\Framework\Services\CollectionService;
 </x-hyde-admin::header>
 
 <div class="px-4 md:px-10 mx-auto w-full -m-28">
-	<div class="flex">
-		<x-hyde-admin::card class="w-full">
+	<div class="flex -mx-3">
+		<x-hyde-admin::card class="w-2/3 mx-3">
 		<div class="rounded-t mb-3 px-0 py-3 border-0">
 			<div class="flex flex-wrap items-center">
 				<div class="relative w-full px-0 max-w-full flex-grow flex-1">
@@ -86,5 +86,33 @@ use Hyde\Framework\Services\CollectionService;
 			</table>
 		</div>
 	</x-hyde-admin::card>
-</div>
+	
+	<x-hyde-admin::card class="w-1/3 mx-3">
+		<div class="rounded-t mb-3 px-0 py-3 border-0">
+			<div class="flex flex-wrap items-center">
+				<div class="relative w-full px-0 max-w-full flex-grow flex-1">
+					<h3 class="font-semibold text-lg text-slate-700"> Image Gallery </h3>
+				</div>
+				<div class="relative w-full px-0 max-w-full flex-grow flex-1 text-right">
+					{{-- <button
+						class="bg-indigo-500 text-white active:bg-indigo-600 text-xs font-bold uppercase px-3 py-1 rounded outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+						type="button">
+						Upload
+					</button> --}}
+				</div>
+			</div>
+		</div>
+		<div class="block w-full overflow-x-auto prose max-w-full">
+			<div class="flex flex-row flex-wrap">
+				@foreach (CollectionService::getMediaAssetFiles() as $file)
+					@if(in_array(pathinfo($file, PATHINFO_EXTENSION), ['jpg', 'jpeg', 'png', 'gif', 'ico', 'svg']))
+						<a href="media/{{ basename($file) }}">
+							<img width="64px" height="auto" class="block" src="media/{{ basename($file) }}" alt="{{ basename($file) }}" title="{{ basename($file) }}">
+						</a>
+					@endif
+				@endforeach
+			</div>
+		</div>
+	</x-hyde-admin::card>
+	</div>
 </div>
