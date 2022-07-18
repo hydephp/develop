54ff9b0adbaf0e13ff992808239fa5872782c92b
Caen De Silva <caen@desilva.se>
2022-07-18 11:28:50 +0200

    Remove config option hyde.pretty_urls

--- config/hyde.php
+++ config/hyde.php
@@ -42,9 +42,6 @@ return [
     */
 
     /** @deprecated v0.50.x - Use equivalent in site.php */
-    'pretty_urls' => false,
-
-    /** @deprecated v0.50.x - Use equivalent in site.php */
     'generate_sitemap' => true,
 
     /*
