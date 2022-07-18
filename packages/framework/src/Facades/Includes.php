b63cea02bac524f5aaf40b2909576815803fce21
Caen De Silva <caen@desilva.se>
2022-07-18 17:25:24 +0200

    Create Includes.php

---
+++ packages/framework/src/Facades/Includes.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Hyde\Framework\Facades;
+
+class Includes implements \Hyde\Framework\Contracts\IncludeFacadeContract
+{
+
+    /**
+     * @inheritDoc
+     */
+    public static function get(string $partial, ?string $default = null): ?string
+    {
+        // TODO: Implement get() method.
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public static function markdown(string $partial, ?string $default = null): ?string
+    {
+        // TODO: Implement markdown() method.
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public static function blade(string $partial, ?string $default = null): ?string
+    {
+        // TODO: Implement blade() method.
+    }
+}
