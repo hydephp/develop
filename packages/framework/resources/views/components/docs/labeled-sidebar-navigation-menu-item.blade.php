e5d8c59e5fcb4898e7aff8f7d9d91baa17513ab9
Caen De Silva <caen@desilva.se>
2022-07-19 15:39:10 +0200

    Begin refactor to use Tailwind for docs layouts

---
+++ packages/framework/resources/views/components/docs/labeled-sidebar-navigation-menu-item.blade.php
@@ -0,0 +1,22 @@
+@props([
+'item',
+'active' => false,
+])
+
+<li @class([ 'sidebar-navigation-item -ml-4 pl-4' , 'active -ml-8 pl-8 bg-darken'=> $active]) {!! $active ? '' : '' !!} role="listitem">
+	@if(! $active)
+	<a class="-ml-8 pl-4 py-1 px-2  block  border-l-[0.325rem] border-transparent transition-colors duration-300	ease-in-out hover:bg-darken	"
+		href="{{ Hyde::pageLink($item->destination . '.html') }}">{{ $item->label }}</a>
+
+	@else
+
+	<a class="-ml-8 pl-4 py-1 px-2  block font-medium text-indigo-400 border-l-[0.325rem] border-indigo-500 transition-colors duration-300	ease-in-out	" 
+		href="{{ Hyde::pageLink($item->destination . '.html') }}" aria-current="true">{{ $item->label }}</a>
+
+
+	@if(isset($page->tableOfContents))
+	<span class="sr-only">Table of contents</span>
+	{!! ($page->tableOfContents) !!}
+	@endif
+	@endif
+</li>
--- packages/framework/resources/views/layouts/docs.blade.php
+++ packages/framework/resources/views/layouts/docs.blade.php
@@ -2,16 +2,22 @@
 <html lang="{{ config('site.language', 'en') }}">
 <head>
     @include('hyde::layouts.head')
+
+	<style>
+		html [id], body [id] {
+			scroll-margin: 1rem;
+		}
+	</style>
 </head>
 	
-<body id="hyde-docs" class="bg-white dark:bg-gray-900">
+<body id="hyde-docs" class="bg-white dark:bg-gray-900 dark:text-white min-h-screen w-screen relative overflow-x-hidden overflow-y-auto">
 	<a href="#content" id="skip-to-content">Skip to content</a>
 	
 	<script>
 		document.body.classList.add('js-enabled');
 	</script>
 
-	<nav id="mobile-navigation">
+	<nav id="mobile-navigation" class="dark:bg-gray-800 hidden">
 		<strong class="mr-auto">
 			@if(DocumentationPage::indexPath() !== false)
 			<a href="{{ Hyde::relativeLink(DocumentationPage::indexPath(), $currentPage) }}">
@@ -23,16 +29,16 @@
 		</strong>
         @include('hyde::components.navigation.theme-toggle-button')
 		<button id="sidebar-toggle" title="Toggle sidebar" aria-label="Toggle sidebar navigation menu">
-			<span class="icon-bar" role="presentation"></span>
-			<span class="icon-bar" role="presentation"></span>
-			<span class="icon-bar" role="presentation"></span>
-			<span class="icon-bar" role="presentation"></span>
+			<span class="icon-bar dark:bg-white h-0" role="presentation"></span>
+			<span class="icon-bar dark:bg-white h-0" role="presentation"></span>
+			<span class="icon-bar dark:bg-white h-0" role="presentation"></span>
+			<span class="icon-bar dark:bg-white h-0" role="presentation"></span>
 		</button>
 	</nav>
-	<aside id="sidebar">
-		<header id="sidebar-header">
-			<div id="sidebar-brand">
-				<strong>
+	<aside id="sidebar" class="bg-gray-100 dark:bg-gray-800 dark:text-gray-200 h-screen w-64 fixed z-10">
+		<header id="sidebar-header" class="h-16">
+			<div id="sidebar-brand" class="flex items-center justify-between h-16 py-4 px-2">
+				<strong class="px-2">
 					@if(DocumentationPage::indexPath() !== false)
 					<a href="{{ Hyde::relativeLink(DocumentationPage::indexPath(), $currentPage) }}">
 						{{ config('docs.header_title', 'Documentation') }}
@@ -44,7 +50,7 @@
 				@include('hyde::components.navigation.theme-toggle-button')
 			</div>
 		</header>
-		<nav id="sidebar-navigation">
+		<nav id="sidebar-navigation" class="p-4 overflow-y-auto border-y border-gray-300 dark:border-[#1b2533]" style="height: calc(100vh - 8rem);">
 			@php
 				$sidebar = Hyde\Framework\Services\DocumentationSidebarService::create();
 			@endphp
@@ -55,28 +61,33 @@
 			@include('hyde::components.docs.sidebar-navigation-menu')
 			@endif
 		</nav>
-		<footer id="sidebar-footer">
+		<footer id="sidebar-footer" class="h-16 absolute p-4 w-full bottom-0 left-0 text-center leading-8">
 			<p>
 				<a href="{{ Hyde::relativeLink('index.html', $currentPage) }}">Back to home page</a>
 			</p>
 		</footer>
 	</aside>
-	<main id="content">
+	<main id="content" class="dark:bg-gray-900 min-h-screen bg-white absolute" style="width: calc(100vw - 16rem);
+left: 16rem;">
 		@php
 		$document = \Hyde\Framework\Services\HydeSmartDocs::create($page, $markdown);
 		@endphp
 		<article id="document" itemscope itemtype="http://schema.org/Article" @class(['mx-auto lg:ml-8 prose dark:prose-invert
-			max-w-3xl', 'torchlight-enabled'=> $document->hasTorchlight()])>
+			max-w-3xl', 'torchlight-enabled'=> $document->hasTorchlight()]) style="padding: 3rem 4rem;
+max-width: 1000px;
+min-height: calc(100vh - 4rem);">
+			<style>
 			
+			</style>
 			@yield('content')
 
-			<header id="document-header">
+			<header id="document-header" class="flex items-center flex-wrap">
 				{!! $document->renderHeader() !!}
 			</header>
 			<section id="document-main-content" itemprop="articleBody">
 				{!! $document->renderBody() !!}
 			</section>
-			<footer id="document-footer">
+			<footer id="document-footer" class="flex items-center flex-wrap mt-8 justify-between" style="font-size: 90%;">
 				{!! $document->renderFooter() !!}
 			</footer>
 		</article>
