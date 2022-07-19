3ddf2e54a2e5c1959f394e4dd719b837eefb8bd6
Caen De Silva <caen@desilva.se>
2022-07-19 20:33:40 +0200

    Conditionally load the Dusk service provider

--- packages/framework/src/HydeServiceProvider.php
+++ packages/framework/src/HydeServiceProvider.php
@@ -118,5 +118,9 @@ class HydeServiceProvider extends ServiceProvider
     protected function registerModuleServiceProviders(): void
     {
         $this->app->register(Modules\DataCollections\DataCollectionServiceProvider::class);
+
+        if (env('DUSK_ENABLED', false)){
+            $this->app->register(\Laravel\Dusk\DuskServiceProvider::class);
+        }
     }
 }
