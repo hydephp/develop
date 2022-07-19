cd2c280280b7d1ac4e7b46a01766c4ec48ce9865
Caen De Silva <caen@desilva.se>
2022-07-19 19:07:55 +0200

    Clean up code

--- tests/Cypress/cypress/e2e/bravo.cy.js
+++ tests/Cypress/cypress/e2e/bravo.cy.js
@@ -1,24 +1,19 @@
 before(() => {
-	// root-level hook
-	// runs once before all tests
 	cy.exec('php ../../hyde publish:homepage posts -n')
 })
   
-
 describe('tests blogging module', () => {
   it('homepage can be changed to blog post feed', () => {
     cy.visit('index.html')
     cy.contains('Latest Posts')
   })
 
-  // blog posts can be scaffolded
   it('blog posts can be scaffolded and shows up in feed', () => {
 	  cy.exec('php ../../hyde make:post -n --force')
     cy.visit('index.html')
 	  cy.get('.text-2xl').should('contain', 'My New Post')
   })
 
-  // blog posts can be clicked to lead to the post
   it('blog posts can be clicked to lead to the post', () => {
     cy.visit('index.html')
     cy.get('.text-2xl').click()
