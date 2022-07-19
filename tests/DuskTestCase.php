dae3874efaafd2c1c747cfe885bdc028bb3cf860
Caen De Silva <caen@desilva.se>
2022-07-19 19:39:08 +0200

    Use custom base test case

--- tests/DuskTestCase.php
+++ tests/DuskTestCase.php
@@ -5,7 +5,7 @@ namespace Hyde\Testing;
 use Facebook\WebDriver\Chrome\ChromeOptions;
 use Facebook\WebDriver\Remote\DesiredCapabilities;
 use Facebook\WebDriver\Remote\RemoteWebDriver;
-use Laravel\Dusk\TestCase as BaseTestCase;
+use Hyde\Testing\DuskBaseTestCase as BaseTestCase;
 
 abstract class DuskTestCase extends BaseTestCase
 {
