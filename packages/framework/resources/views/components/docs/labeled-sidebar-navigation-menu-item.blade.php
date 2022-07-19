a35e7c19fffb4fa4b9935c29bea62605f6ac5c51
Caen De Silva <caen@desilva.se>
2022-07-19 15:40:15 +0200

    Update labeled-sidebar-navigation-menu-item.blade.php

--- packages/framework/resources/views/components/docs/labeled-sidebar-navigation-menu-item.blade.php
+++ packages/framework/resources/views/components/docs/labeled-sidebar-navigation-menu-item.blade.php
@@ -7,14 +7,11 @@
 	@if(! $active)
 	<a class="-ml-8 pl-4 py-1 px-2  block  border-l-[0.325rem] border-transparent transition-colors duration-300	ease-in-out hover:bg-darken	"
 		href="{{ Hyde::pageLink($item->destination . '.html') }}">{{ $item->label }}</a>
-
 	@else
-
-	<a class="-ml-8 pl-4 py-1 px-2  block font-medium text-indigo-400 border-l-[0.325rem] border-indigo-500 transition-colors duration-300	ease-in-out	" 
+	<a class="-ml-8 pl-4 py-1 px-2  block font-medium text-indigo-400 border-l-[0.325rem] border-indigo-500 transition-colors duration-300	ease-in-out	"
 		href="{{ Hyde::pageLink($item->destination . '.html') }}" aria-current="true">{{ $item->label }}</a>
 
-
-	@if(isset($page->tableOfContents))
+	@isset($page->tableOfContents)
 	<span class="sr-only">Table of contents</span>
 	{!! ($page->tableOfContents) !!}
 	@endif
