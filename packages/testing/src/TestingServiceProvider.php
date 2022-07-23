6c55754d03714a67cec7894e4f23b8e90c7589c5
Caen De Silva <caen@desilva.se>
2022-07-23 14:47:58 +0200

    Registration comes before booting

--- packages/testing/src/TestingServiceProvider.php
+++ packages/testing/src/TestingServiceProvider.php
@@ -7,18 +7,18 @@ use Illuminate\Support\ServiceProvider;
 class TestingServiceProvider extends ServiceProvider
 {
     /**
-     * Bootstrap the application services.
-     */
-    public function boot()
-    {
-       //
-    }
-
-    /**
      * Register the application services.
      */
     public function register()
     {
         //
+    }
+
+    /**
+     * Bootstrap the application services.
+     */
+    public function boot()
+    {
+       //
     }
 }
