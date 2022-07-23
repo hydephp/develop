0144cb1ffd1d63e8c44c4de72248b6d094cee0d1
Caen De Silva <caen@desilva.se>
2022-07-23 15:41:15 +0200

    Register DuskServiceProvider in TestingServiceProvider

--- packages/testing/src/TestingServiceProvider.php
+++ packages/testing/src/TestingServiceProvider.php
@@ -19,6 +19,8 @@ class TestingServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-       //
+        if (env('DUSK_ENABLED', false)) {
+            $this->app->register(\Hyde\Testing\DuskServiceProvider::class);
+        }
     }
 }
