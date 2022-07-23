04554a7b7764b715840a36973dbd506c2285dd4a
Caen De Silva <caen@desilva.se>
2022-07-23 15:41:40 +0200

    Import CreatesApplication

--- packages/testing/src/DuskTestCase.php
+++ packages/testing/src/DuskTestCase.php
@@ -10,6 +10,7 @@ use Laravel\Dusk\Browser;
 use Laravel\Dusk\TestCase as BaseTestCase;
 use LaravelZero\Framework\Providers\CommandRecorder\CommandRecorderRepository;
 use NunoMaduro\Collision\ArgumentFormatter;
+use Tests\CreatesApplication;
 
 abstract class DuskTestCase extends BaseTestCase
 {
