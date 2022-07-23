617d36c7ac6f58a78fd62e94e9f1359d60cc1a21
Caen De Silva <caen@desilva.se>
2022-07-23 12:09:39 +0200

    Change previously static Hyde methods to be Kernel instance methods

--- packages/framework/src/HydeKernel.php
+++ packages/framework/src/HydeKernel.php
@@ -41,37 +41,24 @@ class HydeKernel
         return InstalledVersions::getPrettyVersion('hyde/framework') ?: 'unreleased';
     }
 
-    public static function getBasePath(): string
+    public function getBasePath(): string
     {
-        /** @deprecated Set path in constructor when instantiating the Singleton. */
-        if (! isset(static::$basePath)) {
-            static::$basePath = getcwd();
-        }
-
-        return static::$basePath;
-    }
-
-    /**
-     * @deprecated Set path in constructor when instantiating the Singleton.
-     */
-    public static function setBasePath(string $path): void
-    {
-        static::$basePath = $path;
+        return $this->basePath;
     }
 
     // HydeHelperFacade
 
-    public static function features(): Features
+    public function features(): Features
     {
         return new Features;
     }
 
-    public static function hasFeature(string $feature): bool
+    public function hasFeature(string $feature): bool
     {
         return Features::enabled($feature);
     }
 
-    public static function makeTitle(string $slug): string
+    public function makeTitle(string $slug): string
     {
         $alwaysLowercase = ['a', 'an', 'the', 'in', 'on', 'by', 'with', 'of', 'and', 'or', 'but'];
 
@@ -100,7 +87,7 @@ class HydeKernel
      * @param  string  $path
      * @return string
      */
-    public static function path(string $path = ''): string
+    public function path(string $path = ''): string
     {
         if (empty($path)) {
             return static::getBasePath();
@@ -117,9 +104,9 @@ class HydeKernel
      * @param  string  $path
      * @return string
      */
-    public static function vendorPath(string $path = ''): string
+    public function vendorPath(string $path = ''): string
     {
-        return static::path('vendor/hyde/framework/'.unslash($path));
+        return $this->path('vendor/hyde/framework/'.unslash($path));
     }
 
     /**
@@ -127,7 +114,7 @@ class HydeKernel
      *
      * @see \Hyde\Framework\Testing\Unit\FileHelperPageLinkPrettyUrlTest
      */
-    public static function pageLink(string $destination): string
+    public function pageLink(string $destination): string
     {
         if (config('site.pretty_urls', false) === true) {
             if (str_ends_with($destination, '.html')) {
@@ -156,7 +143,7 @@ class HydeKernel
      *
      * @see \Hyde\Framework\Testing\Unit\FileHelperRelativeLinkTest
      */
-    public static function relativeLink(string $destination, ?string $current = null): string
+    public function relativeLink(string $destination, ?string $current = null): string
     {
         if (str_starts_with($destination, '../')) {
             return $destination;
@@ -179,7 +166,7 @@ class HydeKernel
     /**
      * Get the current page path, or fall back to the root path.
      */
-    public static function currentPage(): string
+    public function currentPage(): string
     {
         return View::shared('currentPage', '');
     }
@@ -187,7 +174,7 @@ class HydeKernel
     /**
      * Get the current page route, or fall back to null.
      */
-    public static function currentRoute(): ?RouteContract
+    public function currentRoute(): ?RouteContract
     {
         return View::shared('currentRoute');
     }
@@ -196,7 +183,7 @@ class HydeKernel
      * Gets a relative web link to the given image stored in the _site/media folder.
      * Since v0.50.x you no longer have to supply a current page as it will be automatically retrieved from the View.
      */
-    public static function image(string $name, string $current = null): string
+    public function image(string $name, string $current = null): string
     {
         if ($current === null) {
             $current = static::currentPage();
@@ -215,7 +202,7 @@ class HydeKernel
      * @param  string|null  $path  optional relative path suffix. Omit to return base url.
      * @return string|false
      */
-    public static function uriPath(?string $path = ''): string|false
+    public function uriPath(?string $path = ''): string|false
     {
         if (config('site.url', false)) {
             return rtrim(config('site.url'), '/').'/'.(trim($path, '/') ?? '');
@@ -232,7 +219,7 @@ class HydeKernel
      * @param  bool  $force  If true, existing files will be overwritten.
      * @return bool|int Returns true|false on copy() success|failure, or an error code on failure
      */
-    public static function copy(string $from, string $to, bool $force = false): bool|int
+    public function copy(string $from, string $to, bool $force = false): bool|int
     {
         if (! file_exists($from)) {
             return 404;
@@ -254,33 +241,33 @@ class HydeKernel
      * These are intended to be used as a dynamic alternative to legacy code
      * Hyde::path('_pages/foo') becomes Hyde::getBladePagePath('foo')
      */
-    public static function getModelSourcePath(string $model, string $path = ''): string
+    public function getModelSourcePath(string $model, string $path = ''): string
     {
         if (empty($path)) {
-            return static::path(DiscoveryService::getFilePathForModelClassFiles($model));
+            return $this->path(DiscoveryService::getFilePathForModelClassFiles($model));
         }
 
         $path = unslash($path);
 
-        return static::path(DiscoveryService::getFilePathForModelClassFiles($model).DIRECTORY_SEPARATOR.$path);
+        return $this->path(DiscoveryService::getFilePathForModelClassFiles($model).DIRECTORY_SEPARATOR.$path);
     }
 
-    public static function getBladePagePath(string $path = ''): string
+    public function getBladePagePath(string $path = ''): string
     {
         return static::getModelSourcePath(BladePage::class, $path);
     }
 
-    public static function getMarkdownPagePath(string $path = ''): string
+    public function getMarkdownPagePath(string $path = ''): string
     {
         return static::getModelSourcePath(MarkdownPage::class, $path);
     }
 
-    public static function getMarkdownPostPath(string $path = ''): string
+    public function getMarkdownPostPath(string $path = ''): string
     {
         return static::getModelSourcePath(MarkdownPost::class, $path);
     }
 
-    public static function getDocumentationPagePath(string $path = ''): string
+    public function getDocumentationPagePath(string $path = ''): string
     {
         return static::getModelSourcePath(DocumentationPage::class, $path);
     }
@@ -288,7 +275,7 @@ class HydeKernel
     /**
      * Get the absolute path to the compiled site directory, or a file within it.
      */
-    public static function getSiteOutputPath(string $path = ''): string
+    public function getSiteOutputPath(string $path = ''): string
     {
         if (empty($path)) {
             return StaticPageBuilder::$outputPath;
@@ -302,10 +289,10 @@ class HydeKernel
     /**
      * Decode an absolute path created with a Hyde::path() helper into its relative counterpart.
      */
-    public static function pathToRelative(string $path): string
+    public function pathToRelative(string $path): string
     {
-        return str_starts_with($path, static::path()) ? unslash(str_replace(
-            static::path(),
+        return str_starts_with($path, $this->path()) ? unslash(str_replace(
+            $this->path(),
             '',
             $path
         )) : $path;
