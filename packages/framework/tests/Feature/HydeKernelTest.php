44dc9800d3aee83f0a0eee62c4d1fb676b190530
Caen De Silva <caen@desilva.se>
2022-07-23 13:44:14 +0200

    Test kernel singleton

--- packages/framework/tests/Feature/HydeKernelTest.php
+++ packages/framework/tests/Feature/HydeKernelTest.php
@@ -2,6 +2,9 @@
 
 namespace Hyde\Framework\Testing\Feature;
 
+use Hyde\Framework\Contracts\HydeKernelContract;
+use Hyde\Framework\Hyde;
+use Hyde\Framework\HydeKernel;
 use Hyde\Testing\TestCase;
 
 /**
@@ -9,5 +12,23 @@ use Hyde\Testing\TestCase;
  */
 class HydeKernelTest extends TestCase
 {
-   //
+    public function test_kernel_singleton_can_be_accessed_by_service_container()
+    {
+        $this->assertSame(app(HydeKernelContract::class), app(HydeKernelContract::class));
+    }
+
+    public function test_kernel_singleton_can_be_accessed_by_kernel_static_method()
+    {
+        $this->assertSame(app(HydeKernelContract::class), HydeKernel::getInstance());
+    }
+
+    public function test_kernel_singleton_can_be_accessed_by_hyde_facade_method()
+    {
+        $this->assertSame(app(HydeKernelContract::class), Hyde::getInstance());
+    }
+
+    public function test_kernel_singleton_can_be_accessed_by_helper_function()
+    {
+        $this->assertSame(app(HydeKernelContract::class), hyde());
+    }
 }
