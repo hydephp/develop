f3047285f7e2bb9fcc093d01c1224ce1f35d42c0
Caen De Silva <caen@desilva.se>
2022-07-23 14:47:40 +0200

    Remove service provider boilerplate

--- packages/testing/src/TestingServiceProvider.php
+++ packages/testing/src/TestingServiceProvider.php
@@ -11,37 +11,7 @@ class TestingServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        /*
-         * Optional methods to load your package assets
-         */
-        // $this->loadTranslationsFrom(__DIR__.'/../resources/lang', 'testing');
-        // $this->loadViewsFrom(__DIR__.'/../resources/views', 'testing');
-        // $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
-        // $this->loadRoutesFrom(__DIR__.'/routes.php');
-
-        if ($this->app->runningInConsole()) {
-            $this->publishes([
-                __DIR__.'/../config/config.php' => config_path('testing.php'),
-            ], 'config');
-
-            // Publishing the views.
-            /*$this->publishes([
-                __DIR__.'/../resources/views' => resource_path('views/vendor/testing'),
-            ], 'views');*/
-
-            // Publishing assets.
-            /*$this->publishes([
-                __DIR__.'/../resources/assets' => public_path('vendor/testing'),
-            ], 'assets');*/
-
-            // Publishing the translation files.
-            /*$this->publishes([
-                __DIR__.'/../resources/lang' => resource_path('lang/vendor/testing'),
-            ], 'lang');*/
-
-            // Registering package commands.
-            // $this->commands([]);
-        }
+       //
     }
 
     /**
@@ -49,12 +19,6 @@ class TestingServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        // Automatically apply the package configuration
-        $this->mergeConfigFrom(__DIR__.'/../config/config.php', 'testing');
-
-        // Register the main class to use with the facade
-        $this->app->singleton('testing', function () {
-            return new Testing;
-        });
+        //
     }
 }
