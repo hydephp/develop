96c190306eadffeb03d7076ac42bbe3ea545c72e
Caen De Silva <caen@desilva.se>
2022-07-24 13:00:24 +0200

    Use proper Tailwind pseudo selector

--- packages/framework/resources/views/components/docs/labeled-sidebar-navigation-menu.blade.php
+++ packages/framework/resources/views/components/docs/labeled-sidebar-navigation-menu.blade.php
@@ -1,6 +1,6 @@
 <ul id="sidebar-navigation-menu" role="list">
 	@foreach ($sidebar->getCategories() as $category)
-	<li class="sidebar-category mb-4 not:first-child(mt-4)" role="listitem">
+	<li class="sidebar-category mb-4 mt-4 first:mt-0" role="listitem">
 		<h4 class="sidebar-category-heading text-base font-semibold mb-2 -ml-1">{{ Hyde::makeTitle($category) }}</h4>
 		<ul class="sidebar-category-list ml-4" role="list">
 			@foreach ($sidebar->getItemsInCategory($category) as $item)
