5fca19a8be3458a5c52ab29ffb3283aa3a17db55
Caen De Silva <caen@desilva.se>
2022-07-24 17:01:16 +0200

    Port search window styles to TailwindCSS

--- packages/framework/resources/views/components/docs/search-input.blade.php
+++ packages/framework/resources/views/components/docs/search-input.blade.php
@@ -2,5 +2,5 @@
     <noscript>
         The search feature requires JavaScript to be enabled in your browser.
     </noscript>
-    <input type="search" name="search" id="search-input" placeholder="Search..." autocomplete="off" autofocus class="w-full rounded text-base leading-normal bg-white py-2 px-3">
+    <input type="search" name="search" id="search-input" placeholder="Search..." autocomplete="off" autofocus class="w-full rounded text-base leading-normal bg-white dark:bg-gray-700 py-2 px-3">
 </div>
