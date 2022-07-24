249b2447f948ac13a6806ec45f50d10998118312
Caen De Silva <caen@desilva.se>
2022-07-24 21:00:43 +0200

    Tweak styling of full page search input

--- packages/framework/resources/views/pages/documentation-search.blade.php
+++ packages/framework/resources/views/pages/documentation-search.blade.php
@@ -15,5 +15,5 @@
         #search-results {
             max-height: unset !important;
         }</style>
-    <x-hyde::docs.search-input />
+    <x-hyde::docs.search-input class="max-w-xs border-b-4 border-indigo-400" />
 @endsection
