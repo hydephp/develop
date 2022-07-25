d08a9524cd24890665a4b7f5cf15b525110f0f35
Caen De Silva <caen@desilva.se>
2022-07-25 22:53:17 +0200

    Update HasTableOfContentsTest.php

--- packages/framework/tests/Unit/HasTableOfContentsTest.php
+++ packages/framework/tests/Unit/HasTableOfContentsTest.php
@@ -16,18 +16,9 @@ class HasTableOfContentsTest extends TestCase
 {
     use HasTableOfContents;
 
-    protected string $body;
-
-    public function testHasTableOfContentsProperty()
-    {
-        $this->assertClassHasAttribute('tableOfContents', static::class);
-    }
-
     public function testConstructorCreatesTableOfContentsString()
     {
         $this->body = '## Title';
-        $this->constructTableOfContents();
-        $this->assertIsString($this->tableOfContents);
-        $this->assertEquals('<ul class="table-of-contents"><li><a href="#title">Title</a></li></ul>', str_replace("\n", '', $this->tableOfContents));
+        $this->assertEquals('<ul class="table-of-contents"><li><a href="#title">Title</a></li></ul>', str_replace("\n", '', $this->getTableOfContents()));
     }
 }
