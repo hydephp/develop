8fa8deffc047ede82dfa62bea0a4ea87c0012232
Caen De Silva <caen@desilva.se>
2022-07-25 14:11:35 +0200

    Handle background darken state with Tailwind

--- packages/framework/resources/views/components/docs/labeled-sidebar-navigation-menu-item.blade.php
+++ packages/framework/resources/views/components/docs/labeled-sidebar-navigation-menu-item.blade.php
@@ -3,14 +3,13 @@
 'active' => false,
 ])
 
-<li @class([ 'sidebar-navigation-item -ml-4 pl-4' , 'active -ml-8 pl-8 bg-darken'=> $active]) {!! $active ? '' : '' !!} role="listitem">
+<li @class([ 'sidebar-navigation-item -ml-4 pl-4' , 'active -ml-8 pl-8 bg-black/5 dark:bg-black/10'=> $active]) {!! $active ? '' : '' !!} role="listitem">
 	@if(! $active)
-	<a class="-ml-8 pl-4 py-1 px-2  block  border-l-[0.325rem] border-transparent transition-colors duration-300	ease-in-out hover:bg-darken	"
+	<a class="-ml-8 pl-4 py-1 px-2  block  border-l-[0.325rem] border-transparent transition-colors duration-300	ease-in-out hover:bg-black/10	"
 		href="{{ Hyde::pageLink($item->destination . '.html') }}">{{ $item->label }}</a>
 	@else
-	<a class="-ml-8 pl-4 py-1 px-2  block  text-indigo-600 border-l-[0.325rem] border-indigo-500 transition-colors duration-300	ease-in-out	"
+	<a class="-ml-8 pl-4 py-1 px-2  block  text-indigo-600 border-l-[0.325rem] border-indigo-500 transition-colors duration-300	ease-in-out	hover:bg-black/10"
 		href="{{ Hyde::pageLink($item->destination . '.html') }}" aria-current="true">{{ $item->label }}</a>
-
 	@isset($page->tableOfContents)
 	<span class="sr-only">Table of contents</span>
 	{!! ($page->tableOfContents) !!}
