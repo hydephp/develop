d96835a596023219664509d0d0a71924237732ae
Caen De Silva <caen@desilva.se>
2022-07-25 11:14:42 +0200

    Add x-transition to backdrop

--- packages/framework/resources/views/layouts/docs.blade.php
+++ packages/framework/resources/views/layouts/docs.blade.php
@@ -109,7 +109,7 @@
 
 	@include('hyde::layouts.scripts')
 	<div id="support">
-		<div id="sidebar-backdrop" x-show="sidebarOpen" @click="sidebarOpen = false" title="Click to close sidebar" class="w-screen h-screen fixed top-0 left-0 cursor-pointer z-10 bg-black/50"></div>
+		<div id="sidebar-backdrop" x-show="sidebarOpen" x-transition @click="sidebarOpen = false" title="Click to close sidebar" class="w-screen h-screen fixed top-0 left-0 cursor-pointer z-10 bg-black/50"></div>
 	</div>
 </body>
 </html>
