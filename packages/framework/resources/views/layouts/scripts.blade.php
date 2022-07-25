90d039187f42d57dcc3117304c52da75c4b45e2e
Caen De Silva <caen@desilva.se>
2022-07-25 13:33:10 +0200

    Handle theme toggle with Alpine

--- packages/framework/resources/views/layouts/scripts.blade.php
+++ packages/framework/resources/views/layouts/scripts.blade.php
@@ -13,5 +13,19 @@
 {{-- Alpine.js --}}
 <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.10.3/dist/cdn.min.js" integrity="sha256-gOkV4d9/FmMNEkjOzVlyM2eNAWSUXisT+1RbMTTIgXI=" crossorigin="anonymous"></script>
 
+<script>
+	function toggleTheme() {
+		if (localStorage.getItem('color-theme') === 'dark' || !('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+			document.documentElement.classList.remove("dark");
+			localStorage.setItem('color-theme', 'light');
+			document.getElementById('meta-color-scheme').setAttribute('content', 'light');
+		} else {
+			document.documentElement.classList.add("dark");
+			localStorage.setItem('color-theme', 'dark');
+			document.getElementById('meta-color-scheme').setAttribute('content', 'dark');
+		}
+	}
+</script>
+
 {{-- Add any extra scripts to include before the closing <body> tag --}}
 @stack('scripts')
