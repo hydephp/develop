48e488901892138428ab21613c4f8e0b8858c143
Caen De Silva <caen@desilva.se>
2022-07-25 11:12:53 +0200

    Recreate the sidebar backdrop with Alpine and Tailwind

--- packages/framework/resources/views/layouts/docs.blade.php
+++ packages/framework/resources/views/layouts/docs.blade.php
@@ -108,5 +108,8 @@
 	@endif
 
 	@include('hyde::layouts.scripts')
+	<div id="support">
+		<div id="sidebar-backdrop" x-show="sidebarOpen" @click="sidebarOpen = false" title="Click to close sidebar" class="w-screen h-screen fixed top-0 left-0 cursor-pointer z-10 bg-black/50"></div>
+	</div>
 </body>
 </html>
