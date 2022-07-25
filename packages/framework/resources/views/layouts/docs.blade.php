812090c64a286f893d70aa94678e0b4c60120fe1
Caen De Silva <caen@desilva.se>
2022-07-25 11:16:39 +0200

    Put scripts after support elements

--- packages/framework/resources/views/layouts/docs.blade.php
+++ packages/framework/resources/views/layouts/docs.blade.php
@@ -107,9 +107,10 @@
 	</script>
 	@endif
 
-	@include('hyde::layouts.scripts')
 	<div id="support">
 		<div id="sidebar-backdrop" x-show="sidebarOpen" x-transition @click="sidebarOpen = false" title="Click to close sidebar" class="w-screen h-screen fixed top-0 left-0 cursor-pointer z-10 bg-black/50"></div>
 	</div>
+	
+	@include('hyde::layouts.scripts')
 </body>
 </html>
