7c8bd1756863d56090641ee4087b7d61139095ab
Caen De Silva <caen@desilva.se>
2022-07-25 13:42:38 +0200

    Handle navigation menu with Alpine

--- packages/framework/resources/views/layouts/app.blade.php
+++ packages/framework/resources/views/layouts/app.blade.php
@@ -3,7 +3,8 @@
 <head>
     @include('hyde::layouts.head')
 </head>
-<body id="app" class="flex flex-col min-h-screen overflow-x-hidden dark:bg-gray-900 dark:text-white">
+<body id="app" class="flex flex-col min-h-screen overflow-x-hidden dark:bg-gray-900 dark:text-white"
+    x-data="{ navigationOpen: false }" x-on:keydown.escape="navigationOpen = false;">
     @include('hyde::components.skip-to-content-button')
     @include('hyde::layouts.navigation') 
 
