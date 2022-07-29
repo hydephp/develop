f743bdb0f964785d580e3ec0f649a306bb05b576
Caen De Silva <caen@desilva.se>
2022-07-29 13:20:53 +0200

    Null coalesce group title to "Other"

--- packages/framework/resources/views/components/docs/labeled-sidebar-navigation-menu.blade.php
+++ packages/framework/resources/views/components/docs/labeled-sidebar-navigation-menu.blade.php
@@ -1,7 +1,7 @@
 <ul id="sidebar-navigation-menu" role="list">
 	@foreach ($sidebar->getGroups() as $group)
 	<li class="sidebar-category mb-4 mt-4 first:mt-0" role="listitem">
-		<h4 class="sidebar-category-heading text-base font-semibold mb-2 -ml-1">{{ Hyde::makeTitle($group) }}</h4>
+		<h4 class="sidebar-category-heading text-base font-semibold mb-2 -ml-1">{{ Hyde::makeTitle($group ?? 'Other') }}</h4>
 		<ul class="sidebar-category-list ml-4" role="list">
 				<x-hyde::docs.labeled-sidebar-navigation-menu-item :item="$item" :active="$item->destination === basename($currentPage)" />
 			@foreach ($sidebar->getItemsInGroup($group) as $item)
