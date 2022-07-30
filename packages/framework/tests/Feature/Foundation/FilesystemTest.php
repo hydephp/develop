b172a3360b5d8a7ec221e3f9a06aedf58b755daf
Caen De Silva <caen@desilva.se>
2022-07-30 20:49:14 +0200

    Create FilesystemTest.php

---
+++ packages/framework/tests/Feature/Foundation/FilesystemTest.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Hyde\Framework\Testing\Feature\Foundation;
+
+use Hyde\Testing\TestCase;
+use Hyde\Framework\Foundation\Filesystem;
+
+/**
+ * @covers \Hyde\Framework\Foundation\Filesystem
+ */
+class FilesystemTest extends TestCase
+{
+    public function testGetDocumentationPagePath()
+    {
+
+    }
+
+    public function testGetModelSourcePath()
+    {
+
+    }
+
+    public function testPathToRelative()
+    {
+
+    }
+
+    public function testGetMarkdownPagePath()
+    {
+
+    }
+
+    public function testGetBasePath()
+    {
+
+    }
+
+    public function testVendorPath()
+    {
+
+    }
+
+    public function testGetBladePagePath()
+    {
+
+    }
+
+    public function testCopy()
+    {
+
+    }
+
+    public function testGetMarkdownPostPath()
+    {
+
+    }
+
+    public function testGetSiteOutputPath()
+    {
+
+    }
+
+    public function testPath()
+    {
+
+    }
+}
