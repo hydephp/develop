51f485ac4cb49d76071a9980773b83e1b8ae887d
Caen De Silva <caen@desilva.se>
2022-07-30 21:08:15 +0200

    Fix mismatched expected/actual values

--- packages/framework/tests/Feature/Foundation/FilesystemTest.php
+++ packages/framework/tests/Feature/Foundation/FilesystemTest.php
@@ -56,24 +56,24 @@ class FilesystemTest extends TestCase
 
     public function test_path_method_returns_qualified_file_path_when_supplied_with_argument()
     {
-        $this->assertEquals($this->filesystem->path('file.php'), $this->filesystem->path().DIRECTORY_SEPARATOR.'file.php');
+        $this->assertEquals($this->filesystem->path().DIRECTORY_SEPARATOR.'file.php', $this->filesystem->path('file.php'));
     }
 
     public function test_path_method_strips_trailing_directory_separators_from_argument()
     {
-        $this->assertEquals($this->filesystem->path('\\/file.php/'), $this->filesystem->path().DIRECTORY_SEPARATOR.'file.php');
+        $this->assertEquals($this->filesystem->path().DIRECTORY_SEPARATOR.'file.php', $this->filesystem->path('\\/file.php/'));
     }
 
     public function test_path_method_returns_expected_value_for_nested_path_arguments()
     {
-        $this->assertEquals($this->filesystem->path('directory/file.php'), $this->filesystem->path().DIRECTORY_SEPARATOR.'directory/file.php');
+        $this->assertEquals($this->filesystem->path().DIRECTORY_SEPARATOR.'directory/file.php', $this->filesystem->path('directory/file.php'));
     }
 
     public function test_path_method_returns_expected_value_regardless_of_trailing_directory_separators_in_argument()
     {
-        $this->assertEquals($this->filesystem->path('directory/file.php/'), $this->filesystem->path().DIRECTORY_SEPARATOR.'directory/file.php');
-        $this->assertEquals($this->filesystem->path('/directory/file.php/'), $this->filesystem->path().DIRECTORY_SEPARATOR.'directory/file.php');
-        $this->assertEquals($this->filesystem->path('\\/directory/file.php/'), $this->filesystem->path().DIRECTORY_SEPARATOR.'directory/file.php');
+        $this->assertEquals($this->filesystem->path().DIRECTORY_SEPARATOR.'directory/file.php', $this->filesystem->path('directory/file.php/'));
+        $this->assertEquals($this->filesystem->path().DIRECTORY_SEPARATOR.'directory/file.php', $this->filesystem->path('/directory/file.php/'));
+        $this->assertEquals($this->filesystem->path().DIRECTORY_SEPARATOR.'directory/file.php', $this->filesystem->path('\\/directory/file.php/'));
     }
 
     public function test_vendor_path()
