c2ec349defbfdae3fb1ea4c033f1405307940ef2
Caen De Silva <caen@desilva.se>
2022-07-31 11:34:37 +0200

    Add base method tests

--- packages/framework/tests/Feature/HydeKernelTest.php
+++ packages/framework/tests/Feature/HydeKernelTest.php
@@ -34,4 +34,35 @@ class HydeKernelTest extends TestCase
     {
         $this->assertSame(app(HydeKernelContract::class), hyde());
     }
+
+    public function test_features_helper_returns_new_features_instance()
+    {
+        $this->assertInstanceOf(Features::class, Hyde::features());
+    }
+
+    public function test_has_feature_helper_calls_method_on_features_class()
+    {
+        $this->assertEquals(Features::enabled('foo'), Hyde::hasFeature('foo'));
+    }
+
+    public function test_current_page_helper_returns_current_page_name()
+    {
+        View::share('currentPage', 'foo');
+        $this->assertEquals('foo', Hyde::currentPage());
+    }
+
+    public function test_current_route_helper_returns_current_route_object()
+    {
+        $expected = new Route(new MarkdownPage());
+        View::share('currentRoute', $expected);
+        $this->assertInstanceOf(RouteContract::class, Hyde::currentRoute());
+        $this->assertEquals($expected, Hyde::currentRoute());
+        $this->assertSame($expected, Hyde::currentRoute());
+    }
+
+    public function test_make_title_helper_returns_title_from_page_slug()
+    {
+        $this->assertEquals('Foo Bar', Hyde::makeTitle('foo-bar'));
+    }
+
 }
