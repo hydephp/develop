dee2f9c3ece0c73c21fe44a3bd66dfab43716bb5
Caen De Silva <caen@desilva.se>
2022-07-31 18:36:14 +0200

    Compile pages using the page model's compile method

--- packages/framework/src/StaticPageBuilder.php
+++ packages/framework/src/StaticPageBuilder.php
@@ -48,21 +48,7 @@ class StaticPageBuilder
         $this->needsDirectory(static::$outputPath);
         $this->needsDirectory(dirname(Hyde::getSiteOutputPath($this->page->getOutputPath())));
 
-        if ($this->page instanceof BladePage) {
-            return $this->save($this->compileView());
-        }
-
-        if ($this->page instanceof MarkdownPost) {
-            return $this->save($this->compilePost());
-        }
-
-        if ($this->page instanceof MarkdownPage) {
-            return $this->save($this->compilePage());
-        }
-
-        if ($this->page instanceof DocumentationPage) {
-            return $this->save($this->compileDocs());
-        }
+        return $this->save($this->page->compile());
     }
 
     /**
@@ -78,54 +64,5 @@ class StaticPageBuilder
         file_put_contents($path, $contents);
 
         return $path;
-    }
-
-    /**
-     * Compile a custom Blade View into HTML.
-     *
-     * @return string
-     */
-    protected function compileView(): string
-    {
-        return view($this->page->view)->render();
-    }
-
-    /**
-     * Compile a Post into HTML using the Blade View.
-     *
-     * @return string
-     */
-    protected function compilePost(): string
-    {
-        return view('hyde::layouts/post')->with([
-            'title' => $this->page->title,
-            'markdown' => MarkdownConverter::parse($this->page->body),
-        ])->render();
-    }
-
-    /**
-     * Compile a Markdown Page into HTML using the Blade View.
-     *
-     * @return string
-     */
-    protected function compilePage(): string
-    {
-        return view('hyde::layouts/page')->with([
-            'title' => $this->page->title,
-            'markdown' => MarkdownConverter::parse($this->page->body),
-        ])->render();
-    }
-
-    /**
-     * Compile a Documentation page into HTML using the Blade View.
-     *
-     * @return string
-     */
-    protected function compileDocs(): string
-    {
-        return view('hyde::layouts/docs')->with([
-            'title' => $this->page->title,
-            'markdown' => MarkdownConverter::parse($this->page->body, DocumentationPage::class),
-        ])->render();
     }
 }
