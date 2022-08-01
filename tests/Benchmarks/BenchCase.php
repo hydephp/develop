ba1347cc55d9f72df0bdeb94b4695a8533e96e5b
Caen De Silva <caen@desilva.se>
2022-08-01 18:43:27 +0200

    Create BenchCase.php

---
+++ tests/Benchmarks/BenchCase.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Tests\Benchmarks;
+
+use Hyde\Testing\TestCase;
+use Illuminate\Support\Str;
+use Tests\Benchmarks\CBench\Benchmark;
+use Tests\Benchmarks\CBench\Report;
+
+class BenchCase extends TestCase
+{
+    public function benchmark(callable $callback, int $iterations = 100, ?string $name = null): void
+    {
+        $class = (basename(static::class, 'Test'));
+        $method = substr(debug_backtrace()[1]['function'], 4);
+
+        $benchmark = Benchmark::run($callback, $iterations, $name ?? $class. '::' .$method);
+
+        file_put_contents(sprintf(
+            "%s/reports/%s.json", __DIR__,  (
+                Str::snake($class . '-' . lcfirst($method)))),
+            json_encode(new Report($benchmark), JSON_PRETTY_PRINT));
+
+        $this->assertTrue(true);
+    }
+}
