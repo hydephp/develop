2f7bec0111ba8156294fadb5478bb1181b63bf16
Caen De Silva <caen@desilva.se>
2022-08-01 13:05:51 +0200

    Merge release notes stub into release script

--- projects/release-manager/release.php
+++ projects/release-manager/release.php
@@ -60,7 +60,35 @@ $notes = $notes."\n";
 echo "Done. \n";
 
 echo 'Resetting upcoming release notes stub';
-copy(__DIR__.'/release-notes.stub', __DIR__.'/../../RELEASE_NOTES.md');
+file_put_contents(__DIR__.'/../../RELEASE_NOTES.md', '## [Unreleased] - YYYY-MM-DD
+
+### About
+
+Keep an Unreleased section at the top to track upcoming changes.
+
+This serves two purposes:
+
+1. People can see what changes they might expect in upcoming releases
+2. At release time, you can move the Unreleased section changes into a new release version section.
+
+### Added
+- for new features.
+
+### Changed
+- for changes in existing functionality.
+
+### Deprecated
+- for soon-to-be removed features.
+
+### Removed
+- for now removed features.
+
+### Fixed
+- for any bug fixes.
+
+### Security
+- in case of vulnerabilities.
+');
 
 echo 'Updating changelog with the upcoming release notes... ';
 
