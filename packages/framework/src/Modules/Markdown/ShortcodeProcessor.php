63a38c6ee4327454529c1f9eedc9717fc6f48451
Caen De Silva <caen@desilva.se>
2022-08-01 22:09:15 +0200

    Move AbstractColoredBlockquote into Markdown namespace

--- packages/framework/src/Modules/Markdown/ShortcodeProcessor.php
+++ packages/framework/src/Modules/Markdown/ShortcodeProcessor.php
@@ -4,7 +4,7 @@ namespace Hyde\Framework\Modules\Markdown;
 
 use Hyde\Framework\Contracts\MarkdownProcessorContract;
 use Hyde\Framework\Contracts\MarkdownShortcodeContract;
-use Hyde\Framework\Services\Markdown\Shortcodes\AbstractColoredBlockquote;
+use Hyde\Framework\Modules\Markdown\Shortcodes\AbstractColoredBlockquote;
 
 /**
  * Handle shortcode processing for Markdown conversions.
