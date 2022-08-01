0978031e2316708587f7e94d142124bdc4d2524e
Caen De Silva <caen@desilva.se>
2022-08-01 20:45:38 +0200

    Add results history

--- tests/Benchmarks/MarkdownBenchmark.php
+++ tests/Benchmarks/MarkdownBenchmark.php
@@ -6,6 +6,10 @@ use Hyde\Framework\Actions\MarkdownConverter;
 
 class MarkdownBenchmark extends BenchCase
 {
+    /**
+     * Results history:
+     * #f4d8d452b 'avg_iteration_time': '0.42493788ms',
+     */
     public function testMarkdownParserFacadeShort()
     {
         $this->benchmark(function () {
@@ -13,6 +17,10 @@ class MarkdownBenchmark extends BenchCase
         }, 1500);
     }
 
+    /**
+     * Results history:
+     * #f4d8d452b 'avg_iteration_time': '2.62538815ms',
+     */
     public function testMarkdownParserFacadeFull()
     {
         $markdown = file_get_contents(__DIR__ . '/../fixtures/markdown.md');
