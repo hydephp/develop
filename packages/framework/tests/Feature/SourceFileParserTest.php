aa98fd9478184a90114716d0b2b67536a7b64b6e
Caen De Silva <caen@desilva.se>
2022-08-02 17:53:44 +0200

    Create SourceFileParserTest.php

---
+++ packages/framework/tests/Feature/SourceFileParserTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Hyde\Framework\Testing\Feature;
+
+use Hyde\Framework\Actions\SourceFileParser;
+use Hyde\Testing\TestCase;
+
+/**
+ * @covers \Hyde\Framework\Actions\SourceFileParser
+ */
+class SourceFileParserTest extends TestCase
+{
+    
+}
