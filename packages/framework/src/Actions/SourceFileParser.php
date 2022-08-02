6475dd6e5ddef8d8f198c16c5cd703c7247c476d
Caen De Silva <caen@desilva.se>
2022-08-02 18:57:41 +0200

    Remove premature abstraction

--- packages/framework/src/Actions/SourceFileParser.php
+++ packages/framework/src/Actions/SourceFileParser.php
@@ -14,31 +14,17 @@ class SourceFileParser
 {
     use ValidatesExistence;
 
-    /** @var class-string<PageContract> */
-    protected string $pageClass;
-    protected string $slug;
+    protected PageContract $page;
 
-    /**
-     * @param class-string<PageContract> $pageClass
-     * @param string $slug
-     * @throws \Hyde\Framework\Exceptions\FileNotFoundException
-     */
     public function __construct(string $pageClass, string $slug)
     {
-        $this->slug = $slug;
-        $this->pageClass = $pageClass;
         $this->validateExistence($pageClass, $slug);
 
-        $this->parse();
-    }
-
-    public function parse(): void
-    {
         //
     }
 
     public function get(): PageContract
     {
-        return new $this->pageClass($this->slug);
+        return $this->page;
     }
 }
