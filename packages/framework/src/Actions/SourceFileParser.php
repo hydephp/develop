224047c1a87a760e7aca84b228ca8df6b9f25891
Caen De Silva <caen@desilva.se>
2022-08-02 16:12:22 +0200

    Add method stubs

--- packages/framework/src/Actions/SourceFileParser.php
+++ packages/framework/src/Actions/SourceFileParser.php
@@ -2,6 +2,8 @@
 
 namespace Hyde\Framework\Actions;
 
+use Hyde\Framework\Contracts\PageContract;
+
 /**
  * Parses a source file and returns a new page model instance for it.
  */
@@ -9,6 +11,16 @@ class SourceFileParser
 {
     public function __construct(protected string $model, protected string $slug)
     {
+        $this->parse();
+    }
+
+    public function parse(): void
+    {
         //
+    }
+
+    public function get(): PageContract
+    {
+        return new $this->model($this->slug);
     }
 }
