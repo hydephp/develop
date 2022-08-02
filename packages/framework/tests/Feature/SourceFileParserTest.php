3382622250ae4f8eec0bd28ad27a24db873343f6
Caen De Silva <caen@desilva.se>
2022-08-02 19:56:59 +0200

    Create base tests

--- packages/framework/tests/Feature/SourceFileParserTest.php
+++ packages/framework/tests/Feature/SourceFileParserTest.php
@@ -2,6 +2,11 @@
 
 namespace Hyde\Framework\Testing\Feature;
 
+use Hyde\Framework\Actions\SourceFileParser;
+use Hyde\Framework\Models\Pages\BladePage;
+use Hyde\Framework\Models\Pages\DocumentationPage;
+use Hyde\Framework\Models\Pages\MarkdownPage;
+use Hyde\Framework\Models\Pages\MarkdownPost;
 use Hyde\Testing\TestCase;
 
 /**
@@ -9,4 +14,49 @@ use Hyde\Testing\TestCase;
  */
 class SourceFileParserTest extends TestCase
 {
+    public function test_blade_page_parser()
+    {
+        $this->file('_pages/foo.blade.php');
+
+        $parser = new SourceFileParser(BladePage::class, 'foo');
+        $page = $parser->get();
+        $this->assertInstanceOf(BladePage::class, $page);
+        $this->assertEquals('foo', $page->slug);
+    }
+
+    public function test_markdown_page_parser()
+    {
+        $this->markdown('_pages/foo.md', '# Foo Bar', ['title' => 'Foo Bar Baz']);
+
+        $parser = new SourceFileParser(MarkdownPage::class, 'foo');
+        $page = $parser->get();
+        $this->assertInstanceOf(MarkdownPage::class, $page);
+        $this->assertEquals('foo', $page->slug);
+        $this->assertEquals('# Foo Bar', $page->body);
+        $this->assertEquals('Foo Bar Baz', $page->title);
+    }
+
+    public function test_markdown_post_parser()
+    {
+        $this->markdown('_posts/foo.md', '# Foo Bar', ['title' => 'Foo Bar Baz']);
+
+        $parser = new SourceFileParser(MarkdownPost::class, 'foo');
+        $page = $parser->get();
+        $this->assertInstanceOf(MarkdownPost::class, $page);
+        $this->assertEquals('foo', $page->slug);
+        $this->assertEquals('# Foo Bar', $page->body);
+        $this->assertEquals('Foo Bar Baz', $page->title);
+    }
+
+    public function test_documentation_page_parser()
+    {
+        $this->markdown('_docs/foo.md', '# Foo Bar', ['title' => 'Foo Bar Baz']);
+
+        $parser = new SourceFileParser(DocumentationPage::class, 'foo');
+        $page = $parser->get();
+        $this->assertInstanceOf(DocumentationPage::class, $page);
+        $this->assertEquals('foo', $page->slug);
+        $this->assertEquals('# Foo Bar', $page->body);
+        $this->assertEquals('Foo Bar Baz', $page->title);
+    }
 }
