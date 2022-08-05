60822c3e886194b66b56869378d2d57f1a5917af
Caen De Silva <caen@desilva.se>
2022-08-05 13:03:48 +0200

    Create BladeMatterParserTest.php

---
+++ packages/framework/tests/Feature/BladeMatterParserTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Hyde\Framework\Testing\Feature;
+
+use Hyde\Framework\Actions\BladeMatterParser;
+use Hyde\Testing\TestCase;
+
+/**
+ * @covers \Hyde\Framework\Actions\BladeMatterParser
+ */
+class BladeMatterParserTest extends TestCase
+{
+
+}
