3d4ecbb4379c2cd034920875893af678654395ab
Caen De Silva <caen@desilva.se>
2022-08-05 22:13:42 +0200

    Merge HasFeaturedImageTest into MarkdownPostTest

--- packages/framework/tests/Feature/MarkdownPostTest.php
+++ packages/framework/tests/Feature/MarkdownPostTest.php
@@ -4,6 +4,7 @@ namespace Hyde\Framework\Testing\Feature;
 
 use Hyde\Framework\Models\Author;
 use Hyde\Framework\Models\FrontMatter;
+use Hyde\Framework\Models\Image;
 use Hyde\Framework\Models\Pages\MarkdownPost;
 use Hyde\Testing\TestCase;
 
@@ -39,5 +40,27 @@ class MarkdownPostTest extends TestCase
         $this->assertEquals('john_doe', $post->author->username);
         $this->assertEquals('John Doe', $post->author->name);
         $this->assertEquals('https://example.com', $post->author->website);
+    }
+
+    public function test_constructor_can_create_a_new_image_instance_from_a_string()
+    {
+        $post = new MarkdownPost(matter: FrontMatter::fromArray([
+            'image' => 'https://example.com/image.jpg',
+        ]));
+
+        $this->assertInstanceOf(Image::class, $post->image);
+        $this->assertEquals('https://example.com/image.jpg', $post->image->uri);
+    }
+
+    public function test_constructor_can_create_a_new_image_instance_from_an_array()
+    {
+        $post = new MarkdownPost(matter: FrontMatter::fromArray([
+            'image' => [
+                'uri' => 'https://example.com/image.jpg',
+            ],
+        ]));
+
+        $this->assertInstanceOf(Image::class, $post->image);
+        $this->assertEquals('https://example.com/image.jpg', $post->image->uri);
     }
 }
