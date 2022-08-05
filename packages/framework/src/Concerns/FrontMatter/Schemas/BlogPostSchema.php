4226b8e6525d50042759be23ca4b9c386cb7b59c
Caen De Silva <caen@desilva.se>
2022-08-05 21:18:28 +0200

    Add schemas from the docs

--- packages/framework/src/Concerns/FrontMatter/Schemas/BlogPostSchema.php
+++ packages/framework/src/Concerns/FrontMatter/Schemas/BlogPostSchema.php
@@ -8,4 +8,21 @@ trait BlogPostSchema
     {
         //
     }
+    /** @example "My New Post" */
+    public string $title;
+
+    /** @example "A short description" */
+    public ?string $description;
+
+    /** @example "general", "my favorite recipes" */
+    public ?string $category;
+
+    /** @example "YYYY-MM-DD [HH:MM]" */
+    public ?string $date;
+
+    /** @example See author section */
+    public string|array|null $author;
+
+    /** @example See image section */
+    public string|array|null $image;
 }
