27c6d03b6349b042238c53d31531655f529f5230
Caen De Silva <caen@desilva.se>
2022-08-05 11:07:56 +0200

    Sketch out the class

--- packages/framework/src/Actions/BladeMatterParser.php
+++ packages/framework/src/Actions/BladeMatterParser.php
@@ -4,8 +4,28 @@ namespace Hyde\Framework\Actions;
 
 /**
  * Parse the front matter in a Blade file.
+ *
+ * Accepts a string to make it easier to mock when testing.
  */
 class BladeMatterParser
 {
-    //
+    protected string $contents;
+    protected array $matter;
+
+    public function __construct(string $contents)
+    {
+        $this->contents = $contents;
+    }
+
+    public function parse(): static
+    {
+        $this->matter = [];
+
+        return $this;
+    }
+
+    public function get(): array
+    {
+        return $this->matter;
+    }
 }
