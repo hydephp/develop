f01eb6e0796b315c5cd72dcad43f80644ae53582
Caen De Silva <caen@desilva.se>
2022-08-05 13:27:13 +0200

    Add helper method unit tests

--- packages/framework/tests/Feature/BladeMatterParserTest.php
+++ packages/framework/tests/Feature/BladeMatterParserTest.php
@@ -10,5 +10,33 @@ use Hyde\Testing\TestCase;
  */
 class BladeMatterParserTest extends TestCase
 {
+    public function test_line_matches_front_matter()
+    {
+        $this->assertTrue(BladeMatterParser::lineMatchesFrontMatter('@php($title = "BladeMatter Test")'));
+        $this->assertFalse(BladeMatterParser::lineMatchesFrontMatter('foo bar'));
+    }
 
+    public function test_extract_key()
+    {
+        $this->assertSame('title', BladeMatterParser::extractKey('@php($title = "BladeMatter Test")'));
+    }
+
+    public function test_extract_value()
+    {
+        $this->assertSame('BladeMatter Test', BladeMatterParser::extractValue('@php($title = "BladeMatter Test")'));
+    }
+
+    public function test_normalize_value()
+    {
+        $this->assertSame('string', BladeMatterParser::normalizeValue('string'));
+        $this->assertSame("string", BladeMatterParser::normalizeValue("string"));
+        $this->assertSame(true, BladeMatterParser::normalizeValue('true'));
+        $this->assertSame(false, BladeMatterParser::normalizeValue('false'));
+        $this->assertSame(1, BladeMatterParser::normalizeValue('1'));
+        $this->assertSame(0, BladeMatterParser::normalizeValue('0'));
+        $this->assertSame(1.0, BladeMatterParser::normalizeValue('1.0'));
+        $this->assertSame(0.0, BladeMatterParser::normalizeValue('0.0'));
+        $this->assertSame(null, BladeMatterParser::normalizeValue('null'));
+        $this->assertSame('["foo" => "bar"]', BladeMatterParser::normalizeValue('["foo" => "bar"]'));
+    }
 }
