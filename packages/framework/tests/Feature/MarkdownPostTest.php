3ece8142e996825359aa706fc4c0e1913ffecbd3
Caen De Silva <caen@desilva.se>
2022-08-05 22:01:45 +0200

    Create MarkdownPostTest.php

---
+++ packages/framework/tests/Feature/MarkdownPostTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Hyde\Framework\Testing\Feature;
+
+use Hyde\Framework\Models\Pages\MarkdownPost;
+use Hyde\Testing\TestCase;
+
+/**
+ * @covers \Hyde\Framework\Models\Pages\MarkdownPost
+ */
+class MarkdownPostTest extends TestCase
+{
+    //
+}
