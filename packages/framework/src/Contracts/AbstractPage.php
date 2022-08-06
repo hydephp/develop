5bc8e84eab67a6c9eba035e7894d2dd9d1910266
Caen De Silva <caen@desilva.se>
2022-08-06 11:34:32 +0200

    Use PageSchema

--- packages/framework/src/Contracts/AbstractPage.php
+++ packages/framework/src/Contracts/AbstractPage.php
@@ -3,6 +3,7 @@
 namespace Hyde\Framework\Contracts;
 
 use Hyde\Framework\Actions\SourceFileParser;
+use Hyde\Framework\Concerns\FrontMatter\Schemas\PageSchema;
 use Hyde\Framework\Helpers\Features;
 use Hyde\Framework\Helpers\Meta;
 use Hyde\Framework\Hyde;
@@ -27,6 +28,8 @@ use Illuminate\Support\Collection;
  */
 abstract class AbstractPage implements PageContract, CompilableContract
 {
+    use PageSchema;
+
     public static string $sourceDirectory;
     public static string $outputDirectory;
     public static string $fileExtension;
