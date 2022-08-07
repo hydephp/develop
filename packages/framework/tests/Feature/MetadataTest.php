1a1c4d50e210a40b25eeb2a63a88a3b7bbe9f25a
Caen De Silva <caen@desilva.se>
2022-08-07 22:32:13 +0200

    Covers metadata item types

--- packages/framework/tests/Feature/MetadataTest.php
+++ packages/framework/tests/Feature/MetadataTest.php
@@ -6,6 +6,9 @@ use Hyde\Testing\TestCase;
 
 /**
  * @covers \Hyde\Framework\Models\Metadata\Metadata
+ * @covers \Hyde\Framework\Models\Metadata\LinkItem
+ * @covers \Hyde\Framework\Models\Metadata\MetadataItem
+ * @covers \Hyde\Framework\Models\Metadata\OpenGraphItem
  */
 class MetadataTest extends TestCase
 {
