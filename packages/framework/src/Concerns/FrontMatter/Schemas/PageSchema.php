35d2ca131747accfb561bf59032efd493288f9fa
Caen De Silva <caen@desilva.se>
2022-08-07 13:31:55 +0200

    Move complex constructor to action

--- packages/framework/src/Concerns/FrontMatter/Schemas/PageSchema.php
+++ packages/framework/src/Concerns/FrontMatter/Schemas/PageSchema.php
@@ -2,10 +2,8 @@
 
 namespace Hyde\Framework\Concerns\FrontMatter\Schemas;
 
+use Hyde\Framework\Actions\Constructors\FindsNavigationDataForPage;
 use Hyde\Framework\Actions\Constructors\FindsTitleForPage;
-use Hyde\Framework\Contracts\AbstractMarkdownPage;
-use Hyde\Framework\Models\Pages\DocumentationPage;
-use Hyde\Framework\Models\Pages\MarkdownPost;
 use JetBrains\PhpStorm\ArrayShape;
 
 trait PageSchema
@@ -17,95 +15,12 @@ trait PageSchema
      */
     public string $title;
 
+    #[ArrayShape(['title' => 'string', 'hidden' => 'bool', 'priority' => 'int'])]
     public ?array $navigation = null;
 
     protected function constructPageSchema(): void
     {
         $this->title = FindsTitleForPage::run($this);
-
-        $this->navigation = $this->constructNavigation();
-    }
-
-    #[ArrayShape(['title' => 'string', 'hidden' => 'bool', 'priority' => 'int'])]
-     protected function constructNavigation(): array
-     {
-         return [
-             'title' => $this->getNavigationMenuTitle(),
-             'hidden' => ! $this->getNavigationMenuVisible(),
-             'priority' => $this->getNavigationMenuPriority(),
-         ];
-     }
-
-    protected function getNavigationMenuTitle(): string
-    {
-        if ($this instanceof AbstractMarkdownPage) {
-            if ($this->matter('navigation.title') !== null) {
-                return $this->matter('navigation.title');
-            }
-
-            if ($this->matter('title') !== null) {
-                return $this->matter('title');
-            }
-        }
-
-        if ($this->identifier === 'index') {
-            if ($this instanceof DocumentationPage) {
-                return config('hyde.navigation.labels.docs', 'Docs');
-            }
-
-            return config('hyde.navigation.labels.home', 'Home');
-        }
-
-        return $this->title;
-    }
-
-    protected function getNavigationMenuVisible(): bool
-    {
-        if ($this instanceof MarkdownPost) {
-            return false;
-        }
-
-        if ($this instanceof DocumentationPage) {
-            return $this->identifier === 'index' && ! in_array('docs', config('hyde.navigation.exclude', []));
-        }
-
-        if ($this instanceof AbstractMarkdownPage) {
-            if ($this->matter('navigation.hidden', false)) {
-                return false;
-            }
-        }
-
-        if (in_array($this->identifier, config('hyde.navigation.exclude', ['404']))) {
-            return false;
-        }
-
-        return true;
-    }
-
-    protected function getNavigationMenuPriority(): int
-    {
-        if ($this instanceof AbstractMarkdownPage) {
-            if ($this->matter('navigation.priority') !== null) {
-                return $this->matter('navigation.priority');
-            }
-        }
-
-        if ($this instanceof DocumentationPage) {
-            return (int) config('hyde.navigation.order.docs', 100);
-        }
-
-        if ($this->identifier === 'index') {
-            return (int) config('hyde.navigation.order.index', 0);
-        }
-
-        if ($this->identifier === 'posts') {
-            return (int) config('hyde.navigation.order.posts', 10);
-        }
-
-        if (array_key_exists($this->identifier, config('hyde.navigation.order', []))) {
-            return (int) config('hyde.navigation.order.'.$this->identifier);
-        }
-
-        return 999;
+        $this->navigation = FindsNavigationDataForPage::run($this);
     }
 }
