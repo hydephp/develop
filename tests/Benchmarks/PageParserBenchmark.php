317f22b34b9c6a377092c7ca51a1d025ca190eb6
Caen De Silva <caen@desilva.se>
2022-08-08 17:55:18 +0200

    Benchmark Markdown post file parsing

--- tests/Benchmarks/PageParserBenchmark.php
+++ tests/Benchmarks/PageParserBenchmark.php
@@ -2,7 +2,20 @@
 
 namespace Tests\Benchmarks;
 
+use Hyde\Framework\Hyde;
+use Hyde\Framework\Models\Pages\MarkdownPost;
+
 class PageParserBenchmark extends BenchCase
 {
-    //
+    public function testParseMarkdownPostFile()
+    {
+        $this->mockConsoleOutput = false;
+        $this->artisan('make:post -n');
+
+        $this->benchmark(function () {
+            return MarkdownPost::parse('my-new-post');
+        });
+
+        Hyde::unlink('_posts/my-new-post.md');
+    }
 }
