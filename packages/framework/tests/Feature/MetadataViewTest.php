950f354a66c6ece6ad08fbda3b74d58ec33cf3c5
Caen De Silva <caen@desilva.se>
2022-08-08 15:12:14 +0200

    Assert greater than or equal to instead of equal

--- packages/framework/tests/Feature/MetadataViewTest.php
+++ packages/framework/tests/Feature/MetadataViewTest.php
@@ -57,7 +57,7 @@ class MetadataViewTest extends TestCase
         $actualLinks = substr_count($haystack, '<link');
         $actualMeta = substr_count($haystack, '<meta');
 
-        $this->assertEquals(
+        $this->assertGreaterThanOrEqual(
             $links + $meta,
             $actualLinks + $actualMeta,
             "Failed asserting that all tags were covered in the page '$page'"
