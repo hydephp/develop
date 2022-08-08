d08fbaed8502934ef519a977a4800fbc175c1383
Caen De Silva <caen@desilva.se>
2022-08-08 17:53:04 +0200

    Return the benchmark object

--- tests/Benchmarks/BenchCase.php
+++ tests/Benchmarks/BenchCase.php
@@ -12,7 +12,7 @@ use Tests\Benchmarks\CBench\Report;
  */
 class BenchCase extends TestCase
 {
-    public function benchmark(callable $callback, int $iterations = 100, ?string $name = null): void
+    public function benchmark(callable $callback, int $iterations = 100, ?string $name = null): Benchmark
     {
         $class = (basename(static::class, 'Test'));
         $method = substr(debug_backtrace()[1]['function'], 4);
@@ -27,6 +27,8 @@ class BenchCase extends TestCase
         $this->log($method, "Ran $benchmark->iterations iterations in {$benchmark->getExecutionTimeInMs()}ms ({$benchmark->getAverageExecutionTimeInMs()}ms avg / {$benchmark->getAverageIterationsPerSecond()} per sec)");
 
         $this->assertTrue(true);
+
+        return $benchmark;
     }
 
     protected function log(string $method, string $message): void
