d48beec06aba3156727340b02e25dcde54c3f407
Caen De Silva <caen@desilva.se>
2022-08-08 14:22:40 +0200

    Add testing helpers

--- packages/framework/tests/Feature/MetadataViewTest.php
+++ packages/framework/tests/Feature/MetadataViewTest.php
@@ -2,6 +2,7 @@
 
 namespace Hyde\Framework\Testing\Feature;
 
+use Hyde\Framework\Hyde;
 use Hyde\Testing\TestCase;
 
 /**
@@ -14,5 +15,29 @@ class MetadataViewTest extends TestCase
         parent::setUp();
 
         config(['site.url' => 'http://localhost']);
+    }
+
+    protected function build(?string $page = null): void
+    {
+        if ($page) {
+            $this->artisan("rebuild $page");
+        } else {
+            $this->artisan('build');
+        }
+    }
+
+    protected function assertSee(string $page, string|array $text): void
+    {
+        if (is_array($text)) {
+            foreach ($text as $string) {
+                $this->assertSee($page, $string);
+            }
+
+            return;
+        }
+
+        $this->assertStringContainsString($text,
+            file_get_contents(Hyde::path("_site/$page.html")),
+            "Failed asserting that the page '$page' contains the text '$text'");
     }
 }
