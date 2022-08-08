f2d4b5263cb252bc02651edbe74c9c5edbf2ec99
Caen De Silva <caen@desilva.se>
2022-08-08 14:19:05 +0200

    Create MetadataViewTest.php

---
+++ packages/framework/tests/Feature/MetadataViewTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Hyde\Framework\Testing\Feature;
+
+use Hyde\Testing\TestCase;
+
+/**
+ * @covers \Hyde\Framework\Models\Metadata\Metadata
+ */
+class MetadataViewTest extends TestCase
+{
+    //
+}
