2750d28b8cb025f1d91fa689a4af624c3701fd1c
Caen De Silva <caen@desilva.se>
2022-08-08 13:04:59 +0200

    Compile the site for testing

--- packages/framework/tests/Feature/RelativeLinksAcrossPagesRetainsIntegrityTest.php
+++ packages/framework/tests/Feature/RelativeLinksAcrossPagesRetainsIntegrityTest.php
@@ -21,16 +21,21 @@ class RelativeLinksAcrossPagesRetainsIntegrityTest extends TestCase
     protected function tearDown(): void
     {
         File::deleteDirectory(Hyde::path('_pages/nested'));
+        Hyde::unlink('_site/root.html');
+        Hyde::unlink('_site/root1.html');
+        $this->resetSite();
 
         parent::tearDown();
     }
 
     public function test_relative_links_across_pages_retains_integrity()
     {
-        Hyde::touch('_pages/root.md');
-        Hyde::touch('_pages/root1.md');
+        $this->file('_pages/root.md');
+        $this->file('_pages/root1.md');
         Hyde::touch('_pages/nested/level1.md');
         Hyde::touch('_pages/nested/level1b.md');
         Hyde::touch('_pages/nested/sub-nested/level2.md');
+
+        $this->artisan('build');
     }
 }
