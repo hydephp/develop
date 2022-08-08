7f9bb75267f14272f95077f7c54c3f215a14f8f0
Caen De Silva <caen@desilva.se>
2022-08-08 13:00:35 +0200

    Set up base environment

--- packages/framework/tests/Feature/RelativeLinksAcrossPagesRetainsIntegrityTest.php
+++ packages/framework/tests/Feature/RelativeLinksAcrossPagesRetainsIntegrityTest.php
@@ -2,9 +2,35 @@
 
 namespace Hyde\Framework\Testing\Feature;
 
+use Hyde\Framework\Concerns\InteractsWithDirectories;
+use Hyde\Framework\Hyde;
 use Hyde\Testing\TestCase;
+use Illuminate\Support\Facades\File;
 
 class RelativeLinksAcrossPagesRetainsIntegrityTest extends TestCase
 {
-    //
+    use InteractsWithDirectories;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->needsDirectory('_pages/nested/sub-nested');
+    }
+
+    protected function tearDown(): void
+    {
+        File::deleteDirectory(Hyde::path('_pages/nested'));
+
+        parent::tearDown();
+    }
+
+    public function test_relative_links_across_pages_retains_integrity()
+    {
+        Hyde::touch('_pages/root.md');
+        Hyde::touch('_pages/root1.md');
+        Hyde::touch('_pages/nested/level1.md');
+        Hyde::touch('_pages/nested/level1b.md');
+        Hyde::touch('_pages/nested/sub-nested/level2.md');
+    }
 }
