5d044679bb109648b963ffd52a9410dcd5b5db67
Caen De Silva <caen@desilva.se>
2022-08-08 18:08:14 +0200

    Report output

--- tests/Benchmarks/PageParserBenchmark.php
+++ tests/Benchmarks/PageParserBenchmark.php
@@ -4,6 +4,7 @@ namespace Tests\Benchmarks;
 
 use Hyde\Framework\Hyde;
 use Hyde\Framework\Models\Pages\MarkdownPost;
+use Tests\Benchmarks\CBench\Benchmark;
 
 class PageParserBenchmark extends BenchCase
 {
@@ -12,10 +13,19 @@ class PageParserBenchmark extends BenchCase
         $this->mockConsoleOutput = false;
         $this->artisan('make:post -n');
 
-        $this->benchmark(function () {
+        $result = $this->benchmark(function () {
             return MarkdownPost::parse('my-new-post');
         });
 
+        $this->report($result);
+
         Hyde::unlink('_posts/my-new-post.md');
+    }
+
+    protected function report(Benchmark $benchmark): void
+    {
+        echo "- <b>$benchmark->runName</b> - avg_iteration_time\n";
+        echo "- #".trim(shell_exec('git rev-parse --short HEAD')).
+            ": {$benchmark->getAverageExecutionTimeInMs()}ms\n";
     }
 }
