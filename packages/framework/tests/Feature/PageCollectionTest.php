f3e662e93b21506d306d3eac284eb00cda04b29d
Caen De Silva <caen@desilva.se>
2022-08-09 20:04:02 +0200

    Create PageCollectionTest.php

---
+++ packages/framework/tests/Feature/PageCollectionTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Hyde\Framework\Testing\Feature;
+
+use Hyde\Framework\PageCollection;
+use Hyde\Testing\TestCase;
+
+/**
+ * @covers \Hyde\Framework\PageCollection
+ */
+class PageCollectionTest extends TestCase
+{
+
+}
