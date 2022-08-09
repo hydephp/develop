9d44b22c5b3d36d3c8b1c31e9555e03e2c2d935e
Caen De Silva <caen@desilva.se>
2022-08-09 16:17:28 +0200

    Remove all non-public contract methods and forward to new routing system

--- packages/framework/src/Services/RoutingService.php
+++ packages/framework/src/Services/RoutingService.php
@@ -6,6 +6,7 @@ use Hyde\Framework\Contracts\PageContract;
 use Hyde\Framework\Contracts\RouteContract;
 use Hyde\Framework\Contracts\RoutingServiceContract;
 use Hyde\Framework\Helpers\Features;
+use Hyde\Framework\Hyde;
 use Hyde\Framework\HydeKernel;
 use Hyde\Framework\Models\Pages\BladePage;
 use Hyde\Framework\Models\Pages\DocumentationPage;
@@ -39,98 +40,34 @@ use Illuminate\Support\Collection;
  */
 class RoutingService implements RoutingServiceContract
 {
-    /**
-     * The routes discovered by the router.
-     *
-     * @var \Illuminate\Support\Collection<\Hyde\Framework\Contracts\RouteContract>
-     */
-    protected Collection $routes;
-
-    /**
-     * @var \Hyde\Framework\Services\RoutingService|null The singleton instance of the router.
-     */
-    protected static RoutingService|null $instance = null;
-
     /** @inheritDoc */
     public function __construct()
     {
-        $this->discoverRoutes();
-        HydeKernel::getInstance()->registerRoutingService($this);
     }
 
     /** @inheritDoc */
     public static function getInstance(): self
     {
-        // This creates a singleton pattern. When testing we always create a new instance.
-        if (static::$instance === null || app()->environment('testing')) {
-            static::$instance = new self();
-        }
-
-        return static::$instance;
+        return new self();
     }
 
     /** @inheritDoc */
     public function getRoutes(): Collection
     {
-        return $this->routes;
+        return Hyde::routes();
     }
 
     /** @inheritDoc */
     public function getRoutesForModel(string $pageClass): Collection
     {
         // Return a new filtered collection with only routes that are for the given page class.
-        return $this->routes->filter(function (RouteContract $route) use ($pageClass) {
+        return $this->getRoutes()->filter(function (RouteContract $route) use ($pageClass) {
             return $route->getSourceModel() instanceof $pageClass;
         });
     }
 
-    /**
-     * This internal method adds the specified route to the route index.
-     * It's made public so package developers can hook into the routing system.
-     */
     public function addRoute(RouteContract $route): self
     {
-        $this->routes->put($route->getRouteKey(), $route);
-
         return $this;
-    }
-
-    protected function discover(PageContract $page): static
-    {
-        // Create a new route for the given page, and add it to the index.
-        $this->addRoute(new Route($page));
-
-        return $this;
-    }
-
-    protected function discoverRoutes(): static
-    {
-        $this->routes = new Collection();
-
-        if (Features::hasBladePages()) {
-            $this->discoverPageRoutes(BladePage::class);
-        }
-
-        if (Features::hasMarkdownPages()) {
-            $this->discoverPageRoutes(MarkdownPage::class);
-        }
-
-        if (Features::hasBlogPosts()) {
-            $this->discoverPageRoutes(MarkdownPost::class);
-        }
-
-        if (Features::hasDocumentationPages()) {
-            $this->discoverPageRoutes(DocumentationPage::class);
-        }
-
-        return $this;
-    }
-
-    protected function discoverPageRoutes(string $pageClass): void
-    {
-        /** @var PageContract $pageClass */
-        $pageClass::all()->each(function ($page) {
-            $this->discover($page);
-        });
     }
 }
