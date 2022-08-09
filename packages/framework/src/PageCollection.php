4804ed35bd0eaacc011bb7e28d79e5b164bd4842
Caen De Silva <caen@desilva.se>
2022-08-09 20:16:04 +0200

    Remove kernel instance from constructor as it is never used

--- packages/framework/src/PageCollection.php
+++ packages/framework/src/PageCollection.php
@@ -2,7 +2,6 @@
 
 namespace Hyde\Framework;
 
-use Hyde\Framework\Contracts\HydeKernelContract;
 use Hyde\Framework\Contracts\PageContract;
 use Hyde\Framework\Helpers\Features;
 use Hyde\Framework\Models\Pages\BladePage;
@@ -16,23 +15,14 @@ use Illuminate\Support\Collection;
  */
 final class PageCollection extends Collection
 {
-    protected HydeKernelContract $kernel;
-
-    public static function boot(HydeKernelContract $kernel): self
+    public static function boot(): self
     {
-        return (new self())->setKernel($kernel)->discoverPages();
+        return (new self())->discoverPages();
     }
 
     protected function __construct($items = [])
     {
         parent::__construct($items);
-    }
-
-    protected function setKernel(HydeKernelContract $kernel): self
-    {
-        $this->kernel = $kernel;
-
-        return $this;
     }
 
     public function getPage(string $sourcePath): PageContract
