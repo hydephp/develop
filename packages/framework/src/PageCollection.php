439ac5171ba04aeb47388d6a249e4c11ccadd24b
Caen De Silva <caen@desilva.se>
2022-08-09 14:55:13 +0200

    Discover the pages

--- packages/framework/src/PageCollection.php
+++ packages/framework/src/PageCollection.php
@@ -3,6 +3,12 @@
 namespace Hyde\Framework;
 
 use Hyde\Framework\Contracts\HydeKernelContract;
+use Hyde\Framework\Contracts\PageContract;
+use Hyde\Framework\Helpers\Features;
+use Hyde\Framework\Models\Pages\BladePage;
+use Hyde\Framework\Models\Pages\DocumentationPage;
+use Hyde\Framework\Models\Pages\MarkdownPage;
+use Hyde\Framework\Models\Pages\MarkdownPost;
 use Illuminate\Support\Collection;
 
 class PageCollection extends Collection
@@ -14,5 +20,57 @@ class PageCollection extends Collection
         parent::__construct();
 
         $this->kernel = $kernel;
+        $this->discoverPages();
+    }
+
+    public function getPage(string $sourcePath): PageContract
+    {
+        return $this->firstWhere('sourcePath', $sourcePath);
+    }
+
+    public function getPages(string $pageClass): Collection
+    {
+        return $this->filter(function (PageContract $page) use ($pageClass): bool {
+            return $page instanceof $pageClass;
+        });
+    }
+
+    protected function discoverPages(): static
+    {
+        if (Features::hasBladePages()) {
+            $this->discoverPagesFor(BladePage::class);
+        }
+
+        if (Features::hasMarkdownPages()) {
+            $this->discoverPagesFor(MarkdownPage::class);
+        }
+
+        if (Features::hasBlogPosts()) {
+            $this->discoverPagesFor(MarkdownPost::class);
+        }
+
+        if (Features::hasDocumentationPages()) {
+            $this->discoverPagesFor(DocumentationPage::class);
+        }
+
+        return $this;
+    }
+
+    protected function discoverPagesFor(string $pageClass): void
+    {
+        // @todo Parse the pages here
+
+        /** @var PageContract $pageClass */
+        $pageClass::all()->each(function ($page) {
+            $this->discover($page);
+        });
+    }
+
+    protected function discover(PageContract $page): static
+    {
+        // Create a new route for the given page, and add it to the index.
+        $this->put($page->getSourcePath(), $page);
+
+        return $this;
     }
 }
