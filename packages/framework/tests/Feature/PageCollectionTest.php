1cb7ed7ad5369b5d222997039e7b39496e8cfe06
Caen De Silva <caen@desilva.se>
2022-08-09 21:25:14 +0200

    Test getPages method

--- packages/framework/tests/Feature/PageCollectionTest.php
+++ packages/framework/tests/Feature/PageCollectionTest.php
@@ -69,4 +69,39 @@ class PageCollectionTest extends TestCase
         $collection = PageCollection::boot();
         $this->assertEquals(new BladePage('foo'), $collection->getPage('_pages/foo.blade.php'));
     }
+
+    public function test_get_pages_returns_collection_of_pages_of_given_class()
+    {
+        $this->file('_pages/foo.blade.php');
+        $this->file('_pages/foo.md');
+        $this->file('_posts/foo.md');
+        $this->file('_docs/foo.md');
+        $collection = PageCollection::boot();
+        $this->assertCount(6, $collection);
+
+        $this->assertContainsOnlyInstancesOf(BladePage::class, $collection->getPages(BladePage::class));
+        $this->assertContainsOnlyInstancesOf(MarkdownPage::class, $collection->getPages(MarkdownPage::class));
+        $this->assertContainsOnlyInstancesOf(MarkdownPost::class, $collection->getPages(MarkdownPost::class));
+        $this->assertContainsOnlyInstancesOf(DocumentationPage::class, $collection->getPages(DocumentationPage::class));
+
+        $this->assertEquals(new BladePage('404'), $collection->getPages(BladePage::class)->first());
+        $this->assertEquals(new MarkdownPage('foo'), $collection->getPages(MarkdownPage::class)->first());
+        $this->assertEquals(new MarkdownPost('foo'), $collection->getPages(MarkdownPost::class)->first());
+        $this->assertEquals(new DocumentationPage('foo'), $collection->getPages(DocumentationPage::class)->first());
+    }
+
+    public function test_get_pages_returns_all_pages_when_not_supplied_with_class_string()
+    {
+        $this->file('_pages/foo.blade.php');
+        $this->file('_pages/foo.md');
+        $this->file('_posts/foo.md');
+        $this->file('_docs/foo.md');
+        $collection = PageCollection::boot()->getPages();
+        $this->assertCount(6, $collection);
+
+        $this->assertEquals(new BladePage('foo'), $collection->get('_pages/foo.blade.php'));
+        $this->assertEquals(new MarkdownPage('foo'), $collection->get('_pages/foo.md'));
+        $this->assertEquals(new MarkdownPost('foo'), $collection->get('_posts/foo.md'));
+        $this->assertEquals(new DocumentationPage('foo'), $collection->get('_docs/foo.md'));
+    }
 }
