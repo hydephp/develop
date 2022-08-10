9cee3b8f1188cf633f5a218a64c8fb6e87c86aa1
Caen De Silva <caen@desilva.se>
2022-08-10 11:21:26 +0200

    Create RouteCollectionTest.php

---
+++ packages/framework/tests/Feature/RouteCollectionTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Hyde\Framework\Testing\Feature;
+
+use Hyde\Framework\RouteCollection;
+use Hyde\Testing\TestCase;
+
+/**
+ * @covers \Hyde\Framework\RouteCollection
+ */
+class RouteCollectionTest extends TestCase
+{
+    //
+}
