bc7118d1def326a6b91fd8f97e6083d9f025cca1
Caen De Silva <caen@desilva.se>
2022-08-10 12:58:11 +0200

    Create ConvertsArrayToFrontMatterTest.php

---
+++ packages/framework/tests/Feature/ConvertsArrayToFrontMatterTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Hyde\Framework\Testing\Feature;
+
+use Hyde\Framework\Actions\ConvertsArrayToFrontMatter;
+use Hyde\Testing\TestCase;
+
+/**
+ * @covers \Hyde\Framework\Actions\ConvertsArrayToFrontMatter
+ */
+class ConvertsArrayToFrontMatterTest extends TestCase
+{
+    public function test_action_converts_an_array_to_front_matter()
+    {
+        $array = [
+            'title' => 'My Title',
+            'description' => 'My Description',
+            'author' => 'My Author',
+            'date' => 'My Date',
+            // 'tags' => ['My Tag', 'My Other Tag'],
+        ];
+        $expected = <<<YAML
+---
+title: My Title
+description: My Description
+author: My Author
+date: My Date
+---
+
+YAML;
+        $this->assertEquals($expected, (new ConvertsArrayToFrontMatter)->execute($array));
+    }
+}
