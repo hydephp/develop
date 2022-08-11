d7921b392c9e1bf133373eb538d33d014f415787
Caen De Silva <caen@desilva.se>
2022-08-11 16:37:10 +0200

    Replace class name with static

--- packages/framework/src/PageCollection.php
+++ packages/framework/src/PageCollection.php
@@ -32,7 +32,7 @@ final class PageCollection extends Collection
         return $this->items[$sourcePath] ?? throw new FileNotFoundException($sourcePath.' in page collection');
     }
 
-    public function getPages(?string $pageClass = null): Collection
+    public function getPages(?string $pageClass = null): self
     {
         return ! $pageClass ? $this : $this->filter(function (PageContract $page) use ($pageClass): bool {
             return $page instanceof $pageClass;
