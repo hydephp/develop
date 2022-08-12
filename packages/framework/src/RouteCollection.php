3b694162f5921749cfcf8e23ddcd0040c446fed4
Caen De Silva <caen@desilva.se>
2022-08-12 12:38:31 +0200

    Move class comment from routing service to route collection

--- packages/framework/src/RouteCollection.php
+++ packages/framework/src/RouteCollection.php
@@ -9,8 +9,26 @@ use Hyde\Framework\Models\Route;
 use Illuminate\Support\Collection;
 
 /**
+ * Pseudo-Router for Hyde.
+
  * @see \Hyde\Framework\PageCollection
  * @see \Hyde\Framework\Testing\Feature\RouteTest
+ *
+ * This is not a router in the traditional sense that it decides where to go.
+ * Instead, it creates a pre-generated object encapsulating the Hyde autodiscovery.
+ *
+ * This not only let us emulate Laravel route helpers, but also serve as the
+ * canonical source of truth for the vital HydePHP autodiscovery process.
+ *
+ * The routes defined can then also be used to power the RealtimeCompiler without
+ * having to reverse-engineer the source file mapping.
+ *
+ * Routes are not intended to be added manually, instead the route index is created using
+ * the exact same rules as the current autodiscovery process and compiled file output.
+ *
+ * The route index serves as a multidimensional mapping allowing you to
+ * determine where a source file will be compiled to, and where a compiled
+ * file was generated from.
  */
 final class RouteCollection extends Collection
 {
