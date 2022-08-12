85a35640a3944beeadf9752f622609e16bf3bdd2
Caen De Silva <caen@desilva.se>
2022-08-12 13:56:02 +0200

    Format PHPDoc

--- packages/framework/src/Actions/GeneratesDocumentationSearchIndexFile.php
+++ packages/framework/src/Actions/GeneratesDocumentationSearchIndexFile.php
@@ -11,8 +11,7 @@ use Illuminate\Support\Str;
 use JetBrains\PhpStorm\ArrayShape;
 
 /**
- * Generate a JSON file that can be used as a search index for documentation pages.
- * @internal
+ * @internal Generate a JSON file that can be used as a search index for documentation pages.
  *
  * @todo Convert into Service, and add more strategies, such as slug-only (no file parsing)
  *        search which while dumber, would be much faster to compile and take way less space.
