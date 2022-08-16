1e7447debf16c2d56d6bb0a43d7f90b8c01ddbcb
Caen De Silva <caen@desilva.se>
2022-08-16 15:39:44 +0200

    Add the boot methods

--- packages/framework/src/Foundation/FileCollection.php
+++ packages/framework/src/Foundation/FileCollection.php
@@ -2,9 +2,27 @@
 
 namespace Hyde\Framework\Foundation;
 
+use Hyde\Framework\Contracts\HydeKernelContract;
 use Illuminate\Support\Collection;
 
 final class FileCollection extends Collection
 {
-    //
+    protected HydeKernelContract $kernel;
+
+    public static function boot(HydeKernelContract $kernel): self
+    {
+        return (new self())->setKernel($kernel)->discoverFiles();
+    }
+
+    protected function __construct($items = [])
+    {
+        parent::__construct($items);
+    }
+
+    protected function setKernel(HydeKernelContract $kernel): self
+    {
+        $this->kernel = $kernel;
+
+        return $this;
+    }
 }
