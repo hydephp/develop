41f55bf084a1168c2703083c6cdd515f697b07ba
Caen De Silva <caen@desilva.se>
2022-08-16 15:49:06 +0200

    Implement required method stub

--- packages/framework/src/Foundation/FileCollection.php
+++ packages/framework/src/Foundation/FileCollection.php
@@ -2,7 +2,32 @@
 
 namespace Hyde\Framework\Foundation;
 
+use Hyde\Framework\Helpers\Features;
+use Hyde\Framework\Models\Pages\BladePage;
+use Hyde\Framework\Models\Pages\DocumentationPage;
+use Hyde\Framework\Models\Pages\MarkdownPage;
+use Hyde\Framework\Models\Pages\MarkdownPost;
+
 final class FileCollection extends BaseSystemCollection
 {
-    //
+    protected function runDiscovery(): self
+    {
+        if (Features::hasBladePages()) {
+            $this->discoverFilesFor(BladePage::class);
+        }
+
+        if (Features::hasMarkdownPages()) {
+            $this->discoverFilesFor(MarkdownPage::class);
+        }
+
+        if (Features::hasBlogPosts()) {
+            $this->discoverFilesFor(MarkdownPost::class);
+        }
+
+        if (Features::hasDocumentationPages()) {
+            $this->discoverFilesFor(DocumentationPage::class);
+        }
+
+        return $this;
+    }
 }
