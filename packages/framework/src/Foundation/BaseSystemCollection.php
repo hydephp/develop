2adcefeab690ce332712d9f1f31697e746280bac
Caen De Silva <caen@desilva.se>
2022-08-16 15:45:40 +0200

    Add class PHPDoc

--- packages/framework/src/Foundation/BaseSystemCollection.php
+++ packages/framework/src/Foundation/BaseSystemCollection.php
@@ -5,6 +5,13 @@ namespace Hyde\Framework\Foundation;
 use Illuminate\Support\Collection;
 use Hyde\Framework\Contracts\HydeKernelContract;
 
+/**
+ * @internal Base class for the system collections.
+ *
+ * @see \Hyde\Framework\Foundation\FileCollection
+ * @see \Hyde\Framework\Foundation\PageCollection
+ * @see \Hyde\Framework\Foundation\RouteCollection
+ */
 abstract class BaseSystemCollection extends Collection
 {
     protected HydeKernelContract $kernel;
