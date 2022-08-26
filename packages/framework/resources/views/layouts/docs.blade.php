694fbecbfc0bfd87020977037f102ae35a726d9f
Caen De Silva <caen@desilva.se>
2022-08-26 21:36:02 +0200

    Remove redundant qualifier for namespaced components

--- packages/framework/resources/views/layouts/docs.blade.php
+++ packages/framework/resources/views/layouts/docs.blade.php
@@ -1,32 +1,34 @@
 <!DOCTYPE html>
 <html lang="{{ config('site.language', 'en') }}">
-    <head>
-        @include('hyde::layouts.head')
-    </head>
-    <body id="hyde-docs" class="bg-white dark:bg-gray-900 dark:text-white min-h-screen w-screen relative overflow-x-hidden overflow-y-auto"
-          x-data="{ sidebarOpen: false, searchWindowOpen: false }"
-          x-on:keydown.escape="searchWindowOpen = false; sidebarOpen = false" x-on:keydown.slash="searchWindowOpen = true">
+<head>
+    @include('hyde::layouts.head')
+</head>
+<body id="hyde-docs"
+      class="bg-white dark:bg-gray-900 dark:text-white min-h-screen w-screen relative overflow-x-hidden overflow-y-auto"
+      x-data="{ sidebarOpen: false, searchWindowOpen: false }"
+      x-on:keydown.escape="searchWindowOpen = false; sidebarOpen = false" x-on:keydown.slash="searchWindowOpen = true">
 
-        @include('hyde::components.skip-to-content-button')
+@include('hyde::components.skip-to-content-button')
 
-        @include('hyde::components.docs.documentation-mobile-navigation')
-        @include('hyde::components.docs.documentation-sidebar')
+@include('hyde::components.docs.mobile-navigation')
+@include('hyde::components.docs.documentation-sidebar')
 
-        <main id="content" class="dark:bg-gray-900 min-h-screen bg-gray-50 md:bg-white absolute top-16 md:top-0 w-screen md:left-64 md:w-[calc(100vw_-_16rem)]">
-            <x-hyde::docs.documentation-article :document="\Hyde\Framework\Services\HydeSmartDocs::create($page, $markdown)"/>
-        </main>
+<main id="content"
+      class="dark:bg-gray-900 min-h-screen bg-gray-50 md:bg-white absolute top-16 md:top-0 w-screen md:left-64 md:w-[calc(100vw_-_16rem)]">
+    <x-hyde::docs.documentation-article :document="\Hyde\Framework\Services\HydeSmartDocs::create($page, $markdown)"/>
+</main>
 
-        <div id="support">
-            <div id="sidebar-backdrop" x-show="sidebarOpen" x-transition @click="sidebarOpen = false"
-                 title="Click to close sidebar" class="w-screen h-screen fixed top-0 left-0 cursor-pointer z-10 bg-black/50">
-            </div>
-            
-            @if(Hyde\Framework\Helpers\Features::hasDocumentationSearch())
-                @include('hyde::components.docs.search-widget')
-                @include('hyde::components.docs.search-scripts')
-            @endif
-        </div>
+<div id="support">
+    <div id="sidebar-backdrop" x-show="sidebarOpen" x-transition @click="sidebarOpen = false"
+         title="Click to close sidebar" class="w-screen h-screen fixed top-0 left-0 cursor-pointer z-10 bg-black/50">
+    </div>
 
-        @include('hyde::layouts.scripts')
-    </body>
+    @if(Hyde\Framework\Helpers\Features::hasDocumentationSearch())
+        @include('hyde::components.docs.search-widget')
+        @include('hyde::components.docs.search-scripts')
+    @endif
+</div>
+
+@include('hyde::layouts.scripts')
+</body>
 </html>
