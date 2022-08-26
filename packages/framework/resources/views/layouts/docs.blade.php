24bdd80ac14e028ffccbe285e6ebc64d722af0a1
Caen De Silva <caen@desilva.se>
2022-08-26 21:04:56 +0200

    Extract documentation page search scripts to component

--- packages/framework/resources/views/layouts/docs.blade.php
+++ packages/framework/resources/views/layouts/docs.blade.php
@@ -104,15 +104,7 @@
 		</div>
 		@if(Hyde\Framework\Helpers\Features::hasDocumentationSearch())
 			@include('hyde::components.docs.search-widget')
-			<script src="https://cdn.jsdelivr.net/npm/hydesearch@0.2.1/dist/HydeSearch.min.js" defer></script>
-			<script>
-				window.addEventListener('load', function() {
-					const searchIndexLocation = 'search.json';
-					const Search = new HydeSearch(searchIndexLocation);
-
-					Search.init();
-				});
-			</script>
+			@include('hyde::components.docs.search-scripts')
 		@endif
 	</div>
 
