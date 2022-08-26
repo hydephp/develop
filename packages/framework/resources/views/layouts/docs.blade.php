ca5bd95e4037445478acec547c4fbdf5598a2695
Caen De Silva <caen@desilva.se>
2022-08-26 21:34:20 +0200

    Add newlines

--- packages/framework/resources/views/layouts/docs.blade.php
+++ packages/framework/resources/views/layouts/docs.blade.php
@@ -20,6 +20,7 @@
             <div id="sidebar-backdrop" x-show="sidebarOpen" x-transition @click="sidebarOpen = false"
                  title="Click to close sidebar" class="w-screen h-screen fixed top-0 left-0 cursor-pointer z-10 bg-black/50">
             </div>
+            
             @if(Hyde\Framework\Helpers\Features::hasDocumentationSearch())
                 @include('hyde::components.docs.search-widget')
                 @include('hyde::components.docs.search-scripts')
