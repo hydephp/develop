0fa54a95611568171ec31980fd1a86d8713c5502
Caen De Silva <caen@desilva.se>
2022-08-26 21:27:26 +0200

    Extract documentation page mobile navigation to component

--- packages/framework/resources/views/layouts/docs.blade.php
+++ packages/framework/resources/views/layouts/docs.blade.php
@@ -13,32 +13,7 @@
             document.body.classList.add('js-enabled');
         </script>
 
-        <nav id="mobile-navigation"
-             class="bg-white dark:bg-gray-800 md:hidden flex justify-between w-full h-16 z-40 fixed left-0 top-0 p-4 leading-8 shadow-lg">
-            <strong class="px-2 mr-auto">
-                @if(DocumentationPage::home() !== null)
-                    <a href="{{ DocumentationPage::home() }}">
-                        {{ config('docs.header_title', 'Documentation') }}
-                    </a>
-                @else
-                    {{ config('docs.header_title', 'Documentation') }}
-                @endif
-            </strong>
-            <ul class="flex items-center">
-                <li class="h-8 flex mr-1">
-                    <x-hyde::navigation.theme-toggle-button class="opacity-75 hover:opacity-100"/>
-                </li>
-                <li class="h-8 flex">
-                    <button id="sidebar-toggle" title="Toggle sidebar" aria-label="Toggle sidebar navigation menu"
-                            @click="sidebarOpen = ! sidebarOpen" :class="{'active' : sidebarOpen}">
-                        <span class="icon-bar dark:bg-white h-0" role="presentation"></span>
-                        <span class="icon-bar dark:bg-white h-0" role="presentation"></span>
-                        <span class="icon-bar dark:bg-white h-0" role="presentation"></span>
-                        <span class="icon-bar dark:bg-white h-0" role="presentation"></span>
-                    </button>
-                </li>
-            </ul>
-        </nav>
+        <x-hyde::docs.documentation-mobile-navigation />
         <x-hyde::docs.documentation-sidebar />
         <main id="content" class="dark:bg-gray-900 min-h-screen bg-gray-50 md:bg-white absolute top-16 md:top-0 w-screen md:left-64 md:w-[calc(100vw_-_16rem)]">
             <x-hyde::docs.documentation-article :document="\Hyde\Framework\Services\HydeSmartDocs::create($page, $markdown)"/>
