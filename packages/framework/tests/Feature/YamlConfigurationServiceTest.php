5d577e06d1e0ddfcbd23e9d626cf4c13ab76c9e2
Caen De Silva <caen@desilva.se>
2022-08-28 21:30:18 +0200

    Create YamlConfigurationServiceTest.php

---
+++ packages/framework/tests/Feature/YamlConfigurationServiceTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Hyde\Framework\Testing\Feature;
+
+use Hyde\Framework\Services\YamlConfigurationService;
+use Hyde\Testing\TestCase;
+
+/**
+ * @covers \Hyde\Framework\Services\YamlConfigurationService
+ */
+class YamlConfigurationServiceTest extends TestCase
+{
+    //
+}
