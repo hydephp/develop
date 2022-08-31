9a69f94f1def5d63bac7ee429b7e7f4fe512f35c
Caen De Silva <caen@desilva.se>
2022-08-31 17:13:49 +0200

    Move RSS feed generator command logic to task

--- packages/framework/src/Commands/HydeBuildRssFeedCommand.php
+++ packages/framework/src/Commands/HydeBuildRssFeedCommand.php
@@ -2,52 +2,22 @@
 
 namespace Hyde\Framework\Commands;
 
+use Hyde\Framework\Actions\PostBuildTasks\GenerateRssFeed;
 use Hyde\Framework\Concerns\ActionCommand;
-use Hyde\Framework\Helpers\Features;
-use Hyde\Framework\Hyde;
-use Hyde\Framework\Services\RssFeedService;
 
 /**
- * Hyde Command to run the Build Process for the RSS Feed.
+ * Hyde command to run the build process for the RSS feed.
  *
  * @see \Hyde\Framework\Testing\Feature\Commands\HydeBuildRssFeedCommandTest
  */
 class HydeBuildRssFeedCommand extends ActionCommand
 {
-    /**
-     * The signature of the command.
-     *
-     * @var string
-     */
-    protected $signature = 'build:rss {--no-api : (Not yet supported)}';
+    protected $signature = 'build:rss';
 
-    /**
-     * The description of the command.
-     *
-     * @var string
-     */
     protected $description = 'Generate the RSS feed';
 
-    /**
-     * Execute the console command.
-     *
-     * @return int
-     */
-    public function handle(): int
+    public function handle()
     {
-        if (! Features::rss()) {
-            $this->error('Cannot generate an RSS feed, please check your configuration.');
-
-            return 1;
-        }
-
-        $this->action('Generating RSS feed', function () {
-            file_put_contents(
-                Hyde::getSiteOutputPath(RssFeedService::getDefaultOutputFilename()),
-                RssFeedService::generateFeed()
-            );
-        }, sprintf('Created <info>%s</info>', RssFeedService::getDefaultOutputFilename()));
-
-        return 0;
+        (new GenerateRssFeed($this->output))->handle();
     }
 }
