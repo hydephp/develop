2b5dd36920a2df1ec3af0d10079ddb0cdf63db0c
Caen De Silva <caen@desilva.se>
2022-08-31 22:14:48 +0200

    Convert concatenation to string interpolation

--- packages/framework/src/Actions/PostBuildTasks/GenerateSearch.php
+++ packages/framework/src/Actions/PostBuildTasks/GenerateSearch.php
@@ -25,7 +25,7 @@ class GenerateSearch extends AbstractBuildTask
         if (config('docs.create_search_page', true)) {
             $directory = DocumentationSearchService::generateSearchPage();
 
-            $this->createdSiteFile($directory.'/search.html');
+            $this->createdSiteFile("$directory/search.html");
         }
     }
 
