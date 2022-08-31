6bdd5950176434f1d0a6ee075d685684cbf4bf9c
Caen De Silva <caen@desilva.se>
2022-08-31 20:00:10 +0200

    Update dashboard.blade.php

--- projects/shelf/admin/resources/views/pages/dashboard.blade.php
+++ projects/shelf/admin/resources/views/pages/dashboard.blade.php
@@ -104,21 +104,74 @@ use Hyde\Framework\Services\DiscoveryService;
 <div class="px-4 md:px-10 mx-auto w-full -m-24">
 	<div class="flex flex-wrap">
 		<div class="w-full xl:w-8/12 mb-12 xl:mb-0 px-4">
-			<div class="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded bg-slate-700">
-				<div class="rounded-t mb-0 px-4 py-3 bg-transparent">
-					<div class="flex flex-wrap items-center">
-						<div class="relative w-full max-w-full flex-grow flex-1">
-							<h6 class="uppercase text-slate-100 mb-1 text-xs font-semibold"> Overview </h6>
-							<h2 class="text-white text-xl font-semibold"> Sales value </h2>
+			<div class="relative flex flex-col min-w-0 break-words w-full mb-6 ">
+				<section class="block w-full overflow-x-auto prose max-w-full shadow-lg  rounded bg-white">
+					<header>
+						<div class="rounded-t px-0 py-3 border-0">
+							<div class="flex flex-wrap items-center">
+								<div class="relative w-full px-0 max-w-full flex-grow flex-1">
+									<h3 class="font-semibold text-lg text-slate-700 my-1 px-4">Blade Pages</h3>
+								</div>
+							</div>
 						</div>
-					</div>
-				</div>
-				<div class="p-4 flex-auto">
-					<!-- Chart -->
-					<div class="relative h-350-px">
-						<canvas id="line-chart"></canvas>
-					</div>
-				</div>
+					</header>
+					<table class="table-auto dashboard-table mt-0">
+						<thead class="bg-slate-100">
+						<tr>
+							<th class="px-3 pt-2">Title</th>
+							<th class="px-3 pt-2">Source File</th>
+						</tr>
+						</thead>
+						<tbody>
+						@foreach (\Hyde\Framework\Models\Pages\BladePage::all() as $page)
+							<tr>
+								<td class="px-3">
+									<a href="{{ $page->getRoute() }}">
+										{{ Hyde::makeTitle($page->view) }}
+									</a>
+								</td>
+								<td class="px-3">
+									{{ $page->getSourcePath() }}
+								</td>
+							</tr>
+						@endforeach
+						</tbody>
+					</table>
+				</section>
+
+				<section class="block w-full overflow-x-auto prose max-w-full shadow-lg  rounded bg-white mt-8">
+					<header>
+						<div class="rounded-t px-0 py-3 border-0">
+							<div class="flex flex-wrap items-center">
+								<div class="relative w-full px-0 max-w-full flex-grow flex-1">
+									<h3 class="font-semibold text-lg text-slate-700 my-1 px-4">Markdown Pages</h3>
+								</div>
+							</div>
+						</div>
+					</header>
+					<table class="table-auto dashboard-table mt-0">
+						<thead class="bg-slate-100">
+						<tr>
+							<th class="px-3 pt-2">Title</th>
+							<th class="px-3 pt-2">Source File</th>
+						</tr>
+						</thead>
+						<tbody>
+						@foreach (\Hyde\Framework\Models\Pages\MarkdownPage::all() as $page)
+							<tr>
+								<td class="px-3">
+									<a href="{{ $page->getRoute() }}">
+										{{ Hyde::makeTitle($page->view) }}
+									</a>
+								</td>
+								<td class="px-3">
+									{{ $page->getSourcePath() }}
+								</td>
+							</tr>
+						@endforeach
+						</tbody>
+					</table>
+				</section>
 			</div>
 		</div>
 		<div class="w-full xl:w-4/12 px-4">
@@ -159,263 +212,6 @@ use Hyde\Framework\Services\DiscoveryService;
 							</tbody>
 						</table>
 					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-	<div class="flex flex-wrap mt-4">
-		<div class="w-full xl:w-8/12 mb-12 xl:mb-0 px-4">
-			<div class="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded">
-				<div class="rounded-t mb-0 px-4 py-3 border-0">
-					<div class="flex flex-wrap items-center">
-						<div class="relative w-full px-4 max-w-full flex-grow flex-1">
-							<h3 class="font-semibold text-base text-slate-700"> Page visits </h3>
-						</div>
-						<div class="relative w-full px-4 max-w-full flex-grow flex-1 text-right">
-							<button
-								class="bg-indigo-500 text-white active:bg-indigo-600 text-xs font-bold uppercase px-3 py-1 rounded outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-								type="button"> See all </button>
-						</div>
-					</div>
-				</div>
-				<div class="block w-full overflow-x-auto">
-					<!-- Projects table -->
-					<table class="items-center w-full bg-transparent border-collapse">
-						<thead>
-							<tr>
-								<th
-									class="px-6 bg-slate-50 text-slate-500 align-middle border border-solid border-slate-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
-									Page name </th>
-								<th
-									class="px-6 bg-slate-50 text-slate-500 align-middle border border-solid border-slate-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
-									Visitors </th>
-								<th
-									class="px-6 bg-slate-50 text-slate-500 align-middle border border-solid border-slate-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
-									Unique users </th>
-								<th
-									class="px-6 bg-slate-50 text-slate-500 align-middle border border-solid border-slate-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
-									Bounce rate </th>
-							</tr>
-						</thead>
-						<tbody>
-							<tr>
-								<th
-									class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left">
-									/argon/ </th>
-								<td
-									class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-									4,569 </td>
-								<td
-									class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-									340 </td>
-								<td
-									class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-									<i class="fas fa-arrow-up text-emerald-500 mr-4"></i> 46,53%
-								</td>
-							</tr>
-							<tr>
-								<th
-									class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left">
-									/argon/index.html </th>
-								<td
-									class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-									3,985 </td>
-								<td
-									class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-									319 </td>
-								<td
-									class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-									<i class="fas fa-arrow-down text-orange-500 mr-4"></i> 46,53%
-								</td>
-							</tr>
-							<tr>
-								<th
-									class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left">
-									/argon/charts.html </th>
-								<td
-									class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-									3,513 </td>
-								<td
-									class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-									294 </td>
-								<td
-									class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-									<i class="fas fa-arrow-down text-orange-500 mr-4"></i> 36,49%
-								</td>
-							</tr>
-							<tr>
-								<th
-									class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left">
-									/argon/tables.html </th>
-								<td
-									class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-									2,050 </td>
-								<td
-									class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-									147 </td>
-								<td
-									class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-									<i class="fas fa-arrow-up text-emerald-500 mr-4"></i> 50,87%
-								</td>
-							</tr>
-							<tr>
-								<th
-									class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left">
-									/argon/profile.html </th>
-								<td
-									class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-									1,795 </td>
-								<td
-									class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-									190 </td>
-								<td
-									class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-									<i class="fas fa-arrow-down text-red-500 mr-4"></i> 46,53%
-								</td>
-							</tr>
-						</tbody>
-					</table>
-				</div>
-			</div>
-		</div>
-		<div class="w-full xl:w-4/12 px-4">
-			<div class="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded">
-				<div class="rounded-t mb-0 px-4 py-3 border-0">
-					<div class="flex flex-wrap items-center">
-						<div class="relative w-full px-4 max-w-full flex-grow flex-1">
-							<h3 class="font-semibold text-base text-slate-700"> Social traffic </h3>
-						</div>
-						<div class="relative w-full px-4 max-w-full flex-grow flex-1 text-right">
-							<button
-								class="bg-indigo-500 text-white active:bg-indigo-600 text-xs font-bold uppercase px-3 py-1 rounded outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-								type="button"> See all </button>
-						</div>
-					</div>
-				</div>
-				<div class="block w-full overflow-x-auto">
-					<!-- Projects table -->
-					<table class="items-center w-full bg-transparent border-collapse">
-						<thead class="thead-light">
-							<tr>
-								<th
-									class="px-6 bg-slate-50 text-slate-500 align-middle border border-solid border-slate-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
-									Referral </th>
-								<th
-									class="px-6 bg-slate-50 text-slate-500 align-middle border border-solid border-slate-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
-									Visitors </th>
-								<th
-									class="px-6 bg-slate-50 text-slate-500 align-middle border border-solid border-slate-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left min-w-140-px">
-								</th>
-							</tr>
-						</thead>
-						<tbody>
-							<tr>
-								<th
-									class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left">
-									Facebook </th>
-								<td
-									class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-									1,480 </td>
-								<td
-									class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-									<div class="flex items-center">
-										<span class="mr-2">60%</span>
-										<div class="relative w-full">
-											<div class="overflow-hidden h-2 text-xs flex rounded bg-red-200">
-												<div style="width: 60%"
-													class="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-red-500">
-												</div>
-											</div>
-										</div>
-									</div>
-								</td>
-							</tr>
-							<tr>
-								<th
-									class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left">
-									Facebook </th>
-								<td
-									class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-									5,480 </td>
-								<td
-									class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-									<div class="flex items-center">
-										<span class="mr-2">70%</span>
-										<div class="relative w-full">
-											<div class="overflow-hidden h-2 text-xs flex rounded bg-emerald-200">
-												<div style="width: 70%"
-													class="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-emerald-500">
-												</div>
-											</div>
-										</div>
-									</div>
-								</td>
-							</tr>
-							<tr>
-								<th
-									class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left">
-									Google </th>
-								<td
-									class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-									4,807 </td>
-								<td
-									class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-									<div class="flex items-center">
-										<span class="mr-2">80%</span>
-										<div class="relative w-full">
-											<div class="overflow-hidden h-2 text-xs flex rounded bg-purple-200">
-												<div style="width: 80%"
-													class="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-purple-500">
-												</div>
-											</div>
-										</div>
-									</div>
-								</td>
-							</tr>
-							<tr>
-								<th
-									class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left">
-									Instagram </th>
-								<td
-									class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-									3,678 </td>
-								<td
-									class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-									<div class="flex items-center">
-										<span class="mr-2">75%</span>
-										<div class="relative w-full">
-											<div class="overflow-hidden h-2 text-xs flex rounded bg-indigo-200">
-												<div style="width: 75%"
-													class="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-indigo-500">
-												</div>
-											</div>
-										</div>
-									</div>
-								</td>
-							</tr>
-							<tr>
-								<th
-									class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left">
-									twitter </th>
-								<td
-									class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-									2,645 </td>
-								<td
-									class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-									<div class="flex items-center">
-										<span class="mr-2">30%</span>
-										<div class="relative w-full">
-											<div class="overflow-hidden h-2 text-xs flex rounded bg-orange-200">
-												<div style="width: 30%"
-													class="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-emerald-500">
-												</div>
-											</div>
-										</div>
-									</div>
-								</td>
-							</tr>
-						</tbody>
-					</table>
 				</div>
 			</div>
 		</div>
