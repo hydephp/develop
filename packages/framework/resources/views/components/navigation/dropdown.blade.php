8973484f4daf8c4c7fd5ec63979d2b325d854565
Caen De Silva <caen@desilva.se>
2022-09-01 14:58:36 +0200

    Add Alpine interactivity

--- packages/framework/resources/views/components/navigation/dropdown.blade.php
+++ packages/framework/resources/views/components/navigation/dropdown.blade.php
@@ -1,8 +1,9 @@
-<div class="dropdown-container relative">
-    <button class="dropdown-button block my-2 md:my-0 md:inline-block py-1 text-gray-700 hover:text-gray-900 dark:text-gray-100">
+<div class="dropdown-container relative" x-data="{ open: false }">
+    <button class="dropdown-button block my-2 md:my-0 md:inline-block py-1 text-gray-700 hover:text-gray-900 dark:text-gray-100"
+            x-on:click="open = ! open">
         {{ $label }}
     </button>
-    <ul class="dropdown-items absolute">
+    <ul class="dropdown-items absolute" :class="open ? '' : 'hidden'">
         {{ $slot }}
     </ul>
 </div>
