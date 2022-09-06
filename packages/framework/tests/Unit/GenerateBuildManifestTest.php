d93053006e425fd7691c2618df969dd53c95a6c4
Caen De Silva <caen@desilva.se>
2022-09-06 15:19:47 +0200

    Test the manifest

--- packages/framework/tests/Unit/GenerateBuildManifestTest.php
+++ packages/framework/tests/Unit/GenerateBuildManifestTest.php
@@ -16,5 +16,17 @@ class GenerateBuildManifestTest extends TestCase
         (new GenerateBuildManifest())->run();
 
         $this->assertFileExists(Hyde::path('storage/framework/cache/build-manifest.json'));
+
+        $manifest = json_decode(file_get_contents(Hyde::path('storage/framework/cache/build-manifest.json')), true);
+
+        $this->assertIsArray($manifest);
+        $this->assertCount(2, $manifest);
+
+        $this->assertArrayHasKey('page', $manifest[0]);
+        $this->assertArrayHasKey('source_hash', $manifest[0]);
+        $this->assertArrayHasKey('output_hash', $manifest[0]);
+
+        $this->assertStringContainsString('_pages/404.blade.php', $manifest[0]['page']);
+        $this->assertStringContainsString('_pages/index.blade.php', $manifest[1]['page']);
     }
 }
