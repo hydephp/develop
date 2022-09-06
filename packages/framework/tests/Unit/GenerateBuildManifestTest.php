f119e60da1141b0e7c1bcf885348285b0f5cf027
Caen De Silva <caen@desilva.se>
2022-09-06 14:56:53 +0200

    Create GenerateBuildManifestTest.php

---
+++ packages/framework/tests/Unit/GenerateBuildManifestTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Hyde\Framework\Testing\Unit;
+
+use Hyde\Testing\TestCase;
+use Hyde\Framework\Actions\PostBuildTasks\GenerateBuildManifest;
+
+/**
+ * @covers \Hyde\Framework\Actions\PostBuildTasks\GenerateBuildManifest
+ */
+class GenerateBuildManifestTest extends TestCase
+{
+    //
+}
