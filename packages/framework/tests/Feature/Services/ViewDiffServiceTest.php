a3c679f1a8071ae554c9518e7025b4f33f8ff9b4
Caen De Silva <caen@desilva.se>
2022-09-08 19:18:08 +0200

    Inline test alias

--- packages/framework/tests/Feature/Services/ViewDiffServiceTest.php
+++ packages/framework/tests/Feature/Services/ViewDiffServiceTest.php
@@ -4,7 +4,6 @@ namespace Hyde\Framework\Testing\Feature\Services;
 
 use Hyde\Framework\Hyde;
 use Hyde\Framework\Services\ViewDiffService;
-use Hyde\Framework\Services\ViewDiffService as Service;
 use Hyde\Testing\TestCase;
 
 /**
@@ -52,51 +51,51 @@ class ViewDiffServiceTest extends TestCase
 
     public function test_method_returns_string()
     {
-        $this->assertIsString(Service::unixsum('foo'));
+        $this->assertIsString(ViewDiffService::unixsum('foo'));
     }
 
     public function test_method_returns_string_with_length_of_32()
     {
-        $this->assertEquals(32, strlen(Service::unixsum('foo')));
+        $this->assertEquals(32, strlen(ViewDiffService::unixsum('foo')));
     }
 
     public function test_method_returns_string_matching_expected_format()
     {
-        $this->assertMatchesRegularExpression('/^[a-f0-9]{32}$/', Service::unixsum('foo'));
+        $this->assertMatchesRegularExpression('/^[a-f0-9]{32}$/', ViewDiffService::unixsum('foo'));
     }
 
     public function test_method_returns_same_value_for_same_string_using_normal_method()
     {
-        $this->assertEquals(md5('foo'), Service::unixsum('foo'));
+        $this->assertEquals(md5('foo'), ViewDiffService::unixsum('foo'));
     }
 
     public function test_method_returns_different_value_for_different_string()
     {
-        $this->assertNotEquals(Service::unixsum('foo'), Service::unixsum('bar'));
+        $this->assertNotEquals(ViewDiffService::unixsum('foo'), ViewDiffService::unixsum('bar'));
     }
 
     public function test_function_is_case_sensitive()
     {
-        $this->assertNotEquals(Service::unixsum('foo'), Service::unixsum('FOO'));
+        $this->assertNotEquals(ViewDiffService::unixsum('foo'), ViewDiffService::unixsum('FOO'));
     }
 
     public function test_function_is_space_sensitive()
     {
-        $this->assertNotEquals(Service::unixsum(' foo '), Service::unixsum('foo'));
+        $this->assertNotEquals(ViewDiffService::unixsum(' foo '), ViewDiffService::unixsum('foo'));
     }
 
     public function test_method_returns_same_value_regardless_of_end_of_line_sequence()
     {
-        $this->assertEquals(Service::unixsum('foo'), Service::unixsum('foo'));
-        $this->assertEquals(Service::unixsum("foo\n"), Service::unixsum("foo\n"));
-        $this->assertEquals(Service::unixsum("foo\n"), Service::unixsum("foo\r"));
-        $this->assertEquals(Service::unixsum("foo\n"), Service::unixsum("foo\r\n"));
+        $this->assertEquals(ViewDiffService::unixsum('foo'), ViewDiffService::unixsum('foo'));
+        $this->assertEquals(ViewDiffService::unixsum("foo\n"), ViewDiffService::unixsum("foo\n"));
+        $this->assertEquals(ViewDiffService::unixsum("foo\n"), ViewDiffService::unixsum("foo\r"));
+        $this->assertEquals(ViewDiffService::unixsum("foo\n"), ViewDiffService::unixsum("foo\r\n"));
     }
 
     public function test_method_returns_same_value_for_string_with_mixed_end_of_line_sequences()
     {
-        $this->assertEquals(Service::unixsum("foo\nbar\r\nbaz\r\n"),
-            Service::unixsum("foo\nbar\nbaz\n"));
+        $this->assertEquals(ViewDiffService::unixsum("foo\nbar\r\nbaz\r\n"),
+            ViewDiffService::unixsum("foo\nbar\nbaz\n"));
     }
 
     public function test_method_returns_same_value_when_loaded_from_file()
@@ -105,7 +104,7 @@ class ViewDiffServiceTest extends TestCase
         $file = tempnam(sys_get_temp_dir(), 'foo');
         file_put_contents($file, $string);
 
-        $this->assertEquals(Service::unixsum($string), Service::unixsum(file_get_contents($file)));
+        $this->assertEquals(ViewDiffService::unixsum($string), ViewDiffService::unixsum(file_get_contents($file)));
 
         unlink($file);
     }
@@ -116,7 +115,7 @@ class ViewDiffServiceTest extends TestCase
         $file = tempnam(sys_get_temp_dir(), 'foo');
         file_put_contents($file, $string);
 
-        $this->assertEquals(Service::unixsum($string), Service::unixsumFile($file));
+        $this->assertEquals(ViewDiffService::unixsum($string), ViewDiffService::unixsumFile($file));
 
         unlink($file);
     }
