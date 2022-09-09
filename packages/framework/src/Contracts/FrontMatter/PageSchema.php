f3843cc63cbed63dd9d015e050cdf5074c472102
Caen De Silva <caen@desilva.se>
2022-09-09 17:31:21 +0200

    Update PageSchema.php

--- packages/framework/src/Contracts/FrontMatter/PageSchema.php
+++ packages/framework/src/Contracts/FrontMatter/PageSchema.php
@@ -3,7 +3,7 @@
 namespace Hyde\Framework\Contracts\FrontMatter;
 
 /**
- * The front matter properties supported by the following HydePHP page types and their children:.
+ * The front matter properties supported by the following HydePHP page types and their children.
  *
  * @see \Hyde\Framework\Concerns\AbstractPage
  */
