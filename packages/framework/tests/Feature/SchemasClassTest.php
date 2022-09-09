91fd29e224c5aebb97d15a9c35d2c3a9f264459d
Caen De Silva <caen@desilva.se>
2022-09-09 16:06:26 +0200

    Create SchemasClassTest.php

---
+++ packages/framework/tests/Feature/SchemasClassTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Hyde\Framework\Testing\Feature;
+
+use Hyde\Testing\TestCase;
+
+/**
+ * @covers \Hyde\Framework\Concerns\FrontMatter\Schemas\Schemas
+ */
+class SchemasClassTest extends TestCase
+{
+    //
+}
