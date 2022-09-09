b2bfc9e56899a3fd004a762c90f75c6d54e66ed7
Caen De Silva <caen@desilva.se>
2022-09-09 21:38:13 +0200

    Inline method usage for generating route key

--- packages/framework/src/Concerns/AbstractPage.php
+++ packages/framework/src/Concerns/AbstractPage.php
@@ -51,7 +51,7 @@ abstract class AbstractPage implements PageContract, CompilableContract, PageSch
     public function __construct(string $identifier = '', FrontMatter|array $matter = [])
     {
         $this->identifier = $identifier;
-        $this->routeKey = $this->getCurrentPagePath();
+        $this->routeKey = trim(static::getOutputDirectory().'/'.$this->identifier, '/');
 
         $this->matter = $matter instanceof FrontMatter ? $matter : new FrontMatter($matter);
         $this->constructPageSchemas();
