0e473bdeed141ee97c32e604bd435059ad189190
Caen De Silva <caen@desilva.se>
2022-09-09 19:39:52 +0200

    Inline schema trait

--- packages/framework/src/Models/Pages/DocumentationPage.php
+++ packages/framework/src/Models/Pages/DocumentationPage.php
@@ -18,6 +18,29 @@ class DocumentationPage extends AbstractMarkdownPage
     public static string $outputDirectory = 'docs';
     public static string $template = 'hyde::layouts/docs';
 
+    /**
+     * The sidebar category group, if any.
+     *
+     * Can be overridden in front matter, or by putting the
+     * source file in a subdirectory of the same category name.
+     */
+    public ?string $category = null;
+
+    /**
+     * The label for the page shown in the sidebar.
+     */
+    public ?string $label = null;
+
+    /**
+     * Hides the page from the sidebar.
+     */
+    public ?bool $hidden = null;
+
+    /**
+     * The priority of the page used for ordering the sidebar.
+     */
+    public ?int $priority = null;
+
     /** @inheritDoc */
     public function __construct(string $identifier = '', ?FrontMatter $matter = null, ?Markdown $markdown = null)
     {
