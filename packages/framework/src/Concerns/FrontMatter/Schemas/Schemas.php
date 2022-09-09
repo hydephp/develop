a2969ad8529ed2c9b09223231a0b2d1f170b0f83
Caen De Silva <caen@desilva.se>
2022-09-09 16:04:38 +0200

    Create Schemas.php

---
+++ packages/framework/src/Concerns/FrontMatter/Schemas/Schemas.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Hyde\Framework\Concerns\FrontMatter\Schemas;
+
+/**
+ * Class representation of all the available schema traits with helpers to access them.
+ */
+class Schemas
+{
+    //
+}
