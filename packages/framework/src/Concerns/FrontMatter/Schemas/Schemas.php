06fe982c22d23c7ade067919f6188b992ce2634e
Caen De Silva <caen@desilva.se>
2022-09-09 16:10:32 +0200

    Add note about optional types

--- packages/framework/src/Concerns/FrontMatter/Schemas/Schemas.php
+++ packages/framework/src/Concerns/FrontMatter/Schemas/Schemas.php
@@ -4,6 +4,8 @@ namespace Hyde\Framework\Concerns\FrontMatter\Schemas;
 
 /**
  * Class representation of all the available schema traits with helpers to access them.
+ *
+ * All front matter properties are always optional in HydePHP.
  */
 final class Schemas
 {
