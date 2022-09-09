4364d6ec57858179dd2f4823d820ad77a16d8de7
Caen De Silva <caen@desilva.se>
2022-09-09 19:52:22 +0200

    Create DocumentationPageSchema.php

---
+++ packages/framework/src/Contracts/FrontMatter/DocumentationPageSchema.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Hyde\Framework\Contracts\FrontMatter;
+
+interface DocumentationPageSchema
+{
+    public const DOCUMENTATION_PAGE_SCHEMA = [
+        'category' => 'string',
+        'label'    => 'string',
+        'hidden'   => 'bool',
+        'priority' => 'int',
+    ];
+}
