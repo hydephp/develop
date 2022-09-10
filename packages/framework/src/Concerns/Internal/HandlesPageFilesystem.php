85a2b6e41f838e6b79e061f06671fc02e3aac21d
Caen De Silva <caen@desilva.se>
2022-09-10 22:21:38 +0200

    Overhaul PHPDocs

--- packages/framework/src/Concerns/Internal/HandlesPageFilesystem.php
+++ packages/framework/src/Concerns/Internal/HandlesPageFilesystem.php
@@ -2,14 +2,15 @@
 
 namespace Hyde\Framework\Concerns\Internal;
 
+/**
+ * @internal This trait is not meant to be used outside of Hyde.
+ *
+ * Handles the filesystem logic for Hyde pages. All paths are relative to the project root.
+ */
 trait HandlesPageFilesystem
 {
     /**
      * Get the directory in where source files are stored.
-     *
-     * @return string Path relative to the root of the project
-     *
-     * @example output: '_pages'
      */
     final public static function sourceDirectory(): string
     {
@@ -18,10 +19,6 @@ trait HandlesPageFilesystem
 
     /**
      * Get the output subdirectory to store compiled HTML.
-     *
-     * @return string Relative to the site output directory.
-     *
-     * @example output: '', 'posts, 'docs
      */
     final public static function outputDirectory(): string
     {
@@ -30,8 +27,6 @@ trait HandlesPageFilesystem
 
     /**
      * Get the file extension of the source files.
-     *
-     * @return string (e.g. ".md")
      */
     final public static function fileExtension(): string
     {
@@ -40,12 +35,6 @@ trait HandlesPageFilesystem
 
     /**
      * Qualify a page identifier into a referenceable local file path.
-     *
-     * @param  string  $identifier  for the page model source file.
-     * @return string path to the file relative to project root
-     *
-     * @example input: MarkdownPost::qualifyFilepath('hello-world')
-     * @example output: '_posts/hello-world.md'
      */
     public static function sourcePath(string $identifier): string
     {
@@ -54,11 +43,6 @@ trait HandlesPageFilesystem
 
     /**
      * Get the proper site output path for a page model.
-     *
-     * @param  string  $identifier  for the page model source file.
-     * @return string of the output file relative to the site output directory.
-     *
-     * @example DocumentationPage::getOutputPath('index') => 'docs/index.html'
      */
     public static function outputPath(string $identifier): string
     {
@@ -68,8 +52,6 @@ trait HandlesPageFilesystem
     /**
      * Get the path to the source file, relative to the project root.
      * In other words, qualify the identifier of the page instance.
-     *
-     * @return string Path relative to the project root.
      */
     public function getSourcePath(): string
     {
@@ -77,9 +59,7 @@ trait HandlesPageFilesystem
     }
 
     /**
-     * Get the path where the compiled page will be saved.
-     *
-     * @return string Path relative to the site output directory.
+     * Get the path where the compiled page instance will be saved.
      */
     public function getOutputPath(): string
     {
