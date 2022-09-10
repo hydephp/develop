c9e3a4670511b2664e77eb31a09c81112d69f107
Caen De Silva <caen@desilva.se>
2022-09-10 19:05:00 +0200

    Inline AbstractMarkdownPage methods to MarkdownPage

--- packages/framework/src/Models/Pages/MarkdownPage.php
+++ packages/framework/src/Models/Pages/MarkdownPage.php
@@ -3,10 +3,55 @@
 namespace Hyde\Framework\Models\Pages;
 
 use Hyde\Framework\Concerns\AbstractMarkdownPage;
+use Hyde\Framework\Concerns\HydePage;
+use Hyde\Framework\Models\FrontMatter;
+use Hyde\Framework\Models\Markdown;
 
 class MarkdownPage extends AbstractMarkdownPage
 {
     public static string $sourceDirectory = '_pages';
     public static string $outputDirectory = '';
     public static string $template = 'hyde::layouts/page';
+    public static string $fileExtension = '.md';
+    public string $identifier;
+    public Markdown $markdown;
+
+    /** @interitDoc */
+    public function __construct(string $identifier = '', ?FrontMatter $matter = null, ?Markdown $markdown = null)
+    {
+        $this->identifier = $identifier;
+        $this->matter = $matter ?? new FrontMatter();
+        $this->markdown = $markdown ?? new Markdown();
+
+        HydePage::__construct($this->identifier, $this->matter);
+    }
+
+    /** @interitDoc */
+    public static function make(string $identifier = '', array $matter = [], string $body = ''): static
+    {
+        return new static($identifier, new FrontMatter($matter), new Markdown($body));
+    }
+
+    /** @inheritDoc */
+    public function compile(): string
+    {
+        return view($this->getBladeView())->with([
+            'title' => $this->title,
+            'markdown' => $this->markdown->compile(static::class),
+        ])->render();
+    }
+
+    /** @inheritDoc */
+    public function save(): static
+    {
+        file_put_contents(Hyde::path($this->getSourcePath()), ltrim("$this->matter\n$this->markdown"));
+
+        return $this;
+    }
+
+    /** @inheritDoc */
+    public function markdown(): Markdown
+    {
+        return $this->markdown;
+    }
 }
