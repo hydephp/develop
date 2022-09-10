7761eb87601b93bbee5224a99a396188d4b07896
Caen De Silva <caen@desilva.se>
2022-09-10 23:33:36 +0200

    Merge temporary internal filesystem trait back into main class

--- packages/framework/src/Concerns/HydePage.php
+++ packages/framework/src/Concerns/HydePage.php
@@ -29,7 +29,6 @@ use Hyde\Framework\Services\DiscoveryService;
 abstract class HydePage implements CompilableContract, PageSchema
 {
     use ConstructsPageSchemas;
-    use Internal\HandlesPageFilesystem;
     use Internal\HandlesPageRouting;
     use Internal\HandlesPageMatter;
 
@@ -91,6 +90,65 @@ abstract class HydePage implements CompilableContract, PageSchema
     public static function all(): PageCollection
     {
         return Hyde::pages()->getPages(static::class);
+    }
+
+    // Section: Filesystem
+
+    /**
+     * Get the directory in where source files are stored.
+     */
+    final public static function sourceDirectory(): string
+    {
+        return unslash(static::$sourceDirectory);
+    }
+
+    /**
+     * Get the output subdirectory to store compiled HTML.
+     */
+    final public static function outputDirectory(): string
+    {
+        return unslash(static::$outputDirectory);
+    }
+
+    /**
+     * Get the file extension of the source files.
+     */
+    final public static function fileExtension(): string
+    {
+        return '.'.ltrim(static::$fileExtension, '.');
+    }
+
+    /**
+     * Qualify a page identifier into a referenceable local file path.
+     */
+    public static function sourcePath(string $identifier): string
+    {
+        return static::sourceDirectory().'/'.unslash($identifier).static::fileExtension();
+    }
+
+    /**
+     * Get the proper site output path for a page model.
+     */
+    public static function outputPath(string $identifier): string
+    {
+        return static::routeKey($identifier).'.html';
+    }
+
+    /**
+     * Get the path to the source file, relative to the project root.
+     * In other words, qualify the identifier of the page instance.
+     */
+    public function getSourcePath(): string
+    {
+        return static::sourcePath($this->identifier);
+    }
+
+    /**
+     * Get the path where the compiled page instance will be saved.
+     */
+    public function getOutputPath(): string
+    {
+        return static::outputPath($this->identifier);
     }
 
     // Section: Getters
