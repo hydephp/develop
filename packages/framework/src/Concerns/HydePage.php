f847287bffaa431b8696d409c38cfdb5a6d8d23a
Caen De Silva <caen@desilva.se>
2022-09-10 23:37:37 +0200

    Merge temporary internal front matter trait back into main class

--- packages/framework/src/Concerns/HydePage.php
+++ packages/framework/src/Concerns/HydePage.php
@@ -31,7 +31,6 @@ use Hyde\Framework\Services\DiscoveryService;
 abstract class HydePage implements CompilableContract, PageSchema
 {
     use ConstructsPageSchemas;
-    use Internal\HandlesPageMatter;
 
     public static string $sourceDirectory;
     public static string $outputDirectory;
@@ -212,6 +211,40 @@ abstract class HydePage implements CompilableContract, PageSchema
     public function getBladeView(): string
     {
         return static::$template;
+    }
+
+    // Section: Front Matter
+
+    /**
+     * Get a value from the computed page data, or fallback to the page's front matter, then to the default value.
+     *
+     * @return \Hyde\Framework\Models\FrontMatter|mixed
+     */
+    public function get(string $key = null, mixed $default = null): mixed
+    {
+        if ($key !== null && property_exists($this, $key) && isset($this->$key)) {
+            return $this->$key;
+        }
+
+        return $this->matter($key, $default);
+    }
+
+    /**
+     * Get the front matter object, or a value from within.
+     *
+     * @return \Hyde\Framework\Models\FrontMatter|mixed
+     */
+    public function matter(string $key = null, mixed $default = null): mixed
+    {
+        return $this->matter->get($key, $default);
+    }
+
+    /**
+     * See if a value exists in the computed page data or the front matter.
+     */
+    public function has(string $key): bool
+    {
+        return ! blank($this->get($key));
     }
 
     // Section: Accessors
