091a55571a92e61cadc7ac09230bb7070fd6c2f7
Caen De Silva <caen@desilva.se>
2022-09-11 14:02:36 +0200

    Rename private helper method

--- packages/framework/src/Concerns/Internal/HasNavigationData.php
+++ packages/framework/src/Concerns/Internal/HasNavigationData.php
@@ -49,8 +49,8 @@ trait HasNavigationData
             return $this->matter('navigation.label');
         }
 
-        if (isset(static::getDefaultNavigationLabelsConfig()[$this->routeKey])) {
-            return static::getDefaultNavigationLabelsConfig()[$this->routeKey];
+        if (isset(static::getNavigationLabelConfig()[$this->routeKey])) {
+            return static::getNavigationLabelConfig()[$this->routeKey];
         }
 
         return $this->matter('title') ?? $this->title;
@@ -90,7 +90,7 @@ trait HasNavigationData
         return 999;
     }
 
-    private static function getDefaultNavigationLabelsConfig(): array
+    private static function getNavigationLabelConfig(): array
     {
         return array_merge([
             'index' => 'Home',
