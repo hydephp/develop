1f4af3b03b1b5c1f0c60fe5637a6e44efadd4b61
Caen De Silva <caen@desilva.se>
2022-09-11 19:49:01 +0200

    Make properties nullable as front matter is optional

--- packages/framework/src/Models/NavigationData.php
+++ packages/framework/src/Models/NavigationData.php
@@ -6,8 +6,8 @@ use Hyde\Framework\Contracts\FrontMatter\Support\NavigationSchema;
 
 class NavigationData implements NavigationSchema
 {
-    public string $label;
-    public string $group;
-    public bool $hidden;
-    public int $priority;
+    public ?string $label;
+    public ?string $group;
+    public ?bool $hidden;
+    public ?int $priority;
 }
