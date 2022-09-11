34dcc1cd84568dd8c3063bb36a6b73a4beb4a4c2
Caen De Silva <caen@desilva.se>
2022-09-11 20:14:40 +0200

    Create NavigationDataTest.php

---
+++ packages/framework/tests/Models/NavigationDataTest.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Hyde\Framework\Testing\Models;
+
+use Hyde\Framework\Models\NavigationData;
+use Hyde\Testing\TestCase;
+
+/**
+ * @covers \Hyde\Framework\Models\NavigationData
+ */
+class NavigationDataTest extends TestCase
+{
+    public function test__construct()
+    {
+        $navigationData = new NavigationData('label', 'group', true, 1);
+
+        $this->assertEquals('label', $navigationData->label);
+        $this->assertEquals('group', $navigationData->group);
+        $this->assertEquals(true, $navigationData->hidden);
+        $this->assertEquals(1, $navigationData->priority);
+    }
+
+    public function testMake()
+    {
+        $navigationData = NavigationData::make([
+            'label' => 'label',
+            'group' => 'group',
+            'hidden' => true,
+            'priority' => 1,
+        ]);
+
+        $this->assertEquals($navigationData, new NavigationData('label', 'group', true, 1));
+    }
+
+    public function testLabel()
+    {
+        $this->assertSame('label', (new NavigationData(label: 'label'))->label());
+    }
+
+    public function testGroup()
+    {
+        $this->assertSame('group', (new NavigationData(group: 'group'))->group());
+    }
+
+    public function testHidden()
+    {
+        $this->assertSame(true, (new NavigationData(hidden: true))->hidden());
+        $this->assertSame(false, (new NavigationData(hidden: false))->hidden());
+    }
+
+    public function testPriority()
+    {
+        $this->assertSame(1, (new NavigationData(priority: 1))->priority());
+    }
+}
