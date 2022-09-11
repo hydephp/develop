e2b8b73e61c65847003b90ff14ec00cec2b6f552
Caen De Silva <caen@desilva.se>
2022-09-11 19:49:18 +0200

    Set default value to null

--- packages/framework/src/Models/NavigationData.php
+++ packages/framework/src/Models/NavigationData.php
@@ -6,8 +6,8 @@ use Hyde\Framework\Contracts\FrontMatter\Support\NavigationSchema;
 
 class NavigationData implements NavigationSchema
 {
-    public ?string $label;
-    public ?string $group;
-    public ?bool $hidden;
-    public ?int $priority;
+    public ?string $label = null;
+    public ?string $group = null;
+    public ?bool $hidden = null;
+    public ?int $priority = null;
 }
