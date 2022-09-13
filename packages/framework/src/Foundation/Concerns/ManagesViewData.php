0c6fa6eeba588ceccc274320e108af19f5cc570e
Caen De Silva <caen@desilva.se>
2022-09-13 18:41:48 +0200

    Create ManagesViewData.php

---
+++ packages/framework/src/Foundation/Concerns/ManagesViewData.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Hyde\Framework\Foundation\Concerns;
+
+/**
+ * @internal Single-use trait for the HydeKernel class.
+ *
+ * @see \Hyde\Framework\HydeKernel
+ */
+trait ManagesViewData
+{
+    //
+}
