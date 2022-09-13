a00e83909940f9b5a1e52611d023b7ea49762d5c
Caen De Silva <caen@desilva.se>
2022-09-13 18:42:30 +0200

    Move view data helpers to ManagesViewData trait

--- packages/framework/src/Foundation/Concerns/ManagesViewData.php
+++ packages/framework/src/Foundation/Concerns/ManagesViewData.php
@@ -2,6 +2,9 @@
 
 namespace Hyde\Framework\Foundation\Concerns;
 
+use Hyde\Framework\Models\Route;
+use Illuminate\Support\Facades\View;
+
 /**
  * @internal Single-use trait for the HydeKernel class.
  *
@@ -9,5 +12,13 @@ namespace Hyde\Framework\Foundation\Concerns;
  */
 trait ManagesViewData
 {
-    //
+    public function currentPage(): ?string
+    {
+        return View::shared('currentPage');
+    }
+
+    public function currentRoute(): ?Route
+    {
+        return View::shared('currentRoute');
+    }
 }
