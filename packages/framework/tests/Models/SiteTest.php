6cf35b9b0302b91614880913d233fd953a0293d2
Caen De Silva <caen@desilva.se>
2022-09-13 20:44:30 +0200

    Reset the config for tests

--- packages/framework/tests/Models/SiteTest.php
+++ packages/framework/tests/Models/SiteTest.php
@@ -60,6 +60,8 @@ class SiteTest extends TestCase
 
     public function test_site_metadata_adds_config_defined_metadata()
     {
+        $this->resetConfig();
+
         config(['hyde.meta' => [
             Meta::link('foo', 'bar'),
             Meta::name('foo', 'bar'),
@@ -78,6 +80,8 @@ class SiteTest extends TestCase
 
     public function test_site_metadata_automatically_adds_sitemap_when_enabled()
     {
+        $this->resetConfig();
+
         config(['site.url' => 'foo']);
         config(['site.generate_sitemap' => true]);
 
@@ -86,6 +90,8 @@ class SiteTest extends TestCase
 
     public function test_site_metadata_sitemap_uses_configured_site_url()
     {
+        $this->resetConfig();
+
         config(['site.url' => 'bar']);
         config(['site.generate_sitemap' => true]);
 
@@ -94,6 +100,8 @@ class SiteTest extends TestCase
 
     public function test_site_metadata_automatically_adds_rss_feed_when_enabled()
     {
+        $this->resetConfig();
+
         config(['site.url' => 'foo']);
         config(['hyde.generate_rss_feed' => true]);
         $this->file('_posts/foo.md');
@@ -103,6 +111,8 @@ class SiteTest extends TestCase
 
     public function test_site_metadata_rss_feed_uses_configured_site_url()
     {
+        $this->resetConfig();
+
         config(['site.url' => 'bar']);
         config(['hyde.generate_rss_feed' => true]);
         $this->file('_posts/foo.md');
@@ -112,6 +122,8 @@ class SiteTest extends TestCase
 
     public function test_site_metadata_rss_feed_uses_configured_site_name()
     {
+        $this->resetConfig();
+
         config(['site.url' => 'foo']);
         config(['site.name' => 'Site']);
         config(['hyde.generate_rss_feed' => true]);
@@ -122,6 +134,8 @@ class SiteTest extends TestCase
 
     public function test_site_metadata_rss_feed_uses_configured_rss_file_name()
     {
+        $this->resetConfig();
+
         config(['site.url' => 'foo']);
         config(['hyde.rss_filename' => 'posts.rss']);
         config(['hyde.generate_rss_feed' => true]);
@@ -131,5 +145,13 @@ class SiteTest extends TestCase
             '<link rel="alternate" href="foo/posts.rss" type="application/rss+xml" title="HydePHP RSS Feed">',
             Site::metadata()->render()
         );
+    }
+
+    protected function resetConfig(): void
+    {
+        config(['site.url' => null]);
+        config(['hyde.meta' => []]);
+        config(['hyde.generate_rss_feed' => false]);
+        config(['site.generate_sitemap' => false]);
     }
 }
