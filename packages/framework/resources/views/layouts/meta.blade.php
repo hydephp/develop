30e4d311258ccd5b2b46d5e67f5285b062476246
Caen De Silva <caen@desilva.se>
2022-09-13 20:49:26 +0200

    Use the class alias

--- packages/framework/resources/views/layouts/meta.blade.php
+++ packages/framework/resources/views/layouts/meta.blade.php
@@ -2,7 +2,7 @@
 {!! $page->renderPageMetadata() !!}
 
 {{-- Render the global and config defined meta tags --}}
-{!! \Hyde\Framework\Models\Site::metadata()->render() !!}
+{!! Site::metadata()->render() !!}
 
 {{-- Add any extra tags to include in the <head> section --}}
 @stack('meta')
