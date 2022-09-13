509e899a7e5f5a81e30194d609cc180bdbd23be4
Caen De Silva <caen@desilva.se>
2022-09-13 20:41:13 +0200

    Move global metadata tests to SiteTest.php

--- packages/framework/tests/Models/SiteTest.php
+++ packages/framework/tests/Models/SiteTest.php
@@ -2,6 +2,8 @@
 
 namespace Hyde\Framework\Testing\Models;
 
+use Hyde\Framework\Helpers\Meta;
+use Hyde\Framework\Models\Pages\MarkdownPage;
 use Hyde\Framework\Models\Site;
 use Hyde\Testing\TestCase;
 
@@ -54,5 +56,94 @@ class SiteTest extends TestCase
 
         config(['site.language' => 'foo']);
         $this->assertSame('foo', Site::language());
+    }
+
+    public function test_site_metadata_adds_config_defined_metadata()
+    {
+        config(['hyde.meta' => [
+            Meta::link('foo', 'bar'),
+            Meta::name('foo', 'bar'),
+            Meta::property('foo', 'bar'),
+            'foo' => 'bar',
+            'baz',
+        ]]);
+
+        $page = new MarkdownPage();
+        $this->assertEquals([
+            'links:foo' => Meta::link('foo', 'bar'),
+            'metadata:foo' => Meta::name('foo', 'bar'),
+            'properties:foo' => Meta::property('foo', 'bar'),
+            'generics:0' => 'bar',
+            'generics:1' => 'baz',
+        ], $page->metadata->get());
+    }
+
+    public function test_site_metadata_automatically_adds_sitemap_when_enabled()
+    {
+        config(['site.url' => 'foo']);
+        config(['site.generate_sitemap' => true]);
+
+        $page = new MarkdownPage();
+
+        $this->assertEquals('<link rel="sitemap" href="foo/sitemap.xml" type="application/xml" title="Sitemap">', $page->metadata->render());
+    }
+
+    public function test_site_metadata_sitemap_uses_configured_site_url()
+    {
+        config(['site.url' => 'bar']);
+        config(['site.generate_sitemap' => true]);
+
+        $page = new MarkdownPage();
+
+        $this->assertEquals('<link rel="sitemap" href="bar/sitemap.xml" type="application/xml" title="Sitemap">', $page->metadata->render());
+    }
+
+    public function test_site_metadata_automatically_adds_rss_feed_when_enabled()
+    {
+        config(['site.url' => 'foo']);
+        config(['hyde.generate_rss_feed' => true]);
+        $this->file('_posts/foo.md');
+
+        $page = new MarkdownPage();
+
+        $this->assertEquals('<link rel="alternate" href="foo/feed.xml" type="application/rss+xml" title="HydePHP RSS Feed">', $page->metadata->render());
+    }
+
+    public function test_site_metadata_rss_feed_uses_configured_site_url()
+    {
+        config(['site.url' => 'bar']);
+        config(['hyde.generate_rss_feed' => true]);
+        $this->file('_posts/foo.md');
+
+        $page = new MarkdownPage();
+
+        $this->assertEquals('<link rel="alternate" href="bar/feed.xml" type="application/rss+xml" title="HydePHP RSS Feed">', $page->metadata->render());
+    }
+
+    public function test_site_metadata_rss_feed_uses_configured_site_name()
+    {
+        config(['site.url' => 'foo']);
+        config(['site.name' => 'Site']);
+        config(['hyde.generate_rss_feed' => true]);
+        $this->file('_posts/foo.md');
+
+        $page = new MarkdownPage();
+
+        $this->assertEquals('<link rel="alternate" href="foo/feed.xml" type="application/rss+xml" title="Site RSS Feed">', $page->metadata->render());
+    }
+
+    public function test_site_metadata_rss_feed_uses_configured_rss_file_name()
+    {
+        config(['site.url' => 'foo']);
+        config(['hyde.rss_filename' => 'posts.rss']);
+        config(['hyde.generate_rss_feed' => true]);
+        $this->file('_posts/foo.md');
+
+        $page = new MarkdownPage();
+
+        $this->assertStringContainsString(
+            '<link rel="alternate" href="foo/posts.rss" type="application/rss+xml" title="HydePHP RSS Feed">',
+            $page->metadata->render()
+        );
     }
 }
