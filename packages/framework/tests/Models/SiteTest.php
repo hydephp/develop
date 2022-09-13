bafeff0c1ab758e8880ba9f0427f370f15f1585d
Caen De Silva <caen@desilva.se>
2022-09-13 19:48:58 +0200

    Create SiteTest.php

---
+++ packages/framework/tests/Models/SiteTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Hyde\Framework\Testing\Models;
+
+use Hyde\Framework\Models\Site;
+use Hyde\Testing\TestCase;
+
+/**
+ * @covers \Hyde\Framework\Models\Site
+ */
+class SiteTest extends TestCase
+{
+    //
+}
