bc2ed6ec781860a2acec542d21ac214ce1c86e2f
Caen De Silva <caen@desilva.se>
2022-09-14 17:53:46 +0200

    Add the path property

--- packages/framework/src/Models/Pages/Redirect.php
+++ packages/framework/src/Models/Pages/Redirect.php
@@ -8,18 +8,20 @@ class Redirect extends HydePage
 {
     public static string $outputDirectory = '';
 
+    public string $path;
     public string $destination;
 
-    public function __construct(string $destination)
+    public function __construct(string $path, string $destination)
     {
         parent::__construct();
 
+        $this->path = $path;
         $this->destination = $destination;
     }
 
-    public static function make(string $destination): static
+    public static function make(string $path, string $destination): static
     {
-        return new static($destination);
+        return new static($path, $destination);
     }
 
     public function compile(): string
