75bc20b45a5a4e52688779402aef29ed1cf525de
Caen De Silva <caen@desilva.se>
2022-09-14 15:29:10 +0200

    Revert "Throw exception if the page type is not supported"
    This reverts commit 11216b973ac87e8991a7c67f526bf7b2909a19ed.

--- packages/framework/src/Actions/SourceFileParser.php
+++ packages/framework/src/Actions/SourceFileParser.php
@@ -5,7 +5,6 @@ namespace Hyde\Framework\Actions;
 use Hyde\Framework\Concerns\BaseMarkdownPage;
 use Hyde\Framework\Concerns\HydePage;
 use Hyde\Framework\Concerns\ValidatesExistence;
-use Hyde\Framework\Exceptions\UnsupportedPageTypeException;
 use Hyde\Framework\Models\Pages\BladePage;
 use Hyde\Framework\Modules\Markdown\MarkdownFileParser;
 
@@ -70,7 +69,5 @@ class SourceFileParser
         if (is_subclass_of($pageClass, BaseMarkdownPage::class)) {
             return $this->parseMarkdownPage($pageClass);
         }
-
-        throw new UnsupportedPageTypeException($pageClass);
     }
 }
