fa395991a677dca7384588d659e94fa669c9d89d
Caen De Silva <caen@desilva.se>
2022-09-14 14:12:35 +0200

    Create HtmlPageTest.php

---
+++ packages/framework/tests/Feature/HtmlPageTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Hyde\Framework\Testing\Feature;
+
+use Hyde\Framework\Models\Pages\HtmlPage;
+use Hyde\Testing\TestCase;
+
+/**
+ * @covers \Hyde\Framework\Models\Pages\HtmlPage
+ */
+class HtmlPageTest extends TestCase
+{
+    //
+}
