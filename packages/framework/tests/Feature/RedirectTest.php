198429bf6b913680d02510a8a1e0d786aa4cc990
Caen De Silva <caen@desilva.se>
2022-09-14 18:25:29 +0200

    Test redirect class

--- packages/framework/tests/Feature/RedirectTest.php
+++ packages/framework/tests/Feature/RedirectTest.php
@@ -2,6 +2,8 @@
 
 namespace Hyde\Framework\Testing\Feature;
 
+use Hyde\Framework\Hyde;
+use Hyde\Framework\Models\Pages\Redirect;
 use Hyde\Testing\TestCase;
 
 /**
@@ -9,5 +11,22 @@ use Hyde\Testing\TestCase;
  */
 class RedirectTest extends TestCase
 {
-    //
+    public function test_can_create_a_redirect()
+    {
+        $redirect = Redirect::make('foo', 'bar');
+
+        $this->assertInstanceOf(Redirect::class, $redirect);
+        $this->assertEquals(new Redirect('foo', 'bar'), $redirect);
+        $this->assertSame('foo', $redirect->path);
+        $this->assertSame('bar', $redirect->destination);
+
+        $this->assertSame('', $redirect->render());
+
+        $redirect->store();
+
+        $this->assertFileExists(Hyde::path('_site/foo.html'));
+        $this->assertSame('', file_get_contents(Hyde::path('_site/foo.html')));
+
+        unlink(Hyde::path('_site/foo.html'));
+    }
 }
