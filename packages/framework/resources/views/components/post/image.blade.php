98c0b261a26731623cea9db2fba9bd205b106f45
Caen De Silva <caen@desilva.se>
2022-09-14 19:25:56 +0200

    Render the image styles

--- packages/framework/resources/views/components/post/image.blade.php
+++ packages/framework/resources/views/components/post/image.blade.php
@@ -1,5 +1,5 @@
 <figure aria-label="Cover image" itemprop="image" itemscope itemtype="http://schema.org/ImageObject" role="doc-cover">
-    <img src="{{ $page->image->getLink() }}" alt="{{ $page->image->description ?? '' }}" title="{{ $page->image->title ?? '' }}" itemprop="image" class="mb-0"> 
+    <img src="{{ $page->image->getLink() }}" alt="{{ $page->image->description ?? '' }}" title="{{ $page->image->title ?? '' }}" itemprop="image" class="mb-0" {{ $image->hasStyles() ? $image->getStyles() : '' }}>
     <figcaption aria-label="Image caption" itemprop="caption"> 
         {!! $page->image->getFluentAttribution() !!} 
     </figcaption> 
