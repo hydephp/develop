2c2a5dcc81fa7019c70166055d3e5368667eebcb
Caen De Silva <caen@desilva.se>
2022-09-14 14:11:40 +0200

    Configure source path information

--- packages/framework/src/Models/Pages/HtmlPage.php
+++ packages/framework/src/Models/Pages/HtmlPage.php
@@ -6,6 +6,9 @@ use Hyde\Framework\Concerns\HydePage;
 
 class HtmlPage extends HydePage
 {
+    public static string $sourceDirectory = '_pages';
+    public static string $fileExtension = '.html';
+
     public function compile(): string
     {
         return file_get_contents($this->getSourcePath());
