0e36b988d16d0c7c2f76ff827e84864807d5a59c
Caen De Silva <caen@desilva.se>
2022-09-14 14:09:53 +0200

    Implement compile method

--- packages/framework/src/Models/Pages/HtmlPage.php
+++ packages/framework/src/Models/Pages/HtmlPage.php
@@ -6,5 +6,8 @@ use Hyde\Framework\Concerns\HydePage;
 
 class HtmlPage extends HydePage
 {
-    //
+    public function compile(): string
+    {
+        return file_get_contents($this->getSourcePath());
+    }
 }
