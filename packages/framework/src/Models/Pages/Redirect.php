ed8238ef9cc9b96ad0cceeb76442a436c5c936a4
Caen De Silva <caen@desilva.se>
2022-09-14 18:18:59 +0200

    Revert: Extend HydePage

--- packages/framework/src/Models/Pages/Redirect.php
+++ packages/framework/src/Models/Pages/Redirect.php
@@ -2,21 +2,13 @@
 
 namespace Hyde\Framework\Models\Pages;
 
-use Hyde\Framework\Concerns\HydePage;
-
-class Redirect extends HydePage
+class Redirect
 {
-    public static string $outputDirectory = '';
-    public static string $sourceDirectory = '';
-    public static string $fileExtension = '';
-
     public string $path;
     public string $destination;
 
     public function __construct(string $path, string $destination)
     {
-        parent::__construct($path);
-
         $this->path = $path;
         $this->destination = $destination;
     }
@@ -24,10 +16,5 @@ class Redirect extends HydePage
     public static function make(string $path, string $destination): static
     {
         return new static($path, $destination);
-    }
-
-    public function compile(): string
-    {
-        return '';
     }
 }
