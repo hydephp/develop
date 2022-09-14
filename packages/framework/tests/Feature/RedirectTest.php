fcea5bd8cac396acbfd95b39d4ebd0e67ca178c9
Caen De Silva <caen@desilva.se>
2022-09-14 18:22:27 +0200

    Create RedirectTest.php

---
+++ packages/framework/tests/Feature/RedirectTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Hyde\Framework\Testing\Feature;
+
+use Hyde\Testing\TestCase;
+
+/**
+ * @covers \Hyde\Framework\Models\Pages\Redirect
+ */
+class RedirectTest extends TestCase
+{
+    //
+}
