351c8cf8fd0a880f98be59f1a5d414d6a514b5eb
Caen De Silva <caen@desilva.se>
2022-09-14 12:11:26 +0200

    Update HydeRouteListCommandTest.php

--- packages/framework/tests/Feature/Commands/HydeRouteListCommandTest.php
+++ packages/framework/tests/Feature/Commands/HydeRouteListCommandTest.php
@@ -9,5 +9,22 @@ use Hyde\Testing\TestCase;
  */
 class HydeRouteListCommandTest extends TestCase
 {
-    //
+    public function testRouteListCommand()
+    {
+        $this->artisan('route:list')
+            ->expectsTable(['Route Key', 'Source File', 'Output File', 'Page Type'], [
+                [
+                    '404',
+                    '_pages/404.blade.php',
+                    '404.html',
+                    'BladePage',
+                ],
+                [
+                    'index',
+                    '_pages/index.blade.php',
+                    'index.html',
+                    'BladePage',
+                ]
+            ])->assertExitCode(0);
+    }
 }
