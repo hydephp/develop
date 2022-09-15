0b57a6e8a0d3c46e8d6786056b22b57d89b62a8e
Caen De Silva <caen@desilva.se>
2022-09-15 14:35:18 +0200

    Create SetsUpMarkdownConverter.php

---
+++ packages/framework/src/Concerns/Internal/SetsUpMarkdownConverter.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Hyde\Framework\Concerns\Internal;
+
+/**
+ * @internal Sets up the Markdown converter for the Markdown service.
+ * @see \Hyde\Framework\Services\MarkdownService
+ */
+class SetsUpMarkdownConverter
+{
+    //
+}
