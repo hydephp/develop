9c43366502e05bf9d83480f5beb47659bad00d83
Caen De Silva <caen@desilva.se>
2022-10-25 18:28:18 +0200

    Add PHPDocs

--- packages/framework/src/Modules/Metadata/MetadataElementContract.php
+++ packages/framework/src/Modules/Metadata/MetadataElementContract.php
@@ -2,9 +2,20 @@
 
 namespace Hyde\Framework\Modules\Metadata;
 
+/**
+ * The methods the object representations for Metadata element classes must implement.
+ */
 interface MetadataElementContract extends \Stringable
 {
+    /**
+     * @return string The HTML representation of the element.
+     * @example <meta name="description" content="This is a description.">
+     */
     public function __toString(): string;
 
+    /**
+     * @return string The unique key for the element.
+     * @example description
+     */
     public function uniqueKey(): string;
 }
