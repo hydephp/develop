8619535bc1f0f5aedbba5b6e7137578d735603fd
Caen De Silva <caen@desilva.se>
2022-10-25 15:16:37 +0200

    Add class scaffolding to match similar actions

--- packages/framework/src/Actions/Constructors/FindsNavigationDataForPage.php
+++ packages/framework/src/Actions/Constructors/FindsNavigationDataForPage.php
@@ -2,7 +2,29 @@
 
 namespace Hyde\Framework\Actions\Constructors;
 
-class FindsNavigationDataForPage
+use Hyde\Framework\Concerns\HydePage;
+use Hyde\Framework\Models\Navigation\NavigationData;
+
+/**
+ * Helper for HydePages to discover data used for navigation menus and the documentation sidebar.
+ *
+ * @internal
+ *
+ * @see \Hyde\Framework\Testing\Feature\PageModelConstructorsTest
+ * @see \Hyde\Framework\Concerns\HydePage
+ */
+final class FindsNavigationDataForPage
 {
-    //
+    public static function run(HydePage $page): NavigationData
+    {
+        return (new self($page))->findNavigationForPage();
+    }
+
+    protected function __construct(protected HydePage $page)
+    {
+    }
+
+    private function findNavigationForPage()
+    {
+    }
 }
