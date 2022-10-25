acda7ce1b7f20c069a7822cee552397e22056b19
Caen De Silva <caen@desilva.se>
2022-10-25 19:47:30 +0200

    Create GlobalMetadataBagTest.php

---
+++ packages/framework/tests/Feature/GlobalMetadataBagTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Hyde\Framework\Testing\Feature;
+
+use Hyde\Testing\TestCase;
+
+/**
+ * @covers \Hyde\Framework\Modules\Metadata\Models\GlobalMetadataBag
+ */
+class GlobalMetadataBagTest extends TestCase
+{
+    //
+}
