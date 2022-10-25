47164f52cc2c5b0af02caf75f843a36cfe67a5f7
Caen De Silva <caen@desilva.se>
2022-10-25 15:20:35 +0200

    Remove internal trait GeneratesNavigationData

--- packages/framework/src/Concerns/Internal/GeneratesNavigationData.php
+++ packages/framework/src/Concerns/Internal/GeneratesNavigationData.php
@@ -7,13 +7,3 @@ use Hyde\Framework\Models\Navigation\NavigationData;
 use Hyde\Framework\Models\Pages\DocumentationPage;
 use Hyde\Framework\Models\Pages\MarkdownPost;
 use Illuminate\Support\Str;
-
-/**
- * @deprecated Trait for HydePages to manage data used for navigation menus and the documentation sidebar.
- *
- * @see \Hyde\Framework\Concerns\HydePage
- */
-trait GeneratesNavigationData
-{
-    //
-}
