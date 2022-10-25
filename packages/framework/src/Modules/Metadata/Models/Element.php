c64aeda54243a900e00c913b55c5da3364e9ad3b
Caen De Silva <caen@desilva.se>
2022-10-25 14:20:32 +0200

    Add abstract uniqueKey method stub

--- packages/framework/src/Modules/Metadata/Models/Element.php
+++ packages/framework/src/Modules/Metadata/Models/Element.php
@@ -5,4 +5,6 @@ namespace Hyde\Framework\Modules\Metadata\Models;
 abstract class Element implements \Stringable
 {
     abstract public function __toString(): string;
+
+    abstract public function uniqueKey(): string;
 }
