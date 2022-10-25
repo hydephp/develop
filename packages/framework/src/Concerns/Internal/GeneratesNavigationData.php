99155106b7414b61fedbc772fa17030b7451619e
Caen De Silva <caen@desilva.se>
2022-10-25 15:20:39 +0200

    Delete GeneratesNavigationData.php

--- packages/framework/src/Concerns/Internal/GeneratesNavigationData.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Hyde\Framework\Concerns\Internal;
-
-use Hyde\Framework\Contracts\FrontMatter\DocumentationPageSchema;
-use Hyde\Framework\Models\Navigation\NavigationData;
-use Hyde\Framework\Models\Pages\DocumentationPage;
-use Hyde\Framework\Models\Pages\MarkdownPost;
-use Illuminate\Support\Str;
