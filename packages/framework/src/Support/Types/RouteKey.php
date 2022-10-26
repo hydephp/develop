6dc74f7ad0e6a46d3e1f65a03387e42ad74fd8f6
Caen De Silva <caen@desilva.se>
2022-10-26 21:03:25 +0200

    Create RouteKey.php

---
+++ packages/framework/src/Support/Types/RouteKey.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hyde\Support\Types;
+
+use Stringable;
+
+final class RouteKey implements Stringable
+{
+    private string $key;
+
+    public function __construct(string $key) {
+        $this->key = $key;
+    }
+
+    public function __toString()
+    {
+        return $this->key;
+    }
+}
