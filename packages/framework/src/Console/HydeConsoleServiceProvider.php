4f42e655ce10bda031e1eade5d4a5118663e449a
Caen De Silva <caen@desilva.se>
2022-10-26 20:49:53 +0200

    Move registerHydeConsoleCommands to the new provider

--- packages/framework/src/Console/HydeConsoleServiceProvider.php
+++ packages/framework/src/Console/HydeConsoleServiceProvider.php
@@ -8,5 +8,39 @@ use Illuminate\Support\ServiceProvider;
 
 class HydeConsoleServiceProvider extends ServiceProvider
 {
-    //
+    /**
+     * Register any console services.
+     *
+     * @return void
+     */
+    public function register(): void
+    {
+        $this->registerHydeConsoleCommands();
+    }
+
+    /**
+     * Register the HydeCLI console commands.
+     */
+    protected function registerHydeConsoleCommands(): void
+    {
+        $this->commands([
+            \Hyde\Console\Commands\PublishHomepageCommand::class,
+            \Hyde\Console\Commands\UpdateConfigsCommand::class,
+            \Hyde\Console\Commands\PublishViewsCommand::class,
+            \Hyde\Console\Commands\RebuildStaticSiteCommand::class,
+            \Hyde\Console\Commands\BuildSiteCommand::class,
+            \Hyde\Console\Commands\BuildSitemapCommand::class,
+            \Hyde\Console\Commands\BuildRssFeedCommand::class,
+            \Hyde\Console\Commands\BuildSearchCommand::class,
+            \Hyde\Console\Commands\RouteListCommand::class,
+            \Hyde\Console\Commands\MakePostCommand::class,
+            \Hyde\Console\Commands\MakePageCommand::class,
+            \Hyde\Console\Commands\ValidateCommand::class,
+            \Hyde\Console\Commands\DebugCommand::class,
+            \Hyde\Console\Commands\ServeCommand::class,
+
+            \Hyde\Console\Commands\PackageDiscoverCommand::class,
+        ]);
+    }
+
 }
--- packages/framework/src/Framework/HydeServiceProvider.php
+++ packages/framework/src/Framework/HydeServiceProvider.php
@@ -60,7 +60,6 @@ class HydeServiceProvider extends ServiceProvider
 
         $this->discoverBladeViewsIn(BladePage::sourceDirectory());
 
-        $this->registerHydeConsoleCommands();
         $this->registerModuleServiceProviders();
     }
 
@@ -103,31 +102,6 @@ class HydeServiceProvider extends ServiceProvider
         if (YamlConfigurationService::hasFile()) {
             YamlConfigurationService::boot();
         }
-    }
-
-    /**
-     * Register the HydeCLI console commands.
-     */
-    protected function registerHydeConsoleCommands(): void
-    {
-        $this->commands([
-            \Hyde\Console\Commands\PublishHomepageCommand::class,
-            \Hyde\Console\Commands\UpdateConfigsCommand::class,
-            \Hyde\Console\Commands\PublishViewsCommand::class,
-            \Hyde\Console\Commands\RebuildStaticSiteCommand::class,
-            \Hyde\Console\Commands\BuildSiteCommand::class,
-            \Hyde\Console\Commands\BuildSitemapCommand::class,
-            \Hyde\Console\Commands\BuildRssFeedCommand::class,
-            \Hyde\Console\Commands\BuildSearchCommand::class,
-            \Hyde\Console\Commands\RouteListCommand::class,
-            \Hyde\Console\Commands\MakePostCommand::class,
-            \Hyde\Console\Commands\MakePageCommand::class,
-            \Hyde\Console\Commands\ValidateCommand::class,
-            \Hyde\Console\Commands\DebugCommand::class,
-            \Hyde\Console\Commands\ServeCommand::class,
-
-            \Hyde\Console\Commands\PackageDiscoverCommand::class,
-        ]);
     }
 
     /**
