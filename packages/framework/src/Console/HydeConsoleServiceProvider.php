3bca1500fa92fb5197da01c808f709d4fccb3efc
Caen De Silva <caen@desilva.se>
2022-10-26 20:50:50 +0200

    Remove unnecessary use statement

--- packages/framework/src/Console/HydeConsoleServiceProvider.php
+++ packages/framework/src/Console/HydeConsoleServiceProvider.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace Hyde\Console;
 
 use Illuminate\Support\ServiceProvider;
-use Hyde\Console\Commands;
 
 class HydeConsoleServiceProvider extends ServiceProvider
 {
