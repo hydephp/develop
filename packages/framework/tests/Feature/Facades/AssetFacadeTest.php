cab5797119f53b83f7d687b5c7dad37543d947ac
Caen De Silva <caen@desilva.se>
2022-10-27 18:27:11 +0200

    Update AssetFacadeTest.php

--- packages/framework/tests/Feature/Facades/AssetFacadeTest.php
+++ packages/framework/tests/Feature/Facades/AssetFacadeTest.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Hyde\Framework\Testing\Unit;
+namespace Hyde\Framework\Testing\Feature\Facades;
 
 use Hyde\Facades\Asset;
 use Hyde\Framework\Services\AssetService;
