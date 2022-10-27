e60706bbac8b4867b0ab0569cb3a48909452c92b
Caen De Silva <caen@desilva.se>
2022-10-27 17:20:03 +0200

    Implement the type class

--- packages/framework/src/Support/Models/RouteKey.php
+++ packages/framework/src/Support/Models/RouteKey.php
@@ -6,5 +6,20 @@ namespace Hyde\Support\Models;
 
 class RouteKey
 {
-    //
+    protected readonly string $key;
+
+    public function __construct(string $key)
+    {
+        $this->key = $key;
+    }
+
+    public function __toString(): string
+    {
+        return $this->key;
+    }
+
+    public function get(): string
+    {
+        return $this->key;
+    }
 }
--- packages/framework/tests/Feature/RouteKeyTest.php
+++ packages/framework/tests/Feature/RouteKeyTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Hyde\Framework\Testing\Feature;
 
+use Hyde\Support\Models\RouteKey;
 use Hyde\Testing\TestCase;
 
 /**
@@ -11,5 +12,18 @@ use Hyde\Testing\TestCase;
  */
 class RouteKeyTest extends TestCase
 {
-    //
+    public function test__construct()
+    {
+        $this->assertInstanceOf(RouteKey::class, new RouteKey('test'));
+    }
+
+    public function test__toString()
+    {
+        $this->assertSame('foo', (string) new RouteKey('foo'));
+    }
+
+    public function testGet()
+    {
+        $this->assertSame('foo', (new RouteKey('foo'))->get());
+    }
 }
