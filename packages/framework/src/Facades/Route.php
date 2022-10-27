5de846a370aded47b502e8d62e9f22a1f0df0d02
Caen De Silva <caen@desilva.se>
2022-10-27 18:34:09 +0200

    Add method tags

--- packages/framework/src/Facades/Route.php
+++ packages/framework/src/Facades/Route.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Hyde\Facades;
 
+use Hyde\Foundation\RouteCollection;
 use Hyde\Routing\Router;
 use Illuminate\Support\Facades\Facade;
 
@@ -11,6 +12,16 @@ use Illuminate\Support\Facades\Facade;
  * Provides an easy way to access the Hyde pseudo-router.
  *
  * @see \Hyde\Routing\Router
+ *
+ * @method static ?Route get(string $routeKey)
+ * @method static ?Route getFromKey(string $routeKey)
+ * @method static ?Route getFromSource(string $sourceFilePath)
+ * @method static ?Route getFromModel(HydePage $page)
+ * @method static Route getOrFail(string $routeKey)
+ * @method static RouteCollection all()
+ * @method static bool exists(string $routeKey)
+ * @method static ?Route current()
+ * @method static ?Route home()
  */
 class Route extends Facade
 {
