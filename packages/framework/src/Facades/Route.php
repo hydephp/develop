7fe0cea2fe6b33cabf8838836108b87cbf3f7b3b
Caen De Silva <caen@desilva.se>
2022-10-27 18:43:06 +0200

    Sort method annotations

--- packages/framework/src/Facades/Route.php
+++ packages/framework/src/Facades/Route.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Hyde\Facades;
 
 use Hyde\Foundation\RouteCollection;
+use Hyde\Framework\Concerns\HydePage;
 use Hyde\Routing\Router;
 use Illuminate\Support\Facades\Facade;
 
@@ -13,15 +14,15 @@ use Illuminate\Support\Facades\Facade;
  *
  * @see \Hyde\Routing\Router
  *
+ * @method static Route|null home()
+ * @method static Route|null current()
  * @method static Route|null get(string $routeKey)
  * @method static Route|null getFromKey(string $routeKey)
  * @method static Route|null getFromSource(string $sourceFilePath)
  * @method static Route|null getFromModel(HydePage $page)
  * @method static Route getOrFail(string $routeKey)
- * @method static RouteCollection all()
  * @method static bool exists(string $routeKey)
- * @method static Route|null current()
- * @method static Route|null home()
+ * @method static RouteCollection all()
  */
 class Route extends Facade
 {
