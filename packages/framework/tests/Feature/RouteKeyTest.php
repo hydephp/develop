59f7406850b98b1186221df4ab2c34759b8b87bd
Caen De Silva <caen@desilva.se>
2022-10-27 17:18:10 +0200

    Create RouteKeyTest.php

---
+++ packages/framework/tests/Feature/RouteKeyTest.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hyde\Framework\Testing\Feature;
+
+use Hyde\Testing\TestCase;
+
+/**
+ * @covers \Hyde\Support\Models\RouteKey
+ */
+class RouteKeyTest extends TestCase
+{
+    //
+}
