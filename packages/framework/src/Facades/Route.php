efdc0ad11e4b89108e07886b7495c53dd2750528
Caen De Silva <caen@desilva.se>
2022-10-27 18:31:00 +0200

    Add PHPDoc

--- packages/framework/src/Facades/Route.php
+++ packages/framework/src/Facades/Route.php
@@ -7,6 +7,11 @@ namespace Hyde\Facades;
 use Hyde\Routing\Router;
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * Provides an easy way to access the Hyde pseudo-router.
+ *
+ * @see \Hyde\Routing\Router
+ */
 class Route extends Facade
 {
     protected static function getFacadeAccessor(): string
