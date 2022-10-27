2adc0d480715bb96c5f711b7a2d2afcb7d688052
Caen De Silva <caen@desilva.se>
2022-10-27 21:19:24 +0200

    Move Redirect class to Support\Models namespace

--- packages/framework/tests/Feature/RedirectTest.php
+++ packages/framework/tests/Feature/RedirectTest.php
@@ -4,12 +4,12 @@ declare(strict_types=1);
 
 namespace Hyde\Framework\Testing\Feature;
 
-use Hyde\Helpers\Redirect;
 use Hyde\Hyde;
+use Hyde\Support\Models\Redirect;
 use Hyde\Testing\TestCase;
 
 /**
- * @covers \Hyde\Helpers\Redirect
+ * @covers \Hyde\Support\Models\Redirect
  */
 class RedirectTest extends TestCase
 {
