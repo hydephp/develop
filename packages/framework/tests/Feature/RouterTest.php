4d8e37c15062e360109134640efac60cb80064c9
Caen De Silva <caen@desilva.se>
2022-10-27 18:38:46 +0200

    Create RouterTest.php

---
+++ packages/framework/tests/Feature/RouterTest.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hyde\Framework\Testing\Feature;
+
+use Hyde\Routing\Router;
+use Hyde\Testing\TestCase;
+
+/**
+ * @covers \Hyde\Routing\Router
+ */
+class RouterTest extends TestCase
+{
+    //
+}
