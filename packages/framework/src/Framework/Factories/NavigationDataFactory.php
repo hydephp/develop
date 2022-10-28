36e1246b729c5cb2d76877b60950d70afcbfa720
Caen De Silva <caen@desilva.se>
2022-10-28 19:10:43 +0200

    Split comma-separated values into multiple lines

--- packages/framework/src/Framework/Factories/NavigationDataFactory.php
+++ packages/framework/src/Framework/Factories/NavigationDataFactory.php
@@ -36,7 +36,11 @@ class NavigationDataFactory extends Concerns\PageDataFactory implements Navigati
     protected readonly ?bool $hidden;
     protected readonly ?int $priority;
 
-    public function __construct(private readonly FrontMatter $matter, private readonly string $identifier, private readonly string $routeKey, private readonly string $title, private readonly string $pageClass)
+    public function __construct(private readonly FrontMatter $matter,
+                                private readonly string      $identifier,
+                                private readonly string      $routeKey,
+                                private readonly string      $title,
+                                private readonly string      $pageClass)
     {
         $this->label = $this->makeLabel();
         $this->group = $this->makeGroup();
