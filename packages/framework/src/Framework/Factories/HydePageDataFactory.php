515445dff5cfa941256c6ce074cea8247c79aeb5
Caen De Silva <caen@desilva.se>
2022-10-28 17:44:08 +0200

    Implements PageSchema

--- packages/framework/src/Framework/Factories/HydePageDataFactory.php
+++ packages/framework/src/Framework/Factories/HydePageDataFactory.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 
 namespace Hyde\Framework\Factories;
 
-class HydePageDataFactory extends Concerns\PageDataFactory
+use Hyde\Markdown\Contracts\FrontMatter\PageSchema;
+
+class HydePageDataFactory extends Concerns\PageDataFactory implements PageSchema
 {
     public function toArray(): array
     {
