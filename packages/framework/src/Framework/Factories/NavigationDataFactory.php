f4e676052090bd76aca02ad84b995a6e0243b911
Caen De Silva <caen@desilva.se>
2022-10-28 18:21:12 +0200

    Create NavigationDataFactory.php

---
+++ packages/framework/src/Framework/Factories/NavigationDataFactory.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hyde\Framework\Factories;
+
+class NavigationDataFactory extends Concerns\PageDataFactory
+{
+    public function toArray(): array
+    {
+        // TODO: Implement toArray() method.
+    }
+}
