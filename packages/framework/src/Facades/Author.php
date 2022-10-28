7f163f4bbbfb9941531ecaabd0ac2901a39d5489
Caen De Silva <caen@desilva.se>
2022-10-28 12:32:15 +0200

    Add PHPDoc

--- packages/framework/src/Facades/Author.php
+++ packages/framework/src/Facades/Author.php
@@ -6,6 +6,11 @@ namespace Hyde\Facades;
 
 use Hyde\Framework\Features\Blogging\Models\PostAuthor;
 
+/**
+ * Allows you to easily add pre-defined authors for your blog posts.
+ * 
+ * @see \Hyde\Framework\Features\Blogging\Models\PostAuthor
+ */
 class Author
 {
     public static function create(string $username, ?string $name = null, ?string $website = null): PostAuthor
