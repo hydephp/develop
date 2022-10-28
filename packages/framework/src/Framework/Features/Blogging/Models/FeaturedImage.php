07e8aa5ece5a06469bedb23cc62cbd4c138bc46e
Caen De Silva <caen@desilva.se>
2022-10-28 22:07:05 +0200

    Method getPath must not return basename

--- packages/framework/src/Framework/Features/Blogging/Models/FeaturedImage.php
+++ packages/framework/src/Framework/Features/Blogging/Models/FeaturedImage.php
@@ -255,7 +255,7 @@ class FeaturedImage implements FeaturedImageSchema, Stringable
     protected function getPath(): ?string
     {
         if (isset($this->path)) {
-            return basename($this->path);
+            return ($this->path);
         }
 
         return null;
