5fe7475ac567c14682e78141ed952bbf0544676a
Caen De Silva <caen@desilva.se>
2022-10-28 17:47:31 +0200

    Add the schema constant

--- packages/framework/src/Framework/Factories/HydePageDataFactory.php
+++ packages/framework/src/Framework/Factories/HydePageDataFactory.php
@@ -8,6 +8,11 @@ use Hyde\Markdown\Contracts\FrontMatter\PageSchema;
 
 class HydePageDataFactory extends Concerns\PageDataFactory implements PageSchema
 {
+    /**
+     * The front matter properties supported by this factory.
+     */
+    public const SCHEMA = PageSchema::PAGE_SCHEMA;
+
     public function toArray(): array
     {
         // TODO: Implement toArray() method.
