6971fc1fe6bb76777b224126f5153fd586fdd824
Caen De Silva <caen@desilva.se>
2022-10-28 20:40:51 +0200

    Add class PHPDoc

--- packages/framework/src/Framework/Features/Session/Session.php
+++ packages/framework/src/Framework/Features/Session/Session.php
@@ -4,6 +4,11 @@ declare(strict_types=1);
 
 namespace Hyde\Framework\Features\Session;
 
+/**
+ * Adds a simple session handler, arguably most useful to defer messages, for example,
+ * to asynchronously add warnings to be used at a later point in the request lifecycle.
+ * It's bound into the service container as a singleton and is not persisted.
+ */
 class Session
 {
     protected array $warnings = [];
