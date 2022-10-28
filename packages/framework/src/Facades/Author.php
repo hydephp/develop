1e4681eac260f9aef89c1ed7d8c0cc8c85157ace
Caen De Silva <caen@desilva.se>
2022-10-28 12:31:30 +0200

    Add create facade method

--- packages/framework/src/Facades/Author.php
+++ packages/framework/src/Facades/Author.php
@@ -4,7 +4,12 @@ declare(strict_types=1);
 
 namespace Hyde\Facades;
 
+use Hyde\Framework\Features\Blogging\Models\PostAuthor;
+
 class Author
 {
-    //
+    public static function create(string $username, ?string $name = null, ?string $website = null): PostAuthor
+    {
+        return new PostAuthor($username, ['name' => $name, 'website' => $website]);
+    }
 }
--- packages/framework/tests/Unit/Facades/AuthorTest.php
+++ packages/framework/tests/Unit/Facades/AuthorTest.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Hyde\Framework\Testing\Unit\Facades;
 
 use Hyde\Facades\Author;
+use Hyde\Framework\Features\Blogging\Models\PostAuthor;
 use Hyde\Testing\TestCase;
 
 /**
@@ -12,5 +13,14 @@ use Hyde\Testing\TestCase;
  */
 class AuthorTest extends TestCase
 {
-    //
+    public function testCreate()
+    {
+        $author = Author::create('john_doe', 'John Doe', 'https://johndoe.com');
+
+        $this->assertSame('john_doe', $author->username);
+        $this->assertSame('John Doe', $author->name);
+        $this->assertSame('https://johndoe.com', $author->website);
+        
+        $this->assertEquals(PostAuthor::create('foo'), Author::create('foo'));
+    }
 }
