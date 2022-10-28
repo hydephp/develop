b7261ecccd7edf5ed45e7f026ee4c6777fdb0cfe
Caen De Silva <caen@desilva.se>
2022-10-28 20:31:41 +0200

    Create SessionTest.php

---
+++ packages/framework/tests/Framework/Feature/SessionTest.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hyde\Framework\Testing\Framework\Feature;
+
+use Hyde\Framework\Features\Session\Session;
+use Hyde\Testing\TestCase;
+
+/**
+ * @covers \Hyde\Framework\Features\Session\Session
+ */
+class SessionTest extends TestCase
+{
+    //
+}
