57682ff8e3adfcb836eb8d63040216df6728e082
Caen De Silva <caen@desilva.se>
2022-10-28 12:04:45 +0200

    Add PHPDoc

--- packages/framework/src/Framework/Factories/Concerns/PageFactory.php
+++ packages/framework/src/Framework/Factories/Concerns/PageFactory.php
@@ -6,6 +6,11 @@ namespace Hyde\Framework\Factories\Concerns;
 
 use Illuminate\Contracts\Support\Arrayable;
 
+/**
+ * Streamlines the data construction specific to a page.
+ *
+ * Simply pass along the data the class needs to run, then access the data using the toArray() method.
+ */
 abstract class PageFactory implements Arrayable
 {
     abstract public function toArray(): array;
