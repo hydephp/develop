4f2eecf3605b60581a251058ebdc8da4def4ab63
Caen De Silva <caen@desilva.se>
2022-10-28 12:28:13 +0200

    Create AuthorTest.php

---
+++ packages/framework/tests/Unit/Facades/AuthorTest.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hyde\Framework\Testing\Unit\Facades;
+
+use Hyde\Facades\Author;
+use Hyde\Testing\TestCase;
+
+/**
+ * @covers \Hyde\Facades\Author
+ */
+class AuthorTest extends TestCase
+{
+    //
+}
