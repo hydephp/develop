70ca382373ab62092818edf6a188733b08203e83
Caen De Silva <caen@desilva.se>
2022-10-28 17:27:22 +0200

    Convert braces {!! !!} -> {{ }}

--- packages/framework/resources/views/components/post/image.blade.php
+++ packages/framework/resources/views/components/post/image.blade.php
@@ -5,7 +5,7 @@ $image = $page->image;
 <figure aria-label="Cover image" itemprop="image" itemscope itemtype="http://schema.org/ImageObject" role="doc-cover">
     <img src="{{ $image->getLink() }}" alt="{{ $image->description ?? '' }}" title="{{ $image->title ?? '' }}" itemprop="image" class="mb-0">
     <figcaption aria-label="Image caption" itemprop="caption"> 
-        {!! $image->getFluentAttribution() !!}
+        {{ $image->getFluentAttribution() }}
     </figcaption> 
     @foreach ($image->getMetadataArray() as $name => $value)
 	<meta itemprop="{{ $name }}" content="{{ $value }}"> 
