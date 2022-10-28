d628315d2183fa044c2f4d7584502441e373574f
Caen De Silva <caen@desilva.se>
2022-10-28 13:01:33 +0200

    Document the facade method

--- packages/framework/src/Facades/Author.php
+++ packages/framework/src/Facades/Author.php
@@ -13,6 +13,18 @@ use Hyde\Framework\Features\Blogging\Models\PostAuthor;
  */
 class Author
 {
+    /**
+     * Construct a new Post Author. For Hyde to discover this author,
+     * you must call this method from your hyde.php config file.
+     *
+     * @see https://hydephp.com/docs/master/customization.html#authors
+     *
+     * @param string $username  The username of the author. This is the key used to find authors in the config.
+     * @param string|null $name The optional display name of the author, leave blank to use the username.
+     * @param string|null $website The author's optional website URL. Website, Twitter, etc.
+     *
+     * @return \Hyde\Framework\Features\Blogging\Models\PostAuthor
+     */
     public static function create(string $username, ?string $name = null, ?string $website = null): PostAuthor
     {
         return new PostAuthor($username, $name, $website);
