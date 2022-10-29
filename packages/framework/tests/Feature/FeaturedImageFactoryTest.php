d2a58563c5b5a7db81e12599f71a090fdd2efc38
Caen De Silva <caen@desilva.se>
2022-10-29 14:36:08 +0200

    Create FeaturedImageFactoryTest.php

---
+++ packages/framework/tests/Feature/FeaturedImageFactoryTest.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hyde\Framework\Testing\Feature;
+
+use Hyde\Framework\Factories\FeaturedImageFactory;
+use Hyde\Testing\TestCase;
+
+/**
+ * @covers \Hyde\Framework\Factories\FeaturedImageFactory
+ */
+class FeaturedImageFactoryTest extends TestCase
+{
+    //
+}
