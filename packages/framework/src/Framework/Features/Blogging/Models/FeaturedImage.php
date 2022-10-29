268a09785c4a8cc01680bfa44d87ca36e0e75f04
Caen De Silva <caen@desilva.se>
2022-10-29 11:38:45 +0200

    Add PHPDoc

--- packages/framework/src/Framework/Features/Blogging/Models/FeaturedImage.php
+++ packages/framework/src/Framework/Features/Blogging/Models/FeaturedImage.php
@@ -8,5 +8,9 @@ use Stringable;
 
 abstract class FeaturedImage implements Stringable
 {
+    /**
+     * Get the source of the image, must be usable within the src attribute of an image tag.
+     * @return string The image's url or path
+     */
     abstract public function getSource(): string;
 }
