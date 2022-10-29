546d266e9dfb150a56ba6d2e7af5dd0fb5a2d6de
Caen De Silva <caen@desilva.se>
2022-10-29 13:06:20 +0200

    Add method stubs

--- packages/framework/src/Framework/Features/Blogging/Models/RemoteFeaturedImage.php
+++ packages/framework/src/Framework/Features/Blogging/Models/RemoteFeaturedImage.php
@@ -6,5 +6,13 @@ namespace Hyde\Framework\Features\Blogging\Models;
 
 class RemoteFeaturedImage extends FeaturedImage
 {
-    //
+    public function getSource(): string
+    {
+        // TODO: Implement getSource() method.
+    }
+
+    public function getContentLength(): int
+    {
+        // TODO: Implement getContentLength() method.
+    }
 }
