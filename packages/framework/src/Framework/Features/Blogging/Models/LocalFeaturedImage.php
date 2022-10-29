337614ba43c017d2154d2a25cfb0bcd21f0c51a0
Caen De Silva <caen@desilva.se>
2022-10-29 12:32:47 +0200

    Remove unused import

--- packages/framework/src/Framework/Features/Blogging/Models/LocalFeaturedImage.php
+++ packages/framework/src/Framework/Features/Blogging/Models/LocalFeaturedImage.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace Hyde\Framework\Features\Blogging\Models;
 
-use function func_get_args;
-
 class LocalFeaturedImage extends FeaturedImage
 {
     protected readonly string $source;
