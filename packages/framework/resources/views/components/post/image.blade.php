d9f003ade5b1e82a99b2f2f2b59612bf42f0f968
Caen De Silva <caen@desilva.se>
2022-10-29 09:24:39 +0200

    Add page type annotation

--- packages/framework/resources/views/components/post/image.blade.php
+++ packages/framework/resources/views/components/post/image.blade.php
@@ -1,6 +1,7 @@
 @php
-/** @var \Hyde\Framework\Features\Blogging\Models\FeaturedImage $image */
-$image = $page->image;
+    /** @var \Hyde\Pages\MarkdownPost $page  */
+    /** @var \Hyde\Framework\Features\Blogging\Models\FeaturedImage $image  */
+    $image = $page->image;
 @endphp
 <figure aria-label="Cover image" itemprop="image" itemscope itemtype="http://schema.org/ImageObject" role="doc-cover">
     <img src="{{ $image->getLink() }}" alt="{{ $image->description ?? '' }}" title="{{ $image->title ?? '' }}" itemprop="image" class="mb-0">
