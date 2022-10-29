950c444ddc6f34be2d6c2ce0a3e02d5e75358257
Caen De Silva <caen@desilva.se>
2022-10-29 09:53:17 +0200

    Extend test case class

--- packages/framework/tests/Unit/Views/FeaturedImageViewTest.php
+++ packages/framework/tests/Unit/Views/FeaturedImageViewTest.php
@@ -4,10 +4,12 @@ declare(strict_types=1);
 
 namespace Hyde\Framework\Testing\Unit\Views;
 
+use Hyde\Testing\TestCase;
+
 /**
  * @see resources/views/components/post/image.blade.php
  */
-class FeaturedImageViewTest
+class FeaturedImageViewTest extends TestCase
 {
     //
 }
