dd8e07b737d18c60308488267bc8730a0c873a9d
Caen De Silva <caen@desilva.se>
2022-10-29 12:22:41 +0200

    Add constructor

--- packages/framework/src/Framework/Features/Blogging/Models/LocalFeaturedImage.php
+++ packages/framework/src/Framework/Features/Blogging/Models/LocalFeaturedImage.php
@@ -7,4 +7,10 @@ namespace Hyde\Framework\Features\Blogging\Models;
 class LocalFeaturedImage extends FeaturedImage
 {
     protected readonly string $source;
+
+    public function __construct(string $source, ?string $altText, ?string $titleText, ?string $authorName, ?string $authorUrl, ?string $copyrightText, ?string $licenseName, ?string $licenseUrl)
+    {
+        parent::__construct($altText, $titleText, $authorName, $authorUrl, $copyrightText, $licenseName, $licenseUrl);
+        $this->source = $source;
+    }
 }
