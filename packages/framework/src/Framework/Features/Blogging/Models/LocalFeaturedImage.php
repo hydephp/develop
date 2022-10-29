b8f66e2b56cfdf298ec8799eff247e7ace049d20
Caen De Silva <caen@desilva.se>
2022-10-29 12:26:50 +0200

    Shorten parent constructor

--- packages/framework/src/Framework/Features/Blogging/Models/LocalFeaturedImage.php
+++ packages/framework/src/Framework/Features/Blogging/Models/LocalFeaturedImage.php
@@ -4,13 +4,15 @@ declare(strict_types=1);
 
 namespace Hyde\Framework\Features\Blogging\Models;
 
+use function func_get_args;
+
 class LocalFeaturedImage extends FeaturedImage
 {
     protected readonly string $source;
 
     public function __construct(string $source, ?string $altText, ?string $titleText, ?string $authorName, ?string $authorUrl, ?string $copyrightText, ?string $licenseName, ?string $licenseUrl)
     {
-        parent::__construct($source, $altText, $titleText, $authorName, $authorUrl, $copyrightText, $licenseName, $licenseUrl);
+        parent::__construct(...func_get_args());
         $this->source = $source;
     }
 
