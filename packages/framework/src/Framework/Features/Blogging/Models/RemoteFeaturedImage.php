042870fcb35948d75788c52b5c06a1380df8f072
Caen De Silva <caen@desilva.se>
2022-10-29 13:07:10 +0200

    Add setSource method

--- packages/framework/src/Framework/Features/Blogging/Models/RemoteFeaturedImage.php
+++ packages/framework/src/Framework/Features/Blogging/Models/RemoteFeaturedImage.php
@@ -4,8 +4,22 @@ declare(strict_types=1);
 
 namespace Hyde\Framework\Features\Blogging\Models;
 
+use InvalidArgumentException;
+
 class RemoteFeaturedImage extends FeaturedImage
 {
+    protected readonly string $source;
+
+    protected function setSource(string $source): void
+    {
+        if (! str_starts_with($source, 'http')) {
+            // Throwing an exception here ensures we have a super predictable state.
+            throw new InvalidArgumentException('RemoteFeaturedImage source must start with http/https');
+        }
+
+        $this->source = $source;
+    }
+
     public function getSource(): string
     {
         // TODO: Implement getSource() method.
