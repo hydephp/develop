f13014186c78168a97ba01cb53fdd2805aa1d699
Caen De Silva <caen@desilva.se>
2022-10-29 14:28:33 +0200

    Add required method stub

--- packages/framework/src/Framework/Factories/FeaturedImageFactory.php
+++ packages/framework/src/Framework/Factories/FeaturedImageFactory.php
@@ -9,4 +9,9 @@ use Hyde\Markdown\Contracts\FrontMatter\SubSchemas\FeaturedImageSchema;
 class FeaturedImageFactory extends Concerns\PageDataFactory implements FeaturedImageSchema
 {
     public const SCHEMA = FeaturedImageSchema::FEATURED_IMAGE_SCHEMA;
+
+    public function toArray(): array
+    {
+        // TODO: Implement toArray() method.
+    }
 }
