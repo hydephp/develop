a229802755b3d4f222a14b1ebc25a9fdf96d525c
Caen De Silva <caen@desilva.se>
2022-10-30 14:32:34 +0100

    Test the config

--- packages/framework/tests/Feature/Commands/ServeCommandTest.php
+++ packages/framework/tests/Feature/Commands/ServeCommandTest.php
@@ -38,4 +38,22 @@ class ServeCommandTest extends TestCase
             ->expectsOutput('Starting the HydeRC server... Press Ctrl+C to stop')
             ->assertExitCode(0);
     }
+
+    public function test_hyde_serve_command_with_port_defined_in_config()
+    {
+        $this->app['config']->set('hyde.server.port', 8081);
+
+        $this->artisan('serve')
+            ->expectsOutput('Starting the HydeRC server... Press Ctrl+C to stop')
+            ->assertExitCode(0);
+    }
+
+    public function test_hyde_serve_command_with_port_defined_in_config_and_port_option()
+    {
+        $this->app['config']->set('hyde.server.port', 8081);
+
+        $this->artisan('serve --port=8082')
+            ->expectsOutput('Starting the HydeRC server... Press Ctrl+C to stop')
+            ->assertExitCode(0);
+    }
 }
