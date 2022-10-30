1002fb7b5d2bdef950544dbc5329bcb9e3b142b5
Caen De Silva <caen@desilva.se>
2022-10-30 14:52:13 +0100

    Revert "Convert concatenation and string interpolation to a 'sprintf()' call"
    This reverts commit f8666006db25a36207e4213e1a3e9b4de7f4d738.

--- packages/framework/src/Console/Commands/ServeCommand.php
+++ packages/framework/src/Console/Commands/ServeCommand.php
@@ -31,11 +31,7 @@ class ServeCommand extends Command
         $host = $this->option('host');
         $port = $this->getPort();
 
-        $this->runServerCommand(sprintf('php -S %s:%d %s',
-            $host,
-            $port,
-            Hyde::path('vendor/hyde/realtime-compiler/bin/server.php')
-        ));
+        $this->runServerCommand("php -S $host:$port ".Hyde::path('vendor/hyde/realtime-compiler/bin/server.php'));
 
         return Command::SUCCESS;
     }
