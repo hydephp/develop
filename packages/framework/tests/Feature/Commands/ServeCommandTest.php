98445d29c9d85fa96a1870340cdc26b49113532b
Caen De Silva <caen@desilva.se>
2022-10-30 14:31:09 +0100

    Add extra tests

--- packages/framework/tests/Feature/Commands/ServeCommandTest.php
+++ packages/framework/tests/Feature/Commands/ServeCommandTest.php
@@ -17,4 +17,25 @@ class ServeCommandTest extends TestCase
             ->expectsOutput('Starting the HydeRC server... Press Ctrl+C to stop')
             ->assertExitCode(0);
     }
+
+    public function test_hyde_serve_command_with_port_option()
+    {
+        $this->artisan('serve --port=8081')
+            ->expectsOutput('Starting the HydeRC server... Press Ctrl+C to stop')
+            ->assertExitCode(0);
+    }
+
+    public function test_hyde_serve_command_with_host_option()
+    {
+        $this->artisan('serve --host=foo')
+            ->expectsOutput('Starting the HydeRC server... Press Ctrl+C to stop')
+            ->assertExitCode(0);
+    }
+
+    public function test_hyde_serve_command_with_port_and_host_option()
+    {
+        $this->artisan('serve --port=8081 --host=foo')
+            ->expectsOutput('Starting the HydeRC server... Press Ctrl+C to stop')
+            ->assertExitCode(0);
+    }
 }
