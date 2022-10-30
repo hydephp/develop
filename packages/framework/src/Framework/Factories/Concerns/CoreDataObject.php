54d8a1514985e98d333b02fc415355bd4ebb84b1
Caen De Silva <caen@desilva.se>
2022-10-30 19:07:59 +0100

    Implements Arrayable

--- packages/framework/src/Framework/Factories/Concerns/CoreDataObject.php
+++ packages/framework/src/Framework/Factories/Concerns/CoreDataObject.php
@@ -4,14 +4,20 @@ declare(strict_types=1);
 
 namespace Hyde\Framework\Factories\Concerns;
 
+use Hyde\Support\Concerns\JsonSerializesArrayable;
+use Illuminate\Contracts\Support\Arrayable;
+use JsonSerializable;
+
 /**
  * Experimental class to contain the core data for a page being constructed.
  *
  * It should contain immutable data known at the very start of construction.
  * The data should contain everything needed to identify the unique page.
  */
-final class CoreDataObject
+final class CoreDataObject implements Arrayable, JsonSerializable
 {
+    use JsonSerializesArrayable;
+
     public function __construct(
         public readonly string $pageClass,
         public readonly string $identifier,
@@ -20,5 +26,16 @@ final class CoreDataObject
         public readonly string $routeKey,
     ) {
         //
+    }
+
+    public function toArray(): array
+    {
+        return [
+            'pageClass' => $this->pageClass,
+            'identifier' => $this->identifier,
+            'sourcePath' => $this->sourcePath,
+            'outputPath' => $this->outputPath,
+            'routeKey' => $this->routeKey,
+        ];
     }
 }
