049a5e7cfa683bb1505de6bfafbd8efc86a6e99a
Caen De Silva <caen@desilva.se>
2022-10-30 14:36:19 +0100

    Fix config mock calls

--- packages/framework/tests/Feature/Commands/ServeCommandTest.php
+++ packages/framework/tests/Feature/Commands/ServeCommandTest.php
@@ -41,7 +41,7 @@ class ServeCommandTest extends TestCase
 
     public function test_hyde_serve_command_with_port_defined_in_config()
     {
-        $this->app['config']->set('hyde.server.port', 8081);
+        config(['hyde.server.port' => 8081]);
 
         $this->artisan('serve')
             ->expectsOutput('Starting the HydeRC server... Press Ctrl+C to stop')
@@ -50,7 +50,7 @@ class ServeCommandTest extends TestCase
 
     public function test_hyde_serve_command_with_port_defined_in_config_and_port_option()
     {
-        $this->app['config']->set('hyde.server.port', 8081);
+        config(['hyde.server.port' => 8081]);
 
         $this->artisan('serve --port=8082')
             ->expectsOutput('Starting the HydeRC server... Press Ctrl+C to stop')
@@ -59,7 +59,7 @@ class ServeCommandTest extends TestCase
 
     public function test_hyde_serve_command_with_port_missing_in_config_and_port_option()
     {
-        $this->app['config']->set('hyde.server.port');
+        config(['hyde.server.port' => null]);
 
         $this->artisan('serve')
             ->expectsOutput('Starting the HydeRC server... Press Ctrl+C to stop')
