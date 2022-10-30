0b789f82a6c19e2efb3d243187820feaede9698f
Caen De Silva <caen@desilva.se>
2022-10-30 19:08:14 +0100

    Adds front matter and markdown data

--- packages/framework/src/Framework/Factories/Concerns/CoreDataObject.php
+++ packages/framework/src/Framework/Factories/Concerns/CoreDataObject.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Hyde\Framework\Factories\Concerns;
 
+use Hyde\Markdown\Models\FrontMatter;
+use Hyde\Markdown\Models\Markdown;
 use Hyde\Support\Concerns\JsonSerializesArrayable;
 use Illuminate\Contracts\Support\Arrayable;
 use JsonSerializable;
@@ -12,13 +14,16 @@ use JsonSerializable;
  * Experimental class to contain the core data for a page being constructed.
  *
  * It should contain immutable data known at the very start of construction.
- * The data should contain everything needed to identify the unique page.
+ * In addition to the front matter and markdown, the data should contain
+ * everything needed to identify the unique page being constructed.
  */
 final class CoreDataObject implements Arrayable, JsonSerializable
 {
     use JsonSerializesArrayable;
 
     public function __construct(
+        public readonly FrontMatter $matter,
+        public readonly Markdown|false $markdown,
         public readonly string $pageClass,
         public readonly string $identifier,
         public readonly string $sourcePath,
