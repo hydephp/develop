4bac365f72cdc9d8e1adfb3586ff164990f2a8b2
Caen De Silva <caen@desilva.se>
2022-11-03 17:41:36 +0100

    Expect spaces not tabs

--- packages/framework/tests/Feature/Actions/CreatesNewPageSourceFileTest.php
+++ packages/framework/tests/Feature/Actions/CreatesNewPageSourceFileTest.php
@@ -108,7 +108,7 @@ class CreatesNewPageSourceFileTest extends TestCase
 @php($title = "Test Page")
 
 <main class="mx-auto max-w-7xl py-16 px-8">
-	<h1 class="text-center text-3xl font-bold">Test Page</h1>
+    <h1 class="text-center text-3xl font-bold">Test Page</h1>
 </main>
 
 @endsection
