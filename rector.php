f35d3bb060f055f0adac69a9fe8d73bb32063f44
Caen De Silva <caen@desilva.se>
2022-11-04 20:53:27 +0100

    Create rector.php

---
+++ rector.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use Rector\Laravel\Set\LaravelSetList;
+
+return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->paths([__DIR__ . '/packages/framework/src']);
+    $rectorConfig->sets([
+                            LaravelSetList::LARAVEL_90,
+                            \Rector\Set\ValueObject\SetList::PHP_80,
+                            \Rector\Set\ValueObject\SetList::PHP_81,
+                        ]);
+    $rectorConfig->rule(Rector\Php80\Rector\Class_\ClassPropertyAssignToConstructorPromotionRector::class);
+};
+
