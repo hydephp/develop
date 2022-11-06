1e94b846e6119a1561143cb23b0529ed40a15168
Caen De Silva <caen@desilva.se>
2022-11-06 19:48:21 +0100

    Add escape method to base class

--- packages/framework/src/Framework/Features/XmlGenerators/BaseXmlGenerator.php
+++ packages/framework/src/Framework/Features/XmlGenerators/BaseXmlGenerator.php
@@ -4,7 +4,12 @@ declare(strict_types=1);
 
 namespace Hyde\Framework\Features\XmlGenerators;
 
+use function htmlspecialchars;
+
 abstract class BaseXmlGenerator implements XmlGeneratorContract
 {
-    //
+    protected function escape(string $string): string
+    {
+        return htmlspecialchars($string, ENT_XML1 | ENT_COMPAT, 'UTF-8');
+    }
 }
