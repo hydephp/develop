a3534e235cdc29f0d841f34a19ce37548c5e0921
Caen De Silva <caen@desilva.se>
2022-11-06 19:47:38 +0100

    Add PHPDoc

--- packages/framework/src/Framework/Features/XmlGenerators/XmlGeneratorContract.php
+++ packages/framework/src/Framework/Features/XmlGenerators/XmlGeneratorContract.php
@@ -6,5 +6,8 @@ namespace Hyde\Framework\Features\XmlGenerators;
 
 interface XmlGeneratorContract
 {
+    /**
+     * Generate a new XML document and get the contents as a string.
+     */
     public static function generate(): string;
 }
