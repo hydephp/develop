0a52da7410a974dfab0e6cc429be8f8b254be32d
Caen De Silva <caen@desilva.se>
2022-11-06 19:35:08 +0100

    Use throw_unless

--- packages/framework/src/Framework/Services/RssFeedService.php
+++ packages/framework/src/Framework/Services/RssFeedService.php
@@ -16,6 +16,7 @@ use Hyde\Hyde;
 use Hyde\Pages\MarkdownPost;
 use Hyde\Support\Helpers\XML;
 use SimpleXMLElement;
+use function throw_unless;
 
 /**
  * @see \Hyde\Framework\Testing\Feature\Services\RssFeedServiceTest
@@ -46,9 +47,9 @@ class RssFeedService
 
     public function __construct()
     {
-        if (! extension_loaded('simplexml')) {
-            throw new Exception('The ext-simplexml extension is not installed, but is required to generate RSS feeds.');
-        }
+        throw_unless(extension_loaded('simplexml'),
+            new Exception('The ext-simplexml extension is not installed, but is required to generate RSS feeds.')
+        );
 
         $this->feed = new SimpleXMLElement('<?xml version="1.0" encoding="UTF-8"?>
             <rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom" xmlns:dc="http://purl.org/dc/elements/1.1/" />');
