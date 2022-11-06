a6edc82aa268e394ee512ac1d68c85865a7fd4ba
Caen De Silva <caen@desilva.se>
2022-11-06 15:15:06 +0100

    Add XML::escape function

--- packages/framework/src/Support/Helpers/XML.php
+++ packages/framework/src/Support/Helpers/XML.php
@@ -4,7 +4,12 @@ declare(strict_types=1);
 
 namespace Hyde\Support\Helpers;
 
+use function htmlspecialchars;
+
 class XML
 {
-    //
+    public static function escape(string $string): string
+    {
+        return htmlspecialchars($string, ENT_XML1 | ENT_COMPAT, 'UTF-8');
+    }
 }
