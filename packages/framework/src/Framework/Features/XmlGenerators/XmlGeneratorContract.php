f81a501dfb8289cdd9398a2953de5095a943a2f7
Caen De Silva <caen@desilva.se>
2022-11-06 20:01:07 +0100

    Add more methods to the contract

--- packages/framework/src/Framework/Features/XmlGenerators/XmlGeneratorContract.php
+++ packages/framework/src/Framework/Features/XmlGenerators/XmlGeneratorContract.php
@@ -10,4 +10,21 @@ interface XmlGeneratorContract
      * Generate a new XML document and get the contents as a string.
      */
     public static function make(): string;
+
+    /**
+     * Create a new XML generator instance.
+     */
+    public function __construct();
+
+    /**
+     * Generate the XML document.
+     *
+     * @return $this
+     */
+    public function generate(): static;
+
+    /**
+     * Get the XML document as a string.
+     */
+    public function getXML(): string;
 }
