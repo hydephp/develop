04f91ece78a289bc3752845f93084392d06cc988
Caen De Silva <caen@desilva.se>
2022-11-08 18:56:32 +0100

    Mark experimental trait as internal

--- packages/framework/src/Pages/Concerns/UsesFlattenedOutputPaths.php
+++ packages/framework/src/Pages/Concerns/UsesFlattenedOutputPaths.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace Hyde\Pages\Concerns;
 
+/**
+ * @internal This trait is currently experimental and should not be relied upon outside of Hyde.
+ */
 trait UsesFlattenedOutputPaths
 {
     //
