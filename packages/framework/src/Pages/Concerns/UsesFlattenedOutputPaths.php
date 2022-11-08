8fecb46f46d82d8ff3ecba99fcc4108f264491b2
Caen De Silva <caen@desilva.se>
2022-11-08 19:02:27 +0100

    Document the trait

--- packages/framework/src/Pages/Concerns/UsesFlattenedOutputPaths.php
+++ packages/framework/src/Pages/Concerns/UsesFlattenedOutputPaths.php
@@ -9,6 +9,12 @@ use function trim;
 
 /**
  * @internal This trait is currently experimental and should not be relied upon outside of Hyde.
+ *
+ * This trait is used to flatten the output path of a page. This is only used for the documentation pages,
+ * where all pages are output to the same directory, but where putting the page in a subdirectory will
+ * create a nested navigation structure in the sidebar.
+ *
+ * @see https://hydephp.com/docs/master/documentation-pages#using-subdirectories
  */
 trait UsesFlattenedOutputPaths
 {
