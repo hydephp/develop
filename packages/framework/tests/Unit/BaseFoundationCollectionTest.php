72d90baa76cb2b1e2899431a8d47173e168d55b7
Caen De Silva <caen@desilva.se>
2022-11-09 13:49:46 +0100

    Create unit tests for the base class

--- packages/framework/tests/Unit/BaseFoundationCollectionTest.php
+++ packages/framework/tests/Unit/BaseFoundationCollectionTest.php
@@ -13,7 +13,23 @@ use Hyde\Testing\TestCase;
  */
 class BaseFoundationCollectionTest extends TestCase
 {
-    //
+    public function test_boot()
+    {
+        $booted = BaseFoundationCollectionTestClass::boot(HydeKernel::getInstance());
+
+        $this->assertInstanceOf(BaseFoundationCollection::class, $booted);
+        $this->assertInstanceOf(BaseFoundationCollectionTestClass::class, $booted);
+
+        $this->assertSame(HydeKernel::getInstance(), $booted->getKernel());
+        $this->assertTrue($booted->isDiscovered());
+    }
+
+    public function test_get_instance()
+    {
+        $booted = BaseFoundationCollectionTestClass::boot(HydeKernel::getInstance());
+
+        $this->assertSame($booted, $booted->getInstance());
+    }
 }
 
 class BaseFoundationCollectionTestClass extends BaseFoundationCollection
