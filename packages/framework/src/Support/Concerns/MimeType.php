effc297738952335ba0b7f45738c449456a5635e
Caen De Silva <caen@desilva.se>
2022-11-09 21:46:33 +0100

    Add cases

--- packages/framework/src/Support/Concerns/MimeType.php
+++ packages/framework/src/Support/Concerns/MimeType.php
@@ -6,5 +6,16 @@ namespace Hyde\Support\Concerns;
 
 enum MimeType
 {
-    //
+    case css  = 'text/css';
+    case gif  = 'image/gif';
+    case html = 'text/html';
+    case ico  = 'image/x-icon';
+    case jpg  = 'image/jpeg';
+    case js   = 'application/javascript';
+    case json = 'application/json';
+    case md   = 'text/markdown';
+    case png  = 'image/png';
+    case svg  = 'image/svg+xml';
+    case txt  = 'text/plain';
+    case xml  = 'application/xml';
 }
