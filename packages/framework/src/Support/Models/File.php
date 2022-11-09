514879d7ae4c0e2b5bd3fb702681860b7b2f309f
Caen De Silva <caen@desilva.se>
2022-11-09 21:30:11 +0100

    Deprecate File.php

--- packages/framework/src/Support/Models/File.php
+++ packages/framework/src/Support/Models/File.php
@@ -10,6 +10,8 @@ use Hyde\Support\Contracts\SerializableContract;
 use Stringable;
 
 /**
+ * @deprecated Will be split into a new class structure.
+ *
  * Filesystem abstraction for a file stored in the project.
  *
  * @see \Hyde\Framework\Testing\Feature\FileTest
