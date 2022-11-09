2778532c8069c07fd968eb308ab9e3b78b967cb7
Caen De Silva <caen@desilva.se>
2022-11-09 21:33:27 +0100

    Create MimeTypeTest.php

---
+++ packages/framework/tests/Unit/Concerns/MimeTypeTest.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hyde\Framework\Testing\Unit\Concerns;
+
+use Hyde\Support\Concerns\MimeType;
+use Hyde\Testing\TestCase;
+
+/**
+ * @covers \Hyde\Support\Concerns\MimeType
+ */
+class MimeTypeTest extends TestCase
+{
+    //
+}
