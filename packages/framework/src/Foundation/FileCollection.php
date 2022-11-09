862a315cc95b9ee701f432a2ab7c3d3de0fb7082
Caen De Silva <caen@desilva.se>
2022-11-09 16:37:20 +0100

    Add class documentation

--- packages/framework/src/Foundation/FileCollection.php
+++ packages/framework/src/Foundation/FileCollection.php
@@ -16,8 +16,13 @@ use Hyde\Pages\MarkdownPost;
 use Hyde\Support\Models\File;
 
 /**
+ * The FileCollection contains all the discovered source and media files.
+ *
+ * This class is stored as a singleton in the HydeKernel.
+ * You would commonly access it via one of the facades:
+ *
  * @see \Hyde\Foundation\Facades\FileCollection
- * @see \Hyde\Framework\Testing\Feature\FileCollectionTest
+ * @see \Hyde\Hyde::files()
  */
 final class FileCollection extends BaseFoundationCollection
 {
