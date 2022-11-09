431701d6bff5dc6aae6d731d389b0954561dc780
Caen De Silva <caen@desilva.se>
2022-11-09 13:41:46 +0100

    Create BaseFoundationCollectionTest.php

---
+++ packages/framework/tests/Unit/BaseFoundationCollectionTest.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hyde\Framework\Testing\Unit;
+
+use Hyde\Foundation\Concerns\BaseFoundationCollection;
+use Hyde\Testing\TestCase;
+
+/**
+ * @covers \Hyde\Foundation\Concerns\BaseFoundationCollection
+ */
+class BaseFoundationCollectionTest extends TestCase
+{
+    //
+}
