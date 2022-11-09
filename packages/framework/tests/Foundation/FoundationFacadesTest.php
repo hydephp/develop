6c05f1e580b5503dd65ef2ae224507fdebde7e7c
Caen De Silva <caen@desilva.se>
2022-11-09 13:32:38 +0100

    Update FoundationFacadesTest.php

--- packages/framework/tests/Foundation/FoundationFacadesTest.php
+++ packages/framework/tests/Foundation/FoundationFacadesTest.php
@@ -4,12 +4,20 @@ declare(strict_types=1);
 
 namespace Hyde\Framework\Testing\Foundation;
 
+use Hyde\Foundation\Facades\FileCollectionFacade;
+use Hyde\Hyde;
 use Hyde\Testing\TestCase;
 
 /**
- * @covers \\${TESTED_NAMESPACE}\FoundationFacadesTest
+ * @covers \Hyde\Foundation\Facades\FileCollectionFacade
  */
 class FoundationFacadesTest extends TestCase
 {
-    //
+    public function test_file_collection_facade()
+    {
+        $this->assertEquals(
+            Hyde::getInstance()->files()->getSourceFiles(),
+            FileCollectionFacade::getSourceFiles()
+        );
+    }
 }
