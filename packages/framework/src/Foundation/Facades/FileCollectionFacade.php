47766c82431d0a549c5da13414876d965c7fa983
Caen De Silva <caen@desilva.se>
2022-11-09 13:53:39 +0100

    Remove unneeded facade accessor method

--- packages/framework/src/Foundation/Facades/FileCollectionFacade.php
+++ packages/framework/src/Foundation/Facades/FileCollectionFacade.php
@@ -13,11 +13,6 @@ use Illuminate\Support\Facades\Facade;
  */
 class FileCollectionFacade extends Facade
 {
-    protected static function getFacadeAccessor(): string
-    {
-        return FileCollection::class;
-    }
-
     public static function getFacadeRoot(): FileCollection
     {
         return HydeKernel::getInstance()->files();
