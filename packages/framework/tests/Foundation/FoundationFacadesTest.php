0fcbfaa532e5ea8ca26832978c5fa9f4e42562e8
Caen De Silva <caen@desilva.se>
2022-11-09 13:31:44 +0100

    Create FoundationFacadesTest.php

---
+++ packages/framework/tests/Foundation/FoundationFacadesTest.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hyde\Framework\Testing\Foundation;
+
+use Hyde\Testing\TestCase;
+
+/**
+ * @covers \\${TESTED_NAMESPACE}\FoundationFacadesTest
+ */
+class FoundationFacadesTest extends TestCase
+{
+    //
+}
