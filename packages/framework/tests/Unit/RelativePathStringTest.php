88dd7166944bc7122af626289652e11f87eec50e
Caen De Silva <caen@desilva.se>
2022-11-11 14:43:06 +0100

    Create RelativePathStringTest.php

---
+++ packages/framework/tests/Unit/RelativePathStringTest.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hyde\Framework\Testing\Unit;
+
+use Hyde\Support\Filesystem\RelativePathString;
+use Hyde\Testing\TestCase;
+
+/**
+ * @covers \Hyde\Support\Filesystem\RelativePathString
+ */
+class RelativePathStringTest extends TestCase
+{
+    //
+}
