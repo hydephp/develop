16858967fc8a3c774783dab46b9f2925616ecea2
Caen De Silva <caen@desilva.se>
2022-11-11 14:35:19 +0100

    Add PHPDocs

--- packages/framework/src/Support/Concerns/MediaFile.php
+++ packages/framework/src/Support/Concerns/MediaFile.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace Hyde\Support\Concerns;
 
+/**
+ * File abstraction for a project media file.
+ */
 class MediaFile extends File
 {
     //
--- packages/framework/src/Support/Concerns/SourceFile.php
+++ packages/framework/src/Support/Concerns/SourceFile.php
@@ -4,6 +4,11 @@ declare(strict_types=1);
 
 namespace Hyde\Support\Concerns;
 
+/**
+ * File abstraction for a project source file.
+ *
+ * @see \Hyde\Foundation\FileCollection
+ */
 class SourceFile extends File
 {
     //
