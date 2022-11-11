ab7a814983765a0befd2410251af3244ee76c458
Caen De Silva <caen@desilva.se>
2022-11-11 21:58:55 +0100

    Add class PHPDoc

--- packages/framework/tests/ConfigurableSourceRootsFeatureTest.php
+++ packages/framework/tests/ConfigurableSourceRootsFeatureTest.php
@@ -6,6 +6,14 @@ namespace Hyde\Framework\Testing;
 
 use Hyde\Testing\TestCase;
 
+/**
+ * Test the overall functionality of the configurable source roots feature.
+ *
+ * Also see these tests which cover specific implementation details:
+ *
+ * @see \Hyde\Framework\Testing\Feature\HydeKernelTest
+ * @see \Hyde\Framework\Testing\Unit\HydeServiceProviderTest
+ */
 class ConfigurableSourceRootsFeatureTest extends TestCase
 {
     public function test_default_config_value_is_empty_string()
