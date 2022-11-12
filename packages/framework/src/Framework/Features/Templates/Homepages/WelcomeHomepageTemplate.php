906efb594b8b8390a27b8d5b38c2d80ba2445c74
Caen De Silva <caen@desilva.se>
2022-11-12 19:14:07 +0100

    Create WelcomeHomepageTemplate.php

---
+++ packages/framework/src/Framework/Features/Templates/Homepages/WelcomeHomepageTemplate.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hyde\Framework\Features\Templates\Homepages;
+
+use Hyde\Framework\Features\Templates\PublishableView;
+
+class WelcomeHomepageTemplate extends PublishableView
+{
+    protected static string $title = 'Welcome';
+    protected static string $desc = 'The default welcome page.';
+    protected static string $path = 'resources/views/homepages/welcome.blade.php';
+}
