cb6ff082dc69af248b6ee07dacb2bb149f0e2021
Caen De Silva <caen@desilva.se>
2022-11-12 19:45:57 +0100

    Fix mismatched properties

--- packages/framework/src/Framework/Features/Templates/Homepages/BlankHomepageTemplate.php
+++ packages/framework/src/Framework/Features/Templates/Homepages/BlankHomepageTemplate.php
@@ -9,6 +9,6 @@ use Hyde\Framework\Features\Templates\PublishableView;
 class BlankHomepageTemplate extends PublishableView
 {
     protected static string $title = 'Blank Starter';
-    protected static string $desc =  'resources/views/homepages/blank.blade.php';
-    protected static string $path =  'A blank Blade template with just the base layout.';
+    protected static string $desc =  'A blank Blade template with just the base layout.';
+    protected static string $path =  'resources/views/homepages/blank.blade.php';
 }
