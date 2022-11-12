7183d518029052543201fa648f32d3f8fd46ae17
Caen De Silva <caen@desilva.se>
2022-11-12 21:06:24 +0100

    Inline internal HomepageTemplate classes into Homepages class

--- packages/framework/src/Framework/Features/Templates/Homepages.php
+++ packages/framework/src/Framework/Features/Templates/Homepages.php
@@ -30,16 +30,34 @@ final class Homepages
 
     public static function blank(): PublishableContract
     {
-        return new Homepages\BlankHomepageTemplate;
+        return new class extends PublishableView
+        {
+            protected static string $title = 'Blank Starter';
+            protected static string $desc = 'A blank Blade template with just the base layout.';
+            protected static string $path = 'resources/views/homepages/blank.blade.php';
+            protected static ?string $outputPath = 'index.blade.php';
+        };
     }
 
     public static function posts(): PublishableContract
     {
-        return new Homepages\PostsFeedHomepageTemplate;
+        return new class extends PublishableView
+        {
+            protected static string $title = 'Posts Feed';
+            protected static string $desc = 'A feed of your latest posts. Perfect for a blog site!';
+            protected static string $path = 'resources/views/homepages/post-feed.blade.php';
+            protected static ?string $outputPath = 'index.blade.php';
+        };
     }
 
     public static function welcome(): PublishableContract
     {
-        return new Homepages\WelcomeHomepageTemplate;
+        return new class extends PublishableView
+        {
+            protected static string $title = 'Welcome';
+            protected static string $desc = 'The default welcome page.';
+            protected static string $path = 'resources/views/homepages/welcome.blade.php';
+            protected static ?string $outputPath = 'index.blade.php';
+        };
     }
 }
