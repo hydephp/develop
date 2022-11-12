374eeafe5f649872db14c8c965e5dd916b7d21e7
Caen De Silva <caen@desilva.se>
2022-11-12 20:48:59 +0100

    Create final class Homepages

--- packages/framework/src/Framework/Features/Templates/Homepages.php
+++ packages/framework/src/Framework/Features/Templates/Homepages.php
@@ -4,7 +4,39 @@ declare(strict_types=1);
 
 namespace Hyde\Framework\Features\Templates;
 
-class Homepages
+final class Homepages
 {
-    //
+    public static function options(): array
+    {
+        return [
+            'blank' => self::blank(),
+            'posts' => self::posts(),
+            'welcome' => self::welcome(),
+        ];
+    }
+
+    public static function get(string $page): ?string
+    {
+        return self::options()[$page] ?? null;
+    }
+
+    public static function exists(string $page): bool
+    {
+        return array_key_exists($page, self::options());
+    }
+
+    public static function blank(): string
+    {
+        return Homepages\BlankHomepageTemplate::class;
+    }
+
+    public static function posts(): string
+    {
+        return Homepages\PostsFeedHomepageTemplate::class;
+    }
+
+    public static function welcome(): string
+    {
+        return Homepages\WelcomeHomepageTemplate::class;
+    }
 }
