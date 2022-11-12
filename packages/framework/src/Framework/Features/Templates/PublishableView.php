79477b0dd5174345940910b3ae564ced51f010a8
Caen De Silva <caen@desilva.se>
2022-11-12 19:02:43 +0100

    Add method stubs

--- packages/framework/src/Framework/Features/Templates/PublishableView.php
+++ packages/framework/src/Framework/Features/Templates/PublishableView.php
@@ -6,5 +6,18 @@ namespace Hyde\Framework\Features\Templates;
 
 abstract class PublishableView implements PublishableContract
 {
-    //
+    public static function publish(): bool
+    {
+        // TODO: Implement publish() method.
+    }
+
+    public static function getTitle(): string
+    {
+        // TODO: Implement getTitle() method.
+    }
+
+    public static function getDescription(): string
+    {
+        // TODO: Implement getDescription() method.
+    }
 }
