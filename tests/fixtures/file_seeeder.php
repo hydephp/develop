167074e460bc6e7dcec8543d2033f5d38dc0e23c
Caen De Silva <caen@desilva.se>
2022-11-12 14:43:35 +0100

    Update file_seeeder.php

--- tests/fixtures/file_seeeder.php
+++ tests/fixtures/file_seeeder.php
@@ -16,14 +16,14 @@ $autoloader = require __DIR__.'/../../vendor/autoload.php';
 $app = require_once __DIR__.'/../../app/bootstrap.php';
 
 foreach (['Index', 'Page 1', 'Page 2', 'Page 3'] as $page) {
-    file_put_contents((new CreatesNewPageSourceFile($page, DocumentationPage::class, true))->outputPath,
-        file_get_contents('markdown.md'),
+    file_put_contents((new CreatesNewPageSourceFile($page, DocumentationPage::class, true))->getOutputPath(),
+        file_get_contents(__DIR__.'/markdown.md'),
         FILE_APPEND
     );
 }
 
-file_put_contents((new CreatesNewPageSourceFile('Markdown Page', MarkdownPage::class, true))->outputPath,
-    file_get_contents('markdown.md'),
+file_put_contents((new CreatesNewPageSourceFile('Markdown Page', MarkdownPage::class, true))->getOutputPath(),
+    file_get_contents(__DIR__.'/markdown.md'),
     FILE_APPEND
 );
 
