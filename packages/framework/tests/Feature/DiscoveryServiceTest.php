8ff2154b71325ac6c3f7e63dc6555dcd8575c853
Caen De Silva <caen@desilva.se>
2022-11-18 20:23:55 +0100

    Update DiscoveryServiceTest.php

--- packages/framework/tests/Feature/DiscoveryServiceTest.php
+++ packages/framework/tests/Feature/DiscoveryServiceTest.php
@@ -198,7 +198,7 @@ class DiscoveryServiceTest extends TestCase
     public function test_get_media_asset_files_discovers_files_very_recursively()
     {
         $path = Hyde::path('_media/foo/bar/img.png');
-        @mkdir(dirname($path), recursive: true);
+        mkdir(dirname($path), recursive: true);
         touch($path);
         $this->assertContains($path, DiscoveryService::getMediaAssetFiles());
         File::deleteDirectory(Hyde::path('_media/foo'));
