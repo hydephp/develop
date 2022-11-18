606802c62fa3994571074804deb68f93e1c673ee
Caen De Silva <caen@desilva.se>
2022-11-18 22:04:29 +0100

    Create base PublicationType object

--- packages/framework/src/Framework/Features/Publications/Models/PublicationType.php
+++ packages/framework/src/Framework/Features/Publications/Models/PublicationType.php
@@ -6,5 +6,19 @@ namespace Hyde\Framework\Features\Publications\Models;
 
 class PublicationType
 {
-    // TODO: Refactor to utilize this class
+    protected string $schemaFile;
+    protected string $directory;
+    protected array $schema;
+
+    public function __construct(string $schemaFile)
+    {
+        $this->schemaFile = $schemaFile;
+        $this->directory = dirname($schemaFile);
+        $this->schema = static::parseSchema($schemaFile);
+    }
+
+    protected static function parseSchema(string $schemaFile): array
+    {
+        return json_decode(file_get_contents($schemaFile), true, 512, JSON_THROW_ON_ERROR);
+    }
 }
