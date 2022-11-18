0316554045b587e7bc6a3e3942478304d93d29da
Caen De Silva <caen@desilva.se>
2022-11-18 22:13:45 +0100

    Add publication type to constructor

--- packages/framework/src/Pages/PublicationPage.php
+++ packages/framework/src/Pages/PublicationPage.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Hyde\Pages;
 
 use Hyde\Framework\Features\Publications\Models\PublicationType;
+use Hyde\Markdown\Models\FrontMatter;
 
 /**
  * Publication pages adds an easy way to create custom no-code page types,
@@ -13,6 +14,13 @@ use Hyde\Framework\Features\Publications\Models\PublicationType;
 class PublicationPage extends Concerns\HydePage
 {
     public PublicationType $type;
+
+    public function __construct(PublicationType $type, string $identifier = '', FrontMatter|array $matter = [])
+    {
+        $this->type = $type;
+
+        parent::__construct($identifier, $matter);
+    }
 
     public function compile(): string
     {
