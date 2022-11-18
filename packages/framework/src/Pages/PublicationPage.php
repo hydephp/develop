d421668a142c3ec1f0d637c8f5e1da2953bfa3bb
Caen De Silva <caen@desilva.se>
2022-11-18 22:12:22 +0100

    Add publication type property to publication pages

--- packages/framework/src/Pages/PublicationPage.php
+++ packages/framework/src/Pages/PublicationPage.php
@@ -4,12 +4,16 @@ declare(strict_types=1);
 
 namespace Hyde\Pages;
 
+use Hyde\Framework\Features\Publications\Models\PublicationType;
+
 /**
  * Publication pages adds an easy way to create custom no-code page types,
  * with support using a custom front matter schema and Blade templates.
  */
 class PublicationPage extends Concerns\HydePage
 {
+    protected PublicationType $type;
+
     public function compile(): string
     {
         // TODO: Implement compile() method.
