1462755c42c4316c272b0a556dd558d54784d9d2
Caen De Silva <caen@desilva.se>
2022-11-18 20:37:12 +0100

    Create PublicationPage.php

---
+++ packages/framework/src/Pages/PublicationPage.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hyde\Pages;
+
+class PublicationPage extends Concerns\HydePage
+{
+    public function compile(): string
+    {
+        // TODO: Implement compile() method.
+    }
+}
