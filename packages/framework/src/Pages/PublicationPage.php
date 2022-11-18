58c52296e618cf575107bd7d2c3e4beb4c12b2c3
Caen De Silva <caen@desilva.se>
2022-11-18 20:49:39 +0100

    Add class PHPDoc

--- packages/framework/src/Pages/PublicationPage.php
+++ packages/framework/src/Pages/PublicationPage.php
@@ -4,6 +4,10 @@ declare(strict_types=1);
 
 namespace Hyde\Pages;
 
+/**
+ * Publication pages adds an easy way to create custom no-code page types,
+ * with support using a custom front matter schema and Blade templates.
+ */
 class PublicationPage extends Concerns\HydePage
 {
     public function compile(): string
