50e77be76e026b8cbe980b6beee72004fa9ff6f7
Caen De Silva <caen@desilva.se>
2022-11-19 15:48:42 +0100

    Override directory information

--- packages/framework/src/Framework/Features/Publications/Models/PublicationListPage.php
+++ packages/framework/src/Framework/Features/Publications/Models/PublicationListPage.php
@@ -6,5 +6,7 @@ namespace Hyde\Framework\Features\Publications\Models;
 
 class PublicationListPage extends \Hyde\Pages\BladePage
 {
-    //
+    public static string $sourceDirectory = '%type%';
+    public static string $outputDirectory = '%type%';
+    public static string $fileExtension = '';
 }
