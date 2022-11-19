be98c031db103c51bfcf9ff9c7ea44938a7f5f50
Caen De Silva <caen@desilva.se>
2022-11-19 15:54:46 +0100

    Replace qualifier with an import

--- packages/framework/src/Framework/Features/Publications/Models/PublicationListPage.php
+++ packages/framework/src/Framework/Features/Publications/Models/PublicationListPage.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 
 namespace Hyde\Framework\Features\Publications\Models;
 
-class PublicationListPage extends \Hyde\Pages\BladePage
+use Hyde\Pages\BladePage;
+
+class PublicationListPage extends BladePage
 {
     public static string $sourceDirectory = '';
     public static string $outputDirectory = '';
