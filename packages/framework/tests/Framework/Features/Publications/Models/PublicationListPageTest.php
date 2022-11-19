6bd5d85e35c79f79448b986451b9b00ed4e82b5e
Caen De Silva <caen@desilva.se>
2022-11-19 15:54:35 +0100

    Create PublicationListPageTest.php

---
+++ packages/framework/tests/Framework/Features/Publications/Models/PublicationListPageTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Hyde\Framework\Testing\Framework\Features\Publications\Models;
+
+use Hyde\Framework\Features\Publications\Models\PublicationListPage;
+use Hyde\Testing\TestCase;
+
+/**
+ * @covers \Hyde\Framework\Features\Publications\Models\PublicationListPage
+ */
+class PublicationListPageTest extends TestCase
+{
+    //
+}
