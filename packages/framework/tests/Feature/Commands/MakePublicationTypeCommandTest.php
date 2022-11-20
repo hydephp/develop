61cf0bca96c0aa79e46110a78e005d088d3c0d1f
Caen De Silva <caen@desilva.se>
2022-11-20 16:38:04 +0100

    Copilot generate test

--- packages/framework/tests/Feature/Commands/MakePublicationTypeCommandTest.php
+++ packages/framework/tests/Feature/Commands/MakePublicationTypeCommandTest.php
@@ -12,5 +12,19 @@ use Hyde\Testing\TestCase;
  */
 class MakePublicationTypeCommandTest extends TestCase
 {
-    //
+    public function test_command_creates_publication()
+    {
+        $this->artisan(MakePublicationTypeCommand::class, ['title' => 'Test Publication'])
+            ->expectsOutput('Creating a new Publication Type!')
+            ->expectsOutput('Publication type name')
+            ->expectsOutput('Choose the default field you wish to sort by:')
+            ->expectsOutput('  0: dateCreated (meta field)')
+            ->expectsOutput('  1: title')
+            ->expectsOutput('  2: body')
+            ->expectsOutput('Choose the default sort direction:')
+            ->expectsOutput('  1 - Ascending (oldest items first if sorting by dateCreated)')
+            ->expectsOutput('  2 - Descending (newest items first if sorting by dateCreated)')
+            ->expectsOutput('Publication type created successfully!')
+            ->assertExitCode(0);
+    }
 }
