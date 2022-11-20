259df4a6e9bd68e9e8c36fa9f96022211ecb807e
Caen De Silva <caen@desilva.se>
2022-11-20 22:14:58 +0100

    Remove always null variable as value cannot be gotten from schema file

--- packages/framework/src/Framework/Features/Publications/Models/PublicationType.php
+++ packages/framework/src/Framework/Features/Publications/Models/PublicationType.php
@@ -44,7 +44,7 @@ class PublicationType implements JsonSerializable, Arrayable
     {
         try {
             $schema = static::parseSchemaFile($schemaFile);
-            $schema = array_merge($schema, ['directory' => $schema['directory'] ?? self::getRelativeDirectoryName($schemaFile)]);
+            $schema = array_merge($schema, ['directory' => self::getRelativeDirectoryName($schemaFile)]);
             return new static(...$schema);
         } catch (Exception $exception) {
             throw new RuntimeException("Could not parse schema file $schemaFile", 0, $exception);
