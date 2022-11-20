7d349b341d0ffd89c6fb8af5029f817bfc73ec14
Caen De Silva <caen@desilva.se>
2022-11-20 19:03:54 +0100

    Add base test

--- packages/framework/tests/Feature/PublicationTypeTest.php
+++ packages/framework/tests/Feature/PublicationTypeTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Hyde\Framework\Testing\Feature;
 
+use Hyde\Framework\Features\Publications\Models\PublicationType;
 use Hyde\Testing\TestCase;
 
 /**
@@ -11,5 +12,18 @@ use Hyde\Testing\TestCase;
  */
 class PublicationTypeTest extends TestCase
 {
-    //
+    public function testCanConstructNewPublicationType()
+    {
+        $publicationType = new PublicationType('test', 'canonical', 'sort', 'asc', 10, true, 'detail', 'list', ['foo' => 'bar']);
+
+        $this->assertSame('test', $publicationType->name);
+        $this->assertSame('canonical', $publicationType->canonicalField);
+        $this->assertSame('sort', $publicationType->sortField);
+        $this->assertSame('asc', $publicationType->sortDirection);
+        $this->assertSame(10, $publicationType->pagesize);
+        $this->assertSame(true, $publicationType->prevNextLinks);
+        $this->assertSame('detail', $publicationType->detailTemplate);
+        $this->assertSame('list', $publicationType->listTemplate);
+        $this->assertSame(['foo' => 'bar'], $publicationType->fields);
+    }
 }
