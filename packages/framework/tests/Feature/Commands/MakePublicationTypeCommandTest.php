4791d4a15ba55df0067415e965139398a79df7b1
Caen De Silva <caen@desilva.se>
2022-11-20 16:32:52 +0100

    Create MakePublicationTypeCommandTest.php

---
+++ packages/framework/tests/Feature/Commands/MakePublicationTypeCommandTest.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hyde\Framework\Testing\Feature\Commands;
+
+use Hyde\Console\Commands\MakePublicationTypeCommand;
+use Hyde\Testing\TestCase;
+
+/**
+ * @covers \Hyde\Console\Commands\MakePublicationTypeCommand
+ */
+class MakePublicationTypeCommandTest extends TestCase
+{
+    //
+}
