b952ee002e415c8480c0cca6008b33a27d1497ba
Caen De Silva <caen@desilva.se>
2022-11-20 16:47:55 +0100

    Update MakePublicationTypeCommandTest.php

--- packages/framework/tests/Feature/Commands/MakePublicationTypeCommandTest.php
+++ packages/framework/tests/Feature/Commands/MakePublicationTypeCommandTest.php
@@ -5,7 +5,10 @@ declare(strict_types=1);
 namespace Hyde\Framework\Testing\Feature\Commands;
 
 use Hyde\Console\Commands\MakePublicationTypeCommand;
+use Hyde\Hyde;
 use Hyde\Testing\TestCase;
+
+use function deleteDirectory;
 
 /**
  * @covers \Hyde\Console\Commands\MakePublicationTypeCommand
@@ -14,17 +17,25 @@ class MakePublicationTypeCommandTest extends TestCase
 {
     public function test_command_creates_publication()
     {
-        $this->artisan(MakePublicationTypeCommand::class, ['title' => 'Test Publication'])
-            ->expectsOutput('Creating a new Publication Type!')
-            ->expectsOutput('Publication type name')
+        $this->artisan('make:publicationType')
+            ->expectsQuestion('Publication type name', 'Test Publication')
+            ->expectsQuestion('Field name', 'Title')
+            ->expectsQuestion('Field type (1-7)', 1)
+            ->expectsQuestion('Min value (for strings, this refers to string length)', 'default')
+            ->expectsQuestion('Max value (for strings, this refers to string length)', 'default')
+            ->expectsQuestion('Add another field (y/n)', 'n')
+            ->expectsQuestion('Sort field (0-1)', 0)
+            ->expectsQuestion('Sort field (1-2)', 1)
+            ->expectsQuestion('Enter the pagesize (0 for no limit)', 10)
+            ->expectsQuestion('Generate previous/next links in detail view (y/n)', 'n')
+            ->expectsQuestion('Canonical field (1-1)', 1)
+            ->expectsOutputToContain('Creating a new Publication Type!')
             ->expectsOutput('Choose the default field you wish to sort by:')
-            ->expectsOutput('  0: dateCreated (meta field)')
-            ->expectsOutput('  1: title')
-            ->expectsOutput('  2: body')
             ->expectsOutput('Choose the default sort direction:')
-            ->expectsOutput('  1 - Ascending (oldest items first if sorting by dateCreated)')
-            ->expectsOutput('  2 - Descending (newest items first if sorting by dateCreated)')
-            ->expectsOutput('Publication type created successfully!')
+            // ->expectsOutput('Publication type created successfully!')
+            // ->expectsOutput('Saving publicationType data to [test-publication/schema.json]')
             ->assertExitCode(0);
+
+        deleteDirectory(Hyde::path('test-publication'));
     }
 }
