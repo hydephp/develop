e3729858b90924c1e51a003d44c959a50a120e53
Caen De Silva <caen@desilva.se>
2022-11-20 19:09:13 +0100

    Dynamically handle test

--- packages/framework/tests/Feature/PublicationTypeTest.php
+++ packages/framework/tests/Feature/PublicationTypeTest.php
@@ -14,16 +14,27 @@ class PublicationTypeTest extends TestCase
 {
     public function testCanConstructNewPublicationType()
     {
-        $publicationType = new PublicationType('test', 'canonical', 'sort', 'asc', 10, true, 'detail', 'list', ['foo' => 'bar']);
+        $publicationType = new PublicationType(...$this->getTestData());
 
-        $this->assertSame('test', $publicationType->name);
-        $this->assertSame('canonical', $publicationType->canonicalField);
-        $this->assertSame('sort', $publicationType->sortField);
-        $this->assertSame('asc', $publicationType->sortDirection);
-        $this->assertSame(10, $publicationType->pagesize);
-        $this->assertSame(true, $publicationType->prevNextLinks);
-        $this->assertSame('detail', $publicationType->detailTemplate);
-        $this->assertSame('list', $publicationType->listTemplate);
-        $this->assertSame(['foo' => 'bar'], $publicationType->fields);
+        foreach ($this->getTestData() as $key => $property) {
+            $this->assertEquals($property, $publicationType->$key);
+        }
+    }
+
+    protected function getTestData(): array
+    {
+        return [
+            'name'           => 'test',
+            'canonicalField' => 'canonical',
+            'sortField'      => 'sort',
+            'sortDirection'  => 'asc',
+            'pagesize'       => 10,
+            'prevNextLinks'  => true,
+            'detailTemplate' => 'detail',
+            'listTemplate'   => 'list',
+            'fields'         => [
+                'foo' => 'bar',
+            ]
+        ];
     }
 }
