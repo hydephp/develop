593ee68cac12b8ed713c6d584bf7339518383b36
Caen De Silva <caen@desilva.se>
2022-11-21 13:10:02 +0100

    Create CreatesNewPublicationTypeSchemaTest.php

---
+++ packages/framework/tests/Feature/CreatesNewPublicationTypeSchemaTest.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hyde\Framework\Testing\Feature;
+
+use Hyde\Framework\Actions\CreatesNewPublicationTypeSchema;
+use Hyde\Testing\TestCase;
+
+/**
+ * @covers \Hyde\Framework\Actions\CreatesNewPublicationTypeSchema
+ */
+class CreatesNewPublicationTypeSchemaTest extends TestCase
+{
+    //
+}
