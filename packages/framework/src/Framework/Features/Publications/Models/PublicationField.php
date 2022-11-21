9ab9899f57af21296296b35e00ec15507c234d59
Caen De Silva <caen@desilva.se>
2022-11-21 18:38:23 +0100

    Create PublicationFieldTest.php
    Update PublicationField.php

--- packages/framework/src/Framework/Features/Publications/Models/PublicationField.php
+++ packages/framework/src/Framework/Features/Publications/Models/PublicationField.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace Hyde\Framework\Features\Publications\Models;
 
+/**
+ * @see \Hyde\Framework\Testing\Feature\PublicationFieldTest
+ */
 class PublicationField
 {
     //
---
+++ packages/framework/tests/Feature/PublicationFieldTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Hyde\Framework\Testing\Feature;
+
+use Hyde\Framework\Features\Publications\Models\PublicationField;
+use Hyde\Testing\TestCase;
+
+/**
+ * @covers \Hyde\Framework\Features\Publications\Models\PublicationField
+ */
+class PublicationFieldTest extends TestCase
+{
+    //
+}
