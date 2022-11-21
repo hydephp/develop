0e96fc0089a5b87b263afcdad99e84e46329d411
Caen De Silva <caen@desilva.se>
2022-11-21 22:56:29 +0100

    Add publication field

--- packages/framework/tests/Feature/Actions/CreatesNewPublicationFileTest.php
+++ packages/framework/tests/Feature/Actions/CreatesNewPublicationFileTest.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Hyde\Framework\Testing\Feature\Actions;
 
 use Hyde\Framework\Actions\CreatesNewPublicationFile;
+use Hyde\Framework\Features\Publications\Models\PublicationField;
 use Hyde\Framework\Features\Publications\Models\PublicationType;
 use Hyde\Hyde;
 use Hyde\Testing\TestCase;
@@ -19,7 +20,10 @@ class CreatesNewPublicationFileTest extends TestCase
     public function testCreate()
     {
         $pubType = PublicationType::fromFile(Hyde::path('tests/fixtures/test-publication-schema.json'));
-        $fieldData = Collection::make([]);
+        $fieldData = Collection::make([
+            'test' => new PublicationField('string', 'test', 1, 10),
+        ]);
+
         $creator = new CreatesNewPublicationFile($pubType, $fieldData);
         $creator->create();
 
