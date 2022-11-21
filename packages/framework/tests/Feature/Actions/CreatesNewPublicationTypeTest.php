ab28eaa2ca0b16b4d4dd7c69ab54e3615211d2dd
Caen De Silva <caen@desilva.se>
2022-11-21 13:21:21 +0100

    Implement CreatesNewPublicationTypeTest

--- packages/framework/tests/Feature/Actions/CreatesNewPublicationTypeTest.php
+++ packages/framework/tests/Feature/Actions/CreatesNewPublicationTypeTest.php
@@ -4,12 +4,33 @@ declare(strict_types=1);
 
 namespace Hyde\Framework\Testing\Feature\Actions;
 
+use Hyde\Framework\Actions\CreatesNewPublicationType;
+use Hyde\Hyde;
 use Hyde\Testing\TestCase;
+use Rgasch\Collection\Collection;
+
+use function deleteDirectory;
 
 /**
  * @covers \Hyde\Framework\Actions\CreatesNewPublicationType
  */
 class CreatesNewPublicationTypeTest extends TestCase
 {
-    //
+    public function test_it_creates_a_new_publication_type()
+    {
+        $creator = new CreatesNewPublicationType('name', new Collection(), 'canonical', 'sort', 'asc', 10, true);
+        $creator->create();
+
+        $this->assertFileExists('name/schema.json');
+        $this->assertStringContainsString('"name": "name"', $creator->getResult());
+        $this->assertStringContainsString('"canonicalField": "canonical"', $creator->getResult());
+        $this->assertStringContainsString('"sortField": "sort"', $creator->getResult());
+        $this->assertStringContainsString('"sortDirection": "asc"', $creator->getResult());
+        $this->assertStringContainsString('"pageSize": 10', $creator->getResult());
+        $this->assertStringContainsString('"prevNextLinks": true', $creator->getResult());
+        $this->assertStringContainsString('"detailTemplate": "name_detail"', $creator->getResult());
+        $this->assertStringContainsString('"listTemplate": "name_list"', $creator->getResult());
+
+        deleteDirectory(Hyde::path('name'));
+    }
 }
