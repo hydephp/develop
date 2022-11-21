746c3ae8538385a80081de9c146a871813886bb7
Caen De Silva <caen@desilva.se>
2022-11-21 18:59:44 +0100

    Fully use int type for what is actually integers

--- packages/framework/src/Framework/Features/Publications/Models/PublicationField.php
+++ packages/framework/src/Framework/Features/Publications/Models/PublicationField.php
@@ -17,10 +17,10 @@ class PublicationField implements JsonSerializable, Arrayable
 
     public readonly string $type;
     public readonly string $name;
-    public readonly string $min;
-    public readonly string $max;
+    public readonly int $min;
+    public readonly int $max;
 
-    public function __construct(string $type, string $name, string $min, string $max)
+    public function __construct(string $type, string $name, int $min, int $max)
     {
         $this->type = $type;
         $this->name = $name;
--- packages/framework/tests/Feature/PublicationFieldTest.php
+++ packages/framework/tests/Feature/PublicationFieldTest.php
@@ -19,8 +19,8 @@ class PublicationFieldTest extends TestCase
 
         $this->assertSame('string', $field->type);
         $this->assertSame('test', $field->name);
-        $this->assertSame('1', $field->min);
-        $this->assertSame('10', $field->max);
+        $this->assertSame(1, $field->min);
+        $this->assertSame(10, $field->max);
     }
 
     public function test_can_get_field_as_array()
@@ -28,18 +28,18 @@ class PublicationFieldTest extends TestCase
         $this->assertSame([
             'type' => 'string',
             'name' => 'test',
-            'min'  => '1',
-            'max'  => '10',
+            'min'  => 1,
+            'max'  => 10,
         ], $this->makeField()->toArray());
     }
 
     public function test_can_encode_field_as_json()
     {
-        $this->assertSame('{"type":"string","name":"test","min":"1","max":"10"}', json_encode($this->makeField()));
+        $this->assertSame('{"type":"string","name":"test","min":1,"max":10}', json_encode($this->makeField()));
     }
 
     protected function makeField(): PublicationField
     {
-        return new PublicationField('string', 'test', '1', '10');
+        return new PublicationField('string', 'test', 1, 10);
     }
 }
