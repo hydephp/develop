a29c76525ac4e8a35815247d60f12cba45bbc698
Caen De Silva <caen@desilva.se>
2022-11-22 18:16:56 +0100

    Decouple PlayCDN so it's a superset over the default styles so HydeFront is added

--- packages/framework/resources/views/layouts/styles.blade.php
+++ packages/framework/resources/views/layouts/styles.blade.php
@@ -2,13 +2,15 @@
 <style>[x-cloak] {display: none!important}</style>
 
 {{-- The compiled Tailwind/App styles --}}
-@if(config('hyde.use_play_cdn', false))
-<script src="https://cdn.tailwindcss.com?plugins=typography"></script>
-<script>tailwind.config = { {!! Asset::injectTailwindConfig() !!} }</script>
-@elseif(config('hyde.load_app_styles_from_cdn', false))
+@if(config('hyde.load_app_styles_from_cdn', false))
 <link rel="stylesheet" href="{{ Asset::cdnLink('app.css') }}">
 @elseif(Asset::hasMediaFile('app.css'))
 <link rel="stylesheet" href="{{ Asset::mediaLink('app.css') }}">
+@endif
+
+@if(config('hyde.use_play_cdn', false))
+    <script src="https://cdn.tailwindcss.com?plugins=typography"></script>
+    <script>tailwind.config = { {!! Asset::injectTailwindConfig() !!} }</script>
 @endif
 
 {{-- Add any extra styles to include after the others --}}
