b28131051f92ebc355548d9b9f73d2d2e1b7b285
Caen De Silva <caen@desilva.se>
2022-11-22 18:11:03 +0100

    Format HTML

--- packages/framework/resources/views/layouts/styles.blade.php
+++ packages/framework/resources/views/layouts/styles.blade.php
@@ -4,11 +4,7 @@
 {{-- The compiled Tailwind/App styles --}}
 @if(config('hyde.use_play_cdn', false))
 <script src="https://cdn.tailwindcss.com?plugins=typography"></script>
-<script>
-    tailwind.config = {
-        {!! Asset::injectTailwindConfig() !!}
-    }
-</script>
+<script>tailwind.config = { {!! Asset::injectTailwindConfig() !!} }</script>
 @elseif(config('hyde.load_app_styles_from_cdn', false))
 <link rel="stylesheet" href="{{ Asset::cdnLink('app.css') }}">
 @elseif(Asset::hasMediaFile('app.css'))
