959d2f88eaa5252e5ce3c36c088a4ea513f6678e
Caen De Silva <caen@desilva.se>
2022-11-23 19:55:49 +0100

    Add class PHPDoc

--- packages/framework/src/Console/Concerns/ValidatingCommand.php
+++ packages/framework/src/Console/Concerns/ValidatingCommand.php
@@ -6,6 +6,9 @@ namespace Hyde\Console\Concerns;
 
 use LaravelZero\Framework\Commands\Command;
 
+/**
+ * An extended Command class that provides validation methods.
+ */
 class ValidatingCommand extends Command
 {
     //
