171fde69dee07f245c29a1a6f50d23cd56f924f8
Caen De Silva <caen@desilva.se>
2022-11-23 20:38:53 +0100

    Start sketching mocks for test

--- packages/framework/tests/Feature/ValidatingCommandTest.php
+++ packages/framework/tests/Feature/ValidatingCommandTest.php
@@ -4,12 +4,48 @@ declare(strict_types=1);
 
 namespace Hyde\Framework\Testing\Feature;
 
+use Hyde\Console\Concerns\ValidatingCommand;
 use Hyde\Testing\TestCase;
+use Rgasch\Collection\Collection;
+
+use Illuminate\Console\Application;
+use Illuminate\Console\Command;
+use Illuminate\Console\OutputStyle;
+use Illuminate\Console\View\Components\Factory;
+use Mockery;
+use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\NullOutput;
+use Symfony\Component\Console\Question\ChoiceQuestion;
 
 /**
  * @covers \Hyde\Console\Concerns\ValidatingCommand
  */
 class ValidatingCommandTest extends TestCase
 {
-    //
+    public function testAskWithValidation()
+    {
+        $command = new class extends ValidatingCommand {
+            public function handle()
+            {
+                $this->askWithValidation('name', 'What is your name?', ['required'], 'John Doe');
+            }
+        };
+
+        $output = Mockery::mock(OutputStyle::class);
+
+        $output->shouldReceive('ask')->once()->withArgs(function (string $question) {
+            return $question === 'What is your name?';
+        });
+
+        $output->shouldReceive('writeln')->once()->withArgs(function (string $message) {
+            return $message === '<error>validation.required</error>';
+        });
+
+        $command->setOutput($output);
+
+        $command->handle();
+    }
 }
