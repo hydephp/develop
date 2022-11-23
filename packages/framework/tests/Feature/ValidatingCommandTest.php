2e8c93c01dcfb98b5b20c526b1c7a3b5c084ea24
Caen De Silva <caen@desilva.se>
2022-11-23 21:06:16 +0100

    Test retried validation

--- packages/framework/tests/Feature/ValidatingCommandTest.php
+++ packages/framework/tests/Feature/ValidatingCommandTest.php
@@ -38,4 +38,33 @@ class ValidatingCommandTest extends TestCase
         $command->setOutput($output);
         $command->handle();
     }
+
+    public function testAskWithValidationRetries()
+    {
+        $command = new class extends ValidatingCommand
+        {
+            public function handle()
+            {
+                $name = $this->askWithValidation('name', 'What is your name?', ['required'], 'John Doe');
+                $this->output->writeln("Hello $name!");
+            }
+        };
+
+        $output = Mockery::mock(OutputStyle::class);
+
+        $output->shouldReceive('ask')->times(2)->withArgs(function (string $question) {
+            return $question === 'What is your name?';
+        })->andReturn('', 'Jane Doe');
+
+        $output->shouldReceive('writeln')->times(1)->withArgs(function (string $message) {
+            return $message === '<error>validation.required</error>';
+        });
+
+        $output->shouldReceive('writeln')->once()->withArgs(function (string $message) {
+            return $message === 'Hello Jane Doe!';
+        });
+
+        $command->setOutput($output);
+        $command->handle();
+    }
 }
