54d022e3d50f6d14eb2da0624edf267425c00e06
Caen De Silva <caen@desilva.se>
2022-11-23 21:14:32 +0100

    Document parameter variable

--- packages/framework/src/Console/Concerns/ValidatingCommand.php
+++ packages/framework/src/Console/Concerns/ValidatingCommand.php
@@ -27,7 +27,7 @@ class ValidatingCommand extends Command
      * @param  string  $message
      * @param  \Illuminate\Contracts\Support\Arrayable|array  $rules
      * @param  mixed|null  $default
-     * @param int $retryCount
+     * @param int $retryCount How many times has the validation loop run?
      * @return mixed
      *
      * @throws RuntimeException
