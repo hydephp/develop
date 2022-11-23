27b90c0ec22b2af5a0b305d3b6131ed5182e90ee
Caen De Silva <caen@desilva.se>
2022-11-23 21:07:24 +0100

    Test validation timeout

--- packages/framework/tests/Feature/ValidatingCommandTest.php
+++ packages/framework/tests/Feature/ValidatingCommandTest.php
@@ -8,6 +8,7 @@ use Hyde\Console\Concerns\ValidatingCommand;
 use Hyde\Testing\TestCase;
 use Illuminate\Console\OutputStyle;
 use Mockery;
+use RuntimeException;
 
 /**
  * @covers \Hyde\Console\Concerns\ValidatingCommand
@@ -63,6 +64,34 @@ class ValidatingCommandTest extends TestCase
         $output->shouldReceive('writeln')->once()->withArgs(function (string $message) {
             return $message === 'Hello Jane Doe!';
         });
+
+        $command->setOutput($output);
+        $command->handle();
+    }
+
+    public function testAskWithValidationRetriesTooManyTimes()
+    {
+        $command = new class extends ValidatingCommand
+        {
+            public function handle()
+            {
+                $name = $this->askWithValidation('name', 'What is your name?', ['required'], 'John Doe');
+                $this->output->writeln("Hello $name!");
+            }
+        };
+
+        $output = Mockery::mock(OutputStyle::class);
+
+        $output->shouldReceive('ask')->times(10)->withArgs(function (string $question) {
+            return $question === 'What is your name?';
+        })->andReturn('');
+
+        $output->shouldReceive('writeln')->times(10)->withArgs(function (string $message) {
+            return $message === '<error>validation.required</error>';
+        });
+
+        $this->expectException(RuntimeException::class);
+        $this->expectExceptionMessage("Too many validation errors trying to validate 'name' with rules: [required]");
 
         $command->setOutput($output);
         $command->handle();
