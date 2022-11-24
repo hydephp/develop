4def45d658dc3830f4d7ad16a1a212fb3f9ef361
Caen De Silva <caen@desilva.se>
2022-11-24 19:13:14 +0100

    Sketch out class

--- monorepo/HydeStan/HydeStan.php
+++ monorepo/HydeStan/HydeStan.php
@@ -4,5 +4,15 @@ declare(strict_types=1);
 
 class HydeStan
 {
-    //
+    protected array $errors = [];
+
+    public function run(): void
+    {
+        //
+    }
+
+    public function getErrors(): array
+    {
+        return $this->errors;
+    }
 }
