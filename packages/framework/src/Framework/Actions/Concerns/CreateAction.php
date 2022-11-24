5986acfaebbfd3841953d20be138d40dee2d3062
Caen De Silva <caen@desilva.se>
2022-11-24 16:53:00 +0100

    Create abstract class CreateAction implements CreateActionContract
    Update CreateActionContract.php

---
+++ packages/framework/src/Framework/Actions/Concerns/CreateAction.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hyde\Framework\Actions\Concerns;
+
+use Hyde\Framework\Actions\Contracts\CreateActionContract;
+
+abstract class CreateAction implements CreateActionContract
+{
+    public function create(): void
+    {
+        // TODO: Implement create() method.
+    }
+}
--- packages/framework/src/Framework/Actions/Contracts/CreateActionContract.php
+++ packages/framework/src/Framework/Actions/Contracts/CreateActionContract.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace Hyde\Framework\Actions\Contracts;
 
+/**
+ * @see \Hyde\Framework\Actions\Concerns\CreateAction
+ */
 interface CreateActionContract
 {
     public function create(): void;
