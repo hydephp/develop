2388ee9f70bfd2564923a6fe32428c42705f0389
Caen De Silva <caen@desilva.se>
2022-11-24 19:19:44 +0100

    Discover files

--- monorepo/HydeStan/HydeStan.php
+++ monorepo/HydeStan/HydeStan.php
@@ -5,14 +5,30 @@ declare(strict_types=1);
 class HydeStan
 {
     protected array $errors = [];
+    protected array $files;
 
     public function run(): void
     {
-        //
+        $this->files = $this->getFiles();
     }
 
     public function getErrors(): array
     {
         return $this->errors;
+    }
+
+    private function getFiles(): array
+    {
+        $files = [];
+
+        $directory = new RecursiveDirectoryIterator(BASE_PATH . '/src');
+        $iterator = new RecursiveIteratorIterator($directory);
+        $regex = new RegexIterator($iterator, '/^.+\.php$/i', RecursiveRegexIterator::GET_MATCH);
+
+        foreach ($regex as $file) {
+            $files[] = substr($file[0], strlen(BASE_PATH) + 1);
+        }
+
+        return $files;
     }
 }
