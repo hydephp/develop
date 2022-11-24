1fa759fdb90a40018a69547a070c062d9d0a5958
Caen De Silva <caen@desilva.se>
2022-11-24 17:19:11 +0100

    Add CreateActionTestClass

--- packages/framework/tests/Feature/CreateActionTest.php
+++ packages/framework/tests/Feature/CreateActionTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Hyde\Framework\Testing\Feature;
 
+use Hyde\Framework\Actions\Concerns\CreateAction;
 use Hyde\Testing\TestCase;
 
 /**
@@ -12,4 +13,14 @@ use Hyde\Testing\TestCase;
 class CreateActionTest extends TestCase
 {
     //
+}
+
+class CreateActionTestClass extends CreateAction
+{
+    protected string $outputPath = 'foo';
+
+    protected function handleCreate(): void
+    {
+        //
+    }
 }
