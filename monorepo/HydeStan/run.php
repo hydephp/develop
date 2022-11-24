e7b07680b326147cbf275cf84da48fe0c0a9e46b
Caen De Silva <caen@desilva.se>
2022-11-24 19:10:39 +0100

    Sketch out runner

--- monorepo/HydeStan/run.php
+++ monorepo/HydeStan/run.php
@@ -1,3 +1,18 @@
 <?php
 
 declare(strict_types=1);
+
+require_once __DIR__ . '/HydeStan.php';
+
+$analyser = new HydeStan();
+$analyser->run();
+
+foreach ($analyser->getErrors() as $error) {
+    echo $error . PHP_EOL;
+}
+
+if ($analyser->hasErrors()) {
+    exit(1);
+}
+
+exit(0);
