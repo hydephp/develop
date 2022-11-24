fa35e6c1e1eceaab39bd6df6a4b7558a017c8ac8
Caen De Silva <caen@desilva.se>
2022-11-24 21:38:58 +0100

    Remove internal interface

--- monorepo/HydeStan/vendor/php-console/console.php
+++ monorepo/HydeStan/vendor/php-console/console.php
@@ -8,20 +8,7 @@
  * @internal
  */
 
-interface ConsoleContract
-{
-    public function write(string $string): self;
-    public function line(string $message = ''): self;
-    public function newline(int $count = 1): self;
-
-    public function info(string $message): self;
-    public function warn(string $message): self;
-    public function error(string $message): self;
-    public function debug(string $message): self;
-    public function debugComment(string $message): self;
-}
-
-class Console implements ConsoleContract
+class Console
 {
     use ColoredOutput;
 
