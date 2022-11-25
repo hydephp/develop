c5960933cf60ba4db2ea8c18444af9cd9e0a314b
Caen De Silva <caen@desilva.se>
2022-11-25 20:25:22 +0100

    Test getPublicationTypes helper

--- packages/framework/tests/Feature/Services/PublicationServiceTest.php
+++ packages/framework/tests/Feature/Services/PublicationServiceTest.php
@@ -4,12 +4,45 @@ declare(strict_types=1);
 
 namespace Hyde\Framework\Testing\Feature\Services;
 
+use Hyde\Framework\Features\Publications\Models\PublicationType;
+use Hyde\Framework\Features\Publications\PublicationService;
+use Hyde\Hyde;
 use Hyde\Testing\TestCase;
+use Illuminate\Support\Facades\File;
+use Rgasch\Collection\Collection;
+
+use function mkdir;
 
 /**
  * @covers \Hyde\Framework\Features\Publications\PublicationService
  */
 class PublicationServiceTest extends TestCase
 {
-    //
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        mkdir(Hyde::path('test-publication'));
+    }
+
+    protected function tearDown(): void
+    {
+        File::deleteDirectory(Hyde::path('test-publication'));
+
+        parent::tearDown();
+    }
+
+    public function testGetPublicationTypesWithNoTypes()
+    {
+        $this->assertEquals(new Collection(), PublicationService::getPublicationTypes());
+    }
+
+    public function testGetPublicationTypesWithTypes()
+    {
+        copy(Hyde::path('tests/fixtures/test-publication-schema.json'), Hyde::path('test-publication/schema.json'));
+
+        $this->assertEquals(new Collection([
+            'test-publication' => PublicationType::get('test-publication')
+        ]), PublicationService::getPublicationTypes());
+    }
 }
