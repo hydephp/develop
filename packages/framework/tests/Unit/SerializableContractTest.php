91a05cd009c206516e2004928254ce96c7da219a
Caen De Silva <caen@desilva.se>
2022-11-26 20:43:32 +0100

    Create SerializableContractTest.php

---
+++ packages/framework/tests/Unit/SerializableContractTest.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hyde\Framework\Testing\Unit;
+
+use Hyde\Support\Contracts\SerializableContract;
+use Hyde\Testing\TestCase;
+
+/**
+ * @see \Hyde\Support\Contracts\SerializableContract
+ */
+class SerializableContractTest extends TestCase
+{
+    //
+}
