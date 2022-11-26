ebc4b2f2ad6fda2b30e713988151afed3a7ccaa6
Caen De Silva <caen@desilva.se>
2022-11-26 12:19:42 +0100

    Add class description

--- packages/framework/src/Facades/Filesystem.php
+++ packages/framework/src/Facades/Filesystem.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 namespace Hyde\Facades;
 
 /**
+ * Proxies the Laravel File facade with extra features and helpers tailored for HydePHP.
+ * 
  * @see \Hyde\Foundation\Filesystem
  * @see \Illuminate\Filesystem\Filesystem
  */
