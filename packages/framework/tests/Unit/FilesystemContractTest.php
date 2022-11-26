bfb364571dde82fd48f3f6d35c4228ea81a48373
Caen De Silva <caen@desilva.se>
2022-11-26 12:50:06 +0100

    Create FilesystemContractTest.php

---
+++ packages/framework/tests/Unit/FilesystemContractTest.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hyde\Framework\Testing\Unit;
+
+use Hyde\Support\Contracts\FilesystemContract;
+use Hyde\Testing\TestCase;
+
+/**
+ * @covers \Hyde\Support\Contracts\FilesystemContract
+ */
+class FilesystemContractTest extends TestCase
+{
+    //
+}
