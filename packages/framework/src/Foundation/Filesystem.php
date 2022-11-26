038e89b2c289bcbff86890b35c6cca41c4c3e72d
Caen De Silva <caen@desilva.se>
2022-11-26 19:08:17 +0100

    Trim trailing slashes from assembled paths

--- packages/framework/src/Foundation/Filesystem.php
+++ packages/framework/src/Foundation/Filesystem.php
@@ -185,6 +185,6 @@ class Filesystem
      */
     public static function implode(string $base, string ...$paths): string
     {
-        return implode(DIRECTORY_SEPARATOR, array_merge([$base], $paths));
+        return unslash(implode(DIRECTORY_SEPARATOR, array_merge([$base], $paths)));
     }
 }
