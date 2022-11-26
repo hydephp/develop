d9736313fb7ba2b6334da80546cda533e15b96da
Caen De Silva <caen@desilva.se>
2022-11-26 13:30:08 +0100

    Add test method stubs

--- packages/framework/tests/Feature/FilesystemFacadeTest.php
+++ packages/framework/tests/Feature/FilesystemFacadeTest.php
@@ -11,5 +11,228 @@ use Hyde\Testing\TestCase;
  */
 class FilesystemFacadeTest extends TestCase
 {
-    //
+    public function testExists()
+    {
+        //
+    }
+
+    public function testMissing()
+    {
+        //
+    }
+
+    public function testGet()
+    {
+        //
+    }
+
+    public function testSharedGet()
+    {
+        //
+    }
+
+    public function testGetRequire()
+    {
+        //
+    }
+
+    public function testRequireOnce()
+    {
+        //
+    }
+
+    public function testLines()
+    {
+        //
+    }
+
+    public function testHash()
+    {
+        //
+    }
+
+    public function testPut()
+    {
+        //
+    }
+
+    public function testReplace()
+    {
+        //
+    }
+
+    public function testReplaceInFile()
+    {
+        //
+    }
+
+    public function testPrepend()
+    {
+        //
+    }
+
+    public function testAppend()
+    {
+        //
+    }
+
+    public function testChmod()
+    {
+        //
+    }
+
+    public function testDelete()
+    {
+        //
+    }
+
+    public function testMove()
+    {
+        //
+    }
+
+    public function testCopy()
+    {
+        //
+    }
+
+    public function testLink()
+    {
+        //
+    }
+
+    public function testRelativeLink()
+    {
+        //
+    }
+
+    public function testName()
+    {
+        //
+    }
+
+    public function testBasename()
+    {
+        //
+    }
+
+    public function testDirname()
+    {
+        //
+    }
+
+    public function testExtension()
+    {
+        //
+    }
+
+    public function testGuessExtension()
+    {
+        //
+    }
+
+    public function testType()
+    {
+        //
+    }
+
+    public function testMimeType()
+    {
+        //
+    }
+
+    public function testSize()
+    {
+        //
+    }
+
+    public function testLastModified()
+    {
+        //
+    }
+
+    public function testIsDirectory()
+    {
+        //
+    }
+
+    public function testIsEmptyDirectory()
+    {
+        //
+    }
+
+    public function testIsReadable()
+    {
+        //
+    }
+
+    public function testIsWritable()
+    {
+        //
+    }
+
+    public function testHasSameHash()
+    {
+        //
+    }
+
+    public function testIsFile()
+    {
+        //
+    }
+
+    public function testGlob()
+    {
+        //
+    }
+
+    public function testFiles()
+    {
+        //
+    }
+
+    public function testAllFiles()
+    {
+        //
+    }
+
+    public function testDirectories()
+    {
+        //
+    }
+
+    public function testEnsureDirectoryExists()
+    {
+        //
+    }
+
+    public function testMakeDirectory()
+    {
+        //
+    }
+
+    public function testMoveDirectory()
+    {
+        //
+    }
+
+    public function testCopyDirectory()
+    {
+        //
+    }
+
+    public function testDeleteDirectory()
+    {
+        //
+    }
+
+    public function testDeleteDirectories()
+    {
+        //
+    }
+
+    public function testCleanDirectory()
+    {
+        //
+    }
 }
