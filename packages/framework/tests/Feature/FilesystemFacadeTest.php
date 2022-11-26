d0f64c136bb7a9b7930ce92d86b5d4fdda647933
Caen De Silva <caen@desilva.se>
2022-11-26 12:14:20 +0100

    Create FilesystemFacadeTest.php

---
+++ packages/framework/tests/Feature/FilesystemFacadeTest.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hyde\Framework\Testing\Feature;
+
+use Hyde\Facades\Filesystem;
+use Hyde\Testing\TestCase;
+
+/**
+ * @covers \Hyde\Facades\Filesystem
+ */
+class FilesystemFacadeTest extends TestCase
+{
+    //
+}
