502d7f4a9ade2ab573f3f336088a3e80a9a2b66a
Caen De Silva <caen@desilva.se>
2022-11-26 13:09:23 +0100

    Test all base methods are covered by the interface

--- packages/framework/tests/Unit/FilesystemContractTest.php
+++ packages/framework/tests/Unit/FilesystemContractTest.php
@@ -6,11 +6,29 @@ namespace Hyde\Framework\Testing\Unit;
 
 use Hyde\Support\Contracts\FilesystemContract;
 use Hyde\Testing\TestCase;
+use Illuminate\Filesystem\Filesystem;
+use ReflectionClass;
+use function array_diff;
+use function get_class_methods;
 
 /**
  * @covers \Hyde\Support\Contracts\FilesystemContract
  */
 class FilesystemContractTest extends TestCase
 {
-    //
+    public function testAllBaseMethodsAreCoveredByInterface()
+    {
+        foreach ($this->getBaseMethods(new ReflectionClass(Filesystem::class)) as $method) {
+            $this->assertContains($method->name, get_class_methods(FilesystemContract::class));
+        }
+    }
+
+    protected function getBaseMethods(ReflectionClass $reflectionClass): array
+    {
+        $baseMethods = $reflectionClass->getMethods();
+        foreach ($reflectionClass->getTraits() as $trait) {
+            $baseMethods = array_diff($baseMethods, $trait->getMethods());
+        }
+        return $baseMethods;
+    }
 }
