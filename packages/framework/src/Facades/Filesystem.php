a2f075d05e6d3dd3f344a2cc96f48d8a2de60665
Caen De Silva <caen@desilva.se>
2022-11-26 13:15:08 +0100

    Class Filesystem implements FilesystemContract

--- packages/framework/src/Facades/Filesystem.php
+++ packages/framework/src/Facades/Filesystem.php
@@ -4,13 +4,15 @@ declare(strict_types=1);
 
 namespace Hyde\Facades;
 
+use Hyde\Support\Contracts\FilesystemContract;
+
 /**
  * Proxies the Laravel File facade with extra features and helpers tailored for HydePHP.
  *
  * @see \Hyde\Foundation\Filesystem
  * @see \Illuminate\Filesystem\Filesystem
  */
-class Filesystem
+class Filesystem implements FilesystemContract
 {
     //
 }
