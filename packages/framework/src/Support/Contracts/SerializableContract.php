6004db86a908c2f7a577784bad53b15232e82dd8
Caen De Silva <caen@desilva.se>
2022-11-26 20:54:05 +0100

    Inline inheritDoc tags

--- packages/framework/src/Support/Contracts/SerializableContract.php
+++ packages/framework/src/Support/Contracts/SerializableContract.php
@@ -10,12 +10,27 @@ use JsonSerializable;
 
 interface SerializableContract extends Arrayable, Jsonable, JsonSerializable
 {
-    /** @inheritDoc */
+    /**
+     * Get the instance as an array.
+     *
+     * @return array<TKey, TValue>
+     */
     public function toArray(): array;
 
-    /** @inheritDoc */
+    /**
+     * Convert the object to its JSON representation.
+     *
+     * @param  int  $options
+     * @return string
+     */
     public function toJson($options = 0): string;
 
-    /** @inheritDoc */
+    /**
+     * Specify data which should be serialized to JSON
+     * @link https://php.net/manual/en/jsonserializable.jsonserialize.php
+     * @return mixed data which can be serialized by <b>json_encode</b>,
+     * which is a value of any type other than a resource.
+     * @since 5.4
+     */
     public function jsonSerialize(): mixed;
 }
