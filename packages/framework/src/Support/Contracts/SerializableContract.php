dd2d234350137d46acd544c73dcd4d26a7adb45f
Caen De Silva <caen@desilva.se>
2022-11-26 20:52:54 +0100

    Add inheritDoc tags

--- packages/framework/src/Support/Contracts/SerializableContract.php
+++ packages/framework/src/Support/Contracts/SerializableContract.php
@@ -10,9 +10,12 @@ use JsonSerializable;
 
 interface SerializableContract extends Arrayable, Jsonable, JsonSerializable
 {
+    /** @inheritDoc */
     public function toArray(): array;
 
+    /** @inheritDoc */
     public function toJson($options = 0): string;
 
+    /** @inheritDoc */
     public function jsonSerialize(): mixed;
 }
