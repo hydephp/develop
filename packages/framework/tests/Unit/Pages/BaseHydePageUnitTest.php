9e360e509086761ec51f0fcf19a9114e8de2585b
Caen De Silva <caen@desilva.se>
2022-11-27 12:26:01 +0100

    Extract interface

--- packages/framework/tests/Unit/Pages/BaseHydePageUnitTest.php
+++ packages/framework/tests/Unit/Pages/BaseHydePageUnitTest.php
@@ -13,7 +13,7 @@ use Hyde\Testing\TestCase;
  *
  * @coversNothing
  */
-abstract class BaseHydePageUnitTest extends TestCase
+abstract class BaseHydePageUnitTest extends TestCase implements BaseHydePageUnitTestMethods
 {
     /**
      * @var class-string<\Hyde\Pages\Concerns\HydePage>|HydePage
@@ -211,4 +211,38 @@ class PendingExpectation
     {
         $this->test->addExpectation($this->property, $expected);
     }
+}
+
+interface BaseHydePageUnitTestMethods
+{
+    public function testPath();
+    public function testGetBladeView();
+    public function testSourcePath();
+    public function testFiles();
+    public function testNavigationMenuLabel();
+    public function testGetOutputPath();
+    public function testGet();
+    public function testOutputDirectory();
+    public function testParse();
+    public function testNavigationMenuGroup();
+    public function testNavigationMenuPriority();
+    public function testGetRouteKey();
+    public function testHtmlTitle();
+    public function testAll();
+    public function testMetadata();
+    public function test__construct();
+    public function testMake();
+    public function testGetRoute();
+    public function testShowInNavigation();
+    public function testGetSourcePath();
+    public function testGetLink();
+    public function testGetIdentifier();
+    public function testHas();
+    public function testToCoreDataObject();
+    public function testConstructFactoryData();
+    public function testFileExtension();
+    public function testSourceDirectory();
+    public function testCompile();
+    public function testMatter();
+    public function testOutputPath();
 }
