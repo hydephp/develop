4142c8ca8453fc3f813849740c731009270eeb54
Caen De Silva <caen@desilva.se>
2022-11-27 11:44:06 +0100

    Create BaseHydePageUnitTest.php

---
+++ packages/framework/tests/Unit/Pages/BaseHydePageUnitTest.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hyde\Framework\Testing\Unit\Pages;
+
+use Hyde\Testing\TestCase;
+
+/**
+ * This extendable base text class provides dynamic unit testing for the specified page class.
+ */
+abstract class BaseHydePageUnitTest extends TestCase
+{
+    //
+}
