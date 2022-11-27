0ce043aa7e801762ba61ba453939d2af33b08e32
Caen De Silva <caen@desilva.se>
2022-11-27 11:51:26 +0100

    Create BladePageUnitTest.php

---
+++ packages/framework/tests/Unit/Pages/BladePageUnitTest.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hyde\Framework\Testing\Unit\Pages;
+
+require_once __DIR__ . '/BaseHydePageUnitTest.php';
+
+/**
+ * @covers \Hyde\Pages\BladePage
+ */
+class BladePageUnitTest extends BaseHydePageUnitTest
+{
+    //
+}
