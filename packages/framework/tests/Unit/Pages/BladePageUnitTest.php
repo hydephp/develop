5b5d4cb649c2068dff3c2af70e0b9ca40806cadf
Caen De Silva <caen@desilva.se>
2022-11-27 13:01:28 +0100

    Update test to work for BladePages

--- packages/framework/tests/Unit/Pages/BladePageUnitTest.php
+++ packages/framework/tests/Unit/Pages/BladePageUnitTest.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Hyde\Framework\Testing\Unit\Pages;
 
-use Hyde\Framework\Testing\Feature\TestPage;
 use Hyde\Hyde;
 use Hyde\Pages\BladePage;
 use Hyde\Pages\DocumentationPage;
@@ -21,119 +20,102 @@ class BladePageUnitTest extends TestCase
     public function testSourceDirectory()
     {
         $this->assertSame(
-            'source',
-            TestPage::sourceDirectory()
+            '_pages',
+            BladePage::sourceDirectory()
         );
     }
 
     public function testOutputDirectory()
     {
         $this->assertSame(
-            'output',
-            TestPage::outputDirectory()
+            '',
+            BladePage::outputDirectory()
         );
     }
 
     public function testFileExtension()
     {
         $this->assertSame(
-            '.md',
-            TestPage::fileExtension()
+            '.blade.php',
+            BladePage::fileExtension()
         );
     }
 
     public function testSourcePath()
     {
         $this->assertSame(
-            'source/hello-world.md',
-            TestPage::sourcePath('hello-world')
+            '_pages/hello-world.blade.php',
+            BladePage::sourcePath('hello-world')
         );
     }
 
     public function testOutputPath()
     {
         $this->assertSame(
-            'output/hello-world.html',
-            TestPage::outputPath('hello-world')
+            'hello-world.html',
+            BladePage::outputPath('hello-world')
         );
     }
 
     public function testPath()
     {
         $this->assertSame(
-            Hyde::path('source/hello-world'),
-            TestPage::path('hello-world')
+            Hyde::path('_pages/hello-world.blade.php'),
+            BladePage::path('hello-world.blade.php')
         );
     }
 
     public function testGetSourcePath()
     {
         $this->assertSame(
-            'source/hello-world.md',
-            (new TestPage('hello-world'))->getSourcePath()
+            '_pages/hello-world.blade.php',
+            (new BladePage('hello-world'))->getSourcePath()
         );
     }
 
     public function testGetOutputPath()
     {
         $this->assertSame(
-            'output/hello-world.html',
-            (new TestPage('hello-world'))->getOutputPath()
+            'hello-world.html',
+            (new BladePage('hello-world'))->getOutputPath()
         );
     }
 
     public function testGetLink()
     {
         $this->assertSame(
-            'output/hello-world.html',
-            (new TestPage('hello-world'))->getLink()
+            'hello-world.html',
+            (new BladePage('hello-world'))->getLink()
         );
     }
 
     public function testMake()
     {
-        $this->assertEquals(TestPage::make(), new TestPage());
+        $this->assertEquals(BladePage::make(), new BladePage());
 
         $this->assertEquals(
-            TestPage::make('foo', ['foo' => 'bar']),
-            new TestPage('foo', ['foo' => 'bar'])
+            BladePage::make('foo', ['foo' => 'bar']),
+            new BladePage('foo', ['foo' => 'bar'])
         );
     }
 
     public function testShowInNavigation()
     {
         $this->assertTrue((new BladePage('foo'))->showInNavigation());
-        $this->assertTrue((new MarkdownPage())->showInNavigation());
-        $this->assertTrue((new DocumentationPage())->showInNavigation());
-        $this->assertFalse((new MarkdownPost())->showInNavigation());
-        $this->assertTrue((new HtmlPage())->showInNavigation());
     }
 
     public function testNavigationMenuPriority()
     {
         $this->assertSame(999, (new BladePage('foo'))->navigationMenuPriority());
-        $this->assertSame(999, (new MarkdownPage())->navigationMenuPriority());
-        $this->assertSame(999, (new DocumentationPage())->navigationMenuPriority());
-        $this->assertSame(10, (new MarkdownPost())->navigationMenuPriority());
-        $this->assertSame(999, (new HtmlPage())->navigationMenuPriority());
     }
 
     public function testNavigationMenuLabel()
     {
         $this->assertSame('Foo', (new BladePage('foo'))->navigationMenuLabel());
-        $this->assertSame('Foo', (new MarkdownPage('foo'))->navigationMenuLabel());
-        $this->assertSame('Foo', (new MarkdownPost('foo'))->navigationMenuLabel());
-        $this->assertSame('Foo', (new DocumentationPage('foo'))->navigationMenuLabel());
-        $this->assertSame('Foo', (new HtmlPage('foo'))->navigationMenuLabel());
     }
 
     public function testNavigationMenuGroup()
     {
         $this->assertNull((new BladePage('foo'))->navigationMenuGroup());
-        $this->assertNull((new MarkdownPage())->navigationMenuGroup());
-        $this->assertNull((new MarkdownPost())->navigationMenuGroup());
-        $this->assertNull((new HtmlPage())->navigationMenuGroup());
-        $this->assertSame('other', (new DocumentationPage())->navigationMenuGroup());
-        $this->assertSame('foo', DocumentationPage::make(matter: ['navigation' => ['group' => 'foo']])->navigationMenuGroup());
     }
 }
