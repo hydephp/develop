51e3f6a4ac756302a9243d9554ad89d8dafaca35
Caen De Silva <caen@desilva.se>
2022-11-27 12:18:59 +0100

    Add expectation stubs

--- packages/framework/tests/Unit/Pages/BladePageUnitTest.php
+++ packages/framework/tests/Unit/Pages/BladePageUnitTest.php
@@ -22,5 +22,121 @@ class BladePageUnitTest extends BaseHydePageUnitTest
         parent::setUp();
 
         $this->expect('path')->toReturn(Hyde::path('_pages'));
+
+        $this->expect('getBladeView')->toReturn(
+            //
+        );
+
+        $this->expect('sourcePath')->toReturn(
+            //
+        );
+
+        $this->expect('files')->toReturn(
+            //
+        );
+
+        $this->expect('navigationMenuLabel')->toReturn(
+            //
+        );
+
+        $this->expect('getOutputPath')->toReturn(
+            //
+        );
+
+        $this->expect('get')->toReturn(
+            //
+        );
+
+        $this->expect('outputDirectory')->toReturn(
+            //
+        );
+
+        $this->expect('parse')->toReturn(
+            //
+        );
+
+        $this->expect('navigationMenuGroup')->toReturn(
+            //
+        );
+
+        $this->expect('navigationMenuPriority')->toReturn(
+            //
+        );
+
+        $this->expect('getRouteKey')->toReturn(
+            //
+        );
+
+        $this->expect('htmlTitle')->toReturn(
+            //
+        );
+
+        $this->expect('all')->toReturn(
+            //
+        );
+
+        $this->expect('metadata')->toReturn(
+            //
+        );
+
+        $this->expect('__construct')->toReturn(
+            //
+        );
+
+        $this->expect('make')->toReturn(
+            //
+        );
+
+        $this->expect('getRoute')->toReturn(
+            //
+        );
+
+        $this->expect('showInNavigation')->toReturn(
+            //
+        );
+
+        $this->expect('getSourcePath')->toReturn(
+            //
+        );
+
+        $this->expect('getLink')->toReturn(
+            //
+        );
+
+        $this->expect('getIdentifier')->toReturn(
+            //
+        );
+
+        $this->expect('has')->toReturn(
+            //
+        );
+
+        $this->expect('toCoreDataObject')->toReturn(
+            //
+        );
+
+        $this->expect('constructFactoryData')->toReturn(
+            //
+        );
+
+        $this->expect('fileExtension')->toReturn(
+            //
+        );
+
+        $this->expect('sourceDirectory')->toReturn(
+            //
+        );
+
+        $this->expect('compile')->toReturn(
+            //
+        );
+
+        $this->expect('matter')->toReturn(
+            //
+        );
+
+        $this->expect('outputPath')->toReturn(
+            //
+        );
     }
 }
