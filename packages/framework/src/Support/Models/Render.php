f1fc28aa1b8cda3dcc0b41f2c4935c12c6c74540
Caen De Silva <caen@desilva.se>
2022-12-01 22:52:15 +0100

    Scaffold base public API

--- packages/framework/src/Support/Models/Render.php
+++ packages/framework/src/Support/Models/Render.php
@@ -18,4 +18,43 @@ class Render
     protected static HydePage $page;
     protected static Route $currentRoute;
     protected static string $currentPage;
+
+    protected static array $data = [];
+
+    public static function setPage(HydePage $page): void
+    {
+        static::$page = $page;
+        static::$currentRoute = $page->getRoute();
+        static::$currentPage = $page->getRouteKey();
+    }
+
+    public static function getPage(): HydePage
+    {
+        return static::$page;
+    }
+
+    public static function getCurrentRoute(): Route
+    {
+        return static::$currentRoute;
+    }
+
+    public static function getCurrentPage(): string
+    {
+        return static::$currentPage;
+    }
+
+    public static function share(string $key, mixed $value): void
+    {
+        static::$data[$key] = $value;
+    }
+
+    public static function shared(string $key, mixed $default = null): mixed
+    {
+        return static::$data[$key] ?? $default;
+    }
+
+    public static function has(string $key): bool
+    {
+        return isset(static::$data[$key]);
+    }
 }
