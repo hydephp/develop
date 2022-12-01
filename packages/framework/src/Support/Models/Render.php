d441e7908a20cf70582db024d32a4e3bb9133a0a
Caen De Silva <caen@desilva.se>
2022-12-01 22:41:46 +0100

    Create RenderHelperTest.php
    Update Render.php

--- packages/framework/src/Support/Models/Render.php
+++ packages/framework/src/Support/Models/Render.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace Hyde\Support\Models;
 
+/**
+ * @see \Hyde\Framework\Testing\Feature\RenderHelperTest
+ */
 class Render
 {
     //
---
+++ packages/framework/tests/Feature/RenderHelperTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Hyde\Framework\Testing\Feature;
+
+use Hyde\Support\Models\Render;
+use Hyde\Testing\TestCase;
+
+/**
+ * @covers \Hyde\Support\Models\Render
+ */
+class RenderHelperTest extends TestCase
+{
+    //
+}
