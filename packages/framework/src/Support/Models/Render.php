a674a2f6f2e9cfd392fed1551b5febcc38d594a0
Caen De Silva <caen@desilva.se>
2022-12-01 22:44:44 +0100

    Add class documentation

--- packages/framework/src/Support/Models/Render.php
+++ packages/framework/src/Support/Models/Render.php
@@ -5,6 +5,10 @@ declare(strict_types=1);
 namespace Hyde\Support\Models;
 
 /**
+ * Contains data for the current page being rendered/compiled.
+ *
+ * All public data here will be available in the Blade views through @see ManagesViewData::shareViewData().
+ *
  * @see \Hyde\Framework\Testing\Feature\RenderHelperTest
  */
 class Render
