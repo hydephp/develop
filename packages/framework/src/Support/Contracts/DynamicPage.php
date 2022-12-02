7b24b74918c14935524fb130450a5550b014b17c
Caen De Silva <caen@desilva.se>
2022-12-02 20:51:42 +0100

    Add interface PHPDoc

--- packages/framework/src/Support/Contracts/DynamicPage.php
+++ packages/framework/src/Support/Contracts/DynamicPage.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace Hyde\Support\Contracts;
 
+/**
+ * This interface is used to mark page classes that are dynamically generated (i.e. not based on a source file).
+ */
 interface DynamicPage
 {
     //
