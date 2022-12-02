f753e54dc16a5f0226a7c70fbaa8b8f1ac7a821a
Caen De Silva <caen@desilva.se>
2022-12-02 18:03:22 +0100

    Revert "Revert "Add method annotations to facade""
    This reverts commit 1ab90eef857112f851441e1b360c8fb6a399c89e.

--- packages/framework/src/Support/Facades/Render.php
+++ packages/framework/src/Support/Facades/Render.php
@@ -4,12 +4,22 @@ declare(strict_types=1);
 
 namespace Hyde\Support\Facades;
 
+use Hyde\Pages\Concerns\HydePage;
+use Hyde\Support\Models\Route;
 use Illuminate\Support\Facades\Facade;
 
 /**
  * Manages data for the current page being rendered/compiled.
  *
  * @see \Hyde\Support\Models\Render
+ *
+ * @method static void setPage(HydePage $page)
+ * @method static HydePage getPage()
+ * @method static Route getCurrentRoute()
+ * @method static string getCurrentPage()
+ * @method static void share(string $key, mixed $value)
+ * @method static mixed shared(string $key, mixed $default = null)
+ * @method static bool has(string $key)
  */
 class Render extends Facade
 {
