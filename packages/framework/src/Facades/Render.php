75ce78dcd9b3502c5ee0915ad29d3aa233664e91
Caen De Silva <caen@desilva.se>
2022-12-02 16:23:24 +0100

    Configure facade

--- packages/framework/src/Facades/Render.php
+++ packages/framework/src/Facades/Render.php
@@ -4,12 +4,17 @@ declare(strict_types=1);
 
 namespace Hyde\Facades;
 
+use Illuminate\Support\Facades\Facade;
+
 /**
  * Manages data for the current page being rendered/compiled.
  *
  * @see \Hyde\Support\Models\Render
  */
-class Render
+class Render extends Facade
 {
-    //
+    protected static function getFacadeAccessor(): string
+    {
+        return \Hyde\Support\Models\Render::class;
+    }
 }
