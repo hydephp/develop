38f179e2d1b3967b7ac3949e0c0329d981d35db0
Caen De Silva <caen@desilva.se>
2022-12-02 16:50:24 +0100

    Test null handling

--- packages/framework/tests/Feature/RenderHelperTest.php
+++ packages/framework/tests/Feature/RenderHelperTest.php
@@ -16,18 +16,24 @@ class RenderHelperTest extends TestCase
 {
     public function testSetAndGetPage()
     {
+        $this->assertNull(Render::getPage());
+
         Render::setPage($page = new MarkdownPage());
         $this->assertSame($page, Render::getPage());
     }
 
     public function testGetCurrentRoute()
     {
+        $this->assertNull(Render::getCurrentRoute());
+
         Render::setPage($page = new MarkdownPage());
         $this->assertEquals($page->getRoute(), Render::getCurrentRoute());
     }
 
     public function testGetCurrentPage()
     {
+        $this->assertNull(Render::getCurrentPage());
+
         Render::setPage($page = new MarkdownPage());
         $this->assertSame($page->getRouteKey(), Render::getCurrentPage());
     }
