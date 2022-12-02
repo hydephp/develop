ca3ebeab0c5d14ba7a2a6e992356fc0c5673286f
Caen De Silva <caen@desilva.se>
2022-12-02 11:44:35 +0100

    Implement the test

--- packages/framework/tests/Feature/RenderHelperTest.php
+++ packages/framework/tests/Feature/RenderHelperTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Hyde\Framework\Testing\Feature;
 
+use Hyde\Pages\MarkdownPage;
 use Hyde\Support\Models\Render;
 use Hyde\Testing\TestCase;
 
@@ -12,5 +13,39 @@ use Hyde\Testing\TestCase;
  */
 class RenderHelperTest extends TestCase
 {
-    //
+    public function testSetAndGetPage()
+    {
+        Render::setPage($page = new MarkdownPage());
+        $this->assertSame($page, Render::getPage());
+    }
+
+    public function testGetCurrentRoute()
+    {
+        Render::setPage($page = new MarkdownPage());
+        $this->assertEquals($page->getRoute(), Render::getCurrentRoute());
+    }
+
+    public function testGetCurrentPage()
+    {
+        Render::setPage($page = new MarkdownPage());
+        $this->assertSame($page->getRouteKey(), Render::getCurrentPage());
+    }
+
+    public function testShareAndShared()
+    {
+        Render::share('foo', 'bar');
+        $this->assertEquals('bar', Render::shared('foo'));
+    }
+
+    public function testSharedWithDefault()
+    {
+        $this->assertEquals('bar', Render::shared('foo', 'bar'));
+    }
+
+    public function testHas()
+    {
+        Render::share('foo', 'bar');
+        $this->assertTrue(Render::has('foo'));
+        $this->assertFalse(Render::has('bar'));
+    }
 }
