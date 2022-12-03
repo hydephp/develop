2f98b8d618c7f47148c241a656c0d9a71f3c54cd
Caen De Silva <caen@desilva.se>
2022-12-03 22:26:35 +0100

    Use the infoComment helper

--- packages/framework/src/Console/Commands/MakePublicationTagCommand.php
+++ packages/framework/src/Console/Commands/MakePublicationTagCommand.php
@@ -58,7 +58,7 @@ class MakePublicationTagCommand extends ValidatingCommand implements CommandHand
     {
         if ($this->argument('tagName')) {
             $value = $this->argument('tagName');
-            $this->line("<info>Using tag name</info> [<comment>$value</comment>] <info>from command line argument</info>");
+            $this->infoComment('Using tag name', $value, 'from command line argument');
             $this->newLine();
 
             return $value;
