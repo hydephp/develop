d50b7041d227d48b5aadc8be73efc33bd7b12cac
Caen De Silva <caen@desilva.se>
2022-12-03 17:47:34 +0100

    Update DynamicPage interface documentation to extend usage
    Extends its usage to publication pages

--- packages/framework/src/Support/Contracts/DynamicPage.php
+++ packages/framework/src/Support/Contracts/DynamicPage.php
@@ -5,7 +5,8 @@ declare(strict_types=1);
 namespace Hyde\Support\Contracts;
 
 /**
- * This interface is used to mark page classes that are dynamically generated (i.e. not based on a source file).
+ * This interface is used to mark page classes that are dynamically generated (i.e. not based on a source file),
+ * or that has dynamic path information like publication pages.
  */
 interface DynamicPage
 {
