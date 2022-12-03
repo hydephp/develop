15925537f932e6ab121736fddd61eec71761ffd0
Caen De Silva <caen@desilva.se>
2022-12-03 14:59:17 +0100

    Scaffold class

--- packages/framework/src/Framework/Actions/PublicationPageCompiler.php
+++ packages/framework/src/Framework/Actions/PublicationPageCompiler.php
@@ -4,10 +4,23 @@ declare(strict_types=1);
 
 namespace Hyde\Framework\Actions;
 
+use Hyde\Framework\Features\Publications\Models\PublicationListPage;
+use Hyde\Pages\PublicationPage;
+
 /**
  * @see \Hyde\Framework\Testing\Feature\Actions\PublicationPageCompilerTest
  */
 class PublicationPageCompiler
 {
-    //
+    protected PublicationPage|PublicationListPage $page;
+
+    public function __construct(PublicationPage|PublicationListPage $page)
+    {
+        $this->page = $page;
+    }
+
+    public function __invoke(): string
+    {
+        // TODO: Implement __invoke() method.
+    }
 }
