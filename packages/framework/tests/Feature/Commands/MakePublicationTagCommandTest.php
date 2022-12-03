c78dbdfb8cc731be18790bfc56f4d38e68c470e8
Caen De Silva <caen@desilva.se>
2022-12-03 20:34:07 +0100

    Implement base test

--- packages/framework/tests/Feature/Commands/MakePublicationTagCommandTest.php
+++ packages/framework/tests/Feature/Commands/MakePublicationTagCommandTest.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Hyde\Framework\Testing\Feature\Commands;
 
+use Hyde\Console\Commands\MakePublicationTagCommand;
+use Hyde\Hyde;
 use Hyde\Testing\TestCase;
 
 /**
@@ -11,5 +13,23 @@ use Hyde\Testing\TestCase;
  */
 class MakePublicationTagCommandTest extends TestCase
 {
-    //
+    public function testCanCreateNewPublicationType()
+    {
+        MakePublicationTagCommand::mockInput('foo
+bar
+baz
+');
+
+        $this->artisan('make:publicationTag')
+             ->expectsQuestion('Tag name', 'foo')
+             ->assertExitCode(0);
+
+        $this->assertFileExists(Hyde::path('tags.json'));
+        $this->assertSame(
+            json_encode(['foo' => ['foo', 'bar', 'baz']], 128),
+            file_get_contents(Hyde::path('tags.json'))
+        );
+
+        unlink(Hyde::path('tags.json'));
+    }
 }
