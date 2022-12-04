9020096adc6e579b580a3efe36e5e3a7930ae164
Caen De Silva <caen@desilva.se>
2022-12-04 19:09:01 +0100

    Remove number from expectation causing false negatives

--- packages/framework/tests/Feature/ValidatingCommandTest.php
+++ packages/framework/tests/Feature/ValidatingCommandTest.php
@@ -28,7 +28,7 @@ class ValidatingCommandTest extends TestCase
         $command = new SafeThrowingValidatingTestCommand();
         $output = Mockery::mock(OutputStyle::class);
         $output->shouldReceive('writeln')->once()->withArgs(function (string $message) {
-            return str_starts_with($message, '<error>Error: This is a test at '.__FILE__.':1');
+            return str_starts_with($message, '<error>Error: This is a test at '.__FILE__.':');
         });
         $command->setOutput($output);
 
