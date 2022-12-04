3cd987c1360f7b728b02c71966d8e9fb93d13da3
Caen De Silva <caen@desilva.se>
2022-12-04 11:22:36 +0100

    Add class description

--- packages/framework/src/Framework/Concerns/InvokableAction.php
+++ packages/framework/src/Framework/Concerns/InvokableAction.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace Hyde\Framework\Concerns;
 
+/**
+ * Base class for invokable actions. Provides a helper to invoke the action statically.
+ */
 abstract class InvokableAction
 {
     abstract public function __invoke();
