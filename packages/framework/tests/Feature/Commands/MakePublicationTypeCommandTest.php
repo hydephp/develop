f08d1075ac9def7f3f5e5e2cabe56f98b6874603
Caen De Silva <caen@desilva.se>
2022-12-04 22:11:24 +0100

    Expect more outputs

--- packages/framework/tests/Feature/Commands/MakePublicationTypeCommandTest.php
+++ packages/framework/tests/Feature/Commands/MakePublicationTypeCommandTest.php
@@ -98,13 +98,19 @@ class MakePublicationTypeCommandTest extends TestCase
     public function test_cannot_create_field_with_lower_max_than_min_value()
     {
         $this->artisan('make:publicationType test-publication')
-            ->expectsQuestion('Field name', 'foo')
-            ->expectsQuestion('Field type', 'foo')
-            ->expectsQuestion('Min value (for strings, this refers to string length)', 10)
-            ->expectsQuestion('Max value (for strings, this refers to string length)', 5)
-            ->expectsQuestion('Min value (for strings, this refers to string length)', 5)
-            ->expectsQuestion('Max value (for strings, this refers to string length)', 10)
-
+             ->expectsQuestion('Field name', 'foo')
+             ->expectsQuestion('Field type', 'foo')
+             ->expectsQuestion('Min value (for strings, this refers to string length)', 10)
+             ->expectsQuestion('Max value (for strings, this refers to string length)', 5)
+             ->expectsQuestion('Min value (for strings, this refers to string length)', 5)
+             ->expectsQuestion('Max value (for strings, this refers to string length)', 10)
+             ->expectsQuestion('<bg=magenta;fg=white>Add another field (y/n)</>', 'n')
+             ->expectsQuestion('Choose the default field you wish to sort by', 'foo')
+             ->expectsQuestion('Choose the default sort direction', 'Ascending (oldest items first if sorting by dateCreated)')
+                ->expectsQuestion('Enter the pageSize (0 for no limit)', 10)
+                ->expectsQuestion('Generate previous/next links in detail view (y/n)', 'n')
+                ->expectsQuestion('Choose a canonical name field (the values of this field have to be unique!)', 'foo')
+                ->expectsOutputToContain('Creating a new Publication Type!')
              ->assertSuccessful();
     }
 }
