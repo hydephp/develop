4c81d0ea81ac4edd4c9dabc05b612b14443e3b93
Caen De Silva <caen@desilva.se>
2022-12-05 14:45:30 +0100

    Create PublicationFieldTypesEnumTest.php
    Update PublicationFieldTypes.php

--- packages/framework/src/Framework/Features/Publications/Concerns/PublicationFieldTypes.php
+++ packages/framework/src/Framework/Features/Publications/Concerns/PublicationFieldTypes.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace Hyde\Framework\Features\Publications\Concerns;
 
+/**
+ * @see \Hyde\Framework\Testing\Feature\PublicationFieldTypesEnumTest
+ */
 enum PublicationFieldTypes
 {
     //
---
+++ packages/framework/tests/Feature/PublicationFieldTypesEnumTest.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hyde\Framework\Testing\Feature;
+
+use Hyde\Framework\Features\Publications\Concerns\PublicationFieldTypes;
+use Hyde\Testing\TestCase;
+
+/**
+ * @covers \Hyde\Framework\Features\Publications\Concerns\PublicationFieldTypes
+ */
+class PublicationFieldTypesEnumTest extends TestCase
+{
+    //
+}
