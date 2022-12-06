beef0278df07f85811f0bc0e538b9e391fd347ec
Caen De Silva <caen@desilva.se>
2022-12-06 12:53:59 +0100

    Create ValidatePublicationTypeCommandTest.php

---
+++ packages/framework/tests/Feature/Commands/ValidatePublicationTypeCommandTest.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hyde\Framework\Testing\Feature\Commands;
+
+use Hyde\Console\Commands\ValidatePublicationTypeCommand;
+use Hyde\Testing\TestCase;
+
+/**
+ * @covers \Hyde\Console\Commands\ValidatePublicationTypeCommand
+ */
+class ValidatePublicationTypeCommandTest extends TestCase
+{
+    //
+}
