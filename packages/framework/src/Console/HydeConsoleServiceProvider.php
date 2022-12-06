619a23bd6acb8db35eca312739d84e3750b833af
Caen De Silva <caen@desilva.se>
2022-12-06 12:55:30 +0100

    Register the command

--- packages/framework/src/Console/HydeConsoleServiceProvider.php
+++ packages/framework/src/Console/HydeConsoleServiceProvider.php
@@ -39,6 +39,8 @@ class HydeConsoleServiceProvider extends ServiceProvider
                 Commands\ValidateCommand::class,
                 Commands\ServeCommand::class,
                 Commands\DebugCommand::class,
+
+                Commands\ValidatePublicationTypeCommand::class,
             ]
         );
     }
