5e21948168bf23ff1d7f98b293396beada91fd59
Caen De Silva <caen@desilva.se>
2022-12-07 15:04:43 +0100

    Create SeedPublicationCommandTest.php

---
+++ packages/framework/tests/Feature/Commands/SeedPublicationCommandTest.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hyde\Framework\Testing\Feature\Commands;
+
+use Hyde\Testing\TestCase;
+
+/**
+ * @covers \Hyde\Console\Commands\SeedPublicationCommand
+ */
+class SeedPublicationCommandTest extends TestCase
+{
+   //
+}
