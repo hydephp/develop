fce12e4b3949e5d409211c14a811029f961f9726
Caen De Silva <caen@desilva.se>
2022-12-07 16:09:23 +0100

    Test the string type contents

--- packages/framework/tests/Feature/Actions/SeedsPublicationFilesTest.php
+++ packages/framework/tests/Feature/Actions/SeedsPublicationFilesTest.php
@@ -9,6 +9,9 @@ use Hyde\Framework\Features\Publications\Models\PublicationType;
 use Hyde\Hyde;
 use Hyde\Testing\TestCase;
 
+use function explode;
+use function file_get_contents;
+
 /**
  * @covers \Hyde\Framework\Actions\SeedsPublicationFiles
  */
@@ -30,6 +33,22 @@ class SeedsPublicationFilesTest extends TestCase
         $this->assertFileExists($this->getPublicationFiles()[0]);
     }
 
+    public function testCreateWithStringType()
+    {
+        $action = new SeedsPublicationFiles(PublicationType::get('test-publication'));
+        $action->create();
+
+        $lines = explode("\n", file_get_contents($this->getPublicationFiles()[0]));
+
+        $this->assertSame('---', $lines[0]);
+        $this->assertStringStartsWith('__createdAt: ', $lines[1]);
+        $this->assertStringStartsWith('title: ', $lines[2]);
+        $this->assertSame('---', $lines[3]);
+        $this->assertSame('', $lines[4]);
+        $this->assertSame('## Write something awesome.', $lines[5]);
+        $this->assertSame('', $lines[6]);
+        $this->assertSame('', $lines[7]);
+    }
     protected function getPublicationFiles(): array
     {
         return glob(Hyde::path('test-publication/*.md'));
