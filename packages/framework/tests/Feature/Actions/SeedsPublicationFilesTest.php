a14ed255bef4d89051b602370ed90f75e63d2d75
Caen De Silva <caen@desilva.se>
2022-12-07 15:04:04 +0100

    Create SeedsPublicationFilesTest.php

---
+++ packages/framework/tests/Feature/Actions/SeedsPublicationFilesTest.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hyde\Framework\Testing\Feature\Actions;
+
+use Hyde\Testing\TestCase;
+
+/**
+ * @covers \Hyde\Framework\Actions\SeedsPublicationFiles
+ */
+class SeedsPublicationFilesTest extends TestCase
+{
+    //
+}
