0d5f2dda3699a5ab938029de7525bed4a21e8646
Caen De Silva <caen@desilva.se>
2022-12-07 21:44:26 +0100

    Implement the base test

--- packages/framework/tests/Feature/Commands/SeedPublicationCommandTest.php
+++ packages/framework/tests/Feature/Commands/SeedPublicationCommandTest.php
@@ -4,12 +4,34 @@ declare(strict_types=1);
 
 namespace Hyde\Framework\Testing\Feature\Commands;
 
+use Hyde\Framework\Features\Publications\Models\PublicationType;
 use Hyde\Testing\TestCase;
+
+use function config;
 
 /**
  * @covers \Hyde\Console\Commands\SeedPublicationCommand
  */
 class SeedPublicationCommandTest extends TestCase
 {
-    //
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->directory('test-publication');
+        $this->setupTestPublication();
+        $this->pubType = PublicationType::get('test-publication');
+
+        config(['app.throw_on_console_exception' => true]);
+    }
+
+    public function test_can_seed_publications()
+    {
+        $this->artisan('seed:publications')
+            ->expectsOutputToContain('Seeding new publications!')
+            ->expectsQuestion('Which publication type would you like to seed?', 'test-publication')
+            ->expectsQuestion('How many publications would you like to generate', 1)
+            ->expectsOutputToContain('1 publications for Test Publication created!')
+            ->assertExitCode(0);
+    }
 }
