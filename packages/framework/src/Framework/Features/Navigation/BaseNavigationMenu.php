eab141a91fc51f76f176f9ee4ef8afc8e2a69ef3
Caen De Silva <caen@desilva.se>
2022-12-08 20:04:42 +0100

    Extract base class BaseNavigationMenu

---
+++ packages/framework/src/Framework/Features/Navigation/BaseNavigationMenu.php
@@ -0,0 +1,83 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hyde\Framework\Features\Navigation;
+
+use function collect;
+use function config;
+use Hyde\Foundation\Facades\Router;
+use Hyde\Pages\DocumentationPage;
+use Hyde\Support\Models\Route;
+use Illuminate\Support\Collection;
+
+/**
+ * @see \Hyde\Framework\Testing\Feature\NavigationMenuTest
+ * @phpstan-consistent-constructor
+ */
+class BaseNavigationMenu
+{
+    public Collection $items;
+
+    public function __construct()
+    {
+        $this->items = new Collection();
+    }
+
+    public static function create(): static
+    {
+        return (new static())->generate()->filter()->sort();
+    }
+
+    /** @return $this */
+    public function generate(): static
+    {
+        Router::each(function (Route $route): void {
+            $this->items->push(NavItem::fromRoute($route));
+        });
+
+        collect(config('hyde.navigation.custom', []))->each(function (NavItem $item): void {
+            $this->items->push($item);
+        });
+
+        return $this;
+    }
+
+    /** @return $this */
+    public function filter(): static
+    {
+        $this->items = $this->filterHiddenItems();
+        $this->items = $this->filterDuplicateItems();
+
+        return $this;
+    }
+
+    /** @return $this */
+    public function sort(): static
+    {
+        $this->items = $this->items->sortBy('priority')->values();
+
+        return $this;
+    }
+
+    protected function filterHiddenItems(): Collection
+    {
+        return $this->items->reject(function (NavItem $item): bool {
+            return $item->hidden || $this->filterDocumentationPage($item);
+        })->values();
+    }
+
+    protected function filterDuplicateItems(): Collection
+    {
+        return $this->items->unique(function (NavItem $item): string {
+            return $item->resolveLink();
+        });
+    }
+
+    protected function filterDocumentationPage(NavItem $item): bool
+    {
+        return isset($item->route)
+            && $item->route->getPage() instanceof DocumentationPage
+            && $item->route->getRouteKey() !== 'docs/index';
+    }
+}
