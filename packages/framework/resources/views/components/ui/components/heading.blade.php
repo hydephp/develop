9050d86f2e946615196df202dbddedb47d2fd18e
Caen De Silva <caen@desilva.se>
2022-12-09 21:19:08 +0100

    Allow align property to be set on the heading

--- packages/framework/resources/views/components/ui/components/heading.blade.php
+++ packages/framework/resources/views/components/ui/components/heading.blade.php
@@ -1,1 +1,1 @@
-<h1 {{ $attributes->class(['text-center text-3xl font-bold']) }}>{{ $slot }}</h1>
+<h1 {{ $attributes->class(['text-'.($attributes['align'] ?? 'center').' text-3xl font-bold']) }}>{{ $slot }}</h1>
