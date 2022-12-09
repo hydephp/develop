5ed34575837fed498792beed13d3a691c28c8c94
Caen De Silva <caen@desilva.se>
2022-12-09 20:26:26 +0100

    Test the stripIndentation method

--- packages/framework/tests/Feature/MarkdownServiceTest.php
+++ packages/framework/tests/Feature/MarkdownServiceTest.php
@@ -220,6 +220,52 @@ class MarkdownServiceTest extends TestCase
         $this->assertFalse($service->canEnableTorchlight());
     }
 
+    public function test_stripIndentation_method_with_unindented_markdown()
+    {
+        $service = $this->makeService();
+
+        $markdown = "foo\nbar\nbaz";
+        $this->assertSame($markdown, $service->stripIndentation($markdown));
+    }
+
+    public function test_stripIndentation_method_with_indented_markdown()
+    {
+        $service = $this->makeService();
+
+        $markdown = "foo\n  bar\n  baz";
+        $this->assertSame("foo\nbar\nbaz", $service->stripIndentation($markdown));
+
+        $markdown = "  foo\n  bar\n  baz";
+        $this->assertSame("foo\nbar\nbaz", $service->stripIndentation($markdown));
+
+        $markdown = "    foo\n    bar\n    baz";
+        $this->assertSame("foo\nbar\nbaz", $service->stripIndentation($markdown));
+    }
+
+    public function test_stripIndentation_method_with_tab_indented_markdown()
+    {
+        $service = $this->makeService();
+
+        $markdown = "foo\n\tbar\n\tbaz";
+        $this->assertSame("foo\nbar\nbaz", $service->stripIndentation($markdown));
+    }
+
+    public function test_stripIndentation_method_with_carriage_return_line_feed()
+    {
+        $service = $this->makeService();
+
+        $markdown = "foo\r\n  bar\r\n  baz";
+        $this->assertSame("foo\nbar\nbaz", $service->stripIndentation($markdown));
+    }
+
+    public function test_stripIndentation_method_with_code_indentation()
+    {
+        $service = $this->makeService();
+
+        $markdown = "foo\n    bar\n        baz";
+        $this->assertSame("foo\nbar\n    baz", $service->stripIndentation($markdown));
+    }
+
     protected function makeService()
     {
         return new class extends MarkdownService
