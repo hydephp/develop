d753c6c5c676be878e0351dfd12b25b8e626fe6c
Caen De Silva <caen@desilva.se>
2022-12-09 12:01:28 +0100

    Implement the unit test

--- packages/framework/tests/Unit/DropdownNavItemTest.php
+++ packages/framework/tests/Unit/DropdownNavItemTest.php
@@ -12,5 +12,19 @@ use Hyde\Testing\TestCase;
  */
 class DropdownNavItemTest extends TestCase
 {
-    //
+    public function testConstruct()
+    {
+        $item = new DropdownNavItem('foo', []);
+
+        $this->assertSame('foo', $item->name);
+        $this->assertSame([], $item->items);
+    }
+
+    public function testFromArray()
+    {
+        $item = DropdownNavItem::fromArray('foo', []);
+
+        $this->assertSame('foo', $item->name);
+        $this->assertSame([], $item->items);
+    }
 }
