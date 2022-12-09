63e34bd6b5608d53888d32ba82bd50a14269619e
Caen De Silva <caen@desilva.se>
2022-12-09 11:56:54 +0100

    Create DropdownNavItemTest.php

---
+++ packages/framework/tests/Unit/DropdownNavItemTest.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hyde\Framework\Testing\Unit;
+
+use Hyde\Framework\Features\Navigation\DropdownNavItem;
+use Hyde\Testing\TestCase;
+
+/**
+ * @covers \Hyde\Framework\Features\Navigation\DropdownNavItem
+ */
+class DropdownNavItemTest extends TestCase
+{
+    //
+}
