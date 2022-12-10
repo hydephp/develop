da7b2599722bfaddf03c339028e6be01df442f6f
Caen De Silva <caen@desilva.se>
2022-12-10 19:46:58 +0100

    Extract inline CSS

--- packages/ui-kit/docs/layout-demos/focus.blade.php
+++ packages/ui-kit/docs/layout-demos/focus.blade.php
@@ -1,5 +1,16 @@
+<style>
+    #contentArea {
+        width: 100%;
+        height: 100%;
+        min-height: calc(100vh - 8rem - 66px - 62px);
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        background: #80808080;
+    }
+</style>
 <x-hyde-ui::layouts.focus>
-    <div style="width: 100%; height: 100%; min-height: calc(100vh - 8rem - 66px - 62px); display: flex; align-items: center; justify-content: center; background: #80808080;">
+    <div id="contentArea">
         <x-hyde-ui::heading>
             Content Area
         </x-hyde-ui::heading>
