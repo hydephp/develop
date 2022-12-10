4ac974fd05c623b1c48f9d30b4cf35f87cf41705
Caen De Silva <caen@desilva.se>
2022-12-10 18:22:08 +0100

    Remove redundant PHPDoc comments

--- packages/ui-kit/src/UIKitServiceProvider.php
+++ packages/ui-kit/src/UIKitServiceProvider.php
@@ -8,21 +8,11 @@ use Illuminate\Support\ServiceProvider;
 
 class UIKitServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
     public function register(): void
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
     public function boot(): void
     {
         $this->loadViewsFrom(__DIR__.'/../resources/views', 'hyde-ui');
