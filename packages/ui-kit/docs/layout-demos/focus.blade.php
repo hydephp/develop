e65a7fada08824d5d91d5098a0081895f5c7971e
Caen De Silva <caen@desilva.se>
2022-12-10 19:44:05 +0100

    Create focus.blade.php

---
+++ packages/ui-kit/docs/layout-demos/focus.blade.php
@@ -0,0 +1,7 @@
+<x-hyde-ui::layouts.focus>
+    <div style="width: 100%; height: 100%; min-height: calc(100vh - 8rem - 66px - 62px); display: flex; align-items: center; justify-content: center; background: #80808080;">
+        <x-hyde-ui::heading>
+            Content Area
+        </x-hyde-ui::heading>
+    </div>
+</x-hyde-ui::layouts.focus>
