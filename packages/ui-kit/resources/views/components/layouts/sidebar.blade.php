d34a14b12bed466c240ff3a30b55936825ecffc8
Caen De Silva <caen@desilva.se>
2022-12-10 20:16:58 +0100

    Create sidebar.blade.php

---
+++ packages/ui-kit/resources/views/components/layouts/sidebar.blade.php
@@ -0,0 +1,14 @@
+@extends('hyde::layouts.app')
+@section('content')
+
+    <div class="mx-auto max-w-7xl py-8 px-4 flex">
+        <main id="content" class="py-8 px-4 flex-grow">
+            {{ $slot }}
+        </main>
+
+        <aside id="sidebar" class="py-8 px-4 w-80">
+            {{ $aside }}
+        </aside>
+    </div>
+
+@endsection
