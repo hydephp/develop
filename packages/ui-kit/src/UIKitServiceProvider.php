b9ea63d4a41d7b053146b89b2f5fa4d0a7940ea6
Caen De Silva <caen@desilva.se>
2022-12-10 12:42:22 +0100

    Register the view paths

--- packages/ui-kit/src/UIKitServiceProvider.php
+++ packages/ui-kit/src/UIKitServiceProvider.php
@@ -25,6 +25,6 @@ class UIKitServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        $this->loadViewsFrom(__DIR__.'/../resources/views', 'hyde-ui');
     }
 }
