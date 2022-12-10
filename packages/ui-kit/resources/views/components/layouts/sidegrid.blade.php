cd8f41aa50f060b56305141ac10753a207f84973
Caen De Silva <caen@desilva.se>
2022-12-10 22:30:29 +0100

    Support attributes and classes to be passed to layout

--- packages/ui-kit/resources/views/components/layouts/sidegrid.blade.php
+++ packages/ui-kit/resources/views/components/layouts/sidegrid.blade.php
@@ -2,11 +2,11 @@
 @section('content')
 
     <div class="mx-auto max-w-7xl py-8 px-4 flex">
-        <main id="content" class="py-8 px-4 flex-grow">
+        <main id="content" {{ $attributes->class(['py-8 px-4 flex-grow']) }}>
             {{ $slot }}
         </main>
 
-        <aside id="sidegrid" class="py-8 px-4 w-80">
+        <aside id="sidegrid" {{ $aside->attributes->class(['py-8 px-4 w-80']) }}>
             {{ $aside }}
         </aside>
     </div>
