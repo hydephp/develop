b5a6df6b0e549a69255a9a680fa6ca667f710c0e
Caen De Silva <caen@desilva.se>
2022-12-10 19:58:21 +0100

    Add a faded bottom to indicate that the area expands downwards

--- packages/ui-kit/docs/layout-demos/focus.blade.php
+++ packages/ui-kit/docs/layout-demos/focus.blade.php
@@ -1,11 +1,23 @@
 <style>
     #contentArea {
         width: 100%;
-        height: calc(100vh - 8rem - 66px - 62px);
+        height: calc(100vh - 8rem - 66px - 62px - 4rem);
         display: flex;
         align-items: center;
         justify-content: center;
         background: #80808080;
+        position: relative;
+    }
+    #contentArea::after {
+        content: "";
+        z-index: 1;
+        bottom: -4rem;
+        position: absolute;
+        background-image: linear-gradient(to bottom,
+            rgba(128, 128, 128, 0.5),
+            rgba(128, 128, 128, 0));
+        width: 100%;
+        height: 4rem;
     }
 </style>
 <x-hyde-ui::layouts.focus>
