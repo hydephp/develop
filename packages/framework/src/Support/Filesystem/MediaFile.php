9253d7f1d6946d4ad5bd8d27b15ca61a0d570fa2
Caen De Silva <caen@desilva.se>
2022-12-12 17:58:07 +0100

    Add getContentLength method

--- packages/framework/src/Support/Filesystem/MediaFile.php
+++ packages/framework/src/Support/Filesystem/MediaFile.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Hyde\Support\Filesystem;
 
+use function filesize;
+
 /**
  * File abstraction for a project media file.
  */
@@ -15,5 +17,10 @@ class MediaFile extends ProjectFile
             'length' => $this->getContentLength(),
             'mimeType' => $this->getMimeType(),
         ]);
+    }
+
+    public function getContentLength(): int
+    {
+        return filesize($this->path);
     }
 }
