3af1dcc1ad58460c1b00058fe1e1316203827123
Caen De Silva <caen@desilva.se>
2022-12-12 18:04:33 +0100

    Add withoutDirectoryPrefix method

--- packages/framework/src/Support/Filesystem/SourceFile.php
+++ packages/framework/src/Support/Filesystem/SourceFile.php
@@ -4,6 +4,10 @@ declare(strict_types=1);
 
 namespace Hyde\Support\Filesystem;
 
+use function basename;
+use function strlen;
+use function substr;
+
 /**
  * File abstraction for a project source file.
  *
@@ -23,5 +27,11 @@ class SourceFile extends ProjectFile
         return array_merge(parent::toArray(), [
             'model' => $this->model,
         ]);
+    }
+
+    public function withoutDirectoryPrefix(): string
+    {
+        // Works like basename, but keeps subdirectory names.
+        return substr($this->__toString(), strlen($this->model::$sourceDirectory) + 1);
     }
 }
