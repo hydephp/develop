cd42da3448a71b640b28a75044e23dc83dbc0e41
Caen De Silva <caen@desilva.se>
2022-12-13 10:56:43 +0100

    Create ProjectFileTest.php

---
+++ packages/framework/tests/Feature/Support/ProjectFileTest.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hyde\Framework\Testing\Feature\Support;
+
+use Hyde\Testing\TestCase;
+
+/**
+ * @covers \Hyde\Support\Filesystem\ProjectFile
+ */
+class ProjectFileTest extends TestCase
+{
+    //
+}
