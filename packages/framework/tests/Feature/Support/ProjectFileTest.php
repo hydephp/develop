d0dc6a2262658b25c51d0fb82f3fdb3f4f5f384b
Caen De Silva <caen@desilva.se>
2022-12-13 10:58:17 +0100

    Add test class

--- packages/framework/tests/Feature/Support/ProjectFileTest.php
+++ packages/framework/tests/Feature/Support/ProjectFileTest.php
@@ -4,12 +4,18 @@ declare(strict_types=1);
 
 namespace Hyde\Framework\Testing\Feature\Support;
 
+use Hyde\Support\Filesystem\ProjectFile;
 use Hyde\Testing\TestCase;
 
 /**
  * @covers \Hyde\Support\Filesystem\ProjectFile
  */
 class ProjectFileTest extends TestCase
+{
+    //
+}
+
+class ProjectFileTestClass extends ProjectFile
 {
     //
 }
