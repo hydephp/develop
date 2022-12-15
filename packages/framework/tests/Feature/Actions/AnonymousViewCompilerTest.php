4f6f9d5ed5841459f951790ad9a2a465a166dd38
Caen De Silva <caen@desilva.se>
2022-12-15 11:17:02 +0100

    Create AnonymousViewCompilerTest.php

---
+++ packages/framework/tests/Feature/Actions/AnonymousViewCompilerTest.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hyde\Framework\Testing\Feature\Actions;
+
+use Hyde\Framework\Actions\AnonymousViewCompiler;
+use Hyde\Testing\TestCase;
+
+/**
+ * @covers \Hyde\Framework\Actions\AnonymousViewCompiler
+ */
+class AnonymousViewCompilerTest extends TestCase
+{
+    //
+}
