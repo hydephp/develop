e423afa93285848cb5123451f473a28e01dbc718
Caen De Silva <caen@desilva.se>
2022-12-15 11:21:41 +0100

    Implement the __invoke method

--- packages/framework/src/Framework/Actions/AnonymousViewCompiler.php
+++ packages/framework/src/Framework/Actions/AnonymousViewCompiler.php
@@ -5,6 +5,13 @@ declare(strict_types=1);
 namespace Hyde\Framework\Actions;
 
 use Hyde\Framework\Concerns\InvokableAction;
+use Hyde\Hyde;
+use Illuminate\Support\Facades\Blade;
+use InvalidArgumentException;
+
+use function file_exists;
+use function file_get_contents;
+use function sprintf;
 
 
 /**
@@ -20,5 +27,17 @@ class AnonymousViewCompiler extends InvokableAction
     {
         $this->viewPath = $viewPath;
         $this->data = $data;
+    }
+
+    public function __invoke(): string
+    {
+        if (! file_exists(Hyde::path($this->viewPath))) {
+            throw new InvalidArgumentException(sprintf('View [%s] not found.', $this->viewPath));
+        }
+
+        return Blade::render(
+            file_get_contents(Hyde::path($this->viewPath)),
+            $this->data
+        );
     }
 }
