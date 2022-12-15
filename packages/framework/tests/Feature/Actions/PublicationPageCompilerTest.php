6f5b0f9f102f0c1991b786d67e86a81e6cfb1342
Caen De Silva <caen@desilva.se>
2022-12-15 14:20:11 +0100

    Add tests for compiling pages with registered namespaced view

--- packages/framework/tests/Feature/Actions/PublicationPageCompilerTest.php
+++ packages/framework/tests/Feature/Actions/PublicationPageCompilerTest.php
@@ -9,7 +9,16 @@ use Hyde\Framework\Exceptions\FileNotFoundException;
 use Hyde\Framework\Features\Publications\Models\PublicationType;
 use Hyde\Hyde;
 use Hyde\Pages\PublicationPage;
+use Hyde\Support\Facades\Render;
 use Hyde\Testing\TestCase;
+
+use Illuminate\Support\Facades\View;
+use Illuminate\Support\HtmlString;
+
+use function file_get_contents;
+use function file_put_contents;
+use function json_decode;
+use function json_encode;
 
 /**
  * @covers \Hyde\Framework\Actions\PublicationPageCompiler
@@ -39,6 +48,35 @@ class PublicationPageCompilerTest extends TestCase
         $string = PublicationPageCompiler::call(PublicationType::get('test-publication')->getListPage());
 
         $this->assertEquals('List: My Publication', $string);
+    }
+
+    public function test_can_compile_publication_pages_with_registered_namespaced_view()
+    {
+        $this->directory('test-publication');
+        $this->setupTestPublication();
+
+        $schema = json_decode(file_get_contents(Hyde::path('test-publication/schema.json')));
+        $schema->detailTemplate = 'hyde::layouts.publication';
+        file_put_contents(Hyde::path('test-publication/schema.json'), json_encode($schema));
+
+        $publicationPage = new PublicationPage('my-publication', type: PublicationType::get('test-publication'));
+        Render::setPage($publicationPage);
+        $this->assertStringContainsString('My Publication', PublicationPageCompiler::call($publicationPage));
+    }
+
+    public function test_can_compile_publication_list_pages_with_registered_namespaced_view()
+    {
+        $this->directory('test-publication');
+        $this->setupTestPublication();
+
+        $schema = json_decode(file_get_contents(Hyde::path('test-publication/schema.json')));
+        $schema->listTemplate = 'hyde::layouts.publication_list';
+        file_put_contents(Hyde::path('test-publication/schema.json'), json_encode($schema));
+        file_put_contents(Hyde::path('test-publication/my-publication.md'), 'Foo');
+
+        $publicationType = PublicationType::get('test-publication');
+        $publicationPage = $publicationType->getListPage();
+        $this->assertEquals("The default publication list template\n", PublicationPageCompiler::call($publicationPage));
     }
 
     public function test_with_missing_detail_blade_view()
