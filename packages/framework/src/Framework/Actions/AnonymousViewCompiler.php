49cc85696f82493b00f1b28a0b2d94724923ca06
Caen De Silva <caen@desilva.se>
2022-12-15 11:19:55 +0100

    Add class documentation

--- packages/framework/src/Framework/Actions/AnonymousViewCompiler.php
+++ packages/framework/src/Framework/Actions/AnonymousViewCompiler.php
@@ -6,6 +6,11 @@ namespace Hyde\Framework\Actions;
 
 use Hyde\Framework\Concerns\InvokableAction;
 
+
+/**
+ * Compile any Blade file using the Blade facade as it allows us to render
+ * it without having to register the directory with the view finder.
+ */
 class AnonymousViewCompiler extends InvokableAction
 {
     //
