fccbd74fed5b6c6c46c5540275111adf4100bec9
Caen De Silva <caen@desilva.se>
2022-12-16 09:17:13 +0100

    Implement the base test

--- packages/framework/tests/Unit/InputStreamHandlerTest.php
+++ packages/framework/tests/Unit/InputStreamHandlerTest.php
@@ -7,6 +7,8 @@ namespace Hyde\Framework\Testing\Unit;
 use Hyde\Console\Commands\Helpers\InputStreamHandler;
 use Hyde\Testing\TestCase;
 use Illuminate\Console\Command;
+use Illuminate\Console\OutputStyle;
+use Mockery;
 
 use function implode;
 
@@ -15,7 +17,19 @@ use function implode;
  */
 class InputStreamHandlerTest extends TestCase
 {
-    //
+    public function testCanCollectInput()
+    {
+        InputStreamHandler::mockInput('foo');
+
+        $command = new TestCommand ;
+
+        $output = Mockery::mock(OutputStyle::class);
+        $output->shouldReceive('writeln')->once()->withArgs(function (string $message) {
+            return $message === 'foo';
+        });
+        $command->setOutput($output);
+        $this->assertSame(0, $command->handle());
+    }
 }
 
 class TestCommand extends Command {
