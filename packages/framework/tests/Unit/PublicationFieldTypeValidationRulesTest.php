23e28b8ab392c6074b205309e3dcd78eb735b385
Caen De Silva <caen@desilva.se>
2022-12-19 20:39:47 +0100

    Create PublicationFieldTypeValidationRulesTest.php

---
+++ packages/framework/tests/Unit/PublicationFieldTypeValidationRulesTest.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hyde\Framework\Testing\Unit;
+
+use Hyde\Testing\TestCase;
+
+/**
+ * @covers \Hyde\Framework\Features\Publications\Models\PublicationFieldType
+ */
+class PublicationFieldTypeValidationRulesTest extends TestCase
+{
+    //
+}
