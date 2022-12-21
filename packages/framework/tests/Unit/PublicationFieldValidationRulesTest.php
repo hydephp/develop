353e66d5809eece9ea3e71166080a80f28d1b4c1
Caen De Silva <caen@desilva.se>
2022-12-21 17:04:46 +0100

    Remove range validation tests
    See https://github.com/hydephp/develop/pull/765#issuecomment-1361564784

--- packages/framework/tests/Unit/PublicationFieldValidationRulesTest.php
+++ packages/framework/tests/Unit/PublicationFieldValidationRulesTest.php
@@ -16,43 +16,31 @@ class PublicationFieldValidationRulesTest extends TestCase
 {
     public function testGetRulesForArray()
     {
-        $rules = (new PublicationField('array', 'myArray', '4', '8'))->getValidationRules();
+        $rules = (new PublicationField('array', 'myArray'))->getValidationRules();
         $this->assertSame(['array'], $rules->toArray());
     }
 
     public function testValidateArrayPasses()
     {
-        $validated = (new PublicationField('array', 'myArray', '4', '8'))->validate(['foo', 'bar', 'baz']);
+        $validated = (new PublicationField('array', 'myArray'))->validate(['foo', 'bar', 'baz']);
         $this->assertSame(['my-array' => ['foo', 'bar', 'baz']], $validated);
     }
 
     public function testValidateArrayFails()
     {
         $this->expectValidationException('The my-array must be an array.');
-        (new PublicationField('array', 'myArray', '4', '8'))->validate('foo');
+        (new PublicationField('array', 'myArray'))->validate('foo');
     }
 
     public function testGetRulesForDatetime()
     {
-        $rules = (new PublicationField('datetime', 'myDatetime', '2021-01-01', '2022-01-01'))->getValidationRules();
-        $this->assertSame(['date', 'after:2021-01-01 00:00:00', 'before:2022-01-01 00:00:00'], $rules->toArray());
-
-        $rules = (new PublicationField('datetime', 'myDatetime', '2021-01-01'))->getValidationRules();
-        $this->assertSame(['date', 'after:2021-01-01 00:00:00'], $rules->toArray());
-
-        $rules = (new PublicationField('datetime', 'myDatetime', null, '2022-01-01'))->getValidationRules();
-        $this->assertSame(['date', 'before:2022-01-01 00:00:00'], $rules->toArray());
+        $rules = (new PublicationField('datetime', 'myDatetime'))->getValidationRules();
+        $this->assertSame(['date'], $rules->toArray());
     }
 
     public function testValidateDatetimePasses()
     {
         $validated = (new PublicationField('datetime', 'myDatetime'))->validate('2021-01-01');
-        $this->assertSame(['my-datetime' => '2021-01-01'], $validated);
-
-        $validated = (new PublicationField('datetime', 'myDatetime', '2021-01-01'))->validate('2021-01-02');
-        $this->assertSame(['my-datetime' => '2021-01-02'], $validated);
-
-        $validated = (new PublicationField('datetime', 'myDatetime', null, '2021-01-02'))->validate('2021-01-01');
         $this->assertSame(['my-datetime' => '2021-01-01'], $validated);
     }
 
@@ -62,41 +50,28 @@ class PublicationFieldValidationRulesTest extends TestCase
         (new PublicationField('datetime', 'myDatetime'))->validate('string');
     }
 
-    public function testValidateDatetimeFailsForInvalidMinValue()
-    {
-        $this->expectValidationException('The my-datetime must be a date after 2021-01-01 00:00:00.');
-        (new PublicationField('datetime', 'myDatetime', '2021-01-01'))->validate('2020-12-31');
-    }
-
-    public function testValidateDatetimeFailsForInvalidMaxValue()
-    {
-        $this->expectValidationException('The my-datetime must be a date before 2021-01-02 00:00:00.');
-        (new PublicationField('datetime', 'myDatetime', null, '2021-01-02'))->validate('2021-01-03');
-    }
-
     public function testGetRulesForFloat()
     {
-        $rules = (new PublicationField('float', 'myFloat', '4', '8'))->getValidationRules();
-        $this->assertSame(['between:4,8'], $rules->toArray());
-        $this->assertSame(['numeric', 'between:4,8'], $rules->toArray());
+        $rules = (new PublicationField('float', 'myFloat'))->getValidationRules();
+        $this->assertSame(['numeric'], $rules->toArray());
     }
 
     public function testGetRulesForInteger()
     {
-        $rules = (new PublicationField('integer', 'myInteger', '4', '8'))->getValidationRules();
-        $this->assertSame(['between:4,8'], $rules->toArray());
+        $rules = (new PublicationField('integer', 'myInteger'))->getValidationRules();
+        $this->assertSame([], $rules->toArray());
     }
 
     public function testGetRulesForString()
     {
-        $rules = (new PublicationField('string', 'myString', '4', '8'))->getValidationRules();
-        $this->assertSame(['between:4,8'], $rules->toArray());
+        $rules = (new PublicationField('string', 'myString'))->getValidationRules();
+        $this->assertSame([], $rules->toArray());
     }
 
     public function testGetRulesForText()
     {
-        $rules = (new PublicationField('text', 'myText', '4', '8'))->getValidationRules();
-        $this->assertSame(['between:4,8'], $rules->toArray());
+        $rules = (new PublicationField('text', 'myText'))->getValidationRules();
+        $this->assertSame([], $rules->toArray());
     }
 
     public function testGetRulesForImage()
@@ -104,19 +79,19 @@ class PublicationFieldValidationRulesTest extends TestCase
         $this->directory('_media/foo');
         $this->file('_media/foo/bar.jpg');
         $this->file('_media/foo/baz.png');
-        $rules = (new PublicationField('image', 'myImage', '4', '8', publicationType: new PublicationType('foo')))->getValidationRules();
+        $rules = (new PublicationField('image', 'myImage', publicationType: new PublicationType('foo')))->getValidationRules();
         $this->assertSame(['in:_media/foo/bar.jpg,_media/foo/baz.png'], $rules->toArray());
     }
 
     public function testGetRulesForTag()
     {
-        $rules = (new PublicationField('tag', 'myTag', '4', '8', 'foo'))->getValidationRules();
+        $rules = (new PublicationField('tag', 'myTag', tagGroup: 'foo'))->getValidationRules();
         $this->assertSame(['in:'], $rules->toArray());
     }
 
     public function testGetRulesForUrl()
     {
-        $rules = (new PublicationField('url', 'myUrl', '4', '8'))->getValidationRules();
+        $rules = (new PublicationField('url', 'myUrl'))->getValidationRules();
         $this->assertSame(['url'], $rules->toArray());
     }
 
