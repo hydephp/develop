551ee4b47aa71b298632066e7b6a5a9a4d643236
Caen De Silva <caen@desilva.se>
2022-12-22 20:46:04 +0100

    Get tags via publication type

--- packages/framework/src/Framework/Actions/SeedsPublicationFiles.php
+++ packages/framework/src/Framework/Actions/SeedsPublicationFiles.php
@@ -98,7 +98,7 @@ class SeedsPublicationFiles
             'image' => 'https://picsum.photos/id/'.rand(1, 1000).'/400/400',
             'integer' => rand(-100000, 100000),
             'string' => substr($this->fakeSentence(10), 0, rand(0, 255)),
-            'tag' => $this->getTags($field),
+            'tag' => $this->getTags(),
             'text' => $this->getTextValue(rand(3, 20)),
             'url' => $this->fakeUrl(),
         };
@@ -128,9 +128,9 @@ class SeedsPublicationFiles
         return $arrayItems;
     }
 
-    protected function getTags(PublicationField $field): string
+    protected function getTags(): string
     {
-        $tags = PublicationService::getValuesForTagName($field->tagGroup, false);
+        $tags = PublicationService::getValuesForTagName($this->pubType->name, false);
 
         return $tags->isEmpty() ? '' : $tags->random();
     }
