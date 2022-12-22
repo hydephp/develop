ff820a8cd84ade4b3609bbf405e34333a5f5c7aa
Caen De Silva <caen@desilva.se>
2022-12-22 11:43:29 +0100

    Use built in push helper instead of foreach loop

--- packages/framework/src/Framework/Features/Publications/Models/PublicationField.php
+++ packages/framework/src/Framework/Features/Publications/Models/PublicationField.php
@@ -65,9 +65,7 @@ class PublicationField implements SerializableContract
         // Here we could check for a "strict" mode type of thing and add 'required' to the rules if we wanted to.
 
         // Apply any field rules.
-        foreach ($this->rules as $rule) {
-            $fieldRules->add($rule);
-        }
+        $fieldRules->push(...$this->rules);
 
         // Apply any dynamic rules.
         switch ($this->type->value) {
