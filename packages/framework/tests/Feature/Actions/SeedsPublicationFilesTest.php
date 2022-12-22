6a3a66c7849d27b157f24a9530d3678ec4b92cd8
Caen De Silva <caen@desilva.se>
2022-12-22 20:50:39 +0100

    Use the publication type's name as the tagGroup

--- packages/framework/tests/Feature/Actions/SeedsPublicationFilesTest.php
+++ packages/framework/tests/Feature/Actions/SeedsPublicationFilesTest.php
@@ -131,7 +131,7 @@ class SeedsPublicationFilesTest extends TestCase
 
     public function testWithTagType()
     {
-        $tags = ['foo' => ['foo', 'bar', 'baz']];
+        $tags = ['Test Publication' => ['foo', 'bar', 'baz']];
         $this->file('tags.json', json_encode($tags));
         $this->updateSchema('tag', 'tag');
         (new SeedsPublicationFiles($this->pubType))->create();
@@ -141,7 +141,7 @@ class SeedsPublicationFilesTest extends TestCase
         $this->assertBaseline($publication);
         $this->assertNotEmpty($publication->matter('tag'));
         $this->assertIsString($publication->matter('tag'));
-        $this->assertTrue(in_array($publication->matter('tag'), $tags['foo']));
+        $this->assertTrue(in_array($publication->matter('tag'), $tags['Test Publication']));
     }
 
     public function testWithTextType()
