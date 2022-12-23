29a8a2e01e27c0f5535f2e45274f5fe237fdef03
Caen De Silva <caen@desilva.se>
2022-12-23 10:35:32 +0100

    Update rector.php

--- rector.php
+++ rector.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
+use Rector\Php80\Rector\Class_\ClassPropertyAssignToConstructorPromotionRector;
 use Rector\Set\ValueObject\SetList;
 use RectorLaravel\Set\LaravelSetList;
 
@@ -12,5 +13,9 @@ return static function (RectorConfig $rectorConfig): void {
         LaravelSetList::LARAVEL_90,
         SetList::PHP_80,
         SetList::PHP_81,
+    ]);
+
+    $rectorConfig->skip([
+        ClassPropertyAssignToConstructorPromotionRector::class,
     ]);
 };
