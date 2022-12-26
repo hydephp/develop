090f2cde471072ee0051bb91c8298ab8bf5e982e
Caen De Silva <caen@desilva.se>
2022-12-26 17:48:40 +0100

    Inline local variable chain

--- packages/framework/src/Framework/Actions/CreatesNewPublicationPage.php
+++ packages/framework/src/Framework/Actions/CreatesNewPublicationPage.php
@@ -31,11 +31,7 @@ class CreatesNewPublicationPage extends CreateAction implements CreateActionCont
         protected bool $force = false,
         protected ?OutputStyle $output = null,
     ) {
-        $canonicalFieldName = $this->pubType->canonicalField;
-        $canonicalValue = $this->getCanonicalValue($canonicalFieldName);
-        $canonicalStr = Str::of($canonicalValue)->substr(0, 64);
-
-        $fileName = $this->formatStringForStorage($canonicalStr->slug()->toString());
+        $fileName = $this->formatStringForStorage(Str::of($this->getCanonicalValue($this->pubType->canonicalField))->substr(0, 64)->slug()->toString());
         $directory = $this->pubType->getDirectory();
         $this->outputPath = "$directory/$fileName.md";
     }
