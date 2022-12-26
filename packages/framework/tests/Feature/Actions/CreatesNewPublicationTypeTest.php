24a8e83bc6116c665aa2f93a9a6edd48cff91f9e
Caen De Silva <caen@desilva.se>
2022-12-26 22:28:42 +0100

    Split comma-separated values into multiple lines

--- packages/framework/tests/Feature/Actions/CreatesNewPublicationTypeTest.php
+++ packages/framework/tests/Feature/Actions/CreatesNewPublicationTypeTest.php
@@ -26,7 +26,15 @@ class CreatesNewPublicationTypeTest extends TestCase
 
     public function test_it_creates_a_new_publication_type()
     {
-        $creator = new CreatesNewPublicationType('Test Publication', new Collection(), 'canonical', 'sort', true, true, 10);
+        $creator = new CreatesNewPublicationType(
+            'Test Publication',
+            new Collection(),
+            'canonical',
+            'sort',
+            true,
+            true,
+            10
+        );
         $creator->create();
 
         $this->assertFileExists(Hyde::path('test-publication/schema.json'));
