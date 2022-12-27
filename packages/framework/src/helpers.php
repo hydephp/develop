c75353f377d38b8a46bc0c3a4601575d32e9f12c
Caen De Silva <caen@desilva.se>
2022-12-27 21:23:28 +0100

    Enter all global helper functions into the global namespace

--- packages/framework/src/helpers.php
+++ packages/framework/src/helpers.php
@@ -2,24 +2,26 @@
 
 declare(strict_types=1);
 
-use Hyde\Foundation\HydeKernel;
+namespace {
+    use Hyde\Foundation\HydeKernel;
 
-if (! function_exists('hyde')) {
-    /**
-     * Get the available HydeKernel instance.
-     */
-    function hyde(): HydeKernel
-    {
-        return app(HydeKernel::class);
+    if (! function_exists('hyde')) {
+        /**
+         * Get the available HydeKernel instance.
+         */
+        function hyde(): HydeKernel
+        {
+            return app(HydeKernel::class);
+        }
     }
-}
 
-if (! function_exists('unslash')) {
-    /**
-     * Remove trailing slashes from the start and end of a string.
-     */
-    function unslash(string $string): string
-    {
-        return trim($string, '/\\');
+    if (! function_exists('unslash')) {
+        /**
+         * Remove trailing slashes from the start and end of a string.
+         */
+        function unslash(string $string): string
+        {
+            return trim($string, '/\\');
+        }
     }
 }
