b6da32e196522d34b8459c2621f3a489e6f19316
Caen De Silva <caen@desilva.se>
2022-12-27 17:09:43 +0100

    Remove resolved todo

--- packages/framework/src/Framework/Actions/PublicationPageCompiler.php
+++ packages/framework/src/Framework/Actions/PublicationPageCompiler.php
@@ -12,8 +12,6 @@ use Illuminate\Support\Facades\View;
 use function str_ends_with;
 
 /**
- * @todo Consider changing to check if template key ends with .blade.php and using that to signify if it's an anonymous view.
- *
  * @see \Hyde\Framework\Testing\Feature\Actions\PublicationPageCompilerTest
  */
 class PublicationPageCompiler extends InvokableAction
