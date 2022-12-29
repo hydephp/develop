3fe1deff53462dc372f7b6b4dad00e53258dc7a3
Caen De Silva <caen@desilva.se>
2022-12-29 20:44:29 +0100

    Make class abstract

--- packages/framework/src/Framework/Features/Publications/Models/PublicationFieldValues/PublicationFieldValue.php
+++ packages/framework/src/Framework/Features/Publications/Models/PublicationFieldValues/PublicationFieldValue.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Hyde\Framework\Features\Publications\Models\PublicationFieldValues;
 
-class PublicationFieldValue
+abstract class PublicationFieldValue
 {
     //
 }
