9dc49e09682012fd3f4f1469164ad922bb5d8a41
Caen De Silva <caen@desilva.se>
2022-12-29 21:26:50 +0100

    Test should extend base test

--- packages/framework/tests/Unit/PublicationFieldValueTests/StringFieldValueTest.php
+++ packages/framework/tests/Unit/PublicationFieldValueTests/StringFieldValueTest.php
@@ -6,44 +6,14 @@ namespace Hyde\Framework\Testing\Unit\PublicationFieldValueTests;
 
 use Hyde\Framework\Features\Publications\Models\PublicationFieldValues\PublicationFieldValue;
 use Hyde\Framework\Features\Publications\Models\PublicationFieldValues\StringField;
-use Hyde\Framework\Features\Publications\PublicationFieldTypes;
-use Hyde\Testing\TestCase;
+
+require_once __DIR__.'/BaseFieldValueTest.php';
 
 /**
  * @covers \Hyde\Framework\Features\Publications\Models\PublicationFieldValues\StringField
  */
-class StringFieldValueTest extends TestCase
+class StringFieldValueTest extends BaseFieldValueTest
 {
     /** @var class-string|\Hyde\Framework\Features\Publications\Models\PublicationFieldValues\PublicationFieldValue */
     protected static string|PublicationFieldValue $fieldClass = StringField::class;
-
-    public function testConstruct()
-    {
-        $value = new static::$fieldClass('foo');
-        $this->assertSame('foo', $value->getValue());
-    }
-
-    public function testGetValue()
-    {
-        $value = new static::$fieldClass('foo');
-        $this->assertSame('foo', $value->getValue());
-    }
-
-    public function testGetType()
-    {
-        $this->assertSame(static::$fieldClass::TYPE, static::$fieldClass::getType());
-        $this->assertSame(PublicationFieldTypes::String, static::$fieldClass::getType());
-    }
-
-    public function testParseInput()
-    {
-        $value = static::$fieldClass::parseInput('foo');
-        $this->assertSame('foo', $value);
-    }
-
-    public function testToYamlType()
-    {
-        $value = static::$fieldClass::toYamlType('foo');
-        $this->assertSame('foo', $value);
-    }
 }
