4eea146e11d5681650894324fa0b53049cd9abba
Caen De Silva <caen@desilva.se>
2022-12-29 21:15:28 +0100

    Use the String type

--- packages/framework/src/Framework/Features/Publications/Models/PublicationFieldValues/StringField.php
+++ packages/framework/src/Framework/Features/Publications/Models/PublicationFieldValues/StringField.php
@@ -4,8 +4,12 @@ declare(strict_types=1);
 
 namespace Hyde\Framework\Features\Publications\Models\PublicationFieldValues;
 
+use Hyde\Framework\Features\Publications\PublicationFieldTypes;
+
 final class StringField extends PublicationFieldValue
 {
+    public const TYPE = PublicationFieldTypes::String;
+
     public static function parseInput(string $input): string
     {
         return $input;
