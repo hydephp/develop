893e2dfe38e641b9a5db4ea2ebe299a6f2b598a7
Caen De Silva <caen@desilva.se>
2022-12-29 16:46:29 +0100

    Revert "Merge newline into output"
    This reverts commit 817afbeba77a158df2fcdb45326a775f2b13f059.

--- packages/framework/src/Console/Commands/MakePublicationCommand.php
+++ packages/framework/src/Console/Commands/MakePublicationCommand.php
@@ -225,7 +225,8 @@ class MakePublicationCommand extends ValidatingCommand
             throw new InvalidArgumentException("Unable to create publication: $message");
         }
 
-        $this->warn("\n <fg=red>Warning:</> $message");
+        $this->newLine();
+        $this->warn(" <fg=red>Warning:</> $message");
         if ($this->confirm('Would you like to skip this field?', true)) {
             return null;
         } else {
