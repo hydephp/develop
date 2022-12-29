9b7267eafdb33ac79f4927b747afe3a0d1c7d239
Caen De Silva <caen@desilva.se>
2022-12-29 20:50:18 +0100

    Add abstract method stubs

--- packages/framework/src/Framework/Features/Publications/Models/PublicationFieldValues/PublicationFieldValue.php
+++ packages/framework/src/Framework/Features/Publications/Models/PublicationFieldValues/PublicationFieldValue.php
@@ -6,5 +6,19 @@ namespace Hyde\Framework\Features\Publications\Models\PublicationFieldValues;
 
 abstract class PublicationFieldValue
 {
-    //
+    /**
+     * Parse an input string from the command line into a value with the appropriate type for this field.
+     *
+     * @param  string  $input
+     * @return mixed
+     */
+    abstract public static function parseInput(string $input): mixed;
+
+    /**
+     * Return the value with the appropriate type for this field's YAML representation.
+     *
+     * @param  string  $input
+     * @return mixed
+     */
+    abstract public static function toYamlType(string $input): mixed;
 }
