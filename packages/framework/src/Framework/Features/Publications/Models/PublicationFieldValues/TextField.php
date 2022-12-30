d86b95645cb90da37e3fc34941ccaf716353ce6e
Caen De Silva <caen@desilva.se>
2022-12-30 15:31:23 +0100

    Trim the TextField input
    Ports normalization from the CreatesNewPublicationPage action

--- packages/framework/src/Framework/Features/Publications/Models/PublicationFieldValues/TextField.php
+++ packages/framework/src/Framework/Features/Publications/Models/PublicationFieldValues/TextField.php
@@ -6,12 +6,17 @@ namespace Hyde\Framework\Features\Publications\Models\PublicationFieldValues;
 
 use Hyde\Framework\Features\Publications\PublicationFieldTypes;
 
+use function trim;
+
 final class TextField extends PublicationFieldValue
 {
     public const TYPE = PublicationFieldTypes::Text;
 
     protected static function parseInput(string $input): string
     {
-        return $input;
+        // In order to properly store text fields as block literals,
+        // we need to make sure they end with a newline.
+
+        return trim($input)."\n";
     }
 }
