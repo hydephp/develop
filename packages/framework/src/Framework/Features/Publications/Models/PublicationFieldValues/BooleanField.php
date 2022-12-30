240003173aebdb4cda0b158f89b8d773d5981064
Caen De Silva <caen@desilva.se>
2022-12-30 10:54:53 +0100

    Refactor to use match expression

--- packages/framework/src/Framework/Features/Publications/Models/PublicationFieldValues/BooleanField.php
+++ packages/framework/src/Framework/Features/Publications/Models/PublicationFieldValues/BooleanField.php
@@ -8,25 +8,16 @@ use Hyde\Framework\Features\Publications\PublicationFieldTypes;
 
 use InvalidArgumentException;
 
-use function in_array;
-
 final class BooleanField extends PublicationFieldValue
 {
     public const TYPE = PublicationFieldTypes::Boolean;
 
     protected static function parseInput(string $input): bool
     {
-        $true = ['true', '1'];
-        $false = ['false', '0'];
-
-        if (in_array($input, $true)) {
-            return true;
-        }
-
-        if (in_array($input, $false)) {
-            return false;
-        }
-
-        throw new InvalidArgumentException("BooleanField: Unable to parse invalid boolean value '$input'");
+        return match ($input) {
+            'true', '1' => true,
+            'false', '0' => false,
+            default => throw new InvalidArgumentException("BooleanField: Unable to parse invalid boolean value '$input'")
+        };
     }
 }
