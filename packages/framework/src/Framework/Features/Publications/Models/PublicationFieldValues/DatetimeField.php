c1ca25eb1e1316d569a330a5ea852e3ed203be36
Caen De Silva <caen@desilva.se>
2022-12-30 22:47:37 +0100

    Update all canonicable types to implement the interface

--- packages/framework/src/Framework/Features/Publications/Models/PublicationFieldValues/DatetimeField.php
+++ packages/framework/src/Framework/Features/Publications/Models/PublicationFieldValues/DatetimeField.php
@@ -5,10 +5,13 @@ declare(strict_types=1);
 namespace Hyde\Framework\Features\Publications\Models\PublicationFieldValues;
 
 use DateTime;
+use Hyde\Framework\Features\Publications\Models\PublicationFieldValues\Contracts\Canonicable;
 use Hyde\Framework\Features\Publications\PublicationFieldTypes;
 
-final class DatetimeField extends PublicationFieldValue
+final class DatetimeField extends PublicationFieldValue implements Canonicable
 {
+    use CanonicableTrait;
+
     public const TYPE = PublicationFieldTypes::Datetime;
 
     protected static function parseInput(string $input): DateTime
