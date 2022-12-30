4cdbfc414eba5fad86e422ec835f8cf598f92bf2
Caen De Silva <caen@desilva.se>
2022-12-30 22:23:32 +0100

    StringField implements Stringable

--- packages/framework/src/Framework/Features/Publications/Models/PublicationFieldValues/StringField.php
+++ packages/framework/src/Framework/Features/Publications/Models/PublicationFieldValues/StringField.php
@@ -5,13 +5,19 @@ declare(strict_types=1);
 namespace Hyde\Framework\Features\Publications\Models\PublicationFieldValues;
 
 use Hyde\Framework\Features\Publications\PublicationFieldTypes;
+use Stringable;
 
-final class StringField extends PublicationFieldValue
+final class StringField extends PublicationFieldValue implements Stringable
 {
     public const TYPE = PublicationFieldTypes::String;
 
     protected static function parseInput(string $input): string
     {
         return $input;
+    }
+
+    public function __toString(): string
+    {
+        return $this->getValue();
     }
 }
