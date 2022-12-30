edf0d91cd36ac9da23ad9019f2bfa36f5c5866e1
Caen De Silva <caen@desilva.se>
2022-12-30 17:29:39 +0100

    Implement the rule

--- packages/framework/src/Framework/Features/Publications/Rules/BooleanRule.php
+++ packages/framework/src/Framework/Features/Publications/Rules/BooleanRule.php
@@ -6,6 +6,8 @@ namespace Hyde\Framework\Features\Publications\Rules;
 
 use Illuminate\Contracts\Validation\InvokableRule;
 
+use function in_array;
+
 class BooleanRule implements InvokableRule
 {
     /**
@@ -18,6 +20,10 @@ class BooleanRule implements InvokableRule
      */
     public function __invoke($attribute, $value, $fail)
     {
-        //
+        $acceptable = ['true', 'false', true, false, 0, 1, '0', '1'];
+
+        if (! in_array($value, $acceptable, true)) {
+            $fail('The :attribute must be true or false');
+        }
     }
 }
