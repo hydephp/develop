72a9ce3bbc30d31d7311ac12dffc8ac232c1fcaf
Caen De Silva <caen@desilva.se>
2022-12-31 12:01:04 +0100

    Use is_a instead of instanceof

--- packages/framework/src/Console/Commands/MakePublicationTypeCommand.php
+++ packages/framework/src/Console/Commands/MakePublicationTypeCommand.php
@@ -131,7 +131,7 @@ class MakePublicationTypeCommand extends ValidatingCommand
     protected function getCanonicalField(): PublicationField
     {
         $selectableFields = $this->fields->reject(function (PublicationField $field): bool {
-            return $field->type->fieldClass() instanceof Canonicable;
+            return ! is_a($field->type->fieldClass(), Canonicable::class, true);
         });
 
         if ($this->option('use-defaults')) {
