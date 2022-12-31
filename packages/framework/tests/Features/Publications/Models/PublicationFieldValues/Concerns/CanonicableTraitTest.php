cc445cccb008e5af66401c3750f63dcfbbb14263
Caen De Silva <caen@desilva.se>
2022-12-31 11:09:57 +0100

    Create CanonicableTraitTest.php

---
+++ packages/framework/tests/Features/Publications/Models/PublicationFieldValues/Concerns/CanonicableTraitTest.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hyde\Framework\Testing\Features\Publications\Models\PublicationFieldValues\Concerns;
+
+use Hyde\Framework\Features\Publications\Models\PublicationFieldValues\Concerns\CanonicableTrait;
+use Hyde\Testing\TestCase;
+
+/**
+ * @covers \Hyde\Framework\Features\Publications\Models\PublicationFieldValues\Concerns\CanonicableTrait
+ */
+class CanonicableTraitTest extends TestCase
+{
+    //
+}
