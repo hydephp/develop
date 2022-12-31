5d37d89064035693b9916268d464c8c0d50f3bd6
Caen De Silva <caen@desilva.se>
2022-12-31 11:11:29 +0100

    Create a test class to test the trait

--- packages/framework/tests/Features/Publications/Models/PublicationFieldValues/Concerns/CanonicableTraitTest.php
+++ packages/framework/tests/Features/Publications/Models/PublicationFieldValues/Concerns/CanonicableTraitTest.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 namespace Hyde\Framework\Testing\Features\Publications\Models\PublicationFieldValues\Concerns;
 
 use Hyde\Framework\Features\Publications\Models\PublicationFieldValues\Concerns\CanonicableTrait;
+use Hyde\Framework\Features\Publications\Models\PublicationFieldValues\Contracts\Canonicable;
+use Hyde\Framework\Features\Publications\Models\PublicationFieldValues\PublicationFieldValue;
 use Hyde\Testing\TestCase;
 
 /**
@@ -20,5 +22,15 @@ class CanonicableTraitTest extends TestCase
     public function test__toString()
     {
         //
+    }
+}
+
+class CanonicableTraitTestClass extends PublicationFieldValue implements Canonicable
+{
+    use CanonicableTrait;
+
+    protected static function parseInput(string $input): string
+    {
+        return $input;
     }
 }
