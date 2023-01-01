d61e55f4b84ea004ff9a77c71da5d0d7f157c834
Caen De Silva <caen@desilva.se>
2023-01-01 21:43:39 +0100

    Define methods to add

--- packages/framework/src/Framework/Features/Publications/PublicationFieldService.php
+++ packages/framework/src/Framework/Features/Publications/PublicationFieldService.php
@@ -4,7 +4,17 @@ declare(strict_types=1);
 
 namespace Hyde\Framework\Features\Publications;
 
+use Hyde\Framework\Features\Publications\Models\PublicationType;
+
 class PublicationFieldService
 {
-    //
+    public static function getDefaultValidationRulesForFieldType(PublicationFieldTypes $fieldType): array
+    {
+        //
+    }
+
+    public static function getValidationRulesForPublicationFieldEntry(PublicationType $publicationType, string $fieldName): array
+    {
+        //
+    }
 }
