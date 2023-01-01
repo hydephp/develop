2ace6ce3f57fdc96760ed6d7e6fde6f169732c2b
Caen De Silva <caen@desilva.se>
2023-01-01 22:04:53 +0100

    Link to the field classes

--- packages/framework/src/Framework/Features/Publications/PublicationFieldService.php
+++ packages/framework/src/Framework/Features/Publications/PublicationFieldService.php
@@ -6,6 +6,18 @@ namespace Hyde\Framework\Features\Publications;
 
 use Hyde\Framework\Features\Publications\Models\PublicationType;
 
+/**
+ * @see  \Hyde\Framework\Features\Publications\Models\PublicationFields\StringField
+ * @see  \Hyde\Framework\Features\Publications\Models\PublicationFields\DatetimeField
+ * @see  \Hyde\Framework\Features\Publications\Models\PublicationFields\BooleanField
+ * @see  \Hyde\Framework\Features\Publications\Models\PublicationFields\IntegerField
+ * @see  \Hyde\Framework\Features\Publications\Models\PublicationFields\FloatField
+ * @see  \Hyde\Framework\Features\Publications\Models\PublicationFields\ImageField
+ * @see  \Hyde\Framework\Features\Publications\Models\PublicationFields\ArrayField
+ * @see  \Hyde\Framework\Features\Publications\Models\PublicationFields\TextField
+ * @see  \Hyde\Framework\Features\Publications\Models\PublicationFields\UrlField
+ * @see  \Hyde\Framework\Features\Publications\Models\PublicationFields\TagField
+ */
 class PublicationFieldService
 {
     public static function getDefaultValidationRulesForFieldType(PublicationFieldTypes $fieldType): array
