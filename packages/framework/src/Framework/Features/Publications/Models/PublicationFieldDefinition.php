dc24625f86010fb908c137c1f5dff623187a25f8
Caen De Silva <caen@desilva.se>
2023-01-02 17:17:28 +0100

    Add publicationType parameter to constructor as optional

--- packages/framework/src/Framework/Features/Publications/Models/PublicationFieldDefinition.php
+++ packages/framework/src/Framework/Features/Publications/Models/PublicationFieldDefinition.php
@@ -37,11 +37,15 @@ class PublicationFieldDefinition implements SerializableContract
         return new static(...$array);
     }
 
-    public function __construct(PublicationFieldTypes|string $fieldType, string $name, array $rules = [])
+    public function __construct(PublicationFieldTypes|string $fieldType, string $name, array $rules = [], ?PublicationType $publicationType = null)
     {
         $this->fieldType = $fieldType instanceof PublicationFieldTypes ? $fieldType : PublicationFieldTypes::from(strtolower($fieldType));
         $this->name = str_starts_with($name, '__') ? $name : Str::kebab($name);
         $this->rules = $rules;
+
+        if ($publicationType !== null) {
+            $this->publicationType = $publicationType;
+        }
     }
 
     public function toArray(): array
