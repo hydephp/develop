86fd80fae352688deb937e3486f80e0e7871fdfd
Caen De Silva <caen@desilva.se>
2023-01-02 17:13:57 +0100

    Breaking: Rename named constructor parameter  to match renamed property

--- packages/framework/src/Framework/Features/Publications/Models/PublicationFieldDefinition.php
+++ packages/framework/src/Framework/Features/Publications/Models/PublicationFieldDefinition.php
@@ -37,9 +37,9 @@ class PublicationFieldDefinition implements SerializableContract
         return new static(...$array);
     }
 
-    public function __construct(PublicationFieldTypes|string $type, string $name, array $rules = [])
+    public function __construct(PublicationFieldTypes|string $fieldType, string $name, array $rules = [])
     {
-        $this->fieldType = $type instanceof PublicationFieldTypes ? $type : PublicationFieldTypes::from(strtolower($type));
+        $this->fieldType = $fieldType instanceof PublicationFieldTypes ? $fieldType : PublicationFieldTypes::from(strtolower($fieldType));
         $this->name = str_starts_with($name, '__') ? $name : Str::kebab($name);
         $this->rules = $rules;
     }
