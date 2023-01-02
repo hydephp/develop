e453406d7df9b9dc3965e26c02fc3b0739022af7
Caen De Silva <caen@desilva.se>
2023-01-02 16:17:02 +0100

    Revert "Make normalization helpers public to open up their usages"
    This reverts commit 47a5691e20419bee7c63158a3f645459fee84627.

--- packages/framework/src/Framework/Features/Publications/Models/PublicationFields/PublicationFieldValue.php
+++ packages/framework/src/Framework/Features/Publications/Models/PublicationFields/PublicationFieldValue.php
@@ -50,17 +50,17 @@ class PublicationFieldValue
         };
     }
 
-    public static function parseStringValue(string $value): string
+    protected static function parseStringValue(string $value): string
     {
         return $value;
     }
 
-    public static function parseDatetimeValue(string $value): DateTime
+    protected static function parseDatetimeValue(string $value): DateTime
     {
         return new DateTime($value);
     }
 
-    public static function parseBooleanValue(string $value): bool
+    protected static function parseBooleanValue(string $value): bool
     {
         return match ($value) {
             'true', '1' => true,
@@ -69,7 +69,7 @@ class PublicationFieldValue
         };
     }
 
-    public static function parseIntegerValue(string $value): int
+    protected static function parseIntegerValue(string $value): int
     {
         if (! is_numeric($value)) {
             throw self::parseError('integer', $value);
@@ -77,7 +77,7 @@ class PublicationFieldValue
         return (int) $value;
     }
 
-    public static function parseFloatValue(string $value): float
+    protected static function parseFloatValue(string $value): float
     {
         if (! is_numeric($value)) {
             throw self::parseError('float', $value);
@@ -85,18 +85,18 @@ class PublicationFieldValue
         return (float) $value;
     }
 
-    public static function parseImageValue(string $value): string
+    protected static function parseImageValue(string $value): string
     {
         // TODO Validate file exists as the dynamic validation rules does the same
         return $value;
     }
 
-    public static function parseArrayValue(string|array $value): array
+    protected static function parseArrayValue(string|array $value): array
     {
         return (array) $value;
     }
 
-    public static function parseTextValue(string $value): string
+    protected static function parseTextValue(string $value): string
     {
         // In order to properly store multi-line text fields as block literals,
         // we need to make sure the string ends with a newline character.
@@ -106,7 +106,7 @@ class PublicationFieldValue
         return $value;
     }
 
-    public static function parseUrlValue(string $value): string
+    protected static function parseUrlValue(string $value): string
     {
         if (! filter_var($value, FILTER_VALIDATE_URL)) {
             throw self::parseError('url', $value);
@@ -114,12 +114,12 @@ class PublicationFieldValue
         return $value;
     }
 
-    public static function parseTagValue(string|array $value): array
+    protected static function parseTagValue(string|array $value): array
     {
         return (array) $value;
     }
 
-    public static function parseError(string $typeName, string $input): InvalidArgumentException
+    protected static function parseError(string $typeName, string $input): InvalidArgumentException
     {
         return new InvalidArgumentException(sprintf("%s: Unable to parse invalid %s value '%s'",
             (ucfirst($typeName).'Field'), $typeName, $input
