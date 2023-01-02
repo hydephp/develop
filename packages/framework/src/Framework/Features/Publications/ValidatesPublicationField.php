554b72ec05af1130333ac90ab8070e281acc3bfd
Caen De Silva <caen@desilva.se>
2023-01-02 18:59:31 +0100

    Copy over validation methods from PublicationFieldDefinition

--- packages/framework/src/Framework/Features/Publications/ValidatesPublicationField.php
+++ packages/framework/src/Framework/Features/Publications/ValidatesPublicationField.php
@@ -6,6 +6,13 @@ namespace Hyde\Framework\Features\Publications;
 
 use Hyde\Framework\Features\Publications\Models\PublicationFieldDefinition;
 use Hyde\Framework\Features\Publications\Models\PublicationType;
+use Illuminate\Contracts\Support\Arrayable;
+
+use Illuminate\Support\Collection;
+
+use function collect;
+use function Hyde\evaluate_arrayable;
+use function validator;
 
 /**
  * @see \Hyde\Framework\Testing\Feature\ValidatesPublicationsTest
@@ -19,5 +26,20 @@ class ValidatesPublicationField
     {
         $this->publicationType = $publicationType;
         $this->fieldDefinition = $fieldDefinition;
+    }
+
+    /**
+     * @param  \Hyde\Framework\Features\Publications\Models\PublicationType|null  $publicationType  Required only when using the 'image' type.
+     */
+    public function getValidationRules(?PublicationType $publicationType = null): Collection
+    {
+        return collect(PublicationFieldService::getValidationRulesForPublicationFieldDefinition($publicationType, $this));
+    }
+
+    public function validate(mixed $input = null, Arrayable|array|null $fieldRules = null, ?PublicationType $publicationType = null): array
+    {
+        $rules = evaluate_arrayable($fieldRules ?? $this->getValidationRules($publicationType));
+
+        return validator([$this->name => $input], [$this->name => $rules])->validate();
     }
 }
