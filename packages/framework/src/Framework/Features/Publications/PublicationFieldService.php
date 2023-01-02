381bf4c5fbb1b4b16ac37c968135d75f7750def0
Caen De Silva <caen@desilva.se>
2023-01-02 13:23:43 +0100

    Link to the test

--- packages/framework/src/Framework/Features/Publications/PublicationFieldService.php
+++ packages/framework/src/Framework/Features/Publications/PublicationFieldService.php
@@ -27,6 +27,8 @@ use function is_numeric;
  * @see  \Hyde\Framework\Features\Publications\Models\PublicationFields\TextField
  * @see  \Hyde\Framework\Features\Publications\Models\PublicationFields\UrlField
  * @see  \Hyde\Framework\Features\Publications\Models\PublicationFields\TagField
+ *
+ * @see \Hyde\Framework\Testing\Feature\PublicationFieldServiceTest
  */
 class PublicationFieldService
 {
