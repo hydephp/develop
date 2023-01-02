99c7d7b61416f63c0f81da06768724b3d29eb19d
Caen De Silva <caen@desilva.se>
2023-01-02 18:53:18 +0100

    Create ValidatesPublications.php

---
+++ packages/framework/src/Framework/Features/Publications/ValidatesPublications.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hyde\Framework\Features\Publications;
+
+/**
+ * @see \Hyde\Framework\Testing\Feature\ValidatesPublicationsTest
+ */
+class ValidatesPublications
+{
+    //
+}
