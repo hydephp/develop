53bca8ce383fdf470f691f03d4ba8f998db63ff4
Caen De Silva <caen@desilva.se>
2023-01-02 14:19:29 +0100

    Implement class based on base class PublicationField

--- packages/framework/src/Framework/Features/Publications/Models/PublicationFields/PublicationFieldValue.php
+++ packages/framework/src/Framework/Features/Publications/Models/PublicationFields/PublicationFieldValue.php
@@ -4,7 +4,30 @@ declare(strict_types=1);
 
 namespace Hyde\Framework\Features\Publications\Models\PublicationFields;
 
+use Hyde\Framework\Features\Publications\PublicationFieldService;
+use Hyde\Framework\Features\Publications\PublicationFieldTypes;
+
+/**
+ * Represents a single value for a field in a publication,
+ * as defined in the "fields" array of a publication type schema.
+ *
+ * @see \Hyde\Framework\Features\Publications\PublicationFieldTypes
+ * @see \Hyde\Framework\Testing\Feature\PublicationFieldServiceTest
+ */
 class PublicationFieldValue
 {
-    //
+    public readonly PublicationFieldTypes $type;
+    protected mixed $value;
+
+    public function __construct(PublicationFieldTypes $type, string $value = null)
+    {
+        if ($value !== null) {
+            $this->value = PublicationFieldService::parseFieldValue($type, $value);
+        }
+    }
+
+    public function getValue(): mixed
+    {
+        return $this->value;
+    }
 }
