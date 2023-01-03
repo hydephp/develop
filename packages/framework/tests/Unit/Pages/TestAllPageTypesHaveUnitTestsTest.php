6d380ce856e9613e7e2e33c3956fbe4aa9cff19b
Caen De Silva <caen@desilva.se>
2023-01-03 17:07:34 +0100

    Update test to not be dependent on monorepo structure

--- packages/framework/tests/Unit/Pages/TestAllPageTypesHaveUnitTestsTest.php
+++ packages/framework/tests/Unit/Pages/TestAllPageTypesHaveUnitTestsTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Hyde\Framework\Testing\Unit\Pages;
 
+use Hyde\Hyde;
 use Hyde\Testing\TestCase;
 
 class TestAllPageTypesHaveUnitTestsTest extends TestCase
@@ -11,6 +12,10 @@ class TestAllPageTypesHaveUnitTestsTest extends TestCase
     public function testAllPageTypesHaveUnitTests()
     {
         $pages = glob(__DIR__.'/../../../src/Pages/*.php');
+        if (empty($pages)) {
+            // If above path is empty, we are probably not in the monorepo, so we use the vendor path
+            $pages = glob(Hyde::vendorPath('/src/Pages/*.php'));
+        }
         $this->assertNotEmpty($pages);
 
         // Simple assertion to make sure we got the right directory
