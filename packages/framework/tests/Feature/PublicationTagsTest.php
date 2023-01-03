c8d01f2ea44b863a553e9625e348bcdf10e05c94
Caen De Silva <caen@desilva.se>
2023-01-03 11:55:50 +0100

    Create PublicationTagsTest.php

---
+++ packages/framework/tests/Feature/PublicationTagsTest.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hyde\Framework\Testing\Feature;
+
+use Hyde\Framework\Features\Publications\Models\PublicationTags;
+use Hyde\Testing\TestCase;
+
+/**
+ * @covers \Hyde\Framework\Features\Publications\Models\PublicationTags
+ */
+class PublicationTagsTest extends TestCase
+{
+    //
+}
