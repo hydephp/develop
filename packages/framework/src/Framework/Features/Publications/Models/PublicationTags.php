b90be60ecb0ffdbe56f2eb301f8c342cc5f23276
Caen De Silva <caen@desilva.se>
2023-01-03 11:56:18 +0100

    Create class PHPDoc

--- packages/framework/src/Framework/Features/Publications/Models/PublicationTags.php
+++ packages/framework/src/Framework/Features/Publications/Models/PublicationTags.php
@@ -4,6 +4,11 @@ declare(strict_types=1);
 
 namespace Hyde\Framework\Features\Publications\Models;
 
+/**
+ * Object representation for the tags.json file.
+ *
+ * @see \Hyde\Framework\Testing\Feature\PublicationTagsTest
+ */
 class PublicationTags
 {
     //
