42e9aa942a8c212afc2f6e59911b19c0fb74b3d1
Caen De Silva <caen@desilva.se>
2023-01-04 21:50:16 +0100

    Revert "Register the page models in the HydeServiceProvider"
    This reverts commit 5697f5cc58dbd85fb5b8eda276adc085167a4a86.

--- packages/framework/src/Framework/HydeServiceProvider.php
+++ packages/framework/src/Framework/HydeServiceProvider.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace Hyde\Framework;
 
 use Hyde\Console\HydeConsoleServiceProvider;
-use Hyde\Facades\Features;
 use Hyde\Foundation\HydeKernel;
 use Hyde\Framework\Concerns\RegistersFileLocations;
 use Hyde\Framework\Features\DataCollections\DataCollectionServiceProvider;
@@ -44,8 +43,6 @@ class HydeServiceProvider extends ServiceProvider
         });
 
         Hyde::setSourceRoot(config('hyde.source_root', ''));
-
-        $this->registerPageModels();
 
         $this->registerSourceDirectories([
             HtmlPage::class => '_pages',
@@ -106,32 +103,6 @@ class HydeServiceProvider extends ServiceProvider
     {
         if (YamlConfigurationService::hasFile()) {
             YamlConfigurationService::boot();
-        }
-    }
-
-    /**
-     * Register the page model classes that Hyde should use.
-     */
-    protected function registerPageModels(): void
-    {
-        if (Features::hasHtmlPages()) {
-            Hyde::registerPageClass(HtmlPage::class);
-        }
-
-        if (Features::hasBladePages()) {
-            Hyde::registerPageClass(BladePage::class);
-        }
-
-        if (Features::hasMarkdownPages()) {
-            Hyde::registerPageClass(MarkdownPage::class);
-        }
-
-        if (Features::hasMarkdownPosts()) {
-            Hyde::registerPageClass(MarkdownPost::class);
-        }
-
-        if (Features::hasDocumentationPages()) {
-            Hyde::registerPageClass(DocumentationPage::class);
         }
     }
 
