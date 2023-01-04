abb860e2c70943e712a419057e71977724334e8a
Caen De Silva <caen@desilva.se>
2023-01-04 17:00:04 +0100

    Revert "Update seeder to handle missing tagGroup gracefully"
    This reverts commit 4610ee7055c8b27423436e990ecb35924b5c7d6d.

--- packages/framework/src/Framework/Actions/SeedsPublicationFiles.php
+++ packages/framework/src/Framework/Actions/SeedsPublicationFiles.php
@@ -98,7 +98,7 @@ class SeedsPublicationFiles
             'image' => 'https://picsum.photos/id/'.rand(1, 1000).'/400/400',
             'integer' => rand(-100000, 100000),
             'string' => substr($this->fakeSentence(10), 0, rand(0, 255)),
-            'tag' => $this->getTags($field->tagGroup ?? ''),
+            'tag' => $this->getTags($field->tagGroup),
             'text' => $this->getTextValue(rand(3, 20)),
             'url' => $this->fakeUrl(),
         };
