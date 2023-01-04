112d13b82289d35b3b02c222678fe7e687429959
Caen De Silva <caen@desilva.se>
2023-01-04 19:51:25 +0100

    Create HydeKernelDynamicPageClassesTest.php

---
+++ packages/framework/tests/Unit/HydeKernelDynamicPageClassesTest.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hyde\Framework\Testing\Unit;
+
+use Hyde\Hyde;
+use Hyde\Testing\TestCase;
+
+/**
+ * @covers \Hyde\Foundation\HydeKernel
+ * @covers \Hyde\Foundation\Concerns\ManagesHydeKernel
+ */
+class HydeKernelDynamicPageClassesTest extends TestCase
+{
+    //
+}
