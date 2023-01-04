3a483a6c326dac4c2e63b0a1a0cc72d9ad489516
Caen De Silva <caen@desilva.se>
2023-01-04 20:19:34 +0100

    Register page models dynamically based on the features enabled
    Ports logic from https://github.com/hydephp/develop/blob/14c4b0a487603b408c915b5e50065910b456f826/packages/framework/src/Foundation/PageCollection.php#L46-L64 and https://github.com/hydephp/develop/blob/14c4b0a487603b408c915b5e50065910b456f826/packages/framework/src/Foundation/FileCollection.php#L63-L81

--- packages/framework/src/Framework/HydeServiceProvider.php
+++ packages/framework/src/Framework/HydeServiceProvider.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Hyde\Framework;
 
 use Hyde\Console\HydeConsoleServiceProvider;
+use Hyde\Facades\Features;
 use Hyde\Foundation\HydeKernel;
 use Hyde\Framework\Concerns\RegistersFileLocations;
 use Hyde\Framework\Features\DataCollections\DataCollectionServiceProvider;
@@ -44,13 +45,7 @@ class HydeServiceProvider extends ServiceProvider
 
         Hyde::setSourceRoot(config('hyde.source_root', ''));
 
-        $this->registerPageModels([
-            HtmlPage::class,
-            BladePage::class,
-            MarkdownPage::class,
-            MarkdownPost::class,
-            DocumentationPage::class,
-        ]);
+        $this->registerPageModels();
 
         $this->registerSourceDirectories([
             HtmlPage::class => '_pages',
@@ -117,10 +112,26 @@ class HydeServiceProvider extends ServiceProvider
     /**
      * Register the page model classes that Hyde should use.
      */
-    protected function registerPageModels(array $pageTypes): void
+    protected function registerPageModels(): void
     {
-        foreach ($pageTypes as $pageType) {
-            Hyde::registerPageClass($pageType);
+        if (Features::hasHtmlPages()) {
+            Hyde::registerPageClass(HtmlPage::class);
+        }
+
+        if (Features::hasBladePages()) {
+            Hyde::registerPageClass(BladePage::class);
+        }
+
+        if (Features::hasMarkdownPages()) {
+            Hyde::registerPageClass(MarkdownPage::class);
+        }
+
+        if (Features::hasMarkdownPosts()) {
+            Hyde::registerPageClass(MarkdownPost::class);
+        }
+
+        if (Features::hasDocumentationPages()) {
+            Hyde::registerPageClass(DocumentationPage::class);
         }
     }
 
