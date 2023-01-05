8fe008ff77e949ca205f448241279eab618f4f24
Caen De Silva <caen@desilva.se>
2023-01-05 18:45:34 +0100

    Create test publication

---
+++ test/list.blade.php
@@ -0,0 +1,16 @@
+@extends('hyde::layouts.app')
+@section('content')
+    <main id="content" class="mx-auto max-w-7xl py-16 px-8">
+        <div class="prose dark:prose-invert">
+            <h1>Publications for type {{ $page->type->name }}</h1>
+            <ol>
+                @php/** @var \Hyde\Pages\PublicationPage $publication*/@endphp
+                @foreach($publications as $publication)
+                <li>
+                    <x-link :href="$publication->getRoute()">{{ $publication->title }}</x-link>
+                </li>
+                @endforeach
+            </ol>
+        </div>
+    </main>
+@endsection
