42c16c9d381698dabf2fdc08762516d0bb632813
Caen De Silva <caen@desilva.se>
2023-01-05 19:11:07 +0100

    add todo

--- packages/framework/src/Support/Contracts/DynamicPage.php
+++ packages/framework/src/Support/Contracts/DynamicPage.php
@@ -7,6 +7,8 @@ namespace Hyde\Support\Contracts;
 /**
  * This interface is used to mark page classes that are dynamically generated (i.e. not based on a source file),
  * or that has dynamic path information like publication pages.
+ *
+ * maybe rename to VirtualPage and/or create a class for that
  */
 interface DynamicPage
 {
