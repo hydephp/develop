7f566364b36a3b2bfd65f96f20cd6f27774e05b8
Caen De Silva <caen@desilva.se>
2023-01-05 22:15:24 +0100

    Add class PHPDoc

--- packages/framework/src/Pages/VirtualPage.php
+++ packages/framework/src/Pages/VirtualPage.php
@@ -6,6 +6,9 @@ namespace Hyde\Pages;
 
 use Hyde\Pages\Concerns\HydePage;
 
+/**
+ * A virtual page is a page that does not have a source file.
+ */
 class VirtualPage extends HydePage
 {
     public function compile(): string
