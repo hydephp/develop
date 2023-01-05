63a0232aba8a3c80aefbeefa77c4eca710c336ef
Caen De Silva <caen@desilva.se>
2023-01-05 22:15:31 +0100

    Add developer documentation

--- packages/framework/src/Pages/VirtualPage.php
+++ packages/framework/src/Pages/VirtualPage.php
@@ -8,6 +8,11 @@ use Hyde\Pages\Concerns\HydePage;
 
 /**
  * A virtual page is a page that does not have a source file.
+ *
+ * This can be useful for creating pagination pages and the like.
+ * When used in a package, it's on the package developer to ensure
+ * that the virtual page is registered with Hyde, usually within the
+ * boot method of the package's service provider so it can be compiled.
  */
 class VirtualPage extends HydePage
 {
