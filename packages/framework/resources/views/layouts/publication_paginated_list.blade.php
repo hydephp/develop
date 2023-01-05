68f4a4288f2e1b94d5c264297f6813c67cacf922
Caen De Silva <caen@desilva.se>
2023-01-05 19:21:40 +0100

    Create basic pagination navigation

--- packages/framework/resources/views/layouts/publication_paginated_list.blade.php
+++ packages/framework/resources/views/layouts/publication_paginated_list.blade.php
@@ -12,7 +12,25 @@
                 @endforeach
             </ol>
 
-            //  next prev links + page numbers
+            <nav>
+                @if($paginator->previous)
+{{--                    useful if using routes: <x-link :href="$paginator->previous">Prev</x-link>--}}
+                    <a href="page-{{ $paginator->previous }}.html">Prev</a> <!-- fixme support pretty urls -->
+                @endif
+
+                @foreach(range(1, $paginator->total) as $number)
+                    @if($paginator->current === $number)
+                        <span><strong>{{ $number }}</strong></span>
+                    @else
+{{--                        <x-link :href="$page->url">{{ $number }}</x-link>--}}
+                        <a href="page-{{ $number }}.html">{{ $number }}</a> <!-- fixme support pretty urls -->
+                    @endif
+                @endforeach
+
+                @if($paginator->next)
+                        <a href="page-{{ $paginator->next }}.html">Next</a> <!-- fixme support pretty urls -->
+                @endif
+            </nav>
         </div>
     </main>
 @endsection
