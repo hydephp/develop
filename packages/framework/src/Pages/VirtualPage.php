c9e1c267818d9aed5c9a71ffebd08277c208dd6e
Caen De Silva <caen@desilva.se>
2023-01-05 22:15:12 +0100

    Create VirtualPage.php

---
+++ packages/framework/src/Pages/VirtualPage.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hyde\Pages;
+
+use Hyde\Pages\Concerns\HydePage;
+
+class VirtualPage extends HydePage
+{
+    public function compile(): string
+    {
+        // TODO: Implement compile() method.
+    }
+}
