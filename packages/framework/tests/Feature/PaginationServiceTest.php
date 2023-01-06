288d1bdd31e0f841853f525cd792607b3c6009c2
Caen De Silva <caen@desilva.se>
2023-01-06 17:58:38 +0100

    Create PaginationServiceTest.php
    Update PaginationService.php

---
+++ packages/framework/tests/Feature/PaginationServiceTest.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hyde\Framework\Testing\Feature;
+
+use Hyde\Framework\Features\Publications\PaginationService;
+use Hyde\Testing\TestCase;
+
+/**
+ * @covers \Hyde\Framework\Features\Publications\PaginationService
+ */
+class PaginationServiceTest extends TestCase
+{
+    //
+}
