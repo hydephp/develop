3090264b5cdd5b75c8286eb547c32bc56b96d01f
Caen De Silva <caen@desilva.se>
2023-01-06 21:46:03 +0100

    Code formatting

--- packages/framework/tests/Feature/PaginationServiceTest.php
+++ packages/framework/tests/Feature/PaginationServiceTest.php
@@ -28,8 +28,11 @@ class PaginationServiceTest extends TestCase
 
     public function testGetPaginatedPageCollectionWithPages()
     {
-        $collection = (new PaginationService(new PaginationSettings(),
-            collect(range(1, 50))))->getPaginatedPageCollection();
+        $collection = (new PaginationService(
+            new PaginationSettings(),
+            collect(range(1, 50))
+        ))->getPaginatedPageCollection();
+
         $this->assertCount(2, $collection);
         $this->assertCount(25, $collection->first());
         $this->assertCount(25, $collection->last());
@@ -42,8 +45,10 @@ class PaginationServiceTest extends TestCase
 
     public function testCollectionIsChunkedBySpecifiedSettingValue()
     {
-        $collection = (new PaginationService(new PaginationSettings(pageSize: 10),
-            collect(range(1, 50))))->getPaginatedPageCollection();
+        $collection = (new PaginationService(
+            new PaginationSettings(pageSize: 10),
+            collect(range(1, 50)))
+        )->getPaginatedPageCollection();
 
         $this->assertCount(5, $collection);
         $this->assertCount(10, $collection->first());
