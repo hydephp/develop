e80cb14e9dbee10087970a95635fa68392eca136
Caen De Silva <caen@desilva.se>
2023-01-06 19:00:54 +0100

    Extract testing helper

--- packages/framework/tests/Feature/PaginationServiceTest.php
+++ packages/framework/tests/Feature/PaginationServiceTest.php
@@ -16,34 +16,38 @@ class PaginationServiceTest extends TestCase
 {
     public function test_it_can_be_instantiated(): void
     {
-        $this->directory('test-publication');
-        $this->setupTestPublication();
+        $publicationType = $this->setupPublication();
 
         $this->assertInstanceOf(PaginationService::class,
-            new PaginationService(PublicationType::get('test-publication'))
+            new PaginationService($publicationType)
         );
     }
 
     public function testGetPaginatedPageCollection()
     {
-        $this->directory('test-publication');
-        $this->setupTestPublication();
+        $publicationType = $this->setupPublication();
 
-        $this->assertEquals(collect([]), (new PaginationService(PublicationType::get('test-publication')))->getPaginatedPageCollection());
+        $this->assertEquals(collect([]), (new PaginationService($publicationType))->getPaginatedPageCollection());
     }
 
     public function testGetPaginatedPageCollectionWithPages()
     {
-        $this->directory('test-publication');
-        $this->setupTestPublication();
+        $publicationType = $this->setupPublication();
 
         foreach (range(1, 50) as $i) {
             $this->file("test-publication/$i.md", "title: $i");
         }
 
-        $collection = (new PaginationService(PublicationType::get('test-publication')))->getPaginatedPageCollection();
+        $collection = (new PaginationService($publicationType))->getPaginatedPageCollection();
         $this->assertCount(2, $collection);
         $this->assertCount(25, $collection->first());
         $this->assertCount(25, $collection->last());
+    }
+
+    protected function setupPublication(): PublicationType
+    {
+        $this->directory('test-publication');
+        $this->setupTestPublication();
+        return PublicationType::get('test-publication');
     }
 }
