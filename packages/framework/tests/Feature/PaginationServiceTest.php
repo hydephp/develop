46af81d908cc9cfe02070413c6e9a45982cfbd80
Caen De Silva <caen@desilva.se>
2023-01-06 18:00:02 +0100

    Create initial data structure for pagination service

--- packages/framework/tests/Feature/PaginationServiceTest.php
+++ packages/framework/tests/Feature/PaginationServiceTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Hyde\Framework\Testing\Feature;
 
+use Hyde\Framework\Features\Publications\Models\PublicationType;
 use Hyde\Framework\Features\Publications\PaginationService;
 use Hyde\Testing\TestCase;
 
@@ -12,5 +13,10 @@ use Hyde\Testing\TestCase;
  */
 class PaginationServiceTest extends TestCase
 {
-    //
+    public function test_it_can_be_instantiated(): void
+    {
+        $this->assertInstanceOf(PaginationService::class,
+            new PaginationService($this->createMock(PublicationType::class))
+        );
+    }
 }
