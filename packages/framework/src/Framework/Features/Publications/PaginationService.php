def9e53e59cd5546b461d3759b1c95625fd4adb2
Caen De Silva <caen@desilva.se>
2023-01-06 18:00:02 +0100

    Create initial data structure for pagination service

--- packages/framework/src/Framework/Features/Publications/PaginationService.php
+++ packages/framework/src/Framework/Features/Publications/PaginationService.php
@@ -4,10 +4,17 @@ declare(strict_types=1);
 
 namespace Hyde\Framework\Features\Publications;
 
+use Hyde\Framework\Features\Publications\Models\PublicationType;
+
 /**
  * @see \Hyde\Framework\Testing\Feature\PaginationServiceTest
  */
 class PaginationService
 {
-    //
+    protected PublicationType $publicationType;
+
+    public function __construct(PublicationType $publicationType)
+    {
+        $this->publicationType = $publicationType;
+    }
 }
