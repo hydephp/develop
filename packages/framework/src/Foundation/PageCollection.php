0fb8eb200ec758ee659c1219b448e93b5332a13d
Caen De Silva <caen@desilva.se>
2023-01-07 20:37:16 +0100

    Mark added method as internal and deprecate it

--- packages/framework/src/Foundation/PageCollection.php
+++ packages/framework/src/Foundation/PageCollection.php
@@ -133,6 +133,10 @@ final class PageCollection extends BaseFoundationCollection
         }
     }
 
+    /**
+     * @deprecated This method will be removed before merging into master.
+     * @internal This method will be removed before merging into master.
+     */
     protected function generatePublicationPaginatedListingPagesForType(PublicationType $type): void
     {
         $paginator = $type->getPaginator();
