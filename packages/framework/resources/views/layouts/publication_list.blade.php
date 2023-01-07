9dc916597cce12ecc4a33d407357e933dec87c3a
Caen De Silva <caen@desilva.se>
2023-01-07 20:53:31 +0100

    Split the if-else block into dedicated view

--- packages/framework/resources/views/layouts/publication_list.blade.php
+++ packages/framework/resources/views/layouts/publication_list.blade.php
@@ -4,28 +4,13 @@
         <div class="prose dark:prose-invert">
             @php/** @var \Hyde\Framework\Features\Publications\Models\PublicationType $publicationType*/@endphp
             <h1>Publications for type {{ $publicationType->name }}</h1>
-            @if($publicationType->usesPagination())
-                @php
-                    $paginator = $publicationType->getPaginator($page->matter('paginatorPage'));
-                @endphp
-                <ol start="{{ $paginator->itemsStartNumber() }}">
-                    @foreach($paginator->getItemsForPage() as $publication)
-                        <li>
-                            <x-link :href="$publication->getRoute()">{{ $publication->title }}</x-link>
-                        </li>
-                    @endforeach
-                </ol>
-
-                @include('hyde::components.publications.pagination')
-            @else
-                <ol>
-                    @foreach($publicationType->getPublications() as $publication)
-                        <li>
-                            <x-link :href="$publication->getRoute()">{{ $publication->title }}</x-link>
-                        </li>
-                    @endforeach
-                </ol>
-            @endif
+            <ol>
+                @foreach($publicationType->getPublications() as $publication)
+                    <li>
+                        <x-link :href="$publication->getRoute()">{{ $publication->title }}</x-link>
+                    </li>
+                @endforeach
+            </ol>
         </div>
     </main>
 @endsection
---
+++ packages/framework/resources/views/layouts/publication_paginated_list.blade.php
@@ -0,0 +1,21 @@
+@extends('hyde::layouts.app')
+@section('content')
+    <main id="content" class="mx-auto max-w-7xl py-16 px-8">
+        <div class="prose dark:prose-invert">
+            @php/** @var \Hyde\Framework\Features\Publications\Models\PublicationType $publicationType*/@endphp
+            <h1>Publications for type {{ $publicationType->name }}</h1>
+            @php
+                $paginator = $publicationType->getPaginator($page->matter('paginatorPage'));
+            @endphp
+            <ol start="{{ $paginator->itemsStartNumber() }}">
+                @foreach($paginator->getItemsForPage() as $publication)
+                    <li>
+                        <x-link :href="$publication->getRoute()">{{ $publication->title }}</x-link>
+                    </li>
+                @endforeach
+            </ol>
+
+            @include('hyde::components.publications.pagination')
+        </div>
+    </main>
+@endsection
