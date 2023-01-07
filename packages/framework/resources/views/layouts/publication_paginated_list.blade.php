81adf11a507190d95c856c9afd471ddf8b9df576
Caen De Silva <caen@desilva.se>
2023-01-07 20:54:55 +0100

    Move up paginator retrieval in Blade file

--- packages/framework/resources/views/layouts/publication_paginated_list.blade.php
+++ packages/framework/resources/views/layouts/publication_paginated_list.blade.php
@@ -1,12 +1,12 @@
 @php/** @var \Hyde\Framework\Features\Publications\Models\PublicationType $publicationType*/@endphp
+@php
+    $paginator = $publicationType->getPaginator($page->matter('paginatorPage'));
+@endphp
 @extends('hyde::layouts.app')
 @section('content')
     <main id="content" class="mx-auto max-w-7xl py-16 px-8">
         <div class="prose dark:prose-invert">
             <h1>Publications for type {{ $publicationType->name }}</h1>
-            @php
-                $paginator = $publicationType->getPaginator($page->matter('paginatorPage'));
-            @endphp
             <ol start="{{ $paginator->itemsStartNumber() }}">
                 @foreach($paginator->getItemsForPage() as $publication)
                     <li>
