4d7a30790d0920c643e53cf646e127fbef479153
Caen De Silva <caen@desilva.se>
2023-01-07 22:28:12 +0100

    Convert concatenation to string interpolation

--- packages/framework/src/Framework/Features/Publications/PaginationService.php
+++ packages/framework/src/Framework/Features/Publications/PaginationService.php
@@ -164,7 +164,7 @@ class PaginationService
         $pageRange = range(1, $this->totalPages());
         if ($this->paginationRouteBasename) {
             foreach ($pageRange as $number) {
-                $array[$number] = Route::getOrFail("$this->paginationRouteBasename/page-".$number);
+                $array[$number] = Route::getOrFail("$this->paginationRouteBasename/page-$number");
             }
         } else {
             foreach ($pageRange as $number) {
