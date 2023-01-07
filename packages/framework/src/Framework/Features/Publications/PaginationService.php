aeab2591b545d853358ea95d27eaf3c76239f861
Caen De Silva <caen@desilva.se>
2023-01-07 22:37:59 +0100

    Rename method getPaginatedCollection to getPaginatedItems

--- packages/framework/src/Framework/Features/Publications/PaginationService.php
+++ packages/framework/src/Framework/Features/Publications/PaginationService.php
@@ -65,7 +65,7 @@ class PaginationService
     }
 
     /** Get the paginated collection */
-    public function getPaginatedCollection(): Collection
+    public function getPaginatedItems(): Collection
     {
         return $this->items;
     }
