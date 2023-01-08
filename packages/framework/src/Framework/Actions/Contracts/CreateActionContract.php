07601a90dbb15157155746776ddcc00ef9e19f1f
Caen De Silva <caen@desilva.se>
2023-01-08 22:05:45 +0100

    Deprecate internal experimental interface CreateActionContract

--- packages/framework/src/Framework/Actions/Contracts/CreateActionContract.php
+++ packages/framework/src/Framework/Actions/Contracts/CreateActionContract.php
@@ -7,6 +7,7 @@ namespace Hyde\Framework\Actions\Contracts;
 /**
  * @see \Hyde\Framework\Actions\Concerns\CreateAction
  *
+ * @deprecated 
  * @internal This class is experimental and is not meant to be used outside the Hyde framework.
  */
 interface CreateActionContract
