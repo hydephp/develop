ad6a94c704ec64608170b22571b946da8369cd8d
Caen De Silva <caen@desilva.se>
2023-01-08 21:51:10 +0100

    Rename protected helper method

--- packages/framework/src/Framework/Features/Publications/Models/PublicationType.php
+++ packages/framework/src/Framework/Features/Publications/Models/PublicationType.php
@@ -173,7 +173,7 @@ class PublicationType implements SerializableContract
 
     public function getPaginator(int $currentPageNumber = null): Paginator
     {
-        return new Paginator($this->getPublicationsSortedByPaginationField(),
+        return new Paginator($this->getSortedPublications(),
             $this->pageSize,
             $currentPageNumber,
             $this->getIdentifier()
@@ -207,7 +207,7 @@ class PublicationType implements SerializableContract
         return ['directory' => Hyde::pathToRelative(dirname($schemaFile))];
     }
 
-    protected function getPublicationsSortedByPaginationField(): Collection
+    protected function getSortedPublications(): Collection
     {
         return $this->getPublications()->sortBy(function (PublicationPage $page): mixed {
             return $page->matter($this->sortField);
