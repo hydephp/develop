651859adef4e39f612a6b0a5bc0d1f2033d5284e
Caen De Silva <caen@desilva.se>
2023-01-09 18:54:58 +0100

    Return gracefully if the kernel is not ready to be booted

--- packages/framework/src/Foundation/Concerns/BootsHydeKernel.php
+++ packages/framework/src/Foundation/Concerns/BootsHydeKernel.php
@@ -20,7 +20,7 @@ trait BootsHydeKernel
 
     public function boot(): void
     {
-        if ($this->booting) {
+        if (! $this->readyToBoot ||  $this->booting) {
             return;
         }
 
