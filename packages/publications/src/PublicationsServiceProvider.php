3bb56ab4223c288b2452493039bb0756005a2107
Caen De Silva <caen@desilva.se>
2023-01-09 20:25:35 +0100

    Create PublicationsServiceProvider.php

---
+++ packages/publications/src/PublicationsServiceProvider.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hyde\Publications;
+
+use Illuminate\Support\ServiceProvider;
+
+class PublicationsServiceProvider extends ServiceProvider
+{
+    /**
+     * Register the application services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap the application services.
+     */
+    public function boot(): void
+    {
+        //
+    }
+}
