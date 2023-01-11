d1c82c96c86fd905bace4277e09c78eacd0f4d9c
Caen De Silva <caen@desilva.se>
2023-01-11 19:18:15 +0100

    Test handler methods are called by discovery

--- packages/framework/tests/Feature/HydeExtensionFeatureTest.php
+++ packages/framework/tests/Feature/HydeExtensionFeatureTest.php
@@ -5,7 +5,10 @@ declare(strict_types=1);
 namespace Hyde\Framework\Testing\Feature;
 
 use Hyde\Foundation\Concerns\HydeExtension;
+use Hyde\Foundation\FileCollection;
 use Hyde\Foundation\HydeKernel;
+use Hyde\Foundation\PageCollection;
+use Hyde\Foundation\RouteCollection;
 use Hyde\Pages\Concerns\HydePage;
 use Hyde\Testing\TestCase;
 
@@ -33,15 +36,46 @@ class HydeExtensionFeatureTest extends TestCase
         $this->kernel->registerExtension(HydeTestExtension::class);
         $this->assertSame([HydeTestExtension::class], $this->kernel->getRegisteredExtensions());
     }
+
+    public function testHandlerMethodsAreCalledByDiscovery()
+    {
+        $this->kernel->registerExtension(HydeTestExtension::class);
+
+        $this->assertSame([], HydeTestExtension::$callCache);
+
+        $this->kernel->boot();
+
+        $this->assertSame(['files', 'pages', 'routes'], HydeTestExtension::$callCache);
+
+        HydeTestExtension::$callCache = [];
+    }
 }
 
 class HydeTestExtension extends HydeExtension
 {
+    // An easy way to assert the handlers are called.
+    public static array $callCache = [];
+
     public static function getPageClasses(): array
     {
         return [
             HydeExtensionTestPage::class,
         ];
+    }
+
+    public static function discoverFiles(FileCollection $collection): void
+    {
+        static::$callCache[] = 'files';
+    }
+
+    public static function discoverPages(PageCollection $collection): void
+    {
+        static::$callCache[] = 'pages';
+    }
+
+    public static function discoverRoutes(RouteCollection $collection): void
+    {
+        static::$callCache[] = 'routes';
     }
 }
 
