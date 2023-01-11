cf9083ea358e1fea0d309a927d791ba80b7c9a3d
Caen De Silva <caen@desilva.se>
2023-01-11 15:35:24 +0100

    Create PublicationsExtension.php

---
+++ packages/publications/src/PublicationsExtension.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hyde\Publications;
+
+use Hyde\Foundation\Extensions\HydeExtension;
+
+class PublicationsExtension extends HydeExtension
+{
+    //
+}
