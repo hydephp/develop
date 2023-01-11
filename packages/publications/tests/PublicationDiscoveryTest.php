fbef5f8cb64bf834dc5bd2e447cf6136e985b6fc
Caen De Silva <caen@desilva.se>
2023-01-11 15:44:52 +0100

    Create PublicationDiscoveryTest.php

---
+++ packages/publications/tests/PublicationDiscoveryTest.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hyde\Publications\Testing;
+
+use Hyde\Publications\PublicationsExtension;
+use Hyde\Testing\TestCase;
+
+/**
+ * @covers \Hyde\Publications\PublicationsExtension
+ */
+class PublicationDiscoveryTest extends TestCase
+{
+    //
+}
