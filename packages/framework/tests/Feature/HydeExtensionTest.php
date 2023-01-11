1f7f834def21e4848513c668cd81be1e52304aa2
Caen De Silva <caen@desilva.se>
2023-01-11 19:01:31 +0100

    Add additional testing classes

--- packages/framework/tests/Feature/HydeExtensionTest.php
+++ packages/framework/tests/Feature/HydeExtensionTest.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Hyde\Framework\Testing\Feature;
 
 use Hyde\Foundation\Concerns\HydeExtension;
+use Hyde\Pages\Concerns\HydePage;
 use Hyde\Testing\TestCase;
 
 /**
@@ -17,5 +18,18 @@ class HydeExtensionTest extends TestCase
 
 class HydeTestExtension extends HydeExtension
 {
-    //
+    public static function getPageClasses(): array
+    {
+        return [
+            HydeExtensionTestPage::class,
+        ];
+    }
+}
+
+class HydeExtensionTestPage extends HydePage
+{
+    public function compile(): string
+    {
+        return '';
+    }
 }
