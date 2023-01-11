5304588b5ad115b7197191b3eda8354adf54ce9d
Caen De Silva <caen@desilva.se>
2023-01-11 13:46:19 +0100

    Add static void method stubs for discovery collections

--- packages/framework/src/Foundation/Extensions/HydeExtension.php
+++ packages/framework/src/Foundation/Extensions/HydeExtension.php
@@ -6,5 +6,18 @@ namespace Hyde\Foundation\Extensions;
 
 abstract class HydeExtension
 {
-    //
+    public static function discoverFiles(): void
+    {
+        //
+    }
+
+    public static function discoverPages(): void
+    {
+        //
+    }
+
+    public static function discoverRoutes(): void
+    {
+        //
+    }
 }
