97ab59754faf1b596f9fdaab61792e8369e1bc3f
Caen De Silva <caen@desilva.se>
2023-01-11 13:17:38 +0100

    Remove now unused internal deprecated test helper setupTestPublication

--- packages/testing/src/TestCase.php
+++ packages/testing/src/TestCase.php
@@ -49,13 +49,6 @@ abstract class TestCase extends BaseTestCase
         parent::tearDown();
     }
 
-    /** @deprecated Will be removed as it's no longer part of the main package. */
-    #[Deprecated (reason: "Will be removed as it's no longer part of the main package. In most cases you can use the following replacement:", replacement: "(new PublicationType('Test Publication'))->save();")]
-    protected function setupTestPublication(string $directory = 'test-publication')
-    {
-        Filesystem::copy('tests/fixtures/test-publication-schema.json', "$directory/schema.json");
-    }
-
     protected function assertFileEqualsString(string $string, string $path, bool $strict = false): void
     {
         if ($strict) {
