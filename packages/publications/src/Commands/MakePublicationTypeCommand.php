d20e0956f31c8528fe65982977f89fd1bce2975a
Caen De Silva <caen@desilva.se>
2023-01-13 14:09:39 +0100

    Revert "Document method"
    This reverts commit 39fc53b2a8465bb9686dd68a504e814057a1a668.

--- packages/publications/src/Commands/MakePublicationTypeCommand.php
+++ packages/publications/src/Commands/MakePublicationTypeCommand.php
@@ -202,7 +202,6 @@ class MakePublicationTypeCommand extends ValidatingCommand
         return $this->fields->count() + $offset;
     }
 
-    /** Get a collection of names for added fields that can be canonicable */
     protected function availableCanonicableFieldNames(): Collection
     {
         return $this->fields->reject(function (PublicationFieldDefinition $field): bool {
