47bb4650f97e1e6b1df72cc97959797515468a20
Caen De Silva <caen@desilva.se>
2023-01-14 20:32:32 +0100

    Add throws tag for validate method

--- packages/publications/src/Actions/ValidatesPublicationField.php
+++ packages/publications/src/Actions/ValidatesPublicationField.php
@@ -37,6 +37,7 @@ class ValidatesPublicationField
         );
     }
 
+    /** @throws \Illuminate\Validation\ValidationException */
     public function validate(mixed $input = null): array
     {
         return $this->makeValidator($input, $this->getValidationRules())->validate();
