0c4e7b699f9c54c779324386784c4d691eefabcd
Caen De Silva <caen@desilva.se>
2023-01-18 14:11:06 +0100

    Create constructor

--- packages/publications/src/Actions/ValidatesPublicationSchema.php
+++ packages/publications/src/Actions/ValidatesPublicationSchema.php
@@ -4,13 +4,25 @@ declare(strict_types=1);
 
 namespace Hyde\Publications\Actions;
 
+use Hyde\Facades\Filesystem;
 use Hyde\Framework\Concerns\InvokableAction;
+
+use function json_decode;
 
 /**
  * @see \Hyde\Publications\Testing\Feature\ValidatesPublicationSchemaTest
  */
 class ValidatesPublicationSchema extends InvokableAction
 {
+    protected array $schema;
+    protected bool $throw;
+
+    public function __construct(string $pubTypeName, bool $throw = true)
+    {
+        $this->schema = json_decode(Filesystem::getContents("$pubTypeName/schema.json"));
+        $this->throw = $throw;
+    }
+
     public function __invoke()
     {
         // TODO: Implement __invoke() method.
