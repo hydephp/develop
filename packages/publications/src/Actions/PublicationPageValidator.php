cedf6e46f8a5c014db21daf5af45960bee1685dc
Caen De Silva <caen@desilva.se>
2023-01-18 20:16:04 +0100

    Create PublicationPageValidator.php

---
+++ packages/publications/src/Actions/PublicationPageValidator.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hyde\Publications\Actions;
+
+use Hyde\Framework\Concerns\InvokableAction;
+
+/**
+ * @see \Hyde\Publications\Testing\Feature\PublicationPageValidatorTest
+ */
+class PublicationPageValidator extends InvokableAction
+{
+    public function __invoke()
+    {
+        // TODO: Implement __invoke() method.
+    }
+}
