865c4b9f5bef5a7833707d6304f9e58dc01cf39b
Caen De Silva <caen@desilva.se>
2023-01-18 14:09:10 +0100

    Create ValidatesPublicationSchema.php

---
+++ packages/publications/src/Actions/ValidatesPublicationSchema.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hyde\Publications\Actions;
+
+use Hyde\Framework\Concerns\InvokableAction;
+
+/**
+ * @see \Hyde\Publications\Testing\Feature\ValidatesPublicationSchemaTest
+ */
+class ValidatesPublicationSchema extends InvokableAction
+{
+    public function __invoke()
+    {
+        // TODO: Implement __invoke() method.
+    }
+}
