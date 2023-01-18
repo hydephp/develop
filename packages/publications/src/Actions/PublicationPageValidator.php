85cb32a7d8befa14b3c4aa44406c58eaa291bba3
Caen De Silva <caen@desilva.se>
2023-01-18 20:25:03 +0100

    Create base class data structure and constructor

--- packages/publications/src/Actions/PublicationPageValidator.php
+++ packages/publications/src/Actions/PublicationPageValidator.php
@@ -5,12 +5,23 @@ declare(strict_types=1);
 namespace Hyde\Publications\Actions;
 
 use Hyde\Framework\Concerns\InvokableAction;
+use Hyde\Markdown\Models\MarkdownDocument;
+use Hyde\Publications\Models\PublicationType;
 
 /**
  * @see \Hyde\Publications\Testing\Feature\PublicationPageValidatorTest
  */
 class PublicationPageValidator extends InvokableAction
 {
+    protected PublicationType $publicationType;
+    protected array $matter;
+
+    public function __construct(PublicationType $publicationType, string $pageIdentifier)
+    {
+        $this->publicationType = $publicationType;
+        $this->matter = MarkdownDocument::parse("{$publicationType->getDirectory()}/$pageIdentifier.md")->matter()->toArray();
+    }
+
     public function __invoke()
     {
         // TODO: Implement __invoke() method.
