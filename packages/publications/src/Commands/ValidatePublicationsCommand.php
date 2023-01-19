99a3ff4d4fe7b1ce42d6adad40531ef77c5941cc
Caen De Silva <caen@desilva.se>
2023-01-19 16:56:46 +0100

    Shift spacing location

--- packages/publications/src/Commands/ValidatePublicationsCommand.php
+++ packages/publications/src/Commands/ValidatePublicationsCommand.php
@@ -169,9 +169,9 @@ class ValidatePublicationsCommand extends ValidatingCommand
     {
         $isWarning = str_starts_with($message, 'Warning: ');
         $icon = $isWarning
-            ? ' '.self::WARNING
-            : ' '.self::CROSS_MARK;
-        $this->line(sprintf('    <fg=bright-cyan>Field [%s]</>%s', $fieldName, $icon));
+            ? self::WARNING
+            : self::CROSS_MARK;
+        $this->line(sprintf('    <fg=bright-cyan>Field [%s]</> %s', $fieldName, $icon));
 
         $this->line($isWarning
             ? "      <fg=yellow>$message</>"
