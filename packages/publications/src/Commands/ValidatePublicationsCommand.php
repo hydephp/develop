7d3dc1385b7362c1da674bf1745d02b007ba1c0b
Caen De Silva <caen@desilva.se>
2023-01-19 16:34:59 +0100

    Introduce local variable

--- packages/publications/src/Commands/ValidatePublicationsCommand.php
+++ packages/publications/src/Commands/ValidatePublicationsCommand.php
@@ -161,7 +161,8 @@ class ValidatePublicationsCommand extends ValidatingCommand
         $this->line(sprintf('  <fg=cyan>File %s.md</> %s', $publicationName, $icon));
 
         foreach ($results as $fieldName => $message) {
-            $fieldHasErrors = ! str_starts_with($message, 'Warning: '); // FIXME when fields that pass validation are added to the results
+            $isWarning = str_starts_with($message, 'Warning: ');
+            $fieldHasErrors = !$isWarning; // FIXME when fields that pass validation are added to the results
             $this->line(sprintf('    <fg=bright-cyan>Field [%s]</>%s', $fieldName,
                 $fieldHasErrors ? sprintf(' <fg=red>%s</>', self::CROSS_MARK) : sprintf(' <info>%s</info>',
                     self::CHECKMARK)));
