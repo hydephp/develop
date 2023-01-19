19ffd174881e6831c14e7fdfff0db5db3a947be9
Caen De Silva <caen@desilva.se>
2023-01-19 20:23:40 +0100

    Update success message to clarify context

--- packages/publications/src/Actions/PublicationPageValidator.php
+++ packages/publications/src/Actions/PublicationPageValidator.php
@@ -103,7 +103,7 @@ class PublicationPageValidator extends InvokableAction
                 $passed = false;
                 $results[$key] = "Error: $errors[$key]";
             } else if ($passed) {
-                $results[$key] = 'Passed';
+                $results[$key] = "Field $key passed";
             }
         }
 
