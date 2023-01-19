4113c8a90cf76e4effa715ecfcb28cb393bc2f05
Caen De Silva <caen@desilva.se>
2023-01-19 16:58:05 +0100

    Split out indentation from ternary statement

--- packages/publications/src/Commands/ValidatePublicationsCommand.php
+++ packages/publications/src/Commands/ValidatePublicationsCommand.php
@@ -172,9 +172,9 @@ class ValidatePublicationsCommand extends ValidatingCommand
             $isWarning ? self::WARNING : self::CROSS_MARK
         ));
 
-        $this->line($isWarning
-            ? "      <fg=yellow>$message</>"
-            : "      <fg=red>Error: $message</>"
+        $this->line('      '. $isWarning
+            ? "<fg=yellow>$message</>"
+            : "fg=red>Error: $message</>"
         );
     }
 
