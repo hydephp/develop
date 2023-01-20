1a8674918a88c06cdcba64aa06480b9d560b4ea7
Caen De Silva <caen@desilva.se>
2023-01-20 19:23:42 +0100

    Return early as we already know the collection is not empty

--- packages/publications/src/Commands/ValidatePublicationsCommand.php
+++ packages/publications/src/Commands/ValidatePublicationsCommand.php
@@ -108,7 +108,7 @@ class ValidatePublicationsCommand extends ValidatingCommand
             if (! $publicationTypes->has($name)) {
                 throw new InvalidArgumentException("Publication type [$name] does not exist");
             }
-            $publicationTypes = collect([$name => PublicationType::get($name)]);
+            return collect([$name => PublicationType::get($name)]);
         }
 
         if ($publicationTypes->isEmpty()) {
