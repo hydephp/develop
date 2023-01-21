2d03ee87db7e7b6b13915389a659d1d75f4d5119
Caen De Silva <caen@desilva.se>
2023-01-21 21:36:55 +0100

    Import Class as Alias

--- packages/framework/src/Console/Concerns/Command.php
+++ packages/framework/src/Console/Concerns/Command.php
@@ -4,10 +4,12 @@ declare(strict_types=1);
 
 namespace Hyde\Console\Concerns;
 
+use LaravelZero\Framework\Commands\Command as BaseCommand;
+
 /**
  * @see \Hyde\Framework\Testing\Feature\CommandTest
  */
-abstract class Command extends \LaravelZero\Framework\Commands\Command
+abstract class Command extends BaseCommand
 {
     //
 }
