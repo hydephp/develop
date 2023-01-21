908a940fed7aae8c45e6e4e849a17571f2bf3041
Caen De Silva <caen@desilva.se>
2023-01-21 21:35:39 +0100

    Create CommandTest.php

---
+++ packages/framework/tests/Feature/CommandTest.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hyde\Framework\Testing\Feature;
+
+use Hyde\Console\Concerns\Command;
+use Hyde\Testing\TestCase;
+
+/**
+ * @covers \Hyde\Console\Concerns\Command
+ */
+class CommandTest extends TestCase
+{
+    //
+}
