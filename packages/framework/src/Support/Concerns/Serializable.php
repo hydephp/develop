a9b4dea301709432ebb669c1a1f82038c45e3b3d
Caen De Silva <caen@desilva.se>
2023-01-21 19:23:07 +0100

    Strongly type the parameter type

--- packages/framework/src/Support/Concerns/Serializable.php
+++ packages/framework/src/Support/Concerns/Serializable.php
@@ -22,7 +22,7 @@ trait Serializable
     }
 
     /** @inheritDoc */
-    public function toJson($options = 0): string
+    public function toJson(int $options = 0): string
     {
         return json_encode($this->jsonSerialize(), $options);
     }
