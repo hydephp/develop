add8c909c4ebd1b1f63e67115438498340076d67
Caen De Silva <caen@desilva.se>
2023-01-22 12:25:35 +0100

    Add helper to start the clock

--- packages/framework/src/Framework/Concerns/TracksExecutionTime.php
+++ packages/framework/src/Framework/Concerns/TracksExecutionTime.php
@@ -4,7 +4,14 @@ declare(strict_types=1);
 
 namespace Hyde\Framework\Concerns;
 
+use function microtime;
+
 trait TracksExecutionTime
 {
     protected float $timeStart;
+
+    protected function startClock(): void
+    {
+        $this->timeStart = microtime(true);
+    }
 }
