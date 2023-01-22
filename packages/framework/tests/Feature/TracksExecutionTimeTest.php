82326fd12431afd8236f24b99b34ad741e188a29
Caen De Silva <caen@desilva.se>
2023-01-22 12:34:48 +0100

    Create test class to spy on the protected trait methods

--- packages/framework/tests/Feature/TracksExecutionTimeTest.php
+++ packages/framework/tests/Feature/TracksExecutionTimeTest.php
@@ -14,3 +14,22 @@ class TracksExecutionTimeTest extends TestCase
 {
     //
 }
+
+class TracksExecutionTimeTestClass {
+    use TracksExecutionTime;
+
+    public function __call(string $name, array $arguments)
+    {
+        return $this->$name(...$arguments);
+    }
+
+    public function __get(string $name)
+    {
+        return $this->$name;
+    }
+
+    public function isset(string $name): bool
+    {
+        return isset($this->$name);
+    }
+}
