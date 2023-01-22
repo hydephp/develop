92c9ca567b40172449aa7d20270e2a78f543e024
Caen De Silva <caen@desilva.se>
2023-01-22 12:24:04 +0100

    Create TracksExecutionTimeTest.php

---
+++ packages/framework/tests/Feature/TracksExecutionTimeTest.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hyde\Framework\Testing\Feature;
+
+use Hyde\Framework\Concerns\TracksExecutionTime;
+use Hyde\Testing\TestCase;
+
+/**
+ * @covers \Hyde\Framework\Concerns\TracksExecutionTime
+ */
+class TracksExecutionTimeTest extends TestCase
+{
+    //
+}
