f6e36b3decdcff00c6b30807fc3a95ec2f55d8ec
Caen De Silva <caen@desilva.se>
2023-01-23 22:32:18 +0100

    Update navbar brand

--- packages/realtime-compiler/resources/dashboard.blade.php
+++ packages/realtime-compiler/resources/dashboard.blade.php
@@ -10,7 +10,7 @@
 <body>
 <main class="container py-4">
     <nav class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
-        <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3 fs-6" href="#">Company name</a>
+        <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3 fs-6" href="/dashboard">{{ $title }}</a>
         <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
