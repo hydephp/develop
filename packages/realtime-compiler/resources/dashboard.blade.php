adbf4bec0e0818d60a8c303abb64183f71af1a3d
Caen De Silva <caen@desilva.se>
2023-01-23 16:11:07 +0100

    Break footer into pseudo-grid

--- packages/realtime-compiler/resources/dashboard.blade.php
+++ packages/realtime-compiler/resources/dashboard.blade.php
@@ -79,12 +79,14 @@
     </section>
 </main>
 <footer class="bg-light text-center py-3 mt-3">
-    <div class="container d-flex align-items-center justify-content-center">
+    <div class="container d-flex align-items-center justify-content-between">
+        <div class="col-lg-3"></div>
         <div class="col-lg-6">
             <p class="mb-1">
                 Lorem ipsum dolor sit amet.
             </p>
         </div>
+        <div class="col-lg-3"></div>
     </div>
 </footer>
 </body>
