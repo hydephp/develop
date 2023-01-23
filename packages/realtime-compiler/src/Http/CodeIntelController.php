832c43f657e8d45b13b68ff1f45eb35db5fb1149
Caen De Silva <caen@desilva.se>
2023-01-23 13:05:32 +0100

    Boot the application on construct

--- packages/realtime-compiler/src/Http/CodeIntelController.php
+++ packages/realtime-compiler/src/Http/CodeIntelController.php
@@ -4,8 +4,17 @@ declare(strict_types=1);
 
 namespace Hyde\RealtimeCompiler\Http;
 
+use Hyde\RealtimeCompiler\Concerns\InteractsWithLaravel;
+
 class CodeIntelController
 {
+    use InteractsWithLaravel;
+
+    public function __construct()
+    {
+        $this->bootApplication();
+    }
+
     public function show(): string
     {
         return 'Hello World';
