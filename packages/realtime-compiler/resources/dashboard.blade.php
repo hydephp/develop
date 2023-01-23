4babcc9f7ea48bf5ef0ca65ef5fb6ebbcf02758f
Caen De Silva <caen@desilva.se>
2023-01-23 13:24:36 +0100

    Switch to Bootstrap for the layout

--- packages/realtime-compiler/resources/dashboard.blade.php
+++ packages/realtime-compiler/resources/dashboard.blade.php
@@ -2,11 +2,30 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
     <title>{{ $title }}</title>
 </head>
 <body>
-
+<main class="container py-4">
+    <div class="col-xl-10 mx-auto">
+        <header class="px-4 py-5 my-5 text-center bg-light">
+            <h1 class="display-6 fw-bold">{{ $title }}</h1>
+            <div class="mx-auto">
+                <h2 class="h4">Welcome to the dashboard for your HydePHP site.</h2>
+                <p class="lead mb-4">This page is accessible through the Hyde Realtime Compiler and will not be saved to your static site.</p>
+            </div>
+        </header>
+    </div>
+</main>
+<footer class="bg-light text-center py-3 mt-3">
+    <div class="container d-flex align-items-center justify-content-center">
+        <div class="col-lg-6">
+            <p class="mb-1">
+                Lorem ipsum dolor sit amet.
+            </p>
+        </div>
+    </div>
+</footer>
 </body>
 </html>
