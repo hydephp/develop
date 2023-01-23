5d5167c03dde0991a309c33dc16e55b5e21c85bb
Caen De Silva <caen@desilva.se>
2023-01-23 13:16:22 +0100

    Create and inject a virtual page

--- packages/realtime-compiler/src/Http/DashboardController.php
+++ packages/realtime-compiler/src/Http/DashboardController.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 namespace Hyde\RealtimeCompiler\Http;
 
 use Hyde\Framework\Actions\AnonymousViewCompiler;
+use Hyde\Hyde;
+use Hyde\Pages\VirtualPage;
 use Hyde\RealtimeCompiler\Concerns\InteractsWithLaravel;
 
 class DashboardController
@@ -12,14 +14,18 @@ class DashboardController
     use InteractsWithLaravel;
 
     public string $title = 'Dashboard';
+    public VirtualPage $page;
 
     public function __construct()
     {
         $this->bootApplication();
+
+        $this->page = new VirtualPage($this->title);
     }
 
     public function show(): string
     {
+        Hyde::shareViewData($this->page);
         return (new AnonymousViewCompiler(__DIR__.'/../../resources/dashboard.blade.php', (array) $this))->__invoke();
     }
 }
