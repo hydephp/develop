04f9ed8fe47acecee2e917dc2c6382a27d73cd55
Caen De Silva <caen@desilva.se>
2023-01-23 16:18:59 +0100

    Revert "Add info badge icon"
    This reverts commit 989436f84797c0ddc23f25fd5b0c112502a3aac3.

--- packages/realtime-compiler/resources/dashboard.blade.php
+++ packages/realtime-compiler/resources/dashboard.blade.php
@@ -89,7 +89,6 @@
         <div class="col-lg-3">
             @if(\Hyde\RealtimeCompiler\Http\DashboardApiController::enabled())
                 <span class="badge bg-success">Enhanced Mode Enabled</span>
-                <button class="btn btn-info btn-sm rounded-circle">🛈</button>
             @endif
         </div>
     </div>
