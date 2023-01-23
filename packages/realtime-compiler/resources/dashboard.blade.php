9ef0612fe85cd50ad77687a18212d37fb1353dba
Caen De Silva <caen@desilva.se>
2023-01-23 22:34:46 +0100

    Remove navbar toggler

--- packages/realtime-compiler/resources/dashboard.blade.php
+++ packages/realtime-compiler/resources/dashboard.blade.php
@@ -11,9 +11,6 @@
 <main class="container py-4">
     <nav class="navbar navbar-dark bg-dark flex-md-nowrap col-xl-10 mx-auto p-0">
         <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3 fs-6" href="/dashboard">{{ $title }}</a>
-        <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
         <div class="navbar-nav">
             <div class="nav-item text-nowrap">
                 <a class="nav-link px-3" href="/">Back to site</a>
