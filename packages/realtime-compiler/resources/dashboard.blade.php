589fa06a0fec72120bcf6d9187d709937eec0f28
Caen De Silva <caen@desilva.se>
2023-01-23 22:34:09 +0100

    Style navbar to match layout

--- packages/realtime-compiler/resources/dashboard.blade.php
+++ packages/realtime-compiler/resources/dashboard.blade.php
@@ -9,7 +9,7 @@
 </head>
 <body>
 <main class="container py-4">
-    <nav class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
+    <nav class="navbar navbar-dark bg-dark flex-md-nowrap col-xl-10 mx-auto p-0">
         <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3 fs-6" href="/dashboard">{{ $title }}</a>
         <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
