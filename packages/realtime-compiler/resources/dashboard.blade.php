7c70baee9b906091311b02a7fcc09750c85fbb45
Caen De Silva <caen@desilva.se>
2023-01-24 15:12:24 +0100

    Add base target of _parent
    https://stackoverflow.com/a/2656798

--- packages/realtime-compiler/resources/dashboard.blade.php
+++ packages/realtime-compiler/resources/dashboard.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
     <title>{{ $title }}</title>
+    <base target="_parent">
 </head>
 <body class="d-flex flex-column min-vh-100">
 <nav class="navbar navbar-dark bg-dark flex-md-nowrap p-2">
