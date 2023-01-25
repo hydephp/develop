c0bf038ffa4b21e8f5da02f0d6fe305c0597630c
Caen De Silva <caen@desilva.se>
2023-01-25 21:16:43 +0100

    Deprecate config/cache.php

--- config/cache.php
+++ config/cache.php
@@ -1,4 +1,9 @@
 <?php
+/**
+ * @deprecated There should be virtually no reason for this file to edited from within a HydePHP application.
+ *             We should instead attempt to inline the changes here, but provide the ability to load from
+ *             a custom file if one is present.
+ */
 
 return [
     'default' => 'file',
