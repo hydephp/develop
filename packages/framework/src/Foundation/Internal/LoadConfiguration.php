27993074eec34c43cb1fd28a5f576e88dd19e03b
Caen De Silva <caen@desilva.se>
2023-01-26 13:54:30 +0100

    Method override loadConfigurationFiles

--- packages/framework/src/Foundation/Internal/LoadConfiguration.php
+++ packages/framework/src/Foundation/Internal/LoadConfiguration.php
@@ -4,10 +4,25 @@ declare(strict_types=1);
 
 namespace Hyde\Foundation\Internal;
 
+use Exception;
+use Illuminate\Contracts\Config\Repository as RepositoryContract;
+use Illuminate\Contracts\Foundation\Application;
+
 /**
  * @internal
  */
 class LoadConfiguration extends \Illuminate\Foundation\Bootstrap\LoadConfiguration
 {
-    //
+    protected function loadConfigurationFiles(Application $app, RepositoryContract $repository)
+    {
+        $files = $this->getConfigurationFiles($app);
+
+        if (! isset($files['app'])) {
+            throw new Exception('Unable to load the "app" configuration file.');
+        }
+
+        foreach ($files as $key => $path) {
+            $repository->set($key, require $path);
+        }
+    }
 }
