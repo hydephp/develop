d1fd9f7f4a340d9a90847d9d9d65dcba9d459d6e
Caen De Silva <caen@desilva.se>
2023-01-26 13:54:11 +0100

    Create LoadConfiguration.php

---
+++ packages/framework/src/Foundation/Internal/LoadConfiguration.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hyde\Foundation\Internal;
+
+/**
+ * @internal
+ */
+class LoadConfiguration extends \Illuminate\Foundation\Bootstrap\LoadConfiguration
+{
+    //
+}
