9716465d1d9ff53b28bafa5f97219605ffae2f63
Caen De Silva <caen@desilva.se>
2023-01-26 14:08:10 +0100

    Revert "Method override loadConfigurationFiles"
    This reverts commit 27993074eec34c43cb1fd28a5f576e88dd19e03b.

--- packages/framework/src/Foundation/Internal/LoadConfiguration.php
+++ packages/framework/src/Foundation/Internal/LoadConfiguration.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace Hyde\Foundation\Internal;
 
-use Exception;
-use Illuminate\Contracts\Config\Repository as RepositoryContract;
 use Illuminate\Contracts\Foundation\Application;
 
 /**
@@ -13,19 +11,6 @@ use Illuminate\Contracts\Foundation\Application;
  */
 class LoadConfiguration extends \Illuminate\Foundation\Bootstrap\LoadConfiguration
 {
-    protected function loadConfigurationFiles(Application $app, RepositoryContract $repository)
-    {
-        $files = $this->getConfigurationFiles($app);
-
-        if (! isset($files['app'])) {
-            throw new Exception('Unable to load the "app" configuration file.');
-        }
-
-        foreach ($files as $key => $path) {
-            $repository->set($key, require $path);
-        }
-    }
-
     /**
      * Get all of the configuration files for the application.
      *
