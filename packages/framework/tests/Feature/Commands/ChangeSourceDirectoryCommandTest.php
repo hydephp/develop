5de378480dcf8b5f5a07939eede658e1cb8d4855
Caen De Silva <caen@desilva.se>
2023-01-27 14:47:01 +0100

    Create base test

--- packages/framework/tests/Feature/Commands/ChangeSourceDirectoryCommandTest.php
+++ packages/framework/tests/Feature/Commands/ChangeSourceDirectoryCommandTest.php
@@ -11,5 +11,14 @@ use Hyde\Testing\TestCase;
  */
 class ChangeSourceDirectoryCommandTest extends TestCase
 {
-    //
+    public function test_command_moves_source_directories_to_new_supplied_directory_and_updates_the_configuration_file()
+    {
+        $this->artisan('change:sourceDirectory test')
+            ->expectsOutput('Setting [test] as the project source directory!')
+            ->expectsOutput('Creating directory')
+            ->expectsOutput('Moving source directories')
+            ->expectsOutput('Updating configuration file')
+            ->expectsOutput('All done!')
+            ->assertExitCode(0);
+    }
 }
