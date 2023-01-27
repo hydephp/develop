764b2f577a6ee1378412c22dcd09d71579cacbce
Caen De Silva <caen@desilva.se>
2023-01-27 15:49:18 +0100

    Test with missing config search string

--- packages/framework/tests/Feature/Commands/ChangeSourceDirectoryCommandTest.php
+++ packages/framework/tests/Feature/Commands/ChangeSourceDirectoryCommandTest.php
@@ -51,6 +51,45 @@ class ChangeSourceDirectoryCommandTest extends TestCase
         Filesystem::putContents('config/hyde.php', $config);
     }
 
+    public function test_with_missing_config_search_string()
+    {
+        $this->file('_pages/tracker.txt', 'This should be moved to the new location');
+
+        $config = Filesystem::getContents('config/hyde.php');
+        $config = str_replace("'source_root' => '',", "'no_source_root' => '',", $config);
+        Filesystem::putContents('config/hyde.php', $config);
+
+        $this->artisan('change:sourceDirectory test')
+            ->expectsOutput('Setting [test] as the project source directory!')
+            ->expectsOutput('Creating directory')
+            ->expectsOutput('Moving source directories')
+            ->expectsOutput('Updating configuration file')
+            ->expectsOutput("Automatic configuration update failed, to finalize the change, please set the `source_root` setting to 'test' in `config/hyde.php`")
+            ->expectsOutput('All done!')
+            ->assertExitCode(0);
+
+        $this->assertDirectoryDoesNotExist(Hyde::path('_pages'));
+        $this->assertDirectoryDoesNotExist(Hyde::path('_posts'));
+        $this->assertDirectoryDoesNotExist(Hyde::path('_docs'));
+
+        $this->assertDirectoryExists(Hyde::path('test/_pages'));
+        $this->assertDirectoryExists(Hyde::path('test/_posts'));
+        $this->assertDirectoryExists(Hyde::path('test/_docs'));
+
+        $this->assertFileExists(Hyde::path('test/_pages/tracker.txt'));
+        $this->assertSame('This should be moved to the new location',
+            file_get_contents(Hyde::path('test/_pages/tracker.txt'))
+        );
+
+        Filesystem::moveDirectory('test/_pages', '_pages');
+        Filesystem::moveDirectory('test/_posts', '_posts');
+        Filesystem::moveDirectory('test/_docs', '_docs');
+
+        $config = Filesystem::getContents('config/hyde.php');
+        $config = str_replace("'no_source_root' => '',", "'source_root' => '',", $config);
+        Filesystem::putContents('config/hyde.php', $config);
+    }
+
     public function test_with_name_matching_current_value()
     {
         $this->artisan('change:sourceDirectory /')
