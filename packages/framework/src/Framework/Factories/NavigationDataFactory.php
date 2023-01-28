dcbaadeabf27b61786567fd2f5216ade1bf8e44d
Caen De Silva <caen@desilva.se>
2023-01-28 13:47:59 +0100

    Hide docs/index from sidebar
    Fixes https://github.com/hydephp/develop/issues/867#issuecomment-1407357015

--- packages/framework/src/Framework/Factories/NavigationDataFactory.php
+++ packages/framework/src/Framework/Factories/NavigationDataFactory.php
@@ -184,6 +184,10 @@ class NavigationDataFactory extends Concerns\PageDataFactory implements Navigati
 
     private function determineIfDocumentationPageIsHiddenFromSidebar(): bool
     {
+        if ($this->isInstanceOf(DocumentationPage::class) && $this->page->identifier === 'docs/index') {
+            return true;
+        }
+
         return ! $this->isInstanceOf(DocumentationPage::class) && $this->pageIsInSubdirectory() && ($this->getSubdirectoryConfiguration() === 'hidden');
     }
 
