8bb7d1e05698d99ac38ef70027bab387c65483f6
Caen De Silva <caen@desilva.se>
2023-01-29 22:32:40 +0100

    Inline argument

--- packages/framework/src/Console/Commands/ChangeSourceDirectoryCommand.php
+++ packages/framework/src/Console/Commands/ChangeSourceDirectoryCommand.php
@@ -79,7 +79,7 @@ class ChangeSourceDirectoryCommand extends Command
     protected function getValidatedName(string $name): string
     {
         $this->validateName($name);
-        $this->validateDirectoryCanBeUsed($name, $this->getPageDirectories());
+        $this->validateDirectoryCanBeUsed($name);
         $this->infoComment('Setting', $name, 'as the project source directory!');
 
         return $name;
@@ -92,7 +92,7 @@ class ChangeSourceDirectoryCommand extends Command
         }
     }
 
-    protected function validateDirectoryCanBeUsed(string $name, array $pageDirectories): void
+    protected function validateDirectoryCanBeUsed(string $name): void
     {
         if (Filesystem::isFile($name)) {
             throw new InvalidArgumentException('A file already exists at this path!');
@@ -101,7 +101,7 @@ class ChangeSourceDirectoryCommand extends Command
         if (Filesystem::isDirectory($name) && ! Filesystem::isEmptyDirectory($name)) {
             // If any of the subdirectories we want to move already exist, we need to abort as we don't want to overwrite any existing files
             // The reason we check these individually is mainly so that the change can be reverted (by setting the $name to '/')
-            foreach ($pageDirectories as $directory) {
+            foreach ($this->getPageDirectories() as $directory) {
                 $subdirectoryPath = $this->assembleSubdirectoryPath($name, $directory);
                 if (is_file($subdirectoryPath) || $this->directoryContainsFiles($subdirectoryPath)) {
                     throw new InvalidArgumentException('Directory already exists!');
