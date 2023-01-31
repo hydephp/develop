15d14a2bdb2f28bda4d3037b3e965897747acba1
Caen De Silva <caen@desilva.se>
2023-01-31 19:33:55 +0100

    Test it removes headings

--- packages/framework/tests/Feature/Actions/ConvertsMarkdownToPlainTextTest.php
+++ packages/framework/tests/Feature/Actions/ConvertsMarkdownToPlainTextTest.php
@@ -12,6 +12,29 @@ use Hyde\Testing\TestCase;
  */
 class ConvertsMarkdownToPlainTextTest extends TestCase
 {
+    public function testItRemovesHeadings()
+    {
+        $markdown = <<<'MD'
+        # Heading level 1       
+        ## Heading level 2	       
+        ### Heading level 3	       
+        #### Heading level 4	       
+        ##### Heading level 5	       
+        ###### Heading level 6
+        MD;
+
+        $text = <<<'TXT'
+        Heading level 1
+        Heading level 2
+        Heading level 3
+        Heading level 4
+        Heading level 5
+        Heading level 6
+        TXT;
+
+        $this->assertSame($text, $this->convert($markdown));
+    }
+
     protected function convert(string $markdown): string
     {
         return (new ConvertsMarkdownToPlainText($markdown))->execute();
