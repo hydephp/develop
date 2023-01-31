5335d1739f8fbaa5c2280cf01157d7e78412a800
Caen De Silva <caen@desilva.se>
2023-01-31 17:11:13 +0100

    Add class PHPDoc

--- packages/framework/src/Framework/Actions/ConvertsMarkdownToPlainText.php
+++ packages/framework/src/Framework/Actions/ConvertsMarkdownToPlainText.php
@@ -4,6 +4,11 @@ declare(strict_types=1);
 
 namespace Hyde\Framework\Actions;
 
+/**
+ * Converts Markdown to plain text.
+ *
+ * @see \Hyde\Framework\Testing\Feature\Actions\ConvertsMarkdownToPlainTextTest
+ */
 class ConvertsMarkdownToPlainText
 {
     //
