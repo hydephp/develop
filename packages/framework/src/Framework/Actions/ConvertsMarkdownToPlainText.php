39f44eb76c67be3c3f3bac6a138ab28b4cdc92b7
Caen De Silva <caen@desilva.se>
2023-01-31 21:53:15 +0100

    Remove overlapping pattern covered by ATX rule

--- packages/framework/src/Framework/Actions/ConvertsMarkdownToPlainText.php
+++ packages/framework/src/Framework/Actions/ConvertsMarkdownToPlainText.php
@@ -21,9 +21,6 @@ use function strip_tags;
  */
 class ConvertsMarkdownToPlainText
 {
-    /** Classic headers */
-    protected const HEADERS = ['/\n={2,}/' => "\n"];
-
     /** Atx-style headers */
     protected const ATX_HEADERS = ['/^(\n)?\s{0,}#{1,6}\s+| {0,}(\n)?\s{0,}#{0,} {0,}(\n)?\s{0,}$/m' => '$1$2$3'];
 
@@ -88,7 +85,6 @@ class ConvertsMarkdownToPlainText
         $markdown = strip_tags($this->markdown);
 
         $patterns = [
-            static::HEADERS,
             static::ATX_HEADERS,
             static::SETEXT_HEADERS,
             static::HORIZONTAL_RULES,
