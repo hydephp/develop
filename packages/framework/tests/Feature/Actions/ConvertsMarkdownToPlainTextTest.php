85a81701aa9876a7ff2caafa53ee07cbabe42647
Caen De Silva <caen@desilva.se>
2023-01-31 20:26:35 +0100

    Change method name semantics

--- packages/framework/tests/Feature/Actions/ConvertsMarkdownToPlainTextTest.php
+++ packages/framework/tests/Feature/Actions/ConvertsMarkdownToPlainTextTest.php
@@ -286,7 +286,7 @@ class ConvertsMarkdownToPlainTextTest extends TestCase
         $this->assertSame($text, $this->convert($markdown));
     }
 
-    public function testItRemovesLinks()
+    public function testItUnwrapsLinks()
     {
         $markdown = <<<'MD'
         My favorite search engine is [Duck Duck Go](https://duckduckgo.com).
@@ -322,7 +322,7 @@ class ConvertsMarkdownToPlainTextTest extends TestCase
         $this->assertSame($text, $this->convert($markdown));
     }
 
-    public function testItRemovesImages()
+    public function testItUnwrapsImages()
     {
         $markdown = <<<'MD'
         ![The San Juan Mountains are beautiful!](/assets/images/san-juan-mountains.jpg)
@@ -337,7 +337,7 @@ class ConvertsMarkdownToPlainTextTest extends TestCase
         $this->assertSame($text, $this->convert($markdown));
     }
 
-    public function testItRemovesLinkingImages()
+    public function testItUnwrapsLinkingImages()
     {
         $markdown = <<<'MD'
         [![An old rock in the desert](/assets/images/shiprock.jpg "Shiprock, New Mexico by Beau Rogers")](https://www.flickr.com/photos/beaurogers/31833779864/in/photolist-Qv3rFw-34mt9F-a9Cmfy-5Ha3Zi-9msKdv-o3hgjr-hWpUte-4WMsJ1-KUQ8N-deshUb-vssBD-6CQci6-8AFCiD-zsJWT-nNfsgB-dPDwZJ-bn9JGn-5HtSXY-6CUhAL-a4UTXB-ugPum-KUPSo-fBLNm-6CUmpy-4WMsc9-8a7D3T-83KJev-6CQ2bK-nNusHJ-a78rQH-nw3NvT-7aq2qf-8wwBso-3nNceh-ugSKP-4mh4kh-bbeeqH-a7biME-q3PtTf-brFpgb-cg38zw-bXMZc-nJPELD-f58Lmo-bXMYG-bz8AAi-bxNtNT-bXMYi-bXMY6-bXMYv)
