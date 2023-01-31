9b2d86d33af42fc072c0a91cf7b88df942dfbe93
Caen De Silva <caen@desilva.se>
2023-01-31 21:45:29 +0100

    Invert constant index

--- packages/framework/src/Framework/Actions/ConvertsMarkdownToPlainText.php
+++ packages/framework/src/Framework/Actions/ConvertsMarkdownToPlainText.php
@@ -40,10 +40,10 @@ class ConvertsMarkdownToPlainText
     protected const CODE_BLOCKS = ['/(`{3,})(.*?)\1/m' => '$2'];
 
     /** Fenced codeblocks (~~~) */
-    protected const FENCED_CODEBLOCKS = ['/~{3}.*\n/' => ''];
+    protected const FENCED_CODEBLOCKS_2 = ['/~{3}.*\n/' => ''];
 
     /** Fenced codeblocks (```) */
-    protected const FENCED_CODEBLOCKS_2 = ['/`{3}.*\n/' => ''];
+    protected const FENCED_CODEBLOCKS = ['/`{3}.*\n/' => ''];
 
     /** Fenced end tags */
     protected const FENCED_END_TAGS = ['/`{3}/' => ''];
