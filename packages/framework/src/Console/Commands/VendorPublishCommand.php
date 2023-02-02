8e599cac5fb7b1332f06e155e0221f8ebdee4108
Caen De Silva <caen@desilva.se>
2023-02-02 18:17:27 +0100

    Create VendorPublishCommand.php

---
+++ packages/framework/src/Console/Commands/VendorPublishCommand.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hyde\Console\Commands;
+
+use Illuminate\Foundation\Console\VendorPublishCommand as BaseCommand;
+
+class VendorPublishCommand extends BaseCommand
+{
+    //
+}
