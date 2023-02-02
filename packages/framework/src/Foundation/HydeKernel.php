37c40963ea131dab288ad655f347689671bbe421
Caen De Silva <caen@desilva.se>
2023-02-02 11:30:45 +0100

    Remove psalm-return array shape

--- packages/framework/src/Foundation/HydeKernel.php
+++ packages/framework/src/Foundation/HydeKernel.php
@@ -89,7 +89,6 @@ class HydeKernel implements SerializableContract
      * @todo Add source root and output path to the array?
      *
      * @inheritDoc
-     * @psalm-return array{basePath: string, features: \Hyde\Facades\Features, pages: \Hyde\Foundation\PageCollection, routes: \Hyde\Foundation\RouteCollection}
      */
     public function toArray(): array
     {
