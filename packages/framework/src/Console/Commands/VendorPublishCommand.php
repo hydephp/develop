6917c82a2c4afe390ed5e3bd3e907aa1b5ecbd99
Caen De Silva <caen@desilva.se>
2023-02-03 14:14:25 +0100

    Revert all style overrides as it's not worth the complexity

--- packages/framework/src/Console/Commands/VendorPublishCommand.php
+++ packages/framework/src/Console/Commands/VendorPublishCommand.php
@@ -4,64 +4,14 @@ declare(strict_types=1);
 
 namespace Hyde\Console\Commands;
 
-use Hyde\Hyde;
 use Illuminate\Foundation\Console\VendorPublishCommand as BaseCommand;
 
 /**
  * Publish any publishable assets from vendor packages.
  *
- * @property \Illuminate\Console\View\Components\Factory $illuminateComponents
  * @see \Hyde\Framework\Testing\Feature\Commands\VendorPublishCommandTest
  */
 class VendorPublishCommand extends BaseCommand
 {
-    public function handle()
-    {
-        $this->illuminateComponents = $this->components;
-        $this->components = $this->voidComponents();
-
-        parent::handle();
-    }
-
-    /**
-     * Write a status message to the console.
-     *
-     * @param  string  $from
-     * @param  string  $to
-     * @param  string  $type
-     * @return void
-     */
-    protected function status($from, $to, $type): void
-    {
-        $from = ltrim(Hyde::pathToRelative(str_replace(base_path(), '', realpath($from))), '/\\');
-
-        $to = ltrim(Hyde::pathToRelative(str_replace(base_path(), '', realpath($to))), '/\\');
-
-        $this->line('<info>Copied '.$type.'</info> <comment>['.$from.']</comment> <info>To</info> <comment>['.$to.']</comment>');
-    }
-
-    /**
-     * Dynamically revert to the same output style as our other commands.
-     */
-    protected function voidComponents(): object
-    {
-        return new class($this) {
-            protected VendorPublishCommand $command;
-
-            public function __construct(VendorPublishCommand $command)
-            {
-                $this->command = $command;
-            }
-
-            public function __call($method, $parameters)
-            {
-                if (method_exists($this->command, $method)) {
-                    return $this->command->$method(...$parameters);
-                }
-
-                return $this->command->illuminateComponents->$method(...$parameters);
-            }
-        };
-    }
+    //
 }
-
