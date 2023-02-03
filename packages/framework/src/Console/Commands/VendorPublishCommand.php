966085883667145a24fcaf34fba01b9350546c50
Caen De Silva <caen@desilva.se>
2023-02-03 16:36:29 +0100

    Remove now unused helpers and overrides

--- packages/framework/src/Console/Commands/VendorPublishCommand.php
+++ packages/framework/src/Console/Commands/VendorPublishCommand.php
@@ -33,36 +33,4 @@ class VendorPublishCommand extends BaseCommand
         ServiceProvider::$publishes = $originalPublishers;
         ServiceProvider::$publishGroups = $originalGroups;
     }
-
-    protected function publishableChoices(): array
-    {
-        $array = parent::publishableChoices();
-
-        $array = $this->replaceByValue($array, '<fg=gray>Tag:</> config', '<fg=gray>Tag:</> config (Vendor Configs)');
-
-        return $this->withoutProvider($array, LaravelConsoleSummaryServiceProvider::class);
-    }
-
-    protected function withoutProvider(array $array, string $provider): array
-    {
-        return $this->unsetByValue($array, "<fg=gray>Provider:</> $provider");
-    }
-
-    protected function unsetByValue(array &$array, string $value): array
-    {
-        $key = array_search($value, $array);
-        if ($key !== false) {
-            unset($array[$key]);
-        }
-        return $array;
-    }
-
-    protected function replaceByValue(array &$array, string $value, string $newValue): array
-    {
-        $key = array_search($value, $array);
-        if ($key !== false) {
-            $array[$key] = $newValue;
-        }
-        return $array;
-    }
 }
