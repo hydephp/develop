44e66b451e84df38b87ac1bdf0a9dfd288c02431
Caen De Silva <caen@desilva.se>
2023-02-03 13:44:31 +0100

    Test command prompts for provider or tag

--- packages/framework/tests/Feature/Commands/VendorPublishCommandTest.php
+++ packages/framework/tests/Feature/Commands/VendorPublishCommandTest.php
@@ -5,11 +5,28 @@ declare(strict_types=1);
 namespace Hyde\Framework\Testing\Feature\Commands;
 
 use Hyde\Testing\TestCase;
+use Illuminate\Support\ServiceProvider;
 
 /**
  * @covers \Hyde\Console\Commands\VendorPublishCommand
  */
 class VendorPublishCommandTest extends TestCase
 {
-    //
+    public function test_command_prompts_for_provider_or_tag()
+    {
+        ServiceProvider::$publishes = [
+            'ExampleProvider' => '',
+        ];
+        ServiceProvider::$publishGroups = [
+            'example-configs' => [],
+        ];
+
+        $this->artisan('vendor:publish')
+            ->expectsChoice('Which provider or tag\'s files would you like to publish?', 'Tag: example-configs', [
+                '<comment>Publish files from all providers and tags listed below</comment>',
+                '<fg=gray>Provider:</> ExampleProvider',
+                '<fg=gray>Tag:</> example-configs',
+            ])
+            ->assertExitCode(0);
+    }
 }
