7968dcaf98e43413bf113f4915c8f301227fbadf
Caen De Silva <caen@desilva.se>
2023-02-03 22:53:04 +0100

    Flip '?:'

--- packages/framework/src/Console/Commands/PublishViewsCommand.php
+++ packages/framework/src/Console/Commands/PublishViewsCommand.php
@@ -74,7 +74,7 @@ class PublishViewsCommand extends Command
 
         $choice = $this->parseChoiceIntoKey($choice);
 
-        $this->infoComment(sprintf("Selected category [%s]\n", empty($choice) ? 'all' : $choice));
+        $this->infoComment(sprintf("Selected category [%s]\n", !empty($choice) ? $choice : 'all'));
 
         return $choice;
     }
