07e015e6010fb3805da70b2745830786dc6006b4
Caen De Silva <caen@desilva.se>
2023-02-03 20:37:38 +0100

    Revert "Revert "Override status method""
    This reverts commit 85da432bb4c2b7efa762778c11e239071d1a012f.

--- packages/framework/src/Console/Commands/VendorPublishCommand.php
+++ packages/framework/src/Console/Commands/VendorPublishCommand.php
@@ -4,9 +4,16 @@ declare(strict_types=1);
 
 namespace Hyde\Console\Commands;
 
+use Hyde\Hyde;
 use Illuminate\Foundation\Console\VendorPublishCommand as BaseCommand;
 use Illuminate\Support\ServiceProvider;
 use NunoMaduro\LaravelConsoleSummary\LaravelConsoleSummaryServiceProvider;
+
+use function base_path;
+use function ltrim;
+use function realpath;
+use function sprintf;
+use function str_replace;
 
 /**
  * Publish any publishable assets from vendor packages.
@@ -36,5 +43,27 @@ class VendorPublishCommand extends BaseCommand
 
         ServiceProvider::$publishes = $originalPublishers;
         ServiceProvider::$publishGroups = $originalGroups;
+    }
+
+    /**
+     * Write a status message to the console.
+     *
+     * @param  string  $from
+     * @param  string  $to
+     * @param  string  $type
+     * @return void
+     */
+    protected function status($from, $to, $type): void
+    {
+        $from = ltrim(Hyde::pathToRelative(str_replace(base_path(), '', realpath($from))), '/\\');
+
+        $to = ltrim(Hyde::pathToRelative(str_replace(base_path(), '', realpath($to))), '/\\');
+
+        $this->components->task(sprintf(
+            'Copying %s [%s] to [%s]',
+            $type,
+            $from,
+            $to,
+        ));
     }
 }
