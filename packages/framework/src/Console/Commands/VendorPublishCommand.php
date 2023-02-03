8f32a6593e5eb85eac2dc598b0a40e71a499fd5e
Caen De Silva <caen@desilva.se>
2023-02-03 13:26:19 +0100

    Add command PHPDoc

--- packages/framework/src/Console/Commands/VendorPublishCommand.php
+++ packages/framework/src/Console/Commands/VendorPublishCommand.php
@@ -6,6 +6,11 @@ namespace Hyde\Console\Commands;
 
 use Illuminate\Foundation\Console\VendorPublishCommand as BaseCommand;
 
+/**
+ * Publish any publishable assets from vendor packages.
+ *
+ * @see \Hyde\Framework\Testing\Feature\Commands\VendorPublishCommandTest
+ */
 class VendorPublishCommand extends BaseCommand
 {
     /**
