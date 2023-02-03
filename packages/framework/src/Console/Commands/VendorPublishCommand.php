3649d8c8103e31e3779e3b2f9b63c30fb9f7a41d
Caen De Silva <caen@desilva.se>
2023-02-03 15:16:39 +0100

    Override status method

--- packages/framework/src/Console/Commands/VendorPublishCommand.php
+++ packages/framework/src/Console/Commands/VendorPublishCommand.php
@@ -6,6 +6,11 @@ namespace Hyde\Console\Commands;
 
 use Illuminate\Foundation\Console\VendorPublishCommand as BaseCommand;
 
+use function base_path;
+use function realpath;
+use function sprintf;
+use function str_replace;
+
 /**
  * Publish any publishable assets from vendor packages.
  *
@@ -13,5 +18,25 @@ use Illuminate\Foundation\Console\VendorPublishCommand as BaseCommand;
  */
 class VendorPublishCommand extends BaseCommand
 {
-    //
+    /**
+     * Write a status message to the console.
+     *
+     * @param  string  $from
+     * @param  string  $to
+     * @param  string  $type
+     * @return void
+     */
+    protected function status($from, $to, $type)
+    {
+        $from = str_replace(base_path().'/', '', realpath($from));
+
+        $to = str_replace(base_path().'/', '', realpath($to));
+
+        $this->components->task(sprintf(
+            'Copying %s [%s] to [%s]',
+            $type,
+            $from,
+            $to,
+        ));
+    }
 }
