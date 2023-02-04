f8fe20740a71e747514db005ef3aa7e6108f28ec
Caen De Silva <caen@desilva.se>
2023-02-04 13:21:28 +0100

    Only pass along output if there will be a conflict, as it's only needed then

--- packages/framework/src/Console/Commands/PublishHomepageCommand.php
+++ packages/framework/src/Console/Commands/PublishHomepageCommand.php
@@ -70,7 +70,7 @@ class PublishHomepageCommand extends Command
         Artisan::call('vendor:publish', [
             '--tag' => $this->options[$selected]['group'] ?? $selected,
             '--force' => true, // Todo add force state dynamically depending on existing file state
-        ], $this->output);
+        ], $willConflict ? $this->output : null);
 
         if (! $willConflict) {
             $this->line("<info>Published page</info> [<comment>$selected</comment>]");
