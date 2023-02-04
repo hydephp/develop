d38e7ac254b14c91396e5ed95452634c4b454885
Caen De Silva <caen@desilva.se>
2023-02-04 11:51:09 +0100

    Deprecate internal experimental class Homepages.php

--- packages/framework/src/Framework/Features/Templates/Homepages.php
+++ packages/framework/src/Framework/Features/Templates/Homepages.php
@@ -7,6 +7,7 @@ namespace Hyde\Framework\Features\Templates;
 use Illuminate\Support\Collection;
 
 /**
+ * @deprecated May be replaced by vendor:publish in the future.
  * @internal This class is currently experimental and should not be relied upon outside the framework as it may change at any time.
  * @experimental
  */
