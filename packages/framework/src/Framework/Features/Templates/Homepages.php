61c5ff466b543b7e0145a418216cd65964f73b7d
Caen De Silva <caen@desilva.se>
2023-02-04 12:14:18 +0100

    Inline anonymous class creator methods

--- packages/framework/src/Framework/Features/Templates/Homepages.php
+++ packages/framework/src/Framework/Features/Templates/Homepages.php
@@ -17,9 +17,24 @@ final class Homepages
     public static function options(): Collection
     {
         return new Collection([
-            'welcome' => self::welcome(),
-            'posts' => self::posts(),
-            'blank' => self::blank(),
+            'welcome' => new class extends PublishableView {
+                protected static string $title = 'Welcome';
+                protected static string $desc = 'The default welcome page.';
+                protected static string $path = 'resources/views/homepages/welcome.blade.php';
+                protected static ?string $outputPath = 'index.blade.php';
+            },
+            'posts' => new class extends PublishableView {
+                protected static string $title = 'Posts Feed';
+                protected static string $desc = 'A feed of your latest posts. Perfect for a blog site!';
+                protected static string $path = 'resources/views/homepages/post-feed.blade.php';
+                protected static ?string $outputPath = 'index.blade.php';
+            },
+            'blank' => new class extends PublishableView {
+                protected static string $title = 'Blank Starter';
+                protected static string $desc = 'A blank Blade template with just the base layout.';
+                protected static string $path = 'resources/views/homepages/blank.blade.php';
+                protected static ?string $outputPath = 'index.blade.php';
+            },
         ]);
     }
 
@@ -33,36 +48,4 @@ final class Homepages
         return self::options()->get($page);
     }
 
-    public static function welcome(): PublishableContract
-    {
-        return new class extends PublishableView
-        {
-            protected static string $title = 'Welcome';
-            protected static string $desc = 'The default welcome page.';
-            protected static string $path = 'resources/views/homepages/welcome.blade.php';
-            protected static ?string $outputPath = 'index.blade.php';
-        };
-    }
-
-    public static function posts(): PublishableContract
-    {
-        return new class extends PublishableView
-        {
-            protected static string $title = 'Posts Feed';
-            protected static string $desc = 'A feed of your latest posts. Perfect for a blog site!';
-            protected static string $path = 'resources/views/homepages/post-feed.blade.php';
-            protected static ?string $outputPath = 'index.blade.php';
-        };
-    }
-
-    public static function blank(): PublishableContract
-    {
-        return new class extends PublishableView
-        {
-            protected static string $title = 'Blank Starter';
-            protected static string $desc = 'A blank Blade template with just the base layout.';
-            protected static string $path = 'resources/views/homepages/blank.blade.php';
-            protected static ?string $outputPath = 'index.blade.php';
-        };
-    }
 }
