920fb2127ffb217f284750544a09eeca7f67b2c6
Caen De Silva <caen@desilva.se>
2023-02-04 14:53:43 +0100

    Extend our command class

--- packages/framework/src/Console/Commands/PublishHomepageCommand.php
+++ packages/framework/src/Console/Commands/PublishHomepageCommand.php
@@ -5,12 +5,12 @@ declare(strict_types=1);
 namespace Hyde\Console\Commands;
 
 use Hyde\Console\Concerns\AsksToRebuildSite;
+use Hyde\Console\Concerns\Command;
 use Hyde\Framework\Features\Templates\Homepages;
 use Hyde\Framework\Services\ChecksumService;
 use Hyde\Hyde;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Artisan;
-use LaravelZero\Framework\Commands\Command;
 use function array_key_exists;
 use function file_exists;
 use function str_replace;
