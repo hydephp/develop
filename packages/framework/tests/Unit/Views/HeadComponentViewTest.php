0491b883d4778bb15f9b1ec9a66c58455c7ce0f3
Caen De Silva <caen@desilva.se>
2023-02-05 18:51:03 +0100

    Create render testing helper

--- packages/framework/tests/Unit/Views/HeadComponentViewTest.php
+++ packages/framework/tests/Unit/Views/HeadComponentViewTest.php
@@ -4,12 +4,24 @@ declare(strict_types=1);
 
 namespace Hyde\Framework\Testing\Unit\Views;
 
+use Hyde\Hyde;
+use Hyde\Pages\VirtualPage;
 use Hyde\Testing\TestCase;
+use Illuminate\Support\Facades\Blade;
 
 /**
  * @see resources/views/layouts/styles.blade.php
  */
 class HeadComponentViewTest extends TestCase
 {
-    //
+    protected ?string $mockCurrentPage = null;
+
+    protected function renderTestView(): string
+    {
+        $this->mockPage(new VirtualPage($this->mockCurrentPage ?? ''));
+
+        return Blade::render(file_get_contents(
+            Hyde::vendorPath('resources/views/layouts/head.blade.php')
+        ));
+    }
 }
