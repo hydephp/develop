fba5f486fe8d982a73bba1d6e0c960f790989f46
Caen De Silva <caen@desilva.se>
2023-02-05 18:50:25 +0100

    Create HeadComponentViewTest.php

---
+++ packages/framework/tests/Unit/Views/HeadComponentViewTest.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hyde\Framework\Testing\Unit\Views;
+
+use Hyde\Testing\TestCase;
+
+/**
+ * @see resources/views/layouts/styles.blade.php
+ */
+class HeadComponentViewTest extends TestCase
+{
+    //
+}
