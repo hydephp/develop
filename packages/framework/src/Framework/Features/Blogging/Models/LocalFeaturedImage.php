9cebefb38dbffc0ef058859051b05366062bbdb2
Caen De Silva <caen@desilva.se>
2023-02-05 20:49:43 +0100

    LocalFeaturedImage now expects all referenced files to be in the _media directory

--- packages/framework/src/Framework/Features/Blogging/Models/LocalFeaturedImage.php
+++ packages/framework/src/Framework/Features/Blogging/Models/LocalFeaturedImage.php
@@ -16,6 +16,8 @@ use function substr;
  * The internal data structure forces the image source to reference a file in the _media directory,
  * and thus that is what is required for the input. However, when outputting data, the data will
  * be used for the _site/media directory, so it will provide data relative to the site root.
+ *
+ * The source information is stored in $this->source, which is a file in the _media directory.
  */
 class LocalFeaturedImage extends FeaturedImage
 {
