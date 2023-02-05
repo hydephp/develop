6b9b9c99df524c83f3bef6d19c21fa89f9d49ee4
Caen De Silva <caen@desilva.se>
2023-02-05 15:05:26 +0100

    Add class PHPDoc

--- packages/framework/tests/Unit/MediaDirectoryCanBeChangedTest.php
+++ packages/framework/tests/Unit/MediaDirectoryCanBeChangedTest.php
@@ -6,6 +6,13 @@ namespace Hyde\Framework\Testing\Unit;
 
 use Hyde\Testing\TestCase;
 
+/**
+ * High level test of the feature that allows the media source (_media) directory,
+ * and the media output directory (_site/media) to be changed.
+ *
+ * @see \Hyde\Framework\Testing\Unit\BuildOutputDirectoryCanBeChangedTest
+ * @see \Hyde\Framework\Testing\Feature\ConfigurableSourceRootsFeatureTest
+ */
 class MediaDirectoryCanBeChangedTest extends TestCase
 {
     //
