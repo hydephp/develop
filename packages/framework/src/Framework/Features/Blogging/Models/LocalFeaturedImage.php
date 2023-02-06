1d19071556e59ee85fc91197f19abbde050709af
Caen De Silva <caen@desilva.se>
2023-02-06 11:48:19 +0100

    Revert "Add ReturnTypeWillChange attribute"
    This reverts commit d0bffd012dd6869999bb0988d5646b0e7b310c8c.

--- packages/framework/src/Framework/Features/Blogging/Models/LocalFeaturedImage.php
+++ packages/framework/src/Framework/Features/Blogging/Models/LocalFeaturedImage.php
@@ -7,7 +7,6 @@ namespace Hyde\Framework\Features\Blogging\Models;
 use Hyde\Framework\Exceptions\FileNotFoundException;
 use Hyde\Hyde;
 use Illuminate\Support\Str;
-use ReturnTypeWillChange;
 
 /**
  * A featured image object, for a file stored locally.
@@ -29,7 +28,6 @@ class LocalFeaturedImage extends FeaturedImage
         return Str::after($source, Hyde::getMediaDirectory().'/');
     }
 
-    #[ReturnTypeWillChange /** Todo: I don't think this should return a relativeLink */]
     public function getSource(): string
     {
         // Return value is relative to the site's root.
