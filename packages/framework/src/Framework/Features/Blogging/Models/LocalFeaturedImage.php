a942359044879392f697edfad56147a35d2dd90a
Caen De Silva <caen@desilva.se>
2023-02-06 13:14:59 +0100

    Revert "Add todo"
    This reverts commit 10130ad520eaecb542e736bb872aa42ba35717df.

--- packages/framework/src/Framework/Features/Blogging/Models/LocalFeaturedImage.php
+++ packages/framework/src/Framework/Features/Blogging/Models/LocalFeaturedImage.php
@@ -39,7 +39,6 @@ class LocalFeaturedImage extends FeaturedImage
         return filesize($this->validatedStoragePath());
     }
 
-    /** @todo Consider returning relative path */
     protected function storagePath(): string
     {
         return Hyde::path(Hyde::getMediaDirectory()."/$this->source");
