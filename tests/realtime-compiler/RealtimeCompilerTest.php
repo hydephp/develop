60749db855678b727f625a14701c8c584af6fd6d
Caen De Silva <caen@desilva.se>
2023-02-07 10:55:22 +0100

    Expect file to be created and equal response

--- tests/realtime-compiler/RealtimeCompilerTest.php
+++ tests/realtime-compiler/RealtimeCompilerTest.php
@@ -20,6 +20,9 @@ test('handle routes index page', function () {
     expect($response->statusCode)->toBe(200);
     expect($response->statusMessage)->toBe('OK');
     expect($response->body)->toContain('<title>Welcome to HydePHP!</title>');
+
+    expect(hyde()->path('_site/index.html'))->toBeFile()
+        ->and(file_get_contents(hyde()->path('_site/index.html')))->toBe($response->body);
 });
 
 test('handle routes custom pages', function () {
