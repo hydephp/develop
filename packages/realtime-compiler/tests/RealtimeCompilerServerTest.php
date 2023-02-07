5b6cb73bd7e745d5aeb1c9ec5793e3e5cb4bfecc
Caen De Silva <caen@desilva.se>
2023-02-07 18:13:11 +0100

    Add server binary tests

--- packages/realtime-compiler/tests/RealtimeCompilerServerTest.php
+++ packages/realtime-compiler/tests/RealtimeCompilerServerTest.php
@@ -1,3 +1,21 @@
 <?php
 
-//
+ob_start();
+
+test('server binary exists', function () {
+    expect(__DIR__.'/../bin/server.php')->toBeFile();
+});
+
+test('server binary is executable', function () {
+    ob_start();
+
+    $_SERVER['REQUEST_METHOD'] = 'GET';
+    $_SERVER['REQUEST_URI'] = '/';
+
+    $server = require __DIR__.'/../bin/server.php';
+
+    $output = ob_get_clean();
+
+    expect($output)->toContain('Welcome to HydePHP!');
+    expect($server)->toBeInt()->and($server)->toBe(1);
+});
