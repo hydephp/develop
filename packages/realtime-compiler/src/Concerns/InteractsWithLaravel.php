bfada82b19dadd90480f8709bc1c3dbac486c1f8
Caen De Silva <caen@desilva.se>
2023-02-07 16:49:57 +0100

    Update trait documentation

--- packages/realtime-compiler/src/Concerns/InteractsWithLaravel.php
+++ packages/realtime-compiler/src/Concerns/InteractsWithLaravel.php
@@ -6,9 +6,11 @@ use Illuminate\Contracts\Console\Kernel;
 use Illuminate\Foundation\Application;
 
 /**
- * Provides methods to interact with the Laravel (Hyde)
- * application instance. This is needed to bootstrap
- * core Hyde services, for example page compilers.
+ * Provides methods to interact with the Laravel (Hyde) application instance.
+ * This is needed to bootstrap core Hyde services, for example page compilers.
+ *
+ * The application is lazy-loaded and only booted when necessary as it takes ~80ms.
+ * This is so routes that don't need the application don't suffer the performance hit.
  */
 trait InteractsWithLaravel
 {
