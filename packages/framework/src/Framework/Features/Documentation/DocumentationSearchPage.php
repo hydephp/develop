493cba6189d1b8cac97a860b8344d7f443fb56ca
Caen De Silva <caen@desilva.se>
2023-02-08 15:08:01 +0100

    Create DocumentationSearchPage.php

---
+++ packages/framework/src/Framework/Features/Documentation/DocumentationSearchPage.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hyde\Framework\Features\Documentation;
+
+use Hyde\Pages\VirtualPage;
+
+class DocumentationSearchPage extends VirtualPage
+{
+    //
+}
