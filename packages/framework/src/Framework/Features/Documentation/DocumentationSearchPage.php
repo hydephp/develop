3817db23e263ef14dfe61f045edb9749f3934493
Caen De Silva <caen@desilva.se>
2023-02-08 15:21:49 +0100

    Document how to override the page
    Not yet implemented, doing documentation driven design here.

--- packages/framework/src/Framework/Features/Documentation/DocumentationSearchPage.php
+++ packages/framework/src/Framework/Features/Documentation/DocumentationSearchPage.php
@@ -10,6 +10,9 @@ use Hyde\Pages\VirtualPage;
  * This page is used to render the search page for the documentation.
  *
  * It is not based on a source file, but is dynamically generated when Search is enabled.
+ * If you want to override the search page, you can create a file at: `_pages/docs/search.blade.php`,
+ * then this class will not be applied.
+ *
  * @see \Hyde\Framework\Testing\Feature\DocumentationSearchPageTest
  */
 class DocumentationSearchPage extends VirtualPage
