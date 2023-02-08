ffb2699a78e3f01e7c08868cba391401939981c4
Caen De Silva <caen@desilva.se>
2023-02-08 12:24:42 +0100

    Replace `DIRECTORY_SEPARATOR` with `Filesystem::DIRECTORY_SEPARATOR`

--- packages/framework/tests/Unit/HydePathHelperTest.php
+++ packages/framework/tests/Unit/HydePathHelperTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Hyde\Framework\Testing\Unit;
 
+use Hyde\Foundation\Filesystem;
 use Hyde\Foundation\HydeKernel;
 use Hyde\Hyde;
 use Hyde\Testing\TestCase;
@@ -28,10 +29,10 @@ class HydePathHelperTest extends TestCase
     public function test_returned_directory_contains_content_expected_to_be_in_the_project_directory()
     {
         $this->assertTrue(
-            file_exists(Hyde::path().DIRECTORY_SEPARATOR.'hyde') &&
-                file_exists(Hyde::path().DIRECTORY_SEPARATOR.'_pages') &&
-                file_exists(Hyde::path().DIRECTORY_SEPARATOR.'_posts') &&
-                file_exists(Hyde::path().DIRECTORY_SEPARATOR.'_site')
+            file_exists(Hyde::path().Filesystem::DIRECTORY_SEPARATOR.'hyde') &&
+                file_exists(Hyde::path().Filesystem::DIRECTORY_SEPARATOR.'_pages') &&
+                file_exists(Hyde::path().Filesystem::DIRECTORY_SEPARATOR.'_posts') &&
+                file_exists(Hyde::path().Filesystem::DIRECTORY_SEPARATOR.'_site')
         );
     }
 }
