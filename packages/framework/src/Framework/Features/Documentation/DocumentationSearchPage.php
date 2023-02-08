db8ef86656e78d8aa80fe1cc9f08fe1fff104a74
Caen De Silva <caen@desilva.se>
2023-02-08 15:09:53 +0100

    Create DocumentationSearchPageTest.php
    Update DocumentationSearchPage.php

--- packages/framework/src/Framework/Features/Documentation/DocumentationSearchPage.php
+++ packages/framework/src/Framework/Features/Documentation/DocumentationSearchPage.php
@@ -6,6 +6,9 @@ namespace Hyde\Framework\Features\Documentation;
 
 use Hyde\Pages\VirtualPage;
 
+/**
+ * @see \Hyde\Framework\Testing\Feature\DocumentationSearchPageTest
+ */
 class DocumentationSearchPage extends VirtualPage
 {
     //
---
+++ packages/framework/tests/Feature/DocumentationSearchPageTest.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hyde\Framework\Testing\Feature;
+
+use Hyde\Framework\Features\Documentation\DocumentationSearchPage;
+use Hyde\Testing\TestCase;
+
+/**
+ * @covers \Hyde\Framework\Features\Documentation\DocumentationSearchPage
+ */
+class DocumentationSearchPageTest extends TestCase
+{
+    //
+}
