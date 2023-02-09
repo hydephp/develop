273af232ea3cabcf246651145c53345609f0b80a
Caen De Silva <caen@desilva.se>
2023-02-09 12:31:08 +0100

    Register the ViewServiceProvider in HydeServiceProvider

--- packages/framework/src/Framework/HydeServiceProvider.php
+++ packages/framework/src/Framework/HydeServiceProvider.php
@@ -8,6 +8,7 @@ use Hyde\Console\HydeConsoleServiceProvider;
 use Hyde\Facades\Features;
 use Hyde\Foundation\HydeKernel;
 use Hyde\Foundation\Providers\ConfigurationServiceProvider;
+use Hyde\Foundation\Providers\ViewServiceProvider;
 use Hyde\Framework\Concerns\RegistersFileLocations;
 use Hyde\Framework\Services\AssetService;
 use Hyde\Framework\Views\Components\LinkComponent;
@@ -149,5 +150,6 @@ class HydeServiceProvider extends ServiceProvider
     protected function registerModuleServiceProviders(): void
     {
         $this->app->register(HydeConsoleServiceProvider::class);
+        $this->app->register(ViewServiceProvider::class);
     }
 }
