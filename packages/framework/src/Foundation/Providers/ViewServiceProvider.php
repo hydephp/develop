7efb94a6df99863e28f788abc32898d85a2d9263
Caen De Silva <caen@desilva.se>
2023-02-09 12:28:44 +0100

    Create ViewServiceProvider.php

---
+++ packages/framework/src/Foundation/Providers/ViewServiceProvider.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hyde\Foundation\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class ViewServiceProvider extends ServiceProvider
+{
+    public function register(): void
+    {
+        //
+    }
+
+    public function boot(): void
+    {
+        //
+    }
+}
