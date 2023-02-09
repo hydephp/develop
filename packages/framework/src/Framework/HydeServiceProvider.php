bb43f9d959d422ffe9442ab55375a0f5b2411625
Caen De Silva <caen@desilva.se>
2023-02-09 12:31:40 +0100

    Boot view logic in ViewServiceProvider

--- packages/framework/src/Framework/HydeServiceProvider.php
+++ packages/framework/src/Framework/HydeServiceProvider.php
@@ -11,7 +11,6 @@ use Hyde\Foundation\Providers\ConfigurationServiceProvider;
 use Hyde\Foundation\Providers\ViewServiceProvider;
 use Hyde\Framework\Concerns\RegistersFileLocations;
 use Hyde\Framework\Services\AssetService;
-use Hyde\Framework\Views\Components\LinkComponent;
 use Hyde\Hyde;
 use Hyde\Markdown\MarkdownConverter;
 use Hyde\Pages\BladePage;
@@ -19,7 +18,6 @@ use Hyde\Pages\DocumentationPage;
 use Hyde\Pages\HtmlPage;
 use Hyde\Pages\MarkdownPage;
 use Hyde\Pages\MarkdownPost;
-use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
 
 /**
@@ -76,37 +74,10 @@ class HydeServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        $this->loadViewsFrom(__DIR__.'/../../resources/views', 'hyde');
 
         $this->publishes([
             __DIR__.'/../../config' => config_path(),
         ], 'configs');
-
-        $this->publishes([
-            __DIR__.'/../../resources/views/layouts' => resource_path('views/vendor/hyde/layouts'),
-        ], 'hyde-layouts');
-
-        $this->publishes([
-            __DIR__.'/../../resources/views/components' => resource_path('views/vendor/hyde/components'),
-        ], 'hyde-components');
-
-        $this->publishes([
-            Hyde::vendorPath('resources/views/pages/404.blade.php') => Hyde::path('_pages/404.blade.php'),
-        ], 'hyde-page-404');
-
-        $this->publishes([
-            Hyde::vendorPath('resources/views/homepages/welcome.blade.php') => Hyde::path('_pages/index.blade.php'),
-        ], 'hyde-welcome-page');
-
-        $this->publishes([
-            Hyde::vendorPath('resources/views/homepages/post-feed.blade.php') => Hyde::path('_pages/index.blade.php'),
-        ], 'hyde-posts-page');
-
-        $this->publishes([
-            Hyde::vendorPath('resources/views/homepages/blank.blade.php') => Hyde::path('_pages/index.blade.php'),
-        ], 'hyde-blank-page');
-
-        Blade::component('link', LinkComponent::class);
 
         HydeKernel::getInstance()->readyToBoot();
     }
