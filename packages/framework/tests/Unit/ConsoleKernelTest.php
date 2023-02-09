86c1127d3ed4250d2c0d5240d7a6222b59590333
Caen De Silva <caen@desilva.se>
2023-02-09 18:07:54 +0100

    Create ConsoleKernelTest.php

---
+++ packages/framework/tests/Unit/ConsoleKernelTest.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hyde\Framework\Testing\Unit;
+
+use Hyde\Foundation\ConsoleKernel;
+use Hyde\Testing\TestCase;
+
+/**
+ * @covers \Hyde\Foundation\ConsoleKernel
+ */
+class ConsoleKernelTest extends TestCase
+{
+    //
+}
