703f3c107afffb4c667465be4321ca2a61ee9c3d
Caen De Silva <caen@desilva.se>
2023-02-09 19:21:57 +0100

    Refactor method to tap array

--- packages/framework/src/Foundation/ConsoleKernel.php
+++ packages/framework/src/Foundation/ConsoleKernel.php
@@ -13,10 +13,10 @@ class ConsoleKernel extends Kernel
     {
         // Combine the parent bootstrappers into an associative array, so we can easily access them by class name.
         $array = array_combine(parent::bootstrappers(), parent::bootstrappers());
-
-        // Replace the LoadConfiguration bootstrapper with our own.
-        $array[\LaravelZero\Framework\Bootstrap\LoadConfiguration::class] = \Hyde\Foundation\Internal\LoadConfiguration::class;
-
+        $array = tap($array, function (array &$array) {
+            // Replace the LoadConfiguration bootstrapper with our own.
+            $array[\LaravelZero\Framework\Bootstrap\LoadConfiguration::class] = \Hyde\Foundation\Internal\LoadConfiguration::class;
+        });
         // Return the bootstrappers without the added keys.
         return array_values($array);
     }
