380b8920987e9c938fe2f9a549b0086e8f12e98d
Caen De Silva <caen@desilva.se>
2023-02-10 16:44:31 +0100

    Create ReadingTimeTest.php

---
+++ packages/framework/tests/Feature/ReadingTimeTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Hyde\Framework\Testing\Feature;
+
+use Hyde\Support\ReadingTime;
+use Hyde\Testing\TestCase;
+
+/**
+ * @covers \Hyde\Support\ReadingTime
+ */
+class ReadingTimeTest extends TestCase
+{
+    //
+}
