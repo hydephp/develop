ab03c3bbfa7bc6477f95f1708fc723c95ad0b21e
Caen De Silva <caen@desilva.se>
2023-02-11 19:47:23 +0100

    Override the Kernel::bootstrappers() method

--- packages/framework/src/Foundation/ConsoleKernel.php
+++ packages/framework/src/Foundation/ConsoleKernel.php
@@ -8,5 +8,13 @@ use LaravelZero\Framework\Kernel;
 
 class ConsoleKernel extends Kernel
 {
-    //
+    /**
+     * Get the bootstrap classes for the application.
+     *
+     * @return array
+     */
+    protected function bootstrappers()
+    {
+        return $this->bootstrappers;
+    }
 }
