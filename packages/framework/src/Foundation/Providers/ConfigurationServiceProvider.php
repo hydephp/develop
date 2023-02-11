46edf75af396d8e60c1b12627109b2877a7f83ac
Caen De Silva <caen@desilva.se>
2023-02-11 20:40:41 +0100

    Remove method ConfigurationServiceProvider::initialize

--- packages/framework/src/Foundation/Providers/ConfigurationServiceProvider.php
+++ packages/framework/src/Foundation/Providers/ConfigurationServiceProvider.php
@@ -8,15 +8,6 @@ use Illuminate\Support\ServiceProvider;
 
 class ConfigurationServiceProvider extends ServiceProvider
 {
-    /**
-     * @deprecated Move to bootstrapper so this class can be registered in app.php so users can override it.
-     * Run any logic before the Hyde Service Provider is registered.
-     */
-    public function initialize(): void
-    {
-        //
-    }
-
     public function register(): void
     {
         $this->mergeConfigFrom(__DIR__.'/../../../config/hyde.php', 'hyde');
