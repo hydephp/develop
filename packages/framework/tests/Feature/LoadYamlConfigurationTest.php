3e093e37fa5860cbd26c91f7ccb60287ad26070a
Caen De Silva <caen@desilva.se>
2023-02-11 20:32:04 +0100

    Move test to to bootstrapper test as provider is no longer responsible for this task

--- packages/framework/tests/Feature/LoadYamlConfigurationTest.php
+++ packages/framework/tests/Feature/LoadYamlConfigurationTest.php
@@ -7,10 +7,18 @@ namespace Hyde\Framework\Testing\Feature;
 use Hyde\Foundation\Services\LoadYamlConfiguration;
 use Hyde\Testing\TestCase;
 
+use function config;
+
 /**
  * @covers \Hyde\Foundation\Services\LoadYamlConfiguration
  */
 class LoadYamlConfigurationTest extends TestCase
 {
-    //
+    public function test_bootstrapper_applies_yaml_configuration_when_present()
+    {
+        $this->assertEquals('HydePHP', config('site.name'));
+        $this->file('hyde.yml', 'name: Foo');
+        $this->app->bootstrapWith([LoadYamlConfiguration::class]);
+        $this->assertEquals('Foo', config('site.name'));
+    }
 }
