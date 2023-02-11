b8fa5a7f17c21c889e9d936f7a8a527ce8ac9251
Caen De Silva <caen@desilva.se>
2023-02-11 20:03:05 +0100

    Revert "Override the Kernel $bootstrappers array"
    This reverts commit 3bc461466d9e30b080fa43e941ea06d6d09dcfc8.

--- packages/framework/src/Foundation/ConsoleKernel.php
+++ packages/framework/src/Foundation/ConsoleKernel.php
@@ -11,21 +11,6 @@ use function array_merge;
 class ConsoleKernel extends Kernel
 {
     /**
-     * The application's bootstrap classes.
-     *
-     * @var string[]
-     */
-    protected $bootstrappers = [
-        \LaravelZero\Framework\Bootstrap\CoreBindings::class,
-        \LaravelZero\Framework\Bootstrap\LoadEnvironmentVariables::class,
-        \LaravelZero\Framework\Bootstrap\LoadConfiguration::class,
-        \Illuminate\Foundation\Bootstrap\HandleExceptions::class,
-        \LaravelZero\Framework\Bootstrap\RegisterFacades::class,
-        \LaravelZero\Framework\Bootstrap\RegisterProviders::class,
-        \Illuminate\Foundation\Bootstrap\BootProviders::class,
-    ];
-
-    /**
      * Get the bootstrap classes for the application.
      */
     protected function bootstrappers(): array
