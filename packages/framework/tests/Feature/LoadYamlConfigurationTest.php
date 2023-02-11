06d64cf392e50296d9cb94037cdde6efb61fd240
Caen De Silva <caen@desilva.se>
2023-02-11 20:27:24 +0100

    Create LoadYamlConfigurationTest.php

---
+++ packages/framework/tests/Feature/LoadYamlConfigurationTest.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hyde\Framework\Testing\Feature;
+
+use Hyde\Foundation\Services\LoadYamlConfiguration;
+use Hyde\Testing\TestCase;
+
+/**
+ * @covers \Hyde\Foundation\Services\LoadYamlConfiguration
+ */
+class LoadYamlConfigurationTest extends TestCase
+{
+    //
+}
