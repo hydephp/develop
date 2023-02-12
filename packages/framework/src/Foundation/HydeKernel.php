64e1f7941ebed26500b44a6128b1701793383f18
Caen De Silva <caen@desilva.se>
2023-02-12 18:31:46 +0100

    Revert "Add todo"
    This reverts commit 5cf025e8eb07cb6c6e3cd4c7fa4c9cee3382e236. See https://github.com/hydephp/develop/pull/988#discussion_r1103844167

--- packages/framework/src/Foundation/HydeKernel.php
+++ packages/framework/src/Foundation/HydeKernel.php
@@ -73,8 +73,6 @@ class HydeKernel implements SerializableContract
         $this->setBasePath($basePath ?? getcwd());
         $this->filesystem = new Filesystem($this);
         $this->hyperlinks = new Hyperlinks($this);
-
-        // Todo: load extensions from config
     }
 
     public static function version(): string
