c53731aea67ed375427a128257050ae86a97b7dc
Caen De Silva <caen@desilva.se>
2023-02-12 10:09:38 +0100

    Update new YamlConfig bootstrapper to use merged config file

--- packages/framework/tests/Feature/LoadYamlConfigurationTest.php
+++ packages/framework/tests/Feature/LoadYamlConfigurationTest.php
@@ -16,38 +16,38 @@ class LoadYamlConfigurationTest extends TestCase
 {
     public function test_bootstrapper_applies_yaml_configuration_when_present()
     {
-        $this->assertSame('HydePHP', config('site.name'));
+        $this->assertSame('HydePHP', config('hyde.name'));
         $this->file('hyde.yml', 'name: Foo');
         $this->app->bootstrapWith([LoadYamlConfiguration::class]);
-        $this->assertSame('Foo', config('site.name'));
+        $this->assertSame('Foo', config('hyde.name'));
     }
 
     public function test_changes_in_yaml_file_override_changes_in_site_config()
     {
-        $this->assertSame('HydePHP', Config::get('site.name'));
+        $this->assertSame('HydePHP', Config::get('hyde.name'));
         $this->file('hyde.yml', 'name: Foo');
         $this->app->bootstrapWith([LoadYamlConfiguration::class]);
-        $this->assertSame('Foo', Config::get('site.name'));
+        $this->assertSame('Foo', Config::get('hyde.name'));
     }
 
     public function test_changes_in_yaml_file_override_changes_in_site_config_when_using_yaml_extension()
     {
-        $this->assertSame('HydePHP', Config::get('site.name'));
+        $this->assertSame('HydePHP', Config::get('hyde.name'));
         $this->file('hyde.yaml', 'name: Foo');
         $this->app->bootstrapWith([LoadYamlConfiguration::class]);
-        $this->assertSame('Foo', Config::get('site.name'));
+        $this->assertSame('Foo', Config::get('hyde.name'));
     }
 
     public function test_service_gracefully_handles_missing_file()
     {
         $this->app->bootstrapWith([LoadYamlConfiguration::class]);
-        $this->assertSame('HydePHP', Config::get('site.name'));
+        $this->assertSame('HydePHP', Config::get('hyde.name'));
     }
 
     public function test_service_gracefully_handles_empty_file()
     {
         $this->file('hyde.yml', '');
         $this->app->bootstrapWith([LoadYamlConfiguration::class]);
-        $this->assertSame('HydePHP', Config::get('site.name'));
+        $this->assertSame('HydePHP', Config::get('hyde.name'));
     }
 }
