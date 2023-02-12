2fd0d15000f80103cd06c0dfa4be84f98cf758c0
Caen De Silva <caen@desilva.se>
2023-02-12 17:42:57 +0100

    Remove deprecated HydeKernel::registerPageClass() method

--- packages/framework/src/Foundation/Concerns/ManagesHydeKernel.php
+++ packages/framework/src/Foundation/Concerns/ManagesHydeKernel.php
@@ -74,43 +74,6 @@ trait ManagesHydeKernel
         return ltrim($this->getMediaDirectory(), '_');
     }
 
-    /**
-     * Developer Information.
-     *
-     * @experimental This feature is experimental and may change substantially before the 1.0.0 release.
-     *
-     * @deprecated This feature may be replaced by the {@see \Hyde\Foundation\Concerns\HydeExtension} system.
-     *
-     * If you are a package developer, and want a custom page class to be discovered,
-     * you'll need to register it sometime before the boot process, before discovery is run.
-     * Typically, you would do this by calling this method in the register method of a service provider.
-     * Hyde will then automatically discover source files for the new page class, and compile them during the build process.
-     *
-     * @param  class-string<\Hyde\Pages\Concerns\HydePage>  $pageClass
-     */
-    public function registerPageClass(string $pageClass): void
-    {
-        if ($this->booted) {
-            // We throw an exception here to prevent the developer from registering a page class after the Kernel has been booted.
-            // The reason we do this is because at this point all the source files have already been discovered and parsed.
-            // If we allowed new classes after this point, we would have to reboot everything which adds complexity.
-
-            throw new BadMethodCallException('Cannot register a page class after the Kernel has been booted.');
-        }
-
-        if (! is_subclass_of($pageClass, HydePage::class)) {
-            throw new InvalidArgumentException('The specified class must be a subclass of HydePage.');
-        }
-
-        if (is_subclass_of($pageClass, DynamicPage::class)) {
-            throw new InvalidArgumentException('The specified class must not be a subclass of DynamicPage.');
-        }
-
-        if (! in_array($pageClass, $this->pageClasses, true)) {
-            $this->pageClasses[] = $pageClass;
-        }
-    }
-
     /** @return array<class-string<\Hyde\Pages\Concerns\HydePage>> */
     public function getRegisteredPageClasses(): array
     {
