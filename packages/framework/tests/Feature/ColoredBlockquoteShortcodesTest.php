98848f4267b09dec93ea7e4628c836685725167f
Caen De Silva <caen@desilva.se>
2023-02-12 13:21:24 +0100

    Test Markdown can be used within the coloured blockquote

--- packages/framework/tests/Feature/ColoredBlockquoteShortcodesTest.php
+++ packages/framework/tests/Feature/ColoredBlockquoteShortcodesTest.php
@@ -26,4 +26,11 @@ class ColoredBlockquoteShortcodesTest extends TestCase
         $this->assertContainsOnlyInstancesOf(ColoredBlockquotes::class,
             ColoredBlockquotes::get());
     }
+
+    public function test_can_use_markdown_within_blockquote()
+    {
+        $this->assertEquals('<blockquote class="color"><p>foo <strong>bar</strong></p></blockquote>',
+            ColoredBlockquotes::resolve('>color foo **bar**')
+        );
+    }
 }
