74660f33162fb2e59d3a0935c18bae8f69fe4270
Caen De Silva <caen@desilva.se>
2023-02-12 15:07:58 +0100

    Revert "Allow tag to be selected through argument"
    This reverts commit 3df68b0094467d58d60b52d3345383ed89005522.

--- packages/framework/src/Console/Commands/UpdateConfigsCommand.php
+++ packages/framework/src/Console/Commands/UpdateConfigsCommand.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Artisan;
 class UpdateConfigsCommand extends Command
 {
     /** @var string */
-    protected $signature = 'update:configs {tag?}';
+    protected $signature = 'update:configs';
 
     /** @var string */
     protected $description = 'Publish the default configuration files';
@@ -26,7 +26,7 @@ class UpdateConfigsCommand extends Command
 
     public function handle(): int
     {
-        $tag = $this->argument('tag') ?? $this->choice('Which configuration files do you want to publish?', [
+        $tag = $this->choice('Which configuration files do you want to publish?', [
             'configs',
             'hyde-configs',
             'support-configs',
