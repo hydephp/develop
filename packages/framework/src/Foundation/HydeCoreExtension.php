ce796e93e704c139ce2854c9ad5b6b4f4fc5165b
Caen De Silva <caen@desilva.se>
2023-02-12 17:56:41 +0100

    Create HydeCoreExtensionTest.php

--- packages/framework/src/Foundation/HydeCoreExtension.php
+++ packages/framework/src/Foundation/HydeCoreExtension.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace Hyde\Foundation;
 
+/**
+ * @see \Hyde\Framework\Testing\Feature\HydeCoreExtensionTest
+ */
 class HydeCoreExtension
 {
     //
---
+++ packages/framework/tests/Feature/HydeCoreExtensionTest.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hyde\Framework\Testing\Feature;
+
+use Hyde\Foundation\HydeCoreExtension;
+use Hyde\Testing\TestCase;
+
+/**
+ * @covers \Hyde\Foundation\HydeCoreExtension
+ */
+class HydeCoreExtensionTest extends TestCase
+{
+    //
+}
