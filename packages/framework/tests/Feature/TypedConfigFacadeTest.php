95a492cc5addc37f76f536bcc2ba33aa23ff00c1
Caen De Silva <caen@desilva.se>
2023-02-13 20:15:40 +0100

    Create TypedConfigFacadeTest.php

---
+++ packages/framework/tests/Feature/TypedConfigFacadeTest.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hyde\Framework\Testing\Feature;
+
+use Hyde\Facades\Config;
+use Hyde\Testing\TestCase;
+
+/**
+ * @covers \Hyde\Facades\Config
+ */
+class TypedConfigFacadeTest extends TestCase
+{
+    public function testGetArray()
+    {
+        //
+    }
+
+    public function testGetString()
+    {
+        //
+    }
+
+    public function testGetBool()
+    {
+        //
+    }
+
+    public function testGetInt()
+    {
+        //
+    }
+
+    public function testGetFloat()
+    {
+        //
+    }
+}
