fd06c8d7e5102a3e381bbd73cd51ab6f7c5aa5fd
Caen De Silva <caen@desilva.se>
2023-02-13 12:11:08 +0100

    Test default source directories values in config match declared values

--- packages/framework/tests/Unit/ConfigFileTest.php
+++ packages/framework/tests/Unit/ConfigFileTest.php
@@ -4,6 +4,13 @@ declare(strict_types=1);
 
 namespace Hyde\Framework\Testing\Unit;
 
+use Hyde\Foundation\HydeCoreExtension;
+use Hyde\Hyde;
+use Hyde\Pages\BladePage;
+use Hyde\Pages\DocumentationPage;
+use Hyde\Pages\HtmlPage;
+use Hyde\Pages\MarkdownPage;
+use Hyde\Pages\MarkdownPost;
 use Hyde\Testing\TestCase;
 
 /**
@@ -11,5 +18,29 @@ use Hyde\Testing\TestCase;
  */
 class ConfigFileTest extends TestCase
 {
-    //
+    public function test_default_source_directories_values_match_declared_values()
+    {
+        $config = require Hyde::path('config/hyde.php');
+
+        $expected = [
+            HtmlPage::class => '_pages',
+            BladePage::class => '_pages',
+            MarkdownPage::class => '_pages',
+            MarkdownPost::class => '_posts',
+            DocumentationPage::class => '_docs',
+        ];
+
+        $this->assertSame($config['source_directories'], $expected);
+    }
+
+    public function test_default_source_directories_values_cover_all_core_extension_classes()
+    {
+        $config = require Hyde::path('config/hyde.php');
+
+        $expected = collect(HydeCoreExtension::getPageClasses())
+            ->mapWithKeys(fn ($pageClass) => [$pageClass => $pageClass::$sourceDirectory])
+            ->toArray();
+
+        $this->assertSame($config['source_directories'], $expected);
+    }
 }
