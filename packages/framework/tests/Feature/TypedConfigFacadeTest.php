b04c71903d8622205e536d9b96b0fc64ac813034
Caen De Silva <caen@desilva.se>
2023-02-13 21:17:30 +0100

    Implement baseline unit tests

--- packages/framework/tests/Feature/TypedConfigFacadeTest.php
+++ packages/framework/tests/Feature/TypedConfigFacadeTest.php
@@ -14,26 +14,26 @@ class TypedConfigFacadeTest extends TestCase
 {
     public function testGetArray()
     {
-        //
+        $this->assertIsArray(Config::getArray('foo'));
     }
 
     public function testGetString()
     {
-        //
+        $this->assertIsString(Config::getString('foo'));
     }
 
     public function testGetBool()
     {
-        //
+        $this->assertIsBool(Config::getBool('foo'));
     }
 
     public function testGetInt()
     {
-        //
+        $this->assertIsInt(Config::getInt('foo'));
     }
 
     public function testGetFloat()
     {
-        //
+        $this->assertIsFloat(Config::getFloat('foo'));
     }
 }
