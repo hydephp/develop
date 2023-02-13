ecd88cbf9fbba3ff99ea01f7d5544765ed1aa1bd
Caen De Silva <caen@desilva.se>
2023-02-13 12:24:48 +0100

    Convert test to Pest

--- packages/framework/tests/Unit/ConfigFileTest.php
+++ packages/framework/tests/Unit/ConfigFileTest.php
@@ -2,8 +2,6 @@
 
 declare(strict_types=1);
 
-namespace Hyde\Framework\Testing\Unit;
-
 use Hyde\Foundation\HydeCoreExtension;
 use Hyde\Hyde;
 use Hyde\Pages\BladePage;
@@ -11,39 +9,33 @@ use Hyde\Pages\DocumentationPage;
 use Hyde\Pages\HtmlPage;
 use Hyde\Pages\MarkdownPage;
 use Hyde\Pages\MarkdownPost;
-use Hyde\Testing\TestCase;
 
-/**
- * @see \Hyde\Framework\Testing\Unit\HydeConfigFilesAreMatchingTest
- */
-class ConfigFileTest extends TestCase
+beforeAll(function () {
+    \Hyde\Foundation\HydeKernel::setInstance(new \Hyde\Foundation\HydeKernel());
+});
+
+test('test_default_source_directories_values_are_defined', function () {
+    test()->assertArrayHasKey('source_directories', getConfig());
+});
+
+test('test_default_source_directories_values_match_declared_values', function () {
+    test()->assertSame(getConfig()['source_directories'], [
+        HtmlPage::class => '_pages',
+        BladePage::class => '_pages',
+        MarkdownPage::class => '_pages',
+        MarkdownPost::class => '_posts',
+        DocumentationPage::class => '_docs',
+    ]);
+});
+
+test('test_default_source_directories_values_cover_all_core_extension_classes', function () {
+    test()->assertSame(getConfig()['source_directories'], collect(HydeCoreExtension::getPageClasses())
+        ->mapWithKeys(fn ($pageClass) => [$pageClass => $pageClass::$sourceDirectory])
+        ->toArray()
+    );
+});
+
+function getConfig(): array
 {
-    public function test_default_source_directories_values_are_defined()
-    {
-        $this->assertArrayHasKey('source_directories', $this->getConfig());
-    }
-
-    public function test_default_source_directories_values_match_declared_values()
-    {
-        $this->assertSame($this->getConfig()['source_directories'], [
-            HtmlPage::class => '_pages',
-            BladePage::class => '_pages',
-            MarkdownPage::class => '_pages',
-            MarkdownPost::class => '_posts',
-            DocumentationPage::class => '_docs',
-        ]);
-    }
-
-    public function test_default_source_directories_values_cover_all_core_extension_classes()
-    {
-        $this->assertSame($this->getConfig()['source_directories'], collect(HydeCoreExtension::getPageClasses())
-            ->mapWithKeys(fn ($pageClass) => [$pageClass => $pageClass::$sourceDirectory])
-            ->toArray()
-        );
-    }
-
-    protected function getConfig(): array
-    {
-        return require Hyde::vendorPath('config/hyde.php');
-    }
+    return require Hyde::vendorPath('config/hyde.php');
 }
