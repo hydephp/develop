666fea84333fe0406281005d0247c368d7f6097d
Caen De Silva <caen@desilva.se>
2023-02-13 20:13:44 +0100

    Implement custom strongly typed Config accessors

--- packages/framework/src/Facades/Config.php
+++ packages/framework/src/Facades/Config.php
@@ -6,5 +6,28 @@ namespace Hyde\Facades;
 
 class Config extends \Illuminate\Support\Facades\Config
 {
-    //
+    public static function getArray(array|string $key, array $default = null): array
+    {
+        return (array) static::get($key, $default);
+    }
+
+    public static function getString(string $key, string $default = null): string
+    {
+        return (string) static::get($key, $default);
+    }
+
+    public static function getInt(string $key, int $default = null): int
+    {
+        return (int) static::get($key, $default);
+    }
+
+    public static function getBool(string $key, bool $default = null): bool
+    {
+        return (bool) static::get($key, $default);
+    }
+
+    public static function getFloat(string $key, float $default = null): float
+    {
+        return (float) static::get($key, $default);
+    }
 }
