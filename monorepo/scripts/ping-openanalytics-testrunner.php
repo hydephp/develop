f2ed32187d893e13e6637ccf7fb51b16e871938a
Caen De Silva <caen@desilva.se>
2023-02-14 09:51:51 +0100

    Create ping-openanalytics-testrunner.php
    Instead of downloading it from this Gist https://gist.github.com/caendesilva/d76fc6d73cb488863a8f6fda18a7c8c4, we can just have a local copy that's easily version controlled.

---
+++ monorepo/scripts/ping-openanalytics-testrunner.php
@@ -0,0 +1,29 @@
+<?php
+
+echo "Pinging statistics server\n";
+
+$runner = $argv[1];
+$token = $argv[2];
+
+$url = 'https://analytics.hydephp.se/api/test_runs';
+
+$curl = curl_init($url);
+curl_setopt($curl, CURLOPT_URL, $url);
+curl_setopt($curl, CURLOPT_POST, true);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+$headers = array(
+    'Accept: application/json',
+    "Authorization: Bearer $token",
+    'Content-Type: application/json',
+);
+curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+
+curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode([
+    'runner' => json_encode($runner),
+    'tests' => substr_count(file_get_contents('testdox.txt') ?: exit(404), '[x]')
+]));
+
+$resp = curl_exec($curl);
+curl_close($curl);
+var_dump($resp);
