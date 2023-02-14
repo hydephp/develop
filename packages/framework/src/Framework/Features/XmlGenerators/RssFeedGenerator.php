295de2a55e3d75e8fc6c5508647ec682ea78345c
Caen De Silva <caen@desilva.se>
2023-02-14 12:02:12 +0100

    Convert void arrow function to a traditional closure

--- packages/framework/src/Framework/Features/XmlGenerators/RssFeedGenerator.php
+++ packages/framework/src/Framework/Features/XmlGenerators/RssFeedGenerator.php
@@ -23,7 +23,9 @@ class RssFeedGenerator extends BaseXmlGenerator
 {
     public function generate(): static
     {
-        MarkdownPost::getLatestPosts()->each(fn (MarkdownPost $post) => $this->addItem($post));
+        MarkdownPost::getLatestPosts()->each(function (MarkdownPost $post) {
+            $this->addItem($post);
+        });
 
         return $this;
     }
