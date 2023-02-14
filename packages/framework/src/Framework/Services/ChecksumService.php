a688291719dd87483cf351858f5211a694caf017
Caen De Silva <caen@desilva.se>
2023-02-14 11:16:19 +0100

    Remove code comment as file should be in project directory
    (Vendor is acceptable, and included)

--- packages/framework/src/Framework/Services/ChecksumService.php
+++ packages/framework/src/Framework/Services/ChecksumService.php
@@ -68,9 +68,6 @@ class ChecksumService
 
     /**
      * Shorthand for {@see static::unixsum()} but loads a file.
-     *
-     * Note that unlink most filesystem methods, the file path is expected
-     * to be absolute as the file might not be in the project directory.
      */
     public static function unixsumFile(string $file): string
     {
