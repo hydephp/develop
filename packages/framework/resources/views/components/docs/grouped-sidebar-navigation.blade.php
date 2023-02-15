97f2da71dd5774cacc7ff35cc926004cd3cd61e1
Caen De Silva <caen@desilva.se>
2023-02-15 22:11:43 +0100

    Further dim unhovered chevron state

--- packages/framework/resources/views/components/docs/grouped-sidebar-navigation.blade.php
+++ packages/framework/resources/views/components/docs/grouped-sidebar-navigation.blade.php
@@ -4,7 +4,7 @@
 	<li class="sidebar-group mb-4 mt-4 first:mt-0" role="listitem" x-data="{ groupOpen: {{ $page->navigationMenuGroup() === $group ? 'true' : 'false' }} }">
 		<header class="sidebar-group-header flex justify-between items-center group mb-2" @click="groupOpen = ! groupOpen">
             <h4 class="sidebar-group-heading text-base font-semibold -ml-1 cursor-pointer">{{ Hyde::makeTitle($group) }}</h4>
-            <button class="sidebar-group-toggle opacity-75 group-hover:opacity-100">
+            <button class="sidebar-group-toggle opacity-50 group-hover:opacity-100">
                 <svg class="sidebar-group-toggle-icon sidebar-group-toggle-icon-open" x-show="groupOpen" width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M8 12L12 8L4 8L8 12Z" fill="currentColor" />
                 </svg>
