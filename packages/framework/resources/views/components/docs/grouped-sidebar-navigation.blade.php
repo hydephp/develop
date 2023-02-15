b68ea0b8818b3735c9fa35df23ed1bfc8868d6d7
Caen De Silva <caen@desilva.se>
2023-02-15 21:09:25 +0100

    Change icon depending on toggle state

--- packages/framework/resources/views/components/docs/grouped-sidebar-navigation.blade.php
+++ packages/framework/resources/views/components/docs/grouped-sidebar-navigation.blade.php
@@ -5,8 +5,11 @@
 		<header class="sidebar-group-header flex justify-between items-center mb-2">
             <h4 class="sidebar-group-heading text-base font-semibold -ml-1">{{ Hyde::makeTitle($group) }}</h4>
             <button class="sidebar-group-toggle" @click="groupOpen = ! groupOpen">
-                <svg class="sidebar-group-toggle-icon" width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <svg class="sidebar-group-toggle-icon sidebar-group-toggle-icon-open" x-show="groupOpen" width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M8 12L12 8L4 8L8 12Z" fill="currentColor" />
+                </svg>
+                <svg class="sidebar-group-toggle-icon sidebar-group-toggle-icon-closed" x-show="! groupOpen" width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M12 8L8 12L8 4L12 8Z" fill="currentColor" />
                 </svg>
             </button>
         </header>
