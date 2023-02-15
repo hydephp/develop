533c9dc9421b34524e665bdf9fbb2189aa43586b
Caen De Silva <caen@desilva.se>
2023-02-15 20:58:09 +0100

    Give button same margin as header

--- packages/framework/resources/views/components/docs/grouped-sidebar-navigation.blade.php
+++ packages/framework/resources/views/components/docs/grouped-sidebar-navigation.blade.php
@@ -4,7 +4,7 @@
 	<li class="sidebar-group mb-4 mt-4 first:mt-0" role="listitem">
 		<header class="sidebar-group-header flex justify-between items-center">
             <h4 class="sidebar-group-heading text-base font-semibold mb-2 -ml-1">{{ Hyde::makeTitle($group) }}</h4>
-            <button class="sidebar-group-toggle">
+            <button class="sidebar-group-toggle mb-2">
                 <svg class="sidebar-group-toggle-icon" width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M8 12L12 8L4 8L8 12Z" fill="currentColor" />
                 </svg>
