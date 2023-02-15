9201bb1ac256b37c1e793c8e0c4a48aa0257603d
Caen De Silva <caen@desilva.se>
2023-02-15 22:36:52 +0100

    Split out collapsible sidebar component from default

--- packages/framework/resources/views/components/docs/grouped-sidebar-navigation.blade.php
+++ packages/framework/resources/views/components/docs/grouped-sidebar-navigation.blade.php
@@ -1,23 +1,13 @@
 @php /** @var \Hyde\Framework\Features\Navigation\DocumentationSidebar $sidebar */ @endphp
 <ul id="sidebar-navigation-items" role="list">
-	@foreach ($sidebar->getGroups() as $group)
-	<li class="sidebar-group mb-4 mt-4 first:mt-0" role="listitem" x-data="{ groupOpen: {{ config('sidebar_collapsible', false) === false ? 'true' : (($page->navigationMenuGroup() === $group) ? 'true' : 'false') }} }">
-		<header class="sidebar-group-header flex justify-between items-center group mb-2" @click="groupOpen = ! groupOpen">
-            <h4 class="sidebar-group-heading text-base font-semibold -ml-1 cursor-pointer dark:group-hover:text-white">{{ Hyde::makeTitle($group) }}</h4>
-            <button class="sidebar-group-toggle opacity-50 group-hover:opacity-100">
-                <svg class="sidebar-group-toggle-icon sidebar-group-toggle-icon-open" x-show="groupOpen" width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M8 12L12 8L4 8L8 12Z" fill="currentColor" />
-                </svg>
-                <svg class="sidebar-group-toggle-icon sidebar-group-toggle-icon-closed" x-show="! groupOpen" width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M12 8L8 12L8 4L12 8Z" fill="currentColor" />
-                </svg>
-            </button>
-        </header>
-		<ul class="sidebar-group-list ml-4" role="list" x-show="groupOpen">
-			@foreach ($sidebar->getItemsInGroup($group) as $item)
-				<x-hyde::docs.grouped-sidebar-item :item="$item" :active="$item->route->getRouteKey() === $currentRoute->getRouteKey()" />
-			@endforeach
-		</ul>
-	</li>
-	@endforeach
+    @foreach ($sidebar->getGroups() as $group)
+        <li class="sidebar-group mb-4 mt-4 first:mt-0" role="listitem">
+            <h4 class="sidebar-group-heading text-base font-semibold mb-2 -ml-1">{{ Hyde::makeTitle($group) }}</h4>
+            <ul class="sidebar-group-list ml-4" role="list">
+                @foreach ($sidebar->getItemsInGroup($group) as $item)
+                    <x-hyde::docs.grouped-sidebar-item :item="$item" :active="$item->route->getRouteKey() === $currentRoute->getRouteKey()" />
+                @endforeach
+            </ul>
+        </li>
+    @endforeach
 </ul>
