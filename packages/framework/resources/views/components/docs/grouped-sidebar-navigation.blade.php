f49b6a3ace894ee70a60af09bf67e33258ae820d
Caen De Silva <caen@desilva.se>
2023-02-15 20:56:43 +0100

    Copilot generate SVG

--- packages/framework/resources/views/components/docs/grouped-sidebar-navigation.blade.php
+++ packages/framework/resources/views/components/docs/grouped-sidebar-navigation.blade.php
@@ -4,7 +4,11 @@
 	<li class="sidebar-group mb-4 mt-4 first:mt-0" role="listitem">
 		<header class="sidebar-group-header">
             <h4 class="sidebar-group-heading text-base font-semibold mb-2 -ml-1">{{ Hyde::makeTitle($group) }}</h4>
-            <button class="sidebar-group-toggle"></button>
+            <button class="sidebar-group-toggle">
+                <svg class="sidebar-group-toggle-icon" width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M8 12L12 8L4 8L8 12Z" fill="currentColor" />
+                </svg>
+            </button>
         </header>
 		<ul class="sidebar-group-list ml-4" role="list">
 			@foreach ($sidebar->getItemsInGroup($group) as $item)
