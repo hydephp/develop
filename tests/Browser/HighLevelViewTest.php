417f89dca44548e9ee24c8b6bf2cdfb5aa3a0392
Caen De Silva <caen@desilva.se>
2023-02-16 16:59:06 +0100

    Debug test

--- tests/Browser/HighLevelViewTest.php
+++ tests/Browser/HighLevelViewTest.php
@@ -176,6 +176,7 @@ date: 2022-01-01 12:00
 
         $this->browse(function (Browser $browser) {
             $browser->visit('/docs/page1')
+                ->assertNotPresent('button.sidebar-group-toggle')
                 ->assertSee('HydePHP Docs')
                 ->assertSee('Page1')
                 ->assertSee('Page2')
