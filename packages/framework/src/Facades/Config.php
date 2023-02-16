5a1b23a5784df9a5d686a81af645775bb1d649aa
Caen De Silva <caen@desilva.se>
2023-02-16 19:30:20 +0100

    Deprecate strict parameter

--- packages/framework/src/Facades/Config.php
+++ packages/framework/src/Facades/Config.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Hyde\Facades;
 
+use JetBrains\PhpStorm\Deprecated;
 use TypeError;
 
 /**
@@ -24,32 +25,32 @@ class Config extends \Illuminate\Support\Facades\Config
     /** @var true */
     protected const STRICT_DEFAULT = true;
 
-    public static function getArray(string $key, array $default = null, bool $strict = self::STRICT_DEFAULT): array
+    public static function getArray(string $key, array $default = null, #[Deprecated] bool $strict = self::STRICT_DEFAULT): array
     {
         return (array) self::validated(static::get($key, $default), 'array', $key, $strict);
     }
 
-    public static function getString(string $key, string $default = null, bool $strict = self::STRICT_DEFAULT): string
+    public static function getString(string $key, string $default = null, #[Deprecated] bool $strict = self::STRICT_DEFAULT): string
     {
         return (string) self::validated(static::get($key, $default), 'string', $key, $strict);
     }
 
-    public static function getInt(string $key, int $default = null, bool $strict = self::STRICT_DEFAULT): int
+    public static function getInt(string $key, int $default = null, #[Deprecated] bool $strict = self::STRICT_DEFAULT): int
     {
         return (int) self::validated(static::get($key, $default), 'int', $key, $strict);
     }
 
-    public static function getBool(string $key, bool $default = null, bool $strict = self::STRICT_DEFAULT): bool
+    public static function getBool(string $key, bool $default = null, #[Deprecated] bool $strict = self::STRICT_DEFAULT): bool
     {
         return (bool) self::validated(static::get($key, $default), 'bool', $key, $strict);
     }
 
-    public static function getFloat(string $key, float $default = null, bool $strict = self::STRICT_DEFAULT): float
+    public static function getFloat(string $key, float $default = null, #[Deprecated] bool $strict = self::STRICT_DEFAULT): float
     {
         return (float) self::validated(static::get($key, $default), 'float', $key, $strict);
     }
 
-    protected static function validated(mixed $value, string $type, string $key, bool $strict): mixed
+    protected static function validated(mixed $value, string $type, string $key, #[Deprecated] bool $strict): mixed
     {
         if ($strict && ! ("is_$type")($value)) {
             throw new TypeError(sprintf('%s(): Config value %s must be of type %s, %s given', __METHOD__, $key, $type, gettype($value)));
