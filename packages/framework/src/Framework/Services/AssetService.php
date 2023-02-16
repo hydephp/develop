0b2df34c10dca4e049a70fc1f8cda87dd3e123d1
Caen De Silva <caen@desilva.se>
2023-02-16 22:23:02 +0100

    Move down protected method

--- packages/framework/src/Framework/Services/AssetService.php
+++ packages/framework/src/Framework/Services/AssetService.php
@@ -42,11 +42,6 @@ class AssetService
         return $this->version;
     }
 
-    protected function constructCdnPath(string $file): string
-    {
-        return $this->hydefrontUrl ?? 'https://cdn.jsdelivr.net/npm/hydefront@'.$this->version().'/dist/'.$file;
-    }
-
     public function cdnLink(string $file): string
     {
         return $this->constructCdnPath($file);
@@ -73,6 +68,11 @@ class AssetService
         }
 
         return preg_replace('/\s+/', ' ', "/* tailwind.config.js */ \n".rtrim($config, ",\n\r"));
+    }
+
+    protected function constructCdnPath(string $file): string
+    {
+        return $this->hydefrontUrl ?? 'https://cdn.jsdelivr.net/npm/hydefront@'.$this->version().'/dist/'.$file;
     }
 
     protected function getCacheBustKey(string $file): string
