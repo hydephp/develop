63a4ff3dcf089f0a60bcafc667a0e9078caf63db
Caen De Silva <caen@desilva.se>
2023-02-17 19:54:30 +0100

    Port PHPDoc comment from trait to base class

--- packages/framework/src/Pages/Concerns/Discoverable.php
+++ packages/framework/src/Pages/Concerns/Discoverable.php
@@ -5,9 +5,6 @@ declare(strict_types=1);
 namespace Hyde\Pages\Concerns;
 
 /**
- * This trait implements the DiscoverableContract interface,
- * and is used by auto-discoverable HydePage classes.
- *
  * @deprecated Use base DiscoverablePage class instead.
  */
 trait Discoverable
--- packages/framework/src/Pages/Concerns/DiscoverablePage.php
+++ packages/framework/src/Pages/Concerns/DiscoverablePage.php
@@ -6,6 +6,10 @@ namespace Hyde\Pages\Concerns;
 
 use Hyde\Support\Contracts\DiscoverableContract;
 
+/**
+ * This class implements the DiscoverableContract interface,
+ * and is used by auto-discoverable HydePage classes.
+ */
 abstract class DiscoverablePage extends HydePage implements DiscoverableContract
 {
     //
