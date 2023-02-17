f506d910d38bd3a4fbddcb5f21c97e046637a91c
Caen De Silva <caen@desilva.se>
2023-02-17 19:13:07 +0100

    Move contracted methods to Discoverable trait

--- packages/framework/src/Pages/Concerns/Discoverable.php
+++ packages/framework/src/Pages/Concerns/Discoverable.php
@@ -13,4 +13,52 @@ trait Discoverable
     protected static string $sourceDirectory;
     protected static string $outputDirectory;
     protected static string $fileExtension;
+
+    /**
+     * Get the directory in where source files are stored.
+     */
+    public static function sourceDirectory(): string
+    {
+        return static::$sourceDirectory;
+    }
+
+    /**
+     * Get the output subdirectory to store compiled HTML.
+     */
+    public static function outputDirectory(): string
+    {
+        return static::$outputDirectory;
+    }
+
+    /**
+     * Get the file extension of the source files.
+     */
+    public static function fileExtension(): string
+    {
+        return static::$fileExtension;
+    }
+
+    /**
+     * Set the output directory for the HydePage class.
+     */
+    public static function setSourceDirectory(string $sourceDirectory): void
+    {
+        static::$sourceDirectory = unslash($sourceDirectory);
+    }
+
+    /**
+     * Set the source directory for the HydePage class.
+     */
+    public static function setOutputDirectory(string $outputDirectory): void
+    {
+        static::$outputDirectory = unslash($outputDirectory);
+    }
+
+    /**
+     * Set the file extension for the HydePage class.
+     */
+    public static function setFileExtension(string $fileExtension): void
+    {
+        static::$fileExtension = rtrim('.'.ltrim($fileExtension, '.'), '.');
+    }
 }
--- packages/framework/src/Pages/Concerns/HydePage.php
+++ packages/framework/src/Pages/Concerns/HydePage.php
@@ -132,54 +132,6 @@ abstract class HydePage implements PageSchema, DiscoverableContract
     // Section: Filesystem
 
     /**
-     * Get the directory in where source files are stored.
-     */
-    public static function sourceDirectory(): string
-    {
-        return static::$sourceDirectory;
-    }
-
-    /**
-     * Get the output subdirectory to store compiled HTML.
-     */
-    public static function outputDirectory(): string
-    {
-        return static::$outputDirectory;
-    }
-
-    /**
-     * Get the file extension of the source files.
-     */
-    public static function fileExtension(): string
-    {
-        return static::$fileExtension;
-    }
-
-    /**
-     * Set the output directory for the HydePage class.
-     */
-    public static function setSourceDirectory(string $sourceDirectory): void
-    {
-        static::$sourceDirectory = unslash($sourceDirectory);
-    }
-
-    /**
-     * Set the source directory for the HydePage class.
-     */
-    public static function setOutputDirectory(string $outputDirectory): void
-    {
-        static::$outputDirectory = unslash($outputDirectory);
-    }
-
-    /**
-     * Set the file extension for the HydePage class.
-     */
-    public static function setFileExtension(string $fileExtension): void
-    {
-        static::$fileExtension = rtrim('.'.ltrim($fileExtension, '.'), '.');
-    }
-
-    /**
      * Qualify a page identifier into a local file path for the page source file relative to the project root.
      */
     public static function sourcePath(string $identifier): string
