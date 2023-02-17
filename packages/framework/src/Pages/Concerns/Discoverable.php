107683e8ce541836008fd080e829d7b3ea2c6643
Caen De Silva <caen@desilva.se>
2023-02-17 19:11:10 +0100

    Add trait documentation

--- packages/framework/src/Pages/Concerns/Discoverable.php
+++ packages/framework/src/Pages/Concerns/Discoverable.php
@@ -4,6 +4,10 @@ declare(strict_types=1);
 
 namespace Hyde\Pages\Concerns;
 
+/**
+ * This trait implements the DiscoverableContract interface,
+ * and is used by auto-discoverable HydePage classes.
+ */
 trait Discoverable
 {
     //
