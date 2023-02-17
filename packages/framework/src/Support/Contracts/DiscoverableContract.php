eb7eaf8aa70fa20d6cbf6b4314942aa985466bf6
Caen De Silva <caen@desilva.se>
2023-02-17 13:03:22 +0100

    Add property static annotations for interface required properties

--- packages/framework/src/Support/Contracts/DiscoverableContract.php
+++ packages/framework/src/Support/Contracts/DiscoverableContract.php
@@ -6,6 +6,10 @@ namespace Hyde\Support\Contracts;
 
 /**
  * Interface for discoverable HydePage classes.
+ *
+ * @property static string $sourceDirectory
+ * @property static string $outputDirectory
+ * @property static string $fileExtension
  */
 interface DiscoverableContract
 {
