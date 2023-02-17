8df2351c87b7b7c53aca16c022f174a6c4442a20
Caen De Silva <caen@desilva.se>
2023-02-17 19:55:02 +0100

    Move discovery properties to DiscoverablePage class

--- packages/framework/src/Pages/Concerns/DiscoverablePage.php
+++ packages/framework/src/Pages/Concerns/DiscoverablePage.php
@@ -12,5 +12,18 @@ use Hyde\Support\Contracts\DiscoverableContract;
  */
 abstract class DiscoverablePage extends HydePage implements DiscoverableContract
 {
-    //
+    /**
+     * @var non-empty-string The directory in where source files are stored. Relative to the Hyde root directory.
+     */
+    protected static string $sourceDirectory;
+
+    /**
+     * @var string The output subdirectory to store compiled page HTML. Relative to the _site directory.
+     */
+    protected static string $outputDirectory;
+
+    /**
+     * @var string The file extension of the source files. Normalized to include a leading dot.
+     */
+    protected static string $fileExtension;
 }
