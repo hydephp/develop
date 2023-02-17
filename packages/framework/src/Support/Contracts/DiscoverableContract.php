7ed356ad7ce80f3730d457ce220b531530272b87
Caen De Silva <caen@desilva.se>
2023-02-17 13:07:17 +0100

    Contract required discovery property setters

--- packages/framework/src/Support/Contracts/DiscoverableContract.php
+++ packages/framework/src/Support/Contracts/DiscoverableContract.php
@@ -18,4 +18,10 @@ interface DiscoverableContract
     public static function getOutputDirectory(): string;
 
     public static function getFileExtension(): string;
+
+    public static function setSourceDirectory(string $sourceDirectory): void;
+
+    public static function setOutputDirectory(string $outputDirectory): void;
+
+    public static function setFileExtension(string $fileExtension): void;
 }
