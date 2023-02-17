497077b0e8113aeae68494d33b5d1042d7141962
Caen De Silva <caen@desilva.se>
2023-02-17 13:09:29 +0100

    Add PHPDoc comments to interface methods

--- packages/framework/src/Support/Contracts/DiscoverableContract.php
+++ packages/framework/src/Support/Contracts/DiscoverableContract.php
@@ -13,15 +13,33 @@ namespace Hyde\Support\Contracts;
  */
 interface DiscoverableContract
 {
+    /**
+     * Get the source directory for the HydePage class.
+     */
     public static function getSourceDirectory(): string;
 
+    /**
+     * Get the output directory for the HydePage class.
+     */
     public static function getOutputDirectory(): string;
 
+    /**
+     * Get the file extension for the HydePage class.
+     */
     public static function getFileExtension(): string;
 
+    /**
+     * Set the source directory for the HydePage class.
+     */
     public static function setSourceDirectory(string $sourceDirectory): void;
 
+    /**
+     * Set the output directory for the HydePage class.
+     */
     public static function setOutputDirectory(string $outputDirectory): void;
 
+    /**
+     * Set the file extension for the HydePage class.
+     */
     public static function setFileExtension(string $fileExtension): void;
 }
