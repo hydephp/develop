0cd2952fb5ed2f5bb0e20a21fce61a39819333f0
Caen De Silva <caen@desilva.se>
2023-02-17 22:08:13 +0100

    Remove outputDirectory property and accessors from contract

--- packages/framework/src/Support/Contracts/DiscoverableContract.php
+++ packages/framework/src/Support/Contracts/DiscoverableContract.php
@@ -8,7 +8,6 @@ namespace Hyde\Support\Contracts;
  * Interface for discoverable HydePage classes.
  *
  * @property string $sourceDirectory (static)
- * @property string $outputDirectory (static)
  * @property string $fileExtension (static)
  */
 interface DiscoverableContract
@@ -21,11 +20,6 @@ interface DiscoverableContract
     public static function sourceDirectory(): string;
 
     /**
-     * Get the output directory for the HydePage class.
-     */
-    public static function outputDirectory(): string;
-
-    /**
      * Get the file extension for the HydePage class.
      */
     public static function fileExtension(): string;
@@ -36,11 +30,6 @@ interface DiscoverableContract
      * @param  non-empty-string  $sourceDirectory
      */
     public static function setSourceDirectory(string $sourceDirectory): void;
-
-    /**
-     * Set the output directory for the HydePage class.
-     */
-    public static function setOutputDirectory(string $outputDirectory): void;
 
     /**
      * Set the file extension for the HydePage class.
