ff5a98265948514d7fec206daa4928a9666e91ac
Caen De Silva <caen@desilva.se>
2023-02-19 14:52:58 +0100

    Add todo

--- packages/framework/src/Framework/Features/Metadata/PageMetadataBag.php
+++ packages/framework/src/Framework/Features/Metadata/PageMetadataBag.php
@@ -74,6 +74,7 @@ class PageMetadataBag extends MetadataBag
         // Since this is run before the page is rendered, we don't have the currentPage property
         // but since we know that this is for a blog post we know what the property will be
         // since Hyde does not currently support custom Blog post output directories.
+        // TODO: Refactor to be dynamic as Hyde now supports custom blog post output directories.
         return str_starts_with($image, 'http') ? $image : "../$image";
     }
 }
