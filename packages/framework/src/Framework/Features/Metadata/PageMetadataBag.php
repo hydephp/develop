89350f3c9ca1a41cddfa329499a807fcf0163fb4
Caen De Silva <caen@desilva.se>
2023-02-19 15:16:58 +0100

    Flip concatenated terms for more logical and accurate flow

--- packages/framework/src/Framework/Features/Metadata/PageMetadataBag.php
+++ packages/framework/src/Framework/Features/Metadata/PageMetadataBag.php
@@ -74,6 +74,6 @@ class PageMetadataBag extends MetadataBag
         // Since this is run before the page is rendered, we don't have the currentPage property.
         // But since we need to run some of the same calculations here to resolve the image path link.
         return str_starts_with($image, 'http') ? $image
-            : str_repeat('../', substr_count($this->page->identifier . MarkdownPost::outputDirectory(), '/') + 1) . $image;
+            : str_repeat('../', substr_count(MarkdownPost::outputDirectory().$this->page->identifier, '/') + 1) . $image;
     }
 }
