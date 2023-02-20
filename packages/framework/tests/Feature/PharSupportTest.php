4c6f09bbb5d05c4b53f8ea702f0af9b2793a46d6
Caen De Silva <caen@desilva.se>
2023-02-20 16:45:32 +0100

    Create PharSupportTest.php

---
+++ packages/framework/tests/Feature/PharSupportTest.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hyde\Framework\Testing\Feature;
+
+use Hyde\Foundation\PharSupport;
+use Hyde\Testing\TestCase;
+
+/**
+ * @covers \Hyde\Foundation\PharSupport
+ */
+class PharSupportTest extends TestCase
+{
+    //
+}
