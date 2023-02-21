6ec06d097a5b9176e88bbd722f96222250464c64
Caen De Silva <caen@desilva.se>
2023-02-21 10:28:54 +0100

    Create FileConflictExceptionTest.php

---
+++ packages/framework/tests/Unit/FileConflictExceptionTest.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hyde\Framework\Testing\Unit;
+
+use Hyde\Framework\Exceptions\FileConflictException;
+use Hyde\Testing\TestCase;
+
+/**
+ * @covers \Hyde\Framework\Exceptions\FileConflictException
+ */
+class FileConflictExceptionTest extends TestCase
+{
+    //
+}
