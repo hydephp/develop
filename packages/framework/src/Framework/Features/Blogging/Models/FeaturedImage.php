d41d108feb43497b4953a773fd39bbf885d57226
Caen De Silva <caen@desilva.se>
2023-02-21 22:07:10 +0100

    Narrow down mixed return type for dynamic call method

--- packages/framework/src/Framework/Features/Blogging/Models/FeaturedImage.php
+++ packages/framework/src/Framework/Features/Blogging/Models/FeaturedImage.php
@@ -100,7 +100,7 @@ abstract class FeaturedImage implements Stringable, FeaturedImageSchema
         return $metadata;
     }
 
-    public function __call(string $name, array $arguments): mixed
+    public function __call(string $name, array $arguments): null|bool|string
     {
         if (Str::startsWith($name, 'get')) {
             $property = Str::camel(Str::after($name, 'get'));
