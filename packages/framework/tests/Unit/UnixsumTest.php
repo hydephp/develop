b23e2a5d9af5f4632c42316d0b7d304c642fcdf1
Caen De Silva <caen@desilva.se>
2023-02-21 15:54:28 +0100

    Replace function qualifiers with imports

--- packages/framework/tests/Unit/UnixsumTest.php
+++ packages/framework/tests/Unit/UnixsumTest.php
@@ -6,55 +6,58 @@ namespace Hyde\Framework\Testing\Unit;
 
 use Hyde\Testing\TestCase;
 
+use function hyde\unixsum;
+use function hyde\unixsum_file;
+
 class UnixsumTest extends TestCase
 {
     public function test_method_returns_string()
     {
-        $this->assertIsString(\hyde\unixsum('foo'));
+        $this->assertIsString(unixsum('foo'));
     }
 
     public function test_method_returns_string_with_length_of_32()
     {
-        $this->assertEquals(32, strlen(\hyde\unixsum('foo')));
+        $this->assertEquals(32, strlen(unixsum('foo')));
     }
 
     public function test_method_returns_string_matching_expected_format()
     {
-        $this->assertMatchesRegularExpression('/^[a-f0-9]{32}$/', \hyde\unixsum('foo'));
+        $this->assertMatchesRegularExpression('/^[a-f0-9]{32}$/', unixsum('foo'));
     }
 
     public function test_method_returns_same_value_for_same_string_using_normal_method()
     {
-        $this->assertEquals(md5('foo'), \hyde\unixsum('foo'));
+        $this->assertEquals(md5('foo'), unixsum('foo'));
     }
 
     public function test_method_returns_different_value_for_different_string()
     {
-        $this->assertNotEquals(\hyde\unixsum('foo'), \hyde\unixsum('bar'));
+        $this->assertNotEquals(unixsum('foo'), unixsum('bar'));
     }
 
     public function test_function_is_case_sensitive()
     {
-        $this->assertNotEquals(\hyde\unixsum('foo'), \hyde\unixsum('FOO'));
+        $this->assertNotEquals(unixsum('foo'), unixsum('FOO'));
     }
 
     public function test_function_is_space_sensitive()
     {
-        $this->assertNotEquals(\hyde\unixsum(' foo '), \hyde\unixsum('foo'));
+        $this->assertNotEquals(unixsum(' foo '), unixsum('foo'));
     }
 
     public function test_method_returns_same_value_regardless_of_end_of_line_sequence()
     {
-        $this->assertEquals(\hyde\unixsum('foo'), \hyde\unixsum('foo'));
-        $this->assertEquals(\hyde\unixsum("foo\n"), \hyde\unixsum("foo\n"));
-        $this->assertEquals(\hyde\unixsum("foo\n"), \hyde\unixsum("foo\r"));
-        $this->assertEquals(\hyde\unixsum("foo\n"), \hyde\unixsum("foo\r\n"));
+        $this->assertEquals(unixsum('foo'), unixsum('foo'));
+        $this->assertEquals(unixsum("foo\n"), unixsum("foo\n"));
+        $this->assertEquals(unixsum("foo\n"), unixsum("foo\r"));
+        $this->assertEquals(unixsum("foo\n"), unixsum("foo\r\n"));
     }
 
     public function test_method_returns_same_value_for_string_with_mixed_end_of_line_sequences()
     {
-        $this->assertEquals(\hyde\unixsum("foo\nbar\r\nbaz\r\n"),
-            \hyde\unixsum("foo\nbar\nbaz\n"));
+        $this->assertEquals(unixsum("foo\nbar\r\nbaz\r\n"),
+            unixsum("foo\nbar\nbaz\n"));
     }
 
     public function test_method_returns_same_value_when_loaded_from_file()
@@ -63,7 +66,7 @@ class UnixsumTest extends TestCase
         $file = tempnam(sys_get_temp_dir(), 'foo');
         file_put_contents($file, $string);
 
-        $this->assertEquals(\hyde\unixsum($string), \hyde\unixsum(file_get_contents($file)));
+        $this->assertEquals(unixsum($string), unixsum(file_get_contents($file)));
 
         unlink($file);
     }
@@ -73,7 +76,7 @@ class UnixsumTest extends TestCase
         $string = "foo\nbar\r\nbaz\r\n";
         file_put_contents('foo', $string);
 
-        $this->assertEquals(\hyde\unixsum($string), \hyde\unixsum_file('foo'));
+        $this->assertEquals(unixsum($string), unixsum_file('foo'));
 
         unlink('foo');
     }
