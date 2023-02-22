10b1c2cf37234b1ee33c73f093dd880aebfd03ae
Caen De Silva <caen@desilva.se>
2023-02-22 09:50:48 +0100

    Remove now unused deprecated LocalFeaturedImage class

--- packages/framework/src/Framework/Features/Blogging/Models/LocalFeaturedImage.php
+++ packages/framework/src/Framework/Features/Blogging/Models/LocalFeaturedImage.php
@@ -4,8 +4,3 @@ declare(strict_types=1);
 
 namespace Hyde\Framework\Features\Blogging\Models;
 
-/** @deprecated Will be merged into FeaturedImage.php */
-class LocalFeaturedImage extends FeaturedImage
-{
-    //
-}
