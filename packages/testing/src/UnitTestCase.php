0c549823f7b272ccd7c1ecc7e6298a62ed9fe2b2
Caen De Silva <caen@desilva.se>
2023-02-22 10:52:51 +0100

    Add unit testing helper to instantiate the kernel

--- packages/testing/src/UnitTestCase.php
+++ packages/testing/src/UnitTestCase.php
@@ -4,9 +4,13 @@ declare(strict_types=1);
 
 namespace Hyde\Testing;
 
+use Hyde\Foundation\HydeKernel;
 use PHPUnit\Framework\TestCase as BaseTestCase;
 
 abstract class UnitTestCase extends BaseTestCase
 {
-    //
+    protected function setupKernel(): void
+    {
+        HydeKernel::setInstance(new HydeKernel());
+    }
 }
