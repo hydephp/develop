770447f85e263a5ae5d7f528659649100c8ed02d
Caen De Silva <caen@desilva.se>
2023-02-22 09:51:02 +0100

    Remove now unused deprecated RemoteFeaturedImage class

--- packages/framework/src/Framework/Features/Blogging/Models/RemoteFeaturedImage.php
+++ packages/framework/src/Framework/Features/Blogging/Models/RemoteFeaturedImage.php
@@ -4,8 +4,3 @@ declare(strict_types=1);
 
 namespace Hyde\Framework\Features\Blogging\Models;
 
-/** @deprecated Will be merged into FeaturedImage.php */
-class RemoteFeaturedImage extends FeaturedImage
-{
-    //
-}
