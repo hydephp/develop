5aa178d0bfc34d05d863972478177b623ccde2b4
Caen De Silva <caen@desilva.se>
2023-02-22 07:23:02 +0100

    Deprecate protected $type property

--- packages/framework/src/Framework/Features/Blogging/Models/FeaturedImage.php
+++ packages/framework/src/Framework/Features/Blogging/Models/FeaturedImage.php
@@ -42,7 +42,11 @@ abstract class FeaturedImage implements Stringable, FeaturedImageSchema
     /** @deprecated Can be implicitly determined by source prefix */
     protected final const TYPE_REMOTE = 'remote';
 
-    /** @var self::TYPE_* */
+    /**
+     * @deprecated Can be implicitly determined by source prefix
+     *
+     * @var self::TYPE_*
+     */
     protected readonly string $type;
     protected readonly string $source;
 
