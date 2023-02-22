f2558aa406fe34502f0b9336ddc4e4582543f2ae
Caen De Silva <caen@desilva.se>
2023-02-22 10:25:15 +0100

    Use empty instead of is_null

--- packages/framework/src/Framework/Factories/FeaturedImageFactory.php
+++ packages/framework/src/Framework/Factories/FeaturedImageFactory.php
@@ -67,7 +67,7 @@ class FeaturedImageFactory extends Concerns\PageDataFactory implements FeaturedI
     {
         $value = $this->getStringMatter('image') ?? $this->getStringMatter('image.source');
 
-        if (is_null($value)) {
+        if (empty($value)) {
             // Todo, we might want to add a note about which file caused the error.
             // We could also check for these before calling the factory, and just ignore the image if it's not valid.
             throw new RuntimeException('No featured image source was found');
