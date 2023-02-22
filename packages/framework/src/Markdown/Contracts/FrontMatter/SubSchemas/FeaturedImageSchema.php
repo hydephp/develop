b963070d871f53d1e50eab1ca10dbd0a955b02bd
Caen De Silva <caen@desilva.se>
2023-02-22 07:20:11 +0100

    Revert "Add precedence annotation to schema property definition"
    This reverts commit 43f9b52a6f54a6aa61c7c9c9eeb5ff563becc142.

--- packages/framework/src/Markdown/Contracts/FrontMatter/SubSchemas/FeaturedImageSchema.php
+++ packages/framework/src/Markdown/Contracts/FrontMatter/SubSchemas/FeaturedImageSchema.php
@@ -12,7 +12,7 @@ interface FeaturedImageSchema
 {
     public const FEATURED_IMAGE_SCHEMA = [
         'path'           => 'string',
-        'url'            => 'string', // Takes precedence over path
+        'url'            => 'string',
         'description'    => 'string',
         'title'          => 'string',
         'copyright'      => 'string',
