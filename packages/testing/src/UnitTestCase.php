f59924dc8016baaa3665d5b4de9ee8dbbaf32b5d
Caen De Silva <caen@desilva.se>
2023-02-22 11:05:31 +0100

    Add helper to cache setup of kernel since static kernel only needs to be set up once
    This just saves 0.002ms/call which is nothing at all. But I think it's semantically nicer to write.

--- packages/testing/src/UnitTestCase.php
+++ packages/testing/src/UnitTestCase.php
@@ -9,8 +9,18 @@ use PHPUnit\Framework\TestCase as BaseTestCase;
 
 abstract class UnitTestCase extends BaseTestCase
 {
+    protected static bool $hasSetUpKernel = false;
+
+    protected function needsKernel(): void
+    {
+        if (! self::$hasSetUpKernel) {
+            $this->setupKernel();
+        }
+    }
+
     protected function setupKernel(): void
     {
         HydeKernel::setInstance(new HydeKernel());
+        self::$hasSetUpKernel = true;
     }
 }
