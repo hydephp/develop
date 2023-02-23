c65fcb0d8bdff59448d0e7e8a27a6556104f8b1d
Caen De Silva <caen@desilva.se>
2023-02-23 13:08:35 +0100

    Move logic to new invokable class method

--- packages/publications/src/Actions/GeneratesPublicationTagPages.php
+++ packages/publications/src/Actions/GeneratesPublicationTagPages.php
@@ -4,7 +4,73 @@ declare(strict_types=1);
 
 namespace Hyde\Publications\Actions;
 
+use Hyde\Foundation\Kernel\PageCollection;
+
 class GeneratesPublicationTagPages
 {
-    //
+    public function __invoke(PageCollection $collection): void
+    {
+        // Retrieve publication types and publication tags
+        $publicationTypes = \Hyde\Publications\PublicationService::getPublicationTypes();
+        $tagGroups = new \Hyde\Publications\Models\PublicationTags();
+
+        // Initialize arrays to hold tag counts and pages by tag
+        $tagCounts = [];
+        $pagesByTag = [];
+
+        // Loop through each publication type to retrieve publications and associated tags
+        foreach ($publicationTypes as $publicationType) {
+            // Retrieve tag fields for the current publication type
+            $publicationTagFieldsByName = [];
+            foreach ($publicationType->getFields() as $fieldDefinition) {
+                if ($fieldDefinition->type->name == 'Tag') {
+                    $publicationTagFieldsByName[] = $fieldDefinition->name;
+                }
+            }
+
+            // Skip the current publication type if no tag fields are found
+            if (! $publicationTagFieldsByName) {
+                continue;
+            }
+
+            // Retrieve publications for the current publication type
+            $publications = \Hyde\Publications\PublicationService::getPublicationsForPubType($publicationType);
+
+            // Loop through each publication to retrieve associated tags
+            foreach ($publications as $publication) {
+                foreach ($publicationTagFieldsByName as $tagFieldName) {
+                    $tags = (array) $publication->matter->get($tagFieldName);
+                    foreach ($tags as $tag) {
+                        // Increment tag count for the current tag
+                        if (! isset($tagCounts[$tag])) {
+                            $tagCounts[$tag] = 0;
+                        }
+                        $tagCounts[$tag]++;
+
+                        // Add the current publication to the list of pages for the current tag
+                        if (! isset($pagesByTag[$tag])) {
+                            $pagesByTag[$tag] = [];
+                        }
+                        $pagesByTag[$tag][] = $publication->getIdentifier();
+                    }
+                }
+            }
+        }
+
+        // Build the index tags page
+        $indexTagsPage = new \Hyde\Pages\InMemoryPage('tags/index', ['tagCounts' => $tagCounts], 'blah', 'pages/tags.blade.php');
+        $pageCollection = $collection;
+        $pageCollection->addPage($indexTagsPage);
+
+        // Build individual page lists for each tag
+        foreach ($pagesByTag as $tag => $pages) {
+            $tagPage = new \Hyde\Pages\InMemoryPage(
+                "tags/$tag",
+                ['tag' => $tag, 'pages' => $pages],
+                'blah',
+                'pages/tagPageList.blade.php'
+            );
+            $pageCollection->addPage($tagPage);
+        }
+    }
 }
--- packages/publications/src/PublicationsExtension.php
+++ packages/publications/src/PublicationsExtension.php
@@ -8,6 +8,7 @@ use Hyde\Facades\Filesystem;
 use Hyde\Foundation\Concerns\HydeExtension;
 use Hyde\Foundation\Kernel\PageCollection;
 use Hyde\Pages\InMemoryPage;
+use Hyde\Publications\Actions\GeneratesPublicationTagPages;
 use Hyde\Publications\Models\PublicationListPage;
 use Hyde\Publications\Models\PublicationPage;
 use Hyde\Publications\Models\PublicationType;
@@ -85,67 +86,6 @@ class PublicationsExtension extends HydeExtension
 
     protected static function generatePublicationTagPages(PageCollection $collection): void
     {
-        // Retrieve publication types and publication tags
-        $publicationTypes = \Hyde\Publications\PublicationService::getPublicationTypes();
-        $tagGroups = new \Hyde\Publications\Models\PublicationTags();
-
-        // Initialize arrays to hold tag counts and pages by tag
-        $tagCounts = [];
-        $pagesByTag = [];
-
-        // Loop through each publication type to retrieve publications and associated tags
-        foreach ($publicationTypes as $publicationType) {
-            // Retrieve tag fields for the current publication type
-            $publicationTagFieldsByName = [];
-            foreach ($publicationType->getFields() as $fieldDefinition) {
-                if ($fieldDefinition->type->name == 'Tag') {
-                    $publicationTagFieldsByName[] = $fieldDefinition->name;
-                }
-            }
-
-            // Skip the current publication type if no tag fields are found
-            if (! $publicationTagFieldsByName) {
-                continue;
-            }
-
-            // Retrieve publications for the current publication type
-            $publications = \Hyde\Publications\PublicationService::getPublicationsForPubType($publicationType);
-
-            // Loop through each publication to retrieve associated tags
-            foreach ($publications as $publication) {
-                foreach ($publicationTagFieldsByName as $tagFieldName) {
-                    $tags = (array) $publication->matter->get($tagFieldName);
-                    foreach ($tags as $tag) {
-                        // Increment tag count for the current tag
-                        if (! isset($tagCounts[$tag])) {
-                            $tagCounts[$tag] = 0;
-                        }
-                        $tagCounts[$tag]++;
-
-                        // Add the current publication to the list of pages for the current tag
-                        if (! isset($pagesByTag[$tag])) {
-                            $pagesByTag[$tag] = [];
-                        }
-                        $pagesByTag[$tag][] = $publication->getIdentifier();
-                    }
-                }
-            }
-        }
-
-        // Build the index tags page
-        $indexTagsPage = new \Hyde\Pages\InMemoryPage('tags/index', ['tagCounts' => $tagCounts], 'blah', 'pages/tags.blade.php');
-        $pageCollection = $collection;
-        $pageCollection->addPage($indexTagsPage);
-
-        // Build individual page lists for each tag
-        foreach ($pagesByTag as $tag => $pages) {
-            $tagPage = new \Hyde\Pages\InMemoryPage(
-                "tags/$tag",
-                ['tag' => $tag, 'pages' => $pages],
-                'blah',
-                'pages/tagPageList.blade.php'
-            );
-            $pageCollection->addPage($tagPage);
-        }
+        (new GeneratesPublicationTagPages())->__invoke($collection);
     }
 }
