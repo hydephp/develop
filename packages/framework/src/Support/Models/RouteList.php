fd643ee0c78591a9bd1b24f06fd4e478f2b08805
Caen De Silva <caen@desilva.se>
2023-02-24 19:31:00 +0100

    Refactor to move styling logic to new RouteListItem model
    It's better to scope the responsibility to this class

--- packages/framework/src/Support/Models/RouteList.php
+++ packages/framework/src/Support/Models/RouteList.php
@@ -38,38 +38,7 @@ class RouteList implements Arrayable
     protected function generate(): array
     {
         return collect(Hyde::routes())->map(function (Route $route): array {
-            return [
-                'page_type' => $this->stylePageType($route->getPageClass()),
-                'source_file' => $this->styleSourcePath($route->getSourcePath(), $route->getPageClass()),
-                'output_file' => $this->styleOutputPath($route->getOutputPath()),
-                'route_key' => $this->styleRouteKey($route->getRouteKey()),
-            ];
+            return (new RouteListItem($route))->toArray();
         })->values()->toArray();
-    }
-
-    protected function stylePageType(string $class): string
-    {
-        return str_starts_with($class, 'Hyde') ? class_basename($class) : $class;
-    }
-
-    /** @param  class-string<\Hyde\Pages\Concerns\HydePage>  $class */
-    protected function styleSourcePath(string $path, string $class): string
-    {
-        return $class::isDiscoverable() ? $path : $this->getDynamicSourceLabel();
-    }
-
-    protected function styleOutputPath(string $path): string
-    {
-        return Site::getOutputDirectory()."/$path";
-    }
-
-    protected function styleRouteKey(string $key): string
-    {
-        return $key;
-    }
-
-    protected function getDynamicSourceLabel(): string
-    {
-        return 'dynamic';
     }
 }
--- packages/framework/src/Support/Models/RouteListItem.php
+++ packages/framework/src/Support/Models/RouteListItem.php
@@ -4,9 +4,51 @@ declare(strict_types=1);
 
 namespace Hyde\Support\Models;
 
+use Hyde\Facades\Site;
 use Illuminate\Contracts\Support\Arrayable;
 
 class RouteListItem implements Arrayable
 {
-    //
+    protected Route $route;
+
+    public function __construct(Route $route)
+    {
+        $this->route = $route;
+    }
+
+    public function toArray(): array
+    {
+        return [
+            'page_type' => $this->stylePageType($this->route->getPageClass()),
+            'source_file' => $this->styleSourcePath($this->route->getSourcePath(), $this->route->getPageClass()),
+            'output_file' => $this->styleOutputPath($this->route->getOutputPath()),
+            'route_key' => $this->styleRouteKey($this->route->getRouteKey()),
+        ];
+    }
+
+    protected function stylePageType(string $class): string
+    {
+        return str_starts_with($class, 'Hyde') ? class_basename($class) : $class;
+    }
+
+    /** @param  class-string<\Hyde\Pages\Concerns\HydePage>  $class */
+    protected function styleSourcePath(string $path, string $class): string
+    {
+        return $class::isDiscoverable() ? $path : $this->getDynamicSourceLabel();
+    }
+
+    protected function styleOutputPath(string $path): string
+    {
+        return Site::getOutputDirectory()."/$path";
+    }
+
+    protected function styleRouteKey(string $key): string
+    {
+        return $key;
+    }
+
+    protected function getDynamicSourceLabel(): string
+    {
+        return 'dynamic';
+    }
 }
