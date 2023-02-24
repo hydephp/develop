44faa4f257cd985790ce70a837cc260e8b8344dd
Caen De Silva <caen@desilva.se>
2023-02-24 19:42:53 +0100

    Update constructor semantics

--- packages/framework/src/Support/Models/RouteListItem.php
+++ packages/framework/src/Support/Models/RouteListItem.php
@@ -20,10 +20,10 @@ class RouteListItem implements Arrayable
     {
         $this->route = $route;
 
-        $this->pageType = $this->stylePageType($this->route->getPageClass());
-        $this->sourceFile = $this->styleSourcePath($this->route->getSourcePath());
-        $this->outputFile = $this->styleOutputPath($this->route->getOutputPath());
-        $this->routeKey = $this->styleRouteKey($this->route->getRouteKey());
+        $this->pageType = $this->stylePageType($route->getPageClass());
+        $this->sourceFile = $this->styleSourcePath($route->getSourcePath());
+        $this->outputFile = $this->styleOutputPath($route->getOutputPath());
+        $this->routeKey = $this->styleRouteKey($route->getRouteKey());
     }
 
     public function toArray(): array
