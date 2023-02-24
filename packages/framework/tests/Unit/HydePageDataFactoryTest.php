5147b1eb01564865cbbdcbab9cf53296df35dfb9
Caen De Silva <caen@desilva.se>
2023-02-24 15:08:47 +0100

    Create HydePageDataFactoryTest.php

---
+++ packages/framework/tests/Unit/HydePageDataFactoryTest.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hyde\Framework\Testing\Unit;
+
+use Hyde\Framework\Factories\HydePageDataFactory;
+use Hyde\Testing\TestCase;
+
+/**
+ * @covers \Hyde\Framework\Factories\HydePageDataFactory
+ */
+class HydePageDataFactoryTest extends TestCase
+{
+    //
+}
