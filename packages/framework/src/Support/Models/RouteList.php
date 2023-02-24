9a2608c40619beaef6d168d96d70b4b13c0d6a99
Caen De Silva <caen@desilva.se>
2023-02-24 19:25:51 +0100

    Mark RouteList class as internal

--- packages/framework/src/Support/Models/RouteList.php
+++ packages/framework/src/Support/Models/RouteList.php
@@ -9,6 +9,7 @@ use Hyde\Facades\Site;
 use Illuminate\Contracts\Support\Arrayable;
 
 /**
+ * @internal This class is experimental and is subject to change.
  * @experimental This class is experimental and is subject to change.
  * @see \Hyde\Framework\Testing\Feature\RouteListTest
  */
