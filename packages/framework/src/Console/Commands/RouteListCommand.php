6b8fd2a7fca750efa20a611cd55e2b58ff4333ef
Caen De Silva <caen@desilva.se>
2023-02-24 16:35:08 +0100

    Move route list generation logic to model class

--- packages/framework/src/Console/Commands/RouteListCommand.php
+++ packages/framework/src/Console/Commands/RouteListCommand.php
@@ -6,6 +6,7 @@ namespace Hyde\Console\Commands;
 
 use Hyde\Console\Concerns\Command;
 use Hyde\Hyde;
+use Hyde\Support\Models\RouteList;
 
 /**
  * Hyde command to display the list of site routes.
@@ -34,46 +35,6 @@ class RouteListCommand extends Command
 
     protected function getRoutes(): array
     {
-        $routes = [];
-        /** @var \Hyde\Support\Models\Route $route */
-        foreach (Hyde::routes() as $route) {
-            $routes[] = [
-                $this->formatPageType($route->getPageClass()),
-                $this->formatSourcePath($route->getSourcePath(), $route->getPageClass()),
-                $this->formatOutputPath($route->getOutputPath()),
-                $route->getRouteKey(),
-            ];
-        }
-
-        return $routes;
-    }
-
-    protected function formatPageType(string $class): string
-    {
-        return str_starts_with($class, 'Hyde') ? class_basename($class) : $class;
-    }
-
-    /** @param  class-string<\Hyde\Pages\Concerns\HydePage>  $class */
-    protected function formatSourcePath(string $path, string $class): string
-    {
-        if (! $class::isDiscoverable()) {
-            return '<fg=yellow>dynamic</>';
-        }
-
-        return $this->clickablePathLink(static::createClickableFilepath(Hyde::path($path)), $path);
-    }
-
-    protected function formatOutputPath(string $path): string
-    {
-        if (! file_exists(Hyde::sitePath($path))) {
-            return "_site/$path";
-        }
-
-        return $this->clickablePathLink(static::createClickableFilepath(Hyde::sitePath($path)), "_site/$path");
-    }
-
-    protected function clickablePathLink(string $link, string $path): string
-    {
-        return "<href=$link>$path</>";
+        return (new RouteList())->toArray();
     }
 }
--- packages/framework/src/Support/Models/RouteList.php
+++ packages/framework/src/Support/Models/RouteList.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Hyde\Support\Models;
 
+use Hyde\Console\Concerns\Command;
+use Hyde\Hyde;
 use Illuminate\Contracts\Support\Arrayable;
 
 /**
@@ -13,6 +15,46 @@ class RouteList implements Arrayable
 {
     public function toArray(): array
     {
-        //
+        $routes = [];
+        /** @var \Hyde\Support\Models\Route $route */
+        foreach (Hyde::routes() as $route) {
+            $routes[] = [
+                $this->formatPageType($route->getPageClass()),
+                $this->formatSourcePath($route->getSourcePath(), $route->getPageClass()),
+                $this->formatOutputPath($route->getOutputPath()),
+                $route->getRouteKey(),
+            ];
+        }
+
+        return $routes;
+    }
+
+    protected function formatPageType(string $class): string
+    {
+        return str_starts_with($class, 'Hyde') ? class_basename($class) : $class;
+    }
+
+    /** @param  class-string<\Hyde\Pages\Concerns\HydePage>  $class */
+    protected function formatSourcePath(string $path, string $class): string
+    {
+        if (! $class::isDiscoverable()) {
+            return '<fg=yellow>dynamic</>';
+        }
+
+        return $this->clickablePathLink(Command::createClickableFilepath(Hyde::path($path)), $path);
+    }
+
+    protected function formatOutputPath(string $path): string
+    {
+        if (! file_exists(Hyde::sitePath($path))) {
+            return "_site/$path";
+        }
+
+        return $this->clickablePathLink(Command::createClickableFilepath(Hyde::sitePath($path)), "_site/$path");
+    }
+
+    protected function clickablePathLink(string $link, string $path): string
+    {
+        return "<href=$link>$path</>";
     }
 }
