80d2cd5a321351c034b99e584bb9441919cf713d
Caen De Silva <caen@desilva.se>
2023-02-24 16:32:02 +0100

    Class RouteList implements Arrayable

--- packages/framework/src/Support/Models/RouteList.php
+++ packages/framework/src/Support/Models/RouteList.php
@@ -4,10 +4,12 @@ declare(strict_types=1);
 
 namespace Hyde\Support\Models;
 
+use Illuminate\Contracts\Support\Arrayable;
+
 /**
  * @see \Hyde\Framework\Testing\Feature\RouteListTest
  */
-class RouteList
+class RouteList implements Arrayable
 {
     //
 }
