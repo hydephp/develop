e6c9a87f531db80c00e10a1c58cbd5844a780113
Caen De Silva <caen@desilva.se>
2023-02-24 15:09:08 +0100

    Extend UnitTestCase

--- packages/framework/tests/Unit/HydePageDataFactoryTest.php
+++ packages/framework/tests/Unit/HydePageDataFactoryTest.php
@@ -5,12 +5,12 @@ declare(strict_types=1);
 namespace Hyde\Framework\Testing\Unit;
 
 use Hyde\Framework\Factories\HydePageDataFactory;
-use Hyde\Testing\TestCase;
+use Hyde\Testing\UnitTestCase;
 
 /**
  * @covers \Hyde\Framework\Factories\HydePageDataFactory
  */
-class HydePageDataFactoryTest extends TestCase
+class HydePageDataFactoryTest extends UnitTestCase
 {
     //
 }
