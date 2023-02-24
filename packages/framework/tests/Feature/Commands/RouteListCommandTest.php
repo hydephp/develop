ee534a43da302ba719cd48cfc63ad4f146c54308
Caen De Silva <caen@desilva.se>
2023-02-24 17:19:12 +0100

    Remove tests covered by new test class

--- packages/framework/tests/Feature/Commands/RouteListCommandTest.php
+++ packages/framework/tests/Feature/Commands/RouteListCommandTest.php
@@ -4,9 +4,6 @@ declare(strict_types=1);
 
 namespace Hyde\Framework\Testing\Feature\Commands;
 
-use Hyde\Hyde;
-use Hyde\Pages\InMemoryPage;
-use Hyde\Support\Models\Route;
 use Hyde\Testing\TestCase;
 
 /**
@@ -29,40 +26,6 @@ class RouteListCommandTest extends TestCase
                     '_pages/index.blade.php',
                     '_site/index.html',
                     'index',
-                ],
-            ])->assertExitCode(0);
-    }
-
-    public function testClickableLinks()
-    {
-        $this->file('_site/index.html');
-        $this->artisan('route:list')
-            ->assertExitCode(0);
-    }
-
-    public function testWithDynamicPages()
-    {
-        Hyde::routes()->put('foo', new Route(new InMemoryPage('foo')));
-
-        $this->artisan('route:list')
-            ->expectsTable(['Page Type', 'Source File', 'Output File', 'Route Key'], [
-                [
-                    'BladePage',
-                    '_pages/404.blade.php',
-                    '_site/404.html',
-                    '404',
-                ],
-                [
-                    'BladePage',
-                    '_pages/index.blade.php',
-                    '_site/index.html',
-                    'index',
-                ],
-                [
-                    'InMemoryPage',
-                    '<fg=yellow>dynamic</>',
-                    '_site/foo.html',
-                    'foo',
                 ],
             ])->assertExitCode(0);
     }
