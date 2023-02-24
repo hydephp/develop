30cf3540a4583738e4c83eb206b4e4bf644a8a15
Caen De Silva <caen@desilva.se>
2023-02-24 16:29:53 +0100

    Create RouteListTest.php
    Update RouteList.php

---
+++ packages/framework/tests/Feature/RouteListTest.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hyde\Framework\Testing\Feature;
+
+use Hyde\Support\Models\RouteList;
+use Hyde\Testing\TestCase;
+
+/**
+ * @covers \Hyde\Support\Models\RouteList
+ */
+class RouteListTest extends TestCase
+{
+    //
+}
