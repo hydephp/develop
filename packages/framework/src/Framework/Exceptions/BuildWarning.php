94d6993093b6ed2109c3dd08505b3c032504e425
Caen De Silva <caen@desilva.se>
2023-02-25 19:00:41 +0100

    Document experimental state

--- packages/framework/src/Framework/Exceptions/BuildWarning.php
+++ packages/framework/src/Framework/Exceptions/BuildWarning.php
@@ -8,7 +8,8 @@ use RuntimeException;
 use Throwable;
 
 /**
- * @experimental
+ * @experimental Having a class like this extending an exception means it's easy to throw if enabled, however,
+ *               it's not required for the build warnings feature and may be replaced by a simple array.
  */
 class BuildWarning extends RuntimeException
 {
