03dcc1db4da4603e2223ba87a413d0ab3377dcdc
Caen De Silva <caen@desilva.se>
2023-02-25 12:03:20 +0100

    Create BuildWarningsTest.php

---
+++ packages/framework/tests/Unit/BuildWarningsTest.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hyde\Framework\Testing\Unit;
+
+use Hyde\Support\BuildWarnings;
+use Hyde\Testing\TestCase;
+
+/**
+ * @covers \Hyde\Support\BuildWarnings
+ */
+class BuildWarningsTest extends TestCase
+{
+    //
+}
