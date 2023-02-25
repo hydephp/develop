82ee423956b0960f23dad9c8a0117027784abdfb
Caen De Silva <caen@desilva.se>
2023-02-25 12:07:00 +0100

    Reorder test methods to follow dependent execution flow

--- packages/framework/tests/Unit/BuildWarningsTest.php
+++ packages/framework/tests/Unit/BuildWarningsTest.php
@@ -24,7 +24,7 @@ class BuildWarningsTest extends UnitTestCase
         //
     }
 
-    public function testReport()
+    public function testHasWarnings()
     {
         //
     }
@@ -34,7 +34,7 @@ class BuildWarningsTest extends UnitTestCase
         //
     }
 
-    public function testHasWarnings()
+    public function testReport()
     {
         //
     }
