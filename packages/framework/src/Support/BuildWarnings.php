672feff2f8e17b86213c19f5480ffd0a476ea526
Caen De Silva <caen@desilva.se>
2023-02-25 11:13:46 +0100

    Implement base build warnings class

--- packages/framework/src/Support/BuildWarnings.php
+++ packages/framework/src/Support/BuildWarnings.php
@@ -6,5 +6,25 @@ namespace Hyde\Support;
 
 class BuildWarnings
 {
-    //
+    protected static array $warnings = [];
+
+    public static function add(string $warning): void
+    {
+        self::$warnings[] = $warning;
+    }
+
+    public static function get(): array
+    {
+        return self::$warnings;
+    }
+
+    public static function hasWarnings(): bool
+    {
+        return count(self::$warnings) > 0;
+    }
+
+    public static function clear(): void
+    {
+        self::$warnings = [];
+    }
 }
