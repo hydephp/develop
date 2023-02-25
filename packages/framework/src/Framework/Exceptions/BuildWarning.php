dc5ba97fae0057aa54b4a341068c306db5c28d52
Caen De Silva <caen@desilva.se>
2023-02-25 18:40:11 +0100

    Publish parent constructor

--- packages/framework/src/Framework/Exceptions/BuildWarning.php
+++ packages/framework/src/Framework/Exceptions/BuildWarning.php
@@ -5,11 +5,15 @@ declare(strict_types=1);
 namespace Hyde\Framework\Exceptions;
 
 use RuntimeException;
+use Throwable;
 
 /**
  * @experimental
  */
 class BuildWarning extends RuntimeException
 {
-    //
+    public function __construct(string $message = "", int $code = 0, ?Throwable $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+    }
 }
