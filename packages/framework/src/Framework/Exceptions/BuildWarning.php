a594a50148b10f4d00151849141d1fc858102921
Caen De Silva <caen@desilva.se>
2023-02-25 18:40:20 +0100

    Replace quotes

--- packages/framework/src/Framework/Exceptions/BuildWarning.php
+++ packages/framework/src/Framework/Exceptions/BuildWarning.php
@@ -12,7 +12,7 @@ use Throwable;
  */
 class BuildWarning extends RuntimeException
 {
-    public function __construct(string $message = "", int $code = 0, ?Throwable $previous = null)
+    public function __construct(string $message = '', int $code = 0, ?Throwable $previous = null)
     {
         parent::__construct($message, $code, $previous);
     }
