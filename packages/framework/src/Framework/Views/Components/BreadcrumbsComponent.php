05fa93e5a4e125730fc8d5888eae3567331c4c6b
Caen De Silva <caen@desilva.se>
2023-02-26 20:43:13 +0100

    Remove unnecessary parentheses

--- packages/framework/src/Framework/Views/Components/BreadcrumbsComponent.php
+++ packages/framework/src/Framework/Views/Components/BreadcrumbsComponent.php
@@ -43,9 +43,9 @@ class BreadcrumbsComponent extends Component
 
             // if it's not the last field, add a trailing slash (since it must be a directory) otherwise add .html
             if ($key < count($fields) - 1) {
-                $path .= $field.('/');
+                $path .= $field.'/';
             } else {
-                $path .= $field.('.html');
+                $path .= $field.'.html';
             }
             $title = Str::of($field)->replace('-', ' ')->title();
             $breadcrumbs[Hyde::relativeLink($path)] = $title->toString();
