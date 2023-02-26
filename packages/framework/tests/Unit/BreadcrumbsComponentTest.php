2f0279217a0d658ca3d835e2442282ae0874cced
Caen De Silva <caen@desilva.se>
2023-02-26 16:21:34 +0100

    Add baseline rendering test

--- packages/framework/tests/Unit/BreadcrumbsComponentTest.php
+++ packages/framework/tests/Unit/BreadcrumbsComponentTest.php
@@ -9,6 +9,9 @@ use Hyde\Pages\MarkdownPage;
 use Hyde\Support\Facades\Render;
 use Hyde\Support\Models\Route;
 use Hyde\Testing\UnitTestCase;
+use Illuminate\Contracts\View\Factory as ViewFactory;
+use Illuminate\View\Factory;
+use Mockery;
 
 /**
  * @covers \Hyde\Framework\Views\Components\BreadcrumbsComponent
@@ -24,6 +27,8 @@ class BreadcrumbsComponentTest extends UnitTestCase
     protected function tearDown(): void
     {
         Render::clearResolvedInstance(\Hyde\Support\Models\Render::class);
+
+        app()->forgetInstance(Factory::class);
     }
 
     public function testCanConstruct()
@@ -31,5 +36,16 @@ class BreadcrumbsComponentTest extends UnitTestCase
         Render::shouldReceive('getCurrentRoute')->once()->andReturn(new Route(new MarkdownPage()));
 
         $this->assertInstanceOf(BreadcrumbsComponent::class, new BreadcrumbsComponent());
+    }
+
+    public function testCanRender()
+    {
+        Render::shouldReceive('getCurrentRoute')->once()->andReturn(new Route(new MarkdownPage()));
+
+        app()->instance(ViewFactory::class, Mockery::mock(Factory::class, function ($mock) {
+            $mock->shouldReceive('make')->once()->andReturn($mock);
+        }));
+
+        $this->assertInstanceOf(Factory::class, (new BreadcrumbsComponent())->render());
     }
 }
