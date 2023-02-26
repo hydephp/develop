af696132b7a4a305e7688b06cc074c6a5176d0c7
Caen De Silva <caen@desilva.se>
2023-02-26 22:25:35 +0100

    Create CommandUnitTest.php

---
+++ packages/framework/tests/Unit/CommandUnitTest.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hyde\Framework\Testing\Unit;
+
+use Hyde\Testing\UnitTestCase;
+
+/**
+ * @covers \Hyde\Console\Concerns\Command
+ */
+class CommandUnitTest extends UnitTestCase
+{
+    //
+}
