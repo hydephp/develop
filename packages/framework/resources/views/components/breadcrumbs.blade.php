0b7fce184353f7af92e84a221a34563a1673036b
Caen De Silva <caen@desilva.se>
2023-02-26 17:49:51 +0100

    Format PHP code in Blade view

--- packages/framework/resources/views/components/breadcrumbs.blade.php
+++ packages/framework/resources/views/components/breadcrumbs.blade.php
@@ -1,16 +1,16 @@
 @if (count($breadcrumbs) > 1)
     <nav aria-label="breadcrumb">
         <ol>
-            @foreach ($breadcrumbs as $path=>$title)
+            @foreach ($breadcrumbs as $path => $title)
                 <li>
-                    @if (!$loop->last)
+                    @if (! $loop->last)
                         <a href="{{ $path }}" class="hover:underline">{{ $title }}</a>
                     @else
                         <a href="{{ $path }}" aria-current="page">{{ $title }}</a>
                     @endif
                 </li>
 
-                @if (!$loop->last)
+                @if (! $loop->last)
                     &gt;
                 @endif
             @endforeach
