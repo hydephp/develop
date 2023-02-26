c8ecda4be8dd6fe17b0ddd98b50fbf790e39e08f
Caen De Silva <caen@desilva.se>
2023-02-26 15:59:12 +0100

    Move the getBreadcrumbs method from the Route model to view component

--- packages/framework/src/Framework/Views/Components/BreadcrumbComponent.php
+++ packages/framework/src/Framework/Views/Components/BreadcrumbComponent.php
@@ -7,6 +7,7 @@ namespace Hyde\Framework\Views\Components;
 use Hyde\Hyde;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
+use Illuminate\Support\Str;
 use Illuminate\View\Component;
 
 class BreadcrumbComponent extends Component
@@ -15,12 +16,36 @@ class BreadcrumbComponent extends Component
 
     public function __construct()
     {
-        $this->breadcrumbs = Hyde::currentRoute()->getBreadcrumbs();
+        $this->breadcrumbs = $this->getBreadcrumbs();
     }
 
     /** @interitDoc */
     public function render(): Factory|View
     {
         return view('hyde::components.breadcrumb');
+    }
+
+    public function getBreadcrumbs(): array
+    {
+        $identifier = Hyde::currentRoute()->getPage()->getIdentifier();
+        $breadcrumbs = ['/' => 'Home'];
+        if ($identifier == 'index') {
+            return $breadcrumbs;
+        }
+
+        $path = '';
+        $fields = Str::of($identifier)->explode('/');
+        foreach ($fields as $k => $field) {
+            if ($field == 'index') {
+                return $breadcrumbs;
+            }
+
+            // if it's not the last field, add a trailing slash (since it must be a directory)
+            $path .= $field.($k < count($fields) - 1 ? '/' : '');
+            $title = Str::of($field)->replace('-', ' ')->title();
+            $breadcrumbs[$path] = $title->toString();
+        }
+
+        return $breadcrumbs;
     }
 }
--- packages/framework/src/Support/Models/Route.php
+++ packages/framework/src/Support/Models/Route.php
@@ -83,30 +83,6 @@ class Route implements Stringable, SerializableContract
         return $this->page->getOutputPath();
     }
 
-    public function getBreadcrumbs(): array
-    {
-        $identifier = Hyde::currentRoute()->getPage()->getIdentifier();
-        $breadcrumbs = ['/' => 'Home'];
-        if ($identifier == 'index') {
-            return $breadcrumbs;
-        }
-
-        $path = '';
-        $fields = Str::of($identifier)->explode('/');
-        foreach ($fields as $k => $field) {
-            if ($field == 'index') {
-                return $breadcrumbs;
-            }
-
-            // if it's not the last field, add a trailing slash (since it must be a directory)
-            $path .= $field.($k < count($fields) - 1 ? '/' : '');
-            $title = Str::of($field)->replace('-', ' ')->title();
-            $breadcrumbs[$path] = $title->toString();
-        }
-
-        return $breadcrumbs;
-    }
-
     /**
      * Determine if the route instance matches another route or route key.
      */
