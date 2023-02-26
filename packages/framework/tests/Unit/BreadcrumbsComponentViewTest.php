d45381ddb8894117dd4f39eaec7f145d2ea35391
Caen De Silva <caen@desilva.se>
2023-02-26 17:28:52 +0100

    Add tests covering a multitude of scenarios

--- packages/framework/tests/Unit/BreadcrumbsComponentViewTest.php
+++ packages/framework/tests/Unit/BreadcrumbsComponentViewTest.php
@@ -42,6 +42,63 @@ class BreadcrumbsComponentViewTest extends TestCase
         $this->assertSame('', Blade::renderComponent(new BreadcrumbsComponent()));
     }
 
+    public function testRenderedBladeViewOnNestedPage()
+    {
+        Render::shouldReceive('getCurrentRoute')->once()->andReturn(new Route(new MarkdownPage('foo/bar')));
+
+        $html = Blade::renderComponent(new BreadcrumbsComponent());
+
+        $expected = <<<'HTML'
+            <nav aria-label="breadcrumb">
+                <a href="/" class="hover:underline">Home</a>
+                &nbsp;&gt;&gt;&nbsp;
+                <a href="foo/" class="hover:underline">Foo</a>
+                &nbsp;&gt;&gt;&nbsp;
+                Bar
+            </nav>
+        HTML;
+
+        $this->assertSame($this->stripIndentation($expected), $this->stripIndentation($html));
+    }
+
+    public function testRenderedBladeViewOnDeeplyNestedPage()
+    {
+        Render::shouldReceive('getCurrentRoute')->once()->andReturn(new Route(new MarkdownPage('foo/bar/baz')));
+
+        $html = Blade::renderComponent(new BreadcrumbsComponent());
+
+        $expected = <<<'HTML'
+            <nav aria-label="breadcrumb">
+                <a href="/" class="hover:underline">Home</a>
+                &nbsp;&gt;&gt;&nbsp;
+                <a href="foo/" class="hover:underline">Foo</a>
+                &nbsp;&gt;&gt;&nbsp;
+                <a href="foo/bar/" class="hover:underline">Bar</a>
+                &nbsp;&gt;&gt;&nbsp;
+                Baz
+            </nav>
+        HTML;
+
+        $this->assertSame($this->stripIndentation($expected), $this->stripIndentation($html));
+    }
+
+    public function testRenderedBladeViewOnNestedIndexPage()
+    {
+        Render::shouldReceive('getCurrentRoute')->once()->andReturn(new Route(new MarkdownPage('foo/index')));
+
+        $html = Blade::renderComponent(new BreadcrumbsComponent());
+
+        $expected = <<<'HTML'
+            <nav aria-label="breadcrumb">
+                <a href="/" class="hover:underline">Home</a>
+                &nbsp;&gt;&gt;&nbsp;
+                Foo
+            </nav>
+        HTML;
+
+        $this->assertSame($this->stripIndentation($expected), $this->stripIndentation($html));
+    }
+
     protected function stripIndentation(string $string): string
     {
         return implode("\n", array_filter(array_map(fn($line) => ltrim($line), explode("\n", $string))));
