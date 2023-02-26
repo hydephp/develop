0bb6e695f9737d983a6ce2eb4956c4f67439bb4a
Caen De Silva <caen@desilva.se>
2023-02-26 16:07:43 +0100

    Create BreadcrumbsComponentTest.php

---
+++ packages/framework/tests/Unit/BreadcrumbsComponentTest.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hyde\Framework\Testing\Unit;
+
+use Hyde\Framework\Views\Components\BreadcrumbsComponent;
+use Hyde\Testing\UnitTestCase;
+
+/**
+ * @covers \Hyde\Framework\Views\Components\BreadcrumbsComponent
+ */
+class BreadcrumbsComponentTest extends UnitTestCase
+{
+    //
+}
