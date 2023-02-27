283c41b20d80e23f88c638e73d8e0ecb8d60b4e5
Caen De Silva <caen@desilva.se>
2023-02-27 16:41:02 +0100

    Fake processes

--- packages/framework/tests/Feature/Commands/ServeCommandTest.php
+++ packages/framework/tests/Feature/Commands/ServeCommandTest.php
@@ -5,12 +5,20 @@ declare(strict_types=1);
 namespace Hyde\Framework\Testing\Feature\Commands;
 
 use Hyde\Testing\TestCase;
+use Illuminate\Support\Facades\Process;
 
 /**
  * @covers \Hyde\Console\Commands\ServeCommand
  */
 class ServeCommandTest extends TestCase
 {
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        Process::fake();
+    }
+
     public function test_hyde_serve_command()
     {
         $this->artisan('serve')
