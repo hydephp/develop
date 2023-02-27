d446d3a8708d10eac437d1f749c4fea6ade01d6c
Caen De Silva <caen@desilva.se>
2023-02-27 12:57:30 +0100

    Test the HydeCLI binary can run

--- monorepo/scripts/tests/hyde-cli.php
+++ monorepo/scripts/tests/hyde-cli.php
@@ -15,3 +15,10 @@ test('can install composer dependencies', function () {
 
     $this->assert(file_exists(BASE_PATH . '/vendor/autoload.php'), 'Autoloader does not exist');
 });
+
+test('can run the HydeCLI binary', function () {
+    $output = shell_exec('cd ' . BASE_PATH . ' && php hyde --no-ansi');
+    $this->assert(str_contains($output, 'USAGE: hyde <command> [options] [arguments]'),
+        'HydeCLI output does not contain "USAGE: hyde <command> [options] [arguments]"'
+    );
+});
