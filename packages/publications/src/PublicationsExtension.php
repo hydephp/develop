8342a5bf202b1a88060ca9d44ea0babf1da0d4bc
Caen De Silva <caen@desilva.se>
2023-02-27 20:33:53 +0100

    Reset collection on boot so it's cleared (only applied in testing)

--- packages/publications/src/PublicationsExtension.php
+++ packages/publications/src/PublicationsExtension.php
@@ -59,6 +59,7 @@ class PublicationsExtension extends HydeExtension
 
     protected static function discoverPublicationPages(PageCollection $instance): void
     {
+        static::$types = new Collection(); // Reset if we are in a test environment
         static::$types->each(function (PublicationType $type) use ($instance): void {
             static::discoverPublicationPagesForType($type, $instance);
             static::generatePublicationListingPageForType($type, $instance);
