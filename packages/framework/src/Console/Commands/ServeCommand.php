c3475aa0839a93922a0227e4176e63b74e4a3081
Caen De Silva <caen@desilva.se>
2023-02-27 16:59:56 +0100

    Remove default value from the host option

--- packages/framework/src/Console/Commands/ServeCommand.php
+++ packages/framework/src/Console/Commands/ServeCommand.php
@@ -18,7 +18,7 @@ use function sprintf;
 class ServeCommand extends Command
 {
     /** @var string */
-    protected $signature = 'serve {--host=localhost} {--port= : <comment> [default: 8080] </comment>}';
+    protected $signature = 'serve {--host=} {--port= : <comment> [default: 8080] </comment>}';
 
     /** @var string */
     protected $description = 'Start the realtime compiler server.';
