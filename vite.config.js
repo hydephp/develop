2c880ad0aee9ae47d39561341cb0a23899228dbe
Caen De Silva <caen@desilva.se>
2023-02-27 14:29:28 +0100

    Migrate Webpack file documentation to Vite file

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,7 @@
+// Using Vite is optional as the styles you need to get started are already included.
+// However, if you add new Tailwind classes, or any customizations, you can use
+// Vite to compile the assets. See https://hydephp.com/docs/master/managing-assets.html.
+
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
