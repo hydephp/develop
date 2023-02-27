78c0f81341ecf7fe2b80839b78e180a993afee4b
Caen De Silva <caen@desilva.se>
2023-02-27 14:16:54 +0100

    Update asset paths

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,8 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel([
-            'resources/css/app.css',
-            'resources/js/app.js',
+            'resources/assets/app.css',
+            'resources/assets/app.js',
         ]),
     ],
 });
