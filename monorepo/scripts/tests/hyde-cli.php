a012916e84a8841c4488e980d3dfebb76d3faace
Caen De Silva <caen@desilva.se>
2023-02-27 12:50:15 +0100

    Test can install Composer dependencies

--- monorepo/scripts/tests/hyde-cli.php
+++ monorepo/scripts/tests/hyde-cli.php
@@ -7,3 +7,11 @@ require_once __DIR__.'/MicroTest.php';
 test('example', function () {
     $this->assert(true);
 });
+
+test('can install composer dependencies', function () {
+    $this->assert(! file_exists(BASE_PATH . '/vendor/autoload.php'), 'Autoloader already exists');
+
+    shell_exec('cd ' . BASE_PATH . ' && composer install');
+
+    $this->assert(file_exists(BASE_PATH . '/vendor/autoload.php'), 'Autoloader does not exist');
+});
