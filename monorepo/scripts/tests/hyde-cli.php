60afe38cbaf35bdeaad49e13e1bab8830c14825a
Caen De Silva <caen@desilva.se>
2023-02-27 13:00:59 +0100

    Add additional output expectations

--- monorepo/scripts/tests/hyde-cli.php
+++ monorepo/scripts/tests/hyde-cli.php
@@ -11,9 +11,27 @@ test('example', function () {
 test('can install composer dependencies', function () {
     $this->assert(! file_exists(BASE_PATH.'/vendor/autoload.php'), 'Autoloader already exists');
 
-    shell_exec('cd '.BASE_PATH.' && composer install');
+    $output = shell_exec('cd '.BASE_PATH.' && composer install');
 
     $this->assert(file_exists(BASE_PATH.'/vendor/autoload.php'), 'Autoloader does not exist');
+
+    $expected = [
+        'Loading composer repositories with package information',
+        'Installing dependencies from lock file',
+        'Generating optimized autoload files',
+    ];
+
+    foreach ($expected as $line) {
+        $this->assert(str_contains($output, $line), 'Composer output does not contain expected "'.$line.'"');
+    }
+
+    $this->assert(str_contains($output, '@php -r "@unlink(\'./app/storage/framework/cache/packages.php\');"'),
+        'The package cache file was not deleted'
+    );
+
+    $this->assert(str_contains($output, '@php hyde package:discover --ansi'),
+        'The package discovery command was not run'
+    );
 });
 
 test('can run the HydeCLI binary', function () {
