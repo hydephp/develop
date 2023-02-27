8b5af66c97a1e88d6fc748ef984d8e063c72b304
Caen De Silva <caen@desilva.se>
2023-02-27 13:32:35 +0100

    Implement the test

--- monorepo/scripts/tests/project-styles.php
+++ monorepo/scripts/tests/project-styles.php
@@ -7,3 +7,28 @@ require_once __DIR__.'/MicroTest.php';
 test('example', function () {
     $this->assert(true);
 });
+
+test('can install node dependencies', function () {
+    $this->assert(! file_exists(BASE_PATH.'/node_modules'), 'Node modules already exist');
+
+    shell_exec('cd '.BASE_PATH.' && npm install');
+
+    $this->assert(file_exists(BASE_PATH.'/node_modules'), 'Node modules do not exist');
+});
+
+test('can build assets using laravel mix', function () {
+    $output = shell_exec('cd '.BASE_PATH.' && npm run dev');
+    $this->assert(str_contains($output, 'webpack compiled successfully'),
+        'Laravel Mix output does not contain "webpack compiled successfully"'
+    );
+
+    $this->assert(file_exists(BASE_PATH.'/_media/app.css'), 'CSS file does not exist');
+    $this->assert(file_exists(BASE_PATH.'/_site/media/app.css'), 'CSS file does not exist');
+
+    $this->assert(file_exists(BASE_PATH.'/_media/app.js'), 'JS file does not exist');
+    $this->assert(file_exists(BASE_PATH.'/_site/media/app.js'), 'JS file does not exist');
+
+    $this->assert(file_exists(BASE_PATH.'/_media/mix-manifest.json'), 'Mix manifest file does not exist');
+    $this->assert(file_exists(BASE_PATH.'/_site/media/mix-manifest.json'), 'Mix manifest file does not exist');
+});
+
