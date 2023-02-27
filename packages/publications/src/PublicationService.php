154e5899819230eb3d3b1c74f8b7069160899b55
Caen De Silva <caen@desilva.se>
2023-02-27 20:39:34 +0100

    Reset keys to match method data structure

--- packages/publications/src/PublicationService.php
+++ packages/publications/src/PublicationService.php
@@ -37,7 +37,7 @@ class PublicationService
     public static function getPublicationsForType(PublicationType $publicationType, ?string $sortField = null, ?bool $sortAscending = null): Collection
     {
         /** @var Collection<string, PublicationPage> $publications */
-        $publications = Hyde::pages()->getPages(PublicationPage::class);
+        $publications = Hyde::pages()->getPages(PublicationPage::class)->values();
 
         $sortAscending = $sortAscending !== null ? $sortAscending : $publicationType->sortAscending;
         $sortField = $sortField !== null ? $sortField : $publicationType->sortField;
