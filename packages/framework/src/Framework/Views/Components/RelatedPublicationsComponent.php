e94cf5b3246d8585e12af187ccd06b33a0333691
rgasch <rgasch@gmail.com>
2023-02-28 11:08:15 +0100

    Better sorting of results for relevance

--- packages/framework/src/Framework/Views/Components/RelatedPublicationsComponent.php
+++ packages/framework/src/Framework/Views/Components/RelatedPublicationsComponent.php
@@ -5,9 +5,11 @@ declare(strict_types=1);
 namespace Hyde\Framework\Views\Components;
 
 use Hyde\Hyde;
+use Hyde\Publications\Models\PublicationPage;
 use Hyde\Publications\PublicationService;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
+use Illuminate\Support\Collection;
 use Illuminate\View\Component;
 
 class RelatedPublicationsComponent extends Component
@@ -51,7 +53,7 @@ class RelatedPublicationsComponent extends Component
             $thisPageTags = array_merge($thisPageTags, $thisPage->matter->get($tagField->name, []));
         }
 
-        $allRelatedPages = [];
+        $allRelatedPages = collect();
         foreach ($publicationPages as $publicationPage) {
             // Get a list of all tags for $publicationPage
             $pubPageTags = [];
@@ -69,7 +71,13 @@ class RelatedPublicationsComponent extends Component
 
             // We have shared/matching tags, add this page and it's count to $allRelatedPages
             if ($count) {
-                $allRelatedPages[$publicationPage->identifier] = $count;
+                $allRelatedPages->add(
+                    collect( [
+                        'count'      => $count,
+                        'identifier' => $publicationPage->identifier,
+                        'page'       => $publicationPage,
+                    ] )
+                );
             }
         }
 
@@ -78,15 +86,17 @@ class RelatedPublicationsComponent extends Component
             return [];
         }
 
-        // Sort by count, preserve keys
-        arsort($allRelatedPages, SORT_NUMERIC);
-
-        // Now get the top $max pages (or less if there aren't that many)
+        // Sort everything by count and then by most recent date -> seems the most logical & relevant
+        $allRelatedPagesGrouped = $allRelatedPages->groupBy('count')->sortKeysDesc(SORT_NUMERIC);
         $relatedPages = [];
-        $aks = array_keys($allRelatedPages);
-        $lim = min(count($allRelatedPages), $max);
-        for ($i=0; $i<$lim; $i++) {
-            $relatedPages[] = $publicationPages->get($aks[$i]);
+        foreach ($allRelatedPagesGrouped as $k=>$v) {
+            $sorted = $v->sortByDesc('page.matter.__createdAt');
+            foreach ($sorted as $kk=>$vv) {
+                $relatedPages[$vv['identifier']] = $vv['page'];
+                if (count($relatedPages) >= $max) {
+                    break 2;
+                }
+            }
         }
 
         return $relatedPages;
