6bbc9302dba4577db63f3ba6d7be216dcd0e6f7f
Caen De Silva <caen@desilva.se>
2023-02-28 11:41:41 +0100

    Introduce local variable

--- monorepo/scripts/ping-openanalytics-testrunner.php
+++ monorepo/scripts/ping-openanalytics-testrunner.php
@@ -58,6 +58,7 @@ curl_close($curl);
 var_dump($resp);
 
 // if curl has 401 it's probably as it was run in a fork so that's fine, but if it's another error 400 or above we should fail the build
-if (curl_getinfo($curl, CURLINFO_HTTP_CODE) >= 400 && curl_getinfo($curl, CURLINFO_HTTP_CODE) !== 401) {
+$code = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+if ($code >= 400 && $code !== 401) {
     exit($resp);
 }
