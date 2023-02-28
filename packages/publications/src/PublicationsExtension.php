73efaeb037bd72fd17dc8d04309e0d85e7fa674e
Caen De Silva <caen@desilva.se>
2023-02-28 13:06:52 +0100

    Introduce local variable

--- packages/publications/src/PublicationsExtension.php
+++ packages/publications/src/PublicationsExtension.php
@@ -76,7 +76,8 @@ class PublicationsExtension extends HydeExtension
 
     protected static function discoverPublicationPagesForType(PublicationType $type, PageCollection $instance): void
     {
-        static::parsePublicationsForType($type)->each(function (PublicationPage $publication) use ($instance): void {
+        $collection = static::parsePublicationsForType($type);
+        $collection->each(function (PublicationPage $publication) use ($instance): void {
             $instance->addPage($publication);
         });
     }
