5322d13477319609488224760193ee37ba3021b5
Caen De Silva <caen@desilva.se>
2023-03-01 20:02:00 +0100

    Create CleanSiteDirectoryTest.php

---
+++ packages/framework/tests/Unit/CleanSiteDirectoryTest.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hyde\Framework\Testing\Unit;
+
+use Hyde\Framework\Features\BuildTasks\PreBuildTasks\CleanSiteDirectory;
+use Hyde\Testing\UnitTestCase;
+
+/**
+ * @covers \Hyde\Framework\Features\BuildTasks\PreBuildTasks\CleanSiteDirectory
+ */
+class CleanSiteDirectoryTest extends UnitTestCase
+{
+    //
+}
