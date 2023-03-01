bd0cd2ba18d893b181b6fd443430b7f049276c42
Caen De Silva <caen@desilva.se>
2023-03-01 10:49:26 +0100

    Revert "Deprecate static attribute backup in test"
    This reverts commit b37744bbdc8d648a8d5ef3c86d3f4f1ea53d61bf.

--- packages/framework/tests/Feature/Services/BuildTaskServiceTest.php
+++ packages/framework/tests/Feature/Services/BuildTaskServiceTest.php
@@ -21,7 +21,7 @@ use Illuminate\Support\Facades\File;
  *
  * @see \Hyde\Framework\Testing\Unit\BuildTaskServiceUnitTest
  *
- * @backupStaticAttributes enabled (@deprecated)
+ * @backupStaticAttributes enabled
  */
 class BuildTaskServiceTest extends TestCase
 {
