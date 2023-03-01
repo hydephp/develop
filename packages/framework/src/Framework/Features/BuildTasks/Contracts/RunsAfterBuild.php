f13ee17672eae5474e7e6074fc58d3b80d162b35
Caen De Silva <caen@desilva.se>
2023-03-01 17:10:33 +0100

    Revert "Revert "Create RunsAfterBuild.php""
    This reverts commit 7a569c9747407bc0cfd4f9c9cd1814ab2c79bc26.

---
+++ packages/framework/src/Framework/Features/BuildTasks/Contracts/RunsAfterBuild.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hyde\Framework\Features\BuildTasks\Contracts;
+
+/**
+ * Indicates that the build task should be run after the static site build.
+ */
+interface RunsAfterBuild
+{
+    //
+}
