58df303b14df7af7ba307546bd5326a829ea9ab6
Caen De Silva <caen@desilva.se>
2023-03-01 14:15:24 +0100

    Join comma-separated values into a single line

--- packages/framework/src/Framework/Services/BuildTaskService.php
+++ packages/framework/src/Framework/Services/BuildTaskService.php
@@ -80,11 +80,7 @@ class BuildTaskService
     protected static function findTasksInAppDirectory(): array
     {
         return Filesystem::smartGlob('app/Actions/*BuildTask.php')->map(function (string $file): string {
-            return str_replace(
-                ['app', '.php', '/'],
-                ['App', '', '\\'],
-                $file
-            );
+            return str_replace(['app', '.php', '/'], ['App', '', '\\'], $file);
         })->toArray();
     }
 
