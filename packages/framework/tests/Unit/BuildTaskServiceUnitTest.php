04a7156515ebc8c5f4a7aa8dcb882ea78a817b6f
Caen De Silva <caen@desilva.se>
2023-03-01 11:01:07 +0100

    Add additional unit tests

--- packages/framework/tests/Unit/BuildTaskServiceUnitTest.php
+++ packages/framework/tests/Unit/BuildTaskServiceUnitTest.php
@@ -6,6 +6,8 @@ namespace Hyde\Framework\Testing\Unit;
 
 use Hyde\Framework\Services\BuildTaskService;
 use Hyde\Testing\UnitTestCase;
+use Illuminate\Console\OutputStyle;
+use Mockery;
 
 /**
  * @covers \Hyde\Framework\Services\BuildTaskService
@@ -23,6 +25,27 @@ class BuildTaskServiceUnitTest extends UnitTestCase
     public function testConstruct()
     {
         $this->assertInstanceOf(BuildTaskService::class, new BuildTaskService());
+    }
+
+    public function testGetPostBuildTasks()
+    {
+        $this->assertIsArray($this->service()->getPostBuildTasks());
+        $this->assertSame([], $this->service()->getPostBuildTasks());
+    }
+
+    public function testSetOutputWithNull()
+    {
+        $this->assertInstanceOf(BuildTaskService::class, $this->service()->setOutput(null));
+    }
+
+    public function testSetOutputWithOutputStyle()
+    {
+        $this->assertInstanceOf(BuildTaskService::class, $this->service()->setOutput(Mockery::mock(OutputStyle::class)));
+    }
+
+    public function testSetOutputReturnsStatic()
+    {
+        $this->assertSame($service = $this->service(), $service->setOutput(null));
     }
 
     protected function service(): BuildTaskService
