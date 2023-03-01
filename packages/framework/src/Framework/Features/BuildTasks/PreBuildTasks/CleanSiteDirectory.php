a5866641e8805dd9d44f569142e76506010fd382
Caen De Silva <caen@desilva.se>
2023-03-01 20:00:50 +0100

    Create CleanSiteDirectory.php

---
+++ packages/framework/src/Framework/Features/BuildTasks/PreBuildTasks/CleanSiteDirectory.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hyde\Framework\Features\BuildTasks\PreBuildTasks;
+
+use Hyde\Framework\Features\BuildTasks\BuildTask;
+use Hyde\Framework\Features\BuildTasks\Contracts\RunsBeforeBuild;
+
+class CleanSiteDirectory extends BuildTask implements RunsBeforeBuild
+{
+    //
+}
