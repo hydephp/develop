c8243beea717c911889eb88631b1592d6320f2e4
Caen De Silva <caen@desilva.se>
2023-03-01 12:47:26 +0100

    Clean up test

--- packages/framework/tests/Feature/Services/BuildTaskServiceTest.php
+++ packages/framework/tests/Feature/Services/BuildTaskServiceTest.php
@@ -57,9 +57,10 @@ class BuildTaskServiceTest extends TestCase
     public function test_get_post_build_tasks_returns_array_merged_with_config()
     {
         config(['hyde.post_build_tasks' => ['bar']]);
-        app(BuildTaskService::class)->addPostBuildTask('foo');
 
         $service = $this->makeService();
+        $service->addPostBuildTask('foo');
+
         $this->assertEquals(['foo' => 'foo', 'bar' => 'bar'], $service->getPostBuildTasks());
     }
 
