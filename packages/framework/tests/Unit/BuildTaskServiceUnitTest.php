7daf55e1cdb27b4cc98af4aa754ae87b3d544de6
Caen De Silva <caen@desilva.se>
2023-03-01 10:51:40 +0100

    Extend unit test case

--- packages/framework/tests/Unit/BuildTaskServiceUnitTest.php
+++ packages/framework/tests/Unit/BuildTaskServiceUnitTest.php
@@ -5,14 +5,14 @@ declare(strict_types=1);
 namespace Hyde\Framework\Testing\Unit;
 
 use Hyde\Framework\Services\BuildTaskService;
-use Hyde\Testing\TestCase;
+use Hyde\Testing\UnitTestCase;
 
 /**
  * @covers \Hyde\Framework\Services\BuildTaskService
  *
  * @see \Hyde\Framework\Testing\Feature\Services\BuildTaskServiceTest
  */
-class BuildTaskServiceUnitTest extends TestCase
+class BuildTaskServiceUnitTest extends UnitTestCase
 {
     public function testConstruct()
     {
