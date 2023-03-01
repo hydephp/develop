a967851094acbc9fb242cc0c3d593ffb4d0315c9
Caen De Silva <caen@desilva.se>
2023-03-01 10:45:39 +0100

    Create BuildTaskServiceUnitTest.php

---
+++ packages/framework/tests/Unit/BuildTaskServiceUnitTest.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hyde\Framework\Testing\Unit;
+
+use Hyde\Framework\Services\BuildTaskService;
+use Hyde\Testing\TestCase;
+
+/**
+ * @covers \Hyde\Framework\Services\BuildTaskService
+ */
+class BuildTaskServiceUnitTest extends TestCase
+{
+    //
+}
