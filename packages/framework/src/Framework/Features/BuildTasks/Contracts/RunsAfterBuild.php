60b3dc7d94251c448c708966b3c8f0c5cabe0d84
Caen De Silva <caen@desilva.se>
2023-03-02 16:02:25 +0100

    Remove interface RunsAfterBuild

--- packages/framework/src/Framework/Features/BuildTasks/Contracts/RunsAfterBuild.php
+++ packages/framework/src/Framework/Features/BuildTasks/Contracts/RunsAfterBuild.php
@@ -4,12 +4,3 @@ declare(strict_types=1);
 
 namespace Hyde\Framework\Features\BuildTasks\Contracts;
 
-/**
- * Indicates that the build task should be run after the static site build.
- *
- * @deprecated Extend the PostBuildTask class instead.
- */
-interface RunsAfterBuild
-{
-    //
-}
