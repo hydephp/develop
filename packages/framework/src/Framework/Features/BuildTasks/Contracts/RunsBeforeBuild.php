fbebcc898677922d19c6bafd5bfdf774076065cb
Caen De Silva <caen@desilva.se>
2023-03-02 15:47:50 +0100

    Deprecate RunsBeforeBuild interface

--- packages/framework/src/Framework/Features/BuildTasks/Contracts/RunsBeforeBuild.php
+++ packages/framework/src/Framework/Features/BuildTasks/Contracts/RunsBeforeBuild.php
@@ -6,6 +6,8 @@ namespace Hyde\Framework\Features\BuildTasks\Contracts;
 
 /**
  * Indicates that the build task should be run before the static site build.
+ *
+ * @deprecated Extend the PreBuildTask class instead.
  */
 interface RunsBeforeBuild
 {
