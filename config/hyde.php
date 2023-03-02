d5f3d6185b00d12d3689f6152c44843f0d0cf7af
Caen De Silva <caen@desilva.se>
2023-03-02 22:25:08 +0100

    Set default config value to the constant

--- config/hyde.php
+++ config/hyde.php
@@ -433,7 +433,7 @@ return [
 
     // Here you can specify HydeFront version and URL for when loading app.css from the CDN.
     // Only change these if you know what you're doing as some versions may incompatible with your Hyde version.
-    // 'hydefront_version' => '',
+    'hydefront_version' => \Hyde\Framework\Services\AssetService::HYDEFRONT_VERSION,
     // 'hydefront_cdn_url' => ''
 
 ];
