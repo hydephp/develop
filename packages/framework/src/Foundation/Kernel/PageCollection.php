d969f121b71a4897dad2139bbddea2ebc99bff3a
Caen De Silva <caen@desilva.se>
2023-03-03 22:06:32 +0100

    Link to #1194

--- packages/framework/src/Foundation/Kernel/PageCollection.php
+++ packages/framework/src/Foundation/Kernel/PageCollection.php
@@ -17,7 +17,7 @@ use Illuminate\Support\Collection;
  * This class is stored as a singleton in the HydeKernel.
  * You would commonly access it via one of the facades:
  *
- * @todo We could improve this by catching exceptions and rethrowing them using a
+ * @todo #1194 We could improve this by catching exceptions and rethrowing them using a
  *       DiscoveryException to make it clear that the problem is with the discovery process.
  *
  * @see \Hyde\Foundation\Facades\PageCollection
