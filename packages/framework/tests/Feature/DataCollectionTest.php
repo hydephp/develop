7e98b414d315c8496eb0d73cdb5a24bf937ac294
Caen De Silva <caen@desilva.se>
2023-03-03 10:52:05 +0100

    Extract unit test

--- packages/framework/tests/Feature/DataCollectionTest.php
+++ packages/framework/tests/Feature/DataCollectionTest.php
@@ -15,6 +15,7 @@ use Illuminate\Support\Facades\File;
 
 /**
  * @covers \Hyde\Framework\Features\DataCollections\DataCollection
+ * @see \Hyde\Framework\Testing\Unit\DataCollectionUnitTest
  */
 class DataCollectionTest extends TestCase
 {
@@ -25,38 +26,6 @@ class DataCollectionTest extends TestCase
         if (! is_dir(Hyde::path('resources/collections'))) {
             mkdir(Hyde::path('resources/collections'));
         }
-    }
-
-    public function test_constructor_creates_new_data_collection_instance()
-    {
-        $class = new DataCollection('foo');
-        $this->assertInstanceOf(DataCollection::class, $class);
-        $this->assertInstanceOf(Collection::class, $class);
-    }
-
-    public function test_constructor_sets_key()
-    {
-        $class = new DataCollection('foo');
-        $this->assertEquals('foo', $class->key);
-    }
-
-    public function test_key_is_required()
-    {
-        $this->expectException(ArgumentCountError::class);
-        new DataCollection();
-    }
-
-    public function test_get_collection_method_returns_the_collection_instance()
-    {
-        $class = new DataCollection('foo');
-        $this->assertSame($class, $class->getCollection());
-    }
-
-    public function test_get_markdown_files_method_returns_empty_array_if_the_specified_directory_does_not_exist()
-    {
-        $class = new DataCollection('foo');
-        $this->assertIsArray($class->getMarkdownFiles());
-        $this->assertEmpty($class->getMarkdownFiles());
     }
 
     public function test_get_markdown_files_method_returns_empty_array_if_no_files_are_found_in_specified_directory()
@@ -114,11 +83,6 @@ class DataCollectionTest extends TestCase
             Hyde::path('resources/collections/foo/_foo.md'),
         ], (new DataCollection('foo'))->getMarkdownFiles());
         File::deleteDirectory(Hyde::path('resources/collections/foo'));
-    }
-
-    public function test_static_markdown_helper_returns_new_data_collection_instance()
-    {
-        $this->assertInstanceOf(DataCollection::class, DataCollection::markdown('foo'));
     }
 
     public function test_static_markdown_helper_discovers_and_parses_markdown_files_in_the_specified_directory()
---
+++ packages/framework/tests/Unit/DataCollectionUnitTest.php
@@ -0,0 +1,59 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hyde\Framework\Testing\Unit;
+
+use ArgumentCountError;
+use Hyde\Framework\Features\DataCollections\DataCollection;
+use Hyde\Testing\UnitTestCase;
+use Illuminate\Support\Collection;
+
+/**
+ * @covers \Hyde\Framework\Features\DataCollections\DataCollection
+ * @see \Hyde\Framework\Testing\Feature\DataCollectionTest
+ */
+class DataCollectionUnitTest extends UnitTestCase
+{
+    public static function setUpBeforeClass(): void
+    {
+        self::needsKernel();
+    }
+
+    public function test_constructor_creates_new_data_collection_instance()
+    {
+        $class = new DataCollection('foo');
+        $this->assertInstanceOf(DataCollection::class, $class);
+        $this->assertInstanceOf(Collection::class, $class);
+    }
+
+    public function test_constructor_sets_key()
+    {
+        $class = new DataCollection('foo');
+        $this->assertEquals('foo', $class->key);
+    }
+
+    public function test_key_is_required()
+    {
+        $this->expectException(ArgumentCountError::class);
+        new DataCollection();
+    }
+
+    public function test_get_collection_method_returns_the_collection_instance()
+    {
+        $class = new DataCollection('foo');
+        $this->assertSame($class, $class->getCollection());
+    }
+
+    public function test_get_markdown_files_method_returns_empty_array_if_the_specified_directory_does_not_exist()
+    {
+        $class = new DataCollection('foo');
+        $this->assertIsArray($class->getMarkdownFiles());
+        $this->assertEmpty($class->getMarkdownFiles());
+    }
+
+    public function test_static_markdown_helper_returns_new_data_collection_instance()
+    {
+        $this->assertInstanceOf(DataCollection::class, DataCollection::markdown('foo'));
+    }
+}
