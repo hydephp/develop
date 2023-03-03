478b8e10643d4a51d1076f77cf46a98792a74854
Caen De Silva <caen@desilva.se>
2023-03-03 11:08:49 +0100

    Refactor test to use temporary file helpers

--- packages/framework/tests/Feature/DataCollectionTest.php
+++ packages/framework/tests/Feature/DataCollectionTest.php
@@ -4,12 +4,10 @@ declare(strict_types=1);
 
 namespace Hyde\Framework\Testing\Feature;
 
-use Hyde\Facades\Filesystem;
 use Hyde\Framework\Features\DataCollections\DataCollection;
 use Hyde\Hyde;
 use Hyde\Markdown\Models\MarkdownDocument;
 use Hyde\Testing\TestCase;
-use Illuminate\Support\Facades\File;
 
 /**
  * @covers \Hyde\Framework\Features\DataCollections\DataCollection
@@ -27,7 +25,7 @@ class DataCollectionTest extends TestCase
 
     public function test_get_markdown_files_method_returns_empty_array_if_no_files_are_found_in_specified_directory()
     {
-        mkdir(Hyde::path('resources/collections/foo'));
+        $this->directory('resources/collections/foo');
         $class = new DataCollection('foo');
         $this->assertIsArray($class->getMarkdownFiles());
         $this->assertEmpty($class->getMarkdownFiles());
@@ -36,83 +34,75 @@ class DataCollectionTest extends TestCase
 
     public function test_get_markdown_files_method_returns_an_array_of_markdown_files_in_the_specified_directory()
     {
-        mkdir(Hyde::path('resources/collections/foo'));
-        Filesystem::touch('resources/collections/foo/foo.md');
-        Filesystem::touch('resources/collections/foo/bar.md');
+        $this->directory('resources/collections/foo');
+        $this->file('resources/collections/foo/foo.md');
+        $this->file('resources/collections/foo/bar.md');
 
         $this->assertEquals([
             'resources/collections/foo/bar.md',
             'resources/collections/foo/foo.md',
         ], (new DataCollection('foo'))->getMarkdownFiles());
 
-        File::deleteDirectory(Hyde::path('resources/collections/foo'));
     }
 
     public function test_get_markdown_files_method_does_not_include_files_in_subdirectories()
     {
-        mkdir(Hyde::path('resources/collections/foo'));
-        mkdir(Hyde::path('resources/collections/foo/bar'));
-        Filesystem::touch('resources/collections/foo/foo.md');
-        Filesystem::touch('resources/collections/foo/bar/bar.md');
+        $this->directory('resources/collections/foo');
+        $this->directory('resources/collections/foo/bar');
+        $this->file('resources/collections/foo/foo.md');
+        $this->file('resources/collections/foo/bar/bar.md');
         $this->assertEquals([
             'resources/collections/foo/foo.md',
         ], (new DataCollection('foo'))->getMarkdownFiles());
-        File::deleteDirectory(Hyde::path('resources/collections/foo'));
     }
 
     public function test_get_markdown_files_method_does_not_include_files_with_extensions_other_than_md()
     {
-        mkdir(Hyde::path('resources/collections/foo'));
-        Filesystem::touch('resources/collections/foo/foo.md');
-        Filesystem::touch('resources/collections/foo/bar.txt');
+        $this->directory('resources/collections/foo');
+        $this->file('resources/collections/foo/foo.md');
+        $this->file('resources/collections/foo/bar.txt');
         $this->assertEquals([
             'resources/collections/foo/foo.md',
         ], (new DataCollection('foo'))->getMarkdownFiles());
-        File::deleteDirectory(Hyde::path('resources/collections/foo'));
     }
 
     public function test_get_markdown_files_method_does_not_remove_files_starting_with_an_underscore()
     {
-        mkdir(Hyde::path('resources/collections/foo'));
-        Filesystem::touch('resources/collections/foo/_foo.md');
+        $this->directory('resources/collections/foo');
+        $this->file('resources/collections/foo/_foo.md');
 
         $this->assertEquals([
             'resources/collections/foo/_foo.md',
         ], (new DataCollection('foo'))->getMarkdownFiles());
-        File::deleteDirectory(Hyde::path('resources/collections/foo'));
     }
 
     public function test_static_markdown_helper_discovers_and_parses_markdown_files_in_the_specified_directory()
     {
-        mkdir(Hyde::path('resources/collections/foo'));
-        Filesystem::touch('resources/collections/foo/foo.md');
-        Filesystem::touch('resources/collections/foo/bar.md');
+        $this->directory('resources/collections/foo');
+        $this->file('resources/collections/foo/foo.md');
+        $this->file('resources/collections/foo/bar.md');
 
         $collection = DataCollection::markdown('foo');
 
         $this->assertContainsOnlyInstancesOf(MarkdownDocument::class, $collection);
-
-        File::deleteDirectory(Hyde::path('resources/collections/foo'));
     }
 
     public function test_static_markdown_helper_doest_not_ignore_files_starting_with_an_underscore()
     {
-        mkdir(Hyde::path('resources/collections/foo'));
-        Filesystem::touch('resources/collections/foo/foo.md');
-        Filesystem::touch('resources/collections/foo/_bar.md');
+        $this->directory('resources/collections/foo');
+        $this->file('resources/collections/foo/foo.md');
+        $this->file('resources/collections/foo/_bar.md');
         $this->assertCount(2, DataCollection::markdown('foo'));
-        File::deleteDirectory(Hyde::path('resources/collections/foo'));
     }
 
     public function test_source_directory_can_be_changed()
     {
         DataCollection::$sourceDirectory = 'foo';
-        mkdir(Hyde::path('foo/bar'), recursive: true);
-        Filesystem::touch('foo/bar/foo.md');
+        $this->directory('foo/bar');
+        $this->file('foo/bar/foo.md');
         $this->assertEquals([
             'foo/bar/foo.md',
         ], (new DataCollection('bar'))->getMarkdownFiles());
-        File::deleteDirectory(Hyde::path('foo'));
         DataCollection::$sourceDirectory = 'resources/collections';
     }
 }
