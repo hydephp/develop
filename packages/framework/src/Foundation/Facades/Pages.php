c9573bbfbee9c7c9c8f3100c580ae0e50c1e8721
Caen De Silva <caen@desilva.se>
2023-03-04 09:23:20 +0100

    Copy public methods to facade as static stubs

--- packages/framework/src/Foundation/Facades/Pages.php
+++ packages/framework/src/Foundation/Facades/Pages.php
@@ -6,6 +6,7 @@ namespace Hyde\Foundation\Facades;
 
 use Hyde\Foundation\HydeKernel;
 use Hyde\Foundation\Kernel\PageCollection;
+use Hyde\Pages\Concerns\HydePage;
 use Illuminate\Support\Facades\Facade;
 
 /**
@@ -16,5 +17,20 @@ class Pages extends Facade
     public static function getFacadeRoot(): PageCollection
     {
         return HydeKernel::getInstance()->pages();
+    }
+
+    public function getPage(string $sourcePath): HydePage
+    {
+        //
+    }
+
+    public function getPages(?string $pageClass = null): PageCollection
+    {
+        //
+    }
+
+    public function addPage(HydePage $page): PageCollection
+    {
+        //
     }
 }
