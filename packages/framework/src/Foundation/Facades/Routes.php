26814f870de0fa1df57bc1b8b821228256d19e73
Caen De Silva <caen@desilva.se>
2023-03-04 09:59:19 +0100

    Move method logic to the facade

--- packages/framework/src/Foundation/Facades/Routes.php
+++ packages/framework/src/Foundation/Facades/Routes.php
@@ -6,6 +6,7 @@ namespace Hyde\Foundation\Facades;
 
 use Hyde\Foundation\HydeKernel;
 use Hyde\Foundation\Kernel\RouteCollection;
+use Hyde\Framework\Exceptions\RouteNotFoundException;
 use Hyde\Support\Models\Route;
 use Illuminate\Support\Facades\Facade;
 
@@ -21,11 +22,13 @@ class Routes extends Facade
 
     public static function getRoute(string $routeKey): Route
     {
-        return static::getFacadeRoot()->getRoute($routeKey);
+        return static::getFacadeRoot()->items[$routeKey] ?? throw new RouteNotFoundException($routeKey . ' in route collection');
     }
 
     public static function getRoutes(?string $pageClass = null): RouteCollection
     {
-        return static::getFacadeRoot()->getRoutes($pageClass);
+        return ! $pageClass ? static::getFacadeRoot() : static::getFacadeRoot()->filter(function (Route $route) use ($pageClass): bool {
+            return $route->getPage() instanceof $pageClass;
+        });
     }
 }
