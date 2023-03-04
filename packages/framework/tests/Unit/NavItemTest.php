bd55aead230cd0f9a61dbc88e3d8fcef1d4788e3
Caen De Silva <caen@desilva.se>
2023-03-04 22:00:28 +0100

    Revert "Test can also cover ExternalRoute for now"
    This reverts commit 7b195755267acc00fd677272e345e6746be8839b.

--- packages/framework/tests/Unit/NavItemTest.php
+++ packages/framework/tests/Unit/NavItemTest.php
@@ -15,7 +15,6 @@ use Hyde\Testing\UnitTestCase;
  * For the full feature test, see the NavigationMenuTest class.
  *
  * @covers \Hyde\Framework\Features\Navigation\NavItem
- * @covers \Hyde\Framework\Features\Navigation\ExternalRoute
  */
 class NavItemTest extends UnitTestCase
 {
