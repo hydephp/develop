69bdf1572d90aab278f2f39f00c2a7d163af312c
Caen De Silva <caen@desilva.se>
2023-03-04 21:11:14 +0100

    Deprecate DiscoveryService.php as it is no longer used

--- packages/framework/src/Framework/Services/DiscoveryService.php
+++ packages/framework/src/Framework/Services/DiscoveryService.php
@@ -5,11 +5,7 @@ declare(strict_types=1);
 namespace Hyde\Framework\Services;
 
 /**
- * General Discovery Helpers for HydePHP Auto-Discovery.
- *
- * Offloads FoundationCollection logic and provides helpers for common code.
- *
- * @see \Hyde\Framework\Testing\Feature\DiscoveryServiceTest
+ * @deprecated This class is no longer used.
  */
 class DiscoveryService
 {
