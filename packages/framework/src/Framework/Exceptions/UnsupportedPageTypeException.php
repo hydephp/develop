f6bac29978df612ea822a668d1985e4de0646cfb
Caen De Silva <caen@desilva.se>
2023-03-04 20:47:17 +0100

    Convert string interpolation to a 'sprintf()' call

--- packages/framework/src/Framework/Exceptions/UnsupportedPageTypeException.php
+++ packages/framework/src/Framework/Exceptions/UnsupportedPageTypeException.php
@@ -16,6 +16,6 @@ class UnsupportedPageTypeException extends Exception
 
     public function __construct(?string $page = null)
     {
-        parent::__construct($page ? "The page type [$page] is not supported." : $this->message);
+        parent::__construct($page ? sprintf("The page type [%s] is not supported.", $page) : $this->message);
     }
 }
