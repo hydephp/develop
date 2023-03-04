a1eb92cb29fa9ba1ac348d378b4a354e56774208
Caen De Silva <caen@desilva.se>
2023-03-04 13:13:43 +0100

    Sort annotations

--- packages/framework/tests/Unit/CustomExceptionsTest.php
+++ packages/framework/tests/Unit/CustomExceptionsTest.php
@@ -7,10 +7,10 @@ namespace Hyde\Framework\Testing\Unit;
 use Hyde\Testing\UnitTestCase;
 
 /**
- * @covers \Hyde\Framework\Exceptions\BaseUrlNotSetException
  * @covers \Hyde\Framework\Exceptions\FileConflictException
  * @covers \Hyde\Framework\Exceptions\FileNotFoundException
  * @covers \Hyde\Framework\Exceptions\RouteNotFoundException
+ * @covers \Hyde\Framework\Exceptions\BaseUrlNotSetException
  * @covers \Hyde\Framework\Exceptions\UnsupportedPageTypeException
  */
 class CustomExceptionsTest extends UnitTestCase
