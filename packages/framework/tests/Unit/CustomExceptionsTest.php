c06d870b569ff5ba463f6527199b8ae4b1bd4123
Caen De Silva <caen@desilva.se>
2023-03-04 13:13:29 +0100

    Test covers all custom exceptions
    Except BuildWarning as it's experimental and only partly an exception semantically speaking

--- packages/framework/tests/Unit/CustomExceptionsTest.php
+++ packages/framework/tests/Unit/CustomExceptionsTest.php
@@ -6,6 +6,13 @@ namespace Hyde\Framework\Testing\Unit;
 
 use Hyde\Testing\UnitTestCase;
 
+/**
+ * @covers \Hyde\Framework\Exceptions\BaseUrlNotSetException
+ * @covers \Hyde\Framework\Exceptions\FileConflictException
+ * @covers \Hyde\Framework\Exceptions\FileNotFoundException
+ * @covers \Hyde\Framework\Exceptions\RouteNotFoundException
+ * @covers \Hyde\Framework\Exceptions\UnsupportedPageTypeException
+ */
 class CustomExceptionsTest extends UnitTestCase
 {
     //
