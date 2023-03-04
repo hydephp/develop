eecdc7f1f5b4b6ce1b99b03403647df648ffdf6e
Caen De Silva <caen@desilva.se>
2023-03-04 20:05:22 +0100

    Merge method route key description into class documentation

--- packages/framework/src/Facades/Route.php
+++ packages/framework/src/Facades/Route.php
@@ -11,13 +11,13 @@ use Hyde\Support\Models\RouteKey;
 
 /**
  * Provides an easy way to access the Hyde pseudo-router.
+ *
+ * To access a route you need the route key which is the equivalent of the URL path of the compiled page.
  */
 class Route
 {
     /**
      * Get a route from the route index for the specified route key.
-     *
-     * @param  string  $routeKey  Example: posts/foo
      */
     public static function get(string $routeKey): ?\Hyde\Support\Models\Route
     {
