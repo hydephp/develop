7a3b0650db318defb3d122d40a1fb245e880b5c5
Caen De Silva <caen@desilva.se>
2023-03-05 19:11:43 +0100

    Wrap semicolon to new line

--- packages/framework/tests/Feature/SidebarViewTest.php
+++ packages/framework/tests/Feature/SidebarViewTest.php
@@ -29,7 +29,8 @@ class SidebarViewTest extends TestCase
         $this->renderComponent(view('hyde::components.docs.sidebar'))
             ->assertSeeHtml('<nav id="sidebar-navigation"')
             ->assertSeeHtml('<a href="index.html">Back to home page</a>')
-            ->assertSeeHtml('<ul id="sidebar-navigation" role="list" class="pl-2">');
+            ->assertSeeHtml('<ul id="sidebar-navigation" role="list" class="pl-2">')
+        ;
     }
 
     protected function renderComponent(View $view): self
