ab4c910641bca82c73059464cd96b8a856662981
Caen De Silva <caen@desilva.se>
2023-03-05 21:53:55 +0100

    Wrap directive end statement

--- packages/framework/resources/views/components/docs/sidebar-item.blade.php
+++ packages/framework/resources/views/components/docs/sidebar-item.blade.php
@@ -2,11 +2,13 @@
 @props(['grouped' => false])
 <li @class(['sidebar-navigation-item -ml-4 pl-4', $grouped
             ? 'active -ml-8 pl-8 bg-black/5 dark:bg-black/10'
-            : 'active bg-black/5 dark:bg-black/10' => $item->isCurrent()]) role="listitem">
+            : 'active bg-black/5 dark:bg-black/10' => $item->isCurrent()
+            ]) role="listitem">
     @if($item->isCurrent())
         <a href="{{ $item->destination }}" aria-current="true" @class([$grouped
                 ? '-ml-8 pl-4 py-1 px-2 block text-indigo-600 dark:text-indigo-400 dark:font-medium border-l-[0.325rem] border-indigo-500 transition-colors duration-300 ease-in-out hover:bg-black/10'
-                : '-ml-4 p-2 block hover:bg-black/5 dark:hover:bg-black/10 text-indigo-600 dark:text-indigo-400 dark:font-medium border-l-[0.325rem] border-indigo-500 transition-colors duration-300 ease-in-out'])>
+                : '-ml-4 p-2 block hover:bg-black/5 dark:hover:bg-black/10 text-indigo-600 dark:text-indigo-400 dark:font-medium border-l-[0.325rem] border-indigo-500 transition-colors duration-300 ease-in-out'
+                ])>
             {{ $item->label }}
         </a>
 
@@ -17,7 +19,8 @@
     @else
         <a href="{{ $item->destination }}" @class([$grouped
                 ? '-ml-8 pl-4 py-1 px-2 block border-l-[0.325rem] border-transparent transition-colors duration-300 ease-in-out hover:bg-black/10'
-                : 'block -ml-4 p-2 border-l-[0.325rem] border-transparent hover:bg-black/5 dark:hover:bg-black/10'])>
+                : 'block -ml-4 p-2 border-l-[0.325rem] border-transparent hover:bg-black/5 dark:hover:bg-black/10'
+                ])>
             {{ $item->label }}
         </a>
     @endif
