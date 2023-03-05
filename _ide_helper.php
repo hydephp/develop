9965f0acbe4448f4a68113ddfdbddadbc7272728
Caen De Silva <caen@desilva.se>
2023-03-06 00:36:53 +0100

    Document global view variables

--- _ide_helper.php
+++ _ide_helper.php
@@ -2,11 +2,11 @@
 
 declare(strict_types=1);
 
-/** @var \Hyde\Pages\Concerns\HydePage $page */
+/** @var \Hyde\Pages\Concerns\HydePage $page The page being compiled/previewed */
 $page = \Hyde\Support\Facades\Render::getPage();
 
-/** @var \Hyde\Support\Models\Route $currentRoute */
+/** @var \Hyde\Support\Models\Route $currentRoute The route for the page being compiled/previewed */
 $currentRoute = \Hyde\Support\Facades\Render::getCurrentRoute();
 
-/** @var string $currentPage */
+/** @var string $currentPage The route key for the page being compiled/previewed */
 $currentPage = \Hyde\Support\Facades\Render::getCurrentPage();
