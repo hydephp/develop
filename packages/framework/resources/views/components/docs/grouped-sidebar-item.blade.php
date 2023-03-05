8d19143e621ad164b8b4ec2a75ac7c8f71d412b7
Caen De Silva <caen@desilva.se>
2023-03-05 21:38:55 +0100

    Revert "Move styles to array view data"
    This reverts commit 7eceba5abfdee66de9c282dbb5ffc6a367def248.

--- packages/framework/resources/views/components/docs/grouped-sidebar-item.blade.php
+++ packages/framework/resources/views/components/docs/grouped-sidebar-item.blade.php
@@ -1,4 +1,9 @@
 @php /** @var \Hyde\Framework\Features\Navigation\NavItem $item */ @endphp
+@php
+    $activeListClasses = 'active -ml-8 pl-8 bg-black/5 dark:bg-black/10';
+    $activeItemClasses = '-ml-8 pl-4 py-1 px-2 block text-indigo-600 dark:text-indigo-400 dark:font-medium border-l-[0.325rem] border-indigo-500 transition-colors duration-300 ease-in-out hover:bg-black/10';
+    $itemClasses = '-ml-8 pl-4 py-1 px-2 block border-l-[0.325rem] border-transparent transition-colors duration-300 ease-in-out hover:bg-black/10';
+@endphp
 <li @class(['sidebar-navigation-item -ml-4 pl-4', $activeListClasses => $item->isCurrent()]) role="listitem">
     @if($item->isCurrent())
         <a href="{{ $item->destination }}" aria-current="true" @class([$activeItemClasses])>
--- packages/framework/resources/views/components/docs/grouped-sidebar-navigation.blade.php
+++ packages/framework/resources/views/components/docs/grouped-sidebar-navigation.blade.php
@@ -7,11 +7,7 @@
             </header>
             <ul class="sidebar-navigation-group-list ml-4 px-2 mb-2" role="list">
                 @foreach ($sidebar->getItemsInGroup($group) as $item)
-                    @include('hyde::components.docs.grouped-sidebar-item', [
-                        'activeListClasses' => 'active -ml-8 pl-8 bg-black/5 dark:bg-black/10',
-                        'activeItemClasses' => '-ml-8 pl-4 py-1 px-2 block text-indigo-600 dark:text-indigo-400 dark:font-medium border-l-[0.325rem] border-indigo-500 transition-colors duration-300 ease-in-out hover:bg-black/10',
-                        'itemClasses' => '-ml-8 pl-4 py-1 px-2 block border-l-[0.325rem] border-transparent transition-colors duration-300 ease-in-out hover:bg-black/10',
-                    ])
+                    @include('hyde::components.docs.grouped-sidebar-item')
                 @endforeach
             </ul>
         </li>
