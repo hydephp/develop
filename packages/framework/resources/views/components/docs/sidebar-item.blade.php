7d29e7ba4b4b9218da4c595de977cda9f4733975
Caen De Silva <caen@desilva.se>
2023-03-05 21:40:21 +0100

    Add temporary variable name prefixes

--- packages/framework/resources/views/components/docs/sidebar-item.blade.php
+++ packages/framework/resources/views/components/docs/sidebar-item.blade.php
@@ -1,8 +1,8 @@
 @php /** @var \Hyde\Framework\Features\Navigation\NavItem $item */ @endphp
 @php
-    $activeListClasses = 'active bg-black/5 dark:bg-black/10';
-    $activeItemClasses = '-ml-4 p-2 block hover:bg-black/5 dark:hover:bg-black/10 text-indigo-600 dark:text-indigo-400 dark:font-medium border-l-[0.325rem] border-indigo-500 transition-colors duration-300 ease-in-out';
-    $itemClasses = 'block -ml-4 p-2 border-l-[0.325rem] border-transparent hover:bg-black/5 dark:hover:bg-black/10';
+    $standard_activeListClasses = 'active bg-black/5 dark:bg-black/10';
+    $standard_activeItemClasses = '-ml-4 p-2 block hover:bg-black/5 dark:hover:bg-black/10 text-indigo-600 dark:text-indigo-400 dark:font-medium border-l-[0.325rem] border-indigo-500 transition-colors duration-300 ease-in-out';
+    $standard_itemClasses = 'block -ml-4 p-2 border-l-[0.325rem] border-transparent hover:bg-black/5 dark:hover:bg-black/10';
 @endphp
 <li @class(['sidebar-navigation-item -ml-4 pl-4', $activeListClasses => $item->isCurrent()]) role="listitem">
     @if($item->isCurrent())
