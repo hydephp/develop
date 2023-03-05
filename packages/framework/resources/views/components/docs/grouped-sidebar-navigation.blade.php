201d4e0b318a306068547d62134d0edaf8b330a8
Caen De Silva <caen@desilva.se>
2023-03-05 22:49:54 +0100

    Move state variable into element

--- packages/framework/resources/views/components/docs/grouped-sidebar-navigation.blade.php
+++ packages/framework/resources/views/components/docs/grouped-sidebar-navigation.blade.php
@@ -1,6 +1,6 @@
 @php /** @var \Hyde\Framework\Features\Navigation\DocumentationSidebar $sidebar */ @endphp
-@php ($collapsible = config('docs.sidebar.collapsible', true))
 <ul id="sidebar-navigation-items" role="list">
+    @php ($collapsible = config('docs.sidebar.collapsible', true))
     @foreach ($sidebar->getGroups() as $group)
         <li class="sidebar-navigation-group" role="listitem" @if($collapsible) x-data="{ groupOpen: {{ $sidebar->isGroupActive($group) ? 'true' : 'false' }} }" @endif>
             <header class="sidebar-navigation-group-header p-2 px-4 -ml-2 flex justify-between items-center @if($collapsible) group hover:bg-black/10 @endif" @if($collapsible) @click="groupOpen = ! groupOpen" @endif>
