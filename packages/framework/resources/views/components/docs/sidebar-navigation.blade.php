6395965d981ccc55624404804eef2f6f547b5b51
Caen De Silva <caen@desilva.se>
2023-03-05 19:15:15 +0100

    Bugfix: Duplicate sidebar element ID
    Suffixes item to match the other two navigation views

--- packages/framework/resources/views/components/docs/sidebar-navigation.blade.php
+++ packages/framework/resources/views/components/docs/sidebar-navigation.blade.php
@@ -1,5 +1,5 @@
 @php /** @var \Hyde\Framework\Features\Navigation\DocumentationSidebar $sidebar */ @endphp
-<ul id="sidebar-navigation" role="list" class="pl-2">
+<ul id="sidebar-navigation-items" role="list" class="pl-2">
 	@foreach ($sidebar->items as $item)
 	<li @class(['sidebar-navigation-item -ml-4 pl-4' , 'active bg-black/5 dark:bg-black/10'=> $item->isCurrent()])>
 		@if($item->isCurrent())
