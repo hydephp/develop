5ec2365f484dd35eaf3c5e1d94001134965c7596
Caen De Silva <caen@desilva.se>
2023-03-05 22:49:27 +0100

    Add grouped props

--- packages/framework/resources/views/components/docs/sidebar-navigation.blade.php
+++ packages/framework/resources/views/components/docs/sidebar-navigation.blade.php
@@ -1,4 +1,5 @@
 @php /** @var \Hyde\Framework\Features\Navigation\DocumentationSidebar $sidebar */ @endphp
+@props(['grouped' => false])
 <ul id="sidebar-navigation-items" role="list" class="pl-2">
     @foreach ($sidebar->items as $item)
         @include('hyde::components.docs.sidebar-item')
