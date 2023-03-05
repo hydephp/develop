b8b32bea9aacebba682e9048555825feca3af112
Caen De Silva <caen@desilva.se>
2023-03-05 20:45:05 +0100

    Format HTML

--- packages/framework/resources/views/components/docs/grouped-sidebar-item.blade.php
+++ packages/framework/resources/views/components/docs/grouped-sidebar-item.blade.php
@@ -1,12 +1,16 @@
 @php /** @var \Hyde\Framework\Features\Navigation\NavItem $item */ @endphp
 <li @class(['sidebar-navigation-item -ml-4 pl-4', 'active -ml-8 pl-8 bg-black/5 dark:bg-black/10' => $item->isCurrent()]) role="listitem">
     @if($item->isCurrent())
-        <a href="{{ $item->destination }}" aria-current="true"class="-ml-8 pl-4 py-1 px-2 block text-indigo-600 dark:text-indigo-400 dark:font-medium border-l-[0.325rem] border-indigo-500 transition-colors duration-300 ease-in-out hover:bg-black/10">{{ $item->label }}</a>
+        <a href="{{ $item->destination }}" aria-current="true"class="-ml-8 pl-4 py-1 px-2 block text-indigo-600 dark:text-indigo-400 dark:font-medium border-l-[0.325rem] border-indigo-500 transition-colors duration-300 ease-in-out hover:bg-black/10">
+            {{ $item->label }}
+        </a>
         @if(config('docs.table_of_contents.enabled', true))
             <span class="sr-only">Table of contents</span>
             {!! ($page->getTableOfContents()) !!}
         @endif
     @else
-        <a href="{{ $item->destination }}" class="-ml-8 pl-4 py-1 px-2 block border-l-[0.325rem] border-transparent transition-colors duration-300 ease-in-out hover:bg-black/10">{{ $item->label }}</a>
+        <a href="{{ $item->destination }}" class="-ml-8 pl-4 py-1 px-2 block border-l-[0.325rem] border-transparent transition-colors duration-300 ease-in-out hover:bg-black/10">
+            {{ $item->label }}
+        </a>
     @endif
 </li>
