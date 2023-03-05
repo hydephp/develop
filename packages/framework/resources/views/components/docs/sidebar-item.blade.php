ade8a45bc3fbca828c6a231c8ef1dcd572852938
Caen De Silva <caen@desilva.se>
2023-03-05 21:46:59 +0100

    Format ternary branches to multiple lines

--- packages/framework/resources/views/components/docs/sidebar-item.blade.php
+++ packages/framework/resources/views/components/docs/sidebar-item.blade.php
@@ -1,9 +1,15 @@
 @php /** @var \Hyde\Framework\Features\Navigation\NavItem $item */ @endphp
 @props(['grouped' => false])
 @php
-    $activeListClasses = $grouped ? 'active -ml-8 pl-8 bg-black/5 dark:bg-black/10' : 'active bg-black/5 dark:bg-black/10';
-    $activeItemClasses = $grouped ? '-ml-8 pl-4 py-1 px-2 block text-indigo-600 dark:text-indigo-400 dark:font-medium border-l-[0.325rem] border-indigo-500 transition-colors duration-300 ease-in-out hover:bg-black/10' : '-ml-4 p-2 block hover:bg-black/5 dark:hover:bg-black/10 text-indigo-600 dark:text-indigo-400 dark:font-medium border-l-[0.325rem] border-indigo-500 transition-colors duration-300 ease-in-out';
-    $itemClasses = $grouped ? '-ml-8 pl-4 py-1 px-2 block border-l-[0.325rem] border-transparent transition-colors duration-300 ease-in-out hover:bg-black/10' : 'block -ml-4 p-2 border-l-[0.325rem] border-transparent hover:bg-black/5 dark:hover:bg-black/10';
+    $activeListClasses = $grouped
+        ? 'active -ml-8 pl-8 bg-black/5 dark:bg-black/10'
+        : 'active bg-black/5 dark:bg-black/10';
+    $activeItemClasses = $grouped
+        ? '-ml-8 pl-4 py-1 px-2 block text-indigo-600 dark:text-indigo-400 dark:font-medium border-l-[0.325rem] border-indigo-500 transition-colors duration-300 ease-in-out hover:bg-black/10'
+        : '-ml-4 p-2 block hover:bg-black/5 dark:hover:bg-black/10 text-indigo-600 dark:text-indigo-400 dark:font-medium border-l-[0.325rem] border-indigo-500 transition-colors duration-300 ease-in-out';
+    $itemClasses = $grouped
+        ? '-ml-8 pl-4 py-1 px-2 block border-l-[0.325rem] border-transparent transition-colors duration-300 ease-in-out hover:bg-black/10'
+        : 'block -ml-4 p-2 border-l-[0.325rem] border-transparent hover:bg-black/5 dark:hover:bg-black/10';
 @endphp
 <li @class(['sidebar-navigation-item -ml-4 pl-4', $activeListClasses => $item->isCurrent()]) role="listitem">
     @if($item->isCurrent())
