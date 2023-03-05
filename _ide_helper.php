2e5484a8abf1da43561b65b339a389607b2f3259
Caen De Silva <caen@desilva.se>
2023-03-06 00:35:20 +0100

    Add spacing between lines

--- _ide_helper.php
+++ _ide_helper.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 
 /** @var \Hyde\Pages\Concerns\HydePage $page; */
 $page = \Hyde\Support\Facades\Render::getPage();
+
 /** @var \Hyde\Support\Models\Route $currentRoute; */
 $currentRoute = \Hyde\Support\Facades\Render::getCurrentRoute();
+
 /** @var string $currentPage; */
 $currentPage = \Hyde\Support\Facades\Render::getCurrentPage();
