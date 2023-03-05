b36c18418a66bc7cba54fb3869fef4518d70e3d5
Caen De Silva <caen@desilva.se>
2023-03-06 00:39:02 +0100

    Document file

--- _ide_helper.php
+++ _ide_helper.php
@@ -2,6 +2,15 @@
 
 declare(strict_types=1);
 
+/**
+ * @internal This file belongs to the HydePHP development repository,
+ * and is not covered by the backward compatibility promise,
+ * as it is not versioned through any official releases.
+ *
+ * When using a good IDE, such as PhpStorm, this file will be used to provide
+ * rich code completion for "magic" fields, such as the global $page variables.
+ */
+
 /** @var \Hyde\Pages\Concerns\HydePage $page The page being compiled/previewed */
 $page = \Hyde\Support\Facades\Render::getPage();
 
