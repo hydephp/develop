bc4af145e3030679c555da1de5746ef8fad6e9e0
Caen De Silva <caen@desilva.se>
2023-03-05 10:53:36 +0100

    Import class as alias

--- packages/framework/tests/Feature/NavigationMenuTest.php
+++ packages/framework/tests/Feature/NavigationMenuTest.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Hyde\Framework\Testing\Feature;
 
 use BadMethodCallException;
+use Hyde\Support\Models\Route as RouteModel;
 use function collect;
 use function config;
 use Hyde\Foundation\Facades\Routes;
@@ -41,17 +42,17 @@ class NavigationMenuTest extends TestCase
 
     public function test_items_are_sorted_by_priority()
     {
-        Routes::addRoute(new \Hyde\Support\Models\Route(new MarkdownPage('foo', ['navigation.priority' => 1])));
-        Routes::addRoute(new \Hyde\Support\Models\Route(new MarkdownPage('bar', ['navigation.priority' => 2])));
-        Routes::addRoute(new \Hyde\Support\Models\Route(new MarkdownPage('baz', ['navigation.priority' => 3])));
+        Routes::addRoute(new RouteModel(new MarkdownPage('foo', ['navigation.priority' => 1])));
+        Routes::addRoute(new RouteModel(new MarkdownPage('bar', ['navigation.priority' => 2])));
+        Routes::addRoute(new RouteModel(new MarkdownPage('baz', ['navigation.priority' => 3])));
 
         $this->assertSame(['Home', 'Foo', 'Bar', 'Baz'], NavigationMenu::create()->items->pluck('label')->toArray());
     }
 
     public function test_items_with_hidden_property_set_to_true_are_not_added()
     {
-        Routes::addRoute(new \Hyde\Support\Models\Route(new MarkdownPage('foo', ['navigation.hidden' => true])));
-        Routes::addRoute(new \Hyde\Support\Models\Route(new MarkdownPage('bar', ['navigation.hidden' => false])));
+        Routes::addRoute(new RouteModel(new MarkdownPage('foo', ['navigation.hidden' => true])));
+        Routes::addRoute(new RouteModel(new MarkdownPage('bar', ['navigation.hidden' => false])));
 
         $this->assertSame(['Home', 'Bar'], NavigationMenu::create()->items->pluck('label')->toArray());
     }
