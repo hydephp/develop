4ac5ac3e72da5e1d9bd2132c97db522731ff8872
Caen De Silva <caen@desilva.se>
2023-03-05 22:50:47 +0100

    Wrap element in conditional

--- packages/framework/resources/views/components/docs/sidebar-navigation.blade.php
+++ packages/framework/resources/views/components/docs/sidebar-navigation.blade.php
@@ -1,7 +1,9 @@
 @php /** @var \Hyde\Framework\Features\Navigation\DocumentationSidebar $sidebar */ @endphp
 @props(['grouped' => false])
+@if(! $grouped)
 <ul id="sidebar-navigation-items" role="list" class="pl-2">
     @foreach ($sidebar->items as $item)
         @include('hyde::components.docs.sidebar-item')
     @endforeach
 </ul>
+@endif
