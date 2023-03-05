3904a425d545c6322993ec75771795c5b309800a
Caen De Silva <caen@desilva.se>
2023-03-05 20:47:44 +0100

    Element needs listitem role

--- packages/framework/resources/views/components/docs/sidebar-item.blade.php
+++ packages/framework/resources/views/components/docs/sidebar-item.blade.php
@@ -1,5 +1,5 @@
 @php /** @var \Hyde\Framework\Features\Navigation\NavItem $item */ @endphp
-<li @class(['sidebar-navigation-item -ml-4 pl-4', 'active bg-black/5 dark:bg-black/10' => $item->isCurrent()])>
+<li @class(['sidebar-navigation-item -ml-4 pl-4', 'active bg-black/5 dark:bg-black/10' => $item->isCurrent()]) role="listitem">
     @if($item->isCurrent())
         <a href="{{ $item->destination }}" aria-current="true" class="-ml-4 p-2 block hover:bg-black/5 dark:hover:bg-black/10  text-indigo-600 dark:text-indigo-400 dark:font-medium border-l-[0.325rem] border-indigo-500 transition-colors duration-300	ease-in-out">
             {{ $item->label }}
