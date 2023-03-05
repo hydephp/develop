2e6a8784db45d67dcfebd2b8f26d305877179908
Caen De Silva <caen@desilva.se>
2023-03-06 00:34:11 +0100

    Declare variables

--- _ide_helper.php
+++ _ide_helper.php
@@ -1,5 +1,8 @@
 <?php
 
 /** @var \Hyde\Pages\Concerns\HydePage $page; */
+$page = \Hyde\Support\Facades\Render::getPage();
 /** @var \Hyde\Support\Models\Route $currentRoute; */
+$currentRoute = \Hyde\Support\Facades\Render::getCurrentRoute();
 /** @var string $currentPage; */
+$currentPage = \Hyde\Support\Facades\Render::getCurrentPage();
