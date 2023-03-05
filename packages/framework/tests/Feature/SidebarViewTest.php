a350dade5796b952dc0d8e88a9bb3499330a356b
Caen De Silva <caen@desilva.se>
2023-03-05 18:57:06 +0100

    Create micro framework for fluently testing views

--- packages/framework/tests/Feature/SidebarViewTest.php
+++ packages/framework/tests/Feature/SidebarViewTest.php
@@ -7,6 +7,7 @@ namespace Hyde\Framework\Testing\Feature;
 use Hyde\Hyde;
 use Hyde\Foundation\HydeKernel;
 use Hyde\Testing\TestCase;
+use Illuminate\Contracts\View\View;
 
 /**
  * Very high level test of the sidebar views and their combinations of layouts.
@@ -24,12 +25,44 @@ class SidebarViewTest extends TestCase
 
     public function testBaseSidebar()
     {
-        $this->renderComponent(view('hyde::components.docs.sidebar'));
+        $this->renderComponent(view('hyde::components.docs.sidebar'))
+            ->assertViewWasRendered()
+            ->assertSee('sidebar')
+            ->assertSeeText('sidebar')
+            ->assertSeeHtml('sidebar');
     }
 
-    protected function renderComponent(\Illuminate\Contracts\View\View $view): self
+    protected function renderComponent(View $view): self
     {
         $this->html = $view->render();
+
+        return $this;
+    }
+
+    protected function assertViewWasRendered(): self
+    {
+        $this->assertNotNull($this->html);
+
+        return $this;
+    }
+
+    protected function assertSee(string $text, bool $escape = true): self
+    {
+        $this->assertStringContainsString($escape ? e($text) :false, $this->html);
+
+        return $this;
+    }
+
+    protected function assertSeeHtml(string $text, bool $escape = true): self
+    {
+        $this->assertStringContainsString($escape ? e($text) : false, $this->html);
+
+        return $this;
+    }
+
+    protected function assertSeeText(string $text): self
+    {
+        $this->assertSee($text);
 
         return $this;
     }
