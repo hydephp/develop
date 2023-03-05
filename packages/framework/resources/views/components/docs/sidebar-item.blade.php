49d3604f8eb0b48a0d963822b068f4ce2ad5f1f8
Caen De Silva <caen@desilva.se>
2023-03-05 20:59:25 +0100

    Introduce additional variables

--- packages/framework/resources/views/components/docs/sidebar-item.blade.php
+++ packages/framework/resources/views/components/docs/sidebar-item.blade.php
@@ -1,10 +1,12 @@
 @php /** @var \Hyde\Framework\Features\Navigation\NavItem $item */ @endphp
 @php
     $activeListClasses = 'active bg-black/5 dark:bg-black/10';
+    $activeItemClasses = "-ml-4 p-2 block hover:bg-black/5 dark:hover:bg-black/10  text-indigo-600 dark:text-indigo-400 dark:font-medium border-l-[0.325rem] border-indigo-500 transition-colors duration-300	ease-in-out";
+    $itemClasses = "block -ml-4 p-2 border-l-[0.325rem] border-transparent hover:bg-black/5 dark:hover:bg-black/10";
 @endphp
 <li @class(['sidebar-navigation-item -ml-4 pl-4', $activeListClasses => $item->isCurrent()]) role="listitem">
     @if($item->isCurrent())
-        <a href="{{ $item->destination }}" aria-current="true" @class(["-ml-4 p-2 block hover:bg-black/5 dark:hover:bg-black/10  text-indigo-600 dark:text-indigo-400 dark:font-medium border-l-[0.325rem] border-indigo-500 transition-colors duration-300	ease-in-out"])>
+        <a href="{{ $item->destination }}" aria-current="true" @class([$activeItemClasses])>
             {{ $item->label }}
         </a>
 
@@ -13,7 +15,7 @@
             {!! ($page->getTableOfContents()) !!}
         @endif
     @else
-        <a href="{{ $item->destination }}" @class(["block -ml-4 p-2 border-l-[0.325rem] border-transparent hover:bg-black/5 dark:hover:bg-black/10"])>
+        <a href="{{ $item->destination }}" @class([$itemClasses])>
             {{ $item->label }}
         </a>
     @endif
