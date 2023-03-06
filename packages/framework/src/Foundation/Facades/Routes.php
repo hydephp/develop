8a8318ac37835be9345f82fa49f5e2ca1447d1e0
Caen De Silva <caen@desilva.se>
2023-03-06 10:49:47 +0100

    Call the facade root for shorter code with same semantics

--- packages/framework/src/Foundation/Facades/Routes.php
+++ packages/framework/src/Foundation/Facades/Routes.php
@@ -28,24 +28,24 @@ class Routes extends Facade
 
     public static function exists(string $routeKey): bool
     {
-        return HydeKernel::getInstance()->routes()->has(RouteKey::normalize($routeKey));
+        return static::getFacadeRoot()->has(RouteKey::normalize($routeKey));
     }
 
     public static function get(string $routeKey): ?Route
     {
-        return HydeKernel::getInstance()->routes()->get(RouteKey::normalize($routeKey));
+        return static::getFacadeRoot()->get(RouteKey::normalize($routeKey));
     }
 
     /** @throws \Hyde\Framework\Exceptions\RouteNotFoundException */
     public static function getOrFail(string $routeKey): Route
     {
-        return HydeKernel::getInstance()->routes()->getRoute(RouteKey::normalize($routeKey));
+        return static::getFacadeRoot()->getRoute(RouteKey::normalize($routeKey));
     }
 
     /** @return \Hyde\Foundation\Kernel\RouteCollection<\Hyde\Support\Models\Route> */
     public static function all(): RouteCollection
     {
-        return HydeKernel::getInstance()->routes()->getRoutes();
+        return static::getFacadeRoot()->getRoutes();
     }
 
     /** Get the current route for the page being rendered. */
