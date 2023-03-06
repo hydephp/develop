eccad75d48f87579c0bda43a11c4c8e5f5f5e080
Caen De Silva <caen@desilva.se>
2023-03-06 20:48:31 +0100

    Construct body

--- packages/framework/src/Pages/Concerns/PageContents.php
+++ packages/framework/src/Pages/Concerns/PageContents.php
@@ -10,4 +10,9 @@ use Illuminate\Contracts\Support\Arrayable;
 class PageContents implements Arrayable, Stringable
 {
     public string $body;
+
+    public function __construct(string $body = '')
+    {
+        $this->body = $body;
+    }
 }
