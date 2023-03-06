8f9537b4a027b180d3a8bfd65607008d3aed244f
Caen De Silva <caen@desilva.se>
2023-03-06 16:48:49 +0100

    Merge test for inlined class into existing test

--- packages/framework/tests/Feature/RebuildServiceTest.php
+++ packages/framework/tests/Feature/RebuildServiceTest.php
@@ -4,9 +4,6 @@ declare(strict_types=1);
 
 namespace Hyde\Framework\Testing\Feature;
 
-use Hyde\Foundation\Facades\Pages;
-use Hyde\Framework\Actions\StaticPageBuilder;
-use Hyde\Hyde;
 use Hyde\Testing\TestCase;
 
 /**
@@ -17,39 +14,5 @@ use Hyde\Testing\TestCase;
  */
 class RebuildServiceTest extends TestCase
 {
-    public function test_can_rebuild_blade_page()
-    {
-        $this->file('_pages/foo.blade.php');
-        (new StaticPageBuilder(Pages::getPage('_pages/foo.blade.php')))->__invoke();
-
-        $this->assertFileExists('_site/foo.html');
-        unlink(Hyde::path('_site/foo.html'));
-    }
-
-    public function test_can_rebuild_markdown_page()
-    {
-        $this->file('_pages/foo.md');
-        (new StaticPageBuilder(Pages::getPage('_pages/foo.md')))->__invoke();
-
-        $this->assertFileExists('_site/foo.html');
-        unlink(Hyde::path('_site/foo.html'));
-    }
-
-    public function test_can_rebuild_markdown_post()
-    {
-        $this->file('_posts/foo.md');
-        (new StaticPageBuilder(Pages::getPage('_posts/foo.md')))->__invoke();
-
-        $this->assertFileExists('_site/posts/foo.html');
-        unlink(Hyde::path('_site/posts/foo.html'));
-    }
-
-    public function test_can_rebuild_documentation_page()
-    {
-        $this->file('_pages/foo.md');
-        (new StaticPageBuilder(Pages::getPage('_pages/foo.md')))->__invoke();
-
-        $this->assertFileExists('_site/foo.html');
-        unlink(Hyde::path('_site/foo.html'));
-    }
+    //
 }
