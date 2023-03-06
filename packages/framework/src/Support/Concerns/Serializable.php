cae38db29e5be8dcbce62f9e895e35cc71d4f353
Caen De Silva <caen@desilva.se>
2023-03-06 22:41:27 +0100

    Move up method in file

--- packages/framework/src/Support/Concerns/Serializable.php
+++ packages/framework/src/Support/Concerns/Serializable.php
@@ -15,6 +15,12 @@ trait Serializable
     /** @inheritDoc */
     abstract public function toArray(): array;
 
+    /** Recursively serialize Arrayables */
+    public function arraySerialize(): array
+    {
+        return collect($this->toArray())->toArray();
+    }
+
     /** @inheritDoc */
     public function jsonSerialize(): array
     {
@@ -25,11 +31,5 @@ trait Serializable
     public function toJson($options = 0): string
     {
         return json_encode($this->jsonSerialize(), $options);
-    }
-
-    /** Recursively serialize Arrayables */
-    public function arraySerialize(): array
-    {
-        return collect($this->toArray())->toArray();
     }
 }
