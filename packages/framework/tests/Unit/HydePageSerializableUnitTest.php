f2ed86d5cfc0132116d08eb9d1456b78680030c9
Caen De Silva <caen@desilva.se>
2023-03-06 21:45:22 +0100

    Create HydePageSerializableUnitTest.php

---
+++ packages/framework/tests/Unit/HydePageSerializableUnitTest.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hyde\Framework\Testing\Unit;
+
+use Hyde\Pages\Concerns\HydePage;
+use Hyde\Testing\UnitTestCase;
+
+/**
+ * @covers \Hyde\Pages\Concerns\HydePage
+ */
+class HydePageSerializableUnitTest extends UnitTestCase
+{
+    public function testToArray()
+    {
+        //
+    }
+}
