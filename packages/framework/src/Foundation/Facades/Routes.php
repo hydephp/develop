8eda0aedd10d5aa18bf5b368f781822915eb21c2
Caen De Silva <caen@desilva.se>
2023-03-06 10:35:00 +0100

    Update facade method forwarding logic

--- packages/framework/src/Foundation/Facades/Routes.php
+++ packages/framework/src/Foundation/Facades/Routes.php
@@ -24,24 +24,24 @@ class Routes extends Facade
 
     public static function exists(string $routeKey): bool
     {
-        return Routes::has(RouteKey::normalize($routeKey));
+        return HydeKernel::getInstance()->routes()->has(RouteKey::normalize($routeKey));
     }
 
     public static function get(string $routeKey): ?Route
     {
-        return Routes::get(RouteKey::normalize($routeKey));
+        return HydeKernel::getInstance()->routes()->get(RouteKey::normalize($routeKey));
     }
 
     /** @throws \Hyde\Framework\Exceptions\RouteNotFoundException */
     public static function getOrFail(string $routeKey): Route
     {
-        return Routes::getRoute(RouteKey::normalize($routeKey));
+        return HydeKernel::getInstance()->routes()->getRoute(RouteKey::normalize($routeKey));
     }
 
     /** @return \Hyde\Foundation\Kernel\RouteCollection<\Hyde\Support\Models\Route> */
     public static function all(): RouteCollection
     {
-        return Routes::getRoutes();
+        return HydeKernel::getInstance()->routes()->getRoutes();
     }
 
     /** Get the current route for the page being rendered. */
