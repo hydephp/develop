ed3bb2d209eb4b105631462afe894193ad4d3abe
Caen De Silva <caen@desilva.se>
2023-03-06 12:16:24 +0100

    Import used functions

--- packages/framework/src/Markdown/Processing/BladeDownProcessor.php
+++ packages/framework/src/Markdown/Processing/BladeDownProcessor.php
@@ -7,6 +7,14 @@ namespace Hyde\Markdown\Processing;
 use Hyde\Markdown\Contracts\MarkdownPostProcessorContract;
 use Hyde\Markdown\Contracts\MarkdownPreProcessorContract;
 use Illuminate\Support\Facades\Blade;
+use function array_map;
+use function e;
+use function explode;
+use function html_entity_decode;
+use function implode;
+use function strtolower;
+use function substr;
+use function trim;
 
 /**
  * Markdown Processor to render Laravel Blade within Markdown files.
