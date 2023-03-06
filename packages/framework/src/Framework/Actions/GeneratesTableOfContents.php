aaa6b46aced1962a04530ea4bd62b2ee818bc997
Caen De Silva <caen@desilva.se>
2023-03-06 15:07:13 +0100

    Update class description for new broader scope

--- packages/framework/src/Framework/Actions/GeneratesTableOfContents.php
+++ packages/framework/src/Framework/Actions/GeneratesTableOfContents.php
@@ -15,7 +15,7 @@ use function strpos;
 use function substr;
 
 /**
- * Generates a table of contents for the Markdown document.
+ * Generates a table of contents for the Markdown document, most commonly used for the sidebar.
  *
  * @see \Hyde\Framework\Testing\Feature\Actions\GeneratesSidebarTableOfContentsTest
  */
