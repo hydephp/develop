335753958116a0f41d2383b3cc8954ac47c635ac
Caen De Silva <caen@desilva.se>
2023-03-06 23:43:10 +0100

    Reformat view

--- packages/framework/resources/views/pages/redirect.blade.php
+++ packages/framework/resources/views/pages/redirect.blade.php
@@ -1,15 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="refresh" content="0;url='{{ $destination }}'" />
-    <style>@media (prefers-color-scheme:dark){html{background-color:#111827;color:white;}a{color:#9e9eff}}</style>
+    <head>
+        <meta charset="UTF-8" />
+        <meta http-equiv="refresh" content="0;url='{{ $destination }}'" />
+        <style>@media (prefers-color-scheme:dark){html{background-color:#111827;color:white;}a{color:#9e9eff}}</style>
 
-    <title>Redirecting to {{ $destination }}</title>
-</head>
+        <title>Redirecting to {{ $destination }}</title>
+    </head>
 <body>
-    @if($showText)
-        Redirecting to <a href="{{ $destination }}">{{ $destination }}</a>.
-    @endif
+@if($showText)
+    Redirecting to <a href="{{ $destination }}">{{ $destination }}</a>.
+@endif
 </body>
 </html>
