3eb76e73ede165c567d267e1f1e58c13b958a387
Caen De Silva <caen@desilva.se>
2023-03-06 21:51:30 +0100

    Add unit tests for all core extension pages

--- packages/framework/tests/Unit/HydePageSerializableUnitTest.php
+++ packages/framework/tests/Unit/HydePageSerializableUnitTest.php
@@ -6,6 +6,11 @@ namespace Hyde\Framework\Testing\Unit;
 
 use Hyde\Pages\Concerns\HydePage;
 use Hyde\Testing\UnitTestCase;
+use Hyde\Pages\HtmlPage;
+use Hyde\Pages\BladePage;
+use Hyde\Pages\MarkdownPage;
+use Hyde\Pages\MarkdownPost;
+use Hyde\Pages\DocumentationPage;
 
 /**
  * @covers \Hyde\Pages\Concerns\HydePage
@@ -20,6 +25,46 @@ class HydePageSerializableUnitTest extends UnitTestCase
         $this->assertSame(
             ['identifier', 'routeKey', 'matter', 'metadata', 'navigation', 'title', 'canonicalUrl'],
             array_keys((new InstantiableHydePage())->toArray())
+        );
+    }
+
+    public function testHtmlPageToArray()
+    {
+        $this->assertSame(
+            ['identifier', 'routeKey', 'matter', 'metadata', 'navigation', 'title', 'canonicalUrl'],
+            array_keys((new HtmlPage())->toArray())
+        );
+    }
+
+    public function testBladePageToArray()
+    {
+        $this->assertSame(
+            ['identifier', 'routeKey', 'matter', 'metadata', 'navigation', 'title', 'canonicalUrl'],
+            array_keys((new BladePage())->toArray())
+        );
+    }
+
+    public function testMarkdownPageToArray()
+    {
+        $this->assertSame(
+            ['identifier', 'routeKey', 'matter', 'metadata', 'navigation', 'title', 'canonicalUrl'],
+            array_keys((new MarkdownPage())->toArray())
+        );
+    }
+
+    public function testMarkdownPostToArray()
+    {
+        $this->assertSame(
+            ['identifier', 'routeKey', 'matter', 'metadata', 'navigation', 'title', 'canonicalUrl'],
+            array_keys((new MarkdownPost())->toArray())
+        );
+    }
+
+    public function testDocumentationPageToArray()
+    {
+        $this->assertSame(
+            ['identifier', 'routeKey', 'matter', 'metadata', 'navigation', 'title', 'canonicalUrl'],
+            array_keys((new DocumentationPage())->toArray())
         );
     }
 }
