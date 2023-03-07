780b90d7d9a1ecbc56321afb372f47cd95c62d0c
Caen De Silva <caen@desilva.se>
2023-03-07 14:34:29 +0100

    Copilot generate test

--- packages/framework/tests/Unit/EnsureCodeFollowsNamingConventionTest.php
+++ packages/framework/tests/Unit/EnsureCodeFollowsNamingConventionTest.php
@@ -29,5 +29,14 @@ class EnsureCodeFollowsNamingConventionTest extends UnitTestCase
         $this->assertNotEmpty($files, 'No action classes found.');
 
         // Actions must have either a public static handle() method or a public non-static execute() method
+        foreach ($files as $filepath) {
+            $class = 'Hyde\\Framework\\Actions\\' . basename($filepath, '.php');
+            $reflection = new \ReflectionClass($class);
+
+            $hasHandleMethod = $reflection->hasMethod('handle') && $reflection->getMethod('handle')->isPublic() && $reflection->getMethod('handle')->isStatic();
+            $hasExecuteMethod = $reflection->hasMethod('execute') && $reflection->getMethod('execute')->isPublic() && ! $reflection->getMethod('execute')->isStatic();
+
+            $this->assertTrue($hasHandleMethod || $hasExecuteMethod, "Action class $class does not have a public static handle() method or a public non-static execute() method.");
+        }
     }
 }
