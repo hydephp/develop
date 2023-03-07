c2d67832d8e404c25432fcd887dbf9c58f9764b4
Caen De Silva <caen@desilva.se>
2023-03-07 18:04:45 +0100

    Document array types

--- packages/framework/src/Markdown/Contracts/FrontMatter/FrontMatterSchema.php
+++ packages/framework/src/Markdown/Contracts/FrontMatter/FrontMatterSchema.php
@@ -12,6 +12,10 @@ namespace Hyde\Markdown\Contracts\FrontMatter;
  * Please note that while the array keys are directly connected to the supported
  * front matter properties, they are covered by the backwards compatibility
  * promise for HydePHP. However, the formats of the values are not.
+ *
+ * Also note that if a root property value is an array, it means that either one of
+ * the types are supported. If a type within that array is an array, it means that
+ * the property is a sub-schema, which is an array of the supported types.
  */
 interface FrontMatterSchema
 {
