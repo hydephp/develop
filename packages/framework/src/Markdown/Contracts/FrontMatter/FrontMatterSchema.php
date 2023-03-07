85f4edfcd3f0de1f979efa216d6589a52db7f66d
Caen De Silva <caen@desilva.se>
2023-03-07 18:07:38 +0100

    Update documentation

--- packages/framework/src/Markdown/Contracts/FrontMatter/FrontMatterSchema.php
+++ packages/framework/src/Markdown/Contracts/FrontMatter/FrontMatterSchema.php
@@ -19,6 +19,8 @@ namespace Hyde\Markdown\Contracts\FrontMatter;
  * Also note that if a root property value is an array, it means that either one of
  * the types are supported. If a type within that array is an array, it means that
  * the property is a sub-schema, which is an array of the supported types.
+ *
+ * All front matter properties are optional, unless otherwise specified.
  */
 interface FrontMatterSchema
 {
