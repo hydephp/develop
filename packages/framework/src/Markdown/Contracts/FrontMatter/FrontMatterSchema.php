7cb7517bf143f697d980ef7feeb7fa19917fb4bb
Caen De Silva <caen@desilva.se>
2023-03-07 17:43:04 +0100

    Add backwards compatibility note

--- packages/framework/src/Markdown/Contracts/FrontMatter/FrontMatterSchema.php
+++ packages/framework/src/Markdown/Contracts/FrontMatter/FrontMatterSchema.php
@@ -8,6 +8,10 @@ namespace Hyde\Markdown\Contracts\FrontMatter;
  * Front matter schema interfaces are used by various Hyde components to
  * specify what data they provide or contain. They also serve as a
  * convenient way to see the supported front matter properties.
+ *
+ * Please note that while the array keys are directly connected to the supported
+ * front matter properties, they are covered by the backwards compatibility
+ * promise for HydePHP. However, the formats of the values are not.
  */
 interface FrontMatterSchema
 {
