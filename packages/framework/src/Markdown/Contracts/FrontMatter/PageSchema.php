a35af355ce6f70514a2ea2d28c51418a6f513c6b
Caen De Silva <caen@desilva.se>
2023-03-07 17:21:23 +0100

    PageSchema should not extend subschema NavigationSchema

--- packages/framework/src/Markdown/Contracts/FrontMatter/PageSchema.php
+++ packages/framework/src/Markdown/Contracts/FrontMatter/PageSchema.php
@@ -4,12 +4,10 @@ declare(strict_types=1);
 
 namespace Hyde\Markdown\Contracts\FrontMatter;
 
-use Hyde\Markdown\Contracts\FrontMatter\SubSchemas\NavigationSchema;
-
 /**
  * @see \Hyde\Pages\Concerns\HydePage
  */
-interface PageSchema extends NavigationSchema
+interface PageSchema
 {
     public const PAGE_SCHEMA = [
         'title'         => 'string',
