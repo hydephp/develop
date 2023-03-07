1f976d3efa3c167714f45d99e87d85267613b569
Caen De Silva <caen@desilva.se>
2023-03-07 17:38:28 +0100

    Define empty base interface

--- packages/framework/src/Markdown/Contracts/FrontMatter/FrontMatterSchema.php
+++ packages/framework/src/Markdown/Contracts/FrontMatter/FrontMatterSchema.php
@@ -4,6 +4,11 @@ declare(strict_types=1);
 
 namespace Hyde\Markdown\Contracts\FrontMatter;
 
+/**
+ * Front matter schema interfaces are used by various Hyde components to
+ * specify what data they provide or contain. They also serve as a
+ * convenient way to see the supported front matter properties.
+ */
 interface FrontMatterSchema
 {
     //
