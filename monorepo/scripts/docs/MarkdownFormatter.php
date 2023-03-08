8077c152f489b107ebf8d7d6116d54694c01e756
Caen De Silva <caen@desilva.se>
2023-03-08 21:31:56 +0100

    ChatGPT generate base script

--- monorepo/scripts/docs/MarkdownFormatter.php
+++ monorepo/scripts/docs/MarkdownFormatter.php
@@ -1,1 +1,21 @@
 <?php
+
+function find_markdown_files($dir) {
+    $markdown_files = array();
+
+    $iterator = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($dir));
+    foreach ($iterator as $file) {
+        if ($file->isFile() && strtolower($file->getExtension()) == 'md') {
+            $markdown_files[] = $file->getPathname();
+        }
+    }
+
+    return $markdown_files;
+}
+
+$dir = '/path/to/directory';
+$markdown_files = find_markdown_files($dir);
+
+foreach ($markdown_files as $file) {
+    echo $file . "\n";
+}
