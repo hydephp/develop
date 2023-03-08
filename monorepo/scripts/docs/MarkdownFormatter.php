05e078b528e4f8f2c20231840fe4b2ecff2dfaeb
Caen De Silva <caen@desilva.se>
2023-03-08 21:35:56 +0100

    ChatGPT generate formatter function

--- monorepo/scripts/docs/MarkdownFormatter.php
+++ monorepo/scripts/docs/MarkdownFormatter.php
@@ -23,6 +23,30 @@ function handle_file(string $file): void
     echo 'Handling '.$file."\n";
 }
 
+function add_two_lines_above_headings($filename)
+{
+    $lines = file($filename);
+    $new_lines = array();
+
+    $previous_line_is_heading = false;
+    foreach ($lines as $line) {
+        if (preg_match('/^#+\s/', $line)) {
+            if (!$previous_line_is_heading) {
+                $new_lines[] = "\n";
+                $new_lines[] = "\n";
+            }
+            $previous_line_is_heading = true;
+        } else {
+            $previous_line_is_heading = false;
+        }
+        $new_lines[] = $line;
+    }
+
+    $new_content = implode('', $new_lines);
+    file_put_contents($filename, $new_content);
+}
+
+
 $dir = __DIR__.'/../../../docs';
 $markdownFiles = find_markdown_files($dir);
 
