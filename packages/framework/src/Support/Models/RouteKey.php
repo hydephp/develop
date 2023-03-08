91d3e1243cc35bdddf77ab7e11623bc6b9a66598
Caen De Silva <caen@desilva.se>
2023-03-08 13:07:24 +0100

    Deprecate RouteKey::normalize method

--- packages/framework/src/Support/Models/RouteKey.php
+++ packages/framework/src/Support/Models/RouteKey.php
@@ -44,6 +44,7 @@ final class RouteKey implements Stringable
         return $this->key;
     }
 
+    /** @deprecated v1.0.0-RC.2 - This method will be removed before v1.0.0 */
     public static function normalize(string $string): string
     {
         return $string;
