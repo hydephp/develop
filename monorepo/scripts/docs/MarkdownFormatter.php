f048228e5d1d157bcf0ae7b0326fc64f24741866
Caen De Silva <caen@desilva.se>
2023-03-08 21:32:30 +0100

    Clean up script

--- monorepo/scripts/docs/MarkdownFormatter.php
+++ monorepo/scripts/docs/MarkdownFormatter.php
@@ -1,21 +1,24 @@
 <?php
 
-function find_markdown_files($dir) {
+declare(strict_types=1);
+
+function find_markdown_files($dir): array
+{
     $markdown_files = array();
 
     $iterator = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($dir));
     foreach ($iterator as $file) {
         if ($file->isFile() && strtolower($file->getExtension()) == 'md') {
-            $markdown_files[] = $file->getPathname();
+            $markdown_files[] = realpath($file->getPathname());
         }
     }
 
     return $markdown_files;
 }
 
-$dir = '/path/to/directory';
-$markdown_files = find_markdown_files($dir);
+$dir = __DIR__.'/../../../docs';
+$markdownFiles = find_markdown_files($dir);
 
-foreach ($markdown_files as $file) {
+foreach ($markdownFiles as $file) {
     echo $file . "\n";
 }
