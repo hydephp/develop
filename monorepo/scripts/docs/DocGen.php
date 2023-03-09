76195f11f0e4d0c7ab8147fe8dac886246144300
Caen De Silva <caen@desilva.se>
2023-03-09 22:52:53 +0100

    Revert "Break early to test"
    This reverts commit 1b10beb54d4c244ca8b1193a9082bb4c08bd612f.

--- monorepo/scripts/docs/DocGen.php
+++ monorepo/scripts/docs/DocGen.php
@@ -38,17 +38,11 @@ $output = [];
 // Generate static methods
 foreach ($staticMethods as $method) {
     documentMethod($method, $output);
-
-    // test // FIXME: Remove
-    break;
 }
 
 // Generate instance methods
 foreach ($instanceMethods as $method) {
     documentMethod($method, $output);
-
-    // test // FIXME: Remove
-    break;
 }
 
 // Output the documentation
