fdaaa5eedde812bd08e39a13dca0a071294cafe8
Caen De Silva <caen@desilva.se>
2023-03-09 21:53:35 +0100

    Cleanup code

--- monorepo/scripts/docs/DocGen.php
+++ monorepo/scripts/docs/DocGen.php
@@ -5,27 +5,30 @@ declare(strict_types=1);
 /**
  * @internal
  */
+
+use Hyde\Pages\Concerns\HydePage;
+
 require_once __DIR__.'/../../../vendor/autoload.php';
 
-$class = \Hyde\Pages\Concerns\HydePage::class;
+$class = HydePage::class;
 
-$reflection = new \ReflectionClass($class);
+$reflection = new ReflectionClass($class);
 
-$methods = $reflection->getMethods(\ReflectionMethod::IS_PUBLIC);
-$fileName = (new \ReflectionClass($class))->getFileName();
+$methods = $reflection->getMethods(ReflectionMethod::IS_PUBLIC);
+$fileName = (new ReflectionClass($class))->getFileName();
 
 // Remove methods defined in traits or parent classes
-$methods = array_filter($methods, function (\ReflectionMethod $method) use ($fileName) {
+$methods = array_filter($methods, function (ReflectionMethod $method) use ($fileName) {
     return $method->getFileName() === $fileName;
 });
 
 // Split methods into static and non-static
 
-$staticMethods = array_filter($methods, function (\ReflectionMethod $method) {
+$staticMethods = array_filter($methods, function (ReflectionMethod $method) {
     return $method->isStatic();
 });
 
-$instanceMethods = array_filter($methods, function (\ReflectionMethod $method) {
+$instanceMethods = array_filter($methods, function (ReflectionMethod $method) {
     return ! $method->isStatic();
 });
 
