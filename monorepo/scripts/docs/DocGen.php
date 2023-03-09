0524fee0740e5be3c992ed3185bc5c8b2b3f30a9
Caen De Silva <caen@desilva.se>
2023-03-09 21:49:24 +0100

    Add base reflection

--- monorepo/scripts/docs/DocGen.php
+++ monorepo/scripts/docs/DocGen.php
@@ -6,3 +6,9 @@ declare(strict_types=1);
  * @internal
  */
 require_once __DIR__.'/../../../vendor/autoload.php';
+
+$class = \Hyde\Pages\Concerns\HydePage::class;
+
+$reflection = new \ReflectionClass($class);
+
+$methods = $reflection->getMethods(\ReflectionMethod::IS_PUBLIC);
