2c508ebeaa186dc8776a07d004911ba23997943b
Caen De Silva <caen@desilva.se>
2023-03-09 16:39:26 +0100

    Minify

--- sse-reload.php
+++ sse-reload.php
@@ -29,24 +29,4 @@ while (true) {
 
 // and use script
 
-//
-//<script>
-//    const source = new EventSource('http://localhost:8082');
-//
-//    source.addEventListener('open', function(event) {
-//        console.log('Connection open');
-//    });
-//
-//    source.addEventListener('message', function(event) {
-//        const data = event.data;
-//        console.log('Received data:', data);
-//
-//        if (data === 'refresh') {
-//            window.location.reload();
-//        }
-//    });
-//
-//    source.addEventListener('error', function(event) {
-//        console.error('EventSource error', event);
-//    });
-//</script>
+//<script>const source=new EventSource("http://localhost:8082");source.addEventListener("open",(function(e){console.log("Connection open")})),source.addEventListener("message",(function(e){const o=e.data;console.log("Received data:",o),"refresh"===o&&window.location.reload()})),source.addEventListener("error",(function(e){console.error("EventSource error",e)}));</script>
