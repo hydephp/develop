3ebbbf288bf214c39a6f5078f2dae4dfd188be8e
Caen De Silva <caen@desilva.se>
2023-03-09 21:49:37 +0100

    Remove methods defined in traits or parent classes

--- monorepo/scripts/docs/DocGen.php
+++ monorepo/scripts/docs/DocGen.php
@@ -12,3 +12,9 @@ $class = \Hyde\Pages\Concerns\HydePage::class;
 $reflection = new \ReflectionClass($class);
 
 $methods = $reflection->getMethods(\ReflectionMethod::IS_PUBLIC);
+$fileName = (new \ReflectionClass($class))->getFileName();
+
+// Remove methods defined in traits or parent classes
+$methods = array_filter($methods, function (\ReflectionMethod $method) use ($fileName) {
+    return $method->getFileName() === $fileName;
+});
