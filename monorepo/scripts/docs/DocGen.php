92b22ea7234ff7f67ffc16199f6112b3c95c1bb0
Caen De Silva <caen@desilva.se>
2023-03-10 10:11:34 +0100

    Add start/end markers

--- monorepo/scripts/docs/DocGen.php
+++ monorepo/scripts/docs/DocGen.php
@@ -47,6 +47,9 @@ foreach ($instanceMethods as $method) {
 
 // Join the output
 $text = implode("\n", $output);
+$startMarker = '<!-- START GENERATED DOCS FOR '.$class.' -->';
+$endMarker = '<!-- END GENERATED DOCS FOR '.$class.' -->';
+$text = "$startMarker\n$text\n$endMarker";
 
 // Output the documentation
 echo $text;
