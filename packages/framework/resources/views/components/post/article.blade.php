e6b0c27519bf59339f1e4d5a5b83ff3c6dd78fe9
Caen De Silva <caen@desilva.se>
2023-03-12 20:21:23 +0100

    Revert "Use isset directly"
    This reverts commit 84869ba3af28972a454ab8acfce1b770643c50bf.

--- packages/framework/resources/views/components/post/article.blade.php
+++ packages/framework/resources/views/components/post/article.blade.php
@@ -2,7 +2,7 @@
          itemtype="https://schema.org/Article"
     @class(['post-article mx-auto prose dark:prose-invert', 'torchlight-enabled' => Hyde\Facades\Features::hasTorchlight()])>
     <meta itemprop="identifier" content="{{ $page->identifier }}">
-    @isset($page->canonicalUrl)
+    @if($page->canonicalUrl !== null)
         <meta itemprop="url" content="{{ $page->canonicalUrl }}">
     @endif
 
