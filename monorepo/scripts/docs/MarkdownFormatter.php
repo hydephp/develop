c58d19bdc283f7dde3edd1ad6e3b901492e80ca9
Caen De Silva <caen@desilva.se>
2023-03-13 13:13:21 +0100

    Fix wrong logic

--- monorepo/scripts/docs/MarkdownFormatter.php
+++ monorepo/scripts/docs/MarkdownFormatter.php
@@ -425,9 +425,9 @@ if ($warningCount > 0) {
     echo "\033[33m".$warningCount." warnings found.\033[0m";
     if (file_exists(__DIR__.'/../cache/last-run-warnings-count.txt')) {
         $lastRunWarningsCount = (int) file_get_contents(__DIR__.'/../cache/last-run-warnings-count.txt');
-        if ($warningCount > $lastRunWarningsCount) {
+        if ($warningCount < $lastRunWarningsCount) {
             echo ' Good job! You fixed '.($warningCount - $lastRunWarningsCount).' warnings!';
-        } elseif ($warningCount < $lastRunWarningsCount) {
+        } elseif ($warningCount > $lastRunWarningsCount) {
             echo ' Uh oh! You introduced '.($lastRunWarningsCount - $warningCount).' new warnings!';
         }
     }
