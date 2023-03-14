f827334de8547a3d75cb36798113b3a7814f1430
Caen De Silva <caen@desilva.se>
2023-03-14 10:22:50 +0100

    Remove extra star from PHPDoc comment

--- _ide_helper.php
+++ _ide_helper.php
@@ -16,7 +16,7 @@ declare(strict_types=1);
 /** @var \Hyde\Pages\Concerns\HydePage $page The page being compiled/previewed */
 $page = \Hyde\Support\Facades\Render::getPage();
 
-/*** @var \Hyde\Support\Models\Route $currentRoute The route for the page being compiled/previewed */
+/** @var \Hyde\Support\Models\Route $currentRoute The route for the page being compiled/previewed */
 $route = \Hyde\Support\Facades\Render::getRoute();
 
 /** @var string $currentPage The route key for the page being compiled/previewed */
