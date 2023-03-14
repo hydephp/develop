c23ac045e4b796860f921cff18518a110d4bd423
Caen De Silva <caen@desilva.se>
2023-03-14 17:00:56 +0100

    Create ConsoleServiceProviderUnitTest.php

---
+++ packages/framework/tests/Unit/ConsoleServiceProviderUnitTest.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hyde\Framework\Testing\Unit;
+
+use Hyde\Console\ConsoleServiceProvider;
+use Hyde\Testing\TestCase;
+
+/**
+ * @covers \Hyde\Console\ConsoleServiceProvider
+ */
+class ConsoleServiceProviderUnitTest extends TestCase
+{
+    //
+}
