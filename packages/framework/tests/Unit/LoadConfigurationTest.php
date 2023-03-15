4135f648163d1f16afd66e27e26a0abe0392c6f7
Caen De Silva <caen@desilva.se>
2023-03-15 15:12:44 +0100

    Test runtime configuration setting

--- packages/framework/tests/Unit/LoadConfigurationTest.php
+++ packages/framework/tests/Unit/LoadConfigurationTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Hyde\Framework\Testing\Unit;
 
+use Hyde\Foundation\Application;
 use Hyde\Foundation\Internal\LoadConfiguration;
 use Hyde\Testing\UnitTestCase;
 
@@ -12,5 +13,22 @@ use Hyde\Testing\UnitTestCase;
  */
 class LoadConfigurationTest extends UnitTestCase
 {
-    //
+    public function testItLoadsRuntimeConfiguration()
+    {
+        $serverBackup = $_SERVER;
+
+        $_SERVER['argv'] = ['--pretty-urls'];
+
+        $app = new Application(getcwd());
+
+        $loader = new LoadConfiguration();
+        $loader->bootstrap($app);
+
+        $this->assertTrue(config('hyde.pretty_urls'));
+
+        $_SERVER = $serverBackup;
+
+        $loader->bootstrap($app);
+        $this->assertFalse(config('hyde.pretty_urls'));
+    }
 }
