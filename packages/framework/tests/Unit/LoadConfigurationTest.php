8345817bfc20decec4d72899b6f9db97ff3d0073
Caen De Silva <caen@desilva.se>
2023-03-15 11:42:09 +0100

    Create LoadConfigurationTest.php

---
+++ packages/framework/tests/Unit/LoadConfigurationTest.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hyde\Framework\Testing\Unit;
+
+use Hyde\Foundation\Internal\LoadConfiguration;
+use Hyde\Testing\UnitTestCase;
+
+/**
+ * @covers \Hyde\Foundation\Internal\LoadConfiguration
+ */
+class LoadConfigurationTest extends UnitTestCase
+{
+    //
+}
