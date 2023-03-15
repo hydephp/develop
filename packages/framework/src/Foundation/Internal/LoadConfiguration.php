18dfe57896929fc02266ebf75708ccc336f1a047
Caen De Silva <caen@desilva.se>
2023-03-15 22:03:05 +0100

    Cast tapped result to array

--- packages/framework/src/Foundation/Internal/LoadConfiguration.php
+++ packages/framework/src/Foundation/Internal/LoadConfiguration.php
@@ -20,7 +20,7 @@ class LoadConfiguration extends BaseLoadConfiguration
     /** Get all the configuration files for the application. */
     protected function getConfigurationFiles(Application $app): array
     {
-        return tap(parent::getConfigurationFiles($app), function (array &$files) use ($app): void {
+        return (array) tap(parent::getConfigurationFiles($app), function (array &$files) use ($app): void {
             // Inject our custom config file which is stored in `app/config.php`.
             $files['app'] = $app->basePath().DIRECTORY_SEPARATOR.'app'.DIRECTORY_SEPARATOR.'config.php';
 
