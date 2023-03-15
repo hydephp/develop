08a855fac8c3d6a573930e82439ad5bc93d1e106
Caen De Silva <caen@desilva.se>
2023-03-15 15:38:56 +0100

    Use assertSame

--- packages/framework/tests/Unit/HydeHelperFacadeMakeTitleTest.php
+++ packages/framework/tests/Unit/HydeHelperFacadeMakeTitleTest.php
@@ -13,37 +13,37 @@ class HydeHelperFacadeMakeTitleTest extends UnitTestCase
 
     public function testMakeTitleHelperParsesKebabCaseIntoTitle()
     {
-        $this->assertEquals('Hello World', Hyde::makeTitle('hello-world'));
+        $this->assertSame('Hello World', Hyde::makeTitle('hello-world'));
     }
 
     public function testMakeTitleHelperParsesSnakeCaseIntoTitle()
     {
-        $this->assertEquals('Hello World', Hyde::makeTitle('hello_world'));
+        $this->assertSame('Hello World', Hyde::makeTitle('hello_world'));
     }
 
     public function testMakeTitleHelperParsesCamelCaseIntoTitle()
     {
-        $this->assertEquals('Hello World', Hyde::makeTitle('helloWorld'));
+        $this->assertSame('Hello World', Hyde::makeTitle('helloWorld'));
     }
 
     public function testMakeTitleHelperParsesPascalCaseIntoTitle()
     {
-        $this->assertEquals('Hello World', Hyde::makeTitle('HelloWorld'));
+        $this->assertSame('Hello World', Hyde::makeTitle('HelloWorld'));
     }
 
     public function testMakeTitleHelperParsesTitleCaseIntoTitle()
     {
-        $this->assertEquals('Hello World', Hyde::makeTitle('Hello World'));
+        $this->assertSame('Hello World', Hyde::makeTitle('Hello World'));
     }
 
     public function testMakeTitleHelperParsesTitleCaseWithSpacesIntoTitle()
     {
-        $this->assertEquals('Hello World', Hyde::makeTitle('Hello World'));
+        $this->assertSame('Hello World', Hyde::makeTitle('Hello World'));
     }
 
     public function testMakeTitleHelperDoesNotCapitalizeAuxiliaryWords()
     {
-        $this->assertEquals('The a an the in on by with of and or but',
+        $this->assertSame('The a an the in on by with of and or but',
             Hyde::makeTitle('the_a_an_the_in_on_by_with_of_and_or_but'));
     }
 }
