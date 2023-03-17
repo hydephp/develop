4a9862af614d2b1cd7ae13022595c68385e4cc04
Caen De Silva <caen@desilva.se>
2023-03-17 13:26:44 +0100

    Create RelatedPublicationsComponentTest.php

---
+++ packages/publications/tests/Feature/RelatedPublicationsComponentTest.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hyde\Publications\Testing\Feature;
+
+use Hyde\Testing\TestCase;
+
+/**
+ * @covers \Hyde\Publications\Views\Components\RelatedPublicationsComponent
+ */
+class RelatedPublicationsComponentTest extends TestCase
+{
+    //
+}
