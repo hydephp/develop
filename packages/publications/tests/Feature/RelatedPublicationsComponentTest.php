dbfc2cc3366103a2f777e4dc19bd075354bfba1b
Caen De Silva <caen@desilva.se>
2023-03-17 17:52:42 +0100

    Add new base test

--- packages/publications/tests/Feature/RelatedPublicationsComponentTest.php
+++ packages/publications/tests/Feature/RelatedPublicationsComponentTest.php
@@ -4,12 +4,25 @@ declare(strict_types=1);
 
 namespace Hyde\Publications\Testing\Feature;
 
+use Hyde\Publications\Models\PublicationPage;
+use Hyde\Publications\Models\PublicationType;
+use Hyde\Publications\Views\Components\RelatedPublicationsComponent;
+use Hyde\Support\Models\Route;
 use Hyde\Testing\TestCase;
+use Illuminate\Support\Collection;
 
 /**
  * @covers \Hyde\Publications\Views\Components\RelatedPublicationsComponent
  */
 class RelatedPublicationsComponentTest extends TestCase
 {
-    //
+    public function testWithBlankPublicationType()
+    {
+        $type = new PublicationType('foo');
+        $page = new PublicationPage('foo', type: $type);
+        $this->mockRoute(new Route($page));
+
+        $component = new RelatedPublicationsComponent();
+        $this->assertEquals(new Collection(), $component->relatedPublications);
+    }
 }
