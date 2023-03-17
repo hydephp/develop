90f867bcf7bb7410e0f57fe1f899f872b4bb7c61
Caen De Silva <caen@desilva.se>
2023-03-17 17:14:47 +0100

    Replace opinionated styles with prose to match other publication views
    These components are likely to be changed by the user, so we want to keep them as basic as possible, only offering the needed markup to build upon.

--- packages/publications/resources/views/components/related-publications.blade.php
+++ packages/publications/resources/views/components/related-publications.blade.php
@@ -1,14 +1,14 @@
 @if (count($relatedPublications) > 0)
-    <section>
-        <h2 class="text-2xl font-semibold text-gray-700 dark:text-gray-200 mt-8 mb-2">{{ $title }}</h2>
-        <nav {{ $attributes->merge(['aria-label' => 'related']) }} class="text-xl mb-4">
-            <ul class="flex pl-6">
+    <section class="prose dark:prose-invert">
+        <h2>{{ $title }}</h2>
+        <nav {{ $attributes->merge(['aria-label' => 'related']) }}>
+            <ul>
                 @foreach ($relatedPublications as $publication)
                     @php
                         $date = \Carbon\Carbon::parse($publication->matter->__createdAt)->format('Y-m-d');
                     @endphp
-                    <li class="list-disc">
-                        <a href="{{ $publication->getRoute() }}" class="text-primary-600 hover:text-primary-400 dark:text-primary-500 dark:hover:text-primary-600">
+                    <li>
+                        <a href="{{ $publication->getRoute() }}">
                             {{ $publication->title }} ({{ $date }})
                         </a>
                     </li>
