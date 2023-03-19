e5f2e94b67fcd33d14598e02e42f383f45b86855
Caen De Silva <caen@desilva.se>
2023-03-19 20:42:03 +0100

    Create StaticSiteBuilderDocumentationModuleTest.php

---
+++ packages/framework/tests/Feature/StaticSiteBuilderDocumentationModuleTest.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hyde\Framework\Testing\Feature;
+
+use Hyde\Testing\TestCase;
+
+/**
+ * Test the documentation page compiler module.
+ */
+class StaticSiteBuilderDocumentationModuleTest extends TestCase
+{
+    //
+}
