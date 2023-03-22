eb26c1587146e5a631d81a38cf6e4f3353eb1031
Caen De Silva <caen@desilva.se>
2023-03-22 10:54:36 +0100

    Change extraction to get table of contents from the bottom of document

--- packages/framework/src/Framework/Actions/GeneratesTableOfContents.php
+++ packages/framework/src/Framework/Actions/GeneratesTableOfContents.php
@@ -57,7 +57,7 @@ class GeneratesTableOfContents
 
     protected function extractTableOfContents(string $html): string
     {
-        // Return everything before the [[END_TOC]] marker.
-        return substr($html, 0, strpos($html, '<p>[[END_TOC]]'));
+        // The table of contents is always at the bottom of the document, so we can just extract it from the bottom of the HTML.
+        return substr($html, strpos($html, '<ul class="table-of-contents">'));
     }
 }
