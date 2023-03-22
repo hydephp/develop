93819ff1e45ec7f7ec20a2a71a9249604c41c642
Caen De Silva <caen@desilva.se>
2023-03-22 13:46:50 +0100

    Explicitly define Blade extension
    Doesn't change any semantics as it's already covered, but makes code more apparent

--- monorepo/scripts/FileFormatter.php
+++ monorepo/scripts/FileFormatter.php
@@ -204,7 +204,7 @@ function find_files(): array
 function find_files_in_directory(string $directory): array
 {
     $files = [];
-    $extensions = ['php'];
+    $extensions = ['php', 'blade.php'];
 
     $directory = realpath($directory);
     if ($directory === false) {
