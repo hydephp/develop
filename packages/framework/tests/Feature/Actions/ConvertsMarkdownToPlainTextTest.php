051394cccf2a441def529bb99751e41bb3be59ad
Caen De Silva <caen@desilva.se>
2023-03-22 13:54:40 +0100

    Replace tabs with spaces

--- packages/framework/tests/Feature/Actions/ConvertsMarkdownToPlainTextTest.php
+++ packages/framework/tests/Feature/Actions/ConvertsMarkdownToPlainTextTest.php
@@ -16,10 +16,10 @@ class ConvertsMarkdownToPlainTextTest extends TestCase
     {
         $markdown = <<<'MD'
         # Heading level 1       
-        ## Heading level 2	       
-        ### Heading level 3	       
-        #### Heading level 4	       
-        ##### Heading level 5	       
+        ## Heading level 2           
+        ### Heading level 3           
+        #### Heading level 4           
+        ##### Heading level 5           
         ###### Heading level 6
         MD;
 
