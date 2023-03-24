86a027fdd612c85e3a75c66e3f2ce499be386fec
Caen De Silva <caen@desilva.se>
2023-03-24 13:43:45 +0100

    Change Git command

--- monorepo/scripts/ping-openanalytics-testrunner.php
+++ monorepo/scripts/ping-openanalytics-testrunner.php
@@ -57,7 +57,7 @@ foreach (explode(' ', $junit) as $pair) {
 }
 
 $data['commit'] = shell_exec('git rev-parse HEAD');
-$data['branch'] = shell_exec('git rev-parse --abbrev-ref HEAD');
+$data['branch'] = shell_exec('git branch --show-current');
 $data['runner_os'] = php_uname('s');
 
 curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($data));
