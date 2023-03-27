dc94f492bb2ca2f28e8a5d48726207d01179eb83
Caen De Silva <caen@desilva.se>
2023-03-27 20:28:40 +0200

    Get version from package.json

--- packages/hydefront/.github/scripts/post-build.php
+++ packages/hydefront/.github/scripts/post-build.php
@@ -7,6 +7,11 @@ require_once __DIR__ . '/minima.php';
 main(function (): int {
     $this->info('Verifying build files...');
 
+    $baseDir = __DIR__ . '/../../';
+    $package = json_decode(file_get_contents($baseDir . 'package.json'), true);
+    $version = $package['version'];
+    $this->line("Found version: $version");
+
     if ($this->hasOption('fix')) {
         $this->info('Fixing build files...');
     }
