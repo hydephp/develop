ad62481ffe0c0f91913ab542db31a152c3c04dbf
Caen De Silva <caen@desilva.se>
2023-03-27 21:12:18 +0200

    Display the current working directory

--- packages/hydefront/.github/scripts/version.php
+++ packages/hydefront/.github/scripts/version.php
@@ -7,6 +7,7 @@ require_once __DIR__ . '/minima.php';
 exit(main(function (): int {
     if (! is_dir(getcwd() . '/packages')) {
         $this->error('This script must be run from the root of the monorepo');
+        $this->warning('Current working directory: ' . getcwd());
         return 1;
     }
 
