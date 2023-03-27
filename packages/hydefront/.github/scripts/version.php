ef1fb2e6cde3ade6edebfe09ea4b9edcc284422a
Caen De Silva <caen@desilva.se>
2023-03-27 21:11:56 +0200

    Validate script is run from monorepo root

--- packages/hydefront/.github/scripts/version.php
+++ packages/hydefront/.github/scripts/version.php
@@ -5,5 +5,10 @@ declare(strict_types=1);
 require_once __DIR__ . '/minima.php';
 
 exit(main(function (): int {
+    if (! is_dir(getcwd() . '/packages')) {
+        $this->error('This script must be run from the root of the monorepo');
+        return 1;
+    }
+
     return 0;
 }));
