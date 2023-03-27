f9f7e99670ff2828add3bffb71b88a4a9fa8b606
Caen De Silva <caen@desilva.se>
2023-03-27 20:27:42 +0200

    Make verify default and unwrap conditionals

--- packages/hydefront/.github/scripts/post-build.php
+++ packages/hydefront/.github/scripts/post-build.php
@@ -5,13 +5,10 @@ declare(strict_types=1);
 require_once __DIR__ . '/minima.php';
 
 main(function (): int {
-    if ($this->hasOption('verify')) {
-        $this->info('Verifying build files...');
-    } else if ($this->hasOption('fix')) {
+    $this->info('Verifying build files...');
+
+    if ($this->hasOption('fix')) {
         $this->info('Fixing build files...');
-    } else {
-        $this->error('Invalid option: Must be either --verify or --fix');
-        return 400;
     }
 
     return 0;
