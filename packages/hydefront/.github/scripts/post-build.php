0f57d8851420dc0c7ac1c4beddff132ce6a11f97
Caen De Silva <caen@desilva.se>
2023-03-27 20:34:36 +0200

    Get the versions shown in stylesheets

--- packages/hydefront/.github/scripts/post-build.php
+++ packages/hydefront/.github/scripts/post-build.php
@@ -10,7 +10,13 @@ main(function (): int {
     $baseDir = __DIR__ . '/../../';
     $package = json_decode(file_get_contents($baseDir . 'package.json'), true);
     $version = $package['version'];
-    $this->line("Found version: $version");
+    $this->line("Found version '$version' in package.json");
+
+    $hydeCssVersion = getCssVersion($baseDir . 'dist/hyde.css');
+    $this->line("Found version '$hydeCssVersion' in hyde.css");
+
+    $appCssVersion = getCssVersion($baseDir . 'dist/app.css');
+    $this->line("Found version '$appCssVersion' in app.css");
 
     if ($this->hasOption('fix')) {
         $this->info('Fixing build files...');
@@ -18,3 +24,20 @@ main(function (): int {
 
     return 0;
 });
+
+function getCssVersion(string $path): string
+{
+    $contents = file_get_contents($path);
+    $prefix = '/*! HydeFront v';
+    if (! str_starts_with($contents, $prefix)) {
+        throw new Exception('Invalid CSS file');
+    }
+    $contents = substr($contents, strlen($prefix));
+    // Get everything before  |
+    $pipePos = strpos($contents, '|');
+    if ($pipePos === false) {
+        throw new Exception('Invalid CSS file');
+    }
+    $contents = substr($contents, 0, $pipePos);
+    return trim($contents);
+}
