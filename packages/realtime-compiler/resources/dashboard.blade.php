bf94918544f1f8a0830de7e7a90a274b3bf70f20
Caen De Silva <caen@desilva.se>
2023-10-20 20:38:08 +0200

    Add Bootstrap script to dashboard

--- packages/realtime-compiler/resources/dashboard.blade.php
+++ packages/realtime-compiler/resources/dashboard.blade.php
@@ -127,6 +127,9 @@
     </div>
 </footer>
 @if($dashboard->enableEditor())
+    {{-- Interactivity is not needed when editor is disabled --}}
+    <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+
     <script>
         /**
          * Progressive enhancement when JavaScript is enabled to intercept form requests
