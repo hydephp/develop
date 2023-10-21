73f3439d0872cd4c6ff8311eae1d7ce273533124
Caen De Silva <caen@desilva.se>
2023-10-21 17:14:09 +0200

    Unit test save method

--- packages/framework/tests/Unit/CreatesNewMarkdownPostFileTest.php
+++ packages/framework/tests/Unit/CreatesNewMarkdownPostFileTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Hyde\Framework\Testing\Unit;
 
+use Hyde\Hyde;
 use Hyde\Testing\TestCase;
 use Illuminate\Support\Carbon;
 use Hyde\Framework\Actions\CreatesNewMarkdownPostFile;
@@ -45,5 +46,30 @@ class CreatesNewMarkdownPostFileTest extends TestCase
             'author' => 'qux',
             'date' => '2024-01-01 00:00',
         ], $array);
+    }
+
+    public function testSave()
+    {
+        Carbon::setTestNow(Carbon::create(2024));
+
+        $action = new CreatesNewMarkdownPostFile('Example Post', null, null, null);
+        $action->save();
+
+        $path = Hyde::path('_posts/example-post.md');
+
+        $this->assertSame(<<<'MARKDOWN'
+        ---
+        title: 'Example Post'
+        description: 'A short description used in previews and SEO'
+        category: blog
+        author: default
+        date: '2024-01-01 00:00'
+        ---
+
+        ## Write something awesome.
+        
+        MARKDOWN, file_get_contents($path));
+
+        unlink($path);
     }
 }
