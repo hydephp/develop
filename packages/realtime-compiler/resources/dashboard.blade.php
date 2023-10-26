12f845244843bb57a9b5bcc31f74422ff381f5d7
Caen De Silva <caen@desilva.se>
2023-10-26 14:19:01 +0200

    Add dashboard button to copy project path to clipboard

--- packages/realtime-compiler/resources/dashboard.blade.php
+++ packages/realtime-compiler/resources/dashboard.blade.php
@@ -77,6 +77,35 @@
                                 <td>
                                     <strong class="h6">{{ $type }}:</strong>
                                     <span class="card-text">{{ $info }}</span>
+                                    @if($loop->last)
+                                        <button id="copyPathToClipboardButton" class="btn float-end px-1 py-0" onclick="copyPathToClipboard()">
+                                            <svg id="copyPathToClipboardButtonIcon" style="display: inline;" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-clipboard" viewBox="0 0 16 16">
+                                                <path d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1v-1z"/>
+                                                <path d="M9.5 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5h3zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3z"/>
+                                            </svg>
+                                            <svg id="copyPathToClipboardButtonIconSuccess" style="display: none;" title="Copied!" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-clipboard-check" viewBox="0 0 16 16">
+                                                <path fill-rule="evenodd" d="M10.854 7.146a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7.5 9.793l2.646-2.647a.5.5 0 0 1 .708 0z"/>
+                                                <path d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1v-1z"/>
+                                                <path d="M9.5 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5h3zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3z"/>
+                                            </svg>
+                                        </button>
+                                        <script>
+                                            async function copyPathToClipboard() {
+                                                let data = '{{ str_replace('\\', '\\\\', $info) }}';
+                                                try {
+                                                    await navigator.clipboard.writeText(data);
+                                                    document.getElementById("copyPathToClipboardButtonIcon").style.display = 'none';
+                                                    document.getElementById("copyPathToClipboardButtonIconSuccess").style.display = 'inline';
+                                                    await new Promise(resolve => setTimeout(resolve, 3000)).then(function () {
+                                                        document.getElementById("copyPathToClipboardButtonIcon").style.display = 'inline';
+                                                        document.getElementById("copyPathToClipboardButtonIconSuccess").style.display = 'none';
+                                                    });
+                                                } catch (error) {
+                                                    window.prompt("Copy to clipboard: Ctrl+C", data);
+                                                }
+                                            }
+                                        </script>
+                                    @endif
                                 </td>
                             @endforeach
                         </tr>
