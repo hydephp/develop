44a88bd5f0d42558a3aedf823662b9bff3e5f175
Caen De Silva <caen@desilva.se>
2023-10-26 16:59:38 +0200

    Add title to button

--- packages/realtime-compiler/resources/dashboard.blade.php
+++ packages/realtime-compiler/resources/dashboard.blade.php
@@ -78,7 +78,7 @@
                                     <strong class="h6">{{ $type }}:</strong>
                                     <span class="card-text">{{ $info }}</span>
                                     @if($loop->last)
-                                        <button id="copyPathToClipboardButton" class="btn float-end px-1 py-0" onclick="copyPathToClipboard()">
+                                        <button id="copyPathToClipboardButton" class="btn float-end px-1 py-0" onclick="copyPathToClipboard()" title="Copy path to clipboard">
                                             <svg id="copyPathToClipboardButtonIcon" style="display: inline;" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-clipboard" viewBox="0 0 16 16">
                                                 <path d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1v-1z"/>
                                                 <path d="M9.5 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5h3zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3z"/>
