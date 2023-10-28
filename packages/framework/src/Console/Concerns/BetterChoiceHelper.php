320606eec8f8e97bba7b7ebcd0e969cd713224c3
Caen De Silva <caen@desilva.se>
2023-10-28 11:52:32 +0200

    Create BetterChoiceHelper.php

---
+++ packages/framework/src/Console/Concerns/BetterChoiceHelper.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hyde\Console\Concerns;
+
+/**
+ * @interal Adds a better choice helper for console commands.
+ */
+trait BetterChoiceHelper
+{
+    protected function betterChoice(): string
+    {
+        //
+    }
+}
