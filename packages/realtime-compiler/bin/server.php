7268f558c7cce8ec7cc2fc9760b216238acfee3c
Caen De Silva <caen@desilva.se>
2023-11-09 15:48:09 +0100

    Move down console writing to run after all server handlers

--- packages/realtime-compiler/bin/server.php
+++ packages/realtime-compiler/bin/server.php
@@ -10,16 +10,6 @@ try {
         $app = \Desilva\Microserve\Microserve::boot(\Hyde\RealtimeCompiler\Http\HttpKernel::class);
         $response = $app->handle(); // Process the request and create the response
         $response->send(); // Send the response to the client
-
-        // Write to console to emulate the built-in PHP server output
-        file_put_contents('php://stderr', sprintf(
-            "[%s] %s [%d]: %s %s\n",
-            date('D M j H:i:s Y'),
-            str_replace('::1', '[::1]', $_SERVER['REMOTE_ADDR'] ). ':' . $_SERVER['REMOTE_PORT'],
-            $response->statusCode,
-            \Desilva\Microserve\Request::capture()->method,
-            \Desilva\Microserve\Request::capture()->path,
-        ));
     } catch (Throwable $exception) {
         \Hyde\RealtimeCompiler\Http\ExceptionHandler::handle($exception)->send();
         exit($exception->getCode());
@@ -30,4 +20,15 @@ try {
     echo '<p>An error occurred that the core exception handler failed to process. Here\'s all we know:</p>';
     echo '<h2>Initial exception:</h2><pre>'.print_r($exception, true).'</pre>';
     echo '<h2>Auxiliary exception:</h2><pre>'.print_r($th, true).'</pre>';
+} finally {
+    // Write to console to emulate the standard built-in PHP server output
+    $request = \Desilva\Microserve\Request::capture();
+    file_put_contents('php://stderr', sprintf(
+        "[%s] %s [%d]: %s %s\n",
+        date('D M j H:i:s Y'),
+        str_replace('::1', '[::1]', $_SERVER['REMOTE_ADDR'] ). ':' . $_SERVER['REMOTE_PORT'],
+        $response->statusCode ?? 500,
+        $request->method,
+        $request->path,
+    ));
 }
