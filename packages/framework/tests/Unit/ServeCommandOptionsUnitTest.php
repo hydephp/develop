ef5863a976e4ca7e0c95cc1e7e7d56ecb78bccce
Caen De Silva <caen@desilva.se>
2023-11-11 13:43:10 +0100

    Create ServeCommandOptionsUnitTest.php

---
+++ packages/framework/tests/Unit/ServeCommandOptionsUnitTest.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hyde\Framework\Testing\Unit;
+
+use Hyde\Testing\TestCase;
+
+/**
+ * @covers \Hyde\Console\Commands\ServeCommand
+ * @see \Hyde\Framework\Testing\Feature\Commands\ServeCommandTest
+ */
+class ServeCommandOptionsUnitTest extends TestCase
+{
+    //
+}
