40d2cc0400d4310cf55677e3f17d34c9e3537a6e
Caen De Silva <caen@desilva.se>
2023-11-12 20:32:05 +0100

    Create BaseController.php

---
+++ packages/realtime-compiler/src/Http/BaseController.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hyde\RealtimeCompiler\Http;
+
+/**
+ * @internal This class is not intended to be edited outside the Hyde Realtime Compiler.
+ */
+abstract class BaseController
+{
+    //
+}
