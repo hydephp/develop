2d6647e52367af8d14640df0b37df0635b03e0fe
Caen De Silva <caen@desilva.se>
2023-11-13 18:52:27 +0100

    Handle HttpExceptions

--- packages/realtime-compiler/src/Http/LiveEditController.php
+++ packages/realtime-compiler/src/Http/LiveEditController.php
@@ -4,7 +4,8 @@ declare(strict_types=1);
 
 namespace Hyde\RealtimeCompiler\Http;
 
-use Desilva\Microserve\Response;
+use Desilva\Microserve\JsonResponse;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 
 /**
  * @internal This class is not intended to be edited outside the Hyde Realtime Compiler.
@@ -14,9 +15,24 @@ class LiveEditController extends BaseController
     protected bool $withConsoleOutput = true;
     protected bool $withSession = true;
 
-    public function handle(): Response
+    public function handle(): JsonResponse
     {
-        $this->authorizePostRequest();
+        try {
+            $this->authorizePostRequest();
+
+            return $this->handleRequest();
+        } catch (HttpException $exception) {
+            if ($this->expectsJson()) {
+                return $this->sendJsonErrorResponse($exception->getStatusCode(), $exception->getMessage());
+            }
+
+            throw $exception;
+        }
+    }
+
+    protected function handleRequest(): JsonResponse
+    {
+        //
     }
 
     public static function enabled(): bool
