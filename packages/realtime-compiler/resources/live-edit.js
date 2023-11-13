dd6ed11095f9a399f622d0701dfba5540eda46f6
Caen De Silva <caen@desilva.se>
2023-11-13 21:37:05 +0100

    Get the article element

--- packages/realtime-compiler/resources/live-edit.js
+++ packages/realtime-compiler/resources/live-edit.js
@@ -1,3 +1,14 @@
 function initLiveEdit() {
-    //
+    function getArticle() {
+        let article = document.querySelector('#content > article');
+
+        if (article === null) {
+            // If no article element is found the user may have a custom template, so we cannot know which element to edit.
+            throw new Error('No article element found, cannot live edit. If you are using a custom template, please make sure to include an article element in the #content container.');
+        }
+
+        return article;
+    }
+
+    const article = getArticle();
 }
