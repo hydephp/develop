da1263a2ce8e6987fef7619d4981cdb47793bd62
Caen De Silva <caen@desilva.se>
2023-11-13 18:40:03 +0100

    Create LiveEditController.php

---
+++ packages/realtime-compiler/src/Http/LiveEditController.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hyde\RealtimeCompiler\Http;
+
+use Desilva\Microserve\Response;
+
+/**
+ * @internal This class is not intended to be edited outside the Hyde Realtime Compiler.
+ */
+class LiveEditController extends BaseController
+{
+    protected bool $withConsoleOutput = true;
+    protected bool $withSession = true;
+
+    public function handle(): Response
+    {
+        // TODO: Implement handle() method.
+    }
+}
