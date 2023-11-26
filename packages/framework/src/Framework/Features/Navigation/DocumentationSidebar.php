300694ce961cef421e36ac68bc3c421f868c9cbb
Caen De Silva <caen@desilva.se>
2023-11-26 16:52:39 +0100

    Specify group

--- packages/framework/src/Framework/Features/Navigation/DocumentationSidebar.php
+++ packages/framework/src/Framework/Features/Navigation/DocumentationSidebar.php
@@ -25,7 +25,7 @@ class DocumentationSidebar extends BaseNavigationMenu
 
         // If there are no pages other than the index page, we add it to the sidebar so that it's not empty
         if ($this->items->count() === 0 && DocumentationPage::home() !== null) {
-            $this->items->push(NavItem::fromRoute(DocumentationPage::home()));
+            $this->items->push(NavItem::fromRoute(DocumentationPage::home(), group: 'other'));
         }
     }
 
