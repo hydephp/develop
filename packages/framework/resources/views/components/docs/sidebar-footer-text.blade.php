4376aae26557366ac2f4667b9017c4096da02787
Caen De Silva <caen@desilva.se>
2023-11-26 20:01:26 +0100

    Revert "Add sidebar footer text configuration option"
    This reverts commit ed19b749143f4fab6a3177bdcc1e21ad377edded. See https://github.com/hydephp/develop/issues/1474#issuecomment-1826841417

--- packages/framework/resources/views/components/docs/sidebar-footer-text.blade.php
+++ packages/framework/resources/views/components/docs/sidebar-footer-text.blade.php
@@ -1,7 +1,3 @@
 <p>
-    @if(config('docs.sidebar.footer_text', true))
-        {{ Hyde::markdown(config('docs.sidebar.footer_text')) }}
-    @else
-        <a href="{{ Hyde::relativeLink('index.html') }}">Back to home page</a>
-    @endif
+    <a href="{{ Hyde::relativeLink('index.html') }}">Back to home page</a>
 </p>
