0838ab4f25f75299f28495dcb9191e03a1ea0f3a
Caen De Silva <caen@desilva.se>
2023-12-15 15:10:29 +0100

    Remove internal `DocumentationSearchPage::routeKey()` method
    This is now no longer needed thanks to https://github.com/hydephp/develop/pull/1498, but is kept as protected so the `enabled` method can use it

--- packages/framework/tests/Feature/DocumentationSearchPageTest.php
+++ packages/framework/tests/Feature/DocumentationSearchPageTest.php
@@ -59,15 +59,4 @@ class DocumentationSearchPageTest extends TestCase
         Hyde::routes()->put('docs/search', new InMemoryPage('docs/search'));
         $this->assertFalse(DocumentationSearchPage::enabled());
     }
-
-    public function testStaticRouteKeyHelper()
-    {
-        $this->assertSame('docs/search', DocumentationSearchPage::routeKey());
-    }
-
-    public function testStaticRouteKeyHelperWithCustomOutputDirectory()
-    {
-        DocumentationPage::setOutputDirectory('foo');
-        $this->assertSame('foo/search', DocumentationSearchPage::routeKey());
-    }
 }
