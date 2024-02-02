c2aced97cedbc7a4894e35c435261d702835dd84
Caen De Silva <caen@desilva.se>
2024-02-02 20:56:35 +0100

    Revert "Update newsletter-signup-example.blade.php"
    This reverts commit 1257e3088f32c58698e6bd1e03861cf626d17a6d.

--- packages/ui-kit/docs/_pages/examples/newsletter-signup-example.blade.php
+++ packages/ui-kit/docs/_pages/examples/newsletter-signup-example.blade.php
@@ -1,65 +1,54 @@
-<!DOCTYPE html>
-<html lang="{{ config('site.language', 'en') }}">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>{{ $page->title() }}</title>
-    @include('hyde::layouts.styles')
-</head>
-<body id="app" class="flex flex-col min-h-screen overflow-x-hidden dark:bg-gray-900 dark:text-white">
-    <main id="content" class="mx-auto max-w-7xl py-16 px-8">
-        <div class="flex">
-            <div class="light">
-                <x-hyde-ui::card class="mx-auto">
-                    <x-slot name="title">
-                        Let your creativity flow!
-                    </x-slot>
-                 
-                    <x-slot name="main" style="padding-top: 0; padding-bottom: 0;">
-                        <x-hyde-ui::prose>
-                            <x-hyde-ui::markdown>
-                                The UI kit is minimal by design. It's up to **you** to create something _amazing_.
-                 
-                                Maybe create a form to collect newsletter subscriptions?
-                            </x-hyde-ui::markdown>
-                        </x-hyde-ui::prose>
-                    </x-slot>
-                 
-                    <x-slot name="footer" class="flex text-center justify-center">
-                        <x-hyde-ui::input placeholder="Enter email" />
-                 
-                        <x-hyde-ui::button-primary>
-                            Subscribe
-                        </x-hyde-ui::button-primary>
-                    </x-slot>
-                </x-hyde-ui::card>
-            </div>
-            <div class="dark">
-                <x-hyde-ui::card class="mx-auto">
-                    <x-slot name="title">
-                        Let your creativity flow!
-                    </x-slot>
-                 
-                    <x-slot name="main" style="padding-top: 0; padding-bottom: 0;">
-                        <x-hyde-ui::prose>
-                            <x-hyde-ui::markdown>
-                                The UI kit is minimal by design. It's up to **you** to create something _amazing_.
-                 
-                                Maybe create a form to collect newsletter subscriptions?
-                            </x-hyde-ui::markdown>
-                        </x-hyde-ui::prose>
-                    </x-slot>
-                 
-                    <x-slot name="footer" class="flex text-center justify-center">
-                        <x-hyde-ui::input placeholder="Enter email" />
-                 
-                        <x-hyde-ui::button-primary>
-                            Subscribe
-                        </x-hyde-ui::button-primary>
-                    </x-slot>
-                </x-hyde-ui::card>
-            </div>
+<x-hyde-ui::layouts.focus>
+    <div class="flex">
+        <div class="light">
+            <x-hyde-ui::card class="mx-auto">
+                <x-slot name="title">
+                    Let your creativity flow!
+                </x-slot>
+             
+                <x-slot name="main" style="padding-top: 0; padding-bottom: 0;">
+                    <x-hyde-ui::prose>
+                        <x-hyde-ui::markdown>
+                            The UI kit is minimal by design. It's up to **you** to create something _amazing_.
+             
+                            Maybe create a form to collect newsletter subscriptions?
+                        </x-hyde-ui::markdown>
+                    </x-hyde-ui::prose>
+                </x-slot>
+             
+                <x-slot name="footer" class="flex text-center justify-center">
+                    <x-hyde-ui::input placeholder="Enter email" />
+             
+                    <x-hyde-ui::button-primary>
+                        Subscribe
+                    </x-hyde-ui::button-primary>
+                </x-slot>
+            </x-hyde-ui::card>
         </div>
-    </main>
-</body>
-</html>
+        <div class="dark">
+            <x-hyde-ui::card class="mx-auto">
+                <x-slot name="title">
+                    Let your creativity flow!
+                </x-slot>
+             
+                <x-slot name="main" style="padding-top: 0; padding-bottom: 0;">
+                    <x-hyde-ui::prose>
+                        <x-hyde-ui::markdown>
+                            The UI kit is minimal by design. It's up to **you** to create something _amazing_.
+             
+                            Maybe create a form to collect newsletter subscriptions?
+                        </x-hyde-ui::markdown>
+                    </x-hyde-ui::prose>
+                </x-slot>
+             
+                <x-slot name="footer" class="flex text-center justify-center">
+                    <x-hyde-ui::input placeholder="Enter email" />
+             
+                    <x-hyde-ui::button-primary>
+                        Subscribe
+                    </x-hyde-ui::button-primary>
+                </x-slot>
+            </x-hyde-ui::card>
+        </div>
+    </div>
+</x-hyde-ui::layouts.focus>
