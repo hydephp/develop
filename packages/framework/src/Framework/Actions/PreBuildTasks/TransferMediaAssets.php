cfb9148e8d5feefc97935b628e95f4cb16e3e714
Caen De Silva <caen@desilva.se>
2024-02-08 12:06:57 +0100

    Move media asset transfer logic to build task

--- packages/framework/src/Framework/Actions/PreBuildTasks/TransferMediaAssets.php
+++ packages/framework/src/Framework/Actions/PreBuildTasks/TransferMediaAssets.php
@@ -4,9 +4,25 @@ declare(strict_types=1);
 
 namespace Hyde\Framework\Actions\PreBuildTasks;
 
+use Hyde\Hyde;
+use Hyde\Support\Filesystem\MediaFile;
 use Hyde\Framework\Features\BuildTasks\PreBuildTask;
+use Hyde\Framework\Concerns\InteractsWithDirectories;
 
 class TransferMediaAssets extends PreBuildTask
 {
     protected static string $message = 'Transferring Media Assets';
+
+    use InteractsWithDirectories;
+
+    public function handle(): void
+    {
+        $this->needsDirectory(Hyde::siteMediaPath());
+
+        $this->withProgressBar(MediaFile::files(), function (string $identifier): void {
+            $sitePath = Hyde::siteMediaPath($identifier);
+            $this->needsParentDirectory($sitePath);
+            copy(Hyde::mediaPath($identifier), $sitePath);
+        });
+    }
 }
