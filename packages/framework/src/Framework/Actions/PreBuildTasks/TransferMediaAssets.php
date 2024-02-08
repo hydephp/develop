ac269982ab9051b54822bbc14336a2d445739958
Caen De Silva <caen@desilva.se>
2024-02-08 12:01:46 +0100

    Create TransferMediaAssets.php

---
+++ packages/framework/src/Framework/Actions/PreBuildTasks/TransferMediaAssets.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hyde\Framework\Actions\PreBuildTasks;
+
+use Hyde\Framework\Features\BuildTasks\PreBuildTask;
+
+class TransferMediaAssets extends PreBuildTask
+{
+    //
+}
