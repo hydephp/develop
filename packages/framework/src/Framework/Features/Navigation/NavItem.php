7fc22d9deea7747688469185525bd8d6100adf7b
Caen De Silva <caen@desilva.se>
2024-02-08 18:58:53 +0100

    Compare current state as string

--- packages/framework/src/Framework/Features/Navigation/NavItem.php
+++ packages/framework/src/Framework/Features/Navigation/NavItem.php
@@ -130,7 +130,7 @@ class NavItem implements Stringable
      */
     public function isCurrent(): bool
     {
-        return Hyde::currentRoute()->getLink() === $this->destination;
+        return Hyde::currentRoute()->getLink() === (string) $this->destination;
     }
 
     protected static function getRouteGroup(Route $route): ?string
