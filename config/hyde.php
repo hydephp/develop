060719a621bc50685dbeb7c282af093e9acb77bf
Caen De Silva <caen@desilva.se>
2024-02-09 11:48:03 +0100

    Sync config files

--- config/hyde.php
+++ config/hyde.php
@@ -226,6 +226,25 @@ return [
 
     /*
     |--------------------------------------------------------------------------
+    | Custom head and body HTML hooks
+    |--------------------------------------------------------------------------
+    |
+    | While the best way to add custom `<head>` and `<body>` code is to use the
+    | Blade components, you can also add them here. This is useful for adding
+    | scripts like analytics codes, chat widgets, or even custom styles.
+    |
+    */
+
+    'hooks' => [
+        // Add any extra HTML to include in the <head> tag
+        'head' => '',
+
+        // Add any extra HTML to include before the closing <body> tag
+        'body' => '',
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
     | Features
     |--------------------------------------------------------------------------
     |
