0fb3a265990c8cd751069860989b6399bcbff592
Caen De Silva <caen@desilva.se>
2024-02-09 11:49:34 +0100

    Move head render hooks to be last in head component
    Since these may contain overrides we want them last

--- packages/framework/resources/views/layouts/meta.blade.php
+++ packages/framework/resources/views/layouts/meta.blade.php
@@ -4,8 +4,5 @@
 {{-- Render the global and config defined meta tags --}}
 {{ Site::metadata() }}
 
-{{-- If the user has defined any custom head tags, render them here --}}
-{!! config('hyde.hooks.head') !!}
-
 {{-- Add any extra tags to include in the <head> section --}}
 @stack('meta')
