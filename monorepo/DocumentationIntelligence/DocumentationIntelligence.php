5a8223ff6f18c5f78bec8811d9605e86e254ad8c
Caen De Silva <caen@desilva.se>
2024-02-10 20:45:11 +0100

    Annotate type

--- monorepo/DocumentationIntelligence/DocumentationIntelligence.php
+++ monorepo/DocumentationIntelligence/DocumentationIntelligence.php
@@ -8,6 +8,7 @@ require_once __DIR__.'/../../packages/hydefront/.github/scripts/minima.php';
 define('TIME_START', microtime(true));
 
 Command::main(function () {
+    /** @var Command $this */
     $this->info('Generating documentation intelligence...');
     $this->info('Time taken: '.round((microtime(true) - TIME_START) * 1000, 2).'ms');
     return 0;
