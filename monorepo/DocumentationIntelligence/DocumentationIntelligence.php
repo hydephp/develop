5445730c00228eff7c4c332617fab9edbb429bf9
Caen De Silva <caen@desilva.se>
2024-02-10 21:18:05 +0100

    Define output path

--- monorepo/DocumentationIntelligence/DocumentationIntelligence.php
+++ monorepo/DocumentationIntelligence/DocumentationIntelligence.php
@@ -14,6 +14,7 @@ require_once __DIR__.'/../../packages/hydefront/.github/scripts/minima.php';
 
 define('TIME_START', microtime(true));
 define('BASE_PATH', realpath(__DIR__.'/../../'));
+define('OUTPUT_PATH', __DIR__.'/results');
 
 Command::main(function () {
     /** @var Command $this */
