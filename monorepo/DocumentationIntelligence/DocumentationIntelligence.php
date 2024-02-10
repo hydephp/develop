79fee61a692dc152257937743d0b79a9b65879a0
Caen De Silva <caen@desilva.se>
2024-02-10 20:33:30 +0100

    Import libraries

--- monorepo/DocumentationIntelligence/DocumentationIntelligence.php
+++ monorepo/DocumentationIntelligence/DocumentationIntelligence.php
@@ -4,6 +4,11 @@ declare(strict_types=1);
 
 namespace DocumentationIntelligence;
 
+require_once __DIR__.'/../../vendor/autoload.php';
+require_once __DIR__.'/../../packages/hydefront/.github/scripts/minima.php';
+
+define('TIME_START', microtime(true));
+
 /**
  * @internal
  */
