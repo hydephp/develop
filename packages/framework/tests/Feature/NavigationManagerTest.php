79b82d506b129add9c28b36c96df48c950b8a499
Caen De Silva <caen@desilva.se>
2024-02-13 17:20:53 +0100

    Copilot generate tests

--- packages/framework/tests/Feature/NavigationManagerTest.php
+++ packages/framework/tests/Feature/NavigationManagerTest.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Hyde\Framework\Testing\Feature;
 
+use Hyde\Framework\Features\Navigation\NavigationManager;
+use Hyde\Framework\Features\Navigation\BaseNavigationMenu;
 use Hyde\Testing\TestCase;
 
 /**
@@ -11,13 +13,45 @@ use Hyde\Testing\TestCase;
  */
 class NavigationManagerTest extends TestCase
 {
-    public function testRegisterMenu()
+    protected NavigationManager $navigationManager;
+
+    protected function setUp(): void
     {
-        // Todo
+        parent::setUp();
+        $this->navigationManager = new NavigationManager();
     }
 
+    /** @author Copilot */
+    public function testRegisterMenu()
+    {
+        $menu = $this->createMock(BaseNavigationMenu::class);
+        $this->navigationManager->registerMenu('test', $menu);
+
+        $reflection = new \ReflectionClass($this->navigationManager);
+        $property = $reflection->getProperty('menus');
+        $property->setAccessible(true);
+
+        $menus = $property->getValue($this->navigationManager);
+
+        $this->assertArrayHasKey('test', $menus);
+        $this->assertSame($menu, $menus['test']);
+    }
+
+    /** @author Copilot */
     public function testGetMenu()
     {
-        // Todo
+        $menu = $this->createMock(BaseNavigationMenu::class);
+        $this->navigationManager->registerMenu('test', $menu);
+
+        $retrievedMenu = $this->navigationManager->getMenu('test');
+
+        $this->assertSame($menu, $retrievedMenu);
+    }
+
+    /** @author Copilot */
+    public function testGetMenuThrowsExceptionForNonExistentMenu()
+    {
+        $this->expectException(\Exception::class);
+        $this->navigationManager->getMenu('nonexistent');
     }
 }
