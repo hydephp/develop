786bdf68203340e588f6b6bcf4da3ed7e7930fa8
Caen De Silva <caen@desilva.se>
2024-02-13 19:21:01 +0100

    Register NavigationManager as a singleton

--- packages/framework/src/Foundation/Providers/NavigationServiceProvider.php
+++ packages/framework/src/Foundation/Providers/NavigationServiceProvider.php
@@ -5,8 +5,14 @@ declare(strict_types=1);
 namespace Hyde\Foundation\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Hyde\Framework\Features\Navigation\NavigationManager;
 
 class NavigationServiceProvider extends ServiceProvider
 {
-    //
+    public function register(): void
+    {
+        $this->app->singleton(NavigationManager::class, function ($app) {
+            return new NavigationManager();
+        });
+    }
 }
