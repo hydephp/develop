e658b6383eaf4535c17a9dd9fbe6973f9a9f453a
Caen De Silva <caen@desilva.se>
2024-02-13 18:47:47 +0100

    Add protected items collection

--- packages/framework/src/Framework/Features/Navigation/NavigationMenu.php
+++ packages/framework/src/Framework/Features/Navigation/NavigationMenu.php
@@ -4,7 +4,10 @@ declare(strict_types=1);
 
 namespace Hyde\Framework\Features\Navigation;
 
+use Illuminate\Support\Collection;
+
 class NavigationMenu
 {
-    //
+    /** @var \Illuminate\Support\Collection<string, \Hyde\Framework\Features\Navigation\NavItem> */
+    protected Collection $items;
 }
