ed6796411e3c39b0c0d837a4e4a0504ab533d8f6
Caen De Silva <caen@desilva.se>
2024-02-13 20:06:35 +0100

    Update constructor to support arrayables

--- packages/framework/src/Framework/Features/Navigation/NavigationMenu.php
+++ packages/framework/src/Framework/Features/Navigation/NavigationMenu.php
@@ -5,14 +5,15 @@ declare(strict_types=1);
 namespace Hyde\Framework\Features\Navigation;
 
 use Illuminate\Support\Collection;
+use Illuminate\Contracts\Support\Arrayable;
 
 class NavigationMenu
 {
     /** @var \Illuminate\Support\Collection<string, \Hyde\Framework\Features\Navigation\NavItem> */
     protected Collection $items;
 
-    public function __construct(Collection $items)
+    public function __construct(Arrayable|array $items = [])
     {
-        $this->items = $items;
+        $this->items = new Collection($items);
     }
 }
