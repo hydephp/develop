850de857bed3ae354f97400d37fdad24d7c98e94
Caen De Silva <caen@desilva.se>
2024-02-14 14:32:20 +0100

    Copy temporary parent methods

--- packages/framework/src/Framework/Features/Navigation/GeneratesMainNavigationMenu.php
+++ packages/framework/src/Framework/Features/Navigation/GeneratesMainNavigationMenu.php
@@ -4,6 +4,11 @@ declare(strict_types=1);
 
 namespace Hyde\Framework\Features\Navigation;
 
+use Hyde\Facades\Config;
+use Hyde\Support\Models\Route;
+use Hyde\Foundation\Facades\Routes;
+use function collect;
+
 /**
  * @experimental This class may change significantly before its release.
  *
@@ -16,5 +21,37 @@ class GeneratesMainNavigationMenu
         $navigation = \Hyde\Framework\Features\Navigation\MainNavigationMenu::temp__generate();
 
         return new NavigationMenu($navigation->items);
+    }
+
+    protected function parent__generate(): void
+    {
+        Routes::each(function (Route $route): void {
+            if ($this->canAddRoute($route)) {
+                $this->items->put($route->getRouteKey(), NavItem::fromRoute($route));
+            }
+        });
+
+        collect(Config::getArray('hyde.navigation.custom', []))->each(function (NavItem $item): void {
+            // Since these were added explicitly by the user, we can assume they should always be shown
+            $this->items->push($item);
+        });
+    }
+
+    protected function parent__canAddRoute(Route $route): bool
+    {
+        return $route->getPage()->showInNavigation();
+    }
+
+    protected function parent__removeDuplicateItems(): void
+    {
+        $this->items = $this->items->unique(function (NavItem $item): string {
+            // Filter using a combination of the group and label to allow duplicate labels in different groups
+            return $item->getGroup().$item->label;
+        });
+    }
+
+    protected function parent__sortByPriority(): void
+    {
+        $this->items = $this->items->sortBy('priority')->values();
     }
 }
