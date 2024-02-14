abd5c186d83963d574d4bba869bfb0d0b99efb97
Caen De Silva <caen@desilva.se>
2024-02-14 21:18:04 +0100

    Test construction

--- packages/framework/tests/Unit/NavigationMenuUnitTest.php
+++ packages/framework/tests/Unit/NavigationMenuUnitTest.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Hyde\Framework\Testing\Unit;
 
 use Hyde\Testing\UnitTestCase;
+use Hyde\Framework\Features\Navigation\NavigationMenu;
 
 /**
  * @covers \Hyde\Framework\Features\Navigation\NavigationMenu
@@ -13,5 +14,24 @@ use Hyde\Testing\UnitTestCase;
  */
 class NavigationMenuUnitTest extends UnitTestCase
 {
-    //
+    public function testCanConstruct()
+    {
+        $this->assertInstanceOf(NavigationMenu::class, new NavigationMenu());
+    }
+
+    public function testCanConstructWithItemsArray()
+    {
+        $this->assertInstanceOf(NavigationMenu::class, new NavigationMenu([
+            'item1' => 'value1',
+            'item2' => 'value2',
+        ]));
+    }
+
+    public function testCanConstructWithItemsArrayable()
+    {
+        $this->assertInstanceOf(NavigationMenu::class, new NavigationMenu(collect([
+            'item1' => 'value1',
+            'item2' => 'value2',
+        ])));
+    }
 }
