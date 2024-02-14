78d7570060718c5bca1428e4464aaf630d66ec6a
Caen De Silva <caen@desilva.se>
2024-02-14 21:03:32 +0100

    Remove NavigationMenu dropdown accessor methods
    These do not seem to be used outside of tests and don't offer anything useful

--- packages/framework/src/Framework/Features/Navigation/NavigationMenu.php
+++ packages/framework/src/Framework/Features/Navigation/NavigationMenu.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Hyde\Framework\Features\Navigation;
 
-use Hyde\Facades\Config;
 use Illuminate\Support\Collection;
 use Illuminate\Contracts\Support\Arrayable;
 
@@ -22,34 +21,5 @@ class NavigationMenu
     public function getItems(): Collection
     {
         return $this->items;
-    }
-
-    /**
-     * @experimental These do not seem to be used outside of tests and may be removed
-     *
-     * @deprecated If kept, they should be renamed to be more generic.
-     */
-    public function legacy_hasDropdowns(): bool
-    {
-        return $this->dropdownsEnabled() && count($this->getDropdowns()) >= 1;
-    }
-
-    /**
-     * @experimental These do not seem to be used outside of tests and may be removed
-     *
-     * @deprecated If kept, they should be renamed to be more generic.
-     *
-     * @return array<string, DropdownNavItem>
-     */
-    public function legacy_getDropdowns(): array
-    {
-        return $this->items->filter(function (NavItem $item): bool {
-            return $item instanceof DropdownNavItem;
-        })->values()->all();
-    }
-
-    protected function dropdownsEnabled(): bool
-    {
-        return Config::getString('hyde.navigation.subdirectories', 'hidden') === 'dropdown';
     }
 }
