7c8ea3b86464095a594ce3e45c4feaa9e82f2964
Caen De Silva <caen@desilva.se>
2024-02-14 21:09:38 +0100

    Create NavigationMenuUnitTest.php

---
+++ packages/framework/tests/Unit/NavigationMenuUnitTest.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hyde\Framework\Testing\Unit;
+
+use Hyde\Testing\UnitTestCase;
+
+/**
+ * @covers \Hyde\Framework\Features\Navigation\NavigationMenu
+ *
+ * @see \Hyde\Framework\Testing\Feature\NavigationMenuTest
+ */
+class NavigationMenuUnitTest extends UnitTestCase
+{
+    //
+}
