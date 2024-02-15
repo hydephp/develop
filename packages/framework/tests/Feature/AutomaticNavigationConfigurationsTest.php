67a98047d6b7bed1241adee5874a480adc45054e
Caen De Silva <caen@desilva.se>
2024-02-15 16:32:07 +0100

    Create AutomaticNavigationConfigurationsTest.php

---
+++ packages/framework/tests/Feature/AutomaticNavigationConfigurationsTest.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hyde\Framework\Testing\Feature;
+
+use Hyde\Testing\TestCase;
+
+/**
+ * High-level broad-spectrum tests for the automatic navigation configurations, testing various setups.
+ *
+ * @covers \Hyde\Framework\Factories\NavigationDataFactory
+ * @covers \Hyde\Framework\Features\Navigation\DocumentationSidebar
+ * @covers \Hyde\Framework\Features\Navigation\GeneratesDocumentationSidebarMenu
+ * @covers \Hyde\Framework\Features\Navigation\GeneratesMainNavigationMenu
+ * @covers \Hyde\Framework\Features\Navigation\NavItem
+ */
+class AutomaticNavigationConfigurationsTest extends TestCase
+{
+    //
+}
