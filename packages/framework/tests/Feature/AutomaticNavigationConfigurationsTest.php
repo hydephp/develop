9c02dcf24e11c1709c64d576a46b0900b0da221a
Caen De Silva <caen@desilva.se>
2024-02-15 20:35:35 +0100

    Test group conflict precedence

--- packages/framework/tests/Feature/AutomaticNavigationConfigurationsTest.php
+++ packages/framework/tests/Feature/AutomaticNavigationConfigurationsTest.php
@@ -211,6 +211,21 @@ class AutomaticNavigationConfigurationsTest extends TestCase
         ]);
     }
 
+    public function testMainNavigationMenuWithFrontMatterGroupAndCategory()
+    {
+        // Since the main key in the navigation schema is 'group', that takes precedence over its 'category' alias
+
+        $this->assertMenuEquals([
+            ['label' => 'Foo', 'group' => 'group-1'],
+            ['label' => 'Bar', 'group' => 'group-1'],
+            ['label' => 'Baz', 'group' => 'group-1'],
+        ], [
+            new MarkdownPage('foo', ['navigation.group' => 'Group 1', 'navigation.category' => 'Group 2']),
+            new MarkdownPage('bar', ['navigation.group' => 'Group 1', 'navigation.category' => 'Group 2']),
+            new MarkdownPage('baz', ['navigation.group' => 'Group 1', 'navigation.category' => 'Group 2']),
+        ]);
+    }
+
     protected function assertMenuEquals(array $expected, array $menuPages): void
     {
         $this->menu($menuPages)->assertEquals($expected);
