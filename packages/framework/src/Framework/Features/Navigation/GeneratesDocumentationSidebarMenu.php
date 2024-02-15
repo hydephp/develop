2a21f77e02e4462fdaaacf334dc46bbab6f889a3
Caen De Silva <caen@desilva.se>
2024-02-15 13:08:47 +0100

    Add design annotations

--- packages/framework/src/Framework/Features/Navigation/GeneratesDocumentationSidebarMenu.php
+++ packages/framework/src/Framework/Features/Navigation/GeneratesDocumentationSidebarMenu.php
@@ -4,6 +4,11 @@ declare(strict_types=1);
 
 namespace Hyde\Framework\Features\Navigation;
 
+/**
+ * @experimental This class may change significantly before its release.
+ *
+ * @todo Consider making into a service which can create the sidebar as well.
+ */
 class GeneratesDocumentationSidebarMenu
 {
     //
