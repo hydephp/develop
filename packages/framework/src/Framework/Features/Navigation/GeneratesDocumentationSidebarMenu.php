80df7fffdabf6fae2d4c2e912654e0e5fea93dd4
Caen De Silva <caen@desilva.se>
2024-02-15 13:10:09 +0100

    Copy over helper methods for refactor

--- packages/framework/src/Framework/Features/Navigation/GeneratesDocumentationSidebarMenu.php
+++ packages/framework/src/Framework/Features/Navigation/GeneratesDocumentationSidebarMenu.php
@@ -4,7 +4,12 @@ declare(strict_types=1);
 
 namespace Hyde\Framework\Features\Navigation;
 
+use Hyde\Support\Models\Route;
+use Hyde\Support\Facades\Render;
+use Hyde\Pages\DocumentationPage;
 use Illuminate\Support\Collection;
+
+use function collect;
 
 /**
  * @experimental This class may change significantly before its release.
@@ -30,5 +35,33 @@ class GeneratesDocumentationSidebarMenu
         $menu->removeDuplicateItems();
 
         return new DocumentationSidebar($menu->items);
+    }
+
+    protected function canAddRoute(Route $route): bool
+    {
+        return $route->getPage()->showInNavigation() && ! $route->is(DocumentationPage::homeRouteName());
+    }
+
+    protected function removeDuplicateItems(): void
+    {
+        $this->items = $this->items->unique(function (NavItem $item): string {
+            // Filter using a combination of the group and label to allow duplicate labels in different groups
+            return $item->getGroup().$item->label;
+        });
+    }
+
+    protected function sortByPriority(): void
+    {
+        $this->items = $this->items->sortBy('priority')->values();
+    }
+
+    private function isPageIndexPage(): bool
+    {
+        return Render::getPage()->getRoute()->is(DocumentationPage::homeRouteName());
+    }
+
+    private function shouldIndexPageBeActive(string $group): bool
+    {
+        return Render::getPage()->navigationMenuGroup() === 'other' && $group === collect($this->getGroups())->first();
     }
 }
