206a57f98281156c19f7adc24d5ded8de5825c40
Caen De Silva <caen@desilva.se>
2024-02-15 14:58:00 +0100

    Add todo

--- packages/framework/src/Framework/Factories/NavigationDataFactory.php
+++ packages/framework/src/Framework/Factories/NavigationDataFactory.php
@@ -210,6 +210,7 @@ class NavigationDataFactory extends Concerns\PageDataFactory implements Navigati
 
     private function defaultGroup(): ?string
     {
+        // TODO: It would be better if this was null in all cases, considering 'other' is used a a sort of confusing pseudo-null
         return $this->isInstanceOf(DocumentationPage::class) ? 'other' : null;
     }
 
