ddc9ef6f2dca39aa2fddd8a684a4cf91d56f3fab
Caen De Silva <caen@desilva.se>
2024-02-16 13:50:07 +0100

    Create base routing system

--- _admin/index.php
+++ _admin/index.php
@@ -15,3 +15,37 @@ declare(strict_types=1);
  *
  * Next, visit http://localhost:3000/admin in your web browser.
  */
+
+(new AdminRouter())->handle($_SERVER['REQUEST_URI'] ?? '/');
+
+class AdminRouter
+{
+    const ROUTES = [
+        '/' => [AdminController::class, 'redirectToIndex'],
+        '/admin' => [AdminController::class, 'index'],
+    ];
+
+    public function handle(string $uri): void
+    {
+        if (isset(self::ROUTES[$uri])) {
+            [$controller, $method] = self::ROUTES[$uri];
+
+            echo (new $controller())->$method();
+        } else {
+            exit(http_response_code(404));
+        }
+    }
+}
+
+class AdminController
+{
+    public function redirectToIndex(): void
+    {
+        header('Location: /admin');
+    }
+
+    public function index(): string
+    {
+        return file_get_contents(__DIR__ . '/index.html');
+    }
+}
