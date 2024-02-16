ede3105c808ff06372e7cb52e17ffe37f1ec96bb
Caen De Silva <caen@desilva.se>
2024-02-16 13:44:10 +0100

    Add base router documentation

--- _admin/index.php
+++ _admin/index.php
@@ -1,1 +1,12 @@
 <?php
+
+/**
+ * The HydePHP CMS Admin Panel Router
+ *
+ * This file is the main entry point for the HydePHP CMS Admin Panel.
+ *
+ * To start the admin server, run the following command from your project root:
+ * > php -S localhost:3000 /_admin/index.php # The port can be changed
+ *
+ * Next, visit http://localhost:3000/admin in your web browser.
+ */
