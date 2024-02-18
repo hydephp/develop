960415b72ef6991292190ef4ba93dc5959459c46
Caen De Silva <caen@desilva.se>
2024-02-18 21:54:56 +0100

    Remove resolved todo

--- packages/framework/src/Framework/Factories/NavigationDataFactory.php
+++ packages/framework/src/Framework/Factories/NavigationDataFactory.php
@@ -210,7 +210,6 @@ class NavigationDataFactory extends Concerns\PageDataFactory implements Navigati
 
     private function defaultGroup(): ?string
     {
-        // TODO: It would be better if this was null in all cases, considering 'other' is used a a sort of confusing pseudo-null
         return $this->isInstanceOf(DocumentationPage::class) ? null : null;
     }
 
