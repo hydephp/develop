0562e13df0f4fc8b4854202e9849ca67108308fa
Caen De Silva <caen@desilva.se>
2024-02-19 22:54:18 +0100

    Refactor view to use dynamic class directives

--- packages/framework/resources/views/components/docs/sidebar-items.blade.php
+++ packages/framework/resources/views/components/docs/sidebar-items.blade.php
@@ -10,8 +10,8 @@
         @php ($collapsible = config('docs.sidebar.collapsible', true))
         @foreach ($sidebar->getItems() as $group)
             <li class="sidebar-group" role="listitem" @if($collapsible) x-data="{ groupOpen: {{ $sidebar->isGroupActive($group->getIdentifier()) ? 'true' : 'false' }} }" @endif>
-                <header class="sidebar-group-header p-2 px-4 -ml-2 flex justify-between items-center @if($collapsible) group hover:bg-black/10 @endif" @if($collapsible) @click="groupOpen = ! groupOpen" @endif>
-                    <h4 class="sidebar-group-heading text-base font-semibold @if($collapsible) cursor-pointer dark:group-hover:text-white @endif">{{ $group->getLabel() }}</h4>
+                <header @class(['sidebar-group-header p-2 px-4 -ml-2 flex justify-between items-center', 'group hover:bg-black/10' => $collapsible]) @if($collapsible) @click="groupOpen = ! groupOpen" @endif>
+                    <h4 @class(['sidebar-group-heading text-base font-semibold', 'cursor-pointer dark:group-hover:text-white' => $collapsible])>{{ $group->getLabel() }}</h4>
                     @if($collapsible)
                         @include('hyde::components.docs.sidebar-group-toggle-button')
                     @endif
