2235f56164e5b5bba66882c5648f1a4dc12f64f3
Caen De Silva <caen@desilva.se>
2024-02-19 20:27:33 +0100

    Update sidebar view to use new navigation item format

--- packages/framework/resources/views/components/docs/sidebar-items.blade.php
+++ packages/framework/resources/views/components/docs/sidebar-items.blade.php
@@ -11,13 +11,13 @@
         @foreach ($sidebar->getItems() as $group)
             <li class="sidebar-group" role="listitem" @if($collapsible) x-data="{ groupOpen: {{ $sidebar->isGroupActive($group) ? 'true' : 'false' }} }" @endif>
                 <header class="sidebar-group-header p-2 px-4 -ml-2 flex justify-between items-center @if($collapsible) group hover:bg-black/10 @endif" @if($collapsible) @click="groupOpen = ! groupOpen" @endif>
-                    <h4 class="sidebar-group-heading text-base font-semibold @if($collapsible) cursor-pointer dark:group-hover:text-white @endif">{{ $sidebar->makeGroupTitle($group) }}</h4>
+                    <h4 class="sidebar-group-heading text-base font-semibold @if($collapsible) cursor-pointer dark:group-hover:text-white @endif">{{ Hyde::makeTitle($group->getLabel()) }}</h4>
                     @if($collapsible)
                         @include('hyde::components.docs.sidebar-group-toggle-button')
                     @endif
                 </header>
                 <ul class="sidebar-group-items ml-4 px-2 mb-2" role="list" @if($collapsible) x-show="groupOpen" @endif>
-                    @foreach ($sidebar->getItemsInGroup($group) as $item)
+                    @foreach ($group->getChildren() as $item)
                         @include('hyde::components.docs.sidebar-item', ['grouped' => true])
                     @endforeach
                 </ul>
