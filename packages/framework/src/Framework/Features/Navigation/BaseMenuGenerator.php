eef96d7b8af58b38300f4943bd97a7d5b90434a3
Caen De Silva <caen@desilva.se>
2024-02-21 14:48:26 +0100

    Mark class as experimental

--- packages/framework/src/Framework/Features/Navigation/BaseMenuGenerator.php
+++ packages/framework/src/Framework/Features/Navigation/BaseMenuGenerator.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace Hyde\Framework\Features\Navigation;
 
+/**
+ * @experimental This class may change significantly before its release.
+ */
 abstract class BaseMenuGenerator
 {
     //
