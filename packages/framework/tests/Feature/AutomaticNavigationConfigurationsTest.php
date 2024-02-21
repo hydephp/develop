ca328ccd00f7282d7c02cd217ead5af12ead6738
Caen De Silva <caen@desilva.se>
2024-02-21 21:05:50 +0100

    Cover the main menu class

--- packages/framework/tests/Feature/AutomaticNavigationConfigurationsTest.php
+++ packages/framework/tests/Feature/AutomaticNavigationConfigurationsTest.php
@@ -28,6 +28,7 @@ use Hyde\Framework\Features\Navigation\NavigationMenuGenerator;
  * @covers \Hyde\Framework\Factories\NavigationDataFactory
  * @covers \Hyde\Framework\Features\Navigation\NavigationMenuGenerator
  * @covers \Hyde\Framework\Features\Navigation\DocumentationSidebar
+ * @covers \Hyde\Framework\Features\Navigation\NavigationMenu
  * @covers \Hyde\Framework\Features\Navigation\NavItem
  */
 class AutomaticNavigationConfigurationsTest extends TestCase
