5e6db49425d723db80e9c28e2bd894913234a16f
Caen De Silva <caen@desilva.se>
2024-02-24 18:22:23 +0100

    Import class

--- packages/testing/src/TestsBladeViews.php
+++ packages/testing/src/TestsBladeViews.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Hyde\Testing;
 
+use Hyde\Testing\Support\TestableView;
+
 trait TestsBladeViews
 {
     public function view(string $view, array $data = []): TestableView
