19c668a0f3ce2ef8c0e8e0d3d18b73ecdcd3060f
Caen De Silva <caen@desilva.se>
2024-02-24 18:47:06 +0100

    Allow passing a view instance

--- packages/testing/src/TestsBladeViews.php
+++ packages/testing/src/TestsBladeViews.php
@@ -4,12 +4,17 @@ declare(strict_types=1);
 
 namespace Hyde\Testing;
 
+use Illuminate\View\View;
 use Illuminate\Testing\TestView;
 
 trait TestsBladeViews
 {
-    protected function view(string $view, $data = []): TestView
+    protected function view(string|View $view, $data = []): TestView
     {
+        if ($view instanceof View) {
+            return new TestView($view);
+        }
+
         return new TestView(view($view, $data));
     }
 }
