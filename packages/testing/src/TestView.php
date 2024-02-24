215c59cdfefa2a12b0b58df897ab849da3d2f6fd
Caen De Silva <caen@desilva.se>
2024-02-24 18:56:01 +0100

    Add assertion shorthand

--- packages/testing/src/TestView.php
+++ packages/testing/src/TestView.php
@@ -6,5 +6,13 @@ namespace Hyde\Testing\Support;
 
 class TestView extends \Illuminate\Testing\TestView
 {
-    //
+    /**
+     * Assert that the given HTML is contained within the view.
+     *
+     * @return $this
+     */
+    public function assertSeeHtml(string $value): static
+    {
+        return $this->assertSee($value, false);
+    }
 }
