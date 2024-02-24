1f433e30f28d42f9dfc41a280c5a911c84fc3323
Caen De Silva <caen@desilva.se>
2024-02-24 13:36:29 +0100

    Create DocumentationSidebarUnitTest.php

---
+++ packages/framework/tests/Unit/DocumentationSidebarUnitTest.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hyde\Framework\Testing\Unit;
+
+use Hyde\Testing\UnitTestCase;
+
+/**
+ * @covers \Hyde\Framework\Features\Navigation\DocumentationSidebar
+ *
+ * @see \Hyde\Framework\Testing\Feature\Services\DocumentationSidebarTest
+ */
+class DocumentationSidebarUnitTest extends UnitTestCase
+{
+    //
+}
