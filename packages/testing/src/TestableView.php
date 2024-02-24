377ed0f723a4ec0e3dd2322b481338f488f5b7cd
Caen De Silva <caen@desilva.se>
2024-02-24 18:22:53 +0100

    Set up constructor

--- packages/testing/src/TestableView.php
+++ packages/testing/src/TestableView.php
@@ -6,5 +6,12 @@ namespace Hyde\Testing\Support;
 
 class TestableView
 {
-    //
+    protected string $view;
+    protected array $data = [];
+
+    public function __construct(string $view, array $data = [])
+    {
+        $this->data = $data;
+        $this->view = $view;
+    }
 }
