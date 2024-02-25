4fea89429bbd3400655651f46942788803a440d2
Caen De Silva <caen@desilva.se>
2024-02-25 11:57:49 +0100

    Add view testing helper to assert ignoring formatting

--- packages/framework/tests/Feature/Views/SidebarBrandViewTest.php
+++ packages/framework/tests/Feature/Views/SidebarBrandViewTest.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Hyde\Framework\Testing\Feature\Views;
 
-use Hyde\Facades\Config;
 use Hyde\Testing\TestCase;
 use Hyde\Testing\TestsBladeViews;
 
--- packages/testing/src/Support/TestView.php
+++ packages/testing/src/Support/TestView.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Hyde\Testing\Support;
 
+use Illuminate\Testing\Assert as PHPUnit;
+
 class TestView extends \Illuminate\Testing\TestView
 {
     /**
@@ -14,5 +16,22 @@ class TestView extends \Illuminate\Testing\TestView
     public function assertSeeHtml(string $value): static
     {
         return $this->assertSee($value, false);
+    }
+
+    /**
+     * Assert that the given HTML is contained within the view text, ignoring whitespace and newlines.
+     *
+     * @return $this
+     */
+    public function assertSeeHtmlIgnoringFormatting(string $value): static
+    {
+        PHPUnit::assertStringContainsString($this->trimNewlinesAndIndentation($value), $this->trimNewlinesAndIndentation($this->rendered));
+
+        return $this;
+    }
+
+    protected function trimNewlinesAndIndentation(string $value): string
+    {
+        return str_replace(['    ', "\t", "\n", "\r"], '', $value);
     }
 }
