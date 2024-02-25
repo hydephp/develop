772ca394af6bceb0e2770cb0204d29557494e262
Caen De Silva <caen@desilva.se>
2024-02-25 11:44:58 +0100

    Create SidebarBrandViewTest.php

---
+++ packages/framework/tests/Feature/Views/SidebarBrandViewTest.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hyde\Framework\Testing\Feature\Views;
+
+use Hyde\Facades\Config;
+use Hyde\Testing\TestCase;
+use Hyde\Testing\TestsBladeViews;
+
+class SidebarBrandViewTest extends TestCase
+{
+    use TestsBladeViews;
+
+    //
+}
