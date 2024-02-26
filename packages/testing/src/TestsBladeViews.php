5cde0f10a94898cba8ba905f2e4fa75a66ba44d7
Caen De Silva <caen@desilva.se>
2024-02-26 17:21:04 +0100

    Add hook to define test view data

--- packages/testing/src/TestsBladeViews.php
+++ packages/testing/src/TestsBladeViews.php
@@ -17,10 +17,20 @@ trait TestsBladeViews
      */
     protected function test(string|View $view, $data = []): TestView
     {
+        $data = array_merge($this->testViewData(), $data);
+
         if ($view instanceof View) {
             return new TestView($view->with($data));
         }
 
         return new TestView(view($view, $data));
+    }
+
+    /**
+     * Define any view data to pass to all views in the test.
+     */
+    protected function testViewData(): array
+    {
+        return [];
     }
 }
