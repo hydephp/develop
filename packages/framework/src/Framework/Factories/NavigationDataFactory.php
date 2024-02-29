b37b99a36e54b3e6e36c9c0413c7157f3f58830b
Caen De Silva <caen@desilva.se>
2024-02-29 10:16:12 +0100

    Update factory to use menu class constant

--- packages/framework/src/Framework/Factories/NavigationDataFactory.php
+++ packages/framework/src/Framework/Factories/NavigationDataFactory.php
@@ -10,6 +10,7 @@ use Hyde\Pages\MarkdownPost;
 use Hyde\Pages\DocumentationPage;
 use Hyde\Markdown\Models\FrontMatter;
 use Hyde\Framework\Factories\Concerns\CoreDataObject;
+use Hyde\Framework\Features\Navigation\NavigationMenu;
 use Hyde\Markdown\Contracts\FrontMatter\SubSchemas\NavigationSchema;
 
 use function basename;
@@ -30,7 +31,6 @@ class NavigationDataFactory extends Concerns\PageDataFactory implements Navigati
      */
     final public const SCHEMA = NavigationSchema::NAVIGATION_SCHEMA;
 
-    protected const FALLBACK_PRIORITY = 999;
     protected const CONFIG_OFFSET = 500;
 
     protected readonly ?string $label;
@@ -99,7 +99,7 @@ class NavigationDataFactory extends Concerns\PageDataFactory implements Navigati
     {
         return $this->searchForPriorityInFrontMatter()
             ?? $this->searchForPriorityInConfigs()
-            ?? self::FALLBACK_PRIORITY;
+            ?? NavigationMenu::LAST;
     }
 
     private function searchForLabelInFrontMatter(): ?string
