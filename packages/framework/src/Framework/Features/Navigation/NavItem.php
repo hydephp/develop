c76ec470fd9f48305e08e2d8b72e22d091bbf95b
Caen De Silva <caen@desilva.se>
2024-03-01 12:12:28 +0100

    Add default value to method signature

--- packages/framework/src/Framework/Features/Navigation/NavItem.php
+++ packages/framework/src/Framework/Features/Navigation/NavItem.php
@@ -96,7 +96,7 @@ class NavItem implements Stringable
      * @param  array<NavItem>  $items  The items to be included in the dropdown.
      * @param  int|null  $priority  The priority of the dropdown item. Leave blank to use the default priority, which is last in the menu.
      */
-    public static function forGroup(string $label, array $items, ?int $priority = null): static
+    public static function forGroup(string $label, array $items, ?int $priority = NavigationMenu::LAST): static
     {
         return new static(null, $label, $priority ?? NavigationMenu::LAST, $label, $items);
     }
