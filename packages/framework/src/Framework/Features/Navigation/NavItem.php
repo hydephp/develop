0479139b2e18aa05d4c3a77b70fefc9282e81f75
Caen De Silva <caen@desilva.se>
2024-03-02 15:15:14 +0100

    No longer construct children in base class

--- packages/framework/src/Framework/Features/Navigation/NavItem.php
+++ packages/framework/src/Framework/Features/Navigation/NavItem.php
@@ -59,7 +59,6 @@ class NavItem implements Stringable
         $this->priority = $priority;
         $this->group = static::normalizeGroupKey($group);
         $this->identifier = static::makeIdentifier($label);
-        $this->addChildren($children);
     }
 
     /**
