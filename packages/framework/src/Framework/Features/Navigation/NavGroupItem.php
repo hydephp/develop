8a3d9baae1e20ac599ce710a03a72d49811f21e5
Caen De Silva <caen@desilva.se>
2024-03-02 14:02:27 +0100

    Simplify conditional returns

--- packages/framework/src/Framework/Features/Navigation/NavGroupItem.php
+++ packages/framework/src/Framework/Features/Navigation/NavGroupItem.php
@@ -96,11 +96,7 @@ class NavGroupItem extends NavItem
     protected function containsOnlyDocumentationPages(): bool
     {
         return collect($this->getChildren())->every(function (NavItem $child): bool {
-            if ($child->getRoute() instanceof ExternalRoute) {
-                return false;
-            }
-
-            return $child->getRoute()->getPage() instanceof DocumentationPage;
+            return ! $child->getRoute() instanceof ExternalRoute && $child->getRoute()->getPage() instanceof DocumentationPage;
         });
     }
 }
