8b52836b6043a81485a9b79e94f71c7aa30834c2
Caen De Silva <caen@desilva.se>
2024-03-11 17:42:14 +0100

    Create NavigationHtmlLayoutsTest.php

---
+++ packages/framework/tests/Unit/Views/NavigationHtmlLayoutsTest.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hyde\Framework\Testing\Unit\Views;
+
+use Hyde\Testing\TestCase;
+
+/**
+ * Very high level tests for navigation menu and sidebar view layouts.
+ */
+class NavigationHtmlLayoutsTest extends TestCase
+{
+    //
+}
