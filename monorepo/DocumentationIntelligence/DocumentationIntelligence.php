be208ed2612dd47dba49394dcb5064725feac0d5
Caen De Silva <caen@desilva.se>
2024-03-15 14:21:45 +0100

    Print the generated dashboard file path

--- monorepo/DocumentationIntelligence/DocumentationIntelligence.php
+++ monorepo/DocumentationIntelligence/DocumentationIntelligence.php
@@ -60,6 +60,9 @@ Command::main(function () {
     ));
 
     $this->line();
+    $this->line('Dashboard page generated at '.OUTPUT_PATH.'/dashboard.html');
+
+    $this->line();
     $this->info(sprintf('Time taken: %s. Memory used: %s',
         number_format((microtime(true) - TIME_START) * 1000, 2).'ms',
         number_format(memory_get_peak_usage() / 1024 / 1024, 2).'MB'
