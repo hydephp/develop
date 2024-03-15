ac5d58c0a39671b10546cca20869d5d33d450439
Caen De Silva <caen@desilva.se>
2024-03-15 17:55:46 +0100

    Add spacing

--- monorepo/CodeIntelligence/dashboard-template.php
+++ monorepo/CodeIntelligence/dashboard-template.php
@@ -120,6 +120,7 @@ declare(strict_types=1);
                 </div>
             </div>
         </section>
+        <br>
     </div>
 </main>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
