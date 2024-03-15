40ab5469599fe0a75cb7e002c933bfcffb633e0c
Caen De Silva <caen@desilva.se>
2024-03-15 15:14:46 +0100

    Begin setting up tab navigation markup

--- monorepo/CodeIntelligence/dashboard-template.php
+++ monorepo/CodeIntelligence/dashboard-template.php
@@ -36,71 +36,84 @@ declare(strict_types=1);
 </header>
 
 <main>
-    <section id="documentation">
-        <div class="container">
-            <div class="row">
-                <div class="col-12">
-                    <h2>Statistics</h2>
-                    <table class="table table-bordered table-sm w-fit">
-                        <caption>Model Statistics</caption>
-                        <?php echo $modelStatistics; ?>
-                    </table>
+    <nav class="container col-12">
+        <ul class="nav nav-tabs" id="myTab" role="tablist">
+          <li class="nav-item" role="presentation">
+            <button class="nav-link active" id="documentation-tab" data-bs-toggle="tab" data-bs-target="#documentation-tab-pane" type="button" role="tab" aria-controls="documentation-tab-pane" aria-selected="true">Documentation</button>
+          </li>
+          <li class="nav-item" role="presentation">
+            <button class="nav-link" id="markup-tab" data-bs-toggle="tab" data-bs-target="#markup-tab-pane" type="button" role="tab" aria-controls="markup-tab-pane" aria-selected="false">Markup Analysis</button>
+          </li>
+        </ul>
+    </nav>
+
+    <div class="tab-content">
+        <section id="documentation-tab-pane" class="tab-pane fade show active">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <h2>Statistics</h2>
+                        <table class="table table-bordered table-sm w-fit">
+                            <caption>Model Statistics</caption>
+                            <?php echo $modelStatistics; ?>
+                        </table>
+                    </div>
                 </div>
             </div>
-        </div>
 
-        <hr>
+            <hr>
 
-        <div class="container">
-            <div class="row">
-                <div class="col-12">
-                    <h2>Textual Analysis</h2>
-                    <p class="lead">
-                        This can be used to get an overview of all the headings, to ensure a consistent writing style.
-                    </p>
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <h2>Textual Analysis</h2>
+                        <p class="lead">
+                            This can be used to get an overview of all the headings, to ensure a consistent writing style.
+                        </p>
 
-                    <article>
-                        <h3>Headings <small>(<?php echo $headingsCount; ?>)</small></h3>
+                        <article>
+                            <h3>Headings <small>(<?php echo $headingsCount; ?>)</small></h3>
 
-                         <table class="table table-bordered table-sm w-fit">
-                             <?php echo $headingsTable; ?>
-                         </table>
-                    </article>
+                             <table class="table table-bordered table-sm w-fit">
+                                 <?php echo $headingsTable; ?>
+                             </table>
+                        </article>
+                    </div>
                 </div>
             </div>
-        </div>
 
-        <hr>
+            <hr>
 
-        <div class="container">
-            <div class="row">
-                <div class="col-12">
-                    <h2>Model Data</h2>
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <h2>Model Data</h2>
 
-                    <br>
-                    <article class="my-2">
-                        <h3>Full model</h3>
-                        <details>
-                            <summary>Click to view the model</summary>
+                        <br>
+                        <article class="my-2">
+                            <h3>Full model</h3>
+                            <details>
+                                <summary>Click to view the model</summary>
 
-                            <textarea rows="30" cols="80" style="width: 100%; white-space: pre; font-family: monospace;"><?php echo $modelRaw; ?></textarea>
+                                <textarea rows="30" cols="80" style="width: 100%; white-space: pre; font-family: monospace;"><?php echo $modelRaw; ?></textarea>
 
-                        </details>
-                    </article>
-                    <br>
-                    <article class="my-2">
-                        <h3>Sections</h3>
-                        <details>
-                            <summary>Click to view the sections</summary>
-                            <?php echo $modelSections; ?>
-                        </details>
-                    </article>
+                            </details>
+                        </article>
+                        <br>
+                        <article class="my-2">
+                            <h3>Sections</h3>
+                            <details>
+                                <summary>Click to view the sections</summary>
+                                <?php echo $modelSections; ?>
+                            </details>
+                        </article>
+                    </div>
                 </div>
             </div>
-        </div>
 
-        <br>
-    </section>
+            <br>
+        </section>
+    </div>
 </main>
 
 </body>
