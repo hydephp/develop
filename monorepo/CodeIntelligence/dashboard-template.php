3b178a7dfa4653ab5dfdc60d97d4f71d7619decf
Caen De Silva <caen@desilva.se>
2024-03-15 15:35:42 +0100

    Prevent flickering

--- monorepo/CodeIntelligence/dashboard-template.php
+++ monorepo/CodeIntelligence/dashboard-template.php
@@ -39,7 +39,7 @@ declare(strict_types=1);
     <nav class="container col-12">
         <ul class="nav nav-tabs" id="myTab" role="tablist">
           <li class="nav-item" role="presentation">
-            <button class="nav-link active" id="documentation-tab" data-bs-toggle="tab" data-bs-target="#documentation-tab-pane" type="button" role="tab" aria-controls="documentation-tab-pane" aria-selected="true">Documentation</button>
+            <button class="nav-link" id="documentation-tab" data-bs-toggle="tab" data-bs-target="#documentation-tab-pane" type="button" role="tab" aria-controls="documentation-tab-pane" aria-selected="false">Documentation</button>
           </li>
           <li class="nav-item" role="presentation">
             <button class="nav-link" id="markup-tab" data-bs-toggle="tab" data-bs-target="#markup-tab-pane" type="button" role="tab" aria-controls="markup-tab-pane" aria-selected="false">Markup Analysis</button>
@@ -48,7 +48,7 @@ declare(strict_types=1);
     </nav>
 
     <div class="tab-content mt-4">
-        <section id="documentation-tab-pane" class="tab-pane fade show active">
+        <section id="documentation-tab-pane" class="tab-pane fade">
             <div class="container">
                 <div class="row">
                     <div class="col-12">
@@ -112,6 +112,8 @@ declare(strict_types=1);
         if (tab) {
             tab.click();
         }
+    } else {
+        document.getElementById('documentation-tab').click();
     }
 
     // Save the last tab selected
