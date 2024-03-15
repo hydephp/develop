2439b721776d550c340ca7e2c65aa216330d3dfc
Caen De Silva <caen@desilva.se>
2024-03-15 21:44:28 +0100

    Create SidebarItemsViewTest.php

---
+++ packages/framework/tests/Unit/Views/SidebarItemsViewTest.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hyde\Framework\Testing\Unit\Views;
+
+use Hyde\Testing\TestCase;
+use Hyde\Testing\TestsBladeViews;
+
+/**
+ * @see resources/views/components/docs/sidebar-items.blade.php
+ */
+class SidebarItemsViewTest extends TestCase
+{
+    use TestsBladeViews;
+
+    //
+}
