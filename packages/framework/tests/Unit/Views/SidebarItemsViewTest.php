4fa8deab206ee06022acf5b9a842ebb6a15858ce
Caen De Silva <caen@desilva.se>
2024-03-15 21:52:29 +0100

    Set up testing helper

--- packages/framework/tests/Unit/Views/SidebarItemsViewTest.php
+++ packages/framework/tests/Unit/Views/SidebarItemsViewTest.php
@@ -6,6 +6,9 @@ namespace Hyde\Framework\Testing\Unit\Views;
 
 use Hyde\Testing\TestCase;
 use Hyde\Testing\TestsBladeViews;
+use Hyde\Testing\Support\TestView;
+use Hyde\Framework\Features\Navigation\DocumentationSidebar;
+use Hyde\Framework\Features\Navigation\NavigationMenuGenerator;
 
 /**
  * @see resources/views/components/docs/sidebar-items.blade.php
@@ -14,5 +17,10 @@ class SidebarItemsViewTest extends TestCase
 {
     use TestsBladeViews;
 
-    //
+    protected function testView(): TestView
+    {
+        return $this->test(view('hyde::components.docs.sidebar-items', [
+            'sidebar' => NavigationMenuGenerator::handle(DocumentationSidebar::class),
+        ]));
+    }
 }
