e42e9255a6a6e0bf95ea8bb6b86cda566de124d4
Caen De Silva <caen@desilva.se>
2024-03-16 11:05:49 +0100

    Document class abstract

--- packages/testing/src/Support/HtmlTesting/TestableHtmlDocument.php
+++ packages/testing/src/Support/HtmlTesting/TestableHtmlDocument.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace Hyde\Testing\Support\HtmlTesting;
 
+/**
+ * A wrapper for an HTML document, parsed into an assertable and queryable object, with an abstract syntax tree.
+ */
 class TestableHtmlDocument
 {
     //
