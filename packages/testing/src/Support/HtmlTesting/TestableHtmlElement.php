02b1e1b47511e2b25b06bc5c67fb696b637f9b22
Caen De Silva <caen@desilva.se>
2024-03-16 11:16:40 +0100

    Add container for element child nodes

--- packages/testing/src/Support/HtmlTesting/TestableHtmlElement.php
+++ packages/testing/src/Support/HtmlTesting/TestableHtmlElement.php
@@ -4,12 +4,17 @@ declare(strict_types=1);
 
 namespace Hyde\Testing\Support\HtmlTesting;
 
+use Illuminate\Support\Collection;
+
 /**
  * A wrapper for an HTML element node, parsed into an assertable and queryable object.
  */
 class TestableHtmlElement
 {
     protected readonly string $html;
+
+    /** @var \Illuminate\Support\Collection<\Hyde\Testing\Support\HtmlTesting\TestableHtmlElement> */
+    protected Collection $nodes;
 
     public function __construct(string $html)
     {
