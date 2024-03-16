ebd00e028391bb2a056dea76663a63083715a885
Caen De Silva <caen@desilva.se>
2024-03-16 14:18:41 +0100

    Lower heading level

--- packages/testing/src/Support/HtmlTesting/TestableHtmlDocument.php
+++ packages/testing/src/Support/HtmlTesting/TestableHtmlDocument.php
@@ -86,7 +86,7 @@ class TestableHtmlDocument
             return $this->createDumpNodeMapEntry($node);
         })->implode(''));
 
-        $html .= '<h2>Document Preview</h2>';
+        $html .= '<h3>Document Preview</h3>';
         $html .= sprintf('<iframe src="data:text/html;base64,%s" width="960px" height="600px"></iframe>', base64_encode($this->html));
 
         $html .= '</body></html>';
