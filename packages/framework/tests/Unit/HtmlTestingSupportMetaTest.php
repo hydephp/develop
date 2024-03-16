49cce13bafc5af5769638c7a14391d956856f046
Caen De Silva <caen@desilva.se>
2024-03-16 10:58:20 +0100

    Set up test environment

--- packages/framework/tests/Unit/HtmlTestingSupportMetaTest.php
+++ packages/framework/tests/Unit/HtmlTestingSupportMetaTest.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 
 namespace Hyde\Framework\Testing\Unit;
 
+use Hyde\Hyde;
 use Hyde\Testing\UnitTestCase;
+use Hyde\Testing\TestsBladeViews;
 
 /**
  * Meta test for the HTML testing support.
@@ -16,5 +18,16 @@ use Hyde\Testing\UnitTestCase;
  */
 class HtmlTestingSupportMetaTest extends UnitTestCase
 {
-    //
+    use TestsBladeViews;
+
+    protected string $html;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        self::needsKernel();
+
+        $this->html ??= file_get_contents(Hyde::vendorPath('resources/views/homepages/welcome.blade.php'));
+    }
 }
