9c930262e3421378cae5ec0dc46e9a68504634f7
Caen De Silva <caen@desilva.se>
2024-03-16 10:47:06 +0100

    Create HtmlTestingSupportMetaTest.php

---
+++ packages/framework/tests/Unit/HtmlTestingSupportMetaTest.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hyde\Framework\Testing\Unit;
+
+use Hyde\Testing\UnitTestCase;
+
+/**
+ * Meta test for the HTML testing support.
+ *
+ * @see \Hyde\Testing\Support\TestView
+ * @see \Hyde\Testing\Support\HtmlTesting
+ *
+ * @coversNothing
+ */
+class HtmlTestingSupportMetaTest extends UnitTestCase
+{
+    //
+}
