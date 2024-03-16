c2751d566b9218b58e9e591a81743e9eed8c097f
Caen De Silva <caen@desilva.se>
2024-03-16 11:15:00 +0100

    Document class abstract

--- packages/testing/src/Support/HtmlTesting/TestableHtmlElement.php
+++ packages/testing/src/Support/HtmlTesting/TestableHtmlElement.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace Hyde\Testing\Support\HtmlTesting;
 
+/**
+ * A wrapper for an HTML element node, parsed into an assertable and queryable object.
+ */
 class TestableHtmlElement
 {
     //
