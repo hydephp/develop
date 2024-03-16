1fc316a9258a19b2fe22d795a9248ce89aeeec5a
Caen De Silva <caen@desilva.se>
2024-03-16 11:15:11 +0100

    Add collection for document nodes

--- packages/testing/src/Support/HtmlTesting/TestableHtmlDocument.php
+++ packages/testing/src/Support/HtmlTesting/TestableHtmlDocument.php
@@ -4,12 +4,19 @@ declare(strict_types=1);
 
 namespace Hyde\Testing\Support\HtmlTesting;
 
+use Illuminate\Support\Collection;
+
 /**
  * A wrapper for an HTML document, parsed into an assertable and queryable object, with an abstract syntax tree.
  */
 class TestableHtmlDocument
 {
     protected readonly string $html;
+
+    /**
+     * @var \Illuminate\Support\Collection<\Hyde\Testing\Support\HtmlTesting\TestableHtmlElement>
+     */
+    protected Collection $nodes;
 
     public function __construct(string $html)
     {
