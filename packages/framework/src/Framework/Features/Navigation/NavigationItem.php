76fc723d00a8a2b91526cb6622f9817d89a1db58
Caen De Silva <caen@desilva.se>
2024-03-21 19:51:40 +0100

    Union annotate plain string type

--- packages/framework/src/Framework/Features/Navigation/NavigationItem.php
+++ packages/framework/src/Framework/Features/Navigation/NavigationItem.php
@@ -54,7 +54,7 @@ class NavigationItem implements Stringable
     /**
      * Create a new navigation menu item, automatically filling in the properties from a Route instance if provided.
      *
-     * @param  \Hyde\Support\Models\Route|string<\Hyde\Support\Models\RouteKey>  $destination  Route instance or route key, or external URI.
+     * @param  \Hyde\Support\Models\Route|string<\Hyde\Support\Models\RouteKey>|string  $destination  Route instance or route key, or external URI.
      * @param  int|null  $priority  Leave blank to use the priority of the route's corresponding page, if there is one tied to the route.
      * @param  string|null  $label  Leave blank to use the label of the route's corresponding page, if there is one tied to the route.
      * @param  string|null  $group  Leave blank to use the group of the route's corresponding page, if there is one tied to the route.
