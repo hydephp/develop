9f133ce658b0bccd0f7f7b02613f31174defcdab
Caen De Silva <caen@desilva.se>
2024-03-22 13:48:30 +0100

    Remove todo for closed PR https://github.com/hydephp/develop/pull/1630

--- packages/framework/src/Framework/Features/Navigation/NavigationGroup.php
+++ packages/framework/src/Framework/Features/Navigation/NavigationGroup.php
@@ -13,8 +13,6 @@ use function collect;
 
 /**
  * Abstraction for a grouped navigation menu item. For main navigation menus, this is a dropdown.
- *
- * @todo Consider extracting trait for shared code with navigation menu class
  */
 class NavigationGroup implements NavigationElement
 {
