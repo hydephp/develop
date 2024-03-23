4c868f242a16984b811b1e4617aa9d9d80ef091a
Caen De Silva <caen@desilva.se>
2024-03-23 10:41:20 +0100

    Refactor test using ChatGPT

--- packages/framework/tests/Unit/DocumentationSidebarGetActiveGroupUnitTest.php
+++ packages/framework/tests/Unit/DocumentationSidebarGetActiveGroupUnitTest.php
@@ -27,8 +27,10 @@ class DocumentationSidebarGetActiveGroupUnitTest extends UnitTestCase
     protected static bool $needsConfig = true;
     protected static bool $needsKernel = true;
 
-    public function testGetActiveGroup()
+    protected function setUp(): void
     {
+        parent::setUp();
+
         View::swap(Mockery::mock(Factory::class)->makePartial());
         $renderData = new RenderData();
         Render::swap($renderData);
@@ -43,29 +45,58 @@ class DocumentationSidebarGetActiveGroupUnitTest extends UnitTestCase
             $page = new DocumentationPage($page, ['navigation.group' => $group]);
             Routes::addRoute($page->getRoute());
         }
+    }
 
+    public function testActiveGroupIsNullInitially()
+    {
+        $menu = NavigationMenuGenerator::handle(DocumentationSidebar::class);
+        $this->assertNull($menu->getActiveGroup());
+    }
+
+    public function testSetActiveGroup()
+    {
         $menu = NavigationMenuGenerator::handle(DocumentationSidebar::class);
 
-        $this->assertNull($menu->getActiveGroup());
-
+        $renderData = Render::getFacadeRoot();
         $renderData->setPage(new DocumentationPage('foo', ['navigation.group' => 'one']));
 
         $this->assertInstanceOf(NavigationGroup::class, $menu->getActiveGroup());
         $this->assertSame('one', $menu->getActiveGroup()->getGroupKey());
+    }
+
+    public function testActiveGroupUpdatesWithPageChanges()
+    {
+        $menu = NavigationMenuGenerator::handle(DocumentationSidebar::class);
+        $renderData = Render::getFacadeRoot();
+
+        $pages = [
+            'foo' => 'one',
+            'bar' => 'two',
+            'baz' => 'three',
+        ];
 
         foreach ($pages as $page => $group) {
             $renderData->setPage(new DocumentationPage($page, ['navigation.group' => $group]));
             $this->assertSame($group, $menu->getActiveGroup()->getGroupKey());
         }
+    }
 
+    public function testActiveGroupItems()
+    {
+        $menu = NavigationMenuGenerator::handle(DocumentationSidebar::class);
+        $renderData = Render::getFacadeRoot();
         $renderData->setPage(new DocumentationPage('foo', ['navigation.group' => 'one']));
 
-        $this->assertSame([
+        $expectedItems = [
             'one' => true,
             'two' => false,
             'three' => false,
-        ], $menu->getItems()->mapWithKeys(fn (NavigationGroup $item): array => [
+        ];
+
+        $actualItems = $menu->getItems()->mapWithKeys(fn (NavigationGroup $item): array => [
             $item->getGroupKey() => $item === $menu->getActiveGroup(),
-        ])->all());
+        ])->all();
+
+        $this->assertSame($expectedItems, $actualItems);
     }
 }
