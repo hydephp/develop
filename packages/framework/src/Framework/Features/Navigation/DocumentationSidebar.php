63ca58a14091407a1e005a34231ea649e2d9d148
Caen De Silva <caen@desilva.se>
2024-03-23 20:01:16 +0100

    Style comment formatting

--- packages/framework/src/Framework/Features/Navigation/DocumentationSidebar.php
+++ packages/framework/src/Framework/Features/Navigation/DocumentationSidebar.php
@@ -77,7 +77,7 @@ class DocumentationSidebar extends NavigationMenu
         }
 
         return $this->items->first(function (NavigationGroup $group) use ($currentPage): bool {
-            // A group is active when it contains the current page being rendered
+            // A group is active when it contains the current page being rendered.
             return $group->getGroupKey() === NavigationItem::normalizeGroupKey($currentPage->navigationMenuGroup());
         });
     }
