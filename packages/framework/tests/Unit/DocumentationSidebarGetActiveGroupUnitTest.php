89debff7926ec93291a7c5f6ec7db94298efc080
Caen De Silva <caen@desilva.se>
2024-03-23 19:15:57 +0100

    Add comment

--- packages/framework/tests/Unit/DocumentationSidebarGetActiveGroupUnitTest.php
+++ packages/framework/tests/Unit/DocumentationSidebarGetActiveGroupUnitTest.php
@@ -68,6 +68,7 @@ class DocumentationSidebarGetActiveGroupUnitTest extends UnitTestCase
         // Create the sidebar
         $sidebar = new DocumentationSidebar($items);
 
+        // Assert is null when there is no active group
         $this->assertNull($sidebar->getActiveGroup());
     }
 
