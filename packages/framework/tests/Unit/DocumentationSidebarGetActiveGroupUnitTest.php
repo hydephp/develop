9387233254bdb3cdfcac90b5bfc268f6a56f9430
Caen De Silva <caen@desilva.se>
2024-03-23 10:31:50 +0100

    Create DocumentationSidebarGetActiveGroupUnitTest.php

---
+++ packages/framework/tests/Unit/DocumentationSidebarGetActiveGroupUnitTest.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hyde\Framework\Testing\Unit;
+
+use Hyde\Testing\UnitTestCase;
+
+/**
+ * @covers \Hyde\Framework\Features\Navigation\DocumentationSidebar
+ */
+class DocumentationSidebarGetActiveGroupUnitTest extends UnitTestCase
+{
+    //
+}
