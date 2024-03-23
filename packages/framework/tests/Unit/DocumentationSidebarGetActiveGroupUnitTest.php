5203626b8032d7583ff7a18ab201f9c391098a4d
Caen De Silva <caen@desilva.se>
2024-03-23 10:33:28 +0100

    Implement the main test method

--- packages/framework/tests/Unit/DocumentationSidebarGetActiveGroupUnitTest.php
+++ packages/framework/tests/Unit/DocumentationSidebarGetActiveGroupUnitTest.php
@@ -4,7 +4,17 @@ declare(strict_types=1);
 
 namespace Hyde\Framework\Testing\Unit;
 
+use Mockery;
+use Illuminate\View\Factory;
 use Hyde\Testing\UnitTestCase;
+use Hyde\Support\Facades\Render;
+use Hyde\Pages\DocumentationPage;
+use Hyde\Support\Models\RenderData;
+use Hyde\Foundation\Facades\Routes;
+use Illuminate\Support\Facades\View;
+use Hyde\Framework\Features\Navigation\NavigationGroup;
+use Hyde\Framework\Features\Navigation\DocumentationSidebar;
+use Hyde\Framework\Features\Navigation\NavigationMenuGenerator;
 
 /**
  * @covers \Hyde\Framework\Features\Navigation\DocumentationSidebar
@@ -14,5 +24,48 @@ use Hyde\Testing\UnitTestCase;
  */
 class DocumentationSidebarGetActiveGroupUnitTest extends UnitTestCase
 {
-    //
+    protected static bool $needsConfig = true;
+    protected static bool $needsKernel = true;
+
+    public function testGetActiveGroup()
+    {
+        View::swap(Mockery::mock(Factory::class)->makePartial());
+        $renderData = new RenderData();
+        Render::swap($renderData);
+
+        $pages = [
+            'foo' => 'one',
+            'bar' => 'two',
+            'baz' => 'three',
+        ];
+
+        foreach ($pages as $page => $group) {
+            $page = new DocumentationPage($page, ['navigation.group' => $group]);
+            Routes::addRoute($page->getRoute());
+        }
+
+        $menu = NavigationMenuGenerator::handle(DocumentationSidebar::class);
+
+        $this->assertNull($menu->getActiveGroup());
+
+        $renderData->setPage(new DocumentationPage('foo', ['navigation.group' => 'one']));
+
+        $this->assertInstanceOf(NavigationGroup::class, $menu->getActiveGroup());
+        $this->assertSame('one', $menu->getActiveGroup()->getGroupKey());
+
+        foreach ($pages as $page => $group) {
+            $renderData->setPage(new DocumentationPage($page, ['navigation.group' => $group]));
+            $this->assertSame($group, $menu->getActiveGroup()->getGroupKey());
+        }
+
+        $renderData->setPage(new DocumentationPage('foo', ['navigation.group' => 'one']));
+
+        $this->assertSame([
+            'one' => true,
+            'two' => false,
+            'three' => false,
+        ], $menu->getItems()->mapWithKeys(fn (NavigationGroup $item): array => [
+            $item->getGroupKey() => $item === $menu->getActiveGroup(),
+        ])->all());
+    }
 }
