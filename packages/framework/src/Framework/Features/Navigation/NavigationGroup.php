de6993bac15fe29da924515a5e4eae8f5e7cd45e
Caen De Silva <caen@desilva.se>
2024-03-24 19:48:51 +0100

    Mark group key normalization method as experimental

--- packages/framework/src/Framework/Features/Navigation/NavigationGroup.php
+++ packages/framework/src/Framework/Features/Navigation/NavigationGroup.php
@@ -82,6 +82,7 @@ class NavigationGroup implements NavigationElement
         });
     }
 
+    /** @experimental This method is subject to change before its release. */
     public static function normalizeGroupKey(string $group): string
     {
         return Str::slug($group);
