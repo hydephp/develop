3ad4b01d99745b66930bacbb927619802ba5463e
Caen De Silva <caen@desilva.se>
2024-03-24 11:08:20 +0100

    Contract vital shared parts of route interface

--- packages/framework/src/Support/Contracts/RouteContract.php
+++ packages/framework/src/Support/Contracts/RouteContract.php
@@ -5,8 +5,18 @@ declare(strict_types=1);
 namespace Hyde\Support\Contracts;
 
 use Stringable;
+use Hyde\Support\Models\Route;
+use Hyde\Support\Models\RouteKey;
 
 interface RouteContract extends SerializableContract, Stringable
 {
-    //
+    /**
+     * Generate a link that resolves to the route destination.
+     */
+    public function getLink(): string;
+
+    /**
+     * Check if the route is the same as the given route.
+     */
+    public function is(Route|RouteKey|string $route): bool;
 }
