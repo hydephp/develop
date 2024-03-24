9e734e0342abc3eb007134433d11aee17231afc8
Caen De Silva <caen@desilva.se>
2024-03-24 11:25:09 +0100

    Class `NavigationDestination` implements `Stringable`

--- packages/framework/src/Framework/Features/Navigation/NavigationDestination.php
+++ packages/framework/src/Framework/Features/Navigation/NavigationDestination.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 
 namespace Hyde\Framework\Features\Navigation;
 
-class NavigationDestination
+use Stringable;
+
+class NavigationDestination implements Stringable
 {
     //
 }
