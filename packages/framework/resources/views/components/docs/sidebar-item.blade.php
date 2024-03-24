b4011e124fcd9cd7b80d23f2e5aeb7f6631859f9
Caen De Silva <caen@desilva.se>
2024-03-24 16:26:31 +0100

    Use the resolved link instead of implicit route resolve

--- packages/framework/resources/views/components/docs/sidebar-item.blade.php
+++ packages/framework/resources/views/components/docs/sidebar-item.blade.php
@@ -4,7 +4,7 @@
         : 'active bg-black/5 dark:bg-black/10' => $item->isActive()
     ]) role="listitem">
     @if($item->isActive())
-        <a href="{{ $item->getRoute() }}" aria-current="true" @class([$grouped
+        <a href="{{ $item->getUrl() }}" aria-current="true" @class([$grouped
             ? '-ml-8 pl-4 py-1 px-2 block text-indigo-600 dark:text-indigo-400 dark:font-medium border-l-[0.325rem] border-indigo-500 transition-colors duration-300 ease-in-out hover:bg-black/10'
             : '-ml-4 p-2 block hover:bg-black/5 dark:hover:bg-black/10 text-indigo-600 dark:text-indigo-400 dark:font-medium border-l-[0.325rem] border-indigo-500 transition-colors duration-300 ease-in-out'
         ])>
@@ -16,7 +16,7 @@
             {!! $page->getTableOfContents() !!}
         @endif
     @else
-        <a href="{{ $item->getRoute() }}" @class([$grouped
+        <a href="{{ $item->getUrl() }}" @class([$grouped
             ? '-ml-8 pl-4 py-1 px-2 block border-l-[0.325rem] border-transparent transition-colors duration-300 ease-in-out hover:bg-black/10'
             : 'block -ml-4 p-2 border-l-[0.325rem] border-transparent hover:bg-black/5 dark:hover:bg-black/10'
         ])>
