72fce42a51c6730a8588dbf0b7d4663e14723c86
Caen De Silva <caen@desilva.se>
2024-03-24 15:51:13 +0100

    Revert "Skip tests needing to be reimplemented for #1636"
    This reverts commit eb81668e91594e9944a5d72cffb5f7d29246f2f9.

--- packages/framework/tests/Unit/NavigationItemTest.php
+++ packages/framework/tests/Unit/NavigationItemTest.php
@@ -129,7 +129,6 @@ class NavigationItemTest extends UnitTestCase
 
     public function testCreateWithLink()
     {
-        $this->markTestSkipped('Todo: Reimplement this test for https://github.com/hydephp/develop/pull/1636');
         $item = NavigationItem::create('foo', 'bar');
 
         $this->assertEquals(new ExternalRoute('foo'), $item->getRoute());
@@ -172,7 +171,6 @@ class NavigationItemTest extends UnitTestCase
 
     public function testCreateWithMissingRouteKey()
     {
-        $this->markTestSkipped('Todo: Reimplement this test for https://github.com/hydephp/develop/pull/1636');
         $this->assertInstanceOf(ExternalRoute::class, NavigationItem::create('foo', 'foo')->getRoute());
     }
 
