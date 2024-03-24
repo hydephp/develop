6f482348067dcf487642af97eb4ec5207cbd806a
Caen De Silva <caen@desilva.se>
2024-03-24 12:33:18 +0100

    Add destination property

--- packages/framework/src/Framework/Features/Navigation/NavigationDestination.php
+++ packages/framework/src/Framework/Features/Navigation/NavigationDestination.php
@@ -5,8 +5,9 @@ declare(strict_types=1);
 namespace Hyde\Framework\Features\Navigation;
 
 use Stringable;
+use Hyde\Support\Models\Route;
 
 class NavigationDestination implements Stringable
 {
-    //
+    protected Route|string $destination;
 }
