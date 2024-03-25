40871f684e0451cf26366b22ca5fd3b78e8fd53e
Caen De Silva <caen@desilva.se>
2024-03-25 11:36:28 +0100

    Add more tests and assertions

--- packages/framework/tests/Unit/TestingSupportHelpersMetaTest.php
+++ packages/framework/tests/Unit/TestingSupportHelpersMetaTest.php
@@ -6,6 +6,7 @@ namespace Hyde\Framework\Testing\Unit;
 
 use Hyde\Pages\InMemoryPage;
 use Hyde\Testing\UnitTestCase;
+use Hyde\Support\Models\Route;
 use Hyde\Testing\MocksKernelFeatures;
 
 /**
@@ -30,5 +31,20 @@ class TestingSupportHelpersMetaTest extends UnitTestCase
         $this->withPages([$page]);
 
         $this->assertEquals([$page->getRoute()], $this->kernel->routes()->all());
+        $this->assertSame($page, $this->kernel->routes()->first()->getPage());
+    }
+
+    public function testWithPagesReplacesExistingRoutes()
+    {
+        $this->withPages([new InMemoryPage('foo')]);
+        $this->assertSame(['foo'], $this->getRouteKeys());
+
+        $this->withPages([new InMemoryPage('bar')]);
+        $this->assertSame(['bar'], $this->getRouteKeys());
+    }
+
+    protected function getRouteKeys()
+    {
+        return $this->kernel->routes()->map(fn (Route $route) => $route->getRouteKey())->all();
     }
 }
