e28a15ccc4513a71e927d438205076b0d5c77229
Caen De Silva <caen@desilva.se>
2024-03-25 11:09:46 +0100

    Create TestingSupportHelpersMetaTest.php

---
+++ packages/framework/tests/Unit/TestingSupportHelpersMetaTest.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hyde\Framework\Testing\Unit;
+
+use Hyde\Testing\UnitTestCase;
+
+/**
+ * Meta test for internal testing helpers.
+ *
+ * @see \Hyde\Testing\Support
+ *
+ * @coversNothing
+ */
+class TestingSupportHelpersMetaTest extends UnitTestCase
+{
+    //
+}
