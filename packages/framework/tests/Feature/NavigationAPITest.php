473ac2056b49b5ffda20f1b985db3f3c66ceaaf7
Caen De Silva <caen@desilva.se>
2024-03-25 12:12:27 +0100

    Draft test setup

--- packages/framework/tests/Feature/NavigationAPITest.php
+++ packages/framework/tests/Feature/NavigationAPITest.php
@@ -5,11 +5,28 @@ declare(strict_types=1);
 namespace Hyde\Framework\Testing\Feature;
 
 use Hyde\Testing\TestCase;
+use Hyde\Foundation\Facades\Routes;
+use Hyde\Testing\MocksKernelFeatures;
+use Hyde\Framework\Features\Navigation\NavigationMenu;
+use Hyde\Framework\Features\Navigation\NavigationItem;
 
 /**
  * High level tests for the Navigation API.
  */
 class NavigationAPITest extends TestCase
 {
-    //
+    use MocksKernelFeatures;
+
+    public function testNavigationMenus()
+    {
+        $this->withPages(['index', 'about', 'contact']);
+
+        $menu = new NavigationMenu([
+            new NavigationItem(Routes::get('index'), 'Home'),
+            new NavigationItem(Routes::get('about'), 'About'),
+            new NavigationItem(Routes::get('contact'), 'Contact'),
+        ]);
+
+        $this->assertCount(3, $menu->getItems());
+    }
 }
