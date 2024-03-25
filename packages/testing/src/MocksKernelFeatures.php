94a34ffc77a4c645cdab07b5cc9f8c2b57f595fa
Caen De Silva <caen@desilva.se>
2024-03-25 11:22:58 +0100

    Create test kernel class with mockable rotues feature

--- packages/testing/src/MocksKernelFeatures.php
+++ packages/testing/src/MocksKernelFeatures.php
@@ -4,10 +4,30 @@ declare(strict_types=1);
 
 namespace Hyde\Testing;
 
+use Hyde\Foundation\HydeKernel;
+use Hyde\Foundation\Kernel\RouteCollection;
+use Illuminate\Contracts\Support\Arrayable;
+
 /**
  * A trait to mock kernel features for testing.
  */
 trait MocksKernelFeatures
 {
     //
+}
+
+class TestKernel extends HydeKernel
+{
+    protected ?RouteCollection $mockRoutes = null;
+
+    public function setRoutes(Arrayable $routes): void
+    {
+        $this->mockRoutes = RouteCollection::make($routes);
+    }
+
+    /** @return \Hyde\Foundation\Kernel\RouteCollection<string, \Hyde\Support\Models\Route> */
+    public function routes(): RouteCollection
+    {
+        return $this->mockRoutes ?? parent::routes();
+    }
 }
