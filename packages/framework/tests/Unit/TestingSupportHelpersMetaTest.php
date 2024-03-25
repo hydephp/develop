e80ebe0beeb063c50324f2b34bb380ad38356fbc
Caen De Silva <caen@desilva.se>
2024-03-25 11:13:00 +0100

    Connect test to added testing trait

--- packages/framework/tests/Unit/TestingSupportHelpersMetaTest.php
+++ packages/framework/tests/Unit/TestingSupportHelpersMetaTest.php
@@ -5,15 +5,19 @@ declare(strict_types=1);
 namespace Hyde\Framework\Testing\Unit;
 
 use Hyde\Testing\UnitTestCase;
+use Hyde\Testing\MocksKernelFeatures;
 
 /**
  * Meta test for internal testing helpers.
  *
  * @see \Hyde\Testing\Support
+ * @see \Hyde\Testing\MocksKernelFeatures
  *
  * @coversNothing
  */
 class TestingSupportHelpersMetaTest extends UnitTestCase
 {
+    use MocksKernelFeatures;
+
     //
 }
