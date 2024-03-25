c462c62f695c44baedab5bf5650421c0994a3d8d
Caen De Silva <caen@desilva.se>
2024-03-25 12:02:44 +0100

    Revert "Set up test kernel"
    This reverts commit 102df43e6619b1907d67426caa54bf4ebd47c34d.

--- packages/framework/tests/Feature/NavigationAPITest.php
+++ packages/framework/tests/Feature/NavigationAPITest.php
@@ -5,36 +5,11 @@ declare(strict_types=1);
 namespace Hyde\Framework\Testing\Feature;
 
 use Hyde\Testing\TestCase;
-use Hyde\Foundation\HydeKernel;
-use Illuminate\Support\Collection;
-use Hyde\Foundation\Kernel\RouteCollection;
 
 /**
  * High level tests for the Navigation API.
  */
 class NavigationAPITest extends TestCase
 {
-    protected function setUp(): void
-    {
-        parent::setUp();
-
-        $this->kernel = new TestKernel();
-        HydeKernel::setInstance($this->kernel);
-    }
-}
-
-class TestKernel extends HydeKernel
-{
-    protected ?RouteCollection $mockRoutes = null;
-
-    public function setRoutes(Collection $routes): void
-    {
-        $this->mockRoutes = RouteCollection::make($routes);
-    }
-
-    /** @return \Hyde\Foundation\Kernel\RouteCollection<string, \Hyde\Support\Models\Route> */
-    public function routes(): RouteCollection
-    {
-        return $this->mockRoutes ?? parent::routes();
-    }
+    //
 }
