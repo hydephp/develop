f8476918644d3244371b023d35f658c2fe479881
Caen De Silva <caen@desilva.se>
2024-03-26 17:23:18 +0100

    Remove outdated class documentation

--- packages/framework/src/Framework/Features/Navigation/NavigationItem.php
+++ packages/framework/src/Framework/Features/Navigation/NavigationItem.php
@@ -14,11 +14,6 @@ use function is_string;
 
 /**
  * Abstraction for a navigation menu item. Used by the MainNavigationMenu and DocumentationSidebar classes.
- *
- * You have a few options to construct a navigation menu item:
- *   1. You can supply a Route directly and explicit properties to the constructor
- *   2. You can use NavigationItem::fromRoute() to use data from the route
- *   3. You can use NavigationItem::create() for an external or un-routed link
  */
 class NavigationItem implements Stringable
 {
