e91503b701cc23bfe51e2c4ce9328a7a40d70ef0
Caen De Silva <caen@desilva.se>
2024-03-28 17:54:49 +0100

    Document trait abstract

--- packages/framework/src/Framework/Features/Navigation/HasNavigationItems.php
+++ packages/framework/src/Framework/Features/Navigation/HasNavigationItems.php
@@ -4,6 +4,12 @@ declare(strict_types=1);
 
 namespace Hyde\Framework\Features\Navigation;
 
+/**
+ * Contains shared logic for classes that have navigation items.
+ *
+ * @see \Hyde\Framework\Features\Navigation\NavigationMenu
+ * @see \Hyde\Framework\Features\Navigation\NavigationGroup
+ */
 trait HasNavigationItems
 {
     //
