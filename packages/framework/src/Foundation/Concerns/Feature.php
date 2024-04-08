f69217a1a214ae74447de6c3f158094ccf75adc6
Caen De Silva <caen@desilva.se>
2024-04-08 18:22:24 +0200

    Add enum description

--- packages/framework/src/Foundation/Concerns/Feature.php
+++ packages/framework/src/Foundation/Concerns/Feature.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 namespace Hyde\Foundation\Concerns;
 
 /**
+ * A configurable feature that belongs to the Features class.
+ *
  * @experimental This class may change significantly before its release.
  */
 enum Feature
