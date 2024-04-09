2928d7182f22b643a90b2497c590a9e87bb28135
Caen De Silva <caen@desilva.se>
2024-04-09 14:23:56 +0200

    Add enum description

--- packages/framework/src/Enums/Feature.php
+++ packages/framework/src/Enums/Feature.php
@@ -4,6 +4,11 @@ declare(strict_types=1);
 
 namespace Hyde\Enums;
 
+/**
+ * A configurable feature that belongs to the Features class.
+ *
+ * @see \Hyde\Facades\Features
+ */
 enum Feature
 {
     //
