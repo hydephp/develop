2a574082e9eced3246e578272079c9b0bb9f026c
Caen De Silva <caen@desilva.se>
2024-04-11 11:42:08 +0200

    Remove string backing from feature enum
    This does not add any new information

--- packages/framework/src/Enums/Feature.php
+++ packages/framework/src/Enums/Feature.php
@@ -9,21 +9,21 @@ namespace Hyde\Enums;
  *
  * @see \Hyde\Facades\Features
  */
-enum Feature: string
+enum Feature
 {
     // Page Modules
-    case HtmlPages = 'html-pages';
-    case MarkdownPosts = 'markdown-posts';
-    case BladePages = 'blade-pages';
-    case MarkdownPages = 'markdown-pages';
-    case DocumentationPages = 'documentation-pages';
+    case HtmlPages;
+    case MarkdownPosts;
+    case BladePages;
+    case MarkdownPages;
+    case DocumentationPages;
 
     // Frontend Features
-    case Darkmode = 'darkmode';
-    case DocumentationSearch = 'documentation-search';
+    case Darkmode;
+    case DocumentationSearch;
 
     // Integrations
-    case Torchlight = 'torchlight';
+    case Torchlight;
 
     /** @deprecated This method will be removed in HydePHP v2.0 */
     public static function match(string $name): self
