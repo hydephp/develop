41b410697b631477c6fc8fd6e26380de1087f1de
Caen De Silva <caen@desilva.se>
2024-04-12 21:14:54 +0200

    Create BuildTaskUnitTest.php

---
+++ packages/framework/tests/Unit/BuildTaskUnitTest.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hyde\Framework\Testing\Unit;
+
+use Hyde\Testing\UnitTestCase;
+
+/**
+ * @covers \Hyde\Framework\Features\BuildTasks\BuildTask
+ */
+class BuildTaskUnitTest extends UnitTestCase
+{
+    //
+}
