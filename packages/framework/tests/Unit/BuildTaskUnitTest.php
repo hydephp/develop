eb66f5a29612c52034b1593cff007ccb6eec84fa
Caen De Silva <caen@desilva.se>
2024-04-12 21:18:11 +0200

    Test can create build task

--- packages/framework/tests/Unit/BuildTaskUnitTest.php
+++ packages/framework/tests/Unit/BuildTaskUnitTest.php
@@ -5,11 +5,23 @@ declare(strict_types=1);
 namespace Hyde\Framework\Testing\Unit;
 
 use Hyde\Testing\UnitTestCase;
+use Hyde\Framework\Features\BuildTasks\BuildTask;
 
 /**
  * @covers \Hyde\Framework\Features\BuildTasks\BuildTask
  */
 class BuildTaskUnitTest extends UnitTestCase
 {
-    //
+    public function testCanCreateBuildTask()
+    {
+        $task = new class extends BuildTask
+        {
+            public function handle(): void
+            {
+                // Do nothing
+            }
+        };
+
+        $this->assertInstanceOf(BuildTask::class, $task);
+    }
 }
