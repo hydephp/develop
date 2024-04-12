4279d17eb71602bf4910de758f49423e341d5c0e
Caen De Silva <caen@desilva.se>
2024-04-12 14:07:46 +0200

    Use `InteractsWithDirectories` trait

--- packages/framework/src/Framework/Actions/PostBuildTasks/GenerateSitemap.php
+++ packages/framework/src/Framework/Actions/PostBuildTasks/GenerateSitemap.php
@@ -6,12 +6,15 @@ namespace Hyde\Framework\Actions\PostBuildTasks;
 
 use Hyde\Hyde;
 use Hyde\Framework\Features\BuildTasks\PostBuildTask;
+use Hyde\Framework\Concerns\InteractsWithDirectories;
 use Hyde\Framework\Features\XmlGenerators\SitemapGenerator;
 
 use function file_put_contents;
 
 class GenerateSitemap extends PostBuildTask
 {
+    use InteractsWithDirectories;
+
     public static string $message = 'Generating sitemap';
 
     protected string $path;
