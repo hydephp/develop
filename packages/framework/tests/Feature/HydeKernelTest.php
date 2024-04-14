87f64a85e117e4f7ef01eb4b862db65e0fe5ef52
Caen De Silva <caen@desilva.se>
2024-04-14 20:18:51 +0200

    Fix comment link

--- packages/framework/tests/Feature/HydeKernelTest.php
+++ packages/framework/tests/Feature/HydeKernelTest.php
@@ -301,7 +301,7 @@ class HydeKernelTest extends TestCase
 
     public function testVersionConstantIsAValidSemverString()
     {
-        // https://semver.org/#is-there-a-suggested-regular-expression-regex-to-check-a-semver-stringd
+        // https://semver.org/#is-there-a-suggested-regular-expression-regex-to-check-a-semver-string
         $this->assertMatchesRegularExpression(
             '/^(?P<major>0|[1-9]\d*)\.(?P<minor>0|[1-9]\d*)\.(?P<patch>0|[1-9]\d*)(?:-(?P<prerelease>(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+(?P<buildmetadata>[0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$/',
             HydeKernel::VERSION
