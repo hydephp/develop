0ee9fd0e2b1e7dc55babd703305222b547333a14
Caen De Silva <caen@desilva.se>
2024-04-14 20:23:49 +0200

    Fix formatting

--- packages/framework/tests/Unit/HasTableOfContentsTest.php
+++ packages/framework/tests/Unit/HasTableOfContentsTest.php
@@ -19,7 +19,8 @@ class HasTableOfContentsTest extends UnitTestCase
 
     public function testConstructorCreatesTableOfContentsString()
     {
-        $this->assertSame('<ul class="table-of-contents"><li><a href="#title">Title</a></li></ul>',
+        $this->assertSame(
+            '<ul class="table-of-contents"><li><a href="#title">Title</a></li></ul>',
             str_replace("\n", '', (new DocumentationPage(markdown: '## Title'))->getTableOfContents())
         );
     }
