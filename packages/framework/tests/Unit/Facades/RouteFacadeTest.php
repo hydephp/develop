524e03ec23de3018ce77aa3e31fbd360a5fc97cf
Caen De Silva <caen@desilva.se>
2024-04-14 22:56:15 +0200

    Use kernel and config state shorthands

--- packages/framework/tests/Unit/Facades/RouteFacadeTest.php
+++ packages/framework/tests/Unit/Facades/RouteFacadeTest.php
@@ -20,11 +20,8 @@ use Hyde\Testing\UnitTestCase;
  */
 class RouteFacadeTest extends UnitTestCase
 {
-    public static function setUpBeforeClass(): void
-    {
-        self::needsKernel();
-        self::mockConfig();
-    }
+    protected static bool $needsKernel = true;
+    protected static bool $needsConfig = true;
 
     public function testRouteFacadeAllMethodReturnsAllRoutes()
     {
