617142a189dfdfb8a4ec09279f9ab8c6cdf2bede
Caen De Silva <caen@desilva.se>
2024-04-15 18:03:55 +0200

    Create sdf.meta.php

--- .phpstorm.meta.php/sdf.meta.php
+++ .phpstorm.meta.php/sdf.meta.php
@@ -1,8 +1,12 @@
 <?php
 
 namespace PHPSTORM_META {
+
     override(\app(0), map([
+        '' => '@',
         'navigation.main' => \Hyde\Framework\Features\Navigation\MainNavigationMenu::class,
         'navigation.sidebar' => \Hyde\Framework\Features\Navigation\DocumentationSidebar::class,
     ]));
+
+    expectedArguments(\app(), 0, 'navigation.main', 'navigation.sidebar');
 }
