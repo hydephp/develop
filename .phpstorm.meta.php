2efd122f53beb115e1b8c3480401dcc4369e1d9d
Caen De Silva <caen@desilva.se>
2024-04-15 17:52:12 +0200

    Create .phpstorm.meta.php

---
+++ .phpstorm.meta.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace PHPSTORM_META {
+    override(\app(0), map([
+        'navigation.main' => \Hyde\Framework\Features\Navigation\MainNavigationMenu::class,
+        'navigation.sidebar' => \Hyde\Framework\Features\Navigation\DocumentationSidebar::class,
+    ]));
+}
