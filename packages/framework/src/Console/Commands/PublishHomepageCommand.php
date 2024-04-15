998c54d142f11984aabd3fd43b94aa763b8ab505
Caen De Silva <caen@desilva.se>
2024-04-15 15:00:59 +0200

    Comment code reasoning

--- packages/framework/src/Console/Commands/PublishHomepageCommand.php
+++ packages/framework/src/Console/Commands/PublishHomepageCommand.php
@@ -65,7 +65,7 @@ class PublishHomepageCommand extends Command
         Artisan::call('vendor:publish', [
             '--tag' => $this->options[$selected]['group'] ?? $selected,
             '--force' => true,
-        ], $tagExists ? null : $this->output);
+        ], $tagExists ? null : $this->output); // If the tag doesn't exist, we pass the output to use the called command's error output.
 
         if ($tagExists) {
             $this->infoComment("Published page [$selected]");
