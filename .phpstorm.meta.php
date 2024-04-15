98529993bf72ee975d4d15791a0656cce18d8dc9
Caen De Silva <caen@desilva.se>
2024-04-15 18:03:58 +0200

    Revert "Create sdf.meta.php"
    This reverts commit 617142a189dfdfb8a4ec09279f9ab8c6cdf2bede.

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -1,12 +1,8 @@
 <?php
 
 namespace PHPSTORM_META {
-
     override(\app(0), map([
-        '' => '@',
         'navigation.main' => \Hyde\Framework\Features\Navigation\MainNavigationMenu::class,
         'navigation.sidebar' => \Hyde\Framework\Features\Navigation\DocumentationSidebar::class,
     ]));
-
-    expectedArguments(\app(), 0, 'navigation.main', 'navigation.sidebar');
 }
