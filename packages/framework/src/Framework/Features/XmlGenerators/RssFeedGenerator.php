cb0b3e6a6d25bde2707c0013917b488e05cb9d8e
Caen De Silva <caen@desilva.se>
2024-04-15 10:40:19 +0200

    Split string in two strings and concatenation

--- packages/framework/src/Framework/Features/XmlGenerators/RssFeedGenerator.php
+++ packages/framework/src/Framework/Features/XmlGenerators/RssFeedGenerator.php
@@ -33,8 +33,8 @@ class RssFeedGenerator extends BaseXmlGenerator
 
     protected function constructBaseElement(): void
     {
-        $this->xmlElement = new SimpleXMLElement('<?xml version="1.0" encoding="UTF-8"?>
-            <rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom" xmlns:dc="http://purl.org/dc/elements/1.1/" />');
+        $this->xmlElement = new SimpleXMLElement('<?xml version="1.0" encoding="UTF-8"?>'
+            .'<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom" xmlns:dc="http://purl.org/dc/elements/1.1/" />');
         $this->xmlElement->addChild('channel');
 
         $this->addBaseChannelItems();
