c5e0433b2c206508be0fb0a27743e675fe265e7b
Caen De Silva <caen@desilva.se>
2024-04-30 21:59:23 +0200

    Revert "Merge inline test into test case"
    This reverts commit 8b7ce81c1cea333cef864d4a60a5096c6882508d.

--- packages/framework/src/Framework/Features/Navigation/FilenamePrefixNavigationHelper.php
+++ packages/framework/src/Framework/Features/Navigation/FilenamePrefixNavigationHelper.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Hyde\Framework\Features\Navigation;
 
 use Hyde\Facades\Config;
+use Hyde\Testing\UnitTestCase;
 
 use function assert;
 use function explode;
@@ -47,5 +48,56 @@ class FilenamePrefixNavigationHelper
         $parts[0] = (int) $parts[0];
 
         return $parts;
+    }
+}
+
+class FilenamePrefixNavigationHelperTest extends UnitTestCase
+{
+    public function testEnabledReturnsTrueWhenEnabled()
+    {
+        $this->assertTrue(FilenamePrefixNavigationHelper::enabled());
+    }
+
+    public function testEnabledReturnsFalseWhenDisabled()
+    {
+        self::mockConfig(['hyde.filename_page_ordering' => false]);
+
+        $this->assertFalse(FilenamePrefixNavigationHelper::enabled());
+    }
+
+    public function testIdentifiersWithNumericalPrefixesAreDetected()
+    {
+        $this->assertTrue(FilenamePrefixNavigationHelper::isIdentifierNumbered('01-home.md'));
+        $this->assertTrue(FilenamePrefixNavigationHelper::isIdentifierNumbered('02-about.md'));
+        $this->assertTrue(FilenamePrefixNavigationHelper::isIdentifierNumbered('03-contact.md'));
+    }
+
+    public function testIdentifiersWithoutNumericalPrefixesAreNotDetected()
+    {
+        $this->assertFalse(FilenamePrefixNavigationHelper::isIdentifierNumbered('home.md'));
+        $this->assertFalse(FilenamePrefixNavigationHelper::isIdentifierNumbered('about.md'));
+        $this->assertFalse(FilenamePrefixNavigationHelper::isIdentifierNumbered('contact.md'));
+    }
+
+    public function testSplitNumberAndIdentifier()
+    {
+        $this->assertSame([1, 'home.md'], FilenamePrefixNavigationHelper::splitNumberAndIdentifier('01-home.md'));
+        $this->assertSame([2, 'about.md'], FilenamePrefixNavigationHelper::splitNumberAndIdentifier('02-about.md'));
+        $this->assertSame([3, 'contact.md'], FilenamePrefixNavigationHelper::splitNumberAndIdentifier('03-contact.md'));
+    }
+
+    public function testSplitNumberAndIdentifierWithMultipleDigits()
+    {
+        $this->assertSame([123, 'home.md'], FilenamePrefixNavigationHelper::splitNumberAndIdentifier('123-home.md'));
+        $this->assertSame([456, 'about.md'], FilenamePrefixNavigationHelper::splitNumberAndIdentifier('456-about.md'));
+        $this->assertSame([789, 'contact.md'], FilenamePrefixNavigationHelper::splitNumberAndIdentifier('789-contact.md'));
+    }
+
+    public function testSplitNumberAndIdentifierThrowsExceptionWhenIdentifierIsNotNumbered()
+    {
+        $this->expectException(\AssertionError::class);
+        // $this->expectExceptionMessage('Identifier "home.md" is not numbered.');
+
+        FilenamePrefixNavigationHelper::splitNumberAndIdentifier('home.md');
     }
 }
