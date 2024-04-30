b2ca812d67bfedc4901edb83198130fe5ebe52cc
Caen De Silva <caen@desilva.se>
2024-04-30 20:26:32 +0200

    Create FilenamePrefixNavigationHelper.php

---
+++ packages/framework/src/Framework/Features/Navigation/FilenamePrefixNavigationHelper.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hyde\Framework\Features\Navigation;
+
+/**
+ * @internal Helper class for the new Filename Prefix Navigation feature.
+ *
+ * @experimental The code herein may be moved to more appropriate locations in the future.
+ */
+class FilenamePrefixNavigationHelper
+{
+    //
+}
