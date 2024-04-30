7e5a1c951a5ba70590f7bdf855d12d5dee1ce4bf
Caen De Silva <caen@desilva.se>
2024-04-30 16:37:02 +0200

    Create FilenamePrefixNavigationPriorityTest.php

---
+++ packages/framework/tests/Feature/FilenamePrefixNavigationPriorityTest.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hyde\Framework\Testing\Feature;
+
+use Hyde\Testing\TestCase;
+
+/**
+ * High level test for the feature that allows navigation items to be sorted by filename prefix.
+ *
+ * @covers \Hyde\Framework\Features\Navigation\MainNavigationMenu
+ * @covers \Hyde\Framework\Features\Navigation\DocumentationSidebar
+ */
+class FilenamePrefixNavigationPriorityTest extends TestCase
+{
+    //
+}
