7f733e0ca089450373b73c73f3a14a1ddf381db1
Caen De Silva <caen@desilva.se>
2024-04-30 16:44:41 +0200

    Define fixture data

--- packages/framework/tests/Feature/FilenamePrefixNavigationPriorityTest.php
+++ packages/framework/tests/Feature/FilenamePrefixNavigationPriorityTest.php
@@ -20,28 +20,51 @@ class FilenamePrefixNavigationPriorityTest extends TestCase
     protected function fixtureFlatMain(): array
     {
         return [
-            //
+            '01-home.md',
+            '02-about.md',
+            '03-contact.md',
         ];
     }
 
     protected function fixtureGroupedMain(): array
     {
         return [
-            //
+            '01-home.md',
+            '02-about.md',
+            '03-contact.md',
+            '04-api' => [
+                '01-readme.md',
+                '02-installation.md',
+                '03-getting-started.md',
+            ],
         ];
     }
 
     protected function fixtureFlatSidebar(): array
     {
         return [
-            //
+            '01-readme.md',
+            '02-installation.md',
+            '03-getting-started.md',
         ];
     }
 
     protected function fixtureGroupedSidebar(): array
     {
         return [
-            //
+            '01-readme.md',
+            '02-installation.md',
+            '03-getting-started.md',
+            '04-introduction' => [
+                '01-features.md',
+                '02-extensions.md',
+                '03-configuration.md',
+            ],
+            '05-advanced' => [
+                '01-features.md',
+                '02-extensions.md',
+                '03-configuration.md',
+            ],
         ];
     }
 }
