8e929965574a853d0c91a3f744a466579d25b223
Caen De Silva <caen@desilva.se>
2024-05-01 18:18:01 +0200

    Map with keys

--- packages/framework/tests/Feature/FilenamePrefixNavigationPriorityTest.php
+++ packages/framework/tests/Feature/FilenamePrefixNavigationPriorityTest.php
@@ -147,12 +147,12 @@ class FilenamePrefixNavigationPriorityTest extends TestCase
     protected function assertOrder(array $expected): void
     {
         $menu = NavigationMenuGenerator::handle(MainNavigationMenu::class);
-        $actual = $menu->getItems()->map(function (NavigationItem|NavigationGroup $item) {
+        $actual = $menu->getItems()->mapWithKeys(function (NavigationItem|NavigationGroup $item, int $key) {
             if ($item instanceof NavigationGroup) {
-                return $item->getItems()->map(fn ($item) => $item->getPage()->getRouteKey())->all();
+                return [$item->getGroupKey() => $item->getItems()->map(fn ($item) => $item->getPage()->getRouteKey())->all()];
             }
 
-            return $item->getPage()->getRouteKey();
+            return [$key => $item->getPage()->getRouteKey()];
         })->all();
 
         $this->assertSame($expected, $actual);
