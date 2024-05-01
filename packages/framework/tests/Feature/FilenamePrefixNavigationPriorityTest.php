a326e027754df1e3f433bb4404c3acac0570c1b5
Caen De Silva <caen@desilva.se>
2024-05-01 18:19:29 +0200

    Get only core part for comparison

--- packages/framework/tests/Feature/FilenamePrefixNavigationPriorityTest.php
+++ packages/framework/tests/Feature/FilenamePrefixNavigationPriorityTest.php
@@ -7,6 +7,7 @@ namespace Hyde\Framework\Testing\Feature;
 use Hyde\Hyde;
 use Hyde\Facades\Config;
 use Hyde\Testing\TestCase;
+use Illuminate\Support\Str;
 use Hyde\Pages\MarkdownPage;
 use Hyde\Framework\Features\Navigation\NavigationItem;
 use Hyde\Framework\Features\Navigation\NavigationGroup;
@@ -150,7 +151,7 @@ class FilenamePrefixNavigationPriorityTest extends TestCase
         $actual = $menu->getItems()->mapWithKeys(function (NavigationItem|NavigationGroup $item, int $key) {
             if ($item instanceof NavigationGroup) {
                 return [$item->getGroupKey() => $item->getItems()->map(function ($item) {
-                    return $item->getPage()->getRouteKey();
+                    return Str::afterLast($item->getPage()->getRouteKey(), '/');
                 })->all()];
             }
 
