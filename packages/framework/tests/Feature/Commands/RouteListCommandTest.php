384fbae07d39fb351f3aadaf5cc92f9ee8432ffd
Caen De Silva <caen@desilva.se>
2024-06-12 15:05:47 +0200

    Test console route list with text format option

--- packages/framework/tests/Feature/Commands/RouteListCommandTest.php
+++ packages/framework/tests/Feature/Commands/RouteListCommandTest.php
@@ -98,6 +98,22 @@ class RouteListCommandTest extends TestCase
             ]])->assertExitCode(0);
     }
 
+    public function testConsoleRouteListWithTextFormatOption()
+    {
+        $this->artisan('route:list --format=txt')
+            ->expectsTable($this->headers(), [[
+                'BladePage',
+                '_pages/404.blade.php',
+                '_site/404.html',
+                '404',
+            ], [
+                'BladePage',
+                '_pages/index.blade.php',
+                '_site/index.html',
+                'index',
+            ]])->assertExitCode(0);
+    }
+
     protected function headers(): array
     {
         return ['Page Type', 'Source File', 'Output File', 'Route Key'];
