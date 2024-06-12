4e7c28a31287409c666515583dac0fa6fae7211f
Caen De Silva <caen@desilva.se>
2024-06-12 20:27:17 +0200

    Create SitesWithoutBaseUrlAreHandledGracefullyTest.php

---
+++ packages/framework/tests/Feature/SitesWithoutBaseUrlAreHandledGracefullyTest.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hyde\Framework\Testing\Feature;
+
+use Hyde\Testing\TestCase;
+
+/**
+ * High level test to ensure that sites without a base URL are handled gracefully.
+ *
+ * @coversNothing This test is not testing a specific class, but a general feature of the framework.
+ */
+class SitesWithoutBaseUrlAreHandledGracefullyTest extends TestCase
+{
+    //
+}
