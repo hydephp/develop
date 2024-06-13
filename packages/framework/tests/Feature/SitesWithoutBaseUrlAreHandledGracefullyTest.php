3da686930a4440706810551ad61e7cfaeb69ab27
Caen De Silva <caen@desilva.se>
2024-06-13 10:40:28 +0200

    Document test abstract

--- packages/framework/tests/Feature/SitesWithoutBaseUrlAreHandledGracefullyTest.php
+++ packages/framework/tests/Feature/SitesWithoutBaseUrlAreHandledGracefullyTest.php
@@ -9,6 +9,14 @@ use Hyde\Testing\TestCase;
 /**
  * High level test to ensure that sites without a base URL are handled gracefully.
  *
+ * For example: In case a user forgot to set a base URL, we don't want their site
+ * to have localhost links in the compiled HTML output since that would break
+ * things when deployed to production. So we fall back to relative links.
+ *
+ * Some things like sitemaps and RSS feeds cannot be generated without a base URL,
+ * as their schemas generally do not allow relative URLs. In those cases, we
+ * don't generate files at all, and we don't add any links to them either.
+ *
  * @coversNothing This test is not testing a specific class, but a general feature of the framework.
  */
 class SitesWithoutBaseUrlAreHandledGracefullyTest extends TestCase
