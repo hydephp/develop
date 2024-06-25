2783e0d4949896014277661f5c859cb5a169e6b2
Caen De Silva <caen@desilva.se>
2024-06-25 16:15:09 +0200

    Document exception scope

--- packages/framework/src/Framework/Exceptions/ParseException.php
+++ packages/framework/src/Framework/Exceptions/ParseException.php
@@ -6,6 +6,9 @@ namespace Hyde\Framework\Exceptions;
 
 use RuntimeException;
 
+/**
+ * Exception class thrown when an error occurs during parsing.
+ */
 class ParseException extends RuntimeException
 {
     //
