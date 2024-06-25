2fba023e1b7f556128d1ab3265007a80f3025198
Caen De Silva <caen@desilva.se>
2024-06-25 16:16:27 +0200

    Mark ParseException class as experimental

--- packages/framework/src/Framework/Exceptions/ParseException.php
+++ packages/framework/src/Framework/Exceptions/ParseException.php
@@ -6,6 +6,7 @@ namespace Hyde\Framework\Exceptions;
 
 use RuntimeException;
 
+/** @experimental This class may change significantly before its release. */
 class ParseException extends RuntimeException
 {
     //
