7f4d48db4577c3070247f9eda58c44ea3a1fc580
Caen De Silva <caen@desilva.se>
2024-06-25 16:17:31 +0200

    Override parent constructor

--- packages/framework/src/Framework/Exceptions/ParseException.php
+++ packages/framework/src/Framework/Exceptions/ParseException.php
@@ -4,10 +4,14 @@ declare(strict_types=1);
 
 namespace Hyde\Framework\Exceptions;
 
+use Throwable;
 use RuntimeException;
 
 /** @experimental This class may change significantly before its release. */
 class ParseException extends RuntimeException
 {
-    //
+    public function __construct(string $message = '', int $code = 0, ?Throwable $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+    }
 }
