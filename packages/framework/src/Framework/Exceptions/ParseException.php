e5d4c820f5c5726571bfe16283b72a665a62f9bb
Caen De Silva <caen@desilva.se>
2024-06-25 16:22:45 +0200

    Refactor to move exception message assembly to custom exception

--- packages/framework/src/Framework/Exceptions/ParseException.php
+++ packages/framework/src/Framework/Exceptions/ParseException.php
@@ -6,12 +6,22 @@ namespace Hyde\Framework\Exceptions;
 
 use Throwable;
 use RuntimeException;
+use Illuminate\Support\Arr;
+
+use function rtrim;
+use function sprintf;
+use function explode;
+use function ucfirst;
+use function str_replace;
 
 /** @experimental This class may change significantly before its release. */
 class ParseException extends RuntimeException
 {
-    public function __construct(string $message = '', int $code = 0, ?Throwable $previous = null)
+    public function __construct(string $file = '', ?Throwable $previous = null)
     {
-        parent::__construct($message, $code, $previous);
+        $extension = Arr::last(explode('.', $file));
+        $type = ucfirst(str_replace(['md', 'yml'], ['markdown', 'yaml'], $extension));
+
+        parent::__construct(sprintf("Invalid %s in file: '%s' (%s)", $type, $file, rtrim($previous->getMessage(), '.')), previous: $previous);
     }
 }
