3a7373641cba3d7237a923bc12de794ae307b795
Caen De Silva <caen@desilva.se>
2024-06-26 10:03:49 +0200

    Create IncludesFacadeUnitTest.php

---
+++ packages/framework/tests/Unit/IncludesFacadeUnitTest.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hyde\Framework\Testing\Unit;
+
+use Hyde\Testing\UnitTestCase;
+
+/**
+ * @covers \Hyde\Support\Includes
+ */
+class IncludesFacadeUnitTest extends UnitTestCase
+{
+    //
+}
