59448a6303036ef734a8821de0dfbd9ee508cc7b
Caen De Silva <caen@desilva.se>
2024-06-26 20:55:41 +0200

    Revert "Wrap declaration in else block"
    This reverts commit 2282b8e78282f1e7fdf1d2cb10544d39c7256a8c.

--- packages/framework/src/Support/Includes.php
+++ packages/framework/src/Support/Includes.php
@@ -93,10 +93,9 @@ class Includes
                 $markdown = $default;
                 return new HtmlString(trim(Markdown::render($markdown, MarkdownDocument::class)));
             }
-        } else {
-            $markdown = Filesystem::get($path);
         }
 
+        $markdown = Filesystem::get($path);
         return new HtmlString(trim(Markdown::render($markdown, MarkdownDocument::class)));
     }
 
