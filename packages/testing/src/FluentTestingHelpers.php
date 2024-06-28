407279fabc592b48d8c8ea59e2de572970561c3a
Caen De Silva <caen@desilva.se>
2024-06-28 11:39:05 +0200

    Move testing helpers to trait

--- packages/testing/src/FluentTestingHelpers.php
+++ packages/testing/src/FluentTestingHelpers.php
@@ -4,7 +4,46 @@ declare(strict_types=1);
 
 namespace Hyde\Testing;
 
+use Hyde\Hyde;
+
+use function config;
+use function file_get_contents;
+use function Hyde\normalize_newlines;
+
 trait FluentTestingHelpers
 {
-    //
+    protected function assertFileEqualsString(string $string, string $path, bool $strict = false): void
+    {
+        if ($strict) {
+            $this->assertSame($string, file_get_contents(Hyde::path($path)));
+        } else {
+            $this->assertEquals(normalize_newlines($string), normalize_newlines(file_get_contents(Hyde::path($path))));
+        }
+    }
+
+    /**
+     * Disable the throwing of exceptions on console commands for the duration of the test.
+     *
+     * Note that this only affects commands using the {@see \Hyde\Console\Concerns\Command::safeHandle()} method.
+     */
+    protected function throwOnConsoleException(bool $throw = true): void
+    {
+        config(['app.throw_on_console_exception' => $throw]);
+    }
+
+    /**
+     * Set the site URL for the duration of the test.
+     */
+    protected function withSiteUrl(?string $url = 'https://example.com'): void
+    {
+        config(['hyde.url' => $url]);
+    }
+
+    /**
+     * Remove the site URL for the duration of the test.
+     */
+    protected function withoutSiteUrl(): void
+    {
+        config(['hyde.url' => null]);
+    }
 }
