ce34eecb6dfe2c72412c4b5cb9857cf165f2e262
Caen De Silva <caen@desilva.se>
2024-06-28 16:54:27 +0200

    Remove the deprecated global `unslash` function

--- packages/framework/src/helpers.php
+++ packages/framework/src/helpers.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace {
     use Hyde\Foundation\HydeKernel;
-    use JetBrains\PhpStorm\Deprecated;
 
     if (! function_exists('hyde')) {
         /**
@@ -13,21 +12,6 @@ namespace {
         function hyde(): HydeKernel
         {
             return HydeKernel::getInstance();
-        }
-    }
-
-    if (! function_exists('unslash')) {
-        /**
-         * Remove trailing slashes from the start and end of a string.
-         *
-         * @deprecated This function will be replaced by {@see \Hyde\unslash()} in v2.0
-         *
-         * @codeCoverageIgnore This function is deprecated and will be removed in a future release.
-         */
-        #[Deprecated(reason: 'Replaced by the \Hyde\unslash() function', replacement: '\Hyde\unslash(%parametersList%)', since: '1.7.0')]
-        function unslash(string $string): string
-        {
-            return \Hyde\unslash($string);
         }
     }
 
