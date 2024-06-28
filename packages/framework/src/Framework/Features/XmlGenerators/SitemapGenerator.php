8ae31f9abe3b18165d67e9a792a9cf34450497d2
Caen De Silva <caen@desilva.se>
2024-06-28 13:46:25 +0200

    Document the priority algorithm intentions

--- packages/framework/src/Framework/Features/XmlGenerators/SitemapGenerator.php
+++ packages/framework/src/Framework/Features/XmlGenerators/SitemapGenerator.php
@@ -61,28 +61,34 @@ class SitemapGenerator extends BaseXmlGenerator
         return date('c', @Filesystem::lastModified($file) ?: Carbon::now()->timestamp);
     }
 
+    /** Intelligently find a good priority for the given page based on assumptions about the site structure. */
     protected function getPriority(string $pageClass, string $identifier): string
     {
         // The default priority, unless we find a better match.
         $priority = 0.5;
 
         if (in_array($pageClass, [BladePage::class, MarkdownPage::class])) {
+            // These pages are usually high up in the site hierarchy, so they get a higher priority.
             $priority = 0.9;
 
             if ($identifier === 'index') {
+                // The homepage is the most important page, so it gets the highest priority.
                 $priority = 1;
             }
         }
 
         if ($pageClass === DocumentationPage::class) {
+            // If a site has documentation pages, they are usually important as well.
             $priority = 0.9;
         }
 
         if ($pageClass === MarkdownPost::class) {
+            // Posts are usually less important than pages as there may be many of them.
             $priority = 0.75;
         }
 
         if ($identifier === '404') {
+            // 404 pages are rarely important to index, so they get a lower priority.
             $priority = 0.5;
         }
 
