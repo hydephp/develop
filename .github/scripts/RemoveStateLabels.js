db9dc2f45bc7e5b0f0b46e489bd71a0d834af4c0
Caen De Silva <caen@desilva.se>
2024-06-30 20:30:47 +0200

    Remove more state control labels

--- .github/scripts/RemoveStateLabels.js
+++ .github/scripts/RemoveStateLabels.js
@@ -14,4 +14,24 @@ module.exports = async ({github, context}) => {
             repo: context.repo.repo
         });
     }
+
+    const hasPausePagesLabel = labels.find(label => label.name === 'pause-pages');
+    if (hasPausePagesLabel) {
+        await github.rest.issues.removeLabel({
+            issue_number: context.issue.number,
+            name: 'pause-pages',
+            owner: context.repo.owner,
+            repo: context.repo.repo
+        });
+    }
+
+    const hasWTDLabel = labels.find(label => label.name === 'WTD');
+    if (hasWTDLabel) {
+        await github.rest.issues.removeLabel({
+            issue_number: context.issue.number,
+            name: 'WTD',
+            owner: context.repo.owner,
+            repo: context.repo.repo
+        });
+    }
 }
