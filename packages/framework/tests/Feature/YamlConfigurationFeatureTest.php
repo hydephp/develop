2634b26d8f2211952c16346b9d6363a941b35c3a
Caen De Silva <caen@desilva.se>
2024-07-01 21:27:58 +0200

    Test covers LoadConfiguration

--- packages/framework/tests/Feature/YamlConfigurationFeatureTest.php
+++ packages/framework/tests/Feature/YamlConfigurationFeatureTest.php
@@ -12,6 +12,7 @@ use Illuminate\Support\Facades\Config;
 /**
  * Test the Yaml configuration feature.
  *
+ * @covers \Hyde\Foundation\Internal\LoadConfiguration
  * @covers \Hyde\Foundation\Internal\LoadYamlConfiguration
  * @covers \Hyde\Foundation\Internal\YamlConfigurationRepository
  */
