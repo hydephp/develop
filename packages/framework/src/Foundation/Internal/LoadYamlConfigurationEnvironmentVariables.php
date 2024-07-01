8b8de6465f73a36fed79e4abef207ff28802403b
Caen De Silva <caen@desilva.se>
2024-07-01 16:27:31 +0200

    Create LoadYamlConfigurationEnvironmentVariables.php

---
+++ packages/framework/src/Foundation/Internal/LoadYamlConfigurationEnvironmentVariables.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hyde\Foundation\Internal;
+
+/**
+ * @internal Inject environment variables parsed from the YAML configuration file.
+ */
+class LoadYamlConfigurationEnvironmentVariables
+{
+    /**
+     * Performs a core task that needs to be performed on
+     * early stages of the framework.
+     */
+    public function bootstrap(): void
+    {
+        //
+    }
+}
