a7e65925fcb2eb8232b8897d46de0e9da876568a
Caen De Silva <caen@desilva.se>
2024-07-01 20:37:58 +0200

    Create EnvDataRepository.php

---
+++ packages/framework/src/Foundation/Internal/EnvDataRepository.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hyde\Foundation\Internal;
+
+/**
+ * @internal Contains dynamic environment data.
+ */
+class EnvDataRepository
+{
+    protected array $data = [];
+}
