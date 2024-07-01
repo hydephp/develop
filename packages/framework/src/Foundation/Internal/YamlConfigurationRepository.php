04993d71e547858d31a81a120b98e1dba28da143
Caen De Silva <caen@desilva.se>
2024-07-01 16:31:33 +0200

    Create YamlConfigurationRepository.php

---
+++ packages/framework/src/Foundation/Internal/YamlConfigurationRepository.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hyde\Foundation\Internal;
+
+/**
+ * @internal Contains shared logic for loading the YAML configuration file.
+ */
+class YamlConfigurationRepository
+{
+    //
+}
