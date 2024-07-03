6b915b08dd79f61a04f2acde6255475487267412
Caen De Silva <caen@desilva.se>
2024-07-03 17:58:36 +0200

    Draft test abstract

--- packages/framework/tests/Feature/HighLevelYamlConfigurationFeatureTest.php
+++ packages/framework/tests/Feature/HighLevelYamlConfigurationFeatureTest.php
@@ -6,6 +6,15 @@ namespace Hyde\Framework\Testing\Feature;
 
 use Hyde\Testing\TestCase;
 
+/**
+ * High level test for the Yaml configuration feature.
+ *
+ * @see \Hyde\Framework\Testing\Feature\YamlConfigurationFeatureTest
+ *
+ * @covers \Hyde\Foundation\Internal\LoadYamlConfiguration
+ * @covers \Hyde\Foundation\Internal\LoadYamlEnvironmentVariables
+ * @covers \Hyde\Foundation\Internal\YamlConfigurationRepository
+ */
 class HighLevelYamlConfigurationFeatureTest extends TestCase
 {
     //
