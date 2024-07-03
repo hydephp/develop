d2f521f58167e8ddddb5ac530d71955f1f1ece61
Caen De Silva <caen@desilva.se>
2024-07-03 18:14:07 +0200

    Revert "Wrap container access in tap call"
    This reverts commit 54241ce763e8e8df0666377ad575413aba3ba722.

--- packages/framework/src/Foundation/Internal/LoadYamlConfiguration.php
+++ packages/framework/src/Foundation/Internal/LoadYamlConfiguration.php
@@ -6,9 +6,7 @@ namespace Hyde\Foundation\Internal;
 
 use Illuminate\Support\Arr;
 use Hyde\Foundation\Application;
-use Illuminate\Config\Repository;
 
-use function tap;
 use function array_merge;
 
 /**
@@ -38,11 +36,11 @@ class LoadYamlConfiguration
         $this->yaml = $app->make(YamlConfigurationRepository::class);
 
         if ($this->yaml->hasYamlConfigFile()) {
-            tap($app->make('config'), function (Repository $config): void {
-                $this->config = $config->all();
-                $this->mergeParsedConfiguration();
-                $config->set($this->config);
-            });
+            $this->config = $app->make('config')->all();
+
+            $this->mergeParsedConfiguration();
+
+            $app->make('config')->set($this->config);
         }
     }
 
