bda5124f094e2b001577f990627107ad9213718b
Caen De Silva <caen@desilva.se>
2024-07-03 18:10:12 +0200

    Test the YAML configuration feature

--- packages/framework/tests/Feature/HighLevelYamlConfigurationFeatureTest.php
+++ packages/framework/tests/Feature/HighLevelYamlConfigurationFeatureTest.php
@@ -26,6 +26,9 @@ class HighLevelYamlConfigurationFeatureTest extends TestCase
 
     protected function tearDown(): void
     {
+        unlink('hyde.yml');
+        unlink('config/custom.php');
+
         $this->clearEnvVars();
 
         parent::tearDown();
@@ -33,7 +36,44 @@ class HighLevelYamlConfigurationFeatureTest extends TestCase
 
     protected function setUpConfigurationBeforeApplicationBoots(): void
     {
-        //
+        file_put_contents('hyde.yml', <<<'YAML'
+        hyde:
+            name: Yaml Site Name
+        docs:
+            sidebar_order:
+                # Reversed compared to the default order
+                - getting-started
+                - installation
+                - readme
+        custom:
+            setting_one: Override
+            setting_two: Added
+        YAML);
+
+        file_put_contents('config/custom.php', <<<'PHP'
+        <?php   
+        return [
+            'setting_one' => 'Default',
+            'setting_three' => 'Inherited'
+        ];
+        PHP);
+    }
+
+    public function testTestTheYamlConfigurationFeature()
+    {
+        $config = config()->all();
+
+        $this->assertSame('Yaml Site Name', env('SITE_NAME'));
+        $this->assertSame('Yaml Site Name', $_ENV['SITE_NAME']);
+        $this->assertSame('Yaml Site Name', $_SERVER['SITE_NAME']);
+
+        $this->assertSame('Yaml Site Name', $config['hyde']['name']);
+        $this->assertSame('Yaml Site Name Docs', $config['docs']['sidebar']['header']);
+        $this->assertSame(['getting-started', 'installation', 'readme'], $config['docs']['sidebar_order']);
+
+        $this->assertSame('Override', $config['custom']['setting_one']);
+        $this->assertSame('Added', $config['custom']['setting_two']);
+        $this->assertSame('Inherited', $config['custom']['setting_three']);
     }
 
     protected function clearEnvVars(): void
