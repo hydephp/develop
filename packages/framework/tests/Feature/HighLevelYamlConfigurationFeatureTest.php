3649b02a566b5b168e57ae072de63313c5b4dbe4
Caen De Silva <caen@desilva.se>
2024-07-03 17:59:01 +0200

    Clean the environment after the test

--- packages/framework/tests/Feature/HighLevelYamlConfigurationFeatureTest.php
+++ packages/framework/tests/Feature/HighLevelYamlConfigurationFeatureTest.php
@@ -17,5 +17,17 @@ use Hyde\Testing\TestCase;
  */
 class HighLevelYamlConfigurationFeatureTest extends TestCase
 {
-    //
+    protected function tearDown(): void
+    {
+        $this->clearEnvVars();
+
+        parent::tearDown();
+    }
+
+    protected function clearEnvVars(): void
+    {
+        // Todo: Can we access loader? https://github.com/vlucas/phpdotenv/pull/107/files
+        putenv('SITE_NAME');
+        unset($_ENV['SITE_NAME'], $_SERVER['SITE_NAME']);
+    }
 }
