d8017ac77b244998a206b25e6aeeb4dae156945e
Caen De Silva <caen@desilva.se>
2024-07-03 20:32:28 +0200

    Normalize whitespace formatting

--- packages/framework/tests/Feature/Commands/MakePostCommandTest.php
+++ packages/framework/tests/Feature/Commands/MakePostCommandTest.php
@@ -25,19 +25,17 @@ class MakePostCommandTest extends TestCase
             ->expectsQuestion('What is your (the author\'s) name?', 'PHPUnit')
             ->expectsQuestion('What is the primary category of the post?', 'general')
             ->expectsOutput('Creating a post with the following details:')
-
             ->expectsOutput('Title: Test Post')
             ->expectsOutput('Description: A short description')
             ->expectsOutput('Category: general')
             ->expectsOutput('Author: PHPUnit')
             ->expectsOutputToContain('Date: '.date('Y-m-d')) // Don't check min/sec to avoid flaky tests
             ->expectsOutput('Identifier: test-post')
-
             ->expectsConfirmation('Do you wish to continue?', 'yes')
-
             ->assertExitCode(0);
 
         $this->assertFileExists(Hyde::path('_posts/test-post.md'));
+
         $this->assertStringContainsString(
             "title: 'Test Post'",
             file_get_contents(Hyde::path('_posts/test-post.md'))
@@ -53,10 +51,8 @@ class MakePostCommandTest extends TestCase
             ->expectsQuestion('What is your (the author\'s) name?', 'PHPUnit')
             ->expectsQuestion('What is the primary category of the post?', 'general')
             ->expectsOutput('Creating a post with the following details:')
-
             ->expectsConfirmation('Do you wish to continue?', 'yes')
             ->expectsOutput('If you want to overwrite the file supply the --force flag.')
-
             ->assertExitCode(409);
 
         $this->assertStringContainsString(
@@ -68,6 +64,7 @@ class MakePostCommandTest extends TestCase
     public function testThatFilesAreOverwrittenWhenForceFlagIsSet()
     {
         $this->file('_posts/test-post.md', 'This should be overwritten');
+
         $this->artisan('make:post --force')
             ->expectsQuestion('What is the title of the post?', 'Test Post')
             ->expectsQuestion('Write a short post excerpt/description', 'A short description')
@@ -75,13 +72,13 @@ class MakePostCommandTest extends TestCase
             ->expectsQuestion('What is the primary category of the post?', 'general')
             ->expectsOutput('Creating a post with the following details:')
             ->expectsConfirmation('Do you wish to continue?', 'yes')
-
             ->assertExitCode(0);
 
         $this->assertStringNotContainsString(
             'This should be overwritten',
             file_get_contents(Hyde::path('_posts/test-post.md'))
         );
+
         $this->assertStringContainsString(
             "title: 'Test Post'",
             file_get_contents(Hyde::path('_posts/test-post.md'))
@@ -91,26 +88,26 @@ class MakePostCommandTest extends TestCase
     public function testThatTitleCanBeSpecifiedInCommandSignature()
     {
         $this->cleanUpWhenDone('_posts/test-post.md');
+
         $this->artisan('make:post "Test Post"')
             ->expectsOutputToContain('Selected title: Test Post')
             ->expectsQuestion('Write a short post excerpt/description', 'A short description')
             ->expectsQuestion('What is your (the author\'s) name?', 'PHPUnit')
             ->expectsQuestion('What is the primary category of the post?', 'general')
             ->expectsConfirmation('Do you wish to continue?', 'yes')
-
             ->assertExitCode(0);
     }
 
     public function testThatCommandCanBeCanceled()
     {
         $this->artisan('make:post "Test Post"')
-        ->expectsOutputToContain('Selected title: Test Post')
-        ->expectsQuestion('Write a short post excerpt/description', 'A short description')
-        ->expectsQuestion('What is your (the author\'s) name?', 'PHPUnit')
-        ->expectsQuestion('What is the primary category of the post?', 'general')
-        ->expectsConfirmation('Do you wish to continue?')
-        ->expectsOutput('Aborting.')
-        ->assertExitCode(130);
+            ->expectsOutputToContain('Selected title: Test Post')
+            ->expectsQuestion('Write a short post excerpt/description', 'A short description')
+            ->expectsQuestion('What is your (the author\'s) name?', 'PHPUnit')
+            ->expectsQuestion('What is the primary category of the post?', 'general')
+            ->expectsConfirmation('Do you wish to continue?')
+            ->expectsOutput('Aborting.')
+            ->assertExitCode(130);
 
         $this->assertFileDoesNotExist(Hyde::path('_posts/test-post.md'));
     }
