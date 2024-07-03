cede6a05655992939e3fa2e54a8030e1216aac22
Caen De Silva <caen@desilva.se>
2024-07-03 20:30:43 +0200

    Use fault tolerant file memory helpers

--- packages/framework/tests/Feature/Commands/MakePostCommandTest.php
+++ packages/framework/tests/Feature/Commands/MakePostCommandTest.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Hyde\Framework\Testing\Feature\Commands;
 
-use Hyde\Facades\Filesystem;
 use Hyde\Hyde;
 use Hyde\Testing\TestCase;
 
@@ -18,6 +17,7 @@ class MakePostCommandTest extends TestCase
     {
         // Assert that no old file exists which would cause issues
         $this->assertFileDoesNotExist(Hyde::path('_posts/test-post.md'));
+        $this->cleanUpWhenDone('_posts/test-post.md');
 
         $this->artisan('make:post')
             ->expectsQuestion('What is the title of the post?', 'Test Post')
@@ -42,13 +42,11 @@ class MakePostCommandTest extends TestCase
             "title: 'Test Post'",
             file_get_contents(Hyde::path('_posts/test-post.md'))
         );
-
-        Filesystem::unlink('_posts/test-post.md');
     }
 
     public function testThatFilesAreNotOverwrittenWhenForceFlagIsNotSet()
     {
-        file_put_contents(Hyde::path('_posts/test-post.md'), 'This should not be overwritten');
+        $this->file('_posts/test-post.md', 'This should not be overwritten');
         $this->artisan('make:post')
             ->expectsQuestion('What is the title of the post?', 'Test Post')
             ->expectsQuestion('Write a short post excerpt/description', 'A short description')
@@ -65,13 +63,11 @@ class MakePostCommandTest extends TestCase
             'This should not be overwritten',
             file_get_contents(Hyde::path('_posts/test-post.md'))
         );
-
-        Filesystem::unlink('_posts/test-post.md');
     }
 
     public function testThatFilesAreOverwrittenWhenForceFlagIsSet()
     {
-        file_put_contents(Hyde::path('_posts/test-post.md'), 'This should be overwritten');
+        $this->file('_posts/test-post.md', 'This should be overwritten');
         $this->artisan('make:post --force')
             ->expectsQuestion('What is the title of the post?', 'Test Post')
             ->expectsQuestion('Write a short post excerpt/description', 'A short description')
@@ -90,12 +86,11 @@ class MakePostCommandTest extends TestCase
             "title: 'Test Post'",
             file_get_contents(Hyde::path('_posts/test-post.md'))
         );
-
-        Filesystem::unlink('_posts/test-post.md');
     }
 
     public function testThatTitleCanBeSpecifiedInCommandSignature()
     {
+        $this->cleanUpWhenDone('_posts/test-post.md');
         $this->artisan('make:post "Test Post"')
             ->expectsOutputToContain('Selected title: Test Post')
             ->expectsQuestion('Write a short post excerpt/description', 'A short description')
@@ -104,8 +99,6 @@ class MakePostCommandTest extends TestCase
             ->expectsConfirmation('Do you wish to continue?', 'yes')
 
             ->assertExitCode(0);
-
-        Filesystem::unlink('_posts/test-post.md');
     }
 
     public function testThatCommandCanBeCanceled()
