64f3e3617970cc706d96884dabbb0bfc9f42fdd1
Caen De Silva <caen@desilva.se>
2024-07-05 19:01:49 +0200

    Reorder tests

--- packages/framework/tests/Feature/HydeKernelTest.php
+++ packages/framework/tests/Feature/HydeKernelTest.php
@@ -547,6 +547,13 @@ class HydeKernelTest extends TestCase
         ], $kernel->getAuthors()->toArray());
     }
 
+    public function testGetAuthorsReturnsSingletonCollection()
+    {
+        $kernel = new HydeKernel();
+
+        $this->assertSame($kernel->getAuthors(), $kernel->getAuthors());
+    }
+
     public function testGetAuthorsReturnsEmptyCollectionWhenNoAuthorsDefined()
     {
         $kernel = new HydeKernel();
@@ -555,13 +562,6 @@ class HydeKernelTest extends TestCase
 
         $this->assertInstanceOf(Collection::class, $kernel->getAuthors());
         $this->assertEmpty($kernel->getAuthors());
-    }
-
-    public function testGetAuthorsReturnsSingletonCollection()
-    {
-        $kernel = new HydeKernel();
-
-        $this->assertSame($kernel->getAuthors(), $kernel->getAuthors());
     }
 }
 
