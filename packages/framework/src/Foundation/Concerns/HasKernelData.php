576f94f11f5f59362b76182f888fae64cb4ecb9b
Caen De Silva <caen@desilva.se>
2024-07-05 18:38:33 +0200

    Create HasKernelData.php

---
+++ packages/framework/src/Foundation/Concerns/HasKernelData.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hyde\Foundation\Concerns;
+
+/**
+ * @internal Single-use trait for the HydeKernel class.
+ *
+ * @see \Hyde\Foundation\HydeKernel
+ */
+trait HasKernelData
+{
+    //
+}
