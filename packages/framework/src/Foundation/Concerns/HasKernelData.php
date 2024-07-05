863ce8a28af2ba04197740602b4a9d8faac2c7c8
Caen De Silva <caen@desilva.se>
2024-07-05 18:39:11 +0200

    Update class description

--- packages/framework/src/Foundation/Concerns/HasKernelData.php
+++ packages/framework/src/Foundation/Concerns/HasKernelData.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 namespace Hyde\Foundation\Concerns;
 
 /**
+ * Contains accessors and containers for data stored in the kernel.
+ *
  * @internal Single-use trait for the HydeKernel class.
  *
  * @see \Hyde\Foundation\HydeKernel
