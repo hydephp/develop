5947a85ba314bd7c2402f649ba9690b3a9731312
Caen De Silva <caen@desilva.se>
2024-07-07 15:08:10 +0200

    Create initial logic to find the errored config line

--- packages/framework/src/Framework/Exceptions/InvalidConfigurationException.php
+++ packages/framework/src/Framework/Exceptions/InvalidConfigurationException.php
@@ -8,8 +8,32 @@ use InvalidArgumentException;
 
 class InvalidConfigurationException extends InvalidArgumentException
 {
-    public function __construct(string $message = 'Invalid configuration detected.')
+    public function __construct(string $message = 'Invalid configuration detected.', ?string $namespace = null, ?string $key = null)
     {
+        if ($namespace && $key) {
+            [$file, $line] = $this->findConfigLine($namespace, $key);
+
+            if ($file && $line) {
+                $this->file = $file;
+                $this->line = $line;
+            }
+        }
+
         parent::__construct($message);
+    }
+
+    protected function findConfigLine(string $namespace, string $key): array
+    {
+        $file = "config/$namespace.php";
+        $contents = file_get_contents(base_path($file));
+        $lines = explode("\n", $contents);
+
+        foreach ($lines as $line => $content) {
+            if (str_contains($content, "'$key' =>")) {
+                break;
+            }
+        }
+
+        return [$file, $line + 1];
     }
 }
