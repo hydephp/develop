0924129c1238cf74fba4bd33bc4f2c03ebbcecaf
caendesilva <caen@desilva.se>
2024-07-09 11:53:11 +0200

    2023-02-16: Use the latest HydeFront version
    From: Caen De Silva
    Date: Thu Feb 16 21:47:43 2023 +0100
    Subject: Use the latest HydeFront version
    
    Original commit: https://github.com/hydephp/develop/commit/da66f3460cac0db334c372e21fd469ea80ef1e89

--- monorepo/gh-pages/gh-pages-config-dev-docs/config/hyde.php
+++ monorepo/gh-pages/gh-pages-config-dev-docs/config/hyde.php
@@ -252,6 +252,7 @@ return [
     */
 
     'load_app_styles_from_cdn' => true,
+    'hydefront_version' => 'latest',
 
     /*
      |--------------------------------------------------------------------------
