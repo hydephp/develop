f580d914d89dbfccb1cdbab51acd941f1d00370d
caendesilva <caen@desilva.se>
2024-07-09 11:53:10 +0200

    2022-10-31: Update footer.php
    From: Caen De Silva
    Date: Mon Oct 31 11:39:20 2022 +0100
    Subject: Update footer.php
    
    Original commit: https://github.com/hydephp/develop/commit/01bf820256e57df63dc899545c5917d8e4ab5943

--- monorepo/gh-pages/gh-pages-config-dev-docs/_includes/footer.php
+++ monorepo/gh-pages/gh-pages-config-dev-docs/_includes/footer.php
@@ -1,6 +1,6 @@
 <?php
 
-return 'HydePHP Monorepo '.Hyde\Framework\Hyde::version().
+return 'HydePHP Monorepo '.Hyde\Hyde::version().
             (file_exists(getcwd() . ('/origin-ref'))
             ? '-'.'<a href="https://github.com/caendesilva/hyde-monorepo/commit/'.file_get_contents(getcwd() . ('/origin-ref')).'">'.substr(file_get_contents(getcwd() . ('/origin-ref')), 0, 7).'</a>'
             : '') . ' (compiled '.date('Y-m-d H:i:s').' '. now()->format('e'). ' - <a href="build-information.html">Build Information</a>)';
