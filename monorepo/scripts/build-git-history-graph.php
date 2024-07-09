6ef4502f597e584041c7cca597136b7ff0d9bd52
Caen De Silva <caen@desilva.se>
2024-07-09 13:18:48 +0200

    Add base documentation

--- monorepo/scripts/build-git-history-graph.php
+++ monorepo/scripts/build-git-history-graph.php
@@ -1,1 +1,8 @@
 <?php
+
+/**
+ * @internal This script is used to build the Git history graphs.
+ *
+ * @link https://hydephp.github.io/develop/master/git/history-graph.html
+ * @link https://hydephp.github.io/develop/master/git/history-graph.txt
+ */
