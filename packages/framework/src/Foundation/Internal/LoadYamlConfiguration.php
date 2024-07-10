ed615a7c9bef361973d8e42c6e4b99bfa6adba8b
Caen De Silva <caen@desilva.se>
2024-07-10 20:41:07 +0200

    Update YAML parser array shapes

--- packages/framework/src/Foundation/Internal/LoadYamlConfiguration.php
+++ packages/framework/src/Foundation/Internal/LoadYamlConfiguration.php
@@ -86,8 +86,8 @@ class LoadYamlConfiguration
     /**
      * @experimental Since the main configuration also uses arrays, the only thing this method really does is to rethrow any exceptions.
      *
-     * @param  array<array{destination: string, label: ?string, priority: ?int}>  $items  Where destination is a route key or an external URI.
-     * @return array<array{destination: string, label: ?string, priority: ?int}>
+     * @param  array<array{destination: string, label: ?string, priority: ?int, attributes: array<string, scalar>}>  $items  Where destination is a route key or an external URI.
+     * @return array<array{destination: string, label: ?string, priority: ?int, attributes: array<string, scalar>}>
      */
     protected function parseNavigationItems(array $items): array
     {
