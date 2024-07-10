286f33c44bc5830e50d48f2929f8454b791795f8
Caen De Silva <caen@desilva.se>
2024-07-10 13:43:05 +0200

    Extract HydeStan helpers to separate file

--- monorepo/HydeStan/HydeStan.php
+++ monorepo/HydeStan/HydeStan.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 use Desilva\Console\Console;
 
 require_once __DIR__.'/includes/contracts.php';
+require_once __DIR__.'/includes/helpers.php';
 
 /**
  * @internal Custom static analysis tool for the HydePHP Development Monorepo.
@@ -363,62 +364,4 @@ class NoTestReferenceAnalyser extends LineAnalyser
                 realpath(__DIR__.'/../../packages/framework/'.$file) ?: $file, $lineNumber + 1));
         }
     }
-}
-
-class AnalysisStatisticsContainer
-{
-    private static int $linesCounted = 0;
-    private static float $expressionsAnalysed = 0;
-
-    public static function countedLine(): void
-    {
-        self::$linesCounted++;
-    }
-
-    public static function countedLines(int $count): void
-    {
-        self::$linesCounted += $count;
-    }
-
-    public static function analysedExpression(): void
-    {
-        self::$expressionsAnalysed++;
-    }
-
-    public static function analysedExpressions(float $countOrEstimate): void
-    {
-        self::$expressionsAnalysed += $countOrEstimate;
-    }
-
-    public static function getLinesCounted(): int
-    {
-        return self::$linesCounted;
-    }
-
-    public static function getExpressionsAnalysed(): int
-    {
-        return (int) round(self::$expressionsAnalysed);
-    }
-}
-
-function check_str_contains_any(array $searches, string $line): bool
-{
-    $strContainsAny = false;
-    foreach ($searches as $search) {
-        AnalysisStatisticsContainer::analysedExpression();
-        if (str_contains($line, $search)) {
-            $strContainsAny = true;
-        }
-    }
-
-    return $strContainsAny;
-}
-
-function fileLink(string $file, ?int $line = null): string
-{
-    $path = (realpath(__DIR__.'/../../packages/framework/'.$file) ?: $file).($line ? ':'.$line : '');
-    $trim = strlen(getcwd()) + 2;
-    $path = substr($path, $trim);
-
-    return str_replace('\\', '/', $path);
 }
---
+++ monorepo/HydeStan/includes/helpers.php
@@ -0,0 +1,61 @@
+<?php
+
+declare(strict_types=1);
+
+class AnalysisStatisticsContainer
+{
+    private static int $linesCounted = 0;
+    private static float $expressionsAnalysed = 0;
+
+    public static function countedLine(): void
+    {
+        self::$linesCounted++;
+    }
+
+    public static function countedLines(int $count): void
+    {
+        self::$linesCounted += $count;
+    }
+
+    public static function analysedExpression(): void
+    {
+        self::$expressionsAnalysed++;
+    }
+
+    public static function analysedExpressions(float $countOrEstimate): void
+    {
+        self::$expressionsAnalysed += $countOrEstimate;
+    }
+
+    public static function getLinesCounted(): int
+    {
+        return self::$linesCounted;
+    }
+
+    public static function getExpressionsAnalysed(): int
+    {
+        return (int) round(self::$expressionsAnalysed);
+    }
+}
+
+function check_str_contains_any(array $searches, string $line): bool
+{
+    $strContainsAny = false;
+    foreach ($searches as $search) {
+        AnalysisStatisticsContainer::analysedExpression();
+        if (str_contains($line, $search)) {
+            $strContainsAny = true;
+        }
+    }
+
+    return $strContainsAny;
+}
+
+function fileLink(string $file, ?int $line = null): string
+{
+    $path = (realpath(__DIR__.'/../../packages/framework/'.$file) ?: $file).($line ? ':'.$line : '');
+    $trim = strlen(getcwd()) + 2;
+    $path = substr($path, $trim);
+
+    return str_replace('\\', '/', $path);
+}
