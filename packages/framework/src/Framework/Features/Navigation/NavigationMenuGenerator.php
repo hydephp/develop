b0bbfa7a8c4aee68bbbf615b00ad1cfa07936f30
Caen De Silva <caen@desilva.se>
2024-07-10 20:53:38 +0200

    Unpack the array directly

--- packages/framework/src/Framework/Features/Navigation/NavigationMenuGenerator.php
+++ packages/framework/src/Framework/Features/Navigation/NavigationMenuGenerator.php
@@ -84,7 +84,7 @@ class NavigationMenuGenerator
             collect(Config::getArray('hyde.navigation.custom', []))->each(function (array $item): void {
                 /** @var array{destination: string, label: ?string, priority: ?int, attributes: array<string, scalar>} $item */
                 // Since these were added explicitly by the user, we can assume they should always be shown
-                $this->items->push(NavigationItem::create($item['destination'], $item['label'] ?? null, $item['priority'] ?? null, $item['attributes'] ?? []));
+                $this->items->push(NavigationItem::create(...$item));
             });
         }
     }
