3f7c34b178a7535106d32db65468052522289ddd
Caen De Silva <caen@desilva.se>
2024-07-10 20:38:00 +0200

    Test the view with extra attributes
    Claude is just insanely good

--- packages/framework/tests/Unit/Views/NavigationLinkViewTest.php
+++ packages/framework/tests/Unit/Views/NavigationLinkViewTest.php
@@ -26,10 +26,10 @@ class NavigationLinkViewTest extends TestCase
         $this->mockPage();
     }
 
-    protected function testView(): TestView
+    protected function testView(array $extraAttributes = []): TestView
     {
         return $this->view(view('hyde::components.navigation.navigation-link', [
-            'item' => NavigationItem::create(new Route(new InMemoryPage('foo')), 'Foo'),
+            'item' => NavigationItem::create(new Route(new InMemoryPage('foo')), 'Foo', null, $extraAttributes),
             'attributes' => new ComponentAttributeBag(),
         ]));
     }
@@ -87,6 +87,55 @@ class NavigationLinkViewTest extends TestCase
         $this->testView()
             ->assertHasClass('navigation-link')
             ->assertHasClass('navigation-link-active');
+    }
+
+    public function testComponentRendersExtraAttributes()
+    {
+        $this->testView(['data-test' => 'value'])
+            ->assertHasAttribute('data-test')
+            ->assertAttributeIs('data-test="value"');
+    }
+
+    public function testComponentRendersMultipleExtraAttributes()
+    {
+        $this->testView(['data-test' => 'value', 'data-foo' => 'bar'])
+            ->assertHasAttribute('data-test')
+            ->assertAttributeIs('data-test="value"')
+            ->assertHasAttribute('data-foo')
+            ->assertAttributeIs('data-foo="bar"');
+    }
+
+    public function testComponentRendersExtraAttributesWithExistingAttributes()
+    {
+        $this->mockCurrentPage('foo');
+
+        $view = $this->testView(['data-test' => 'value']);
+
+        $expected = <<<'HTML'
+        <a href="foo.html" aria-current="page" class="navigation-link block my-2 md:my-0 md:inline-block py-1 text-gray-700 hover:text-gray-900 dark:text-gray-100 navigation-link-active border-l-4 border-indigo-500 md:border-none font-medium -ml-6 pl-5 md:ml-0 md:pl-0 bg-gray-100 dark:bg-gray-800 md:bg-transparent dark:md:bg-transparent" data-test="value">Foo</a>
+        HTML;
+
+        $this->assertSame($expected, $view->getRendered());
+    }
+
+    public function testComponentMergesClassAttributeCorrectly()
+    {
+        $this->testView(['class' => 'custom-class'])
+            ->assertHasClass('navigation-link')
+            ->assertHasClass('custom-class');
+    }
+
+    public function testComponentOverridesDefaultAttributesWithExtraAttributes()
+    {
+        $this->testView(['href' => 'https://example.com'])
+            ->assertAttributeIs('href="https://example.com"');
+    }
+
+    public function testComponentHandlesEmptyExtraAttributes()
+    {
+        $this->testView([])
+            ->assertHasElement('<a>')
+            ->assertTextIs('Foo');
     }
 
     public function testComponentState()
