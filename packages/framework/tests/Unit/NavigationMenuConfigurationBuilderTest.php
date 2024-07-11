283852aae2541fe12c5d16cf784c96a71055387d
Caen De Silva <caen@desilva.se>
2024-07-11 11:02:56 +0200

    Create NavigationMenuConfigurationBuilderTest.php

---
+++ packages/framework/tests/Unit/NavigationMenuConfigurationBuilderTest.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+use Hyde\Testing\UnitTestCase;
+
+/**
+ * @covers \Hyde\Framework\Features\Navigation\NavigationMenuConfigurationBuilder
+ */
+class NavigationMenuConfigurationBuilderTest extends UnitTestCase
+{
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+}
