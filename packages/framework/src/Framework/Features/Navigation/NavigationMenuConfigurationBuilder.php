519ee26b2348b6ff8922ce9c9fa949ac8c4632fc
Caen De Silva <caen@desilva.se>
2024-07-11 10:49:29 +0200

    Draft the class documentation

--- packages/framework/src/Framework/Features/Navigation/NavigationMenuConfigurationBuilder.php
+++ packages/framework/src/Framework/Features/Navigation/NavigationMenuConfigurationBuilder.php
@@ -4,6 +4,13 @@ declare(strict_types=1);
 
 namespace Hyde\Framework\Features\Navigation;
 
+/**
+ * Configuration helper method to define the navigation menu configuration with better IDE support.
+ *
+ * The configured object will be cast to an array that will be used by the framework to set the config data.
+ *
+ * @experimental This method is experimental and may change in the future.
+ */
 class NavigationMenuConfigurationBuilder
 {
     //
