3a489b0115d6f517dccb6d0596dc46fd3eebe499
Caen De Silva <caen@desilva.se>
2024-07-12 11:36:05 +0200

    Parse data

--- monorepo/DevTools/src/RefactorConfigCommand.php
+++ monorepo/DevTools/src/RefactorConfigCommand.php
@@ -5,9 +5,23 @@ declare(strict_types=1);
 namespace Hyde\MonorepoDevTools;
 
 use Hyde\Hyde;
+use Hyde\Enums\Feature;
+use Illuminate\Support\Str;
 use Symfony\Component\Yaml\Yaml;
 use Hyde\Console\Concerns\Command;
+use Hyde\Framework\Features\Blogging\Models\PostAuthor;
+use Hyde\Framework\Features\Metadata\MetadataElementContract;
 
+use function copy;
+use function config;
+use function substr;
+use function collect;
+use function implode;
+use function in_array;
+use function is_array;
+use function is_string;
+use function file_exists;
+use function str_starts_with;
 use function file_put_contents;
 
 /**
@@ -45,14 +59,55 @@ class RefactorConfigCommand extends Command
 
     protected function migrateToYaml(): void
     {
-        // todo if file exists, add backup
+        if (file_exists(Hyde::path('hyde.yml')) && ! file_exists(Hyde::path('hyde.yml.bak'))) {
+            copy(Hyde::path('hyde.yml'), Hyde::path('hyde.yml.bak'));
+        }
 
         $config = config('hyde');
+        $config = $this->serializePhpData($config);
 
-        $yaml = Yaml::dump($config);
+        $yaml = Yaml::dump($config, 16, 4, Yaml::DUMP_MULTI_LINE_LITERAL_BLOCK | Yaml::DUMP_EMPTY_ARRAY_AS_SEQUENCE);
 
-        // todo diff out defaults?
+        // Todo: Diff out defaults? (unless with argument)
 
         file_put_contents(Hyde::path('hyde.yml'), $yaml);
+    }
+
+    protected function serializePhpData(array $config): array
+    {
+        return collect($config)->mapWithKeys(function ($value, $key) {
+            if (is_array($value)) {
+                return [$key => $this->serializePhpData($value)];
+            }
+
+            return $this->serializePhpValue($value, $key);
+        })->toArray();
+    }
+
+    protected function serializePhpValue(mixed $value, string|int $key): array
+    {
+        if ($value instanceof Feature) {
+            return [$key => Str::kebab($value->name)];
+        }
+
+        if (is_string($key) && str_starts_with($key, 'Hyde\Pages\\')) {
+            return [Str::kebab(substr($key, 11)) => $value];
+        }
+
+        if ($value instanceof MetadataElementContract) {
+            // We don't have deserialization logic for this (yet?)
+            return [$key => $value->__toString()];
+        }
+
+        if ($value instanceof PostAuthor) {
+            // Not fully supported in v1
+            return [$key => [
+                'username' => $value->username,
+                'name' => $value->name,
+                'website' => $value->website,
+            ]];
+        }
+
+        return [$key => $value];
     }
 }
