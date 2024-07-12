7701fcf476dbad7c2a4e9eeeabcea0f3b21bfe3a
Caen De Silva <caen@desilva.se>
2024-07-12 20:18:37 +0200

    Check that there are posts

--- packages/framework/src/Framework/Features/Blogging/DynamicBlogPostPageHelper.php
+++ packages/framework/src/Framework/Features/Blogging/DynamicBlogPostPageHelper.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Hyde\Framework\Features\Blogging;
 
 use Hyde\Hyde;
+use Hyde\Pages\MarkdownPost;
 use Hyde\Framework\Features\Blogging\Models\PostAuthor;
 use Hyde\Framework\Features\Blogging\DynamicPages\PostAuthorPage;
 
@@ -19,7 +20,7 @@ class DynamicBlogPostPageHelper
     {
         // Todo: Also check that this feature is enabled
 
-        return Hyde::authors()->isNotEmpty();
+        return Hyde::authors()->isNotEmpty() && MarkdownPost::all()->isNotEmpty();
     }
 
     /** @return array<\Hyde\Framework\Features\Blogging\DynamicPages\PostAuthorPage> */
