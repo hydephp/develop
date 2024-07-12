92cf166b5288f23fab4e1666a58938398cf174ab
Caen De Silva <caen@desilva.se>
2024-07-12 19:45:37 +0200

    Implement author page generation check

--- packages/framework/src/Framework/Features/Blogging/DynamicBlogPostPageHelper.php
+++ packages/framework/src/Framework/Features/Blogging/DynamicBlogPostPageHelper.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Hyde\Framework\Features\Blogging;
 
+use Hyde\Hyde;
+
 /**
  * @internal Initial class to help with dynamic blogging related pages, like author pages, tag pages, etc.
  *
@@ -15,7 +17,7 @@ class DynamicBlogPostPageHelper
 {
     public static function canGenerateAuthorPages(): bool
     {
-        return true;
+        return Hyde::authors()->isNotEmpty();
     }
 
     /** @return array<\Hyde\Pages\InMemoryPage> */
