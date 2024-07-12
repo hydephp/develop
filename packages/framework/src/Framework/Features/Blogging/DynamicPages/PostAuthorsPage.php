9e34aaa649b4dcf00060ba4c82388ee4d2be0fc3
Caen De Silva <caen@desilva.se>
2024-07-12 21:26:56 +0200

    Construct page with author collection

--- packages/framework/src/Framework/Features/Blogging/DynamicPages/PostAuthorsPage.php
+++ packages/framework/src/Framework/Features/Blogging/DynamicPages/PostAuthorsPage.php
@@ -5,14 +5,17 @@ declare(strict_types=1);
 namespace Hyde\Framework\Features\Blogging\DynamicPages;
 
 use Hyde\Pages\InMemoryPage;
+use Illuminate\Support\Collection;
 
 /**
  * @experimental
  */
 class PostAuthorsPage extends InMemoryPage
 {
-    public function __construct()
+    public function __construct(Collection $authors)
     {
         parent::__construct('authors');
+
+        $this->authors = $authors;
     }
 }
