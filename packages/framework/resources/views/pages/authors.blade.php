a72cb7096a848e9b6362508b09b3e6f2ad157670
Caen De Silva <caen@desilva.se>
2024-07-12 22:22:01 +0200

    Revert "Draft authors list"
    This reverts commit e76870152fb8745edc34eb8dd7894aa86b2c4150.

--- packages/framework/resources/views/pages/authors.blade.php
+++ packages/framework/resources/views/pages/authors.blade.php
@@ -3,12 +3,7 @@
 @section('content')
 
     <main id="content" class="mx-auto max-w-7xl py-16 px-8">
-        @foreach($authors as $author)
-            <div class="mb-8">
-                <h2 class="text-3xl font-bold mb-4">{{ $author->name }}</h2>
-                <p class="text-gray-600">{{ $author->bio }}</p>
-            </div>
-        @endforeach
+        //
     </main>
 
 @endsection
