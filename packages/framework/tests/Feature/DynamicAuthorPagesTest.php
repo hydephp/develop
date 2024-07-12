e90fd5563bafea5817a04cad52d553cda3fa3e68
Caen De Silva <caen@desilva.se>
2024-07-12 19:48:23 +0200

    Create DynamicAuthorPagesTest.php

---
+++ packages/framework/tests/Feature/DynamicAuthorPagesTest.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+use Hyde\Testing\TestCase;
+
+/**
+ * High level test for the dynamic author pages feature.
+ */
+class DynamicAuthorPagesTest extends TestCase
+{
+    //
+}
