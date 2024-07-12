ce043fda45ffe25690b9cdde12987ea1ecd8c35d
Caen De Silva <caen@desilva.se>
2024-07-12 21:24:16 +0200

    Create PostAuthorsPage.php

---
+++ packages/framework/src/Framework/Features/Blogging/DynamicPages/PostAuthorsPage.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hyde\Framework\Features\Blogging\DynamicPages;
+
+use Hyde\Pages\InMemoryPage;
+
+/**
+ * @experimental
+ */
+class PostAuthorsPage extends InMemoryPage
+{
+    //
+}
