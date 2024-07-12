75b2463b5d5a13e64ff33a732cb2c37e3e3a307c
Caen De Silva <caen@desilva.se>
2024-07-12 19:53:04 +0200

    Add author configuration setup

--- packages/framework/tests/Feature/DynamicAuthorPagesTest.php
+++ packages/framework/tests/Feature/DynamicAuthorPagesTest.php
@@ -2,7 +2,9 @@
 
 declare(strict_types=1);
 
+use Hyde\Facades\Author;
 use Hyde\Testing\TestCase;
+use Illuminate\Support\Facades\Config;
 
 /**
  * High level test for the dynamic author pages feature.
@@ -11,6 +13,22 @@ class DynamicAuthorPagesTest extends TestCase
 {
     protected function setUpTestEnvironment(): void
     {
-        //
+        Config::set('hyde.authors', [
+            'mr_hyde' => Author::create(
+                name: 'Mr. Hyde',
+                website: 'https://hydephp.com',
+                bio: 'The mysterious author of HydePHP',
+                avatar: 'avatar.png',
+                socials: [
+                    'twitter' => '@HydeFramework',
+                    'github' => 'hydephp',
+                ],
+            ),
+            'jane_doe' => Author::create(
+                name: 'Jane Doe',
+                bio: 'Slightly less evil. We think...',
+            ),
+            'user123' => Author::create(),
+        ]);
     }
 }
