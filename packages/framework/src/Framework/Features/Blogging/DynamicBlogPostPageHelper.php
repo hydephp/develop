6ca4ae68931c3561cf3a59c34cb18a817e01a78b
Caen De Silva <caen@desilva.se>
2024-07-12 18:33:28 +0200

    Document added helper class as internal and experimental

--- packages/framework/src/Framework/Features/Blogging/DynamicBlogPostPageHelper.php
+++ packages/framework/src/Framework/Features/Blogging/DynamicBlogPostPageHelper.php
@@ -4,6 +4,12 @@ declare(strict_types=1);
 
 namespace Hyde\Framework\Features\Blogging;
 
+/**
+ * @internal Initial class to help with dynamic blogging related pages, like author pages, tag pages, etc.
+ * @experimental The code here will later be moved to a more appropriate place.
+ *
+ * @codeCoverageIgnore This feature is experimental and not yet tested.
+ */
 class DynamicBlogPostPageHelper
 {
     //
