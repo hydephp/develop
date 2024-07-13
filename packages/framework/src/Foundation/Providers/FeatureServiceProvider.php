611db55b54cec7d7d67dd96e81a604dfd02acbb3
Caen De Silva <caen@desilva.se>
2024-07-13 13:30:01 +0200

    Create FeatureServiceProvider.php

---
+++ packages/framework/src/Foundation/Providers/FeatureServiceProvider.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hyde\Foundation\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+/**
+ * @experimental General feature service provider for Hyde.
+ */
+class FeatureServiceProvider extends ServiceProvider
+{
+    public function register(): void
+    {
+        //
+    }
+
+    public function boot(): void
+    {
+        //
+    }
+}
