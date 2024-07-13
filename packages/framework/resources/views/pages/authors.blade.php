ea4bd05d90ca5bf168ea8c711ee2908833604610
Caen De Silva <caen@desilva.se>
2024-07-13 09:47:16 +0200

    Remove type not supported in schema

--- packages/framework/resources/views/pages/authors.blade.php
+++ packages/framework/resources/views/pages/authors.blade.php
@@ -21,7 +21,7 @@
                     </div>
                     <div class="mt-4">
                         <p class="text-sm text-gray-500 dark:text-gray-400">
-                            <span itemprop="numberOfItems">{{ $author->getPosts()->count() }}</span> {{ Str::plural('post', $author->getPosts()->count()) }}
+                            {{ $author->getPosts()->count() }} {{ Str::plural('post', $author->getPosts()->count()) }}
                         </p>
                     </div>
                 </div>
