d35d8903f82b11171afd0439d4688a1188f2c823
Caen De Silva <caen@desilva.se>
2024-07-15 10:32:52 +0200

    Create IntegrationTest.php

---
+++ packages/realtime-compiler/tests/Integration/IntegrationTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Hyde\RealtimeCompiler\Tests\Integration;
+
+use PHPUnit\Framework\TestCase;
+
+class IntegrationTest extends TestCase
+{
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+}
