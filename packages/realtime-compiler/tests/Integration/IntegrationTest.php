62a2582777e34b2148c2633eb02e1d54c3649c03
Caen De Silva <caen@desilva.se>
2024-07-15 10:37:49 +0200

    Throw exception when trying to run test from the monorepo

--- packages/realtime-compiler/tests/Integration/IntegrationTest.php
+++ packages/realtime-compiler/tests/Integration/IntegrationTest.php
@@ -2,12 +2,19 @@
 
 namespace Hyde\RealtimeCompiler\Tests\Integration;
 
+use InvalidArgumentException;
 use PHPUnit\Framework\TestCase;
 
 class IntegrationTest extends TestCase
 {
     public static function setUpBeforeClass(): void
     {
+        $monorepo = realpath(__DIR__.'/../../../');
+
+        if ($monorepo && file_exists($monorepo.'/hyde')) {
+            throw new InvalidArgumentException('This test suite is not intended to be run from the monorepo.');
+        }
+
         if (! self::hasTestRunnerSetUp()) {
             self::setUpTestRunner();
         }
