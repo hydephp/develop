17250253b0f8ccbf2629f5e9a4f7482866546b9f
Caen De Silva <caen@desilva.se>
2024-07-15 10:35:08 +0200

    Draft logic to set up test runner

--- packages/realtime-compiler/tests/Integration/IntegrationTest.php
+++ packages/realtime-compiler/tests/Integration/IntegrationTest.php
@@ -6,8 +6,25 @@ use PHPUnit\Framework\TestCase;
 
 class IntegrationTest extends TestCase
 {
+    public static function setUpBeforeClass(): void
+    {
+        if (! self::hasTestRunnerSetUp()) {
+            self::setUpTestRunner();
+        }
+    }
+
     public function testExample()
     {
         $this->assertTrue(true);
+    }
+
+    private static function hasTestRunnerSetUp(): bool
+    {
+        return false;
+    }
+
+    private static function setUpTestRunner(): void
+    {
+        // Set up the test runner
     }
 }
