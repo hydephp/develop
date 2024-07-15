3d0af2e9833a3e97761e99d022f60dd0f52f5a5b
Caen De Silva <caen@desilva.se>
2024-07-15 17:00:42 +0200

    Expand abbreviated variable

--- packages/realtime-compiler/bin/server.php
+++ packages/realtime-compiler/bin/server.php
@@ -12,12 +12,12 @@ try {
     } catch (Throwable $exception) {
         \Hyde\RealtimeCompiler\Http\ExceptionHandler::handle($exception)->send();
     }
-} catch (\Throwable $th) {
+} catch (\Throwable $throwable) {
     // Auxiliary exception handler
     echo '<h1>Something went really wrong!</h1>';
     echo '<p>An error occurred that the core exception handler failed to process. Here\'s all we know:</p>';
     echo '<h2>Initial exception:</h2><pre>'.print_r($exception ?? null, true).'</pre>';
-    echo '<h2>Auxiliary exception:</h2><pre>'.print_r($th, true).'</pre>';
+    echo '<h2>Auxiliary exception:</h2><pre>'.print_r($throwable, true).'</pre>';
 } finally {
     if (getenv('HYDE_SERVER_REQUEST_OUTPUT')) {
         // Write to console to emulate the standard built-in PHP server output
