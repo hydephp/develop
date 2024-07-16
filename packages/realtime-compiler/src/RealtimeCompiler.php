65f294ac2174112eaf38c8313f08c3795b18bb55
Caen De Silva <caen@desilva.se>
2024-07-16 12:46:00 +0200

    Draft method to register virtual route

--- packages/realtime-compiler/src/RealtimeCompiler.php
+++ packages/realtime-compiler/src/RealtimeCompiler.php
@@ -4,7 +4,12 @@ declare(strict_types=1);
 
 namespace Hyde\RealtimeCompiler;
 
+use Hyde\Support\Models\Route;
+
 class RealtimeCompiler
 {
-    //
+    public function registerVirtualRoute(string $uri, Route $route): void
+    {
+        //
+    }
 }
