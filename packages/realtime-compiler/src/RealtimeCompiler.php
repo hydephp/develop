b097711b9024d6145f8b105816271763c3f58650
Caen De Silva <caen@desilva.se>
2024-07-16 12:51:50 +0200

    Implement the drafted class

--- packages/realtime-compiler/src/RealtimeCompiler.php
+++ packages/realtime-compiler/src/RealtimeCompiler.php
@@ -8,13 +8,29 @@ use Hyde\Support\Models\Route;
 
 class RealtimeCompiler
 {
+    /**
+     * @var array<string, Route>
+     */
+    private array $virtualRoutes = [];
+
+    /**
+     * Register a virtual route.
+     *
+     * @param  string  $uri  The URI of the virtual route
+     * @param  Route  $route  The Route object
+     */
     public function registerVirtualRoute(string $uri, Route $route): void
     {
-        //
+        $this->virtualRoutes[$uri] = $route;
     }
 
+    /**
+     * Get all registered virtual routes.
+     *
+     * @return array<string, Route>
+     */
     public function getVirtualRoutes(): array
     {
-        return [];
+        return $this->virtualRoutes;
     }
 }
