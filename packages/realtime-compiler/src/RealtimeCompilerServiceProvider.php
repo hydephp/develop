5d4f264e3ff52ee170e35cded117c1ffcbd9bfc5
Caen De Silva <caen@desilva.se>
2024-07-16 12:20:16 +0200

    Create RealtimeCompilerServiceProvider.php

---
+++ packages/realtime-compiler/src/RealtimeCompilerServiceProvider.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hyde\RealtimeCompiler;
+
+use Illuminate\Support\ServiceProvider;
+
+class RealtimeCompilerServiceProvider extends ServiceProvider
+{
+    //
+}
