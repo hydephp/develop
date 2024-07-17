b8af88d8cfe4ed936f17f470d4e00e6a63ae6217
Caen De Silva <caen@desilva.se>
2024-07-17 16:46:48 +0200

    Revert "Annotate string key"
    This reverts commit 5a0ce28ba4f641e4e20d6677e36da1fe35d13dc7.

--- packages/framework/src/Pages/Concerns/HydePage.php
+++ packages/framework/src/Pages/Concerns/HydePage.php
@@ -140,7 +140,7 @@ abstract class HydePage implements PageSchema, SerializableContract
     /**
      * Get a collection of all pages, parsed into page models.
      *
-     * @return \Hyde\Foundation\Kernel\PageCollection<string, static>
+     * @return \Hyde\Foundation\Kernel\PageCollection<static>
      */
     public static function all(): PageCollection
     {
