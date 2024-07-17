c3edbb649e6028cc2e4808c90269affd7b0d4a81
Caen De Silva <caen@desilva.se>
2024-07-17 17:19:35 +0200

    Use lower level access call to try to bypass Psalm issues

--- packages/framework/src/Framework/Services/BuildService.php
+++ packages/framework/src/Framework/Services/BuildService.php
@@ -6,6 +6,7 @@ namespace Hyde\Framework\Services;
 
 use Hyde\Hyde;
 use Hyde\Foundation\Facades\Routes;
+use Hyde\Foundation\Kernel\PageCollection;
 use Hyde\Foundation\Kernel\RouteCollection;
 use Hyde\Framework\Actions\StaticPageBuilder;
 use Hyde\Framework\Concerns\InteractsWithDirectories;
@@ -87,7 +88,7 @@ class BuildService
     /** @return array<class-string<\Hyde\Pages\Concerns\HydePage>> */
     protected function getPageTypes(): array
     {
-        return Hyde::pages()->map(function (HydePage $page): string {
+        return PageCollection::getPages()->map(function (HydePage $page): string {
             return $page::class;
         })->unique()->values()->toArray();
     }
