d5d119a9dd58200e71ec108912ab41cc83deccf9
Caen De Silva <caen@desilva.se>
2024-07-22 10:29:05 +0200

    Improve asset file locator to support serving subdirectory assets
    Fixes https://github.com/hydephp/realtime-compiler/issues/26

--- packages/realtime-compiler/src/Actions/AssetFileLocator.php
+++ packages/realtime-compiler/src/Actions/AssetFileLocator.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Hyde\RealtimeCompiler\Actions;
 
+use Illuminate\Support\Str;
+
 /**
  * Locate a static file to proxy.
  */
@@ -11,11 +13,13 @@ class AssetFileLocator
 {
     public static function find(string $path): ?string
     {
+        $path = trim($path, '/');
+
         $strategies = [
-            BASE_PATH.'/_site'.$path,
-            BASE_PATH.'/_media'.$path,
-            BASE_PATH.'/_site'.basename($path),
-            BASE_PATH.'/_media/'.basename($path),
+            BASE_PATH.'/_site/'.$path,
+            BASE_PATH.'/_media/'.$path,
+            BASE_PATH.'/_site/'.Str::after($path, 'media/'),
+            BASE_PATH.'/_media/'.Str::after($path, 'media/'),
         ];
 
         foreach ($strategies as $strategy) {
