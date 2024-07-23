4eaea625e801cd77a3ccd9263950c9f807c5e9e1
Caen De Silva <caen@desilva.se>
2024-07-23 17:10:07 +0200

    Reword comments

--- packages/framework/src/Markdown/Contracts/FrontMatter/PageSchema.php
+++ packages/framework/src/Markdown/Contracts/FrontMatter/PageSchema.php
@@ -13,8 +13,8 @@ interface PageSchema extends FrontMatterSchema
 {
     public const PAGE_SCHEMA = [
         'title' => 'string',
-        'description' => 'string', // For per page <meta name='description'> tag values. It is only used as an accessor method for the front matter, not a property.
-        'canonicalUrl' => 'string', // While not present in the page data as a property, it is used for the accessor method, which reads this value from the front matter.
+        'description' => 'string', // For <meta name='description'> values. It is only present as a property in the MarkdownPost class, for other types, the accessor reads it from the front matter.
+        'canonicalUrl' => 'string', // While not present in the page data as a property, it is used by the accessor method, which reads this value from the front matter.
         'navigation' => NavigationSchema::NAVIGATION_SCHEMA,
     ];
 }
