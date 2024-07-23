c0c1dcc6c256591892a59cc186be79ba57817e9c
Caen De Silva <caen@desilva.se>
2024-07-23 17:30:35 +0200

    Update documented scope

--- packages/framework/src/Markdown/Contracts/FrontMatter/PageSchema.php
+++ packages/framework/src/Markdown/Contracts/FrontMatter/PageSchema.php
@@ -13,7 +13,7 @@ interface PageSchema extends FrontMatterSchema
 {
     public const PAGE_SCHEMA = [
         'title' => 'string',
-        'description' => 'string', // For <meta name='description'> values. It is only present as a property in the MarkdownPost class, for other types, the accessor reads it from the front matter.
+        'description' => 'string', // For <meta name='description'> values. It is used by the automatic page metadata generator, which reads this value from the front matter.
         'canonicalUrl' => 'string', // While not present in the page data as a property, it is used by the accessor method, which reads this value from the front matter.
         'navigation' => NavigationSchema::NAVIGATION_SCHEMA,
     ];
