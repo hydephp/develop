e943c198b4c4eece221224664f0c5e8270409bdf
Caen De Silva <caen@desilva.se>
2024-07-24 18:44:58 +0200

    Implement the basic test

--- packages/framework/tests/Feature/DynamicMarkdownLinksFeatureTest.php
+++ packages/framework/tests/Feature/DynamicMarkdownLinksFeatureTest.php
@@ -1,10 +1,16 @@
 <?php
 
+/** @noinspection HtmlUnknownTarget */
+
 declare(strict_types=1);
 
 namespace Hyde\Framework\Testing\Feature;
 
+use Hyde\Hyde;
 use Hyde\Testing\TestCase;
+use Hyde\Pages\InMemoryPage;
+use Hyde\Support\Models\Route;
+use Hyde\Foundation\Facades\Routes;
 
 /**
  * @covers \Hyde\Markdown\Processing\DynamicMarkdownLinkProcessor
@@ -14,8 +20,36 @@ use Hyde\Testing\TestCase;
  */
 class DynamicMarkdownLinksFeatureTest extends TestCase
 {
-    public function testExample()
+    protected function setUp(): void
     {
-        $this->assertTrue(true);
+        parent::setUp();
+
+        Routes::addRoute(new Route(new InMemoryPage('home')));
+        Routes::addRoute(new Route(new InMemoryPage('blog/post')));
+    }
+
+    public function testDynamicMarkdownLinks()
+    {
+        $input = <<<'MARKDOWN'
+        [Home](hyde::route('home'))
+        [About](hyde::relativeLink('about'))
+        ![Logo](hyde::asset('logo.png'))
+        
+        [Home](hyde::route(home))
+        [About](hyde::relativeLink(about))
+        ![Logo](hyde::asset(logo.png))
+        MARKDOWN;
+
+        $expected = <<<'HTML'
+        <p><a href="home.html">Home</a>
+        <a href="about">About</a>
+        <img src="media/logo.png" alt="Logo" /></p>
+        <p><a href="home.html">Home</a>
+        <a href="about">About</a>
+        <img src="media/logo.png" alt="Logo" /></p>
+
+        HTML;
+
+        $this->assertSame($expected, Hyde::markdown($input)->toHtml());
     }
 }
