6cbefcfeff892681cb4d9c586e69ecdf81b03739
Caen De Silva <caen@desilva.se>
2024-07-24 11:43:39 +0200

    Refactor to use the loop variable instead of the index

--- packages/framework/resources/views/components/blog-post-feed.blade.php
+++ packages/framework/resources/views/components/blog-post-feed.blade.php
@@ -1,7 +1,7 @@
 <ol itemscope itemtype="https://schema.org/ItemList">
-    @foreach(MarkdownPost::getLatestPosts()->values() as $index => $post)
+    @foreach(MarkdownPost::getLatestPosts() as $post)
         <li itemprop="itemListElement" itemscope itemtype="https://schema.org/ListItem" class="mt-4 mb-8">
-            <meta itemprop="position" content="{{ $index + 1 }}">
+            <meta itemprop="position" content="{{ $loop->iteration }}">
             @include('hyde::components.article-excerpt')
         </li>
     @endforeach
