d489d9020f85b3a7c8ed6d0653139a8aa7ba59ab
Caen De Silva <caen@desilva.se>
2024-07-24 18:45:24 +0200

    Generate more tests with Claude

--- packages/framework/tests/Feature/DynamicMarkdownLinksFeatureTest.php
+++ packages/framework/tests/Feature/DynamicMarkdownLinksFeatureTest.php
@@ -28,7 +28,7 @@ class DynamicMarkdownLinksFeatureTest extends TestCase
         Routes::addRoute(new Route(new InMemoryPage('blog/post')));
     }
 
-    public function testDynamicMarkdownLinks()
+    public function testBasicDynamicMarkdownLinks()
     {
         $input = <<<'MARKDOWN'
         [Home](hyde::route('home'))
@@ -47,6 +47,107 @@ class DynamicMarkdownLinksFeatureTest extends TestCase
         <p><a href="home.html">Home</a>
         <a href="about">About</a>
         <img src="media/logo.png" alt="Logo" /></p>
+
+        HTML;
+
+        $this->assertSame($expected, Hyde::markdown($input)->toHtml());
+    }
+
+    public function testDynamicMarkdownLinksWithDoubleQuotes()
+    {
+        $input = <<<'MARKDOWN'
+        [Home](hyde::route("home"))
+        [About](hyde::relativeLink("about"))
+        ![Logo](hyde::asset("logo.png"))
+        MARKDOWN;
+
+        $expected = <<<'HTML'
+        <p><a href="home.html">Home</a>
+        <a href="about">About</a>
+        <img src="media/logo.png" alt="Logo" /></p>
+
+        HTML;
+
+        $this->assertSame($expected, Hyde::markdown($input)->toHtml());
+    }
+
+    public function testDynamicMarkdownLinksInParagraphs()
+    {
+        $input = <<<'MARKDOWN'
+        This is a paragraph with a [link to home](hyde::route('home')) and an [about link](hyde::relativeLink('about')).
+
+        Another paragraph with an ![image](hyde::asset('image.jpg')).
+        MARKDOWN;
+
+        $expected = <<<'HTML'
+        <p>This is a paragraph with a <a href="home.html">link to home</a> and an <a href="about">about link</a>.</p>
+        <p>Another paragraph with an <img src="media/image.jpg" alt="image" />.</p>
+
+        HTML;
+
+        $this->assertSame($expected, Hyde::markdown($input)->toHtml());
+    }
+
+    public function testDynamicMarkdownLinksInLists()
+    {
+        $input = <<<'MARKDOWN'
+        - [Home](hyde::route('home'))
+        - [About](hyde::relativeLink('about'))
+        - ![Logo](hyde::asset('logo.png'))
+        MARKDOWN;
+
+        $expected = <<<'HTML'
+        <ul>
+        <li>
+        <a href="home.html">Home</a>
+        </li>
+        <li>
+        <a href="about">About</a>
+        </li>
+        <li>
+        <img src="media/logo.png" alt="Logo" />
+        </li>
+        </ul>
+
+        HTML;
+
+        $this->assertSame($expected, Hyde::markdown($input)->toHtml());
+    }
+
+    public function testDynamicMarkdownLinksWithNestedRoutes()
+    {
+        $input = <<<'MARKDOWN'
+        [Blog Post](hyde::route('blog/post'))
+        [Relative Blog](hyde::relativeLink('blog/post'))
+        MARKDOWN;
+
+        $expected = <<<'HTML'
+        <p><a href="blog/post.html">Blog Post</a>
+        <a href="blog/post">Relative Blog</a></p>
+
+        HTML;
+
+        $this->assertSame($expected, Hyde::markdown($input)->toHtml());
+    }
+
+    public function testMixOfDynamicAndRegularMarkdownLinks()
+    {
+        $input = <<<'MARKDOWN'
+        [Home](hyde::route('home'))
+        [External](https://example.com)
+        [About](hyde::relativeLink('about'))
+        [Regular](regular-link.html)
+        ![Logo](hyde::asset('logo.png'))
+        ![External Image](https://example.com/image.jpg)
+        MARKDOWN;
+
+        $expected = <<<'HTML'
+        <p><a href="home.html">Home</a>
+        <a href="https://example.com">External</a>
+        <a href="about">About</a>
+        <a href="regular-link.html">Regular</a>
+        <img src="media/logo.png" alt="Logo" />
+        <img src="https://example.com/image.jpg" alt="External Image" /></p>
 
         HTML;
 
