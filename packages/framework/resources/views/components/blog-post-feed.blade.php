0f33d15ec68756f16329de9b4f02b788c541c91b
Caen De Silva <caen@desilva.se>
2024-07-24 11:32:08 +0200

    Reformat view

--- packages/framework/resources/views/components/blog-post-feed.blade.php
+++ packages/framework/resources/views/components/blog-post-feed.blade.php
@@ -1,8 +1,8 @@
 <ol itemscope itemtype="https://schema.org/ItemList">
-@foreach(MarkdownPost::getLatestPosts()->values() as $index => $post)
-    <li itemprop="itemListElement" itemscope itemtype="https://schema.org/ListItem" class="mt-4 mb-8">
-        <meta itemprop="position" content="{{ $index + 1 }}">
-        @include('hyde::components.article-excerpt')
-    </li>
-@endforeach
+    @foreach(MarkdownPost::getLatestPosts()->values() as $index => $post)
+        <li itemprop="itemListElement" itemscope itemtype="https://schema.org/ListItem" class="mt-4 mb-8">
+            <meta itemprop="position" content="{{ $index + 1 }}">
+            @include('hyde::components.article-excerpt')
+        </li>
+    @endforeach
 </ol>
