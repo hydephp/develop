47bfef53fb5985da8123466fb055cdbcd9c90d95
Caen De Silva <caen@desilva.se>
2024-07-24 17:26:21 +0200

    Create DynamicMarkdownLinkProcessorTest.php

---
+++ packages/framework/tests/Feature/Services/Markdown/DynamicMarkdownLinkProcessorTest.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hyde\Framework\Testing\Feature\Services\Markdown;
+
+use Hyde\Testing\UnitTestCase;
+
+/**
+ * @covers \Hyde\Markdown\Processing\DynamicMarkdownLinkProcessor
+ */
+class DynamicMarkdownLinkProcessorTest extends UnitTestCase
+{
+    //
+}
