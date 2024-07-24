017355f117a9475bb83eb09a54747d04b11523c5
Caen De Silva <caen@desilva.se>
2024-07-24 18:03:16 +0200

    Split out multiple assertions to dedicated unit tests
    The testMultipleReplacements method does not need to be split since we don't get any new information from testing already tested alternate syntaxes. That test is mainly for humans to visualise the applicable code better in a more real life scenario.

--- packages/framework/tests/Feature/Services/Markdown/DynamicMarkdownLinkProcessorTest.php
+++ packages/framework/tests/Feature/Services/Markdown/DynamicMarkdownLinkProcessorTest.php
@@ -33,82 +33,97 @@ class DynamicMarkdownLinkProcessorTest extends UnitTestCase
 
     public function testRouteReplacement()
     {
+        $input = '<p><a href="hyde::route(\'home\')">Home</a></p>';
         $expected = '<p><a href="home.html">Home</a></p>';
 
-        $inputSingleQuotes = '<p><a href="hyde::route(\'home\')">Home</a></p>';
-        $this->assertSame($expected, DynamicMarkdownLinkProcessor::postprocess($inputSingleQuotes));
+        $this->assertSame($expected, DynamicMarkdownLinkProcessor::postprocess($input));
+    }
 
-        $inputDoubleQuotes = '<p><a href="hyde::route("home")">Home</a></p>';
-        $this->assertSame($expected, DynamicMarkdownLinkProcessor::postprocess($inputDoubleQuotes));
+    public function testRouteReplacementWithDoubleQuotes()
+    {
+        $input = '<p><a href="hyde::route("home")">Home</a></p>';
+        $expected = '<p><a href="home.html">Home</a></p>';
 
-        $inputUnquoted = '<p><a href="hyde::route(home)">Home</a></p>';
-        $this->assertSame($expected, DynamicMarkdownLinkProcessor::postprocess($inputUnquoted));
+        $this->assertSame($expected, DynamicMarkdownLinkProcessor::postprocess($input));
+    }
+
+    public function testRouteReplacementWithoutQuotes()
+    {
+        $input = '<p><a href="hyde::route(home)">Home</a></p>';
+        $expected = '<p><a href="home.html">Home</a></p>';
+
+        $this->assertSame($expected, DynamicMarkdownLinkProcessor::postprocess($input));
     }
 
     public function testRelativeLinkReplacement()
     {
+        $input = '<p><a href="hyde::relativeLink(\'about\')">About</a></p>';
         $expected = '<p><a href="about">About</a></p>';
 
-        $inputSingleQuotes = '<p><a href="hyde::relativeLink(\'about\')">About</a></p>';
-        $this->assertSame($expected, DynamicMarkdownLinkProcessor::postprocess($inputSingleQuotes));
+        $this->assertSame($expected, DynamicMarkdownLinkProcessor::postprocess($input));
+    }
 
-        $inputDoubleQuotes = '<p><a href="hyde::relativeLink("about")">About</a></p>';
-        $this->assertSame($expected, DynamicMarkdownLinkProcessor::postprocess($inputDoubleQuotes));
+    public function testRelativeLinkReplacementWithDoubleQuotes()
+    {
+        $input = '<p><a href="hyde::relativeLink("about")">About</a></p>';
+        $expected = '<p><a href="about">About</a></p>';
 
-        $inputUnquoted = '<p><a href="hyde::relativeLink(about)">About</a></p>';
-        $this->assertSame($expected, DynamicMarkdownLinkProcessor::postprocess($inputUnquoted));
+        $this->assertSame($expected, DynamicMarkdownLinkProcessor::postprocess($input));
+    }
+
+    public function testRelativeLinkReplacementWithoutQuotes()
+    {
+        $input = '<p><a href="hyde::relativeLink(about)">About</a></p>';
+        $expected = '<p><a href="about">About</a></p>';
+
+        $this->assertSame($expected, DynamicMarkdownLinkProcessor::postprocess($input));
     }
 
     public function testAssetReplacement()
     {
+        $input = '<p><img src="hyde::asset(\'image.jpg\')" alt="Image" /></p>';
         $expected = '<p><img src="media/image.jpg" alt="Image" /></p>';
 
-        $inputSingleQuotes = '<p><img src="hyde::asset(\'image.jpg\')" alt="Image" /></p>';
-        $this->assertSame($expected, DynamicMarkdownLinkProcessor::postprocess($inputSingleQuotes));
+        $this->assertSame($expected, DynamicMarkdownLinkProcessor::postprocess($input));
+    }
 
-        $inputDoubleQuotes = '<p><img src="hyde::asset("image.jpg")" alt="Image" /></p>';
-        $this->assertSame($expected, DynamicMarkdownLinkProcessor::postprocess($inputDoubleQuotes));
+    public function testAssetReplacementWithDoubleQuotes()
+    {
+        $input = '<p><img src="hyde::asset("image.jpg")" alt="Image" /></p>';
+        $expected = '<p><img src="media/image.jpg" alt="Image" /></p>';
 
-        $inputUnquoted = '<p><img src="hyde::asset(image.jpg)" alt="Image" /></p>';
-        $this->assertSame($expected, DynamicMarkdownLinkProcessor::postprocess($inputUnquoted));
+        $this->assertSame($expected, DynamicMarkdownLinkProcessor::postprocess($input));
+    }
+
+    public function testAssetReplacementWithoutQuotes()
+    {
+        $input = '<p><img src="hyde::asset(image.jpg)" alt="Image" /></p>';
+        $expected = '<p><img src="media/image.jpg" alt="Image" /></p>';
+
+        $this->assertSame($expected, DynamicMarkdownLinkProcessor::postprocess($input));
     }
 
     public function testMultipleReplacements()
     {
+        $input = <<<'MARKDOWN'
+        <a href="hyde::route('home')">Home</a>
+        <a href="hyde::relativeLink('about')">About</a>
+        <img src="hyde::asset('logo.png')" alt="Logo" />
+        MARKDOWN;
+
         $expected = <<<'HTML'
         <a href="home.html">Home</a>
         <a href="about">About</a>
         <img src="media/logo.png" alt="Logo" />
         HTML;
 
-        $inputSingleQuotes = <<<'MARKDOWN'
-        <a href="hyde::route('home')">Home</a>
-        <a href="hyde::relativeLink('about')">About</a>
-        <img src="hyde::asset('logo.png')" alt="Logo" />
-        MARKDOWN;
-
-        $this->assertSame($expected, DynamicMarkdownLinkProcessor::postprocess($inputSingleQuotes));
-
-        $inputDoubleQuotes = <<<'MARKDOWN'
-        <a href="hyde::route("home")">Home</a>
-        <a href="hyde::relativeLink("about")">About</a>
-        <img src="hyde::asset("logo.png")" alt="Logo" />
-        MARKDOWN;
-
-        $this->assertSame($expected, DynamicMarkdownLinkProcessor::postprocess($inputDoubleQuotes));
-
-        $inputUnquoted = <<<'MARKDOWN'
-        <a href="hyde::route(home)">Home</a>
-        <a href="hyde::relativeLink(about)">About</a>
-        <img src="hyde::asset(logo.png)" alt="Logo" />
-        MARKDOWN;
-
-        $this->assertSame($expected, DynamicMarkdownLinkProcessor::postprocess($inputUnquoted));
+        $this->assertSame($expected, DynamicMarkdownLinkProcessor::postprocess($input));
     }
 
     public function testNoReplacements()
     {
         $input = '<p>This is a regular <a href="https://example.com">link</a> with no Hyde syntax.</p>';
+
         $this->assertSame($input, DynamicMarkdownLinkProcessor::postprocess($input));
     }
 }
