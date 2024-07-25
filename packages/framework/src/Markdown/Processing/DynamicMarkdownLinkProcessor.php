642ec731c2bacfb9872234c0999e8ddb3e588358
Caen De Silva <caen@desilva.se>
2024-07-25 10:44:56 +0200

    Draft new processing loop

--- packages/framework/src/Markdown/Processing/DynamicMarkdownLinkProcessor.php
+++ packages/framework/src/Markdown/Processing/DynamicMarkdownLinkProcessor.php
@@ -11,6 +11,23 @@ class DynamicMarkdownLinkProcessor implements MarkdownPostProcessorContract
 {
     public static function postprocess(string $html): string
     {
+        foreach (static::routeMap() as $sourcePath => $route) {
+            $patterns = [
+                '<a href="'.$sourcePath.'">',
+                '<a href="/'.$sourcePath.'">',
+            ];
+
+            $html = str_replace($patterns, '<a href="'.$route->getLink().'">', $html);
+        }
+
+        foreach (static::assetMap() as $path => $mediaFile) {
+            $patterns = [
+                '<img src="'.$path.'"',
+                '<img src="/'.$path.'"',
+            ];
+
+            $html = str_replace($patterns, '<img src="'.static::assetPath($mediaFile).'"', $html);
+        }
 
         return $html;
     }
