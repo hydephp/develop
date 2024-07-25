dee2c96d87af33711b6636ddc213abe3d802f030
Caen De Silva <caen@desilva.se>
2024-07-25 10:44:37 +0200

    Remove unused import

--- packages/framework/src/Markdown/Processing/DynamicMarkdownLinkProcessor.php
+++ packages/framework/src/Markdown/Processing/DynamicMarkdownLinkProcessor.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace Hyde\Markdown\Processing;
 
 use Hyde\Hyde;
-use Hyde\Framework\Exceptions\RouteNotFoundException;
 use Hyde\Markdown\Contracts\MarkdownPostProcessorContract;
 
 class DynamicMarkdownLinkProcessor implements MarkdownPostProcessorContract
