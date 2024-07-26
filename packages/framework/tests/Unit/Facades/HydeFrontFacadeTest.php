718ac2a22749edbec9424005a934bc4e0b0d9277
Caen De Silva <caen@desilva.se>
2024-07-26 14:26:32 +0200

    Create HydeFrontFacadeTest.php

---
+++ packages/framework/tests/Unit/Facades/HydeFrontFacadeTest.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hyde\Framework\Testing\Unit\Facades;
+
+use Hyde\Testing\UnitTestCase;
+
+/**
+ * @covers \Hyde\Facades\HydeFront
+ */
+class HydeFrontFacadeTest extends UnitTestCase
+{
+    //
+}
