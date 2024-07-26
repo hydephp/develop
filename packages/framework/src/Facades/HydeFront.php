63b414a88724af984843ee2c77b95e586ae986a3
Caen De Silva <caen@desilva.se>
2024-07-26 15:56:09 +0200

    Compress comment formatting

--- packages/framework/src/Facades/HydeFront.php
+++ packages/framework/src/Facades/HydeFront.php
@@ -42,9 +42,7 @@ class HydeFront
         return sprintf(static::HYDEFRONT_CDN_URL, static::version(), $file);
     }
 
-    /**
-     * This method is used to inject the Tailwind CSS configuration into the Play CDN integration.
-     */
+    /** This method is used to inject the Tailwind CSS configuration into the Play CDN integration. */
     public static function injectTailwindConfig(): string
     {
         if (! file_exists(Hyde::path('tailwind.config.js'))) {
