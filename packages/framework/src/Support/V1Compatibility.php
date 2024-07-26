678b6f75c1c4fd60d989d2642e4f34aecae97d07
Caen De Silva <caen@desilva.se>
2024-07-26 17:12:30 +0200

    Reapply "Ignore class from code coverage"
    This reverts commit 2004a4e67e8fd80b20ffb4f96d6dd177bf26b773.

--- packages/framework/src/Support/V1Compatibility.php
+++ packages/framework/src/Support/V1Compatibility.php
@@ -11,6 +11,8 @@ use Hyde\Support\Filesystem\MediaFile;
  * @internal Hyde Facade support to aid in the v1 to v2 transition.
  *
  * @deprecated All code here is deprecated, and exists to help you transition.
+ *
+ * @codeCoverageIgnore This class is ignored from code coverage.
  */
 trait V1Compatibility
 {
