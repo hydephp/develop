24571037a9a2ba5eac12f03ffd60267756b9ca67
Caen De Silva <caen@desilva.se>
2024-07-26 15:11:24 +0200

    Add rich PHPDoc method annotations

--- packages/framework/src/Facades/HydeFront.php
+++ packages/framework/src/Facades/HydeFront.php
@@ -19,11 +19,21 @@ class HydeFront
     /** @var string The default HydeFront CDN path pattern. */
     protected const HYDEFRONT_CDN_URL = 'https://cdn.jsdelivr.net/npm/hydefront@%s/dist/%s';
 
+    /**
+     * Get the current version of the HydeFront package.
+     *
+     * @return string {@see HYDEFRONT_VERSION}
+     */
     public static function version(): string
     {
         return static::HYDEFRONT_VERSION;
     }
 
+    /**
+     * Get the CDN link for a specific file.
+     *
+     * @param  'app.css'|'hyde.css'|'hyde.css.map'  $file
+     */
     public static function cdnLink(string $file): string
     {
         return sprintf(static::HYDEFRONT_CDN_URL, static::version(), $file);
