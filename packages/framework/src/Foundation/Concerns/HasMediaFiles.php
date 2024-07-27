1068d767fad87ad6ebe257ea4299416d0c7fb87d
Caen De Silva <caen@desilva.se>
2024-07-27 18:31:50 +0200

    Create HasMediaFiles.php

---
+++ packages/framework/src/Foundation/Concerns/HasMediaFiles.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hyde\Foundation\Concerns;
+
+/**
+ * @internal Single-use trait for the Filesystem class.
+ *
+ * @see \Hyde\Foundation\Kernel\Filesystem
+ */
+trait HasMediaFiles
+{
+    //
+}
