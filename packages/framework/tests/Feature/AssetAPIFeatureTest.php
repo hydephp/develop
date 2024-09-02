9bb8e6c2f7075f330a59d1a74b43cd2161b6b2e3
Caen De Silva <caen@desilva.se>
2024-09-02 13:26:41 +0200

    Create AssetAPIFeatureTest.php

---
+++ packages/framework/tests/Feature/AssetAPIFeatureTest.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+use Hyde\Testing\TestCase;
+
+/**
+ * @coversNothing High level test for the Asset API
+ */
+class AssetAPIFeatureTest extends TestCase
+{
+    //
+}
