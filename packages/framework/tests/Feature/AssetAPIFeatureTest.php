001cd3ae42fec6c9278d57a7598f92fcc9be9afb
Caen De Silva <caen@desilva.se>
2024-09-02 13:43:53 +0200

    Test Asset API usages in Blade views

--- packages/framework/tests/Feature/AssetAPIFeatureTest.php
+++ packages/framework/tests/Feature/AssetAPIFeatureTest.php
@@ -2,12 +2,44 @@
 
 declare(strict_types=1);
 
+use Hyde\Hyde;
 use Hyde\Testing\TestCase;
+use Illuminate\Support\Facades\Blade;
 
 /**
  * @coversNothing High level test for the Asset API
  */
 class AssetAPIFeatureTest extends TestCase
 {
-    //
+    public function testAssetAPIUsagesInBladeViews()
+    {
+        $view = <<<'Blade'
+        @if(Asset::hasMediaFile('app.css'))
+            <link rel="stylesheet" href="{{ Asset::mediaLink('app.css') }}">
+            <link rel="stylesheet" href="{{ Asset::get('app.css') }}">
+            <link rel="stylesheet" href="{{ Hyde::asset('app.css') }}">
+            <link rel="stylesheet" href="{{ asset('app.css') }}">
+        @endif
+        
+        @if(Asset::hasMediaFile('missing.png'))
+            Found missing.png
+        @else
+            Missing missing.png
+        @endif
+        Blade;
+
+        $html = Blade::render($view);
+
+        $version = Hyde::assets()->get('app.css')->getHash();
+
+        $this->assertSame(<<<HTML
+        <link rel="stylesheet" href="media/app.css?v=$version">
+            <link rel="stylesheet" href="media/app.css">
+            <link rel="stylesheet" href="media/app.css">
+            <link rel="stylesheet" href="media/app.css">
+        
+            Missing missing.png
+
+        HTML, $html);
+    }
 }
