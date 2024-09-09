9165a16797b9eaad69bd0b3319e76c190ad071df
Caen De Silva <caen@desilva.se>
2024-09-09 14:36:40 +0200

    Add additional tests

--- packages/framework/tests/Unit/Facades/AuthorTest.php
+++ packages/framework/tests/Unit/Facades/AuthorTest.php
@@ -29,6 +29,15 @@ class AuthorTest extends UnitTestCase
         $this->assertSame('https://johndoe.com', $author->website);
     }
 
+    public function testCreateWithOnlyRequiredFields()
+    {
+        $author = Author::create('john_doe');
+
+        $this->assertSame('john_doe', $author->username);
+        $this->assertSame('john_doe', $author->name);
+        $this->assertNull($author->website);
+    }
+
     public function testGet()
     {
         $author = Author::get('mr_hyde');
@@ -36,7 +45,15 @@ class AuthorTest extends UnitTestCase
         $this->assertSame('mr_hyde', $author->username);
         $this->assertSame('Mr. Hyde', $author->name);
         $this->assertSame('https://hydephp.com', $author->website);
+    }
 
+    public function testGetWithNotSetUsername()
+    {
+        $this->assertEquals(Author::create('foo'), Author::get('foo'));
+    }
+
+    public function testGetAliasesPostAuthor()
+    {
         $this->assertEquals(PostAuthor::get('foo'), Author::get('foo'));
     }
 
@@ -45,5 +62,12 @@ class AuthorTest extends UnitTestCase
         $authors = Author::all();
         $this->assertCount(1, $authors);
         $this->assertContainsOnlyInstancesOf(PostAuthor::class, $authors);
+        $this->assertEquals(Author::get('mr_hyde'), $authors->first());
+    }
+
+    public function testAllWithNoAuthors()
+    {
+        self::mockConfig(['hyde.authors' => []]);
+        $this->assertEmpty(Author::all());
     }
 }
