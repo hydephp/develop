61d79faf397001aebd8c5dda02d64bd142eea2c4
Caen De Silva <caen@desilva.se>
2024-11-05 21:19:38 +0100

    Remove support for unpadded times in filenames

--- packages/framework/tests/Unit/BlogPostDatePrefixHelperUnitTest.php
+++ packages/framework/tests/Unit/BlogPostDatePrefixHelperUnitTest.php
@@ -61,13 +61,6 @@ class BlogPostDatePrefixHelperUnitTest extends UnitTestCase
         DatePrefixHelper::extractDate('2024-11-05-25-99-my-post.md');
     }
 
-    public function testExtractDateWithMinimalPadding()
-    {
-        $date = DatePrefixHelper::extractDate('2024-1-5-my-post.md');
-        $this->assertInstanceOf(DateTime::class, $date);
-        $this->assertSame('2024-01-05 00:00', $date->format('Y-m-d H:i'));
-    }
-
     public function testStripDatePrefixWithDateOnly()
     {
         $result = DatePrefixHelper::stripDatePrefix('2024-11-05-my-post.md');
