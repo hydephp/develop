2d67433e7c16541f51dd7b572c663134a4724067
Caen De Silva <caen@desilva.se>
2024-11-05 21:15:39 +0100

    Create BlogPostDatePrefixHelperUnitTest.php

---
+++ packages/framework/tests/Unit/BlogPostDatePrefixHelperUnitTest.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+use Hyde\Testing\UnitTestCase;
+
+/**
+ * @covers \Hyde\Framework\Features\Blogging\DatePrefixHelper
+ *
+ * @see \Hyde\Framework\Testing\Feature\BlogPostDatePrefixHelperTest
+ */
+class BlogPostDatePrefixHelperUnitTest extends UnitTestCase
+{
+    protected static bool $needsConfig = true;
+}
