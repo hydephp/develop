bb63922c1d8d19fc070bdc57dc085bdf50747e60
Caen De Silva <caen@desilva.se>
2024-11-05 14:22:55 +0100

    Add deprecated attributes

--- packages/framework/src/Foundation/Kernel/Hyperlinks.php
+++ packages/framework/src/Foundation/Kernel/Hyperlinks.php
@@ -7,6 +7,7 @@ namespace Hyde\Foundation\Kernel;
 use Hyde\Facades\Config;
 use Hyde\Support\Models\Route;
 use Hyde\Foundation\HydeKernel;
+use JetBrains\PhpStorm\Deprecated;
 use Hyde\Framework\Exceptions\BaseUrlNotSetException;
 use Hyde\Framework\Exceptions\FileNotFoundException;
 use Illuminate\Support\Str;
@@ -95,6 +96,7 @@ class Hyperlinks
      *
      * @deprecated This method will be removed in v2.0. Please use `asset()` instead.
      */
+    #[Deprecated(reason: 'Use `asset` method instead.', replacement: '%class%->asset(%parameter0%)')]
     public function mediaLink(string $destination, bool $validate = false): string
     {
         if ($validate && ! file_exists($sourcePath = "{$this->kernel->getMediaDirectory()}/$destination")) {
