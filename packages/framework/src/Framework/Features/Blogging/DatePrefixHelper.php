91a8a0a1c649a9c774edab9e5ef1b3d97f3d2b49
Caen De Silva <caen@desilva.se>
2024-11-05 20:19:42 +0100

    Create BlogPostDatePrefixTest.php

--- packages/framework/src/Framework/Features/Blogging/DatePrefixHelper.php
+++ packages/framework/src/Framework/Features/Blogging/DatePrefixHelper.php
@@ -14,31 +14,31 @@ use Hyde\Support\Models\DateString;
 class DatePrefixHelper
 {
     protected const DATE_PATTERN = '/^(\d{4}-\d{2}-\d{2})(?:-(\d{2}-\d{2}))?-/';
-    
+
     public static function hasDatePrefix(string $identifier): bool
     {
         return preg_match(static::DATE_PATTERN, $identifier) === 1;
     }
-    
+
     public static function extractDate(string $identifier): ?DateTimeInterface
     {
         if (! preg_match(static::DATE_PATTERN, $identifier, $matches)) {
             return null;
         }
-        
+
         $dateString = $matches[1];
         if (isset($matches[2])) {
             $dateString .= ' ' . str_replace('-', ':', $matches[2]);
         }
-        
+
         return new DateTime($dateString);
     }
-    
+
     public static function stripDatePrefix(string $identifier): string
     {
         return preg_replace(static::DATE_PATTERN, '', $identifier);
     }
-    
+
     public static function createDateString(DateTimeInterface $date): DateString
     {
         return new DateString($date->format('Y-m-d H:i'));
---
+++ packages/framework/tests/Feature/BlogPostDatePrefixTest.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Hyde\Framework\Testing\Feature;
+
+use Hyde\Framework\Features\Blogging\DatePrefixHelper;
+use Hyde\Pages\MarkdownPost;
+use Hyde\Testing\TestCase;
+
+class BlogPostDatePrefixTest extends TestCase
+{
+    public function testCanDetectDatePrefix()
+    {
+        $this->assertTrue(DatePrefixHelper::hasDatePrefix('2024-11-05-my-post'));
+        $this->assertTrue(DatePrefixHelper::hasDatePrefix('2024-11-05-10-30-my-post'));
+        $this->assertFalse(DatePrefixHelper::hasDatePrefix('my-post'));
+    }
+
+    public function testCanExtractDateFromPrefix()
+    {
+        $date = DatePrefixHelper::extractDate('2024-11-05-my-post');
+        $this->assertNotNull($date);
+        $this->assertEquals('2024-11-05', $date->format('Y-m-d'));
+
+        $date = DatePrefixHelper::extractDate('2024-11-05-10-30-my-post');
+        $this->assertNotNull($date);
+        $this->assertEquals('2024-11-05 10:30', $date->format('Y-m-d H:i'));
+    }
+
+    public function testDatePrefixIsStrippedFromRouteKey()
+    {
+        $this->file('_posts/2024-11-05-my-post.md', '# Hello World');
+        $post = MarkdownPost::parse('2024-11-05-my-post');
+        
+        $this->assertEquals('posts/my-post', $post->getRouteKey());
+    }
+
+    public function testDateFromPrefixIsUsedWhenNoFrontMatterDate()
+    {
+        $this->file('_posts/2024-11-05-my-post.md', '# Hello World');
+        $post = MarkdownPost::parse('2024-11-05-my-post');
+        
+        $this->assertEquals('2024-11-05 00:00', $post->date->format('Y-m-d H:i'));
+    }
+
+    public function testFrontMatterDateTakesPrecedenceOverPrefix()
+    {
+        $this->file('_posts/2024-11-05-my-post.md', <<<MD
+        ---
+        date: 2024-12-25
+        ---
+        # Hello World
+        MD);
+        
+        $post = MarkdownPost::parse('2024-11-05-my-post');
+        $this->assertEquals('2024-12-25', $post->date->format('Y-m-d'));
+    }
+}
