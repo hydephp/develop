9f3477cfac795d4a9209a04b06a0893265e3977e
Caen De Silva <caen@desilva.se>
2024-11-10 11:52:37 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,7 @@
+// Using Vite is optional, as the styles you need to get started are already included.
+// However, if you customize existing or add new Tailwind classes, you can use Vite
+// to compile the assets. See https://hydephp.com/docs/1.x/managing-assets.html.
+
 import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
