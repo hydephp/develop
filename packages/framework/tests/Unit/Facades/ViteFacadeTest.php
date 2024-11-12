e3dc0a2ad9de7ab2ccd261e9d705b2b558a53a9c
Caen De Silva <caen@desilva.se>
2024-11-12 09:39:01 +0100

    Create ViteFacadeTest.php

---
+++ packages/framework/tests/Unit/Facades/ViteFacadeTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Hyde\Framework\Testing\Unit\Facades;
+
+use Hyde\Testing\UnitTestCase;
+
+/**
+ * @covers \Hyde\Facades\Vite
+ */
+class ViteFacadeTest extends UnitTestCase
+{
+    //
+}
