8aba46bfe8ae67a8f2a80bd87d7a2a746f1e39af
Caen De Silva <caen@desilva.se>
2024-11-12 09:47:53 +0100

    Create Vite server configuration

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,13 @@ import tailwindcss from 'tailwindcss';
 import autoprefixer from 'autoprefixer';
 
 export default defineConfig({
+    server: {
+        port: 3000,
+        hmr: {
+            host: 'localhost',
+            port: 3000,
+        },
+    },
     css: {
         postcss: {
             plugins: [
