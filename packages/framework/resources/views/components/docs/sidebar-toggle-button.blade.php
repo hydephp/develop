0e435902d6856defa017d35dc4528aa7da12fea4
Caen De Silva <caen@desilva.se>
2024-11-15 20:07:45 +0100

    Tweak sidebar toggle styles to blend in better

--- packages/framework/resources/views/components/docs/sidebar-toggle-button.blade.php
+++ packages/framework/resources/views/components/docs/sidebar-toggle-button.blade.php
@@ -4,10 +4,10 @@
     aria-label="Toggle sidebar navigation menu" 
     @click="sidebarOpen = ! sidebarOpen" 
     :class="{'[&>span:first-child]:opacity-0 [&>span:nth-child(2)]:rotate-45 [&>span:nth-child(3)]:-rotate-45 [&>span:last-child]:opacity-0': sidebarOpen}"
-    class="relative inline-block w-8 h-8"
+    class="relative inline-block w-8 h-8 hover:text-gray-700 dark:text-gray-200"
 >
-    <span class="block w-5 h-[2.375px] bg-black dark:bg-white absolute left-[5.5px] top-[9px] transition-all duration-300 ease-out" role="presentation"></span>
-    <span class="block w-5 h-[2.375px] bg-black dark:bg-white absolute left-[5.5px] top-[15px] transition-all duration-300 ease-out origin-center" role="presentation"></span>
-    <span class="block w-5 h-[2.375px] bg-black dark:bg-white absolute left-[5.5px] top-[15px] transition-all duration-300 ease-out origin-center" role="presentation"></span>
-    <span class="block w-5 h-[2.375px] bg-black dark:bg-white absolute left-[5.5px] top-[21px] transition-all duration-300 ease-out" role="presentation"></span>
+    <span class="block w-5 h-0.5 bg-current absolute left-1.5 top-1 transition-all duration-300 ease-out" role="presentation"></span>
+    <span class="block w-5 h-0.5 bg-current absolute left-1.5 top-2.5 transition-all duration-300 ease-out origin-center" role="presentation"></span>
+    <span class="block w-5 h-0.5 bg-current absolute left-1.5 top-2.5 transition-all duration-300 ease-out origin-center" role="presentation"></span>
+    <span class="block w-5 h-0.5 bg-current absolute left-1.5 top-4 transition-all duration-300 ease-out" role="presentation"></span>
 </button>
