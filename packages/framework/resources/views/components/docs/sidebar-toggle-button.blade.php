1ee06c47b52bfa1104a58213a121f3624a2a70c6
Caen De Silva <caen@desilva.se>
2024-11-15 19:40:55 +0100

    Replace the sidebar toggle styles with pure Tailwind

--- packages/framework/resources/views/components/docs/sidebar-toggle-button.blade.php
+++ packages/framework/resources/views/components/docs/sidebar-toggle-button.blade.php
@@ -1,6 +1,13 @@
-<button id="sidebar-toggle" title="Toggle sidebar" aria-label="Toggle sidebar navigation menu" @click="sidebarOpen = ! sidebarOpen" :class="{'active' : sidebarOpen}">
-    <span class="icon-bar dark:bg-white h-0" role="presentation"></span>
-    <span class="icon-bar dark:bg-white h-0" role="presentation"></span>
-    <span class="icon-bar dark:bg-white h-0" role="presentation"></span>
-    <span class="icon-bar dark:bg-white h-0" role="presentation"></span>
+<button 
+    id="sidebar-toggle" 
+    title="Toggle sidebar" 
+    aria-label="Toggle sidebar navigation menu" 
+    @click="sidebarOpen = ! sidebarOpen" 
+    :class="{'[&>span:first-child]:opacity-0 [&>span:nth-child(2)]:rotate-45 [&>span:nth-child(3)]:-rotate-45 [&>span:last-child]:opacity-0': sidebarOpen}"
+    class="relative inline-block w-8 h-8"
+>
+    <span class="block w-5 h-[2.375px] bg-black dark:bg-white absolute left-[5.5px] top-[9px] transition-all duration-300 ease-out" role="presentation"></span>
+    <span class="block w-5 h-[2.375px] bg-black dark:bg-white absolute left-[5.5px] top-[15px] transition-all duration-300 ease-out origin-center" role="presentation"></span>
+    <span class="block w-5 h-[2.375px] bg-black dark:bg-white absolute left-[5.5px] top-[15px] transition-all duration-300 ease-out origin-center" role="presentation"></span>
+    <span class="block w-5 h-[2.375px] bg-black dark:bg-white absolute left-[5.5px] top-[21px] transition-all duration-300 ease-out" role="presentation"></span>
 </button>
