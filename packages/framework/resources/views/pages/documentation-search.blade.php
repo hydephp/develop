b592ae021b5e2d5cf8143191dc853a99fd5c9161
Caen De Silva <caen@desilva.se>
2024-11-16 19:28:17 +0100

    Fix not prose styles

--- packages/framework/resources/views/pages/documentation-search.blade.php
+++ packages/framework/resources/views/pages/documentation-search.blade.php
@@ -2,5 +2,8 @@
 @section('content')
     <h1>Search the documentation site</h1>
     <style>#search-menu-button, .edit-page-link { display: none !important; }</style>
-    <x-hyde::docs.hyde-search class="max-w-sm" :modal="false" />
+
+    <div class="not-prose">
+        <x-hyde::docs.hyde-search class="max-w-sm" :modal="false" />
+    </div>
 @endsection
