8ebf91d0aefdd72b7b7fa4186ccb9802de3c3c0b
Caen De Silva <caen@desilva.se>
2024-11-16 19:27:44 +0100

    Tweak full page search styles

--- packages/framework/resources/views/pages/documentation-search.blade.php
+++ packages/framework/resources/views/pages/documentation-search.blade.php
@@ -1,12 +1,6 @@
 @extends('hyde::layouts.docs')
 @section('content')
     <h1>Search the documentation site</h1>
-    <style>#search-menu-button, .edit-page-link {
-            display: none !important;
-        }
-
-        #search-results {
-            max-height: unset !important;
-        }</style>
-    <x-hyde::docs.hyde-search class="max-w-xs border-b-4 border-indigo-400"/>
+    <style>#search-menu-button, .edit-page-link { display: none !important; }</style>
+    <x-hyde::docs.hyde-search class="max-w-sm" :modal="false" />
 @endsection
