6a0b6381800de98b8ca1e59360aca980d531c09a
Caen De Silva <caen@desilva.se>
2024-11-17 15:50:34 +0100

    Deprecate helper method which may become obsolete

--- packages/framework/src/Facades/HydeFront.php
+++ packages/framework/src/Facades/HydeFront.php
@@ -35,6 +35,8 @@ class HydeFront
     /**
      * Get the CDN link for a specific file.
      *
+     * @deprecated This method may be removed before the final v2.0 release as there is now currently only one file available.
+     *
      * @param  'app.css'  $file
      *
      * @link https://cdn.jsdelivr.net/npm/hydefront@latest/dist/ for the available files.
