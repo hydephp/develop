3a92381384d07620d54ba9b4550acc3baa7ef225
Caen De Silva <caen@desilva.se>
2024-11-17 14:32:34 +0100

    Refactor to use the class directive

--- packages/framework/resources/views/components/filepath-label.blade.php
+++ packages/framework/resources/views/components/filepath-label.blade.php
@@ -1,1 +1,5 @@
-<small class="relative {{ $torchlight ? '-top-1 right-3' : 'top-0 right-0' }} float-right opacity-50 hover:opacity-100 transition-opacity duration-250 not-prose hidden md:block"><span class="sr-only">Filepath: </span>{{ $path }}</small>
+<small @class([
+    'relative float-right opacity-50 hover:opacity-100 transition-opacity duration-250 not-prose hidden md:block',
+    '-top-1 right-3' => $torchlight,
+    'top-0 right-0' => ! $torchlight,
+])><span class="sr-only">Filepath: </span>{{ $path }}</small>
