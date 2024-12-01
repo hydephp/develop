598d9c93cd3228851f041558cdef0e3bc3d89d4d
Caen De Silva <caen@desilva.se>
2024-12-01 23:04:22 +0100

    Implement initial unit test

--- packages/framework/tests/Unit/HeadingRendererUnitTest.php
+++ packages/framework/tests/Unit/HeadingRendererUnitTest.php
@@ -4,17 +4,22 @@ declare(strict_types=1);
 
 namespace Hyde\Framework\Testing\Unit;
 
+use Hyde\Markdown\Processing\HeadingRenderer;
+use Hyde\Pages\DocumentationPage;
 use Hyde\Testing\UnitTestCase;
 use Illuminate\Contracts\View\Factory as FactoryContract;
 use Illuminate\Events\Dispatcher;
+use Illuminate\Filesystem\Filesystem;
 use Illuminate\View\Compilers\BladeCompiler;
 use Illuminate\View\Engines\CompilerEngine;
 use Illuminate\View\Engines\EngineResolver;
 use Illuminate\View\Factory;
-use Hyde\Markdown\Processing\HeadingRenderer;
-use Hyde\Pages\DocumentationPage;
 use Illuminate\View\FileViewFinder;
-use Illuminate\Filesystem\Filesystem;
+use InvalidArgumentException;
+use League\CommonMark\Extension\CommonMark\Node\Block\Heading;
+use League\CommonMark\Node\Node;
+use League\CommonMark\Renderer\ChildNodeRendererInterface;
+use Mockery;
 
 /**
  * @covers \Hyde\Markdown\Processing\HeadingRenderer
@@ -23,6 +28,8 @@ use Illuminate\Filesystem\Filesystem;
  */
 class HeadingRendererUnitTest extends UnitTestCase
 {
+    protected static bool $needsConfig = true;
+
     protected function setUp(): void
     {
         $this->createRealBladeCompilerEnvironment();
@@ -38,6 +45,109 @@ class HeadingRendererUnitTest extends UnitTestCase
     {
         $renderer = new HeadingRenderer(DocumentationPage::class);
         $this->assertInstanceOf(HeadingRenderer::class, $renderer);
+    }
+
+    public function testCanRenderHeading()
+    {
+        $childRenderer = $this->mockChildNodeRenderer();
+        $renderer = new HeadingRenderer(DocumentationPage::class);
+        $rendered = $renderer->render(new Heading(2), $childRenderer);
+
+        $this->assertStringContainsString('Test Heading', $rendered);
+        $this->assertStringContainsString('<h2', $rendered);
+        $this->assertStringContainsString('</h2>', $rendered);
+
+        $this->assertSame('<h2>Test Heading<a id="test-heading" href="#test-heading" class="heading-permalink" title="Permalink"></a></h2>', $rendered);
+    }
+
+    public function testAddsPermalinkToValidHeadings()
+    {
+        $childRenderer = $this->mockChildNodeRenderer();
+        $renderer = new HeadingRenderer(DocumentationPage::class);
+        $heading = new Heading(2);
+        $rendered = $renderer->render($heading, $childRenderer);
+
+        $this->assertStringContainsString('heading-permalink', $rendered);
+        $this->assertStringContainsString('#test-heading', $rendered);
+    }
+
+    public function testDoesNotAddPermalinkToLevelOneHeadingByDefault()
+    {
+        $childRenderer = $this->mockChildNodeRenderer();
+        $renderer = new HeadingRenderer(DocumentationPage::class);
+        $rendered = $renderer->render(new Heading(1), $childRenderer);
+
+        $this->assertStringNotContainsString('heading-permalink', $rendered);
+    }
+
+    public function testAddsPermalinkToLevelOneHeadingIfConfigured()
+    {
+        self::mockConfig(['markdown.permalinks.min_level' => 1]);
+
+        $childRenderer = $this->mockChildNodeRenderer();
+        $renderer = new HeadingRenderer(DocumentationPage::class);
+        $rendered = $renderer->render(new Heading(1), $childRenderer);
+
+        $this->assertStringContainsString('heading-permalink', $rendered);
+    }
+
+    public function testThrowsExceptionForInvalidNode()
+    {
+        $invalidNode = Mockery::mock(Node::class);
+
+        $this->expectException(InvalidArgumentException::class);
+        $renderer = new HeadingRenderer(DocumentationPage::class);
+
+        $childRenderer = Mockery::mock(ChildNodeRendererInterface::class);
+        $renderer->render($invalidNode, $childRenderer);
+    }
+
+    public function testDoesNotAddPermalinksIfDisabledInConfiguration()
+    {
+        self::mockConfig(['markdown.permalinks.enabled' => false]);
+
+        $childRenderer = $this->mockChildNodeRenderer();
+        $renderer = new HeadingRenderer(DocumentationPage::class);
+        $rendered = $renderer->render(new Heading(2), $childRenderer);
+
+        $this->assertStringNotContainsString('heading-permalink', $rendered);
+    }
+
+    public function testForwardsHeadingAttributes()
+    {
+        $childRenderer = $this->mockChildNodeRenderer('Test Content');
+
+        $renderer = new HeadingRenderer(DocumentationPage::class);
+        $heading = new Heading(2);
+        $heading->data->set('attributes', ['class' => 'custom-class']);
+        $rendered = $renderer->render($heading, $childRenderer);
+
+        $this->assertStringContainsString('class="custom-class"', $rendered);
+    }
+
+    public function testForwardsArbitraryHeadingAttributes()
+    {
+        $childRenderer = $this->mockChildNodeRenderer('Test Content');
+
+        $renderer = new HeadingRenderer(DocumentationPage::class);
+        $heading = new Heading(2);
+        $heading->data->set('attributes', ['foo' => 'bar']);
+        $rendered = $renderer->render($heading, $childRenderer);
+
+        $this->assertStringContainsString('foo="bar"', $rendered);
+    }
+
+    public function testMergesAllHeadingAttributes()
+    {
+        $childRenderer = $this->mockChildNodeRenderer('Test Content');
+
+        $renderer = new HeadingRenderer(DocumentationPage::class);
+        $heading = new Heading(2);
+        $heading->data->set('attributes', ['class' => 'custom-class', 'foo' => 'bar']);
+        $rendered = $renderer->render($heading, $childRenderer);
+
+        $this->assertStringContainsString('class="custom-class"', $rendered);
+        $this->assertStringContainsString('foo="bar"', $rendered);
     }
 
     protected function createRealBladeCompilerEnvironment(): void
