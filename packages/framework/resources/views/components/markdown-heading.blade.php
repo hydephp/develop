24e459fc780cc5a6001c4411e11eb38b0d0ebbf0
Caen De Silva <caen@desilva.se>
2024-12-01 19:22:11 +0100

    Support extra attributes

--- packages/framework/resources/views/components/markdown-heading.blade.php
+++ packages/framework/resources/views/components/markdown-heading.blade.php
@@ -1,11 +1,11 @@
-@props(['level' => 1, 'id' => null, 'addPermalink' => config('markdown.features.permalinks', true)])
+@props(['level' => 1, 'id' => null, 'addPermalink' => config('markdown.features.permalinks', true), 'extraAttributes' => []])
 
 @php
     $tag = 'h' . $level;
     $id = $id ?? \Illuminate\Support\Str::slug($slot);
 @endphp
 
-<{{ $tag }} {{ $attributes->merge(['id' => $id]) }}>
+<{{ $tag }} {{ $attributes->merge(['id' => $id, ...$extraAttributes]) }}>
     {!! $slot !!}
     @if($addPermalink === true)
         <a href="#{{ $id }}" class="heading-permalink" aria-label="Permalink for this section"></a>
