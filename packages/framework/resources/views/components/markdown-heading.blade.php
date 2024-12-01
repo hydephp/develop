ed7f281675da62922d181ce7d59621d5730b987a
Caen De Silva <caen@desilva.se>
2024-12-01 18:50:14 +0100

    Create markdown-heading.blade.php

---
+++ packages/framework/resources/views/components/markdown-heading.blade.php
@@ -0,0 +1,13 @@
+@props(['level' => 1, 'id' => null])
+
+@php
+    $tag = 'h' . $level;
+    $id = $id ?? \Illuminate\Support\Str::slug($slot);
+@endphp
+
+<{{ $tag }} {{ $attributes->merge(['id' => $id]) }}>
+{{ $slot }}
+@if(config('markdown.features.permalinks', true))
+    <a href="#{{ $id }}" class="heading-permalink" aria-label="Permalink for this section"></a>
+@endif
+</{{ $tag }}> 
