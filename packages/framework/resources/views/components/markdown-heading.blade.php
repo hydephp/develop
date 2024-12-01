5607c6d63b8f90b5fe568ee361f5acbff15bf441
Caen De Silva <caen@desilva.se>
2024-12-01 18:50:41 +0100

    Indent code

--- packages/framework/resources/views/components/markdown-heading.blade.php
+++ packages/framework/resources/views/components/markdown-heading.blade.php
@@ -6,8 +6,8 @@
 @endphp
 
 <{{ $tag }} {{ $attributes->merge(['id' => $id]) }}>
-{!! $slot !!}
-@if(config('markdown.features.permalinks', true))
-    <a href="#{{ $id }}" class="heading-permalink" aria-label="Permalink for this section"></a>
-@endif
+    {!! $slot !!}
+    @if(config('markdown.features.permalinks', true))
+        <a href="#{{ $id }}" class="heading-permalink" aria-label="Permalink for this section"></a>
+    @endif
 </{{ $tag }}> 
