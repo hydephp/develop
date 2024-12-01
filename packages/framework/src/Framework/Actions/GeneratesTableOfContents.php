2f31aac74b25744d87b3e2181fd8048e05fa1d97
Caen De Silva <caen@desilva.se>
2024-12-01 14:20:04 +0100

    Rewrite table of contents generator with a custom implementation

--- packages/framework/src/Framework/Actions/GeneratesTableOfContents.php
+++ packages/framework/src/Framework/Actions/GeneratesTableOfContents.php
@@ -6,18 +6,11 @@ namespace Hyde\Framework\Actions;
 
 use Hyde\Facades\Config;
 use Hyde\Markdown\Models\Markdown;
+use Illuminate\Support\Str;
 use League\CommonMark\Environment\Environment;
 use League\CommonMark\Extension\CommonMark\CommonMarkCoreExtension;
 use League\CommonMark\Extension\HeadingPermalink\HeadingPermalinkExtension;
-use League\CommonMark\Extension\TableOfContents\TableOfContentsExtension;
-use League\CommonMark\MarkdownConverter;
 
-use function strpos;
-use function substr;
-
-/**
- * Generates a table of contents for the Markdown document, most commonly used for the sidebar.
- */
 class GeneratesTableOfContents
 {
     protected string $markdown;
@@ -29,41 +22,63 @@ class GeneratesTableOfContents
 
     public function execute(): string
     {
-        $config = [
-            'table_of_contents' => [
-                'html_class' => 'table-of-contents',
-                'position' => 'placeholder',
-                'placeholder' => '[[START_TOC]]',
-                'style' => 'bullet',
-                'min_heading_level' => Config::getInt('docs.sidebar.table_of_contents.min_heading_level', 2),
-                'max_heading_level' => Config::getInt('docs.sidebar.table_of_contents.max_heading_level', 4),
-                'normalize' => 'relative',
-            ],
-            'heading_permalink' => [
-                'fragment_prefix' => '',
-            ],
-        ];
+        $headings = $this->parseHeadings();
 
-        $environment = new Environment($config);
-        $environment->addExtension(new CommonMarkCoreExtension());
-        $environment->addExtension(new HeadingPermalinkExtension());
-        $environment->addExtension(new TableOfContentsExtension());
-
-        $converter = new MarkdownConverter($environment);
-        $html = $converter->convert($this->markdown."\n[[START_TOC]]")->getContent();
-
-        return $this->extractTableOfContents($html);
+        return view('hyde::components.docs.table-of-contents', [
+            'items' => $this->buildTableOfContents($headings),
+        ])->render();
     }
 
-    protected function extractTableOfContents(string $html): string
+    protected function parseHeadings(): array
     {
-        // The table of contents is always at the end of the document, so we can just strip everything before it.
-        $position = strpos($html, '<ul class="table-of-contents">');
-        if ($position === false) {
-            // The document has no headings, so we'll just return an empty string.
-            return '';
+        preg_match_all('/^#{2,4}\s+(.+)$/m', $this->markdown, $matches);
+        
+        $headings = [];
+        foreach ($matches[0] as $index => $heading) {
+            $level = substr_count($heading, '#');
+            $title = $matches[1][$index];
+            $slug = Str::slug($title);
+            
+            $headings[] = [
+                'level' => $level,
+                'title' => $title,
+                'slug' => $slug,
+            ];
+        }
+        
+        return $headings;
+    }
+
+    protected function buildTableOfContents(array $headings): array
+    {
+        $minLevel = Config::getInt('docs.sidebar.table_of_contents.min_heading_level', 2);
+        $maxLevel = Config::getInt('docs.sidebar.table_of_contents.max_heading_level', 4);
+        
+        $items = [];
+        $stack = [&$items];
+        $previousLevel = $minLevel;
+
+        foreach ($headings as $heading) {
+            if ($heading['level'] < $minLevel || $heading['level'] > $maxLevel) {
+                continue;
+            }
+
+            $item = [
+                'title' => $heading['title'],
+                'slug' => $heading['slug'],
+                'children' => [],
+            ];
+
+            if ($heading['level'] > $previousLevel) {
+                $stack[] = &$stack[count($stack) - 1][count($stack[count($stack) - 1]) - 1]['children'];
+            } elseif ($heading['level'] < $previousLevel) {
+                array_splice($stack, $heading['level'] - $minLevel + 1);
+            }
+
+            $stack[count($stack) - 1][] = $item;
+            $previousLevel = $heading['level'];
         }
 
-        return substr($html, $position);
+        return $items;
     }
 }
