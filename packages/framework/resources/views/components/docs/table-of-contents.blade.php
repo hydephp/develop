0a1d2e15275602d5663f65494faa177ce9b98cd6
Caen De Silva <caen@desilva.se>
2024-12-01 16:10:32 +0100

    Refactor to closer match existing styles

--- packages/framework/resources/views/components/docs/table-of-contents.blade.php
+++ packages/framework/resources/views/components/docs/table-of-contents.blade.php
@@ -1,10 +1,10 @@
 @props(['items', 'isChild' => false])
 
 @if(! empty($items))
-    <ul class="{{ ! $isChild ? 'table-of-contents py-3 space-y-1.5' : 'space-y-1.5' }}">
+    <ul class="{{ ! $isChild ? 'table-of-contents pb-3' : 'pl-2' }}">
         @foreach($items as $item)
-            <li>
-                <a href="#{{ $item['slug'] }}" class="block pl-8 -ml-8 opacity-80 hover:opacity-100 hover:bg-gray-200/20 transition-all duration-300 relative">
+            <li class="my-[0.35rem]">
+                <a href="#{{ $item['slug'] }}" class="block -ml-8 pl-8 opacity-80 hover:opacity-100 hover:bg-gray-200/20 transition-all duration-300 relative">
                     <span class="text-[75%] opacity-50 mr-1 hover:opacity-100 transition-opacity duration-300">#</span>
                     {{ $item['title'] }}
                 </a>
