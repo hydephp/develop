876fff8f7f3d268c734d76f6a8832ab2ae450b5d
Caen De Silva <caen@desilva.se>
2024-12-01 14:33:05 +0100

    Add lost Setext header support

--- packages/framework/src/Framework/Actions/GeneratesTableOfContents.php
+++ packages/framework/src/Framework/Actions/GeneratesTableOfContents.php
@@ -29,14 +29,28 @@ class GeneratesTableOfContents
 
     protected function parseHeadings(): array
     {
-        preg_match_all('/^#{2,4}\s+(.+)$/m', $this->markdown, $matches);
+        // Match both ATX-style (###) and Setext-style (===, ---) headers
+        $pattern = '/^(?:#{2,4}\s+(.+)|(.+)\n([=\-])\3+)$/m';
+        preg_match_all($pattern, $this->markdown, $matches);
 
         $headings = [];
         foreach ($matches[0] as $index => $heading) {
-            $level = substr_count($heading, '#');
-            $title = $matches[1][$index];
-            $slug = Str::slug($title);
+            // Handle ATX-style headers (###)
+            if (str_starts_with($heading, '#')) {
+                $level = substr_count($heading, '#');
+                $title = $matches[1][$index];
+            }
+            // Handle Setext-style headers (=== or ---)
+            else {
+                $title = trim($matches[2][$index]);
+                $level = $matches[3][$index] === '=' ? 1 : 2;
+                // Only add if the config level is met
+                if ($level < Config::getInt('docs.sidebar.table_of_contents.min_heading_level', 2)) {
+                    continue;
+                }
+            }
 
+            $slug = Str::slug($title);
             $headings[] = [
                 'level' => $level,
                 'title' => $title,
