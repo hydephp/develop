654dd4216a999cb4559ffed7ada51f33dd1bb00c
Caen De Silva <caen@desilva.se>
2024-12-01 15:29:33 +0100

    Cleanup formatting

--- packages/framework/resources/views/components/docs/table-of-contents.blade.php
+++ packages/framework/resources/views/components/docs/table-of-contents.blade.php
@@ -3,8 +3,7 @@
 <ul class="table-of-contents py-3 space-y-1.5">
     @foreach($items as $item)
         <li>
-            <a href="#{{ $item['slug'] }}" 
-               class="block pl-8 -ml-8 opacity-80 hover:opacity-100 hover:bg-gray-200/20 transition-all duration-300 relative">
+            <a href="#{{ $item['slug'] }}" class="block pl-8 -ml-8 opacity-80 hover:opacity-100 hover:bg-gray-200/20 transition-all duration-300 relative">
                 <span class="text-[75%] opacity-50 mr-1 hover:opacity-100 transition-opacity duration-300">#</span>
                 {{ $item['title'] }}
             </a>
