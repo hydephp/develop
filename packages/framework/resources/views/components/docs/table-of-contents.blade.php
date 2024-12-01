713cdc42693cd69ea18b5ce2f502d76c9b9282ff
Caen De Silva <caen@desilva.se>
2024-12-01 16:02:04 +0100

    Only render items when there are any

--- packages/framework/resources/views/components/docs/table-of-contents.blade.php
+++ packages/framework/resources/views/components/docs/table-of-contents.blade.php
@@ -1,16 +1,18 @@
 @props(['items', 'isChild' => false])
 
-<ul class="{{ ! $isChild ? 'table-of-contents py-3 space-y-1.5' : 'space-y-1.5' }}">
-    @foreach($items as $item)
-        <li>
-            <a href="#{{ $item['slug'] }}" class="block pl-8 -ml-8 opacity-80 hover:opacity-100 hover:bg-gray-200/20 transition-all duration-300 relative">
-                <span class="text-[75%] opacity-50 mr-1 hover:opacity-100 transition-opacity duration-300">#</span>
-                {{ $item['title'] }}
-            </a>
-            
-            @if(! empty($item['children']))
-                <x-hyde::docs.table-of-contents :items="$item['children']" :isChild="true" />
-            @endif
-        </li>
-    @endforeach
-</ul>
+@if(! empty($items))
+    <ul class="{{ ! $isChild ? 'table-of-contents py-3 space-y-1.5' : 'space-y-1.5' }}">
+        @foreach($items as $item)
+            <li>
+                <a href="#{{ $item['slug'] }}" class="block pl-8 -ml-8 opacity-80 hover:opacity-100 hover:bg-gray-200/20 transition-all duration-300 relative">
+                    <span class="text-[75%] opacity-50 mr-1 hover:opacity-100 transition-opacity duration-300">#</span>
+                    {{ $item['title'] }}
+                </a>
+
+                @if(! empty($item['children']))
+                    <x-hyde::docs.table-of-contents :items="$item['children']" :isChild="true" />
+                @endif
+            </li>
+        @endforeach
+    </ul>
+@endif
