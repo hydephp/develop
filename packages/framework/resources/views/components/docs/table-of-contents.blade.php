a58d8503360d533b037b25afc810a4f5821d6369
Caen De Silva <caen@desilva.se>
2024-12-01 13:49:33 +0100

    Create table-of-contents.blade.php

---
+++ packages/framework/resources/views/components/docs/table-of-contents.blade.php
@@ -0,0 +1,19 @@
+@props(['items'])
+
+<ul class="py-3 space-y-1.5">
+    @foreach($items as $item)
+        <li>
+            <a href="#{{ $item['slug'] }}" 
+               class="block pl-8 -ml-8 opacity-80 hover:opacity-100 hover:bg-gray-200/20 transition-all duration-300 relative">
+                <span class="text-[75%] opacity-50 mr-1 hover:opacity-100 transition-opacity duration-300">#</span>
+                {{ $item['title'] }}
+            </a>
+            
+            @if(!empty($item['children']))
+                <ul class="pl-2">
+                    <x-hyde::docs.table-of-contents :items="$item['children']" />
+                </ul>
+            @endif
+        </li>
+    @endforeach
+</ul>
