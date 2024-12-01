80ed9d440ed94a0361d0caa29520dc03d0f8bdc3
Caen De Silva <caen@desilva.se>
2024-12-01 21:59:29 +0100

    Create MarkdownHeadingRendererTest.php

---
+++ packages/framework/tests/Feature/MarkdownHeadingRendererTest.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hyde\Framework\Testing\Feature;
+
+use Hyde\Testing\TestCase;
+
+/**
+ * @covers \Hyde\Markdown\Processing\HeadingRenderer
+ *
+ * @see \Hyde\Framework\Testing\Unit\HeadingRendererUnitTest
+ */
+class MarkdownHeadingRendererTest extends TestCase
+{
+    //
+}
