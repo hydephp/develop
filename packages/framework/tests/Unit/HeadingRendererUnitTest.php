9311bd1971dba26b35a6e1426bd0d46867ee29d5
Caen De Silva <caen@desilva.se>
2024-12-01 21:59:07 +0100

    Create HeadingRendererUnitTest.php

---
+++ packages/framework/tests/Unit/HeadingRendererUnitTest.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hyde\Framework\Testing\Unit;
+
+use Hyde\Testing\UnitTestCase;
+
+/**
+ * @covers \Hyde\Markdown\Processing\HeadingRenderer
+ *
+ * @see \Hyde\Framework\Testing\Feature\MarkdownHeadingRendererTest
+ */
+class HeadingRendererUnitTest extends UnitTestCase
+{
+    //
+}
