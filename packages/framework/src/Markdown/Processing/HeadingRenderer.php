aff4388b7d3c786d3e2ec34742b035f1308ac188
Caen De Silva <caen@desilva.se>
2024-12-01 18:50:10 +0100

    Create HeadingRenderer.php

---
+++ packages/framework/src/Markdown/Processing/HeadingRenderer.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hyde\Markdown\Processing;
+
+use League\CommonMark\Extension\CommonMark\Node\Block\Heading;
+use League\CommonMark\Node\Node;
+use League\CommonMark\Renderer\ChildNodeRendererInterface;
+use League\CommonMark\Renderer\NodeRendererInterface;
+
+/**
+ * Renders a heading node, and supports built-in permalink generation.
+ *
+ * @see \League\CommonMark\Extension\CommonMark\Renderer\Block\HeadingRenderer
+ */
+class HeadingRenderer implements NodeRendererInterface
+{
+    public function render(Node $node, ChildNodeRendererInterface $childRenderer): string
+    {
+        if (! ($node instanceof Heading)) {
+            throw new \InvalidArgumentException('Incompatible node type: '.get_class($node));
+        }
+
+        $content = $childRenderer->renderNodes($node->children());
+
+        return view('hyde::components.markdown-heading', [
+            'level' => $node->getLevel(),
+            'slot' => $content,
+        ])->render();
+    }
+}
