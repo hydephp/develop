c78ddeb5918379c0efea06881313ffcf28bae32a
Caen De Silva <caen@desilva.se>
2024-12-05 11:13:18 +0100

    Inline local variable override

--- packages/framework/resources/views/components/markdown-heading.blade.php
+++ packages/framework/resources/views/components/markdown-heading.blade.php
@@ -8,14 +8,12 @@
 @php
     $tag = 'h' . $level;
     $id = $id ?? \Illuminate\Support\Str::slug($slot);
-
-    $extraAttributes = array_merge($extraAttributes, [
-        'id' => $addPermalink ? $id : ($extraAttributes['id'] ?? null),
-        'class' => trim(($extraAttributes['class'] ?? '') . ($addPermalink ? ' group w-fit scroll-mt-2' : '')),
-    ]);
 @endphp
 
-<{{ $tag }} {{ $attributes->merge($extraAttributes) }}>
+<{{ $tag }} {{ $attributes->merge(array_merge($extraAttributes, [
+        'id' => $addPermalink ? $id : ($extraAttributes['id'] ?? null),
+        'class' => trim(($extraAttributes['class'] ?? '') . ($addPermalink ? ' group w-fit scroll-mt-2' : '')),
+    ])) }}>
     {!! $slot !!}
     @if($addPermalink === true)
         <a href="#{{ $id }}" class="heading-permalink opacity-0 ml-1 transition-opacity duration-300 ease-linear px-1 group-hover:opacity-100 focus:opacity-100 group-hover:grayscale-0 focus:grayscale-0" title="Permalink">
