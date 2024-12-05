3feddd34fee93f7536b31aed325f18b3b8a36dea
Caen De Silva <caen@desilva.se>
2024-12-05 11:13:50 +0100

    Revert "Inline local variable override"
    This reverts commit d4336e4210c0dbd4422b7bf1cc0cdb2e07c38e85.

--- packages/framework/resources/views/components/markdown-heading.blade.php
+++ packages/framework/resources/views/components/markdown-heading.blade.php
@@ -8,12 +8,14 @@
 @php
     $tag = 'h' . $level;
     $id = $id ?? \Illuminate\Support\Str::slug($slot);
-@endphp
 
-<{{ $tag }} {{ $attributes->merge(array_merge($extraAttributes, [
+    $extraAttributes = array_merge($extraAttributes, [
         'id' => $addPermalink ? $id : ($extraAttributes['id'] ?? null),
         'class' => trim(($extraAttributes['class'] ?? '') . ($addPermalink ? ' group w-fit scroll-mt-2' : '')),
-    ])) }}>
+    ]);
+@endphp
+
+<{{ $tag }} {{ $attributes->merge($extraAttributes) }}>
     {!! $slot !!}
     @if($addPermalink === true)
         <a href="#{{ $id }}" class="heading-permalink opacity-0 ml-1 transition-opacity duration-300 ease-linear px-1 group-hover:opacity-100 focus:opacity-100 group-hover:grayscale-0 focus:grayscale-0" title="Permalink">
