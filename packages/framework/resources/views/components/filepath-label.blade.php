7d0d58b1691fc8274c174298e82497bee058caba
Caen De Silva <caen@desilva.se>
2024-12-08 20:48:53 +0100

    Merge class directive rows into ternary expression

--- packages/framework/resources/views/components/filepath-label.blade.php
+++ packages/framework/resources/views/components/filepath-label.blade.php
@@ -1,6 +1,5 @@
 @props(['path', 'highlightedByTorchlight' => false])
 <small @class([
     'relative float-right opacity-50 hover:opacity-100 transition-opacity duration-250 not-prose hidden md:block',
-    '-top-1 right-1' => $highlightedByTorchlight,
-    'top-0 right-0' => ! $highlightedByTorchlight,
+    $highlightedByTorchlight ? '-top-1 right-1' : 'top-0 right-0',
 ])><span class="sr-only">Filepath: </span>{{ $path }}</small>
