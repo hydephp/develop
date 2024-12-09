ddb899e194d77c49d5e6ec2ad47da015cb744ad0
Caen De Silva <caen@desilva.se>
2024-12-09 10:41:23 +0100

    Debug workflow issue

--- packages/framework/resources/views/components/docs/table-of-contents.blade.php
+++ packages/framework/resources/views/components/docs/table-of-contents.blade.php
@@ -3,6 +3,9 @@
 @if(! empty($items))
     <ul @class([$isChild ? 'pl-2' : 'table-of-contents pb-3'])>
         @foreach($items as $item)
+            @if(! isset($item['identifier']))
+                @dd($item);
+            @endif
             <li class="my-0.5">
                 <a href="#{{ $item['identifier'] }}" class="-ml-8 pl-8 opacity-80 hover:opacity-100 hover:bg-gray-200/20 transition-all duration-300">
                     <span class="text-[75%] opacity-50 hover:opacity-100 transition-opacity duration-300">#</span>
