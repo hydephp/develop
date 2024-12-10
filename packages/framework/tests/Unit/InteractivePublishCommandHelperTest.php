1b93233d9ed3540d8ea58c1d38ac94c694749bbb
Caen De Silva <caen@desilva.se>
2024-12-10 17:37:48 +0100

    Create InteractivePublishCommandHelperTest.php

---
+++ packages/framework/tests/Unit/InteractivePublishCommandHelperTest.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hyde\Framework\Testing\Unit;
+
+use Hyde\Testing\UnitTestCase;
+
+/**
+ * @covers \Hyde\Console\Helpers\InteractivePublishCommandHelper
+ *
+ * @see \Hyde\Framework\Testing\Feature\Commands\PublishViewsCommandTest
+ */
+class InteractivePublishCommandHelperTest extends UnitTestCase
+{
+    //
+}
