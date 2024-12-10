6cb8db3ca444e8507afd37956340e248c7169658
Caen De Silva <caen@desilva.se>
2024-12-10 17:46:32 +0100

    Mark helper as internal

--- packages/framework/src/Console/Helpers/InteractivePublishCommandHelper.php
+++ packages/framework/src/Console/Helpers/InteractivePublishCommandHelper.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace Hyde\Console\Helpers;
 
+/**
+ * @internal This class offloads logic from the PublishViewsCommand class and should not be used elsewhere.
+ */
 class InteractivePublishCommandHelper
 {
     //
