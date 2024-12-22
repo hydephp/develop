2c221b11b8db550abf6bac1341d3c215fed69382
Caen De Silva <caen@desilva.se>
2024-12-22 15:38:11 +0100

    Code coverage ignore helper

--- packages/framework/src/Console/Helpers/ConsoleHelper.php
+++ packages/framework/src/Console/Helpers/ConsoleHelper.php
@@ -6,6 +6,8 @@ namespace Hyde\Console\Helpers;
 
 /**
  * @internal This class contains internal helpers for interacting with the console, and for easier testing.
+ *
+ * @codeCoverageIgnore This class provides internal testing helpers and does not need to be tested.
  */
 class ConsoleHelper
 {
