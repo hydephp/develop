6fa6b357dffe06432d44ceb736d2d7297f9ff088
Caen De Silva <caen@desilva.se>
2024-12-22 19:16:51 +0100

    Implement the unit test

--- packages/framework/tests/Unit/InteractivePublishCommandHelperTest.php
+++ packages/framework/tests/Unit/InteractivePublishCommandHelperTest.php
@@ -4,12 +4,16 @@ declare(strict_types=1);
 
 namespace Hyde\Framework\Testing\Unit;
 
+use Hyde\Console\Helpers\InteractivePublishCommandHelper;
 use Hyde\Foundation\Providers\ViewServiceProvider;
 use Hyde\Hyde;
 use Hyde\Testing\UnitTestCase;
 use Illuminate\Container\Container;
 use Illuminate\Support\Facades\Blade;
+use Illuminate\Support\Facades\Facade;
+use Illuminate\Support\Facades\File;
 use Mockery;
+use Symfony\Component\Finder\SplFileInfo;
 
 /**
  * @covers \Hyde\Console\Helpers\InteractivePublishCommandHelper
@@ -29,6 +33,7 @@ class InteractivePublishCommandHelperTest extends UnitTestCase
     {
         $this->filesystem = $this->mockFilesystemStrict();
 
+        File::swap($this->filesystem);
         Blade::partialMock()->shouldReceive('component');
 
         $app = $this->setupMockApplication();
@@ -41,6 +46,57 @@ class InteractivePublishCommandHelperTest extends UnitTestCase
         $this->verifyMockeryExpectations();
 
         Container::setInstance($this->originalApp);
+        Facade::clearResolvedInstances();
+    }
+
+    public function testGetFileChoices()
+    {
+        $this->filesystem->shouldReceive('allFiles')->andReturn([
+            new SplFileInfo(Hyde::path('packages/framework/resources/views/layouts/app.blade.php'), '', 'app.blade.php'),
+            new SplFileInfo(Hyde::path('packages/framework/resources/views/layouts/page.blade.php'), '', 'page.blade.php'),
+            new SplFileInfo(Hyde::path('packages/framework/resources/views/layouts/post.blade.php'), '', 'post.blade.php'),
+        ]);
+
+        $helper = new InteractivePublishCommandHelper('hyde-layouts');
+
+        $this->assertSame([
+            'resources/views/vendor/hyde/layouts/app.blade.php' => 'app.blade.php',
+            'resources/views/vendor/hyde/layouts/page.blade.php' => 'page.blade.php',
+            'resources/views/vendor/hyde/layouts/post.blade.php' => 'post.blade.php',
+        ], $helper->getFileChoices());
+    }
+
+    public function testHandle()
+    {
+        $this->filesystem->shouldReceive('allFiles')->andReturn([
+            new SplFileInfo(Hyde::path('packages/framework/resources/views/layouts/app.blade.php'), '', 'app.blade.php'),
+            new SplFileInfo(Hyde::path('packages/framework/resources/views/layouts/page.blade.php'), '', 'page.blade.php'),
+            new SplFileInfo(Hyde::path('packages/framework/resources/views/layouts/post.blade.php'), '', 'post.blade.php'),
+        ]);
+
+        $helper = new InteractivePublishCommandHelper('hyde-layouts');
+
+        $this->filesystem->shouldReceive('ensureDirectoryExists')->twice();
+        $this->filesystem->shouldReceive('copy')->twice();
+
+        $helper->handle([
+            "resources/views/vendor/hyde/layouts/app.blade.php",
+            "resources/views/vendor/hyde/layouts/page.blade.php",
+        ]);
+
+        $this->filesystem->shouldHaveReceived('ensureDirectoryExists')
+            ->with(Hyde::path('resources/views/vendor/hyde/layouts'))
+            ->twice();
+
+        $this->filesystem->shouldHaveReceived('copy')->with(
+            Hyde::path('packages/framework/resources/views/layouts/app.blade.php'),
+            Hyde::path('resources/views/vendor/hyde/layouts/app.blade.php')
+        )->once();
+
+        $this->filesystem->shouldHaveReceived('copy')->with(
+            Hyde::path('packages/framework/resources/views/layouts/page.blade.php'),
+            Hyde::path('resources/views/vendor/hyde/layouts/page.blade.php')
+        )->once();
     }
 
     protected function setupMockApplication(): Container
@@ -48,7 +104,7 @@ class InteractivePublishCommandHelperTest extends UnitTestCase
         $this->originalApp = Container::getInstance();
 
         $app = Mockery::mock(app())->makePartial();
-        $app->shouldReceive('resourcePath')->andReturn(Hyde::path('resources'));
+        $app->shouldReceive('resourcePath')->andReturnUsing(fn (string $path) => "resources/$path");
 
         Container::setInstance($app);
 
