802a9abfc7d8497e1b952d27bcfa8560e1adbe58
Caen De Silva <caen@desilva.se>
2024-12-22 17:38:03 +0100

    Mock the application

--- packages/framework/tests/Unit/InteractivePublishCommandHelperTest.php
+++ packages/framework/tests/Unit/InteractivePublishCommandHelperTest.php
@@ -4,7 +4,12 @@ declare(strict_types=1);
 
 namespace Hyde\Framework\Testing\Unit;
 
+use Hyde\Foundation\Providers\ViewServiceProvider;
+use Hyde\Hyde;
 use Hyde\Testing\UnitTestCase;
+use Illuminate\Container\Container;
+use Illuminate\Support\Facades\Blade;
+use Mockery;
 
 /**
  * @covers \Hyde\Console\Helpers\InteractivePublishCommandHelper
@@ -13,16 +18,40 @@ use Hyde\Testing\UnitTestCase;
  */
 class InteractivePublishCommandHelperTest extends UnitTestCase
 {
+    protected static bool $needsKernel = true;
+
     /** @var \Illuminate\Filesystem\Filesystem&\Mockery\MockInterface */
     protected $filesystem;
+
+    protected $originalApp;
 
     protected function setUp(): void
     {
         $this->filesystem = $this->mockFilesystemStrict();
+
+        Blade::partialMock()->shouldReceive('component');
+
+        $app = $this->setupMockApplication();
+
+        (new ViewServiceProvider($app))->boot();
     }
 
     protected function tearDown(): void
     {
         $this->verifyMockeryExpectations();
+
+        Container::setInstance($this->originalApp);
+    }
+
+    protected function setupMockApplication(): Container
+    {
+        $this->originalApp = Container::getInstance();
+
+        $app = Mockery::mock(app())->makePartial();
+        $app->shouldReceive('resourcePath')->andReturn(Hyde::path('resources'));
+
+        Container::setInstance($app);
+
+        return $app;
     }
 }
