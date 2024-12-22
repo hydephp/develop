4e59a21ed7bcab7ea5602c55d8bb499836469ca4
Caen De Silva <caen@desilva.se>
2024-12-22 15:37:47 +0100

    Mark helper as internal

--- packages/framework/src/Console/Helpers/ConsoleHelper.php
+++ packages/framework/src/Console/Helpers/ConsoleHelper.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace Hyde\Console\Helpers;
 
+/**
+ * @internal This class contains internal helpers for interacting with the console, and for easier testing.
+ */
 class ConsoleHelper
 {
     //
