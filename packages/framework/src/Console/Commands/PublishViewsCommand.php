881960a9f788cf314c3a9c6dba8e1ec4ff75f082
Caen De Silva <caen@desilva.se>
2024-12-25 15:35:26 +0100

    Add spacing

--- packages/framework/src/Console/Commands/PublishViewsCommand.php
+++ packages/framework/src/Console/Commands/PublishViewsCommand.php
@@ -89,6 +89,7 @@ class PublishViewsCommand extends Command
     protected function formatPublishableChoices(): array
     {
         $keys = ['Publish all categories listed below'];
+
         foreach ($this->options as $key => $option) {
             $keys[] = "<comment>$key</comment>: $option->description";
         }
