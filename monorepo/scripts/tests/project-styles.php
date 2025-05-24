d4439514792cf328b152f75ff411cd72e68c3f46
Caen De Silva <caen@desilva.se>
2025-05-24 16:39:13 +0200

    Fix test

--- monorepo/scripts/tests/project-styles.php
+++ monorepo/scripts/tests/project-styles.php
@@ -17,6 +17,9 @@ test('can install node dependencies', function () {
 });
 
 test('can build assets using vite', function () {
+    // Add code to the JavaScript file so it compiles.
+    file_put_contents(BASE_PATH.'/resources/assets/app.js', 'console.log("Hello, world!");');
+
     $output = shell_exec('cd '.BASE_PATH.' && npm run build');
 
     $this->assert(file_exists(BASE_PATH.'/_media/app.css'), 'CSS file does not exist');
