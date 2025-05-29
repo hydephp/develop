b233069843f277456998cddb139b11ba07af120c
laryhills <hilaryokoh@gmail.com>
2025-05-29 15:38:38 +0100

    Add x-cloak directive to search modal and sidebar components

--- packages/framework/resources/views/layouts/docs.blade.php
+++ packages/framework/resources/views/layouts/docs.blade.php
@@ -1,23 +1,28 @@
 <!DOCTYPE html>
 <html lang="{{ config('hyde.language', 'en') }}">
+
 <head>
     @include('hyde::layouts.head')
 </head>
-<body id="hyde-docs" class="bg-white dark:bg-gray-900 dark:text-white min-h-screen w-screen relative overflow-x-hidden overflow-y-auto"
-      x-data="{ sidebarOpen: false, searchWindowOpen: false }" x-on:keydown.escape="searchWindowOpen = false; sidebarOpen = false" x-on:keydown.slash="searchWindowOpen = true">
+
+<body id="hyde-docs"
+    class="relative w-screen min-h-screen overflow-x-hidden overflow-y-auto bg-white dark:bg-gray-900 dark:text-white"
+    x-data="{ sidebarOpen: false, searchWindowOpen: false }" x-cloak x-on:keydown.escape="searchWindowOpen = false; sidebarOpen = false"
+    x-on:keydown.slash="searchWindowOpen = true">
 
     @include('hyde::components.skip-to-content-button')
     @include('hyde::components.docs.mobile-navigation')
     @include('hyde::components.docs.sidebar')
 
-    <main id="content" class="dark:bg-gray-900 min-h-screen bg-gray-50 md:bg-white absolute top-16 md:top-0 w-screen md:left-64 md:w-[calc(100vw_-_16rem)] print:top-0">
+    <main id="content"
+        class="dark:bg-gray-900 min-h-screen bg-gray-50 md:bg-white absolute top-16 md:top-0 w-screen md:left-64 md:w-[calc(100vw_-_16rem)] print:top-0">
         @include('hyde::components.docs.documentation-article')
     </main>
 
     <div id="support">
         @include('hyde::components.docs.sidebar-backdrop')
 
-        @if(Hyde\Facades\Features::hasDocumentationSearch())
+        @if (Hyde\Facades\Features::hasDocumentationSearch())
             @include('hyde::components.docs.search-widget')
             @include('hyde::components.docs.search-scripts')
         @endif
@@ -25,4 +30,5 @@
 
     @include('hyde::layouts.scripts')
 </body>
+
 </html>
