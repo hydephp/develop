c042e8c96a80af375199846c1b9cefc5a8e70245
Caen De Silva <caen@desilva.se>
2025-08-17 09:37:31 +0200

    Revert "Fix so test works with Pest 3"
    This reverts commit b052e2cc525474c999abe6b4aa1415db3f1ff963.

--- packages/realtime-compiler/tests/RealtimeCompilerTest.php
+++ packages/realtime-compiler/tests/RealtimeCompilerTest.php
@@ -1,77 +1,219 @@
 <?php
 
-use Hyde\RealtimeCompiler\RealtimeCompiler;
+declare(strict_types=1);
+
+use Hyde\Testing\UnitTestCase;
+use Desilva\Microserve\JsonResponse;
 use Desilva\Microserve\Request;
 use Desilva\Microserve\Response;
+use Hyde\Facades\Filesystem;
+use Hyde\Framework\Exceptions\RouteNotFoundException;
+use Hyde\RealtimeCompiler\Http\ExceptionHandler;
+use Desilva\Microserve\HtmlResponse;
+use Hyde\RealtimeCompiler\Http\HttpKernel;
 
-uses(\Hyde\Testing\UnitTestCase::class);
+class RealtimeCompilerTest extends UnitTestCase
+{
+    public static function setUpBeforeClass(): void
+    {
+        putenv('SERVER_LIVE_EDIT=false');
 
-test('can instantiate RealtimeCompiler', function () {
-    $compiler = new RealtimeCompiler();
+        define('BASE_PATH', realpath(__DIR__.'/../../../'));
 
-    expect($compiler)->toBeInstanceOf(RealtimeCompiler::class);
-});
+        if (BASE_PATH === false || ! file_exists(BASE_PATH.'/hyde')) {
+            throw new InvalidArgumentException('This test suite must be run from the root of the hydephp/develop monorepo.');
+        }
+    }
 
-test('can register virtual route', function () {
-    $compiler = new RealtimeCompiler();
+    protected function setUp(): void
+    {
+        parent::setUp();
+        ob_start();
+    }
 
-    $route = function (Request $request): Response {
-        return new Response('test', 200);
-    };
+    protected function tearDown(): void
+    {
+        parent::tearDown();
+        ob_end_clean();
+    }
 
-    $compiler->registerVirtualRoute('/test', $route);
+    public function testHandlesRoutesIndexPage()
+    {
+        putenv('SERVER_DASHBOARD=false');
+        $this->mockRoute('');
 
-    $routes = $compiler->getVirtualRoutes();
-    expect($routes)->toHaveKey('/test');
-    expect($routes['/test'])->toBe($route);
-});
+        $kernel = new HttpKernel();
+        $response = $kernel->handle(new Request());
 
-test('can register multiple virtual routes', function () {
-    $compiler = new RealtimeCompiler();
+        $this->assertInstanceOf(Response::class, $response);
+        $this->assertEquals(200, $response->statusCode);
+        $this->assertEquals('OK', $response->statusMessage);
+        $this->assertStringContainsString('<title>Welcome to HydePHP!</title>', $response->body);
+    }
 
-    $route1 = function (Request $request): Response {
-        return new Response('test1', 200);
-    };
+    public function testHandlesRoutesCustomPages()
+    {
+        $this->mockRoute('foo');
 
-    $route2 = function (Request $request): Response {
-        return new Response('test2', 200);
-    };
+        Filesystem::put('_pages/foo.md', '# Hello World!');
 
-    $compiler->registerVirtualRoute('/test1', $route1);
-    $compiler->registerVirtualRoute('/test2', $route2);
+        $kernel = new HttpKernel();
+        $response = $kernel->handle(new Request());
 
-    $routes = $compiler->getVirtualRoutes();
-    expect($routes)->toHaveKey('/test1');
-    expect($routes)->toHaveKey('/test2');
-    expect($routes['/test1'])->toBe($route1);
-    expect($routes['/test2'])->toBe($route2);
-});
+        $this->assertInstanceOf(Response::class, $response);
+        $this->assertEquals(200, $response->statusCode);
+        $this->assertEquals('OK', $response->statusMessage);
+        $this->assertStringContainsString('<h1>Hello World!</h1>', $response->body);
 
-test('getVirtualRoutes returns empty array initially', function () {
-    $compiler = new RealtimeCompiler();
+        Filesystem::unlink('_pages/foo.md');
+    }
 
-    expect($compiler->getVirtualRoutes())->toBe([]);
-});
+    public function testHandlesRoutesPagesWithHtmlExtension()
+    {
+        Filesystem::put('_pages/foo.md', '# Hello World!');
 
-test('virtual routes can be called', function () {
-    $compiler = new RealtimeCompiler();
+        $kernel = new HttpKernel();
+        $response = $kernel->handle(new Request());
 
-    $route = function (Request $request): Response {
-        return new Response(200, 'OK', ['body' => 'Hello World']);
-    };
+        $this->assertInstanceOf(Response::class, $response);
+        $this->assertEquals(200, $response->statusCode);
+        $this->assertEquals('OK', $response->statusMessage);
+        $this->assertStringContainsString('<h1>Hello World!</h1>', $response->body);
 
-    $compiler->registerVirtualRoute('/hello', $route);
+        Filesystem::unlink('_pages/foo.md');
+    }
 
-    $routes = $compiler->getVirtualRoutes();
+    public function testHandlesRoutesStaticAssets()
+    {
+        $this->mockRoute('media/test.css');
+        Filesystem::put('_media/test.css', 'test');
 
-    // Mock $_SERVER variables for Request construction
-    $_SERVER['REQUEST_METHOD'] = 'GET';
-    $_SERVER['REQUEST_URI'] = '/hello';
+        $kernel = new HttpKernel();
+        $response = $kernel->handle(new Request());
 
-    $request = new Request();
-    $response = $routes['/hello']($request);
+        $this->assertInstanceOf(Response::class, $response);
+        $this->assertEquals(200, $response->statusCode);
+        $this->assertEquals('OK', $response->statusMessage);
+        $this->assertEquals('test', $response->body);
 
-    expect($response)->toBeInstanceOf(Response::class);
-    expect($response->body)->toBe('Hello World');
-    expect($response->statusCode)->toBe(200);
-});
+        Filesystem::unlink('_media/test.css');
+    }
+
+    public function testNormalizesMediaPath()
+    {
+        $this->mockRoute('media/test.css');
+        Filesystem::put('_media/test.css', 'test');
+
+        $kernel = new HttpKernel();
+        $response = $kernel->handle(new Request());
+
+        $this->assertInstanceOf(Response::class, $response);
+        $this->assertEquals(200, $response->statusCode);
+        $this->assertEquals('OK', $response->statusMessage);
+        $this->assertEquals('test', $response->body);
+
+        Filesystem::unlink('_media/test.css');
+    }
+
+    public function testThrowsRouteNotFoundExceptionForMissingRoute()
+    {
+        $this->mockRoute('missing');
+
+        $kernel = new HttpKernel();
+
+        $this->expectException(RouteNotFoundException::class);
+        $this->expectExceptionMessage('Route [missing] not found');
+
+        $kernel->handle(new Request());
+    }
+
+    public function testSends404ErrorResponseForMissingAsset()
+    {
+        $this->mockRoute('missing.css');
+
+        $kernel = new HttpKernel();
+        $response = $kernel->handle(new Request());
+
+        $this->assertInstanceOf(Response::class, $response);
+        $this->assertEquals(404, $response->statusCode);
+        $this->assertEquals('Not Found', $response->statusMessage);
+    }
+
+    public function testTrailingSlashesAreNormalizedFromRoute()
+    {
+        $this->mockRoute('foo/');
+
+        Filesystem::put('_pages/foo.md', '# Hello World!');
+
+        $kernel = new HttpKernel();
+        $response = $kernel->handle(new Request());
+
+        $this->assertInstanceOf(Response::class, $response);
+        $this->assertEquals(200, $response->statusCode);
+        $this->assertEquals('OK', $response->statusMessage);
+        $this->assertStringContainsString('<h1>Hello World!</h1>', $response->body);
+
+        Filesystem::unlink('_pages/foo.md');
+    }
+
+    public function testDocsUriPathIsReroutedToDocsIndex()
+    {
+        $this->mockRoute('docs');
+
+        Filesystem::put('_docs/index.md', '# Hello World!');
+
+        $kernel = new HttpKernel();
+        $response = $kernel->handle(new Request());
+
+        $this->assertInstanceOf(Response::class, $response);
+        $this->assertEquals(200, $response->statusCode);
+        $this->assertEquals('OK', $response->statusMessage);
+        $this->assertStringContainsString('HydePHP Docs', $response->body);
+
+        Filesystem::unlink('_docs/index.md');
+    }
+
+    public function testDocsSearchRendersSearchPage()
+    {
+        $this->mockRoute('docs/search');
+        Filesystem::put('_docs/index.md', '# Hello World!');
+
+        $kernel = new HttpKernel();
+        $response = $kernel->handle(new Request());
+
+        $this->assertInstanceOf(HtmlResponse::class, $response);
+        $this->assertEquals(200, $response->statusCode);
+        $this->assertEquals('OK', $response->statusMessage);
+        $this->assertStringContainsString('Search the documentation site', $response->body);
+
+        Filesystem::unlink('_docs/index.md');
+    }
+
+    public function testPingRouteReturnsPingResponse()
+    {
+        $this->mockRoute('ping');
+
+        $kernel = new HttpKernel();
+        $response = $kernel->handle(new Request());
+
+        $this->assertInstanceOf(JsonResponse::class, $response);
+        $this->assertEquals(200, $response->statusCode);
+        $this->assertEquals('OK', $response->statusMessage);
+    }
+
+    public function testExceptionHandling()
+    {
+        $exception = new Exception('foo');
+        $response = ExceptionHandler::handle($exception);
+
+        $this->assertInstanceOf(Response::class, $response);
+        $this->assertEquals(500, $response->statusCode);
+        $this->assertEquals('Internal Server Error', $response->statusMessage);
+    }
+
+    protected function mockRoute(string $route, $method = 'GET'): void
+    {
+        $_SERVER['REQUEST_METHOD'] = $method;
+        $_SERVER['REQUEST_URI'] = "/$route";
+    }
+}
