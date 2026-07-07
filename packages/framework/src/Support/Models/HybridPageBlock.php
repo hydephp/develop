6f76a8e0d8c3bdfeef3e613770b5af0dbd03b551
Emma De Silva <emma@desilva.se>
2026-07-08 01:04:22 +0200

    Define properties

--- packages/framework/src/Support/Models/HybridPageBlock.php
+++ packages/framework/src/Support/Models/HybridPageBlock.php
@@ -2,7 +2,11 @@
 
 namespace Hyde\Support\Models;
 
+use Hyde\Enums\HybridPageBlockType;
+use Hyde\Pages\HybridPage;
+
 class HybridPageBlock
 {
-    //
+    protected HybridPage $page;
+    protected HybridPageBlockType $type;
 }
