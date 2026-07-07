1754b32c471a7180190a143c08163ed290ef60de
Emma De Silva <emma@desilva.se>
2026-07-07 23:17:48 +0200

    Copy over the compile method stub

--- packages/framework/src/Pages/HybridPage.php
+++ packages/framework/src/Pages/HybridPage.php
@@ -2,7 +2,17 @@
 
 namespace Hyde\Pages;
 
+use Illuminate\Support\Facades\View;
+
 class HybridPage extends MarkdownPage
 {
     public static string $fileExtension = '.hmd';
+
+    public function compile(): string
+    {
+        return View::make($this->getBladeView())->with([
+            'title' => $this->title,
+            'content' => $this->markdown->toHtml(static::class),
+        ])->render();
+    }
 }
