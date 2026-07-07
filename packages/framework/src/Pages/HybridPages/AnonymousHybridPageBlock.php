908a03ed6cddf139dad98b22ca9dd1da5bd4aed6
Emma De Silva <emma@desilva.se>
2026-07-08 01:13:30 +0200

    Scaffold child classes

---
+++ packages/framework/src/Pages/HybridPages/AnonymousHybridPageBlock.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hyde\Pages\HybridPages;
+
+class AnonymousHybridPageBlock extends HybridPageBlock
+{
+    public function render()
+    {
+        // TODO: Implement render() method.
+    }
+}
---
+++ packages/framework/src/Pages/HybridPages/ComponentHybridPageBlock.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hyde\Pages\HybridPages;
+
+class ComponentHybridPageBlock extends HybridPageBlock
+{
+    public function render()
+    {
+        // TODO: Implement render() method.
+    }
+}
