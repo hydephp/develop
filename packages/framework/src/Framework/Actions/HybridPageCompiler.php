935355f458b763913f1f8bdf0cff9e25a7d28b6b
Emma De Silva <emma@desilva.se>
2026-07-07 23:21:23 +0200

    Spec out contract

--- packages/framework/src/Framework/Actions/HybridPageCompiler.php
+++ packages/framework/src/Framework/Actions/HybridPageCompiler.php
@@ -2,7 +2,12 @@
 
 namespace Hyde\Framework\Actions;
 
+use Hyde\Pages\HybridPage;
+
 class HybridPageCompiler
 {
-    //
+    public function handle(HybridPage $page): string
+    {
+        //
+    }
 }
