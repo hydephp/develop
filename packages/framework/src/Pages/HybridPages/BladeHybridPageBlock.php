6b6051963b621c217c1dfa7cd72db8b5f0c286d5
Emma De Silva <emma@desilva.se>
2026-07-08 02:33:47 +0200

    Rename the class

--- packages/framework/src/Pages/HybridPages/BladeHybridPageBlock.php
+++ packages/framework/src/Pages/HybridPages/BladeHybridPageBlock.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Hyde\Pages\HybridPages;
 
-class AnonymousHybridPageBlock extends HybridPageBlock
+class BladeHybridPageBlock extends HybridPageBlock
 {
     public function render()
     {
