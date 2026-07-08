5439af7ed3ee5809e88df2380840020ad6777242
Emma De Silva <emma@desilva.se>
2026-07-08 02:58:15 +0200

    Return the rendered content

--- packages/framework/src/Pages/HybridPages/BladePageBlock.php
+++ packages/framework/src/Pages/HybridPages/BladePageBlock.php
@@ -4,10 +4,12 @@ declare(strict_types=1);
 
 namespace Hyde\Pages\HybridPages;
 
+use Illuminate\Support\Facades\Blade;
+
 class BladePageBlock extends HybridPageBlock
 {
     public function render(): string
     {
-        return $this->content;
+        return Blade::render($this->content);
     }
 }
