8c7e9b126068719e2b52b925c55818856a5ab004
Emma De Silva <emma@desilva.se>
2026-07-12 03:45:30 +0200

    Create TranslationServiceProvider.php

---
+++ packages/framework/src/Foundation/Providers/TranslationServiceProvider.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hyde\Foundation\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+/**
+ * Register the Hyde translation services.
+ */
+class TranslationServiceProvider extends ServiceProvider
+{
+    public function register(): void
+    {
+        //
+    }
+
+    public function boot(): void
+    {
+        //
+    }
+}
