895ed5d82852b7a9fb67f6156e91b138f47a7f80
Emma De Silva <emma@desilva.se>
2026-07-15 00:51:37 +0200

    Replace InMemoryPage macros with lazy contents closures

--- packages/framework/src/Pages/InMemoryPage.php
+++ packages/framework/src/Pages/InMemoryPage.php
@@ -4,14 +4,12 @@ declare(strict_types=1);
 
 namespace Hyde\Pages;
 
-use BadMethodCallException;
 use Closure;
 use Hyde\Framework\Actions\AnonymousViewCompiler;
 use Hyde\Markdown\Models\FrontMatter;
 use Hyde\Pages\Concerns\HydePage;
 use Illuminate\Support\Facades\View;
-
-use function sprintf;
+use ReflectionFunction;
 
 /**
  * Extendable class for in-memory (or virtual) Hyde pages that are not based on any source files.
@@ -20,8 +18,11 @@ use function sprintf;
  * usually within the boot method of the package's service provider, or a page collection callback in an extension.
  * This is because these pages cannot be discovered by the auto discovery process as there's no source files to parse.
  *
- * This class is especially useful for one-off custom pages. But if your usage grows, or if you want to utilize
- * Hyde autodiscovery, you may benefit from creating a custom page class instead, as that will give you full control.
+ * Pages may use literal string contents, a lazy closure, or a Blade view. Configured contents take precedence over
+ * a view. Non-static closures are bound to the page instance and may use $this, while static closures run unbound.
+ *
+ * This class is especially useful for one-off custom pages. But if your usage grows, or if you want to utilize Hyde
+ * autodiscovery or control compilation completely, create a custom page class and override compile() instead.
  */
 class InMemoryPage extends HydePage
 {
@@ -29,16 +30,13 @@ class InMemoryPage extends HydePage
     public static string $outputDirectory;
     public static string $fileExtension;
 
-    protected string $contents;
+    protected string|Closure $contents;
     protected string $view;
-
-    /** @var array<string, callable> */
-    protected array $macros = [];
 
     /**
      * Static alias for the constructor.
      */
-    public static function make(string $identifier = '', FrontMatter|array $matter = [], string $contents = '', string $view = ''): static
+    public static function make(string $identifier = '', FrontMatter|array $matter = [], string|Closure $contents = '', string $view = ''): static
     {
         return new static($identifier, $matter, $contents, $view);
     }
@@ -46,12 +44,12 @@ class InMemoryPage extends HydePage
     /**
      * Create a new in-memory/virtual page instance.
      *
-     * The in-memory page class offers two content options. You can either pass a string to the $contents parameter,
-     * Hyde will then save that literally as the page's contents. Alternatively, you can pass a view name to the $view parameter,
-     * and Hyde will use that view to render the page contents with the supplied front matter during the static site build process.
+     * The in-memory page class offers three content strategies. You can pass a literal string or a lazy closure to
+     * the $contents parameter, or pass a view name or Blade file to the $view parameter. Closures are resolved each
+     * time the contents are requested. Non-static closures are bound to this page and may use $this. Static closures
+     * are supported but run unbound and cannot use $this.
      *
-     * Note that $contents take precedence over $view, so if you pass both, only $contents will be used.
-     * You can also register a macro with the name 'compile' to overload the default compile method.
+     * Configured contents take precedence over a view, including closures that return an empty string.
      *
      * @param  string  $identifier  The identifier of the page. This is used to generate the route key which is used to create the output filename.
      *                              If the identifier for an in-memory page is "foo/bar" the page will be saved to "_site/foo/bar.html".
@@ -59,10 +57,10 @@ class InMemoryPage extends HydePage
      *                              Take note that the identifier must be unique to prevent overwriting other pages.
      * @param  \Hyde\Markdown\Models\FrontMatter|array  $matter  The front matter of the page. When using the Blade view rendering option,
      *                                                           all this data will be passed to the view rendering engine.
-     * @param  string  $contents  The contents of the page. This will be saved as-is to the output file.
+     * @param  string|\Closure  $contents  Literal page contents or a closure that lazily generates them.
      * @param  string  $view  The view key or Blade file for the view to use to render the page contents.
      */
-    public function __construct(string $identifier = '', FrontMatter|array $matter = [], string $contents = '', string $view = '')
+    public function __construct(string $identifier = '', FrontMatter|array $matter = [], string|Closure $contents = '', string $view = '')
     {
         parent::__construct($identifier, $matter);
 
@@ -70,10 +68,20 @@ class InMemoryPage extends HydePage
         $this->view = $view;
     }
 
-    /** Get the contents of the page. This will be saved as-is to the output file when this strategy is used. */
+    /** Get the literal contents or lazily resolve the configured content closure. */
     public function getContents(): string
     {
-        return $this->contents;
+        if (! $this->contents instanceof Closure) {
+            return $this->contents;
+        }
+
+        $contents = $this->contents;
+
+        if (! (new ReflectionFunction($contents))->isStatic()) {
+            $contents = $contents->bindTo($this, static::class);
+        }
+
+        return $contents();
     }
 
     /** Get the view key or Blade file for the view to use to render the page contents when this strategy is used. */
@@ -85,17 +93,16 @@ class InMemoryPage extends HydePage
     /**
      * Get the contents that will be saved to disk for this page.
      *
-     * In order to make your virtual page easy to use we provide a few options for how the page can be compiled.
-     * If you want even more control, you can register a macro with the name 'compile' to overload the method,
-     * or simply extend the class and override the method yourself, either in a standard or anonymous class.
+     * Configured literal or closure contents take precedence over Blade views. A view is rendered only when the
+     * configured contents are the empty string. Extend this class and override this method for complete control.
      */
     public function compile(): string
     {
-        if ($this->hasMacro('compile')) {
-            return $this->__call('compile', []);
+        if ($this->contents instanceof Closure || $this->contents !== '') {
+            return $this->getContents();
         }
 
-        if ($this->getBladeView() && ! $this->getContents()) {
+        if ($this->getBladeView() !== '') {
             if (str_ends_with($this->getBladeView(), '.blade.php')) {
                 // If the view key is for a Blade file path, we'll use the anonymous view compiler to compile it.
                 // This allows you to use any arbitrary file, without needing to register its namespace or directory.
@@ -105,49 +112,6 @@ class InMemoryPage extends HydePage
             return View::make($this->getBladeView(), $this->matter->toArray())->render();
         }
 
-        // If there's no macro or view configured, we'll just return the contents as-is.
-        return $this->getContents();
-    }
-
-    /**
-     * Register a macro for the instance.
-     *
-     * Unlike most macros you might be used to, these are not static, meaning they belong to the instance.
-     * If you have the need for a macro to be used for multiple pages, you should create a custom page class instead.
-     */
-    public function macro(string $name, callable $macro): void
-    {
-        $this->macros[$name] = $macro;
-    }
-
-    /**
-     * Determine if a macro with the given name is registered for the instance.
-     */
-    public function hasMacro(string $method): bool
-    {
-        return isset($this->macros[$method]);
-    }
-
-    /**
-     * Dynamically handle macro calls to the class.
-     */
-    public function __call(string $method, array $parameters): mixed
-    {
-        if (! $this->hasMacro($method)) {
-            throw new BadMethodCallException(sprintf(
-                'Method %s::%s does not exist.', static::class, $method
-            ));
-        }
-
-        return $this->callMacro($this->macros[$method], $parameters);
-    }
-
-    protected function callMacro(callable $macro, array $parameters): mixed
-    {
-        if ($macro instanceof Closure) {
-            $macro = $macro->bindTo($this, static::class);
-        }
-
-        return $macro(...$parameters);
+        return '';
     }
 }
--- packages/framework/tests/Unit/Pages/InMemoryPageTest.php
+++ packages/framework/tests/Unit/Pages/InMemoryPageTest.php
@@ -4,9 +4,9 @@ declare(strict_types=1);
 
 namespace Hyde\Framework\Testing\Unit\Pages;
 
-use BadMethodCallException;
 use Hyde\Pages\InMemoryPage;
 use Hyde\Testing\TestCase;
+use TypeError;
 
 /**
  * @see \Hyde\Framework\Testing\Unit\Pages\InMemoryPageUnitTest
@@ -14,133 +14,310 @@ use Hyde\Testing\TestCase;
 #[\PHPUnit\Framework\Attributes\CoversClass(\Hyde\Pages\InMemoryPage::class)]
 class InMemoryPageTest extends TestCase
 {
-    public function testConstructWithContentsString()
+    public function testCanConstructPageWithLiteralContents()
     {
-        $this->assertInstanceOf(InMemoryPage::class, new InMemoryPage('foo', contents: 'bar'));
+        $page = new InMemoryPage('foo', contents: 'bar');
+
+        $this->assertInstanceOf(InMemoryPage::class, $page);
+        $this->assertSame('bar', $page->getContents());
     }
 
-    public function testMakeWithContentsString()
+    public function testCanMakePageWithLiteralContents()
     {
-        $this->assertInstanceOf(InMemoryPage::class, InMemoryPage::make('foo', contents: 'bar'));
-        $this->assertEquals(InMemoryPage::make('foo', contents: 'bar'), new InMemoryPage('foo', contents: 'bar'));
+        $page = InMemoryPage::make('foo', contents: 'bar');
+
+        $this->assertInstanceOf(InMemoryPage::class, $page);
+        $this->assertSame('bar', $page->getContents());
     }
 
-    public function testContentsMethod()
+    public function testGetContentsReturnsLiteralContents()
     {
         $this->assertSame('bar', (new InMemoryPage('foo', contents: 'bar'))->getContents());
     }
 
-    public function testViewMethod()
-    {
-        $this->assertSame('bar', (new InMemoryPage('foo', view: 'bar'))->getBladeView());
-    }
-
-    public function testCompileMethodUsesContentsProperty()
+    public function testCompileReturnsLiteralContents()
     {
         $this->assertSame('bar', (new InMemoryPage('foo', contents: 'bar'))->compile());
     }
 
-    public function testCompileMethodUsesViewProperty()
+    public function testEmptyStringRemainsTheDefaultContents()
     {
-        $this->file('_pages/foo.blade.php', 'bar');
-        $this->assertSame('bar', (new InMemoryPage('foo', view: 'foo'))->compile());
+        $page = new InMemoryPage('foo');
+
+        $this->assertSame('', $page->getContents());
+        $this->assertSame('', $page->compile());
     }
 
-    public function testCompileMethodUsingViewCompileAndFrontMatter()
+    public function testLiteralZeroIsTreatedAsConfiguredContents()
     {
-        $this->file('_pages/foo.blade.php', 'foo {{ $bar }}');
-        $this->assertSame('foo baz', (new InMemoryPage('foo', ['bar' => 'baz'], view: 'foo'))->compile());
+        $this->file('_pages/foo.blade.php', '@php(throw new \RuntimeException("View should not render"))');
+
+        $page = new InMemoryPage('foo', contents: '0', view: 'foo');
+
+        $this->assertSame('0', $page->getContents());
+        $this->assertSame('0', $page->compile());
     }
 
-    public function testCompileMethodPrefersContentsPropertyOverView()
+    public function testCanConstructPageWithRegularClosureContents()
     {
-        $this->file('_pages/foo.blade.php', 'blade');
-        $this->assertSame('contents', (new InMemoryPage('foo', contents: 'contents', view: 'foo'))->compile());
-    }
-
-    public function testCompileMethodCanCompileAnonymousViewFiles()
-    {
-        $this->file('_pages/foo.blade.php', 'blade');
-        $this->assertSame('blade', (new InMemoryPage('foo', view: '_pages/foo.blade.php'))->compile());
-    }
-
-    public function testCompileMethodCanCompileAnonymousViewFilesWithFrontMatter()
-    {
-        $this->file('_pages/foo.blade.php', 'blade {{ $foo }}');
-        $this->assertSame('blade bar', (new InMemoryPage('foo', ['foo' => 'bar'], view: '_pages/foo.blade.php'))->compile());
-    }
-
-    public function testCanCreateInstanceMacros()
-    {
-        $page = InMemoryPage::make('foo');
-
-        $page->macro('foo', function () {
+        $page = new InMemoryPage('foo', contents: function (): string {
             return 'bar';
         });
 
-        /** @noinspection PhpUndefinedMethodInspection */
-        $this->assertSame('bar', $page->foo());
+        $this->assertSame('bar', $page->getContents());
     }
 
-    public function testCanCreateInstanceMacrosUsingCallableObject()
+    public function testCanMakePageWithClosureContents()
     {
-        $page = InMemoryPage::make('foo');
+        $page = InMemoryPage::make('foo', contents: fn (): string => 'bar');
 
-        $page->macro('foo', new class
-        {
-            public function __invoke(): string
-            {
-                return 'bar';
-            }
-        });
-
-        /** @noinspection PhpUndefinedMethodInspection */
-        $this->assertSame('bar', $page->foo());
+        $this->assertSame('bar', $page->getContents());
     }
 
-    public function testCallingMacroWithArguments()
+    public function testArrowFunctionClosureReturnsContents()
     {
-        $page = InMemoryPage::make('foo');
-
-        $page->macro('foo', function (...$args) {
-            return $args;
-        });
-
-        /** @noinspection PhpUndefinedMethodInspection */
-        $this->assertSame(['bar'], $page->foo('bar'));
+        $this->assertSame('bar', (new InMemoryPage('foo', contents: fn (): string => 'bar'))->compile());
     }
 
-    public function testCanUseMacrosToOverloadClassCompileMethod()
+    public function testRegularClosureReturnsContents()
     {
-        $page = InMemoryPage::make('foo');
-
-        $page->macro('compile', function () {
+        $page = new InMemoryPage('foo', contents: function (): string {
             return 'bar';
         });
 
         $this->assertSame('bar', $page->compile());
     }
 
-    public function testCallingUndefinedMacro()
+    public function testStaticClosureRunsUnbound()
     {
-        $this->expectException(BadMethodCallException::class);
-        $this->expectExceptionMessage('Method Hyde\Pages\InMemoryPage::foo does not exist.');
+        $page = new InMemoryPage('foo', contents: static fn (): string => 'bar');
 
-        $page = InMemoryPage::make('foo');
-
-        /** @noinspection PhpUndefinedMethodInspection */
-        $page->foo();
+        $this->assertSame('bar', $page->getContents());
+        $this->assertSame('bar', $page->compile());
     }
 
-    public function testHasMacro()
+    public function testNonStaticClosureCanUseThis()
     {
-        $page = InMemoryPage::make('foo');
+        $page = new InMemoryPage('example.txt', contents: function (): string {
+            return $this->getIdentifier();
+        });
 
-        $page->macro('foo', function () {
+        $this->assertSame('example.txt', $page->getContents());
+    }
+
+    public function testBoundClosureCanAccessPageFrontMatter()
+    {
+        $page = new InMemoryPage('example.txt', ['title' => 'Example'], function (): string {
+            return $this->matter->get('title');
+        });
+
+        $this->assertSame('Example', $page->compile());
+    }
+
+    public function testClosureUsesSubclassAsBindingScope()
+    {
+        $page = new class('foo', contents: function (): string {
+            return $this->subclassContents;
+        }) extends InMemoryPage
+        {
+            protected string $subclassContents = 'subclass';
+        };
+
+        $this->assertSame('subclass', $page->getContents());
+    }
+
+    public function testClosureCanReturnEmptyString()
+    {
+        $page = new InMemoryPage('foo', contents: fn (): string => '');
+
+        $this->assertSame('', $page->getContents());
+        $this->assertSame('', $page->compile());
+    }
+
+    public function testGetContentsPreservesStringReturnTypeEnforcementForClosures()
+    {
+        $this->expectException(TypeError::class);
+
+        (new InMemoryPage('foo', contents: fn () => 123))->getContents();
+    }
+
+    public function testCompilePreservesStringReturnTypeEnforcementForClosures()
+    {
+        $this->expectException(TypeError::class);
+
+        (new InMemoryPage('foo', contents: fn () => 123))->compile();
+    }
+
+    public function testClosureIsLazyWhenPageIsConstructedAndRunsWhenContentsAreRequested()
+    {
+        $invocations = 0;
+        $page = new InMemoryPage('foo', contents: function () use (&$invocations): string {
+            $invocations++;
+
             return 'bar';
         });
 
-        $this->assertTrue($page->hasMacro('foo'));
-        $this->assertFalse($page->hasMacro('bar'));
+        $this->assertSame(0, $invocations);
+        $this->assertSame('bar', $page->getContents());
+        $this->assertSame(1, $invocations);
+    }
+
+    public function testClosureIsLazyWhenPageIsMadeAndRunsWhenPageIsCompiled()
+    {
+        $invocations = 0;
+        $page = InMemoryPage::make('foo', contents: function () use (&$invocations): string {
+            $invocations++;
+
+            return 'bar';
+        });
+
+        $this->assertSame(0, $invocations);
+        $this->assertSame('bar', $page->compile());
+        $this->assertSame(1, $invocations);
+    }
+
+    public function testOneCompilationInvokesClosureExactlyOnce()
+    {
+        $invocations = 0;
+        $page = new InMemoryPage('foo', contents: function () use (&$invocations): string {
+            $invocations++;
+
+            return 'bar';
+        });
+
+        $page->compile();
+
+        $this->assertSame(1, $invocations);
+    }
+
+    public function testClosureContentsAreResolvedAgainForEveryCompilation()
+    {
+        $invocations = 0;
+        $page = new InMemoryPage('foo', contents: function () use (&$invocations): string {
+            return (string) ++$invocations;
+        });
+
+        $this->assertSame('1', $page->compile());
+        $this->assertSame('2', $page->compile());
+        $this->assertSame(2, $invocations);
+    }
+
+    public function testClosureContentsAreResolvedAgainEveryTimeContentsAreRequested()
+    {
+        $invocations = 0;
+        $page = new InMemoryPage('foo', contents: function () use (&$invocations): string {
+            return (string) ++$invocations;
+        });
+
+        $this->assertSame('1', $page->getContents());
+        $this->assertSame('2', $page->getContents());
+        $this->assertSame(2, $invocations);
+    }
+
+    public function testNonEmptyLiteralContentsTakePrecedenceOverNamedView()
+    {
+        $this->file('_pages/foo.blade.php', '@php(throw new \RuntimeException("View should not render"))');
+
+        $this->assertSame('contents', (new InMemoryPage('foo', contents: 'contents', view: 'foo'))->compile());
+    }
+
+    public function testClosureContentsTakePrecedenceOverNamedView()
+    {
+        $this->file('_pages/foo.blade.php', '@php(throw new \RuntimeException("View should not render"))');
+
+        $page = new InMemoryPage('foo', contents: fn (): string => 'contents', view: 'foo');
+
+        $this->assertSame('contents', $page->compile());
+    }
+
+    public function testClosureReturningEmptyStringTakesPrecedenceOverNamedView()
+    {
+        $this->file('_pages/foo.blade.php', '@php(throw new \RuntimeException("View should not render"))');
+
+        $page = new InMemoryPage('foo', contents: fn (): string => '', view: 'foo');
+
+        $this->assertSame('', $page->compile());
+    }
+
+    public function testEmptyLiteralContentsAllowNamedViewToRender()
+    {
+        $this->file('_pages/foo.blade.php', 'bar');
+
+        $this->assertSame('bar', (new InMemoryPage('foo', contents: '', view: 'foo'))->compile());
+    }
+
+    public function testRegisteredBladeViewRenders()
+    {
+        $this->file('_pages/foo.blade.php', 'bar');
+
+        $this->assertSame('bar', (new InMemoryPage('foo', view: 'foo'))->compile());
+    }
+
+    public function testFrontMatterIsPassedToRegisteredBladeView()
+    {
+        $this->file('_pages/foo.blade.php', 'foo {{ $bar }}');
+
+        $this->assertSame('foo baz', (new InMemoryPage('foo', ['bar' => 'baz'], view: 'foo'))->compile());
+    }
+
+    public function testEmptyLiteralContentsAllowArbitraryBladeFileToRender()
+    {
+        $this->file('_pages/foo.blade.php', 'blade');
+
+        $page = new InMemoryPage('foo', contents: '', view: '_pages/foo.blade.php');
+
+        $this->assertSame('blade', $page->compile());
+    }
+
+    public function testArbitraryBladeFileRendersThroughAnonymousViewCompiler()
+    {
+        $this->file('_pages/foo.blade.php', 'blade');
+
+        $this->assertSame('blade', (new InMemoryPage('foo', view: '_pages/foo.blade.php'))->compile());
+    }
+
+    public function testFrontMatterIsPassedToArbitraryBladeFile()
+    {
+        $this->file('_pages/foo.blade.php', 'blade {{ $foo }}');
+
+        $this->assertSame('blade bar', (new InMemoryPage('foo', ['foo' => 'bar'], view: '_pages/foo.blade.php'))->compile());
+    }
+
+    public function testNeitherContentsNorViewCompilesToEmptyString()
+    {
+        $this->assertSame('', (new InMemoryPage('foo'))->compile());
+    }
+
+    public function testViewMethodReturnsConfiguredView()
+    {
+        $this->assertSame('bar', (new InMemoryPage('foo', view: 'bar'))->getBladeView());
+    }
+
+    public function testMacroMethodWasRemoved()
+    {
+        $this->assertFalse(method_exists(InMemoryPage::class, 'macro'));
+    }
+
+    public function testHasMacroMethodWasRemoved()
+    {
+        $this->assertFalse(method_exists(InMemoryPage::class, 'hasMacro'));
+    }
+
+    public function testCustomMacroCallHandlerWasRemoved()
+    {
+        $this->assertFalse(method_exists(InMemoryPage::class, '__call'));
+    }
+
+    public function testSubclassCanOverrideCompileMethod()
+    {
+        $page = new class extends InMemoryPage
+        {
+            public function compile(): string
+            {
+                return 'custom';
+            }
+        };
+
+        $this->assertSame('custom', $page->compile());
     }
 }
